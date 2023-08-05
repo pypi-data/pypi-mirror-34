44bec63fb60a3586bcb3814c04ce6e08e1074e45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:26 +0000

    Add package pyEDFlib/0.1.13/pyEDFlib-0.1.13-cp27-cp27m-win32.whl

---
+++ packages/pyEDFlib/pyEDFlib-0.1.13-cp27-cp27m-win32.whl/pyedflib/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM pyedflib SETUP.PY
+short_version = '0.1.13'
+version = '0.1.13'
+full_version = '0.1.13'
+git_revision = '34328f255795469081a65dd02c144cc914cfb335'
+release = True
+
+if not release:
+    version = full_version

09aadb8e1c46c0dcf205299ea030dca49b281cd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:30 +0000

    Add package pyhttpintercept/0.23.1/pyhttpintercept-0.23.1-py2.py3-none-any.whl

---
+++ packages/pyhttpintercept/pyhttpintercept-0.23.1-py2.py3-none-any.whl/pyhttpintercept/intercept/modifiers/log/request_headers.py
@@ -0,0 +1,27 @@
+# encoding: utf-8
+
+# Description: strip out all personalised recommendations in all timeslots
+# Author: Hywel Thomas
+
+u"""
+================================================================================
+Logs the response.  Use before modifying to see the original response or after
+modifying to see the response sent to the box
+----------------------------------------------------------------------------------
+Filter     : N/A
+Override   : N/A
+Parameters : N/A
+==================================================================================
+"""
+
+import logging_helper
+logging = logging_helper.setup_logging()
+
+
+def modify(request,
+           response,
+           modifier):
+
+    logging.info(u'Request Headers: {r}'.format(r=request.headers))
+
+    return response

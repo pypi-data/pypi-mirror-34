af9ad997bbae5ce2f93859d2c68b881d64be65ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:11 +0000

    Add package python-transcoded/0.4.2/python-transcoded-0.4.2.tar.gz

---
+++ packages/python-transcoded/python-transcoded-0.4.2.tar.gz/python-transcoded-0.4.2/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='python-transcoded',
+      version='0.4.2',
+      description='Daemon for transcoding audio/video with an http API written in python',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author='Martijn Braam',
+      author_email='martijn@brixit.nl',
+      license='MIT',
+      url='https://gitlab.com/MartijnBraam/python-transcoded',
+      packages=['transcoded'],
+      python_requires='>=3',
+      install_requires=['Werkzeug', 'requests'],
+      classifiers=(
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.5",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: POSIX :: Linux",
+          "Intended Audience :: System Administrators",
+          "Topic :: Multimedia :: Video :: Conversion"
+      ),
+      project_urls={
+          'Tracker': 'https://gitlab.com/MartijnBraam/python-transcoded/issues',
+          'Documentation': 'https://python-transcoded.readthedocs.io/en/latest/'
+      },
+      package_data={
+          'transcoded': ['example-config.ini']
+      },
+      entry_points={
+          'console_scripts': [
+              'transcoded=transcoded.__main__:main'
+          ]
+      }
+      )

ddba286d735028cc33d3fe8a16e0c86be8672116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:18 +0000

    Add package orcid/1.0.3/orcid-1.0.3.tar.gz

---
+++ packages/orcid/orcid-1.0.3.tar.gz/orcid-1.0.3/orcid/__init__.py
@@ -0,0 +1,6 @@
+"""Initialization file for python-orcid."""
+
+from .orcid import MemberAPI
+from .orcid import PublicAPI
+
+__all__ = ('MemberAPI', 'PublicAPI')
---
+++ packages/orcid/orcid-1.0.3.tar.gz/orcid-1.0.3/setup.py
@@ -0,0 +1,50 @@
+"""Setup file for python-orcid."""
+
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        raise SystemExit(errno)
+
+setup(author='Mateusz Susik',
+      author_email='mateuszsusik@protonmail.ch',
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet',
+        'Topic :: Utilities'
+      ],
+      cmdclass={'test': PyTest},
+      description='A python wrapper over the ORCID API',
+      install_requires=['html5lib', 'beautifulsoup4', 'requests', 'simplejson', 'lxml'],
+      keywords=['orcid', 'api', 'wrapper'],
+      license='BSD',
+      long_description=open('README.rst', 'r').read(),
+      name='orcid',
+      packages=['orcid'],
+      tests_require=['pytest', 'coverage', 'httpretty'],
+      url='https://github.com/ORCID/python-orcid',
+      version='1.0.3'
+      )

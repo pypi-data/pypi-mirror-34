ba08fc98b1b0babd22b86e7a082ca827a0036b3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:44:41 +0000

    Add package collective.folderorder/1.4.2/collective.folderorder-1.4.2.tar.gz

---
+++ packages/collective.folderorder/collective.folderorder-1.4.2.tar.gz/collective.folderorder-1.4.2/setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup, find_packages
+
+version = '1.4.2'
+shortdesc = 'Set sort order of plone folders'
+longdesc = open('README.rst').read() + '\n'
+longdesc += open('CHANGES.rst').read() + '\n'
+longdesc += open('LICENSE.rst').read()
+
+setup(
+    name='collective.folderorder',
+    version=version,
+    description=shortdesc,
+    long_description=longdesc,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+        "Framework :: Plone",
+    ],
+    keywords='',
+    author='Jens Klein    ',
+    author_email='jens@bluedynamics.com',
+    url=u'http://github.com/collective/collective.folderorder',
+    license='GPLv2',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['collective'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'Products.CMFPlone',
+        'Products.statusmessages',
+        'plone.folder',
+        'yafowil',
+        'yafowil.plone',
+        'yafowil.yaml',
+        'zope.component',
+        'zope.i18nmessageid',
+        'Zope2',  # For Products.Five
+    ],
+    extras_require={
+        'test': [
+            'plone.app.robotframework',
+            'plone.app.testing',
+        ],
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)

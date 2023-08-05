87e57e27e6490cbd10eb34ff478f034bf00d3f3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:15 +0000

    Add package GFD-basisChangeTools/1.0.0/GFD_basisChangeTools-1.0.0.tar.gz

---
+++ packages/GFD-basisChangeTools/GFD_basisChangeTools-1.0.0.tar.gz/GFD_basisChangeTools-1.0.0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='GFD_basisChangeTools',
+      version='1.0.0',
+      author = "Alexandros Avdis",
+      provides=['GFD_basisChangeTools'],
+      description='Tensor basis change tools for geophysical fluid dynamics',
+      license='GPLv3',
+      packages=['GFD_basisChangeTools'],
+     )

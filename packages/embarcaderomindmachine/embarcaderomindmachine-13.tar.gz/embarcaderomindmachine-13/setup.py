4acbe4175e076bc22e9efc8097e68a1a613c8eae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:43 +0000

    Add package embarcaderomindmachine/13/embarcaderomindmachine-13.tar.gz

---
+++ packages/embarcaderomindmachine/embarcaderomindmachine-13.tar.gz/embarcaderomindmachine-13/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+version = "13"
+
+setup(  name='embarcaderomindmachine',
+        version=version,
+        description='The extensible framework for running Github bot flocks.',
+        url='https://pages.charlesreid1.com/embarcadero-mind-machine',
+        author='charlesreid1',
+        author_email='embarcadero@charlesreid1.com',
+        test_suite='nose.collector',
+        tests_require=['nose'],
+        license='MIT',
+        packages=['embarcaderomindmachine'],
+        install_requires=[
+            'boringmindmachine==13',
+            'PyGithub>=1.39'
+        ],
+        zip_safe=False)
+

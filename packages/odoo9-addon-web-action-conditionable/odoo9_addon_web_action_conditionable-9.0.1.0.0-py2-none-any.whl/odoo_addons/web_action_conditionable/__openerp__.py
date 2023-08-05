b031f62178871026745ab35ea4d4f970333766ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:17 +0000

    Add package odoo9-addon-web-action-conditionable/9.0.1.0.0/odoo9_addon_web_action_conditionable-9.0.1.0.0-py2-none-any.whl

---
+++ packages/odoo9-addon-web-action-conditionable/odoo9_addon_web_action_conditionable-9.0.1.0.0-py2-none-any.whl/odoo_addons/web_action_conditionable/__openerp__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": 'Web Action Conditionable',
+    "version": "9.0.1.0.0",
+    "depends": [
+        'web',
+    ],
+    'data': [
+        'views/view.xml'
+    ],
+    "author": "Cristian Salamea,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+}

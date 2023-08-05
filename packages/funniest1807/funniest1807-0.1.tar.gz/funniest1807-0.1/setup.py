8631d7a8eef6a0784def02640351de3e23f3890a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:24 +0000

    Add package funniest1807/0.1/funniest1807-0.1.tar.gz

---
+++ packages/funniest1807/funniest1807-0.1.tar.gz/funniest1807-0.1/setup.py
@@ -0,0 +1,2 @@
+from setuptools import setup
+setup(name='funniest1807',version='0.1',description='This is the funniest thing',author='XXX',author_email='XXX@example.com',license='MIT',zip_safe=False)

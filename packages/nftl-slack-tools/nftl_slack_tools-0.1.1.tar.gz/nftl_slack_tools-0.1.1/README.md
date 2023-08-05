eda95f2229eb5515f816049029ca464776dcbf4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:35 +0000

    Add package nftl-slack-tools/0.1.1/nftl_slack_tools-0.1.1.tar.gz

---
+++ packages/nftl-slack-tools/nftl_slack_tools-0.1.1.tar.gz/nftl_slack_tools-0.1.1/README.md
@@ -0,0 +1,27 @@
+# Library: nftl-slack-tools
+
+This is a tool for Slack API handling.
+Source documentation of api is available [here](https://api.slack.com/)
+
+[PyPI project page](https://pypi.org/project/nftl-slack-tools/)
+
+
+# Installation
+
+```sh
+pip install nftl-slack-tools
+```
+
+# Usage
+
+```python
+    slack = SlackClient(token='xoxp-...')
+    channel = slack.get_channels_api().create('new_order')
+
+    if channel:
+        print 'Yupi!'
+```
+
+# Deployment how to
+
+Available [here](https://packaging.python.org/tutorials/packaging-projects/)
---
+++ packages/nftl-slack-tools/nftl_slack_tools-0.1.1.tar.gz/nftl_slack_tools-0.1.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nftl_slack_tools",
+    version="0.1.1",
+    author="Grzegorz Pawełczuk",
+    author_email="grzegorz.pawelczuk@nftlearning.com",
+    description="Tool 4 partial Slack API handling",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)

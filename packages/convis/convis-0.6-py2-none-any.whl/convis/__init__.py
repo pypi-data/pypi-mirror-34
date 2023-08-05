0264e99785ecedf048610335e2542ebbde4b8457
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:06 +0000

    Add package convis/0.6/convis-0.6-py2-none-any.whl

---
+++ packages/convis/convis-0.6-py2-none-any.whl/convis/__init__.py
@@ -0,0 +1,32 @@
+from __future__ import print_function
+try:
+    import torch
+except ImportError:
+    print('Please download the torch package from https://pytorch.org')
+    raise
+
+__version__ = '0.6'
+
+from . import base
+from . import retina
+from . import models
+from . import samples
+from . import streams
+from . import variables
+from . import numerical_filters
+from . import utils
+from . import analysis
+from . import kernels
+
+from .base import Layer, prepare_input, Output, Runner
+from .variable_describe import help, describe, describe_text, describe_dict, describe_html, animate, animate_to_video
+from .variables import Parameter, default_resolution
+from .utils import plot, plot_5d_matshow, plot_5d_time
+
+def main():
+    import sys
+    print(len(sys.argv))
+
+if __name__ == "__main__":
+    main()
+    sys.exit()

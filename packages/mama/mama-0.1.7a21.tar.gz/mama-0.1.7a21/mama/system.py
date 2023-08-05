b970246848044eedd9e3aa50774fe93e0ea9b51c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:05 +0000

    Add package mama/0.1.7a21/mama-0.1.7a21.tar.gz

---
+++ packages/mama/mama-0.1.7a21.tar.gz/mama-0.1.7a21/mama/system.py
@@ -0,0 +1,21 @@
+import sys, os
+
+## Always flush to properly support Jenkins
+def console(s): print(s, flush=True)
+
+def execute(command, echo=False):
+    if echo: console(command)
+    retcode = os.system(command)
+    if retcode != 0:
+        raise Exception(f'{command} failed with return code {retcode}')
+
+is_windows = sys.platform == 'win32'
+is_linux   = sys.platform.startswith('linux')
+is_macos   = sys.platform == 'darwin'
+if not (is_windows or is_linux or is_macos):
+    raise RuntimeError(f'MamaBuild unsupported platform {sys.platform}')
+
+class System:
+    windows = is_windows
+    linux   = is_linux
+    macos   = is_macos

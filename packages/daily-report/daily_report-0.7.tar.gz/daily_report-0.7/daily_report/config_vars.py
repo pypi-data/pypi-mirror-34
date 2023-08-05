9bb3f5f0f97b5d524cd268183d671f3ea65e25b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:34 +0000

    Add package daily-report/0.7/daily_report-0.7.tar.gz

---
+++ packages/daily-report/daily_report-0.7.tar.gz/daily_report-0.7/daily_report/config_vars.py
@@ -0,0 +1,15 @@
+# State names:
+PUNISH_STATE = "Punish"
+REWARD_STATE = "Reward"
+INVALID_STATE = "Invalid"
+RESPONSE_TIME_STATE = "WaitResponse"
+
+# Variable names:
+STAGE_NUMBER = "STAGE_NUMBER"
+REWARD_SIDE = "REWARD_SIDE"
+
+## Parameters:
+
+# Response time outlier, in seconds:
+R_OUTLIER = 2
+

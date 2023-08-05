59902a2ac22b97083042d5b2d3fdc401e3270b15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:05 +0000

    Add package django-easy-scoping/1.12/django_easy_scoping-1.12.tar.gz

---
+++ packages/django-easy-scoping/django_easy_scoping-1.12.tar.gz/django_easy_scoping-1.12/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name='django_easy_scoping',
+    version='1.12',
+    author='Robert Wells',
+    author_email='wellsroberte@gmail.com',
+    packages=setuptools.find_packages(),
+    url='https://github.com/net-prophet/django-easy-scoping',
+    description='A mixin to allow users to create scopes on Django models.',
+    license='BSD',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    install_requires=[
+        'Django>=1.11,<3.0',
+    ],
+)

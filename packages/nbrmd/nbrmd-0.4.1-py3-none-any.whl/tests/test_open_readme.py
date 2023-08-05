5a5d8ed13734e9cfc8dbc5966642c52fa0dd9083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:46 +0000

    Add package nbrmd/0.4.1/nbrmd-0.4.1-py3-none-any.whl

---
+++ packages/nbrmd/nbrmd-0.4.1-py3-none-any.whl/tests/test_open_readme.py
@@ -0,0 +1,16 @@
+import nbrmd
+import pytest
+import os
+import shutil
+
+
+@pytest.fixture()
+def readme():
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                        '..', 'README.md')
+
+
+def test_open_readme(readme, tmpdir):
+    rmd_file = str(tmpdir.join('README.Rmd'))
+    shutil.copyfile(readme, rmd_file)
+    nb = nbrmd.readf(rmd_file)

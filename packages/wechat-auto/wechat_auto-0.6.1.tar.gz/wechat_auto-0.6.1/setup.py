c9a0db513bd0695de5d171773e89d8c0d5a4a75e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:13 +0000

    Add package wechat-auto/0.6.1/wechat_auto-0.6.1.tar.gz

---
+++ packages/wechat-auto/wechat_auto-0.6.1.tar.gz/wechat_auto-0.6.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r",encoding="utf8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wechat_auto",
+    version="0.6.1",
+    author="Qinluo",
+    author_email="shi12li12@gmail.com",
+    description="A wechat auto reply robot",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/true1023/wechat_auto",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)

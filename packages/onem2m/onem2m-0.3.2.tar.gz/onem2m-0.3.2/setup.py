fca7aacd63ac293aac5e1a030a272da6d242bf4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:44:43 +0000

    Add package onem2m/0.3.2/onem2m-0.3.2.tar.gz

---
+++ packages/onem2m/onem2m-0.3.2.tar.gz/onem2m-0.3.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+ 
+setup(name='onem2m',
+    version='0.3.2',
+    url='https://github.com/kimkeehwan/onem2m_adn_lib_python',
+    license='MIT',
+    author='Kim keehwan',
+    author_email='kimkeehwan@handysoft.co.kr',
+    description='oneM2M ADN Client Lib',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5'
+    ],
+    packages=find_packages(exclude=['tests']),
+    long_description=open('README.md').read(),
+    zip_safe=False,
+    setup_requires=['configparser>=3.5.0', 'requests>=2.18.4'])

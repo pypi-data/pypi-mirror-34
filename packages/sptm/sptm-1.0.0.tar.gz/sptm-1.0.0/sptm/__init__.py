0ca7a3602f8d9f6959ab466b038cde0983877cc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:02 +0000

    Add package sptm/1.0.0/sptm-1.0.0.tar.gz

---
+++ packages/sptm/sptm-1.0.0.tar.gz/sptm-1.0.0/sptm/__init__.py
@@ -0,0 +1,6 @@
+from .model import Model
+from .postprocess import TopicDistanceMap
+from .preprocess import Corpus
+from .utils import force_unicode
+from .conditional import ConditionalMatrix
+from .inference import Inferencer

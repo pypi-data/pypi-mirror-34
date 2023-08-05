a97ab83b42ca466e30e016d8a784c470230fa596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:14 +0000

    Add package ubindex/0.1.1/ubindex-0.1.1-py2-none-any.whl

---
+++ packages/ubindex/ubindex-0.1.1-py2-none-any.whl/ubindex/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+__version__ = '0.1.1'
+__author__ = 'tushare.org'
+
+from ubindex.dataapi import DataAPI

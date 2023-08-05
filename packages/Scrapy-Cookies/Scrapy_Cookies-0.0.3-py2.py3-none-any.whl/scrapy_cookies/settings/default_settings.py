a558a5382f200ea8f3b40a7d54437d51ffc34ed3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:17 +0000

    Add package Scrapy-Cookies/0.0.3/Scrapy_Cookies-0.0.3-py2.py3-none-any.whl

---
+++ packages/Scrapy-Cookies/Scrapy_Cookies-0.0.3-py2.py3-none-any.whl/scrapy_cookies/settings/default_settings.py
@@ -0,0 +1,18 @@
+COOKIES_ENABLED = True
+COOKIES_DEBUG = False
+
+COOKIES_PERSISTENCE = False
+COOKIES_PERSISTENCE_DIR = 'cookies'
+
+# ------------------------------------------------------------------------------
+# IN MEMORY STORAGE
+# ------------------------------------------------------------------------------
+
+COOKIES_STORAGE = 'scrapy_cookies.storage.in_memory.InMemoryStorage'
+
+# ------------------------------------------------------------------------------
+# SQLITE STORAGE
+# ------------------------------------------------------------------------------
+
+# COOKIES_STORAGE = 'scrapy_cookies.storage.sqlite.SQLiteStorage'
+# COOKIES_SQLITE_DATABASE = ':memory:'

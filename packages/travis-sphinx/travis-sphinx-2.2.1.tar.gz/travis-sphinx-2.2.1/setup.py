6e6ad5dc9d3b85c965e4c95c521e08900c3c511f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:53 +0000

    Add package travis-sphinx/2.2.1/travis-sphinx-2.2.1.tar.gz

---
+++ packages/travis-sphinx/travis-sphinx-2.2.1.tar.gz/travis-sphinx-2.2.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'travis-sphinx',
+    version = '2.2.1',
+    description='Manages building sphinx documentation and deploying documentation to gh-pages via travis',
+    author='Grant Mercer',
+    author_email='gmercer015@gmail.com',
+    url='https://github.com/syntaf/travis-sphinx',
+    download_url='https://github.com/Syntaf/travis-sphinx/archive/master.zip',
+    keywords=['documentation', 'travis', 'python', 'deploy'],
+    packages=find_packages(exclude=('tests',)),
+    py_modules = ['travis_sphinx'],
+    entry_points = {
+        'console_scripts' : ['travis-sphinx=travis_sphinx.main:main']
+    },
+    install_requires=[
+        'sphinx',
+        'click',
+        'ghp-import',
+    ],
+    classifiers = [
+        'Topic :: Software Development :: Documentation',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)

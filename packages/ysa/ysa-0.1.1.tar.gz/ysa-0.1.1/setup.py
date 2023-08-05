20b867bfb36c4ab2b8540a1582f16a62e3654249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:52 +0000

    Add package ysa/0.1.1/ysa-0.1.1.tar.gz

---
+++ packages/ysa/ysa-0.1.1.tar.gz/ysa-0.1.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='ysa',
+    version='0.1.1',
+    author='shailyn ortiz(@M_abaader)',
+    author_email='sortizjim@gmail.com',
+    description='python client for the YSA api',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/sjortiz/YSA-PY',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    classifiers=(
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ),
+)

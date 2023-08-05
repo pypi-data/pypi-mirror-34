2287b319e01481349b56bce3b59bd076e6353644
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:46 +0000

    Add package EIA-python/1.20/EIA-python-1.20.tar.gz

---
+++ packages/EIA-python/EIA-python-1.20.tar.gz/EIA-python-1.20/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='EIA-python',
+    version='1.20',
+    packages=['eia'],
+    url='https://github.com/mra1385/EIA-python',
+    license='BSD',
+    author='Mike Azar',
+    author_email='mikeazar@gmail.com',
+    description='Python Wrapper for the Energy Information Administration (EIA) API'
+)

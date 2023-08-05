6c269fbbce01df3166ee369ef96d3a9b938404e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:13 +0000

    Add package auquan-toolbox/1.1.1/auquan_toolbox-1.1.1-py3-none-any.whl

---
+++ packages/auquan-toolbox/auquan_toolbox-1.1.1-py3-none-any.whl/backtester/features/moving_sum.py
@@ -0,0 +1,26 @@
+from backtester.features.feature import Feature
+
+
+class MovingSumFeature(Feature):
+
+    @classmethod
+    def computeForInstrument(cls, updateNum, time, featureParams, featureKey, instrumentManager):
+        instrumentLookbackData = instrumentManager.getLookbackInstrumentFeatures()
+        dataDf = instrumentLookbackData.getFeatureDf(featureParams['featureName'])
+        return dataDf[-featureParams['period']:].sum()
+
+    @classmethod
+    def computeForMarket(cls, updateNum, time, featureParams, featureKey, currentMarketFeatures, instrumentManager):
+        lookbackDataDf = instrumentManager.getDataDf()
+        data = lookbackDataDf[featureParams['featureName']]
+        return data[-featureParams['period']:].sum()
+
+
+    @classmethod
+    def computeForInstrumentData(cls, updateNum, featureParams, featureKey, featureManager):
+        data = featureManager.getFeatureDf(featureParams['featureName'])
+        if data is None:
+            logWarn("[%d] instrument data for \"%s\" is not available, can't calculate \"%s\"" % (updateNum, featureParams['featureName'], featureKey))
+            return None
+        movingSum = data.rolling(window=featureParams['period'], min_periods=1).sum()
+        return movingSum

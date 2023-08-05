94468905ecb9376f5d49db06926d30a2ed45195b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:22 +0000

    Add package cutt-sd/0.0.4/cutt_sd-0.0.4.tar.gz

---
+++ packages/cutt-sd/cutt_sd-0.0.4.tar.gz/cutt_sd-0.0.4/cutt_sd/checker.py
@@ -0,0 +1,42 @@
+import socket
+import threading
+from contextlib import closing
+import cutt_sd
+
+servers = dict()
+
+
+def add_services(ip, port, env, service):
+    key = '%s:%s:%s' % (ip, port, env)
+    if key not in servers:
+        servers[key] = set()
+
+    servers[key].add(service)
+
+
+def remove_services(ip, port, env, service):
+    key = '%s:%s:%s' % (ip, port, env)
+    if key not in servers:
+        servers[key] = set()
+
+    servers[key].remove(service)
+
+    if len(servers[key]) == 0:
+        del servers[key]
+
+
+def check_all():
+    for key, values in servers.items():
+        with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+            host, port, env = key.split(':')
+            if sock.connect_ex((host, int(port))) == 0:
+                pass
+            else:
+                del servers[key]
+                for x in values:
+                    cutt_sd.unregister_api(x, host, port, env)
+
+
+timer = threading.Timer(60, check_all, )
+timer.daemon = True
+timer.start()
---
+++ packages/cutt-sd/cutt_sd-0.0.4.tar.gz/cutt_sd-0.0.4/cutt_sd/zk.py
@@ -0,0 +1,78 @@
+import random
+from kazoo.client import KazooClient
+from kazoo.exceptions import NodeExistsError, NoNodeError
+
+from cutt_sd.config import *
+
+zk = None
+
+
+def init(zookeeper='127.0.0.1:2181'):
+    global zk
+    if not zk:
+        zk = KazooClient(hosts=zookeeper)
+        zk.start()
+
+
+def register_api(uri, ip, port, env=DEFAULT_ENV, zookeeper=None):
+    global zk
+    if not zk:
+        if zookeeper:
+            init(zookeeper)
+        else:
+            raise RuntimeError('You must init zk first')
+    if uri[0] == '/':
+        uri = uri[0:]
+
+    path = '/%s/services/%s' % (env, uri)
+    # zk.ensure_path(path)
+    try:
+        zk.create('%s/%s:%s' % (path, ip, port), ('%s:%s' % (ip, port)).encode('utf-8'), makepath=True, ephemeral=True)
+        from cutt_sd import checker
+        checker.add_services(ip, port, env, uri)
+    except NodeExistsError:
+        pass
+
+
+def unregister_api(uri, ip, port, env=DEFAULT_ENV, zookeeper=None):
+    global zk
+    if not zk:
+        if zookeeper:
+            init(zookeeper)
+        else:
+            raise RuntimeError('You must init zk first')
+    if uri[0] == '/':
+        uri = uri[0:]
+
+    path = '/%s/services/%s' % (env, uri)
+    # zk.ensure_path(path)
+    try:
+        zk.delete('%s/%s:%s' % (path, ip, port))
+        from cutt_sd import checker
+        checker.remove_services(ip, port, env, uri)
+    except NoNodeError:
+        pass
+
+
+def find_api(uri, env=DEFAULT_ENV, zookeeper=None):
+    global zk
+    if not zk:
+        if zookeeper:
+            init(zookeeper)
+        else:
+            raise RuntimeError('You must init zk first')
+
+    if not uri:
+        return None
+
+    if uri[0] == '/':
+        uri = uri[0:]
+
+    path = '/%s/services/%s' % (env, uri)
+    try:
+        children = zk.get_children(path)
+        if children:
+            data, node = zk.get('%s/%s' % (path, random.choice(children)))
+            return data.decode('utf-8') if data else None
+    except:
+        return None

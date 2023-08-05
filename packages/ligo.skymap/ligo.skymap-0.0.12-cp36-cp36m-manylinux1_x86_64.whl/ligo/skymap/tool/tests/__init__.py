ba7d9f7565417a12fefca0a4a714d58739802c7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:30 +0000

    Add package ligo.skymap/0.0.12/ligo.skymap-0.0.12-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/ligo.skymap/ligo.skymap-0.0.12-cp36-cp36m-manylinux1_x86_64.whl/ligo/skymap/tool/tests/__init__.py
@@ -0,0 +1,16 @@
+import pkg_resources
+
+dist = 'ligo.skymap'
+group = 'console_scripts'
+
+__all__ = ('entry_points', 'run_entry_point')
+
+entry_points = sorted(pkg_resources.get_entry_map(dist, group).keys())
+
+
+def run_entry_point(name, *args):
+    main = pkg_resources.load_entry_point(dist, group, name)
+    try:
+        main(args)
+    except SystemExit as e:
+        assert e.code == 0

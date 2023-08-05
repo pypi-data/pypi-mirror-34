2ed6cd7789fccb5001caa595f3aaae76df04a3ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:48 +0000

    Add package jsons/0.2.3/jsons-0.2.3.tar.gz

---
+++ packages/jsons/jsons-0.2.3.tar.gz/jsons-0.2.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(name='jsons',
+      version='0.2.3',
+      author='Ramon Hagenaars',
+      description='For serializing Python objects to JSON and back',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/ramonhagenaars/jsons',
+      packages=['jsons'],
+      zip_safe=False,
+      classifiers=(
+          'Programming Language :: Python :: 3',
+          'Operating System :: OS Independent'
+      )
+)

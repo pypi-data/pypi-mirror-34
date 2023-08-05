804abf4f2de178c2a3b63b5d65cb91978ae4cc07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:20 +0000

    Add package nbsrc/0.4.0/nbsrc-0.4.0.tar.gz

---
+++ packages/nbsrc/nbsrc-0.4.0.tar.gz/nbsrc-0.4.0/HISTORY.rst
@@ -0,0 +1,29 @@
+.. :changelog:
+
+Release History
+---------------
+
+dev
++++
+
+0.4.0 (2018-07-18)
++++++++++++++++++++
+
+**Improvements**
+
+- lighter, pep8 compliant notebook format
+- screenshots in readme
+
+**Bug fixes**
+
+- tests fixed on older python
+
+
+0.3.0 (2018-07-17)
++++++++++++++++++++
+
+**Improvements**
+
+- nbsrc script that converts Jupyter notebooks to R and python scripts
+- src plugin for nbconvert
+- Initial documentation (readme)
---
+++ packages/nbsrc/nbsrc-0.4.0.tar.gz/nbsrc-0.4.0/nbsrc/__init__.py
@@ -0,0 +1,17 @@
+"""Jupyter notebooks as python or R script
+
+Use this module to read or write Jupyter notebooks as R or python scripts
+(methods 'read', 'reads', 'write', 'writes')
+
+Use the 'nbsrc' conversion script to convert Jupyter notebooks from/to
+R or Python scripts
+"""
+
+from .nbsrc import readme
+
+try:
+    from .srcexporter import PyNotebookExporter
+    from .srcexporter import RNotebookExporter
+except ImportError as e:
+    PyNotebookExporter = str(e)
+    RNotebookExporter = str(e)

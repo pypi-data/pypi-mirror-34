6fc01335dcea2dd14dd20c070939d07adddffdc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:35 +0000

    Add package adlib/1.0.2/adlib-1.0.2.tar.gz

---
+++ packages/adlib/adlib-1.0.2.tar.gz/adlib-1.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='adlib',
+    version='1.0.2',
+    description='Game-theoretic adversarial machine learning library providing '
+                'a set of learner and adversary modules.',
+    url='https://github.com/vu-aml/adlib',
+    license='MIT',
+    author='Alexandra, Alexander deGroot, Ethan Raymond, Jiazhi Zhang, '
+           'Bumsu Jung, Yevgeniy Vorobeychik, Matthew Sedam',
+    author_email='yevgeniy.vorobeychik@vanderbilt.edu',
+    platforms=['any'],
+    packages=find_packages(),
+    install_requires=['pyyaml'],
+)

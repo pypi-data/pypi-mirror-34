ef75e2653c0f845e84c0526ecb50acaf94325f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:16 +0000

    Add package GramophoneTools/0.5.17/GramophoneTools-0.5.17-py3-none-any.whl

---
+++ packages/GramophoneTools/GramophoneTools-0.5.17-py3-none-any.whl/GramophoneTools/__init__.py
@@ -0,0 +1,10 @@
+from . import Comms
+from . import Recorder
+from . import LinMaze
+
+
+__version__ = '0.5.17'
+
+def print_version():
+    global __version__
+    print('Gramophone Tools version:', __version__)

1fea49ac2c1152489b46ba988e34079beb41350c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:13 +0000

    Add package phylo-treetime/0.4.1/phylo_treetime-0.4.1-py3-none-any.whl

---
+++ packages/phylo-treetime/phylo_treetime-0.4.1-py3-none-any.whl/treetime/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import print_function, division, absolute_import
+from treetime.treeanc import TreeAnc
+from treetime.treetime import TreeTime, plot_vs_years
+from treetime.clock_tree import ClockTree
+from treetime.treetime import ttconf as treetime_conf
+from treetime.gtr import GTR
+from treetime.merger_models import Coalescent

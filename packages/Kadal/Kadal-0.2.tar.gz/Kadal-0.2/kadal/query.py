9dd685f14cf9b76c2a700561ad027ea14b81ed86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:50 +0000

    Add package Kadal/0.2/Kadal-0.2.tar.gz

---
+++ packages/Kadal/Kadal-0.2.tar.gz/Kadal-0.2/kadal/query.py
@@ -0,0 +1,140 @@
+MEDIA_SEARCH = """
+query ($search: String, $type: MediaType) {
+  Media(search: $search, type: $type) {
+    id
+    type
+    format
+    title {
+      native
+      english
+    }
+    synonyms
+    status
+    description
+    startDate {
+      year
+      month
+      day
+    }
+    endDate {
+      year
+      month
+      day
+    }
+    episodes
+    chapters
+    volumes
+    coverImage {
+      large
+    }
+    genres
+    averageScore
+    siteUrl
+  }
+}
+"""
+
+MEDIA_BY_ID = """
+query ($id: Int, $type: MediaType) {
+  Media(id: $id, type: $type) {
+    id
+    type
+    format
+    title {
+      native
+      english
+    }
+    synonyms
+    status
+    description
+    startDate {
+      year
+      month
+      day
+    }
+    endDate {
+      year
+      month
+      day
+    }
+    episodes
+    chapters
+    coverImage {
+      large
+    }
+    genres
+    averageScore
+  }
+}
+"""
+
+MEDIA_PAGED = """
+query ($id: Int, $page: Int, $perPage: Int, $search: String, $type: MediaType) {
+  Page(page: $page, perPage: $perPage) {
+    media(id: $id, search: $search, type: $type) {
+      id
+      type
+      format
+      title {
+        native
+        english
+      }
+      synonyms
+      status
+      description
+      startDate {
+        year
+        month
+        day
+      }
+      endDate {
+        year
+        month
+        day
+      }
+      episodes
+      chapters
+      volumes
+      coverImage {
+        large
+      }
+      genres
+      averageScore
+      siteUrl
+      popularity
+    }
+  }
+}
+"""
+
+USER_SEARCH = """
+query ($search: String) {
+  User(search: $search) {
+    id
+    name
+    html_about: about(asHtml: true)
+    about
+    avatar {
+      large
+    }
+    bannerImage
+    siteUrl
+  }
+}
+"""
+
+USER_BY_ID = """
+query ($id: Int) {
+  User(id: $id) {
+    id
+    name
+    html_about: about(asHtml: true)
+    about
+    avatar {
+      large
+    }
+    bannerImage
+    siteUrl
+  }
+}
+"""

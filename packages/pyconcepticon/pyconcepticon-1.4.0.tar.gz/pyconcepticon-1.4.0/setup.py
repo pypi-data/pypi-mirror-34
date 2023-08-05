0913e0e88d3db4f4a71bc5e64e64e5a8a21aa67f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:43 +0000

    Add package pyconcepticon/1.4.0/pyconcepticon-1.4.0.tar.gz

---
+++ packages/pyconcepticon/pyconcepticon-1.4.0.tar.gz/pyconcepticon-1.4.0/setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pyconcepticon',
+    version='1.4.0',
+    description='programmatic access to concepticon-data',
+    long_description='',
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+    author='',
+    author_email='forkel@shh.mpg.de',
+    url='',
+    keywords='data linguistics',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'csvw',
+        'clldutils>=2.1.0',
+        'bibtexparser',
+        'tabulate',
+        'attrs',
+        'cdstarcat',
+    ],
+    extras_require={
+        'dev': [
+            'tox',
+            'flake8',
+            'wheel',
+            'twine',
+        ],
+        'test': [
+            'mock',
+            'pytest>=3.1',
+            'pytest-mock',
+            'pytest-cov',
+            'coverage>=4.2',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'concepticon=pyconcepticon.__main__:main',
+        ]
+    },
+    test_suite="pyconcepticon")

38619d502e2cc351c6d16abdcfce0156d5f0e90a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:51 +0000

    Add package pysniff/0.0.2/pysniff-0.0.2.tar.gz

---
+++ packages/pysniff/pysniff-0.0.2.tar.gz/pysniff-0.0.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'pysniff',
+    version = '0.0.2',
+    description = 'A simple network sniffing application',
+    author = 'zWaR',
+    author_email = 'zwar@sharklasers.com',
+    classifiers = [
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7'
+    ],
+    keywords = 'network sniffer monitoring proxy',
+    packages = ['pysniff', 'pysniff/libs'],
+    setup_requires = ['pytest-runner'],
+    install_requires = [
+        'appjar'
+    ],
+    url="https://github.com/zWaR/pysniff",
+    project_urls={
+        "Source Code": "https://github.com/zWaR/pysniff.git",
+        "Bug Tracker": "https://github.com/zWaR/pysniff/issues",
+        "Documentation": "https://github.com/zWaR/pysniff"
+    },
+    entry_points = {
+        'console_scripts': [
+            'pysniff=pysniff.main:main'
+        ]
+    }
+)

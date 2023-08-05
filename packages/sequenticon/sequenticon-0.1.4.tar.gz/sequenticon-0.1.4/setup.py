cd781328893e3a2be6a34b4557a5e34fe487b0b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:23 +0000

    Add package sequenticon/0.1.4/sequenticon-0.1.4.tar.gz

---
+++ packages/sequenticon/sequenticon-0.1.4.tar.gz/sequenticon-0.1.4/setup.py
@@ -0,0 +1,19 @@
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+
+exec(open('sequenticon/version.py').read()) # loads __version__
+
+setup(
+    name='sequenticon',
+    version=__version__,
+    author='Zulko',
+    description='Generate human-friendly icons from DNA sequences',
+    long_description=open('pypi-readme.rst').read(),
+    license='see LICENSE.txt',
+    keywords="DNA sequence barcoding sequenticon identicon hash",
+    packages=find_packages(exclude='docs'),
+    include_package_data=True,
+    install_requires=["Biopython", "pydenticon", "snapgene_reader",
+                      "flametree", "pdf_reports"])

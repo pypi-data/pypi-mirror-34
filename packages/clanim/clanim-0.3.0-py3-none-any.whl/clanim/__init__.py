f122da674f11e5c156f56e6e7e16b6a08ec598e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:27 +0000

    Add package clanim/0.3.0/clanim-0.3.0-py3-none-any.whl

---
+++ packages/clanim/clanim-0.3.0-py3-none-any.whl/clanim/__init__.py
@@ -0,0 +1,5 @@
+from clanim.singleline import spinner, arrow, char_wave
+from clanim.multiline import spinners, arrows, char_waves, scrolling_text
+
+__all__ = 'spinner arrow char_wave spinners arrows char_waves scrolling_text'\
+        .split()

efab91d64d4c80b50b6791f07aa3d0873107ac53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:22 +0000

    Add package cutt-sd/0.0.3/cutt_sd-0.0.3-py3-none-any.whl

---
+++ packages/cutt-sd/cutt_sd-0.0.3-py3-none-any.whl/cutt_sd/server.py
@@ -0,0 +1,44 @@
+import os
+
+from flask import Flask, request
+
+from cutt_sd import register_api, find_api, unregister_api, zk
+
+from .config import *
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+def func():
+    ip = request.remote_addr
+    port = request.form.get('port')
+    uri = request.form.get('uri')
+    action = request.form.get('action')
+    env = request.form.get('env', DEFAULT_ENV)
+
+    if action and uri and port:
+        if 'register' == action:
+            register_api(uri, ip, port, env)
+        elif 'unregister' == action:
+            unregister_api(uri, ip, port, env)
+
+    return 'OK'
+
+
+@app.route('/query', methods=['POST', 'GET'])
+def locate():
+    api_func = request.values.get('uri')
+    env = request.values.get('env', DEFAULT_ENV)
+    if func:
+        ret = find_api(api_func, env)
+        return ret if ret else ''
+
+    return ''
+
+
+if 'ZK_SERVER' in os.environ:
+    zk.init(os.environ['ZK_SERVER'])
+
+if __name__ == '__main__':
+    app.run(port=4367, debug=True)
---
+++ packages/cutt-sd/cutt_sd-0.0.3-py3-none-any.whl/cutt_sd/service.py
@@ -0,0 +1,27 @@
+import os
+
+import requests
+from .config import *
+
+
+def register(uri, port, env=DEFAULT_ENV, sd_server=DEFAULT_SERVER):
+    requests.post(sd_server, {
+        'action': 'register',
+        'port': port,
+        'uri': uri,
+        'env': env
+    })
+
+
+def unregister(uri, port, env=DEFAULT_ENV, sd_server=DEFAULT_SERVER):
+    requests.post(sd_server, {
+        'action': 'unregister',
+        'port': port,
+        'uri': uri,
+        'env': env
+    })
+
+
+def find_api(uri, env=DEFAULT_ENV, sd_server=DEFAULT_SERVER):
+    response = requests.post(sd_server + 'query', {'uri': uri, 'env': env})
+    return response.text

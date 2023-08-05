2df80db88ee983b8f8dbbef349106451ce9ce61f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:18 +0000

    Add package django-tls-middleware/0.1.1/django-tls-middleware-0.1.1.tar.gz

---
+++ packages/django-tls-middleware/django-tls-middleware-0.1.1.tar.gz/django-tls-middleware-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as fp:
+    long_description = fp.read()
+
+setup(
+    name='django-tls-middleware',
+    version='0.1.1',
+    url='https://github.com/sekomy/django-tls-middleware',
+    author='Sekom Yazilim',
+    author_email='info@sekomyazilim.com.tr',
+    license='MIT',
+    description='TLS Middleware',
+    py_modules=["tls_middleware"],
+    long_description=long_description,
+    packages=find_packages(exclude=['docs', 'tests*']),
+    platforms=['any'],
+    include_package_data=True,
+)

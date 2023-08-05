370d07ee5c193cd37fbf076f2917fc11c26135a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:41 +0000

    Add package bob.pad.face/2.1.0/bob.pad.face-2.1.0.zip

---
+++ packages/bob.pad.face/bob.pad.face-2.1.0.zip/bob.pad.face-2.1.0/bob/pad/face/preprocessor/__init__.py
@@ -0,0 +1,37 @@
+from .FaceCropAlign import FaceCropAlign
+from .FrameDifference import FrameDifference
+from .VideoSparseCoding import VideoSparseCoding
+
+from .LiPulseExtraction import LiPulseExtraction
+from .Chrom import Chrom
+from .SSR import SSR
+from .PPGSecure import PPGSecure
+
+def __appropriate__(*args):
+    """Says object was actually declared here, and not in the import module.
+    Fixing sphinx warnings of not being able to find classes, when path is
+    shortened.
+
+    Parameters
+    ----------
+    *args
+        The objects that you want sphinx to beleive that are defined here.
+
+    Resolves `Sphinx referencing issues <https//github.com/sphinx-
+    doc/sphinx/issues/3048>`
+    """
+
+    for obj in args:
+        obj.__module__ = __name__
+
+
+__appropriate__(
+    FaceCropAlign,
+    FrameDifference,
+    VideoSparseCoding,
+    LiPulseExtraction,
+    Chrom,
+    SSR,
+    PPGSecure,
+)
+__all__ = [_ for _ in dir() if not _.startswith('_')]
---
+++ packages/bob.pad.face/bob.pad.face-2.1.0.zip/bob.pad.face-2.1.0/bob/pad/face/test/test_databases.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+# Thu May 24 10:41:42 CEST 2012
+
+from nose.plugins.skip import SkipTest
+
+import bob.bio.base
+from bob.bio.base.test.utils import db_available
+
+
+@db_available('replay')  # the name of the package
+def test_replay():
+    replay_database_instance = bob.bio.base.load_resource(
+        'replay-attack',
+        'database',
+        preferred_package='bob.pad.face',
+        package_prefix='bob.pad.'
+    )  # replay-attack is the name of the configuration file
+    try:
+
+        assert len(
+            replay_database_instance.objects(
+                groups=['train', 'dev', 'eval'])) == 1200
+        assert len(
+            replay_database_instance.objects(groups=['train', 'dev'])) == 720
+        assert len(replay_database_instance.objects(groups=['train'])) == 360
+        assert len(
+            replay_database_instance.objects(
+                groups=['train', 'dev', 'eval'], protocol='grandtest')) == 1200
+        assert len(
+            replay_database_instance.objects(
+                groups=['train', 'dev', 'eval'],
+                protocol='grandtest',
+                purposes='real')) == 200
+        assert len(
+            replay_database_instance.objects(
+                groups=['train', 'dev', 'eval'],
+                protocol='grandtest',
+                purposes='attack')) == 1000
+
+    except IOError as e:
+        raise SkipTest(
+            "The database could not be queried; probably the db.sql3 file is missing. Here is the error: '%s'"
+            % e)
+
+
+@db_available('replaymobile')
+def test_replaymobile():
+    replaymobile = bob.bio.base.load_resource(
+        'replay-mobile',
+        'database',
+        preferred_package='bob.pad.face',
+        package_prefix='bob.pad.')
+    try:
+
+        assert len(
+            replaymobile.objects(groups=['train', 'dev', 'eval'])) == 1030
+        assert len(replaymobile.objects(groups=['train', 'dev'])) == 728
+        assert len(replaymobile.objects(groups=['train'])) == 312
+        assert len(
+            replaymobile.objects(
+                groups=['train', 'dev', 'eval'], protocol='grandtest')) == 1030
+        assert len(
+            replaymobile.objects(
+                groups=['train', 'dev', 'eval'],
+                protocol='grandtest',
+                purposes='real')) == 390
+        assert len(
+            replaymobile.objects(
+                groups=['train', 'dev', 'eval'],
+                protocol='grandtest',
+                purposes='attack')) == 640
+
+    except IOError as e:
+        raise SkipTest(
+            "The database could not be queried; probably the db.sql3 file is missing. Here is the error: '%s'"
+            % e)
+
+
+@db_available('msu_mfsd_mod')
+def test_msu_mfsd():
+    msu_mfsd = bob.bio.base.load_resource(
+        'msu-mfsd',
+        'database',
+        preferred_package='bob.pad.face',
+        package_prefix='bob.pad.')
+    try:
+
+        assert len(msu_mfsd.objects(groups=['train', 'dev', 'eval'])) == 280
+        assert len(msu_mfsd.objects(groups=['train', 'dev'])) == 160
+        assert len(msu_mfsd.objects(groups=['train'])) == 80
+        assert len(
+            msu_mfsd.objects(
+                groups=['train', 'dev', 'eval'], protocol='grandtest')) == 280
+        assert len(
+            msu_mfsd.objects(
+                groups=['train', 'dev', 'eval'],
+                protocol='grandtest',
+                purposes='real')) == 70
+        assert len(
+            msu_mfsd.objects(
+                groups=['train', 'dev', 'eval'],
+                protocol='grandtest',
+                purposes='attack')) == 210
+
+    except IOError as e:
+        raise SkipTest(
+            "The database could not be queried; probably the db.sql3 file is missing. Here is the error: '%s'"
+            % e)
+
+
+# Test the maskattack database
+@db_available('maskattack')
+def test_maskattack():
+    maskattack = bob.bio.base.load_resource(
+        'maskattack',
+        'database',
+        preferred_package='bob.pad.face',
+        package_prefix='bob.pad.')
+    try:
+        # all real sequences: 2 sessions, 5 recordings for 17 individuals
+        assert len(maskattack.objects(groups=['train', 'dev', 'eval'], purposes='real')) == 170
+        # all attacks: 1 session, 5 recordings for 17 individuals
+        assert len(maskattack.objects(groups=['train', 'dev', 'eval'], purposes='attack')) == 85
+        
+        # training real: 7 subjects, 2 sessions, 5 recordings
+        assert len(maskattack.objects(groups=['train'], purposes='real')) == 70
+        # training real: 7 subjects, 1 session, 5 recordings
+        assert len(maskattack.objects(groups=['train'], purposes='attack')) == 35
+
+        # dev and test contains the same number of sequences:
+        # real: 5 subjects, 2 sessions, 5 recordings
+        # attack: 5 subjects, 1 sessions, 5 recordings
+        assert len(maskattack.objects(groups=['dev'], purposes='real')) == 50
+        assert len(maskattack.objects(groups=['eval'], purposes='real')) == 50
+        assert len(maskattack.objects(groups=['dev'], purposes='attack')) == 25
+        assert len(maskattack.objects(groups=['eval'], purposes='attack')) == 25
+
+    except IOError as e:
+        raise SkipTest(
+            "The database could not be queried; probably the db.sql3 file is missing. Here is the error: '%s'"
+            % e)
+
+# Test the Aggregated database, which doesn't have a package
+def test_aggregated_db():
+    aggregated_db = bob.bio.base.load_resource(
+        'aggregated-db',
+        'database',
+        preferred_package='bob.pad.face',
+        package_prefix='bob.pad.')
+    try:
+
+        assert len(
+            aggregated_db.objects(groups=['train', 'dev', 'eval'])) == 2510
+        assert len(aggregated_db.objects(groups=['train', 'dev'])) == 1608
+        assert len(aggregated_db.objects(groups=['train'])) == 752
+
+        assert len(aggregated_db.objects(groups='train')) == 752
+        assert len(aggregated_db.objects(groups='dev')) == 856
+        assert len(aggregated_db.objects(groups='eval')) == 902
+
+        assert len(
+            aggregated_db.objects(
+                groups=['train', 'dev', 'eval'], protocol='grandtest')) == 2510
+        assert len(
+            aggregated_db.objects(
+                groups=['train', 'dev', 'eval'],
+                protocol='grandtest',
+                purposes='real')) == 660
+        assert len(
+            aggregated_db.objects(
+                groups=['train', 'dev', 'eval'],
+                protocol='grandtest',
+                purposes='attack')) == 1850
+
+        assert len(
+            aggregated_db.objects(
+                groups=['train', 'dev', 'eval'],
+                protocol='photo-photo-video')) == 1664
+        assert len(
+            aggregated_db.objects(
+                groups=['train', 'dev'], protocol='photo-photo-video')) == 1176
+        assert len(
+            aggregated_db.objects(groups='eval',
+                                  protocol='photo-photo-video')) == 488
+
+        assert len(
+            aggregated_db.objects(
+                groups=['train', 'dev', 'eval'],
+                protocol='video-video-photo')) == 1506
+        assert len(
+            aggregated_db.objects(
+                groups=['train', 'dev'], protocol='video-video-photo')) == 872
+        assert len(
+            aggregated_db.objects(groups='eval',
+                                  protocol='video-video-photo')) == 634
+
+    except IOError as e:
+        raise SkipTest(
+            "The database could not be queried; probably the db.sql3 file is missing. Here is the error: '%s'"
+            % e)
+

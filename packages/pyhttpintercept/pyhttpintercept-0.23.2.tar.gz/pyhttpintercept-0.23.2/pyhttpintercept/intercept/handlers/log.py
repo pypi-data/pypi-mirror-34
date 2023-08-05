75f4966de3c5a746c99be34ce566438fca7b95d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:43 +0000

    Add package pyhttpintercept/0.23.2/pyhttpintercept-0.23.2.tar.gz

---
+++ packages/pyhttpintercept/pyhttpintercept-0.23.2.tar.gz/pyhttpintercept-0.23.2/pyhttpintercept/intercept/handlers/log.py
@@ -0,0 +1,10 @@
+# encoding: utf-8
+
+import logging_helper
+from ._base import BaseInterceptHandler
+
+logging = logging_helper.setup_logging()
+
+
+class InterceptHandler(BaseInterceptHandler):
+    pass

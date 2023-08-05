e96d5f561265623ce171c522ab69392051d302ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:30 +0000

    Add package ukpopulation/1.0.3/ukpopulation-1.0.3.tar.gz

---
+++ packages/ukpopulation/ukpopulation-1.0.3.tar.gz/ukpopulation-1.0.3/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+def readme():
+  with open('README.md') as f:
+    return f.read()
+
+setuptools.setup(
+  name='ukpopulation',
+  version='1.0.3',
+  description='Download, cache, collate, filter and extrapolate UK Population estimates and projections',
+  long_description=readme(),
+  long_description_content_type="text/markdown",
+  url='https://github.com/nismod/ukpopulation',
+  author='Andrew P Smith, Tom Russell',
+  author_email='a.p.smith@leeds.ac.uk, tom.russell@ouce.ox.ac.uk',
+  packages=setuptools.find_packages(),
+  install_requires=["numpy",
+                    "pandas",
+                    "requests",
+                    "openpyxl",
+                    "beautifulsoup4",
+                    "lxml",
+                    "ukcensusapi"],
+  classifiers=(
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ),
+  test_suite='nose.collector',
+  tests_require=['nose']
+)

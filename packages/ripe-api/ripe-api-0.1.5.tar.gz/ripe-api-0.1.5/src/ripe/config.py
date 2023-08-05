c1dbed0b9df0fe3abe8db942147429deb2d44585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:10 +0000

    Add package ripe-api/0.1.5/ripe-api-0.1.5.tar.gz

---
+++ packages/ripe-api/ripe-api-0.1.5.tar.gz/ripe-api-0.1.5/src/ripe/config.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class ConfigAPI(object):
+
+    def get_config(self):
+        url = self.base_url + "config"
+        contents = self.get(url)
+        return contents
+
+    def sku_config(self, *args, **kwargs):
+        url = self.base_url + "config/sku"
+        contents = self.get(url, auth = False, **kwargs)
+        return contents
+
+    def price_config(self, *args, **kwargs):
+        url = self.base_url + "config/price"
+        contents = self.get(url, auth = False, **kwargs)
+        return contents
+
+    def resolve_config(self, product_id):
+        url = self.base_url + "config/resolve/%s" % product_id
+        contents = self.get(url, auth = False)
+        return contents
+
+    def defaults_model_config(self, model):
+        url = self.base_url + "config/defaults/%s" % model
+        contents = self.get(url, auth = False)
+        return contents

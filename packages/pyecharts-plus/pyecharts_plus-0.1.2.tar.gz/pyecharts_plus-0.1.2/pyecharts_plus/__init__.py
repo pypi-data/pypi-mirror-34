03639c8bd3289498f7609493c32763f028408faf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:17 +0000

    Add package pyecharts-plus/0.1.2/pyecharts_plus-0.1.2.tar.gz

---
+++ packages/pyecharts-plus/pyecharts_plus-0.1.2.tar.gz/pyecharts_plus-0.1.2/pyecharts_plus/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# charts
+from pyecharts_plus.charts.bubble_4D import Bubble_4D
+from pyecharts_plus.charts.map_bin import Map_Bin
+from pyecharts_plus.charts.map_bin_scatter import Map_Bin_Scatter
+from pyecharts_plus.charts.map_bin_bubble import Map_Bin_Bubble
+from pyecharts_plus.charts.warehouse_sketch import Warehouse_Sketch
---
+++ packages/pyecharts-plus/pyecharts_plus-0.1.2.tar.gz/pyecharts_plus-0.1.2/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name="pyecharts_plus",
+    version="0.1.2",
+    description="Some charts which pyecharts don't contain",
+    long_description=open('README.rst').read(),
+    author='Ijustwantyouhappy',
+    author_email='18817363043@163.com',
+    maintainer='',
+    maintainer_email='',
+    license='MIT',
+    packages=find_packages(),
+    platforms=["all"],
+    url='',
+    install_requires=["pandas",
+                      "pyecharts>=0.5.3"],
+    classifiers=[
+        "Environment :: Web Environment",
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    zip_safe=True,
+)

beafdb6de41d3b0c50202f40c3baa6ebfdfc3ea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:38 +0000

    Add package pedal/0.1.dev0/Pedal-0.1.dev0.tar.gz

---
+++ packages/pedal/Pedal-0.1.dev0.tar.gz/Pedal-0.1.dev0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+setuptools.setup(
+    name='Pedal',
+    version='0.1dev',
+    author='acbart,lukesg08',
+    author_email='acbart9@gmail.com',
+    description='Tools for analyzing student code.',
+    packages=['pedal',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.rst').read(),
+    url='https://github.com/acbart/pedal',
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        
+    )
+)

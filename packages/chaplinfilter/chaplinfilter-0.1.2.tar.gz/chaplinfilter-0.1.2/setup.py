3afb397a576c83e3a75405c334220de96b7714a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:26 +0000

    Add package chaplinfilter/0.1.2/chaplinfilter-0.1.2.tar.gz

---
+++ packages/chaplinfilter/chaplinfilter-0.1.2.tar.gz/chaplinfilter-0.1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='chaplinfilter',
+      version='0.1.2',
+      author='Guy R. Davies',
+      author_email='grd349@gmail.com',
+      licence='MIT',
+      packages=['chaplinfilter'],
+      #url='TODO',
+      install_requires=['numpy', 'scikit-learn', 'pandas'])

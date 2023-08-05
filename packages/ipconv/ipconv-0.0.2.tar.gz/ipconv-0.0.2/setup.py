9d3427a06bbc46c268e7a5b901b88f993c6d9abb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:14 +0000

    Add package ipconv/0.0.2/ipconv-0.0.2.tar.gz

---
+++ packages/ipconv/ipconv-0.0.2.tar.gz/ipconv-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ipconv",
+    version="0.0.2",
+    author="Pranav Gajjewar",
+    author_email="apbetahouse45@gmail.com",
+    description="An easy and convenient IP address operation library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Cartmanishere/IPLib",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)

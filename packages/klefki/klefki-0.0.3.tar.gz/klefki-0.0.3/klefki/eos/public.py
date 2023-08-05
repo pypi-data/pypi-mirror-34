e107b08b8e017acbe1649556ce77a7b9635e32c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:44:50 +0000

    Add package klefki/0.0.3/klefki-0.0.3.tar.gz

---
+++ packages/klefki/klefki-0.0.3.tar.gz/klefki-0.0.3/klefki/eos/public.py
@@ -0,0 +1,21 @@
+from klefki.utils import int_to_byte
+from klefki.crypto.ecdsa.secp256k1 import pubkey
+from klefki.types.algebra.concrete import (
+    EllipticCurveGroupSecp256k1 as ECG,
+    FiniteFieldCyclicSecp256k1 as CF
+
+)
+from klefki.utils import ripemd160, b58encode
+
+
+def to_bytes(pub: ECG) -> str:
+    return bytes([2 + (pub.value[1].value % 2)]) + int_to_byte(pub.value[0].value)
+
+
+def checksum(a) -> int:
+    assert len(a) == 33
+    return 'EOS' + b58encode(a + ripemd160(a)[:4])
+
+
+def gen_pub_key(key: CF):
+    return checksum(to_bytes(pubkey(key)))

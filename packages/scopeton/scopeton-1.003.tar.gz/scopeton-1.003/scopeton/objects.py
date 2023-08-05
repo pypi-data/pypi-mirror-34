6c9253724643bf0da50004243ff08ced1867a2be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:29 +0000

    Add package scopeton/1.003/scopeton-1.003.tar.gz

---
+++ packages/scopeton/scopeton-1.003.tar.gz/scopeton-1.003/scopeton/objects.py
@@ -0,0 +1,21 @@
+from scopeton.scopeTools import getBeanName
+
+
+class Bean(object):
+    name = ""
+    cls = None  # type: type
+    lazy = False
+    singleton = True
+
+    def __init__(self, cls, name=None, lazy = False, singleton=True, service = True, checkRegistered = True):
+        self.checkRegistered = True
+        self.cls = cls
+        self.name = getBeanName(name) if name else getBeanName(cls)
+        self.lazy = lazy
+        self.singleton = singleton
+        self.service = service
+        if not singleton and service:
+            raise Exception("Error, cannot initialize service as non singleton")
+
+
+

ed7f475ebaa5dd474283ba582a9b6b7a0ab53311
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:50 +0000

    Add package hepdata-converter/0.1.33/hepdata_converter-0.1.33-py2.7.egg

---
+++ packages/hepdata-converter/hepdata_converter-0.1.33-py2.7.egg/hepdata_converter/writers/utils.py
@@ -0,0 +1,19 @@
+
+def error_value_processor(value, error):
+    """
+    If an error is a percentage, we convert to a float, then
+    calculate the percentage of the supplied value.
+
+    :param value: base value, e.g. 10
+    :param error: e.g. 20.0%
+    :return: the absolute error, e.g. 12 for the above case.
+    """
+    if type(error) is str:
+        if "%" in error:
+            error = error.replace("%", "")
+            error = float(error)
+
+            error_abs = (value/100) * error
+            return error_abs
+
+    return error

ff2ef527f32732568c5dfbab86bd26e1427159ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:09 +0000

    Add package openbanking/0.4.1/openbanking-0.4.1.tar.gz

---
+++ packages/openbanking/openbanking-0.4.1.tar.gz/openbanking-0.4.1/openbanking/exceptions.py
@@ -0,0 +1,23 @@
+class OpenBankingBaseError(Exception):
+    """Base class for all Open Banking errors."""
+    pass
+
+
+class ConfigurationException(OpenBankingBaseError):
+    pass
+
+class UnsupportedException(OpenBankingBaseError):
+    pass
+
+class JSONDecodeError(OpenBankingBaseError):
+    pass
+
+class DecodeError(OpenBankingBaseError):
+    pass
+
+class PathFileError(OpenBankingBaseError):
+    pass
+
+
+class SSAError(OpenBankingBaseError):
+    pass

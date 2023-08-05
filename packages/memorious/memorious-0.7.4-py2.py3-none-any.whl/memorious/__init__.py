5c4be922ba9a6cf7007b110d44038e6fd2bd009b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:09 +0000

    Add package memorious/0.7.4/memorious-0.7.4-py2.py3-none-any.whl

---
+++ packages/memorious/memorious-0.7.4-py2.py3-none-any.whl/memorious/__init__.py
@@ -0,0 +1,13 @@
+import logging
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+logging.basicConfig(level=logging.INFO)
+logging.getLogger('requests').setLevel(logging.WARNING)
+logging.getLogger('zeep').setLevel(logging.WARNING)
+logging.getLogger('httpstream').setLevel(logging.WARNING)
+
+
+# Register signal handlers
+from . import signals  # noqa

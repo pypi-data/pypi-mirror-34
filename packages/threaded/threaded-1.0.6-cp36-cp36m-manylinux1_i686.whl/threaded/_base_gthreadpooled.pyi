701772d145919682ed24babb1c3f1a9dd4172e14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:21 +0000

    Add package threaded/1.0.6/threaded-1.0.6-cp36-cp36m-manylinux1_i686.whl

---
+++ packages/threaded/threaded-1.0.6-cp36-cp36m-manylinux1_i686.whl/threaded/_base_gthreadpooled.pyi
@@ -0,0 +1,20 @@
+import gevent.event  # type: ignore
+import gevent.threadpool  # type: ignore
+import typing
+from . import _base_threaded
+
+class BaseGThreadPooled(_base_threaded.APIPooled):
+    @classmethod
+    def configure(
+        cls: typing.Type[BaseGThreadPooled],
+        max_workers: typing.Optional[int]=...,
+        hub: typing.Optional[gevent.hub.Hub]=...
+    ) -> None: ...
+
+    @classmethod
+    def shutdown(cls) -> None: ...
+
+    @property
+    def executor(self) -> gevent.threadpool.ThreadPool: ...
+
+    def _get_function_wrapper(self, func: typing.Callable) -> typing.Callable[..., gevent.event.AsyncResult]: ...

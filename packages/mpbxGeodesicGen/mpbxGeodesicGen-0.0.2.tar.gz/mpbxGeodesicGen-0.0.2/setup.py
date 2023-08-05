bc1fe651e9afa76c03b11a44bdd02ad81c8efd38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:22 +0000

    Add package mpbxGeodesicGen/0.0.2/mpbxGeodesicGen-0.0.2.tar.gz

---
+++ packages/mpbxGeodesicGen/mpbxGeodesicGen-0.0.2.tar.gz/mpbxGeodesicGen-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+import setuptools
+
+setup(
+    name='mpbxGeodesicGen',
+    version='0.0.2',
+    packages=setuptools.find_packages(),
+    url='',
+    license='',
+    author='blackbird_sings',
+    author_email='maksvl.ch@gmail.com',
+    description=''
+)

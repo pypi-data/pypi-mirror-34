a63b503e16ed16307833175f798fdf2c4c9257b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:08 +0000

    Add package dash-api-python-client/0.1.0/dash_api_python_client-0.1.0-py2-none-any.whl

---
+++ packages/dash-api-python-client/dash_api_python_client-0.1.0-py2-none-any.whl/dash_api/api/__init__.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from dash_api.api.activities_api import ActivitiesApi
+from dash_api.api.charge_api import ChargeApi
+from dash_api.api.environment_api import EnvironmentApi
+from dash_api.api.event_api import EventApi
+from dash_api.api.event_status_api import EventStatusApi
+from dash_api.api.event_type_api import EventTypeApi
+from dash_api.api.organisation_api import OrganisationApi
+from dash_api.api.organisation_type_api import OrganisationTypeApi
+from dash_api.api.resource_api import ResourceApi
+from dash_api.api.restrictions_api import RestrictionsApi
+from dash_api.api.roles_api import RolesApi
+from dash_api.api.teams_api import TeamsApi
+from dash_api.api.user_api import UserApi

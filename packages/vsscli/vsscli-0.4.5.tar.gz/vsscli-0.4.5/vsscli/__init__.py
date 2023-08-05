77408471f157703a12bf9f76f79f8fabcba08437
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:25 +0000

    Add package vsscli/0.4.5/vsscli-0.4.5.tar.gz

---
+++ packages/vsscli/vsscli-0.4.5.tar.gz/vsscli-0.4.5/vsscli/__init__.py
@@ -0,0 +1,16 @@
+import os
+__version__ = '0.4.5'
+__name__ = 'vsscli'
+__default_endpoint__ = 'https://vss-api.eis.utoronto.ca'
+__history_file_path__ = os.path.join('~', '.vss', 'history')
+__config_file_path__ = os.path.join('~', '.vss', 'config.json')
+__default_debug_mode__ = False
+__env_vars__ = {'user': 'VSS_API_USER',
+                'pass': 'VSS_API_USER_PASS',
+                'token': 'VSS_API_TOKEN',
+                'endpoint': 'VSS_API_ENDPOINT',
+                'debug': 'VSS_API_DEBUG',
+                'config': 'VSS_CONFIG_FILE',
+                'output': 'VSS_DEFAULT_OUTPUT'}
+__status_page_id__ = 'ftgqfszqxm8y'
+__status_page_service__ = 'Virtual Server'

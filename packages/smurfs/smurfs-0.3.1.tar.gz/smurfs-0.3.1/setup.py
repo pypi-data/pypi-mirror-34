0b3d25e1b47bb5cb0785136605bb9919a9f08661
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:32 +0000

    Add package smurfs/0.3.1/smurfs-0.3.1.tar.gz

---
+++ packages/smurfs/smurfs-0.3.1.tar.gz/smurfs-0.3.1/setup.py
@@ -0,0 +1,35 @@
+import pip
+ver = pip.__version__.split(".")
+
+#main is not directly accessible anymore on pip > 10
+if int(ver[0]) < 10:
+    from pip import main
+else:
+    from pip._internal import main
+
+from setuptools import setup,find_packages
+
+reqs = ["pandas==0.22.0"
+        ,"numpy==1.14.2"
+        ,"astropy==3.0.1"
+        ,"scipy==1.0.1"
+        ,"plotnine==0.2.1"
+        ,"pandas==0.22.0"
+        ,"stem==1.6.0"
+        ,"matplotlib==2.2.2"]
+
+
+#this is a bit of a hack, due to the inability to build numpy properly in a reasonable amount of time
+for req in reqs:
+        main(["install", req])
+
+setup(
+    name='smurfs',
+    version='0.3.1',
+    packages=find_packages(exclude=["*test","dist","build","venv"]),
+    url='https://github.com/muma7490/SMURFS',
+    license='MIT',
+    author='Marco Müllner',
+    author_email='muellnermarco@gmail.com',
+    description='Smart UseR Frequency analySer, a fast and easy to use frequency analyser.',
+)

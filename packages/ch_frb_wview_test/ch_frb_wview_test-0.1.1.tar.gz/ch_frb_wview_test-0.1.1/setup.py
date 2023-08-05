e87ced149256aede4c91da88d6aa0b5721340202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:36 +0000

    Add package ch_frb_wview_test/0.1.1/ch_frb_wview_test-0.1.1.tar.gz

---
+++ packages/ch_frb_wview_test/ch_frb_wview_test-0.1.1.tar.gz/ch_frb_wview_test-0.1.1/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from setuptools import setup
+from codecs import open
+#from os import path
+
+#here = path.abspath(path.dirname(__file__))
+
+#with open(path.join(here, 'requirements.txt')) as f:
+#    requirements = f.readlines()
+
+setup(
+    name='ch_frb_wview_test',
+    version='0.1.1',
+    description='Web Application to operate the FRB L1 system.'
+,
+
+    author='Dustin Lang, Chitrang Patel',
+    author_email='chitrang.patel@mail.mcgill.ca',
+    url="https://github.com/kmsmith137/ch_frb_l1/webapp",
+    packages= ['webapp'],
+    #py_modules=['webapp', 'chime_frb_operations', 'chlog_database', 'cnc_ssh', 'cnc_client'],
+    install_requires=['flask',
+                      'json',
+                      'yaml',
+                      'msgpack',
+                      'numpy',
+                      'sqlalchemy',
+                      'passlib',
+                      'zmq',
+                      'scipy',
+                      'requests',
+                      'pylab']
+)

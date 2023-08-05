de619a26410ea5e6fb9e70369545f71959be076e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:57 +0000

    Add package django-szuprefix/0.4.31/django_szuprefix-0.4.31-py2-none-any.whl

---
+++ packages/django-szuprefix/django_szuprefix-0.4.31-py2-none-any.whl/django_szuprefix/wechat/admin.py
@@ -0,0 +1,23 @@
+# -*- coding:utf8 -*-
+# Author:DenisHuang
+# Date:
+# Usage:
+
+from django.contrib import admin
+from . import models
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('create_time', 'type', 'from_id', 'to_id', 'content')
+
+
+admin.site.register(models.Message, MessageAdmin)
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('nickname', 'openid', 'province', 'city', 'user')
+    raw_id_fields = ('user',)
+    readonly_fields = ('user',)
+
+
+admin.site.register(models.User, UserAdmin)

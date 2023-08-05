ede237bb7c206986c35b4ae828e842c1854174e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:21 +0000

    Add package beem/0.19.49/beem-0.19.49.tar.gz

---
+++ packages/beem/beem-0.19.49.tar.gz/beem-0.19.49/beembase/signedtransactions.py
@@ -0,0 +1,35 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from builtins import int, str
+from beemgraphenebase.signedtransactions import Signed_Transaction as GrapheneSigned_Transaction
+from .operations import Operation
+from beemgraphenebase.chains import known_chains
+import logging
+log = logging.getLogger(__name__)
+
+
+class Signed_Transaction(GrapheneSigned_Transaction):
+    """ Create a signed transaction and offer method to create the
+        signature
+
+        :param num refNum: parameter ref_block_num (see ``getBlockParams``)
+        :param num refPrefix: parameter ref_block_prefix (see ``getBlockParams``)
+        :param str expiration: expiration date
+        :param Array operations:  array of operations
+    """
+    def __init__(self, *args, **kwargs):
+        super(Signed_Transaction, self).__init__(*args, **kwargs)
+
+    def sign(self, wifkeys, chain=u"STEEM"):
+        return super(Signed_Transaction, self).sign(wifkeys, chain)
+
+    def verify(self, pubkeys=[], chain=u"STEEM", recover_parameter=False):
+        return super(Signed_Transaction, self).verify(pubkeys, chain, recover_parameter)
+
+    def getOperationKlass(self):
+        return Operation
+
+    def getKnownChains(self):
+        return known_chains

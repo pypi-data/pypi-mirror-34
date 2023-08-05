d4966f2e0509c5e8ad9e0b80f08bba584a9f2bf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:25 +0000

    Add package django-cropperjs/0.0.3/django-cropperjs-0.0.3.tar.gz

---
+++ packages/django-cropperjs/django-cropperjs-0.0.3.tar.gz/django-cropperjs-0.0.3/cropperjs/utils.py
@@ -0,0 +1,27 @@
+# coding=utf-8
+
+import re, sys
+from base64 import b64decode
+
+from django.core.files.base import ContentFile
+
+# Used for consistent string/type comparisons across Python 2 and 3
+# without requiring six, future or other external dependencies
+TEXT_TYPE = str if sys.version_info[0] >= 3 else unicode
+
+def cropperImageFile(base64data, defaultName):
+    try:
+        metadata, imagedata = base64data.split("base64")
+        filename, mimetype = metadata[:-1].split(";")
+    except:
+        # Too many values to unpack, unexpected format
+        raise ValueError
+    if 'filename' in metadata:
+        filename = metadata.split(";")[0].split(":")[1]
+        filename = re.sub("[^a-zA-Z0-9]", "", filename.lower())
+    else:
+        filename = defaultName
+    fileext = mimetype.split("/")[1]
+
+    return ContentFile(b64decode(imagedata), "%s.%s" % (filename, fileext))
+

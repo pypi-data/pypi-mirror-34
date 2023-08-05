bc5d0e66376fdb0ea16bc913325ff9f426718379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:42 +0000

    Add package dcicutils/0.3.5/dcicutils-0.3.5.tar.gz

---
+++ packages/dcicutils/dcicutils-0.3.5.tar.gz/dcicutils-0.3.5/setup.py
@@ -0,0 +1,39 @@
+import os
+from setuptools import setup
+
+# variables used in buildout
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+
+requires = [
+    'pytest-runner',
+    'boto3',
+    'elasticsearch>=5.3.0,<6.0.0',
+    'aws_requests_auth'
+]
+
+tests_require = [
+    'pytest',
+    'pytest-mock',
+    'pytest-cov',
+]
+
+setup(
+    name='dcicutils',
+    version=open("dcicutils/_version.py").readlines()[-1].split()[-1].strip("\"'"),
+    description='Utility modules shared amongst several repos in the 4dn-dcic organization',
+    long_description=README,
+    packages=['dcicutils'],
+    include_package_data=True,
+    zip_safe=False,
+    author='4DN Team at Harvard Medical School',
+    author_email='burak_alver@hms.harvard.edu',
+    url='https://data.4dnucleome.org',
+    license='MIT',
+    install_requires=requires,
+    setup_requires=requires,
+    tests_require=tests_require,
+    extras_require={
+        'test': tests_require,
+    },
+)

b5e06fd1f547ad87ea9c8fc0f894e7f2e0c98f95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:38 +0000

    Add package lgad/0.0.11/lgad-0.0.11.tar.gz

---
+++ packages/lgad/lgad-0.0.11.tar.gz/lgad-0.0.11/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+#with open("README.md", "r") as fh:
+#    long_description = fh.read()
+
+setuptools.setup(
+    name="lgad",
+    version="0.0.11",
+    author="William Wyatt",
+    author_email="wwyatt@ucsc.edu",
+    description="Simulator for the LGAD detector.",
+    long_description="Simulator for the LGAD detector.",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    url="https://github.com/Tsangares/LGAD_SIM",
+    package_data={'lgad': ['plates.json']},
+    extras_require={
+        "matplotlib": ["matplotlib"]
+        },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)

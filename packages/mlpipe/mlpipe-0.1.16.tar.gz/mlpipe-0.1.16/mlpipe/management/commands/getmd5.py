d5927589d1d2cdace82308ee09355f5abe3fad9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:45 +0000

    Add package mlpipe/0.1.16/mlpipe-0.1.16.tar.gz

---
+++ packages/mlpipe/mlpipe-0.1.16.tar.gz/mlpipe-0.1.16/mlpipe/management/commands/getmd5.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand, CommandError
+from mlpipe.models import Pipe
+import mlpipe.mlpipe_utils as mlpipeutils
+from mlpipe.job_utils import JobRunner
+import os
+
+class Command(BaseCommand):
+    help = 'get the md5 for a string'
+
+    def add_arguments(self, parser):
+        # Positional arguments
+        parser.add_argument('inputstring')
+
+    def handle(self, *args, **options):
+        if "inputstring" in options: 
+            print(mlpipeutils.get_md5(options["inputstring"]))
---
+++ packages/mlpipe/mlpipe-0.1.16.tar.gz/mlpipe-0.1.16/mlpipe/management/commands/listmodule.py
@@ -0,0 +1,20 @@
+import os
+
+from django.core.management.base import BaseCommand, CommandError
+from mlpipe.settings import *
+import mlpipe.mlpipe_utils as mlpipeutils
+
+
+class Command(BaseCommand):
+    help = 'list all available module'
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        app_dict = mlpipeutils.get_apps()
+        module_list = mlpipeutils.get_modules()
+        print("%d app(s) found : %s " % (len(app_dict) , ",".join(app_dict)))
+        print("%d modules  found :" % (len(module_list) ))
+        for i, k in  enumerate(module_list):
+            print("%d : %s " %(i, k))

a0323cf81b1868f25e202b0dc8d1ef9d8f7331c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:56 +0000

    Add package evalai/1.0.14/evalai-1.0.14-py3-none-any.whl

---
+++ packages/evalai/evalai-1.0.14-py3-none-any.whl/evalai/submissions.py
@@ -0,0 +1,15 @@
+import click
+
+from evalai.utils.submissions import display_submission_details
+
+
+@click.group(invoke_without_command=True)
+@click.argument('SUBMISSION', type=int)
+def submission(submission):
+    """
+    View submission specific details.
+    """
+    """
+    Invoked by `evalai submission SUBMISSION`.
+    """
+    display_submission_details(submission)

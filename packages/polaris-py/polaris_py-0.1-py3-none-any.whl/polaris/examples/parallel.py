e931fd39f0ecbafb72c9e2d9f5d6f677cab4539b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:10 +0000

    Add package polaris-py/0.1/polaris_py-0.1-py3-none-any.whl

---
+++ packages/polaris-py/polaris_py-0.1-py3-none-any.whl/polaris/examples/parallel.py
@@ -0,0 +1,15 @@
+from polaris import Polaris, Trials, Bounds
+from polaris.examples.utils import pseudo_train
+
+
+if __name__ == '__main__':
+    bounds = [
+        Bounds('lr', 0.001, 0.01),
+        Bounds('weight_decay', 0.0002, 0.04),
+    ]
+    trials = Trials()
+    polaris = Polaris(
+            pseudo_train, bounds, 'bayesian_opt',
+            trials, max_evals=100, min_ei=1e-07, exp_key='this_is_test')
+    best_params = polaris.run_parallel()
+    print(best_params)
---
+++ packages/polaris-py/polaris_py-0.1-py3-none-any.whl/polaris/rabbitmq/config.py
@@ -0,0 +1,11 @@
+import os
+from urllib.parse import urlparse
+
+RABBITMQ_URL = os.getenv('RABBITMQ_URL', 'amqp://localhost//')
+url = urlparse(RABBITMQ_URL)
+
+RABBITMQ_HOST = url.hostname
+RABBITMQ_USERNAME = url.username
+RABBITMQ_PASSWORD = url.password
+RABBITMQ_PORT = 5672
+RABBITMQ_VIRTUAL_HOST = url.path[1:]

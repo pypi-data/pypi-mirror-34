974eadfe5bd583ed6a71ee978b40de60859c1c4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:13 +0000

    Add package urpc/0.3.2/urpc-0.3.2.tar.gz

---
+++ packages/urpc/urpc-0.3.2.tar.gz/urpc-0.3.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+setup(name="urpc", version="0.3.2",
+      py_modules=['urpc', 'aiourpc'],
+      url="http://github.com/bobuk/urpc",
+      author="Grigory Bakunov",
+      author_email='thebobuk@ya.ru',
+      description='uRPC is oversimplistic RPC over Redis',
+      install_requires=[
+          'redis'
+      ],
+      scripts = ['scripts/urpc-cli'],
+      classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+
+)

5ab8b0f0f54f3c81e8b4cf0c2f1979b23310e4d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:30 +0000

    Add package Berrys_test_package/0.1.1/Berrys_test_package-0.1.1.tar.gz

---
+++ packages/Berrys_test_package/Berrys_test_package-0.1.1.tar.gz/Berrys_test_package-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+import setuptools
+
+setup(name='Berrys_test_package',
+      version='0.1.1',
+      description='lets figure this out',
+      author='Berry Adams',
+      license='NA',
+      packages=setuptools.find_packages(),
+      zip_safe=False)

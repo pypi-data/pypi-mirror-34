c52e76544291e4c3ed43228dfceb7580d141aa88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:08 +0000

    Add package wrangalytics/0.0.2.dev0/wrangalytics-0.0.2.dev0-py3-none-any.whl

---
+++ packages/wrangalytics/wrangalytics-0.0.2.dev0-py3-none-any.whl/wrangalytics/__init__.py
@@ -0,0 +1,34 @@
+name = "wrangalytics"
+
+### Import statements
+# import wrangalytics
+# import wrangalytics.config.api
+# import wrangalytics.transform.api
+# import wrangalytics.eda.viz.api
+# import wrangalytics.eda.stats.api
+
+### Import base functions
+
+# from IPython.display import display
+
+# import numpy as np
+# import pandas as pd
+
+# import matplotlib.pyplot as plt
+
+# import statsmodels.api as sm
+# import statsmodels.formula.api as smf
+
+# import scipy
+# from scipy import stats
+
+# import sklearn
+
+# import wrangalytics
+# from wrangalytics.config import api as config
+
+# from wrangalytics.transform.api import percent2float as p2f
+# from wrangalytics.transform.api import str2float as s2f
+# from wrangalytics.transform.api import str2int as s2i
+
+# from imp import reload
---
+++ packages/wrangalytics/wrangalytics-0.0.2.dev0-py3-none-any.whl/wranglyzer/__init__.py
@@ -0,0 +1,10 @@
+name = "wranglyzer"
+
+### Import statements
+# import wranglyzer
+# import wranglyzer.config.api
+# import wranglyzer.converters.api
+# import wranglyzer.eda.viz.api
+# import wranglyzer.eda.stats.api
+
+print("Importing wranglyzer")

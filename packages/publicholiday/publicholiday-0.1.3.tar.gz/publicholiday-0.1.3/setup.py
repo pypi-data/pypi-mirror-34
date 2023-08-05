b1a72f724396848c221bd691fd715f3e0fe39b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:53 +0000

    Add package publicholiday/0.1.3/publicholiday-0.1.3.tar.gz

---
+++ packages/publicholiday/publicholiday-0.1.3.tar.gz/publicholiday-0.1.3/setup.py
@@ -0,0 +1,58 @@
+#  python-publicholiday
+#  ---------------
+#  A fast, efficient command line utility for working with public holidays
+#
+#  Author:  Tim Birkett <tim.birkettdev@devopsmakers.com>
+#  Website: https://github.com/timbirk/python-publicholiday
+#  License: Unlicense (see LICENSE file)
+
+import codecs
+from setuptools import find_packages, setup
+
+dependencies = [
+    'click==6.7',
+    'holidays==0.9.5'
+]
+
+setup(
+    name='publicholiday',
+    version='0.1.3',
+    url='https://github.com/timbirk/python-publicholiday',
+    license='BSD',
+    author='Tim Birkett',
+    author_email='tim.birkett@devopsmakers.com',
+    description='Is it a public holiday?',
+    long_description=codecs.open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'publicholiday = publicholiday.cli:main',
+        ],
+    },
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Topic :: Utilities',
+        'Environment :: Console',
+        #'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: Public Domain',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        #'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)

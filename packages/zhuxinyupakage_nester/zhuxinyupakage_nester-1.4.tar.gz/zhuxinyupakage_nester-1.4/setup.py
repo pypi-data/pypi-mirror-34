d77b933b5657b30a16462234bdf37e8b966994d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:17 +0000

    Add package zhuxinyupakage_nester/1.4/zhuxinyupakage_nester-1.4.tar.gz

---
+++ packages/zhuxinyupakage_nester/zhuxinyupakage_nester-1.4.tar.gz/zhuxinyupakage_nester-1.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = "zhuxinyupakage_nester",
+    version = "1.4",
+    author = "XiaoZhuSGM",
+    author_email = "739964005@qq.com",
+    url = "https://github.com/XiaoZhuSGM/XiaoZhuSGM.github.io",
+    description = "A pro printer of nested lists"
+    )
+
+    

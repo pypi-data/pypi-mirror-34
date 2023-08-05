63f85596ab9f6b3bc7672848baf79d1888dadf10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:37 +0000

    Add package modelchimp/0.4.1/modelchimp-0.4.1.tar.gz

---
+++ packages/modelchimp/modelchimp-0.4.1.tar.gz/modelchimp-0.4.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+  name = 'modelchimp',
+  packages = ['modelchimp'],
+  version = '0.4.1',
+  description = 'Python client to upload the machine learning models data to the model chimp cloud',
+  author = 'Samir Madhavan',
+  author_email = 'samir.madhavan@gmail.com',
+  url = 'https://github.com/samzer/modelchimp-client-python',
+  download_url = 'https://github.com/samzer/modelchimp-client-python/archive/0.4.1.tar.gz',
+  keywords = ['modelchimp', 'ai', 'datascience'],
+  install_requires=[
+          'requests',
+          'future',
+          'six',
+          'websocket-client'
+      ],
+  classifiers = [],
+)

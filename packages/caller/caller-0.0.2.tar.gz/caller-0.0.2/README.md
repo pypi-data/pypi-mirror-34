54a39d2d582c13693b6470a73dee5b3d7b57af32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:19 +0000

    Add package caller/0.0.2/caller-0.0.2.tar.gz

---
+++ packages/caller/caller-0.0.2.tar.gz/caller-0.0.2/README.md
@@ -0,0 +1,64 @@
+# caller
+
+[![Build Status](https://travis-ci.org/vtitor/caller.svg?branch=master)](https://travis-ci.org/vtitor/caller)
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/ambv/black)
+
+Caller is a little library for calling a property as a regular function.
+
+Installation
+------------
+
+Install using pip:
+
+    $ pip install caller
+
+Usage
+-----
+
+```python
+from caller import property
+
+class Foo(object):
+    def __init__(self, bar):
+        self._bar = bar
+
+    @property
+    def bar(self):
+        return self._bar
+
+    @bar.setter
+    def bar(self, new_bar):
+        self._bar = new_bar
+
+foo = Foo('bar')
+foo.bar('new bar')
+
+print(foo.bar)
+```
+
+Prints
+
+```
+new bar
+```
+
+Or with a separate caller
+
+```python
+class Foo(object):
+    ...
+    @bar.caller
+    def bar(self, new_bar):
+        self._bar = new_bar[::-1]
+        print(self._bar)
+
+Foo('bar').bar('new bar')
+```
+
+Prints
+
+```
+rab wen
+```
+
+That's all.
---
+++ packages/caller/caller-0.0.2.tar.gz/caller-0.0.2/caller/helpers.py
@@ -0,0 +1,40 @@
+import pickle
+
+from caller.base_types import base_types
+
+
+def call(obj, *args, **kwargs):
+    return obj.__call__(*args, **kwargs)
+
+
+def make_callable(value, func, cache={}):
+    try:
+        value_type = value.__class__
+    except AttributeError:
+        value_type = type(value)
+    cls_name = "Callable{cls}".format(cls=value_type.__name__.capitalize())
+    bases = (base_types.get(value_type, value_type),)
+    attributes = {
+        "__call__": call,
+        "__class__": value_type,
+        "__iadd__": lambda _, other: value + other,
+    }
+    if cls_name not in cache:
+        callable_type = type(cls_name, bases, attributes)
+        try:
+            reduce = pickle.Pickler.dispatch[value_type]
+            pickle.Pickler.dispatch[callable_type] = reduce
+        except (AttributeError, KeyError):
+            pass
+        cache[cls_name] = callable_type
+    else:
+        callable_type = cache[cls_name]
+    callable_value = callable_type(value)
+    callable_value.__call__ = func
+    for attr_name in dir(value):
+        try:
+            if attr_name not in attributes:
+                setattr(callable_value, attr_name, getattr(value, attr_name))
+        except AttributeError:
+            pass
+    return callable_value

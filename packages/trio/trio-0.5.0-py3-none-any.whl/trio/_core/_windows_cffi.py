2644733f42c15e6c624345fd7cbd5d9abcd7ded9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:27 +0000

    Add package trio/0.5.0/trio-0.5.0-py3-none-any.whl

---
+++ packages/trio/trio-0.5.0-py3-none-any.whl/trio/_core/_windows_cffi.py
@@ -0,0 +1,122 @@
+import cffi
+import re
+import enum
+
+LIB = """
+// https://msdn.microsoft.com/en-us/library/windows/desktop/aa383751(v=vs.85).aspx
+typedef int BOOL;
+typedef unsigned char BYTE;
+typedef BYTE BOOLEAN;
+typedef void* PVOID;
+typedef PVOID HANDLE;
+typedef unsigned long DWORD;
+typedef unsigned long ULONG;
+typedef unsigned long u_long;
+typedef ULONG *PULONG;
+
+typedef uintptr_t ULONG_PTR;
+typedef uintptr_t UINT_PTR;
+
+typedef UINT_PTR SOCKET;
+
+typedef struct _OVERLAPPED {
+    ULONG_PTR Internal;
+    ULONG_PTR InternalHigh;
+    union {
+        struct {
+            DWORD Offset;
+            DWORD OffsetHigh;
+        } DUMMYSTRUCTNAME;
+        PVOID Pointer;
+    } DUMMYUNIONNAME;
+
+    HANDLE  hEvent;
+} OVERLAPPED, *LPOVERLAPPED;
+
+typedef OVERLAPPED WSAOVERLAPPED;
+typedef LPOVERLAPPED LPWSAOVERLAPPED;
+
+typedef struct _OVERLAPPED_ENTRY {
+    ULONG_PTR lpCompletionKey;
+    LPOVERLAPPED lpOverlapped;
+    ULONG_PTR Internal;
+    DWORD dwNumberOfBytesTransferred;
+} OVERLAPPED_ENTRY, *LPOVERLAPPED_ENTRY;
+
+// kernel32.dll
+HANDLE WINAPI CreateIoCompletionPort(
+  _In_     HANDLE    FileHandle,
+  _In_opt_ HANDLE    ExistingCompletionPort,
+  _In_     ULONG_PTR CompletionKey,
+  _In_     DWORD     NumberOfConcurrentThreads
+);
+
+BOOL WINAPI CloseHandle(
+  _In_ HANDLE hObject
+);
+
+BOOL WINAPI PostQueuedCompletionStatus(
+  _In_     HANDLE       CompletionPort,
+  _In_     DWORD        dwNumberOfBytesTransferred,
+  _In_     ULONG_PTR    dwCompletionKey,
+  _In_opt_ LPOVERLAPPED lpOverlapped
+);
+
+BOOL WINAPI GetQueuedCompletionStatusEx(
+  _In_  HANDLE             CompletionPort,
+  _Out_ LPOVERLAPPED_ENTRY lpCompletionPortEntries,
+  _In_  ULONG              ulCount,
+  _Out_ PULONG             ulNumEntriesRemoved,
+  _In_  DWORD              dwMilliseconds,
+  _In_  BOOL               fAlertable
+);
+
+BOOL WINAPI CancelIoEx(
+  _In_     HANDLE       hFile,
+  _In_opt_ LPOVERLAPPED lpOverlapped
+);
+
+BOOL WINAPI SetConsoleCtrlHandler(
+  _In_opt_ void*            HandlerRoutine,
+  _In_     BOOL             Add
+);
+"""
+
+# cribbed from pywincffi
+# programmatically strips out those annotations MSDN likes, like _In_
+REGEX_SAL_ANNOTATION = re.compile(
+    r"\b(_In_|_Inout_|_Out_|_Outptr_|_Reserved_)(opt_)?\b"
+)
+LIB = REGEX_SAL_ANNOTATION.sub(" ", LIB)
+
+# Other fixups:
+# - get rid of FAR, cffi doesn't like it
+LIB = re.sub(r"\bFAR\b", " ", LIB)
+# - PASCAL is apparently an alias for __stdcall (on modern compilers - modern
+#   being _MSC_VER >= 800)
+LIB = re.sub(r"\bPASCAL\b", "__stdcall", LIB)
+
+ffi = cffi.FFI()
+ffi.cdef(LIB)
+
+kernel32 = ffi.dlopen("kernel32.dll")
+
+INVALID_HANDLE_VALUE = ffi.cast("HANDLE", -1)
+
+
+def raise_winerror(winerror=None, *, filename=None, filename2=None):
+    if winerror is None:
+        winerror, msg = ffi.getwinerror()
+    else:
+        _, msg = ffi.getwinerror(winerror)
+    # See:
+    # https://docs.python.org/3/library/exceptions.html#exceptions.WindowsError
+    raise OSError(0, msg, filename, winerror, filename2)
+
+
+class ErrorCodes(enum.IntEnum):
+    STATUS_TIMEOUT = 0x102
+    ERROR_IO_PENDING = 997
+    ERROR_OPERATION_ABORTED = 995
+    ERROR_ABANDONED_WAIT_0 = 735
+    ERROR_INVALID_HANDLE = 6

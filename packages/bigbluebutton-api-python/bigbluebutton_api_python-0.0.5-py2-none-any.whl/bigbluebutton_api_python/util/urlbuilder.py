bf46ddf645af4266cef4b87f0fd709709e005947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:12 +0000

    Add package bigbluebutton-api-python/0.0.5/bigbluebutton_api_python-0.0.5-py2-none-any.whl

---
+++ packages/bigbluebutton-api-python/bigbluebutton_api_python-0.0.5-py2-none-any.whl/bigbluebutton_api_python/util/urlbuilder.py
@@ -0,0 +1,44 @@
+
+from hashlib import sha1
+from re import match
+
+import sys
+if sys.version_info[0] == 2:
+    from urllib import quote
+else:
+    from urllib.request import quote
+
+
+class UrlBuilder:
+    def __init__(self, bbbServerBaseUrl, securitySalt):
+        if not match('/[http|https]:\/\/[a-zA-Z1-9.]*\/bigbluebutton\/api\//', bbbServerBaseUrl):
+            if not bbbServerBaseUrl.startswith("http://") and not bbbServerBaseUrl.startswith("https://"):
+                bbbServerBaseUrl = "http://" + bbbServerBaseUrl
+            if not bbbServerBaseUrl.endswith("/bigbluebutton/api/"):
+                bbbServerBaseUrl = bbbServerBaseUrl[:(bbbServerBaseUrl.find("/", 8)
+                    if bbbServerBaseUrl.find("/", 8) != -1 else len(bbbServerBaseUrl))] + "/bigbluebutton/api/"
+
+        self.securitySalt         = securitySalt
+        self.bbbServerBaseUrl     = bbbServerBaseUrl
+
+    def buildUrl(self, api_call, params={}):
+        url = self.bbbServerBaseUrl
+        url += api_call + "?"
+        for key, value in params.items():
+            if not isinstance(value, str):
+                value = str(value)
+            if value:
+                url += key + "=" + quote(value) + "&"
+
+        url += "checksum=" + self.__get_checksum(api_call, params)
+        return url
+
+    def __get_checksum(self, api_call, params={}):
+        secret_str = api_call
+        for key, value in params.items():
+            if value:
+                secret_str += key + "=" + value + "&"
+        if secret_str.endswith("&"):
+            secret_str = secret_str[:-1]
+        secret_str += self.securitySalt
+        return sha1(secret_str.encode('utf-8')).hexdigest()

8d3451367e43595d38ad3068dbfb3344a5f5ad40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:57 +0000

    Add package ie_mbdbl2018_riyadk/0.1/ie_mbdbl2018_riyadk-0.1.tar.gz

---
+++ packages/ie_mbdbl2018_riyadk/ie_mbdbl2018_riyadk-0.1.tar.gz/ie_mbdbl2018_riyadk-0.1/ie_mbdbl2018_riyadk/__init__.py
@@ -0,0 +1,3 @@
+def  sayhi():
+    """ function to return the string 'my first package' """
+    return("This isRiyad Kutabish first package that anyone can install it! How cool!!!")
---
+++ packages/ie_mbdbl2018_riyadk/ie_mbdbl2018_riyadk-0.1.tar.gz/ie_mbdbl2018_riyadk-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ie_mbdbl2018_riyadk',
+      version='0.1',  # Development release
+      description='Python Packaging Starting Kit - Simple Structure to creater your own package - for training purposes!',
+      url='https://github.com/Riyadkutabish',
+      author='Riyad Kutabish',
+      author_email='riyad.kutabish@student.ie.edu',
+      license='MIT',
+          packages=['ie_mbdbl2018_riyadk'],
+      zip_safe=False)

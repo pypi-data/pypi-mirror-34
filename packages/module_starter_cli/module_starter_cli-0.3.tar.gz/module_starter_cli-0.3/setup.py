2d40006cc37e440db28125e84e58ae97009a888f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:34 +0000

    Add package module_starter_cli/0.3/module_starter_cli-0.3.tar.gz

---
+++ packages/module_starter_cli/module_starter_cli-0.3.tar.gz/module_starter_cli-0.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+REQUIRED_MODULES = []
+with open('requirements.txt') as file:
+    REQUIRED_MODULES = [line.strip() for line in file]
+
+
+setup(name='module_starter_cli',
+      version='0.3',
+      description='Starter project for deploying module',
+      long_description=readme(),
+      keywords='Module starter',
+      url='http://github.com/storborg/funniest',
+      author='Cory',
+      author_email='aumitleon@gmail.com',
+      license='MIT',
+      packages=['module_scripts'],
+      install_requires=REQUIRED_MODULES,
+      entry_points={
+          'console_scripts': ['module_starter=module_scripts.command_line:main'],
+      },
+      include_package_data=True,
+      zip_safe=False)

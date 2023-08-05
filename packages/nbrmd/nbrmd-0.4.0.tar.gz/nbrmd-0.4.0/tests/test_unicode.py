56025aed540e3635e79d1e96b2ff071245e29cb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:19 +0000

    Add package nbrmd/0.4.0/nbrmd-0.4.0.tar.gz

---
+++ packages/nbrmd/nbrmd-0.4.0.tar.gz/nbrmd-0.4.0/tests/test_unicode.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+import nbrmd
+import pytest
+from .utils import list_all_notebooks
+import sys
+
+
+@pytest.mark.parametrize('nb_file', list_all_notebooks('.ipynb') +
+                         list_all_notebooks('.Rmd'))
+def test_notebook_contents_is_unicode(nb_file):
+    nb = nbrmd.readf(nb_file)
+
+    for cell in nb.cells:
+        if sys.version_info < (3, 0):
+            assert cell.source == '' or isinstance(cell.source, unicode)
+        else:
+            assert isinstance(cell.source, str)
+
+
+def test_write_non_ascii(tmpdir):
+    nb = nbrmd.reads(u'Non-ascii contênt')
+    nbrmd.writef(nb, str(tmpdir.join('notebook.Rmd')))
+    nbrmd.writef(nb, str(tmpdir.join('notebook.ipynb')))
+
+
+def test_raise_on_non_unicode():
+    py = u"""# No mention of utf8 encoding
+# here, but then
+s = u'Non-ascii contênt'
+"""

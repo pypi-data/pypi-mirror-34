1bffd53487e055c866a54db0729b9624dbf6ec78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:46 +0000

    Add package pyrap/0.0.3/pyrap-0.0.3.tar.gz

---
+++ packages/pyrap/pyrap-0.0.3.tar.gz/pyrap-0.0.3/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyrap",
+    version="0.0.3",
+    author="Toby Slight",
+    author_email="tobyslight@gmail.com",
+    description="Rsync Wrapper to Backup/Restore Users on a Host",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tslight/pyrap",
+    install_requires=['treepick', 'yorn'],
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ),
+    entry_points={
+        'console_scripts': [
+            'pyrap = pyrap.__main__:main',
+        ],
+    }
+)

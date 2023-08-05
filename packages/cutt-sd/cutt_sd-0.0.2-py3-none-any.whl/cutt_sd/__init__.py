319979fdf1bd3ab79f512d5cdb3ffd1c4d8d1148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:21 +0000

    Add package cutt-sd/0.0.2/cutt_sd-0.0.2-py3-none-any.whl

---
+++ packages/cutt-sd/cutt_sd-0.0.2-py3-none-any.whl/cutt_sd/__init__.py
@@ -0,0 +1,12 @@
+import requests
+
+name = 'cutt_sd'
+version = '0.0.2'
+
+from .zk import register_api, init, unregister_api, find_api
+
+from .service import register as http_register, unregister as http_unregister, find_api as http_find
+
+
+def get_myip():
+    return requests.get('http://10.9.88.19:5002').text

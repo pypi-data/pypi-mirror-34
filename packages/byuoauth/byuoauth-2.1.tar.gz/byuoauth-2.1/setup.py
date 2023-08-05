bdfb5cba81a8161ab998dcc94d252348d52ef378
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:23 +0000

    Add package byuoauth/2.1/byuoauth-2.1.tar.gz

---
+++ packages/byuoauth/byuoauth-2.1.tar.gz/byuoauth-2.1/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+version = 2.1
+
+setup(
+    name = "byuoauth",
+    packages = ["byuoauth"],
+    version = version,
+    description = "Scripts to easily allow BYU applications to generate OAuth tokens and JWTs for test use.",
+    author = "Eric Romrell",
+    author_email = "eric_romrell@byu.edu",
+    url = "https://github.com/byu-oit/python-token-scripts",
+    download_url = "https://github.com/byu-oit/python-token-scripts/archive/{}.tar.gz".format(version),
+    keywords = ["byu", "oauth", "token", "jwt"],
+    classifiers = [],
+    requires = ['requests']
+)

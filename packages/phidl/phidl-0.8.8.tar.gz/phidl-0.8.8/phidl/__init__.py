868bce886c9a579a220df66e28963fcbd29cc9ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:34 +0000

    Add package phidl/0.8.8/phidl-0.8.8.tar.gz

---
+++ packages/phidl/phidl-0.8.8.tar.gz/phidl-0.8.8/phidl/__init__.py
@@ -0,0 +1,6 @@
+from phidl.device_layout import Device, Port, Layer, LayerSet
+from phidl.device_layout import quickplot
+from phidl.device_layout import make_device
+from phidl.quickplotter import quickplot2
+from phidl.device_layout import __version__, reset
+from phidl.geometry import device_lru_cache

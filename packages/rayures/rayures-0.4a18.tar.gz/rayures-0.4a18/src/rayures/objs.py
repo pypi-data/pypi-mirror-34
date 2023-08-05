b4da155121b7d83cc5bd4416a059bdc45fe8d702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:13 +0000

    Add package rayures/0.4a18/rayures-0.4a18.tar.gz

---
+++ packages/rayures/rayures-0.4a18.tar.gz/rayures-0.4a18/src/rayures/objs.py
@@ -0,0 +1,10 @@
+class Price:
+    def __init__(self, amount, currency):
+        self.amount = amount
+        self.currency = currency
+
+    def __str__(self):
+        return f'{self.currency} {self.amount}'
+
+    def __eq__(self, other):
+        return self.amount == other.amount and self.currency == other.currency

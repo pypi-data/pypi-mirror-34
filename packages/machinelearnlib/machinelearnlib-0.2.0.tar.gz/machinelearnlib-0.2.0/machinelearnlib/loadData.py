dfd8acfb532828aa90c92e11df2dd744a308ba2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:45 +0000

    Add package machinelearnlib/0.2.0/machinelearnlib-0.2.0.tar.gz

---
+++ packages/machinelearnlib/machinelearnlib-0.2.0.tar.gz/machinelearnlib-0.2.0/machinelearnlib/loadData.py
@@ -0,0 +1,49 @@
+import numpy as np
+import pandas as pd
+from numpy import genfromtxt
+
+def loadData(mlModel):
+    """
+    Load in data from the data folder using the file names specified in teh mlModel
+    """
+
+    if "trainDataFileName" in mlModel:
+        trainFileName = './data/' + mlModel['trainDataFileName']
+        trainData = pd.read_csv(trainFileName)
+        mlModel['features'] = np.array(trainData.iloc[:,:-1])
+        mlModel['labels'] = np.array(trainData.iloc[:,-1])
+
+    if "testDataFileName" in mlModel:
+        testFileName = './data/' + mlModel['testDataFileName']         
+        testData = pd.read_csv(testFileName)
+        mlModel['testFeatures'] = np.array(testData.iloc[:,:-1])
+        mlModel['testLabels'] = np.array(testData.iloc[:,-1])
+
+
+def loadIrisData(mlModel):
+    """
+    Load the Iris Data Set from the data folder into the mlModel
+    """
+
+    if "trainDataFileName" in mlModel:
+        trainFileName = './data/' + mlModel['trainDataFileName']
+        trainData = pd.read_csv(trainFileName)
+
+        # Convert the labels 
+        unique = {}
+        uniqueID = 0
+        row = 0
+        for label in trainData.iloc[:,-1]:
+            if label not in unique:
+                unique[label] = uniqueID
+                uniqueID += 1
+
+            trainData.iloc[row, -1] = unique[trainData.iloc[row, -1]]
+            row += 1
+
+        mlModel['features'] = np.array(trainData.iloc[:,:-1])
+        mlModel['labels'] = np.array(trainData.iloc[:,-1])
+
+
+
+
---
+++ packages/machinelearnlib/machinelearnlib-0.2.0.tar.gz/machinelearnlib-0.2.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+from os import path
+
+setup(name='machinelearnlib',
+      version='0.2.0',
+      description='Implementation of common machine learning algorithms',
+      url='https://github.com/Kai-Bailey/machinelearnlib',
+      author='Kai Bailey',
+      author_email='kbailey1@ualberta.ca',
+      license='MIT',
+      keywords='machine learning data science',
+      packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+      install_requires=['numpy', 'matplotlib'],
+      entry_points={'console_scripts':['ml=machinelearnlib.machinelearnlib:main']})

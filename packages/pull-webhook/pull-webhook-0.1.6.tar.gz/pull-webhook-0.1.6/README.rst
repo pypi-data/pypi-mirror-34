2b88e35b185dc56442c9bce33f54889c251fc331
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:27 +0000

    Add package pull-webhook/0.1.6/pull-webhook-0.1.6.tar.gz

---
+++ packages/pull-webhook/pull-webhook-0.1.6.tar.gz/pull-webhook-0.1.6/README.rst
@@ -0,0 +1,37 @@
+============
+Pull Webhook
+============
+
+
+.. image:: https://img.shields.io/pypi/v/pull-webhook.svg
+        :target: https://pypi.python.org/pypi/pull-webhook
+
+.. image:: https://img.shields.io/travis/jacknova/pull-webhook.svg
+        :target: https://travis-ci.org/jacknova/pull-webhook
+
+.. image:: https://readthedocs.org/projects/pull-webhook/badge/?version=latest
+        :target: https://pull-webhook.readthedocs.io/en/latest/?badge=latest
+        :alt: Documentation Status
+
+
+
+
+Pull latest changes from a git remote
+
+
+* Free software: MIT license
+* Documentation: https://pull-webhook.readthedocs.io.
+
+
+Features
+--------
+
+* TODO
+
+Credits
+-------
+
+This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
---
+++ packages/pull-webhook/pull-webhook-0.1.6.tar.gz/pull-webhook-0.1.6/pull_webhook/pull_webhook.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""Main module."""
+import os
+import subprocess
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+DEFAULT_PORT = 8888
+DEFAULT_REMOTE = "origin"
+DEFAULT_BRANCH = "master"
+
+
+class WebHookHandler(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+
+    def do_POST(self):
+        self._set_headers()
+        Puller.git_pull()
+        self.wfile.write("OK".encode("UTF-8", "replace"))
+
+
+class Puller(object):
+
+    remote = DEFAULT_REMOTE
+    branch = DEFAULT_BRANCH
+    repository_directory = os.path.dirname(os.path.abspath(__file__))
+
+    @classmethod
+    def git_pull(cls):
+        result = subprocess.call([
+            "git", "--git-dir", cls.repository_directory + '/.git', "pull",
+            cls.remote, cls.branch
+        ])
+        if result == 0:
+            print("DONE")
+        else:
+            print("RETURN CODE IS NOT 0 BUT " + str(result))
+
+
+def main(port=DEFAULT_PORT):
+    server_address = ('', port)
+    httpd = HTTPServer(server_address, WebHookHandler)
+    print('Starting httpd...')
+    httpd.serve_forever()
+
+
+if __name__ == '__main__':
+    main()

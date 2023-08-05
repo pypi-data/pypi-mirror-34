3312568f8ab2ec723d27ebac18679b00ac530a8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:46 +0000

    Add package gris/0.5.2/gris-0.5.2.tar.gz

---
+++ packages/gris/gris-0.5.2.tar.gz/gris-0.5.2/setup.py
@@ -0,0 +1,22 @@
+#Copyright (C) 2013 Angel Yanguas-Gil
+#This program is free software, licensed under GPLv2 or later.
+#A copy of the GPLv2 license is provided in the root directory of
+#the source code.
+
+from distutils.core import setup
+setup(name = 'gris',
+    description = "parser of RIS and WOK bibliography file format",
+    version = '0.5.2',
+    author = "Angel Yanguas-Gil",
+    author_email = "angel.yanguas@gmail.com",
+    download_url = "https://github.com/anglyan/gris/tarball/0.5.2",
+    url = "https://github.com/anglyan/gris.git",
+    py_modules = ['gris'],
+    package_dir = {'' : 'gris'},
+    classifiers = ["Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "Environment :: Console",
+        "Topic :: Scientific/Engineering",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Topic :: Utilities"]
+)

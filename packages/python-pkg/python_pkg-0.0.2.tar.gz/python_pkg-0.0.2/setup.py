b23ba112503b8e45644aa0f5be76a3eeea72d509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:56 +0000

    Add package python-pkg/0.0.2/python_pkg-0.0.2.tar.gz

---
+++ packages/python-pkg/python_pkg-0.0.2.tar.gz/python_pkg-0.0.2/setup.py
@@ -0,0 +1,22 @@
+﻿import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python_pkg",
+    version="0.0.2",
+    author="Noelle Rivera",
+    author_email="noelle.rivera.516@gmail.com",
+    description="Un paquete de NLTK y Scrapy por SEO",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
+

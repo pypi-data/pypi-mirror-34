f6ac82bd4b515ab90d5cf16827109fa8170df56e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:41 +0000

    Add package ie_mbdbl2018_rbastardo/0.1/ie_mbdbl2018_rbastardo-0.1.tar.gz

---
+++ packages/ie_mbdbl2018_rbastardo/ie_mbdbl2018_rbastardo-0.1.tar.gz/ie_mbdbl2018_rbastardo-0.1/ie_mbdbl2018_rbastardo/__init__.py
@@ -0,0 +1,3 @@
+def  datareader_doc():
+    """ function to return the string: This is Rafael Bastardo's first package that anyone can install it! How cool!!!"""
+    return("This is Rafael Bastardo's first package that anyone can install it! How cool!!!")
---
+++ packages/ie_mbdbl2018_rbastardo/ie_mbdbl2018_rbastardo-0.1.tar.gz/ie_mbdbl2018_rbastardo-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ie_mbdbl2018_rbastardo',
+      version='0.1',  # Development release
+      description='Python Packaging Starting Kit - Simple Structure to creater your own package - for training purposes!',
+      url='https://github.com/rbastardo13/ie_mbdbl2018_rbastardo',
+      author='Rafael Bastardo',
+      author_email='rbastardo@student.ie.edu',
+      license='MIT',
+          packages=['ie_mbdbl2018_rbastardo'],
+      zip_safe=False)

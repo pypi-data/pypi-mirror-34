ebb2a3afab45ede44442b68542e68d9265fcda06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:44:50 +0000

    Add package cognite-sdk/0.8.11/cognite_sdk-0.8.11-py3-none-any.whl

---
+++ packages/cognite-sdk/cognite_sdk-0.8.11-py3-none-any.whl/cognite/__init__.py
@@ -0,0 +1,4 @@
+
+'Cognite API SDK\n\nThis package has been created to ensure excellent excellent user experience for data scientists using the Cognite\nData Platform (CDP).\n'
+__all__ = ['v04', 'v05', 'preprocessing', 'config']
+__version__ = '0.8.11'

4fa35ba4ddf26abff3d1e3f332ef797fb5bda67a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:23 +0000

    Add package conan_sword_and_sorcery/0.0.dev125/conan_sword_and_sorcery-0.0.dev125.tar.gz

---
+++ packages/conan_sword_and_sorcery/conan_sword_and_sorcery-0.0.dev125.tar.gz/conan_sword_and_sorcery-0.0.dev125/tests/test_ci/test_compilers/test_CompilerRegistry.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+
+
+from conan_sword_and_sorcery.ci.compilers import CompilerRegistry
+from tests.utils import TestCaseEnvClean
+from conan_sword_and_sorcery.utils.environ import context_env
+
+
+class TestCompilerRegistry(TestCaseEnvClean):
+
+    def setUp(self):
+        self.registry = CompilerRegistry()
+        self.os = ["Windows", "Linux", "Macos",]
+
+    def test_base(self):
+        self.assertEqual(len(list(self.registry.get_compilers(os=self.os))), 132)
+        self.assertEqual(len(list(self.registry.get_compilers(os=self.os, arch=['x86', ]))), 66)
+        self.assertEqual(len(list(self.registry.get_compilers(os=self.os, arch=['x86', ], version=[("gcc", "7"), ]))), 4)
+        self.assertEqual(len(list(self.registry.get_compilers(os=self.os, arch=['x86', ], version=[("gcc", "7"), ("Visual Studio", "12"), ]))), 8)
+
+    def test_invalid_argument(self):
+        self.assertEqual(len(list(self.registry.get_compilers(os=self.os, invalid_arg=["x86", ]))), 132)  # TODO: It may be invalid for some, but valid for others, so do not discard compilers if you pass them strange arguments
+
+    def test_invalid_value(self):
+        self.assertEqual(len(list(self.registry.get_compilers(os=self.os, arch=["x86000", ]))), 0)
+
+
+class TestCompilerRegistryEnvironmentFilters(TestCaseEnvClean):
+    def setUp(self):
+        self.registry = CompilerRegistry()
+
+    def test_one_compiler(self):
+        with context_env(CONAN_VISUAL_VERSIONS='10'):
+            self.assertDictEqual(self.registry.environment_filters(),
+                                 {'version': {('Visual Studio', '10'),
+                                              }})
+
+        with context_env(CONAN_VISUAL_VERSIONS='10,12'):
+            self.assertDictEqual(self.registry.environment_filters(),
+                                 {'version': {('Visual Studio', '10'),
+                                              ('Visual Studio', '12')
+                                              }})
+
+    def test_several_compilers(self):
+        with context_env(CONAN_VISUAL_VERSIONS='10', CONAN_GCC_VERSIONS='3,4'):
+            self.assertDictEqual(self.registry.environment_filters(),
+                                 {'version': {('gcc', '3'),
+                                              ('gcc', '4'),
+                                              ('Visual Studio', '10')
+                                              }})
+
+    def test_several_filters(self):
+        with context_env(CONAN_VISUAL_VERSIONS='10', CONAN_VISUAL_RUNTIMES='MT,MTd'):
+            self.assertDictEqual(self.registry.environment_filters(),
+                                 {'version': {('Visual Studio', '10')
+                                              },
+                                  'runtime': {"MT", "MTd"},})
+
+
+if __name__ == '__main__':
+    unittest.main()

9858c879446f5244db3c7b8d02679d3f7ed16002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:22 +0000

    Add package ChatExchange/0.0.4/ChatExchange-0.0.4.tar.gz

---
+++ packages/ChatExchange/ChatExchange-0.0.4.tar.gz/ChatExchange-0.0.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name='ChatExchange',
+    version='0.0.4',
+    url='https://github.com/Manishearth/ChatExchange',
+    packages=[
+        'chatexchange'
+    ],
+    install_requires=[
+        'beautifulsoup4>=4.3.2',
+        'requests>=2.2.1',
+        'websocket-client>=0.13.0',
+        # only for dev:
+        'coverage>=4.5.0',
+        'epydoc>=3.0.1',
+        'httmock>=1.2.2',
+        'pytest-timeout>=0.3',
+        'pytest>=3.4.2',
+        'py>=1.5.0',
+        'bpython>=0.16'
+    ]
+)

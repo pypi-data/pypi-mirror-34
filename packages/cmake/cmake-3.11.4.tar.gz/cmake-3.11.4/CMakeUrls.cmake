7552c2358e27dd9cf485b229fea2751686c87923
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:48 +0000

    Add package cmake/3.11.4/cmake-3.11.4.tar.gz

---
+++ packages/cmake/cmake-3.11.4.tar.gz/cmake-3.11.4/CMakeUrls.cmake
@@ -0,0 +1,26 @@
+
+#-----------------------------------------------------------------------------
+# CMake sources
+set(unix_source_url       "https://cmake.org/files/v3.11/cmake-3.11.2.tar.gz")
+set(unix_source_sha256    "5ebc22bbcf2b4c7a20c4190d42c084cf38680a85b1a7980a2f1d5b4a52bf5248")
+
+set(windows_source_url    "https://cmake.org/files/v3.11/cmake-3.11.2.zip")
+set(windows_source_sha256 "dc24bbbebb428ad7e215e5052eca481b68104dcab4c472cbedd6e5d2dbb23d81")
+
+#-----------------------------------------------------------------------------
+# CMake binaries
+
+set(linux32_binary_url    "NA")  # Linux 32-bit binaries not available
+set(linux32_binary_sha256 "NA")
+
+set(linux64_binary_url    "https://cmake.org/files/v3.11/cmake-3.11.2-Linux-x86_64.tar.gz")
+set(linux64_binary_sha256 "76b745e84ee24323154163f4ab8419076db0c98b89aec05e426ed2f9347a6f62")
+
+set(macosx_binary_url    "https://cmake.org/files/v3.11/cmake-3.11.2-Darwin-x86_64.tar.gz")
+set(macosx_binary_sha256 "1baca0ccec9b412a83a7001cf8dbfd4f80886c42a99d8855816115a6a3a0e672")
+
+set(win32_binary_url    "https://cmake.org/files/v3.11/cmake-3.11.2-win32-x86.zip")
+set(win32_binary_sha256 "0d8bf9bf56519733a09bd9f39423514adfbb6513ed4e0508a3222e31f1f8264d")
+
+set(win64_binary_url    "https://cmake.org/files/v3.11/cmake-3.11.2-win64-x64.zip")
+set(win64_binary_sha256 "8c998fd7c278169e44f19f546de2c9a275e2acc8dbf858f0112a02242dfbbcd2")

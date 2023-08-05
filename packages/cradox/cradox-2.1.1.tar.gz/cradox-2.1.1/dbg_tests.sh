521be2773307c1d8627a0aec42df68fa555c45c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:49 +0000

    Add package cradox/2.1.1/cradox-2.1.1.tar.gz

---
+++ packages/cradox/cradox-2.1.1.tar.gz/cradox-2.1.1/dbg_tests.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -e 
+
+cleanup(){
+    rm -f a.out test.gdb
+}
+
+trap cleanup EXIT
+
+if [ "$1" == "-d" ] ; then
+	cat > test.gdb <<EOF
+set pagination off
+cy run
+cy bt
+EOF
+	opt="-dbg"
+	opt_setup="--cython-gdb"
+	python="python-dbg"
+	launcher="cygdb . -- --batch --command=test.gdb --args"
+	shift
+elif [ "$1" == "-b" ] ; then
+	cat > test.gdb <<EOF
+set pagination off
+run
+bt
+EOF
+	opt="-dbg"
+	opt_setup="--cython-gdb"
+	python="python-dbg"
+	launcher="gdb --batch --command=test.gdb --args"
+	shift
+elif [ "$1" == "-3" ]; then
+	shift
+	python="python3"
+	launcher=
+else
+	python="python"
+	launcher=
+fi
+
+[ "$@" ] && tests=":$@"
+
+ceph osd unset noup
+for i in $(rados lspools | grep -e foo -e test_pool -e foo-cache -e 黄 -e 黅) ; do 
+	rados rmpool $i $i --yes-i-really-really-mean-it ; 
+done ; 
+
+$python -c "import setup; setup.setup_hook()"
+$python setup.py build_ext --inplace $opt_setup --build-lib .
+$launcher $python $(which nosetests) -vds test_rados$tests
---
+++ packages/cradox/cradox-2.1.1.tar.gz/cradox-2.1.1/setup.py
@@ -0,0 +1,76 @@
+#
+#
+#
+
+from __future__ import print_function
+
+import collections
+import os
+import os.path
+import sys
+
+from distutils import ccompiler
+from setuptools import Extension
+from setuptools import setup
+
+ceph_version_map = collections.OrderedDict(sorted({
+    "hammer": "rados_pool_get_base_tier",
+    "jewel": "rados_inconsistent_pg_list",
+    "kraken": "rados_aio_exec",
+    "luminous": "rados_read_op_omap_get_keys2",
+}.items(), key=lambda t: t[0]))
+
+
+def log(msg):
+    print(msg, file=sys.stderr)
+
+
+def setup_hook(cmd_obj, version=None):
+    if version == "latest":
+        version = sorted(ceph_version_map.keys())[-1]
+    elif version is None:
+        comp = ccompiler.new_compiler(force=True, verbose=True)
+        for potential_version, method in ceph_version_map.items():
+            msg = "* checking for librados >= %s (with function %s)" % (
+                potential_version, method)
+            log(msg)
+            found = comp.has_function(method, libraries=['rados'])
+            if found:
+                version = potential_version
+                log("%s done: FOUND" % msg)
+            else:
+                log("%s done: NOT FOUND" % msg)
+                break
+
+        if not version:
+            raise Exception("gcc, python-dev, librados2 or "
+                            "librados-dev >= 0.80 are missing")
+
+    log("building cradox with %s api compatibility" % version)
+
+    # Generate the source file from template
+    from jinja2 import Environment
+    env = Environment(trim_blocks=True, lstrip_blocks=True)
+    cradox_out = os.path.join(os.path.dirname(__file__), 'cradox.pyx')
+    cradox_in = "%s.in" % cradox_out
+    with open(cradox_in, 'r') as src:
+        with open(cradox_out, 'w') as dst:
+            template = env.from_string(src.read())
+            dst.write(template.render(version=version))
+    test_out = os.path.join(os.path.dirname(__file__), 'test_rados.py')
+    test_in = "%s.in" % test_out
+    with open(test_in, 'r') as src:
+        with open(test_out, 'w') as dst:
+            template = env.from_string(src.read())
+            dst.write(template.render(version=version))
+
+
+if __name__ == '__main__':
+    setup(
+        # cradox segfault with 0.28.0
+        setup_requires=['pbr',
+                        'Cython!=0.28.0,!=0.28.1,!=0.28.2,!=0.28.3,!=0.28.4',
+                        'Jinja2'],
+        pbr=True,
+        ext_modules=[Extension("cradox", ["cradox.pyx"], libraries=["rados"])],
+    )

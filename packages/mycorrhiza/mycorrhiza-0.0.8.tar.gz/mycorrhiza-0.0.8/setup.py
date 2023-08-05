ec4637e037e1d575c534397ad2a64b8b34c7a17b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:23 +0000

    Add package mycorrhiza/0.0.8/mycorrhiza-0.0.8.tar.gz

---
+++ packages/mycorrhiza/mycorrhiza-0.0.8.tar.gz/mycorrhiza-0.0.8/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mycorrhiza",
+    version="0.0.8",
+    author="Jeremy Georges-Filteau",
+    author_email="jeremy.georges-filteau@mail.mcgill.ca",
+    description="Mycorrhiza population assignment tools.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jgeofil/mycorrhiza",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'tqdm',
+        'numpy',
+        'scikit-learn',
+        'pathos'
+    ],
+    python_requires='>=3',
+    entry_points={
+        'console_scripts': [
+            'mycocv = scripts:mycocv',
+            ]
+    },
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent",
+    ),
+)

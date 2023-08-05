00ec60447add856044d3927855b4baeca41b1b56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:27 +0000

    Add package tuStockSpider/0.12/tuStockSpider-0.12.tar.gz

---
+++ packages/tuStockSpider/tuStockSpider-0.12.tar.gz/tuStockSpider-0.12/setup.py
@@ -0,0 +1,21 @@
+#-*- encoding: UTF-8 -*-
+from setuptools import setup
+
+setup(
+    name = "tuStockSpider",          # 包名
+    version = "0.12",              # 版本信息
+    author="cansijyun",
+    author_email="can_sijyun@yahoo.co.jp",
+    description="China stock market everyday history data downloader",
+    keywords="China stock market everyday history data downloader",
+    #long_description=open("README.rst").read(),
+    license="MIT",
+    url="https://github.com/cansijyun/StockHistorySpider",
+    packages = ['tuStockSpider'],  # 要打包的项目文件夹
+    include_package_data=True,    # 自动打包文件夹内所有数据
+    zip_safe=True,                # 设定项目包为安全，不用每次都检测其安全性
+    install_requires = [          # 安装依赖的其他包
+    'lxml',
+    'requests',
+    ],
+ )

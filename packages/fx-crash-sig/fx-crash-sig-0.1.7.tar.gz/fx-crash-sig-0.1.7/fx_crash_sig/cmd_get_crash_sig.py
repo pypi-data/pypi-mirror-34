fcac9e1ab5d9c32127c29ad6c3222b8931b73826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:25 +0000

    Add package fx-crash-sig/0.1.7/fx-crash-sig-0.1.7.tar.gz

---
+++ packages/fx-crash-sig/fx-crash-sig-0.1.7.tar.gz/fx-crash-sig-0.1.7/fx_crash_sig/cmd_get_crash_sig.py
@@ -0,0 +1,37 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from __future__ import print_function
+
+import argparse
+import sys
+
+import ujson as json
+
+from fx_crash_sig.crash_processor import CrashProcessor
+
+DESCRIPTION = """
+Takes raw crash trace and symbolicates it to return the crash signature
+"""
+
+
+def cmdline():
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser.add_argument('-v', '--verbose', action='store_true')
+    args = parser.parse_args()
+
+    crash_processor = CrashProcessor(verbose=args.verbose)
+    try:
+        crash_data = json.loads(sys.stdin.read())
+    except ValueError:
+        print('fx-crash-sig: Failed: Invalid input format')
+        return
+
+    try:
+        signature = crash_processor.get_signature(crash_data)
+        if signature is not None:
+            print(json.dumps(signature))
+    except Exception as e:
+        if args.verbose:
+            print('fx-crash-sig: Failed: {}'.format(e.message))

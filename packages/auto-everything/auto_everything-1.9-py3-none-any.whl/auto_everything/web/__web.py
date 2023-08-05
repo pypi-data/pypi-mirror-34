81f6c56b1e39800a6d5e369a350ede37edc94b1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:24 +0000

    Add package auto-everything/1.9/auto_everything-1.9-py3-none-any.whl

---
+++ packages/auto-everything/auto_everything-1.9-py3-none-any.whl/auto_everything/web/__web.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+
+class Selenium():
+    def __init__(self, url):
+        try:
+            self.driver = webdriver.Chrome()
+        except Exception as e:
+            print(e)
+            try:
+                self.driver = webdriver.Firefox()
+            except Exception as e:
+                print(e)
+                self.driver = webdriver.PhantomJS()
+        self.driver.get(url)
+        
+    def wait_until_exists(self, xpath, timeout=600):
+        try:
+            w = WebDriverWait(self.driver, timeout)
+            w.until(EC.presence_of_element_located((By.XPATH, xpath)))
+            elements = self.driver.find_elements_by_xpath(xpath)
+            return elements
+        except Exception as e:
+            print(e)
+            return None
+
+    def click(self, element):
+        self.driver.execute_script("arguments[0].click();", element)

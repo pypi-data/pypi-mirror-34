e91416ffb2fe164a6624be4fb9ddb17273513d95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:46 +0000

    Add package lastpy/1.0.7/lastpy-1.0.7-py2.py3-none-any.whl

---
+++ packages/lastpy/lastpy-1.0.7-py2.py3-none-any.whl/lastpy/__init__.py
@@ -0,0 +1,60 @@
+# Last.fm Syncing Tool
+# Lean, fast, and functional
+import os
+import time
+import requests
+import hashlib
+
+api_head = 'http://ws.audioscrobbler.com/2.0/'
+secret = os.environ['LAST_FM_API_SECRET']
+
+def authorize(user_token):
+    params = {
+            'api_key': os.environ['LAST_FM_API'],
+            'method': 'auth.getSession',
+            'token': user_token
+            }
+    requestHash = hashRequest(params, secret)
+    params['api_sig'] = requestHash
+    apiResp = requests.post(api_head, params)
+    return apiResp.text
+
+def nowPlaying(song_name, artist_name, session_key):
+    params = {
+            'method': 'track.updateNowPlaying',
+            'api_key': os.environ['LAST_FM_API'],
+            'track': song_name,
+            'artist': artist_name,
+            'sk': session_key
+            }
+    requestHash = hashRequest(params, secret)
+    params['api_sig'] = requestHash
+    apiResp = requests.post(api_head, params)
+    return apiResp.text
+
+def scrobble(song_name, artist_name, session_key):
+    # Currently this sort of cheats the timestamp protocol
+    params = {
+            'method': 'track.scrobble',
+            'api_key': os.environ['LAST_FM_API'],
+            'timestamp': str( int(time.time() - 30) ),
+            'track': song_name,
+            'artist': artist_name,
+            'sk': session_key
+            }
+    requestHash = hashRequest(params, secret)
+    params['api_sig'] = requestHash
+    apiResp = requests.post(api_head, params)
+    return apiResp.text
+
+def hashRequest(obj, secretKey):
+    string = ''
+    items = obj.keys()
+    items.sort()
+    for i in items:
+        string += i
+        string += obj[i]
+    string += secretKey
+    stringToHash = string.encode('utf8')
+    requestHash = hashlib.md5(stringToHash).hexdigest()
+    return requestHash
---
+++ packages/lastpy/lastpy-1.0.7-py2.py3-none-any.whl/lastpy/extras.py
@@ -0,0 +1,29 @@
+# Here are a few functions that aren't critical to properly scrobbling a song
+# However, I they were fun to put together and might be of use to others.
+import requests
+import json
+import datetime
+import os
+
+api_root = 'http://ws.audioscrobbler.com/2.0/'
+
+def user_tracks(user_name, page):
+    resp = requests.get(api_root + '?method=user.getrecenttracks&user=' + user_name + '&api_key=' + os.environ['LAST_FM_API'] + '&page=' + str(page) + '&format=json')
+    return json.loads(resp.text)
+
+def user_weekly_tracks(user_name):
+    resp = requests.get(api_root + '?method=user.getweeklytrackchart&user=' + user_name + '&api_key=' + os.environ['LAST_FM_API'] + '&format=json')
+    return json.loads(resp.text)
+
+def user_daily_tracks(user_name, days=0, limit=200):
+    dayStart = datetime.datetime.today().replace(hour=0, minute=0, second=0, microsecond=0) + datetime.timedelta(days=-days)
+    dayStart = dayStart.strftime('%s')
+    dayEnd = datetime.datetime.today().replace(hour=0, minute=0, second=0, microsecond=0) + datetime.timedelta(days=-days+1)
+    dayEnd = dayEnd.strftime('%s')
+    resp = requests.get(api_root + '?method=user.getrecenttracks&user=' + user_name + '&api_key=' + os.environ['LAST_FM_API'] + '&from=' + str(dayStart) + '&to' + str(dayEnd) + '&limit=' + str(limit) +'&format=json')
+    return json.loads(resp.text)
+
+def user_yearly_tracks(user_name):
+    dayStart = datetime.datetime.today().replace(month=1, day=1, hour=0, minute=0, second=0, microsecond=0).strftime("%s")
+    resp = requests.get(api_root + '?method=user.getrecenttracks&user=' + user_name + '&api_key=' + os.environ['LAST_FM_API'] + '&from=' + str(dayStart) + '&limit=1000' +'&format=json&page=1')
+    return json.loads(resp.text)

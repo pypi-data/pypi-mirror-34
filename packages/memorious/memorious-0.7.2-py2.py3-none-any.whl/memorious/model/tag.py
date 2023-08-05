e98419e3f0064816a5f3c42e48811e69d9a16104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:02 +0000

    Add package memorious/0.7.2/memorious-0.7.2-py2.py3-none-any.whl

---
+++ packages/memorious/memorious-0.7.2-py2.py3-none-any.whl/memorious/model/tag.py
@@ -0,0 +1,32 @@
+import json
+import logging
+
+from memorious.model.common import Base
+from memorious.util import make_key
+
+log = logging.getLogger(__name__)
+
+
+class Tag(Base):
+    """A simple key/value table used to store interim results."""
+
+    @classmethod
+    def save(cls, crawler, key, value):
+        data = json.dumps(value)
+        key = make_key(crawler, "tag", key)
+        cls.conn.set(key, data, ex=crawler.expire)
+
+    @classmethod
+    def find(cls, crawler, key):
+        value = cls.conn.get(make_key(crawler, "tag", key))
+        if value is not None:
+            return json.loads(value)
+
+    @classmethod
+    def exists(cls, crawler, key):
+        return cls.conn.exists(make_key(crawler, "tag", key))
+
+    @classmethod
+    def delete(cls, crawler):
+        for key in cls.conn.scan_iter(make_key(crawler, "tag", "*")):
+            cls.conn.delete(key)

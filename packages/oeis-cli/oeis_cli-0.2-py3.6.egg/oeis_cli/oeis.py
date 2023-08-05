3a493d0830e1be29e28f1a0b8b77283ab9928cf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:59 +0000

    Add package oeis-cli/0.2/oeis_cli-0.2-py3.6.egg

---
+++ packages/oeis-cli/oeis_cli-0.2-py3.6.egg/oeis_cli/oeis.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2018 Dominic Benjamin
+#
+# The author is not affiliated with the On-Line Encyclopedia of Integer
+# Sequences (OEIS).
+#
+# For more information on OEIS, visit https://oeis.org/
+#
+# Main logic - parsing arguments and calling relevant functionality.
+
+import argparse
+import oeis_cli.process as process
+import oeis_cli.search as search
+
+def main():
+    parser = argparse.ArgumentParser(description='Lookup an integer sequence.')
+    search_terms = parser.add_argument_group()
+    search_terms.add_argument('--name', '-n', 
+                              help='name of sequence to search for',
+                              default = "")
+    search_terms.add_argument('terms', type=int, nargs='*', default=[],
+                               help='integer terms to search for')
+    parser.add_argument('--unordered', '-u',
+                        help='ignore order of terms',
+                        action='store_true')
+    args = parser.parse_args()
+    if args.terms == [] and args.name == "":
+        parser.print_help()
+    else:
+        data = search.get_data(args.terms, args.name, args.unordered) 
+        print(process.format_data(data))
+

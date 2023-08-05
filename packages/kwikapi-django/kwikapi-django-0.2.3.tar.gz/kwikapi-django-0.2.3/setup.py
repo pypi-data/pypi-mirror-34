39fa3028cde7a6e99f2fd3cb7e68d594bee8028b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:04 +0000

    Add package kwikapi-django/0.2.3/kwikapi-django-0.2.3.tar.gz

---
+++ packages/kwikapi-django/kwikapi-django-0.2.3.tar.gz/kwikapi-django-0.2.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+version = '0.2.3'
+setup(
+    name="kwikapi-django",
+    version=version,
+    packages=['kwikapi.django'],
+    include_package_data=True,
+    license='MIT License',  # example license
+    description='Quickest way to build powerful HTTP APIs in Python',
+    url='https://github.com/deep-compute/kwikapi.django',
+    download_url="https://github.com/deep-compute/kwikapi.django/tarball/%s" % version,
+    author='Deep Compute, LLC',
+    author_email='contact@deepcompute.com',
+    install_requires=[
+    'django==1.11',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)

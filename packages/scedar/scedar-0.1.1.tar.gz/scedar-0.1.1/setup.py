2c7f3c76dce746931554525a386c88acc11faf8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:35 +0000

    Add package scedar/0.1.1/scedar-0.1.1.tar.gz

---
+++ packages/scedar/scedar-0.1.1.tar.gz/scedar-0.1.1/setup.py
@@ -0,0 +1,57 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='scedar',
+    version='0.1.1',
+    description='Single-cell explorative data analysis for RNA-Seq',
+    url='http://github.com/logstar/scedar',
+    author='Yuanchao Zhang',
+    author_email='logstarx@gmail.com',
+    license='MIT',
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    python_requires='>=3.5, <3.7',
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'pandas',
+        'seaborn',
+        'sklearn',
+        'xgboost',
+        'networkx',
+        'fa2',
+        'umap-learn'
+    ],
+    setup_requires=[
+        'pytest-mpl',
+        'pytest-xdist',
+        'pytest-cov',
+        'pytest-runner',
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'pandas',
+        'seaborn',
+        'sklearn',
+        'xgboost',
+        'networkx',
+        'fa2',
+        'umap-learn==0.3.0'
+    ],
+    tests_require=[
+        'pytest'
+    ],
+)

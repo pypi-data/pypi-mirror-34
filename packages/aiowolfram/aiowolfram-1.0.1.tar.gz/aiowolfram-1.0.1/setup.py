6cb3a3adc70e7c262d2300d6320cf2feb2b051f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:24 +0000

    Add package aiowolfram/1.0.1/aiowolfram-1.0.1.tar.gz

---
+++ packages/aiowolfram/aiowolfram-1.0.1.tar.gz/aiowolfram-1.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup (
+	name = 'aiowolfram',
+	version = '1.0.1',
+	description = 'an async wrapper for wolfram-alphas web api',
+	url = 'https://github.com/Apache-HB/aiowolfram',
+	author = 'Elliot Haisley',
+	author_email = 'apachehb@gmail.com',
+	license = 'Apache2.0',
+	packages = ['aiowolfram'],
+	install_requires = [
+		'aiohttp'
+	],
+	keywords = [
+		'wolfram',
+		'wolframalpha',
+		'wolfram-alpha',
+		'async',
+		'json'
+	],
+	zip_safe = False
+)

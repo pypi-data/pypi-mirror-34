04aa4e05ffa6379369cab73e62dbc89ff2029f24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:04 +0000

    Add package inlineplz/0.37.15/inlineplz-0.37.15-py2.py3-none-any.whl

---
+++ packages/inlineplz/inlineplz-0.37.15-py2.py3-none-any.whl/inlineplz/parsers/codenarc.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from inlineplz.parsers.base import ParserBase
+
+
+class CodenarcParser(ParserBase):
+    """Parse Codenarc output."""
+
+    def parse(self, lint_data):
+        messages = set()
+        path = ""
+        msg = ""
+        line_no = -1
+        for line in lint_data.split("\n"):
+            parts = []
+            try:
+                line = line.strip()
+                if line.startswith("File:"):
+                    path = line.split("File:")[-1].strip()
+                    continue
+                if line.startswith("Violation:"):
+                    parts = line.split()
+                    line_no = int(parts[3].split("=")[-1])
+                    msg = " ".join(parts[4:-1]).split("Src=")[0]
+                else:
+                    msg += "\n" + line.split("Src=")[0]
+                if "Src=" in line:
+                    messages.add((path, line_no, msg))
+            except (ValueError, IndexError, TypeError):
+                print("Invalid message: {0}".format(line))
+        return messages

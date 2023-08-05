165a4b404f1ef74c34005acb7e2b2a1f9bb77a53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:08 +0000

    Add package ranking-sorting/0.0.9/ranking_sorting-0.0.9.tar.gz

---
+++ packages/ranking-sorting/ranking_sorting-0.0.9.tar.gz/ranking_sorting-0.0.9/ranking_sorting/hackernews_hot.py
@@ -0,0 +1,13 @@
+import math
+import datetime
+from datetime import timezone
+
+
+def hackernews_hot(votes, published, gravity=1.8):
+    """ Hacker news' hot sort
+    """
+
+    d = datetime.datetime.now(timezone.utc) - published
+    hour_age = d.total_seconds() // 3600
+    res = votes / math.pow(hour_age + 2, gravity)
+    return res

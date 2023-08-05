b050ca9e645d9b66e4208c66bd35e6af5a0b394f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:29 +0000

    Add package mosyn/1.0.9/mosyn-1.0.9.tar.gz

---
+++ packages/mosyn/mosyn-1.0.9.tar.gz/mosyn-1.0.9/setup.py
@@ -0,0 +1,47 @@
+import setuptools
+from os import path
+from io import open
+from setuptools.command.install import install
+
+
+class PostInstall(install):
+    def run(self):
+        install.run(self)
+        import nltk
+        nltk.download("punkt")
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name = 'mosyn',
+    version = '1.0.9',
+    description = 'A morphological dictionary tool.',
+    long_description=long_description,
+    author = 'uagdataanalysis',
+    author_email = 'uagdataanalysis@gmail.com',
+    packages = setuptools.find_packages(),
+    package_data = {'mosyn': ['dict/*.csv']},
+    cmdclass = {'install': PostInstall},
+    install_requires = [
+        'nltk>=3.0'
+    ],
+    entry_points={
+        'console_scripts': [
+            'mosyn=mosyn:main',
+        ],
+    },
+    keywords = ['dictionary' , 'morphological', 'analysis'],
+    classifiers=(
+        "Programming Language :: Python :: 2",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent"
+    ),
+    url = 'https://github.com/uagdataanalysis/mosynapi',
+    download_url = "https://github.com/uagdataanalysis/mosynapi/tarball/1.0.9",
+    project_urls={
+        'Bug Reports': 'https://github.com/uagdataanalysis/mosynapi/issues',
+        'Source': 'https://github.com/uagdataanalysis/mosynapi/'
+    }
+)

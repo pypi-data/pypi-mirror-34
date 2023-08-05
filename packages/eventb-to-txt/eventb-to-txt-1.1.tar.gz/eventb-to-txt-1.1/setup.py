0823215e086ed8b49d232340509f79b7b09b2e65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:42 +0000

    Add package eventb-to-txt/1.1/eventb-to-txt-1.1.tar.gz

---
+++ packages/eventb-to-txt/eventb-to-txt-1.1.tar.gz/eventb-to-txt-1.1/setup.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2018 Ilya Shchepetkov
+# Use of this source code is governed by the MIT license that can be
+# found in the LICENSE file.
+
+import setuptools
+
+setuptools.setup(
+    name='eventb-to-txt',
+    version='1.1',
+    author='Ilya Shchepetkov',
+    author_email='ilya.shchepetkov@yandex.ru',
+    license='LICENSE.txt',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/17451k/eventb-to-txt",
+    packages=['eventb_to_txt'],
+    entry_points={
+        'console_scripts': [
+            'eventb-to-txt=eventb_to_txt.__main__:main',
+        ],
+    },
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+    )
+)

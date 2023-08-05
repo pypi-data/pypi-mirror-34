2492db615a3507e445ca616f2ba682f42391926c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:01 +0000

    Add package pandas-sql-wrapper/0.1.9/pandas-sql-wrapper-0.1.9.tar.gz

---
+++ packages/pandas-sql-wrapper/pandas-sql-wrapper-0.1.9.tar.gz/pandas-sql-wrapper-0.1.9/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pandas-sql-wrapper',
+    description="Tool to help pandas talk to mysql or postgresql databases." +
+                "Introduces upsert and schema updating capabilities when writing dataframes to sql tables.",
+    url="https://github.com/Ianphorsman/PandasSqlWrapper",
+    author='Ian Horsman',
+    author_email='ianphorsman@gmail.com',
+    license='MIT',
+    version='0.1.9',
+    packages=['pandas_sql_wrapper'],
+    install_requires=['numpy', 'pandas', 'pymysql', 'sqlalchemy', 'pg8000']
+)

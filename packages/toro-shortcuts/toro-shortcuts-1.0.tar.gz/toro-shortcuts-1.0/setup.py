781e58f288986410621b2eadb6899f8fb1c8b0fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:41 +0000

    Add package toro-shortcuts/1.0/toro-shortcuts-1.0.tar.gz

---
+++ packages/toro-shortcuts/toro-shortcuts-1.0.tar.gz/toro-shortcuts-1.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="toro-shortcuts",
+    version="1.0",
+    author="Steven Larry Ball",
+    author_email="stevenlarry.ball@torocloud.com",
+    description="A Python Markdown extension for keyboard shortcuts",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.torocloud.com/",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    py_modules=['toro-shortcuts'],
+)

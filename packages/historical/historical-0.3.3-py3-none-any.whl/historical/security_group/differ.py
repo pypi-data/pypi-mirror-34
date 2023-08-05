4c2e09e61a08c93e08e2d1e97ee6e8ba5f8344fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:32 +0000

    Add package historical/0.3.3/historical-0.3.3-py3-none-any.whl

---
+++ packages/historical/historical-0.3.3-py3-none-any.whl/historical/security_group/differ.py
@@ -0,0 +1,30 @@
+"""
+.. module: historical.security_group.differ
+    :platform: Unix
+    :copyright: (c) 2017 by Netflix Inc., see AUTHORS for more
+    :license: Apache, see LICENSE for more details.
+.. author:: Kevin Glisson <kglisson@netflix.com>
+"""
+import logging
+
+from raven_python_lambda import RavenLambdaWrapper
+
+from historical.common.dynamodb import process_dynamodb_differ_record
+from historical.security_group.models import DurableSecurityGroupModel, CurrentSecurityGroupModel
+from historical.constants import LOGGING_LEVEL
+
+logging.basicConfig()
+log = logging.getLogger('historical')
+log.setLevel(LOGGING_LEVEL)
+
+
+@RavenLambdaWrapper()
+def handler(event, context):
+    """
+    Historical security group event differ.
+
+    Listens to the Historical current table and determines if there are differences that need to be persisted in the
+    historical record.
+    """
+    for record in event['Records']:
+        process_dynamodb_differ_record(record, CurrentSecurityGroupModel, DurableSecurityGroupModel)

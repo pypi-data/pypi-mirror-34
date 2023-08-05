269b6e40bc2c39babbd0c66c995e8143e557d642
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:48 +0000

    Add package referenceqvm/0.3/referenceqvm-0.3.tar.gz

---
+++ packages/referenceqvm/referenceqvm-0.3.tar.gz/referenceqvm-0.3/referenceqvm/api.py
@@ -0,0 +1,41 @@
+"""
+Sets up the appropriate QVM, and interfacing.
+"""
+from referenceqvm.gates import gate_matrix
+from referenceqvm.qvm_wavefunction import QVM_Wavefunction
+from referenceqvm.qvm_unitary import QVM_Unitary
+from referenceqvm.qvm_density import QVM_Density
+
+
+def QVMConnection(type_trans='wavefunction',
+                  gate_set=gate_matrix, noise_model=None):
+        """
+        Initialize a qvm of a particular type. The type corresponds to the
+        type of transition the QVM can perform.
+
+        Currently available transitions:
+            wavefunction
+            unitary
+            (density) support to be implemented
+    
+        'Wavefunction' is set by default. No noise/t1/t2 params are supported
+        in this ref-qvm yet. The QVM uses the gate_set in the
+        gate_matrix dictionary by default
+
+        :param type_trans: Transition type of the qvm. Either wavefunction or unitary
+        :param gate_set: The set of gates that each qubit or pair of qubits can perform
+        """
+
+        if type_trans == 'wavefunction':
+            qvm = QVM_Wavefunction(gate_set=gate_set)
+
+        elif type_trans == 'unitary':
+            qvm = QVM_Unitary(gate_set=gate_set)
+
+        elif type_trans == 'density':
+            qvm = QVM_Density(gate_set=gate_set, noise_model=noise_model)
+
+        else:
+            raise TypeError("{} is not a valid QVM type.".format(type_trans))
+
+        return qvm
---
+++ packages/referenceqvm/referenceqvm-0.3.tar.gz/referenceqvm-0.3/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+from setuptools import setup
+from referenceqvm import __version__
+
+setup(
+    name="referenceqvm",
+    version=__version__,
+    author="Rigetti Computing",
+    author_email="softapps@rigetti.com",
+    url="https://github.com/rigetticomputing/reference-qvm.git",
+    description="a reference qvm for simulation of pyQuil programs",
+    packages=[
+        "referenceqvm"
+    ],
+    install_requires=[
+        "numpy >= 1.11.1",
+        "scipy >= 0.18.1",
+        "pyquil >= 1.4.2",
+        "funcsigs"
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=[
+        'pytest >= 3.0.0',
+        'mock'
+    ],
+    license='LICENSE',
+    test_suite='referenceqvm.tests'
+)

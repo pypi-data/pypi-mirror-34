4b96221a2191a729cd9dd6d74ee65c1059d76d9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:02 +0000

    Add package spdown/0.0.5/spdown-0.0.5-py3-none-any.whl

---
+++ packages/spdown/spdown-0.0.5-py3-none-any.whl/spdown/track.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+"""
+MIT License
+
+Copyright (c) 2018 Berke Emrecan ARSLAN
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
+class Track:
+
+    def __init__(self):
+        self.spotify_id = None
+        self.artist = None
+        self.title = None
+        self.youtube_id = None
+        self.album_name = None
+
+    def to_dict(self) -> dict:
+        pass
+
+    def __str__(self):
+        return '{} - {}'.format(
+            self.artist, self.title
+        )
+
+    def __repr__(self):
+        return self.__str__()

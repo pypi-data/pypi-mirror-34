70f535c93e118f20b661db82f8779bf0f7aa2c3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:31 +0000

    Add package valohai-yaml/0.7/valohai_yaml-0.7-py2.py3-none-any.whl

---
+++ packages/valohai-yaml/valohai_yaml-0.7-py2.py3-none-any.whl/valohai_yaml/objs/input.py
@@ -0,0 +1,14 @@
+from .base import _SimpleObject
+
+
+class Input(_SimpleObject):
+
+    def __init__(
+        self,
+        name,
+        default=None,
+        optional=False,
+    ):
+        self.name = name
+        self.default = default
+        self.optional = optional

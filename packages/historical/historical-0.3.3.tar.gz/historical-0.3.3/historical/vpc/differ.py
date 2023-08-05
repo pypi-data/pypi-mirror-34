00d97f2ef46d9709396424af86f9329b224de843
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:32 +0000

    Add package historical/0.3.3/historical-0.3.3.tar.gz

---
+++ packages/historical/historical-0.3.3.tar.gz/historical-0.3.3/historical/vpc/differ.py
@@ -0,0 +1,30 @@
+"""
+.. module: historical.vpc.differ
+    :platform: Unix
+    :copyright: (c) 2017 by Netflix Inc., see AUTHORS for more
+    :license: Apache, see LICENSE for more details.
+.. author:: Kevin Glisson <kglisson@netflix.com>
+"""
+import logging
+
+from raven_python_lambda import RavenLambdaWrapper
+
+from historical.common.dynamodb import process_dynamodb_differ_record
+from historical.constants import LOGGING_LEVEL
+from historical.vpc.models import DurableVPCModel, CurrentVPCModel
+
+logging.basicConfig()
+log = logging.getLogger('historical')
+log.setLevel(LOGGING_LEVEL)
+
+
+@RavenLambdaWrapper()
+def handler(event, context):
+    """
+    Historical security group event differ.
+
+    Listens to the Historical current table and determines if there are differences that need to be persisted in the
+    historical record.
+    """
+    for record in event['Records']:
+        process_dynamodb_differ_record(record, CurrentVPCModel, DurableVPCModel)

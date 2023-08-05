ee94601cb8c205d6247c6ef717482036a05634fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:46 +0000

    Add package fireant/1.0.0.dev39/fireant-1.0.0.dev39.tar.gz

---
+++ packages/fireant/fireant-1.0.0.dev39.tar.gz/fireant-1.0.0.dev39/fireant/slicer/queries/database.py
@@ -0,0 +1,117 @@
+import time
+from typing import Iterable
+
+import pandas as pd
+
+from fireant.database.base import Database
+from fireant.formats import NULL_VALUE
+from fireant.utils import format_dimension_key
+from .logger import (
+    query_logger,
+    slow_query_logger,
+)
+from ..dimensions import (
+    ContinuousDimension,
+    Dimension,
+)
+
+
+def fetch_data(database: Database, query: str, dimensions: Iterable[Dimension]):
+    """
+    Executes a query to fetch data from database middleware and builds/cleans the data as a data frame. The query
+    execution is logged with its duration.
+
+    :param database:
+        instance of `fireant.Database`, database middleware
+    :param query: Query string
+    :param dimensions: A list of dimensions, used for setting the index on the result data frame.
+
+    :return: `pd.DataFrame` constructed from the result of the query
+    """
+    start_time = time.time()
+    query_logger.debug(query)
+
+    data_frame = database.fetch_data(str(query))
+
+    duration = round(time.time() - start_time, 4)
+    query_log_msg = '[{duration} seconds]: {query}'.format(duration=duration, query=query)
+    query_logger.info(query_log_msg)
+
+    if duration >= database.slow_query_log_min_seconds:
+        slow_query_logger.warning(query_log_msg)
+
+    return clean_and_apply_index(data_frame, dimensions)
+
+
+def clean_and_apply_index(data_frame: pd.DataFrame, dimensions: Iterable[Dimension]):
+    """
+    Sets the index on a data frame. This will also replace any nulls from the database with an empty string for
+    non-continuous dimensions. Totals will be indexed with Nones.
+
+    :param data_frame:
+    :param dimensions:
+    :return:
+    """
+    if not dimensions:
+        return data_frame
+
+    dimension_keys = [format_dimension_key(d.key)
+                      for d in dimensions]
+
+    for i, dimension in enumerate(dimensions):
+        if isinstance(dimension, ContinuousDimension):
+            # Continuous dimensions are can never contain null values since they are selected as windows of values
+            # With that in mind, we leave the NaNs in them to represent Totals.
+            continue
+
+        level = format_dimension_key(dimension.key)
+        data_frame[level] = fill_nans_in_level(data_frame, dimension, dimension_keys[:i]) \
+            .apply(
+              # Handles an annoying case of pandas in which the ENTIRE data frame gets converted from int to float if
+              # the are NaNs, even if there are no NaNs in the column :/
+              lambda x: int(x) if isinstance(x, float) and float.is_integer(x) else x) \
+            .apply(lambda x: str(x) if not pd.isnull(x) else None)
+
+    # Set index on dimension columns
+    return data_frame.set_index(dimension_keys)
+
+
+def fill_nans_in_level(data_frame, dimension, preceding_dimension_keys):
+    """
+    In case there are NaN values representing both totals (from ROLLUP) and database nulls, we need to replace the real
+    nulls with an empty string in order to distinguish between them.  We choose to replace the actual database nulls
+    with an empty string in order to let NaNs represent Totals because mixing string values in the pandas index types
+    used by continuous dimensions does work.
+
+    :param data_frame:
+        The data_frame we are replacing values in.
+    :param dimension:
+        The level of the data frame to replace nulls in. This function should be called once per non-conitnuous
+        dimension, in the order of the dimensions.
+    :param preceding_dimension_keys:
+    :return:
+        The level in the data_frame with the nulls replaced with empty string
+    """
+    level = format_dimension_key(dimension.key)
+
+    if dimension.is_rollup:
+        if preceding_dimension_keys:
+            return (data_frame
+                    .groupby(preceding_dimension_keys)[level]
+                    .apply(_fill_nan_for_nulls))
+
+        return _fill_nan_for_nulls(data_frame[level])
+
+    return data_frame[level].fillna(NULL_VALUE)
+
+
+def _fill_nan_for_nulls(df):
+    """
+    Fills the first NaN with a literal string "null" if there are two NaN values, otherwise nothing is filled.
+
+    :param df:
+    :return:
+    """
+    if 1 < pd.isnull(df).sum():
+        return df.fillna(NULL_VALUE, limit=1)
+    return df
---
+++ packages/fireant/fireant-1.0.0.dev39.tar.gz/fireant-1.0.0.dev39/fireant/utils.py
@@ -0,0 +1,135 @@
+from collections import OrderedDict
+
+
+def wrap_list(value):
+    return value if isinstance(value, (tuple, list)) else [value]
+
+
+def deep_get(d, keys, default=None):
+    d_level = d
+    for key in keys:
+        if key not in d_level:
+            return default
+        d_level = d_level[key]
+    return d_level
+
+def flatten(items):
+    return [item for level in items for item in wrap_list(level)]
+
+
+def slice_first(item):
+    if isinstance(item, (tuple, list)):
+        return item[0]
+    return item
+
+
+def filter_duplicates(iterable):
+    filtered_list, seen = [], set()
+    for item in iterable:
+        key = slice_first(item)
+
+        if key in seen:
+            continue
+
+        seen.add(key)
+        filtered_list.append(item)
+
+    return filtered_list
+
+
+def merge_dicts(*dict_args):
+    """
+    Given any number of dicts, shallow copy and merge into a new dict,
+    precedence goes to key value pairs in latter dicts.
+
+    https://stackoverflow.com/questions/38987/how-to-merge-two-python-dictionaries-in-a-single-expression
+    """
+    result = {}
+    for dictionary in dict_args:
+        result.update(dictionary)
+    return result
+
+
+def immutable(func):
+    """
+    Decorator for wrapper "builder" functions.  These are functions on the Query class or other classes used for
+    building queries which mutate the query and return self.  To make the build functions immutable, this decorator is
+    used which will deepcopy the current instance.  This decorator will return the return value of the inner function
+    or the new copy of the instance.  The inner function does not need to return self.
+    """
+    import copy
+
+    def _copy(self, *args, **kwargs):
+        self_copy = copy.deepcopy(self)
+        result = func(self_copy, *args, **kwargs)
+
+        # Return self if the inner function returns None.  This way the inner function can return something
+        # different (for example when creating joins, a different builder is returned).
+        if result is None:
+            return self_copy
+
+        return result
+
+    return _copy
+
+
+def ordered_distinct_list(l):
+    seen = set()
+    return [x
+            for x in l
+            if not x in seen
+            and not seen.add(x)]
+
+
+def ordered_distinct_list_by_attr(l, attr='key'):
+    seen = set()
+    return [x
+            for x in l
+            if not getattr(x, attr) in seen
+            and not seen.add(getattr(x, attr))]
+
+
+def groupby(items, by):
+    """
+    Group items using a function to derive a key.
+    :param items: The items to group
+    :param by: A lambda function to create a key based on the item
+    :return:
+        an Ordered dict
+    """
+
+    result = OrderedDict()
+    for item in items:
+        key = by(item)
+
+        if key in result:
+            result[key].append(item)
+        else:
+            result[key] = [item]
+
+    return result
+
+
+def groupby_first_level(index):
+    seen = set()
+    return [x[1:]
+            for x in list(index)
+            if x[1:] not in seen and not seen.add(x[1:])]
+
+
+def format_key(key, prefix=None):
+    if key is None:
+        return key
+
+    if prefix is not None:
+        return '${}${}'.format(prefix, key)
+
+    return '${}'.format(key)
+
+
+def format_dimension_key(key):
+    return format_key(key, 'd')
+
+
+def format_metric_key(key):
+    return format_key(key, 'm')

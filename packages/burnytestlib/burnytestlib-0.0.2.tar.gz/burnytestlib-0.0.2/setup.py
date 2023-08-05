629277bb0534c85d28eb13973d36aa7052347e4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:32 +0000

    Add package burnytestlib/0.0.2/burnytestlib-0.0.2.tar.gz

---
+++ packages/burnytestlib/burnytestlib-0.0.2.tar.gz/burnytestlib-0.0.2/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name='burnytestlib',    # This is the name of your PyPI-package.
+    author="Burny",
+    author_email="gamingburny@gmail.com",
+    url="https://github.com/BurnySc2/testing-stuff",
+    version='0.0.2',                          # Update the version number for new releases
+    # scripts=['burnytestlib']                  # The name of your scipt, and also the command you'll be using for calling it
+    description="A small example package",
+    long_description="long_description",
+    long_description_content_type="text/markdown",
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    packages=["burnytestlib"],
+    # scripts=["hello.py"]
+)

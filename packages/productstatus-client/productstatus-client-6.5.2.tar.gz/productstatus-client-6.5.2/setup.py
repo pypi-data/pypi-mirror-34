36dd326218c00a4eda57f1df75a4cfc6c8a0797b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:29 +0000

    Add package productstatus-client/6.5.2/productstatus-client-6.5.2.tar.gz

---
+++ packages/productstatus-client/productstatus-client-6.5.2.tar.gz/productstatus-client-6.5.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+config = {
+    'name': 'productstatus-client',
+    'description': 'Productstatus REST API client',
+    'author': 'MET Norway',
+    'url': 'https://github.com/metno/python-productstatus-client',
+    'download_url': 'https://github.com/metno/python-productstatus-client',
+    'version': '6.5.2',
+    'install_requires': [
+        'nose==1.3.7',
+        'requests==2.9.1',
+        'python-dateutil==2.5.0',
+        'httmock==1.2.4',
+        'kafka-python==1.4.2',
+        'mock==2.0.0',
+    ],
+    'packages': find_packages(),
+    'scripts': [],
+}
+
+setup(**config)

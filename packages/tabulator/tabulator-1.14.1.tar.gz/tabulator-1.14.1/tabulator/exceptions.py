3d8fe258bfbc0ca9cfb271976bb222b69ee848af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:50 +0000

    Add package tabulator/1.14.1/tabulator-1.14.1.tar.gz

---
+++ packages/tabulator/tabulator-1.14.1.tar.gz/tabulator-1.14.1/tabulator/exceptions.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+
+# Module API
+
+class TabulatorException(Exception):
+    """https://github.com/frictionlessdata/tabulator-py#exceptions
+    """
+    pass
+
+
+class IOError(TabulatorException):
+    """https://github.com/frictionlessdata/tabulator-py#exceptions
+    """
+    pass
+
+
+class HTTPError(TabulatorException):
+    """https://github.com/frictionlessdata/tabulator-py#exceptions
+    """
+    pass
+
+
+class SourceError(TabulatorException):
+    """https://github.com/frictionlessdata/tabulator-py#exceptions
+    """
+    pass
+
+
+class SchemeError(TabulatorException):
+    """https://github.com/frictionlessdata/tabulator-py#exceptions
+    """
+    pass
+
+
+class FormatError(TabulatorException):
+    """https://github.com/frictionlessdata/tabulator-py#exceptions
+    """
+    pass
+
+
+class EncodingError(TabulatorException):
+    """https://github.com/frictionlessdata/tabulator-py#exceptions
+    """
+    pass
+
+
+# Deprecated
+
+OptionsError = TabulatorException
+ResetError = TabulatorException

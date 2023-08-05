9d27f38eba6c0bbc0f138afda7c95c0072fa4430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:52 +0000

    Add package nadtcp/0.1.1/nadtcp-0.1.1.tar.gz

---
+++ packages/nadtcp/nadtcp-0.1.1.tar.gz/nadtcp-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='nadtcp',
+      version='0.1.1',
+      description='Library for TCP/IP enabled NAD amplifiers. Currently only the C338 is supported.',
+      url='https://gitlab.com/mindig.marton/nadtcp',
+      download_url='https://gitlab.com/mindig.marton/nadtcp/repository/archive.zip?ref=0.1.1',
+      author='mindigmarton',
+      license='MIT',
+      packages=['nadtcp'],
+      install_requires=[],
+      zip_safe=True) 

e4ca3325722ad5cbc7cfcd5cbcecbb44ea110a33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:40:19 +0000

    Add package vcver/0.1.0/vcver-0.1.0-py2.py3-none-any.whl

---
+++ packages/vcver/vcver-0.1.0-py2.py3-none-any.whl/vcver/tests/test_full.py
@@ -0,0 +1,33 @@
+import vcver
+import os
+from vcver.version import _get_scm
+
+
+def test_happy_case():
+    """
+    instead of setting up a git repo,
+    just testing that vcver's own
+    version string matches the
+    expected format.
+    """
+    v = vcver.get_version()
+    version, scm = v.split("+")
+    major, minor, patch, dev = version.split(".")
+    branch, change_id = scm.split(".")
+    assert change_id.startswith("x")
+    change_id = change_id[1:]
+    # scm_change_id should be a git hash
+    assert set(change_id) & set("abcdef1234567890") == set(change_id)
+    # major / minor should be an int
+    int(major)
+    int(minor)
+    int(patch)
+    assert dev.startswith("dev")
+    # commitcount should be an int
+    int(dev[3:])
+
+
+def test_child_directory_detected_as_git_repo():
+    """
+    """
+    assert _get_scm(None, os.path.join(os.curdir, "vcver")) is not None

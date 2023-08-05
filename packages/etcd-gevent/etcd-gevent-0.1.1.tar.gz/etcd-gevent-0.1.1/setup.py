616c06f4779d97da0bc9785f6ded7fe52d60b4fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:13 +0000

    Add package etcd-gevent/0.1.1/etcd-gevent-0.1.1.tar.gz

---
+++ packages/etcd-gevent/etcd-gevent-0.1.1.tar.gz/etcd-gevent-0.1.1/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+import sys, os
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+NEWS = open(os.path.join(here, 'NEWS.txt')).read()
+
+
+version = '0.1.1'
+
+install_requires = [
+    'dnspython>=1.13.0',
+    'geventhttpclient>=1.3.1',
+]
+
+test_requires = [
+    'mock',
+    'nose',
+    'gipc',
+    'pyOpenSSL>=0.14'
+]
+
+setup(
+    name='etcd-gevent',
+    version=version,
+    description="A python client for etcd",
+    long_description=README + '\n\n' + NEWS,
+    classifiers=[
+        "Topic :: System :: Distributed Computing",
+        "Topic :: Software Development :: Libraries",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Database :: Front-Ends",
+    ],
+    keywords='async etcd raft distributed log api client',
+    author='Wenjun Si',
+    author_email='swj0066@gmail.com',
+    url='http://github.com/wjsi/etcd-gevent',
+    license='MIT',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    tests_require=test_requires,
+    test_suite='nose.collector',
+)
---
+++ packages/etcd-gevent/etcd-gevent-0.1.1.tar.gz/etcd-gevent-0.1.1/src/etcd_gevent/tests/unit/__init__.py
@@ -0,0 +1,33 @@
+import etcd_gevent
+import unittest
+import geventhttpclient.response
+import json
+try:
+    import mock
+except ImportError:
+    from unittest import mock
+
+
+class TestClientApiBase(unittest.TestCase):
+
+    def setUp(self):
+        self.client = etcd_gevent.Client()
+
+    def _prepare_response(self, s, d, cluster_id=None):
+        if isinstance(d, dict):
+            data = json.dumps(d).encode('utf-8')
+        else:
+            data = d.encode('utf-8')
+
+        r = mock.create_autospec(geventhttpclient.response.HTTPSocketPoolResponse)(None, None)
+        r.status_code = s
+        r.read.return_value = data
+        r.get.side_effect = lambda x, default=None: "abcd1234" if x == "x-etcd-cluster-id" else default
+        return r
+
+    def _mock_api(self, status, d, cluster_id=None):
+        resp = self._prepare_response(status, d, cluster_id=cluster_id)
+        self.client.api_execute = mock.MagicMock(return_value=resp)
+
+    def _mock_exception(self, exc, msg):
+        self.client.api_execute = mock.Mock(side_effect=exc(msg))

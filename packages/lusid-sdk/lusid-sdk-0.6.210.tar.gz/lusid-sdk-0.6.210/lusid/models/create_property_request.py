8f0907e7ef70a6768d2dcfe8652c8c6d1ef57dc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:28 +0000

    Add package lusid-sdk/0.6.210/lusid-sdk-0.6.210.tar.gz

---
+++ packages/lusid-sdk/lusid-sdk-0.6.210.tar.gz/lusid-sdk-0.6.210/lusid/models/create_property_request.py
@@ -0,0 +1,56 @@
+# coding=utf-8
+# --------------------------------------------------------------------------
+# Copyright © 2018 FINBOURNE TECHNOLOGY LTD
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# --------------------------------------------------------------------------
+
+from msrest.serialization import Model
+
+
+class CreatePropertyRequest(Model):
+    """CreatePropertyRequest.
+
+    :param scope:
+    :type scope: str
+    :param name:
+    :type name: str
+    :param value:
+    :type value: object
+    :param effective_from: Date for which the property is effective from
+    :type effective_from: datetime
+    """
+
+    _validation = {
+        'value': {'required': True},
+    }
+
+    _attribute_map = {
+        'scope': {'key': 'scope', 'type': 'str'},
+        'name': {'key': 'name', 'type': 'str'},
+        'value': {'key': 'value', 'type': 'object'},
+        'effective_from': {'key': 'effectiveFrom', 'type': 'iso-8601'},
+    }
+
+    def __init__(self, value, scope=None, name=None, effective_from=None):
+        super(CreatePropertyRequest, self).__init__()
+        self.scope = scope
+        self.name = name
+        self.value = value
+        self.effective_from = effective_from

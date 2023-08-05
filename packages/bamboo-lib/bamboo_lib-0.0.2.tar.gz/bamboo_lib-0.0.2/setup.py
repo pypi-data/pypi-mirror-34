7e7aab10ef12b8a95dd0bd697d9474a603234f1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:39 +0000

    Add package bamboo-lib/0.0.2/bamboo_lib-0.0.2.tar.gz

---
+++ packages/bamboo-lib/bamboo_lib-0.0.2.tar.gz/bamboo_lib-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bamboo_lib",
+    version="0.0.2",
+    author="Jonathan Speiser",
+    license='All Rights Reserved',
+    author_email="jonathan@datawheel.us",
+    description="Python ETL library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Datawheel/bamboo-lib",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ),
+)

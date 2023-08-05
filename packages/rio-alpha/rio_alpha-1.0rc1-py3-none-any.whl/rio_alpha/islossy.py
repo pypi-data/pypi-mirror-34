47815782d35e3e9fde06ecfc33f863479f08b300
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:43 +0000

    Add package rio-alpha/1.0rc1/rio_alpha-1.0rc1-py3-none-any.whl

---
+++ packages/rio-alpha/rio_alpha-1.0rc1-py3-none-any.whl/rio_alpha/islossy.py
@@ -0,0 +1,30 @@
+"""Evaluate datasets for lossy compression affected nodata masks"""
+
+import numpy as np
+from skimage import measure
+
+from rio_alpha.alpha_mask import mask_exact
+
+
+def count_ndv_regions(img, ndv):
+    """Discover unique labels to count ndv regions.
+
+    Parameters
+    ----------
+    ndv: list
+        a list of floats whose length = band count
+    img: ndarray
+        (depth x rows x cols) array
+
+    Returns
+    -------
+    n_labels: int
+        an integer equal to the number of connected regions
+    """
+    np.set_printoptions(threshold=np.nan)
+
+    img = mask_exact(img, ndv)
+
+    _, n_labels = measure.label(img, background=255, connectivity=1, return_num=True)
+
+    return n_labels

4427ae9278f582fbb4cf7d3fa73822504efaa476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:57 +0000

    Add package ch_frb_wview/0.5.2/ch_frb_wview-0.5.2.tar.gz

---
+++ packages/ch_frb_wview/ch_frb_wview-0.5.2.tar.gz/ch_frb_wview-0.5.2/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from setuptools import setup
+from codecs import open
+
+setup(
+    name='ch_frb_wview',
+    version='0.5.2',
+    description='Web Application to operate the FRB L1 system.'
+,
+
+    author='Dustin Lang, Chitrang Patel',
+    author_email='chitrang.patel@mail.mcgill.ca',
+    url="https://github.com/CHIMEFRB/frb_monitoring.git",
+    packages= ['ch_frb_wview'],
+    package_data={'': ['ch_frb_wview.service',
+                       'Pipfile',
+                       'README.md',
+                       'ch_frb_wview/bin/run-webapp.sh',
+                       'ch_frb_wview/bin/run-uwsgi.sh',
+                       'ch_frb_wview/config/l1_production_8beam_webapp.yaml',
+                       'ch_frb_wview/static/d3.min.js',
+                       'ch_frb_wview/static/fullcalendar.css',
+                       'ch_frb_wview/static/fullcalendar.min.js',
+                       'ch_frb_wview/static/fullcalendar.print.css',
+                       'ch_frb_wview/static/jquery.js',
+                       'ch_frb_wview/static/moment.min.js', 
+                       'ch_frb_wview/static/bootstrap-4/css/*.css', 
+                       'ch_frb_wview/static/bootstrap-4/css/*.map', 
+                       'ch_frb_wview/static/bootstrap-4/js/*.map', 
+                       'ch_frb_wview/static/bootstrap-4/js/*.js', 
+                       'ch_frb_wview/templates/*.html',
+                       'ch_frb_wview/templates/*.js',
+                       'ch_frb_wview/templates/includes/*.html',
+                 ]},
+    scripts=['bin/run-uwsgi.sh', 'bin/run-webapp.sh'],
+    include_package_data=True,
+    install_requires=['flask',
+                      'pyyaml',
+                      'msgpack',
+                      'numpy',
+                      'sqlalchemy',
+                      'passlib',
+                      'zmq',
+                      'scipy',
+                      'requests',
+                      'matplotlib']
+)

6c6e4ec3c45b0eac88a094f157c94f1e5023e335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:24 +0000

    Add package inboxen/0.0.1/inboxen-0.0.1.tar.gz

---
+++ packages/inboxen/inboxen-0.0.1.tar.gz/inboxen-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="inboxen",
+    version="0.0.1",
+    author="Matt Molyneaux",
+    author_email="moggers87+git@moggers87.co.uk",
+    packages=["inboxen"],
+)

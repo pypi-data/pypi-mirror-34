b73785f17199f48060fb8139bd90221a9fb9c3af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:39 +0000

    Add package meesee/0.9/meesee-0.9.tar.gz

---
+++ packages/meesee/meesee-0.9.tar.gz/meesee-0.9/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='meesee',
+    author='Melvin Bijman',
+    author_email='bijman.m.m@gmail.com',
+    version='0.9',
+    py_modules=['meesee'],
+    install_requires=['redis==2.10.6'],
+    license='MIT',
+
+    url='https://github.com/Attumm/meesee',
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        'Intended Audience :: Developers',
+        'Topic :: Database :: Front-Ends',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)

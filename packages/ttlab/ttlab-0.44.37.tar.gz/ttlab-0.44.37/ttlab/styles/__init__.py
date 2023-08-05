85583f9cc98ff197f4b7402a920ae2329f37fd99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:06 +0000

    Add package ttlab/0.44.37/ttlab-0.44.37.tar.gz

---
+++ packages/ttlab/ttlab-0.44.37.tar.gz/ttlab-0.44.37/ttlab/styles/__init__.py
@@ -0,0 +1,13 @@
+ttlab_awesome_style = {
+    'axes.titlesize': 15,
+    'axes.labelsize': 12,
+    'axes.grid': True,
+    'legend.fontsize': 12,
+    'legend.framealpha': 1,
+    'lines.linewidth': 1,
+    'lines.markersize': 10,
+    'xtick.labelsize': 12,
+    'ytick.labelsize': 12,
+    'figure.dpi': 300,
+    'axes.linewidth': 0.5
+}

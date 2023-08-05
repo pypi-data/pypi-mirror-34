9b2a9e2e500b8f21ece7051e69198ed04af57325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:44 +0000

    Add package SteemAX/0.5/SteemAX-0.5-3.0-none-any.whl

---
+++ packages/SteemAX/SteemAX-0.5-3.0-none-any.whl/steemax/default.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+dbuser = "steemax"
+dbpass = "SteemAX_pass23"
+dbname = "steemax"
+msgmode = "quiet"
+logpath = ""
+logfilename = "steemax.log"
+webpath = ""
+recaptcha_url = "https://www.google.com/recaptcha/api/siteverify"
+recaptcha_secret = ""

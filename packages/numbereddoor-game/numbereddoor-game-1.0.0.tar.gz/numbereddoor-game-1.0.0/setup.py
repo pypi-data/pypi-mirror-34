b812d6caeee7ceefe7537fec63b6b0bf46cfad6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:41 +0000

    Add package numbereddoor-game/1.0.0/numbereddoor-game-1.0.0.tar.gz

---
+++ packages/numbereddoor-game/numbereddoor-game-1.0.0.tar.gz/numbereddoor-game-1.0.0/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+import cx_Freeze
+import sys
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+version = '1.0.0'
+short_description = 'A recreation of the door puzzles from the Nonary game, 999.'
+
+setuptools.setup(
+    name='numbereddoor-game',
+    version=version,
+    author='Coul Greer',
+    author_email='coulgreer1@hotmail.com',
+    description=short_description,
+    long_description=long_description,
+    url='https://github.com/cagreer18/NumberedDoorGame',
+    packages=setuptools.find_packages(),
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: OS Independent'
+    ]
+)
+
+base = None
+
+if sys.platform == 'win32':
+    base = 'Win32GUI'
+
+cx_Freeze.setup(
+    name='numbereddoor-game',
+    options={"build_exe": {
+        'include_files': ['images']
+    }},
+    version=version,
+    description=short_description,
+    executables=[cx_Freeze.Executable('main_game.py', base=base)]
+)

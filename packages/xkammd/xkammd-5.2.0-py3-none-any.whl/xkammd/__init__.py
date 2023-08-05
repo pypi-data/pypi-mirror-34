a215dc093e02e53d33d6383f710f3d08c30c25d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:49 +0000

    Add package xkammd/5.2.0/xkammd-5.2.0-py3-none-any.whl

---
+++ packages/xkammd/xkammd-5.2.0-py3-none-any.whl/xkammd/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/7/20 11:36
+# @Author  : LI Jiawei
+# @Email   : jliea@connect.ust.hk
+# @File    : __init__.py.py
+# @Software: PyCharm
+
+name = "xkammd"

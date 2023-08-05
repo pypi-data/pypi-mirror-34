6d05041f40fa3e1cabb17567b030f85943312728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:26 +0000

    Add package ie_mbdbl2018_tsaliba/0.1/ie_mbdbl2018_tsaliba-0.1.tar.gz

---
+++ packages/ie_mbdbl2018_tsaliba/ie_mbdbl2018_tsaliba-0.1.tar.gz/ie_mbdbl2018_tsaliba-0.1/ie_mbdbl2018_tsaliba/myfirstfunc.py
@@ -0,0 +1,3 @@
+def  sayhi():
+    """ function to return the string 'my first package' """
+    return("This is Tarek Saliba's first package that anyone can install it! How cool!!!")
---
+++ packages/ie_mbdbl2018_tsaliba/ie_mbdbl2018_tsaliba-0.1.tar.gz/ie_mbdbl2018_tsaliba-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ie_mbdbl2018_tsaliba',
+      version='0.1',  # Development release
+      description='Python Packaging Starting Kit - Simple Structure to creater your own package - for training purposes!',
+      url='https://github.com/Salibt/ie_mbdbl2018_tsaliba',
+      author='Tarek Saliba',
+      author_email='tarek.saliba@student.ie.edu',
+      license='MIT',
+          packages=['ie_mbdbl2018_tsaliba'],
+      zip_safe=False)

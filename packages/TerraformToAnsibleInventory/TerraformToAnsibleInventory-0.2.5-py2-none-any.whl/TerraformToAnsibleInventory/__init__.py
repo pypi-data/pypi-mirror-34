6ffd5f8ed2719b9ae6dd4d8496a51e46dcc76975
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:08 +0000

    Add package TerraformToAnsibleInventory/0.2.5/TerraformToAnsibleInventory-0.2.5-py2-none-any.whl

---
+++ packages/TerraformToAnsibleInventory/TerraformToAnsibleInventory-0.2.5-py2-none-any.whl/TerraformToAnsibleInventory/__init__.py
@@ -0,0 +1,34 @@
+import os
+from . args import parse as ArgsParse
+from . parse import terraform_tfstate as ParseTerraformTfstate
+from . build_inventory import terraform as BuildTerraformInventory
+from . generate_inventory import ansible as GenerateAnsibleInventory
+
+ARGS = ArgsParse()
+
+CWD = os.getcwd()
+SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
+TERRAFORM_ANSIBLE_GROUPS = []
+TERRAFORM_ANSIBLE_INVENTORY = ('%s/' + ARGS.inventory) % CWD
+TERRAFORM_DATA_TYPES = []
+TERRAFORM_INVENTORY = []
+TERRAFORM_LOAD_BALANCERS = []
+TERRAFORM_NETWORK_INTERFACES = []
+TERRAFORM_PUBLIC_IPS = []
+TERRAFORM_TFSTATE = ('%s/' + ARGS.tfstate) % CWD
+TERRAFORM_VMS = []
+
+ParseTerraformTfstate(ARGS, TERRAFORM_ANSIBLE_GROUPS,
+                      TERRAFORM_NETWORK_INTERFACES,
+                      TERRAFORM_LOAD_BALANCERS,
+                      TERRAFORM_PUBLIC_IPS,
+                      TERRAFORM_TFSTATE,
+                      TERRAFORM_VMS)
+
+BuildTerraformInventory(TERRAFORM_DATA_TYPES, TERRAFORM_INVENTORY,
+                        TERRAFORM_NETWORK_INTERFACES,
+                        TERRAFORM_PUBLIC_IPS, TERRAFORM_VMS)
+
+GenerateAnsibleInventory(
+    TERRAFORM_ANSIBLE_GROUPS, TERRAFORM_ANSIBLE_INVENTORY,
+    TERRAFORM_DATA_TYPES, TERRAFORM_INVENTORY, TERRAFORM_LOAD_BALANCERS)

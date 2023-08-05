4d7fba3cd083b4d95b8dd55792f741f259f851cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:24 +0000

    Add package conan_sword_and_sorcery/0.0.dev126/conan_sword_and_sorcery-0.0.dev126.tar.gz

---
+++ packages/conan_sword_and_sorcery/conan_sword_and_sorcery-0.0.dev126.tar.gz/conan_sword_and_sorcery-0.0.dev126/conan_sword_and_sorcery/ci/compilers/base_compiler.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from conan_sword_and_sorcery.utils import isstr
+
+log = logging.getLogger(__name__)
+
+
+class BaseCompiler(object):
+    id = None
+    osys = None
+
+    def __init__(self, **kwargs):
+        self._data = kwargs
+        self.cmd = None  # Will be set by the runner
+
+        # Validate input arguments
+        for key, val in kwargs.items():
+            if not val or not isstr(val):
+                raise ValueError("Invalid configuration argument for compiler '{}': argument '{}' must be a non empty string.".format(self.id, key))
+
+    def __getattr__(self, item):
+        if item == 'os':
+            return self.osys
+        return self._data.get(item)
+
+    def update_settings(self, settings):
+        settings.build_type = self.build_type
+        settings.arch = self.arch
+        settings.compiler = self.id
+        settings.compiler.version = self.version
+
+    def populate_profile_settings(self, f):
+        f.write("os={}\n".format(self.osys))
+        f.write("arch={}\n".format(self.arch))
+        f.write("build_type={}\n".format(self.build_type))
+        f.write("compiler={}\n".format(self.id))
+        f.write("compiler.version={}\n".format(self.version))
+
+    def populate_profile_env(self, f):
+        pass
+
+    @classmethod
+    def validate(cls, **kwargs):
+        # Raise error if given configuration is not supported
+        return True
+
+    @classmethod
+    def environment_filters(cls):
+        raise NotImplementedError("Compiler must explicitly declare its environment filters.")
+
+    def run(self, command_plain):
+        log.debug("BaseCompiler::run")
+        return self.cmd(command_plain)
+

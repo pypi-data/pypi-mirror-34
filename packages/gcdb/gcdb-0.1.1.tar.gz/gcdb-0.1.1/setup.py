159f6191985837e4b38b76b7f42864ace3beaebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:31 +0000

    Add package gcdb/0.1.1/gcdb-0.1.1.tar.gz

---
+++ packages/gcdb/gcdb-0.1.1.tar.gz/gcdb-0.1.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+description = None
+
+if os.path.exists('README.md'):
+    with open('README.md') as fp:
+        description = fp.read()
+
+setup(
+    name='gcdb',
+    version='0.1.1',
+    description=description,
+    url='https://github.com/kylef/gcdb',
+    author='Kyle Fuller',
+    author_email='kyle@fuller.li',
+    packages=('gcdb',),
+    install_requires=('Click'),
+    entry_points={
+        'console_scripts': (
+            'gcdb = gcdb:cli',
+        )
+    },
+)
+

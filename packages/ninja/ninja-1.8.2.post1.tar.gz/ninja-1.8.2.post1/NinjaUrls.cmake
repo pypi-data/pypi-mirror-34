7a5221f44e9ed271d555072b61a407e334e6be05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:45 +0000

    Add package ninja/1.8.2.post1/ninja-1.8.2.post1.tar.gz

---
+++ packages/ninja/ninja-1.8.2.post1.tar.gz/ninja-1.8.2.post1/NinjaUrls.cmake
@@ -0,0 +1,25 @@
+
+#-----------------------------------------------------------------------------
+# Ninja sources
+set(unix_source_url       "https://github.com/ninja-build/ninja/archive/v1.8.2.tar.gz")
+set(unix_source_sha256    "86b8700c3d0880c2b44c2ff67ce42774aaf8c28cbf57725cb881569288c1c6f4")
+
+set(windows_source_url    "https://github.com/ninja-build/ninja/archive/v1.8.2.zip")
+set(windows_source_sha256 "b4301eab214959a13865047e0741cbbf5f98ee94fc84b3aeb3066a27a8ce18a2")
+
+#-----------------------------------------------------------------------------
+# Ninja binaries
+set(linux32_binary_url    "NA")  # Linux 32-bit binaries not available
+set(linux32_binary_sha256 "NA")
+
+set(linux64_binary_url    "https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-linux.zip")
+set(linux64_binary_sha256 "d2fea9ff33b3ef353161ed906f260d565ca55b8ca0568fa07b1d2cab90a84a07")
+
+set(macosx_binary_url    "https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-mac.zip")
+set(macosx_binary_sha256 "0347d55c66061652b26f48769d566761630ffde3143793b29064a57f356542cc")
+
+set(win32_binary_url    "NA")  # Windows 32-bit binaries not available
+set(win32_binary_sha256 "NA")
+
+set(win64_binary_url    "https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-win.zip")
+set(win64_binary_sha256 "c80313e6c26c0b9e0c241504718e2d8bbc2798b73429933adf03fdc6d84f0e70")

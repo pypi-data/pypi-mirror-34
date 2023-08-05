cbaf718cfa08b4817e2d3ac77a3ac42e3c31c17b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:08 +0000

    Add package adb-enhanced/1.7.4/adb-enhanced-1.7.4.tar.gz

---
+++ packages/adb-enhanced/adb-enhanced-1.7.4.tar.gz/adb-enhanced-1.7.4/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '1.7.4'
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='adb-enhanced',
+        version=version,
+        description="An ADB wrapper for Android developers",
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        classifiers=["Intended Audience :: Developers"], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        keywords='Android ADB developer',
+        author='Ashish Bhatia',
+        author_email='ashishb@ashishb.net',
+        url='https://github.com/ashishb/adb-enhanced',
+        license='Apache',
+        packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+        include_package_data=True,
+        zip_safe=True,
+        install_requires=[
+                # -*- Extra requirements: -*-
+                'docopt',
+                ],
+        entry_points={
+                # -*- Entry points: -*-
+                'console_scripts': [
+                        'adbe=adbe:main',
+                        ],
+                }
+        )

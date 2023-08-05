6debb0867228f9517afacbf4c7ca8b19b611cb9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:44 +0000

    Add package null-object/0.1.0/null-object-0.1.0.tar.gz

---
+++ packages/null-object/null-object-0.1.0.tar.gz/null-object-0.1.0/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(name='null-object',
+      version='0.1.0',
+      description='Null object',
+      author='Sergei Pikhovkin',
+      author_email='s@pikhovkin.ru',
+      url='https://github.com/pikhovkin/null_object',
+      packages=['null_object'],
+      include_package_data=False,
+      install_requires=[],
+      dependency_links=[],
+      classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Environment :: Web Environment',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Topic :: Software Development :: Libraries'
+      ])

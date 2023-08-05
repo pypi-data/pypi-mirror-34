7551407d578de2d5921234021df0273013d7d9e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:00 +0000

    Add package sqlservice/0.22.1/sqlservice-0.22.1-py2.py3-none-any.whl

---
+++ packages/sqlservice/sqlservice-0.22.1-py2.py3-none-any.whl/sqlservice/utils.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Utilities
+---------
+
+The utilities module.
+"""
+
+from functools import wraps
+
+from ._compat import Iterable, string_types
+
+
+def classonce(meth):
+    """Decorator that executes a class method once, stores the results at the
+    class level, and subsequently returns those results for every future method
+    call.
+    """
+    @wraps(meth)
+    def decorated(cls, *args, **kargs):
+        cached_attr = '__{0}'.format(meth.__name__)
+        if not hasattr(cls, cached_attr):
+            result = meth(cls, *args, **kargs)
+            setattr(cls, cached_attr, result)
+        return getattr(cls, cached_attr)
+    return decorated
+
+
+def is_sequence(obj):
+    """Test if `obj` is an iterable but not ``dict`` or ``str``. This function
+    is mainly used to determine if `obj` can be treated like a ``list`` for
+    iteration purposes.
+    """
+    return (isinstance(obj, Iterable) and
+            not isinstance(obj, string_types) and
+            not isinstance(obj, dict))

cf86a91b9db00aa87496a05d30beaddc6c682068
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:22 +0000

    Add package amdataset/0.2/amdataset-0.2.tar.gz

---
+++ packages/amdataset/amdataset-0.2.tar.gz/amdataset-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+name = 'amdataset',
+version = '0.2',
+packages = find_packages(),
+author = 'Ammad Khalid',
+install_requires = ['dataset'],
+author_email = 'Ammadkhalid12@gmail.com'
+)

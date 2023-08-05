cb2fda230096f8e5b36503a9aa009269c4c59b9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:44 +0000

    Add package chaostoolkit-reporting/0.9.0/chaostoolkit-reporting-0.9.0.tar.gz

---
+++ packages/chaostoolkit-reporting/chaostoolkit-reporting-0.9.0.tar.gz/chaostoolkit-reporting-0.9.0/chaosreport/cli.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import click
+from logzero import logger
+
+from chaosreport import generate_report
+
+__all__ = ["cli"]
+
+
+@click.command()
+@click.option('--export-format', default="markdown",
+              help='Format to export the report to: html, markdown, pdf.')
+@click.argument('journal', type=click.Path(exists=True))
+@click.argument('report', type=click.Path(exists=False))
+def report(export_format: str = "markdown", journal: str = "journal.json",
+           report: str = "report.md"):
+    """
+    Generate a report from the run journal.
+    """
+    generate_report(journal, report, export_format)
+    click.echo("Report generated as '{f}'".format(f=report))

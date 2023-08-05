c35eaac47a6ef5123427bb0dd84e267b3412b86e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:57 +0000

    Add package anaconda/0.0.1/anaconda-0.0.1.tar.gz

---
+++ packages/anaconda/anaconda-0.0.1.tar.gz/anaconda-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = "anaconda",
+    version = "0.0.1",
+    author = "Kale Franz",
+    author_email = "kfranz@anaconda.com",
+    description = ("You cannot 'pip install anaconda'. Please use the Anaconda installer."),
+)

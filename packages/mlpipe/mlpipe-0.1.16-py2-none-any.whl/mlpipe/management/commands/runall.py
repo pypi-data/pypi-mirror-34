85d60b4ed7fe0d069090f6f1a83d06511f819acd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:45 +0000

    Add package mlpipe/0.1.16/mlpipe-0.1.16-py2-none-any.whl

---
+++ packages/mlpipe/mlpipe-0.1.16-py2-none-any.whl/mlpipe/management/commands/runall.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand, CommandError
+from mlpipe.models import Pipe
+from mlpipe.mlpipe_utils import mlpipeutils
+from mlpipe.job_utils import JobRunner
+import os
+
+class Command(BaseCommand):
+    help = 'run all jobs'
+
+    def handle(self, *args, **options):
+        next_job = get_next_excutable_job()
+        while (next_job):
+          jr = JobRunner(next_job)
+          jr.run()
+          next_job = get_next_excutable_job()
+    
+        print("no jobs can be run at this point of time")

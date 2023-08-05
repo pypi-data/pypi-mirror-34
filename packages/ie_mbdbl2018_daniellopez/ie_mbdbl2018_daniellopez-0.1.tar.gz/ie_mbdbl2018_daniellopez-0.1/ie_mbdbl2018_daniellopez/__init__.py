ab69fc3d8cbe64dfd37614995611a092caf6ff0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:28 +0000

    Add package ie_mbdbl2018_daniellopez/0.1/ie_mbdbl2018_daniellopez-0.1.tar.gz

---
+++ packages/ie_mbdbl2018_daniellopez/ie_mbdbl2018_daniellopez-0.1.tar.gz/ie_mbdbl2018_daniellopez-0.1/ie_mbdbl2018_daniellopez/__init__.py
@@ -0,0 +1,3 @@
+def  datareader_doc():
+    """ function to return the string 'My first Python package that anyone call install it! How cool!!!' """
+    return("This is Daniel Lopez first package that anyone can install it! ")
---
+++ packages/ie_mbdbl2018_daniellopez/ie_mbdbl2018_daniellopez-0.1.tar.gz/ie_mbdbl2018_daniellopez-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ie_mbdbl2018_daniellopez',
+      version='0.1',  # Development release
+      description='Python Packaging Starting Kit - Simple Structure to creater your own package - for training purposes!',
+      url='https://github.com/dlopezmaci001/ie_mbdbl2018_daniellopez.git',
+      author='Daniel Lopez',
+      author_email='daniel.lopez@student.ie.edu',
+      license='MIT',
+          packages=['ie_mbdbl2018_daniellopez'],
+      zip_safe=False)

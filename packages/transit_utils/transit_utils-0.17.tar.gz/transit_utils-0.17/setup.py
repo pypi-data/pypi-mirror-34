932f466e4d1bf5a849a9971c604cfcd2fcb9f1dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:27 +0000

    Add package transit_utils/0.17/transit_utils-0.17.tar.gz

---
+++ packages/transit_utils/transit_utils-0.17.tar.gz/transit_utils-0.17/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+version = '0.17'
+
+setup(
+    name='transit_utils',
+    version=version,
+    description='A small collection of methods for analyzing transit data',
+    url='https://github.com/BoiseStatePlanetary/transit_utils',
+    download_url='https://github.com/BoiseStatePlanetary/transit_utils/archive/'+version+'.tar.gz',
+    license='BSD',
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Developers',
+      'Programming Language :: Python :: 3',
+    ],
+    packages=['transit_utils'],
+    keywords='',
+    author='Brian Jackson',
+    install_requires=['statsmodels', 'scipy', 'numpy', 'PyAstronomy'],
+    author_email='bjackson@boisestate.edu'
+)

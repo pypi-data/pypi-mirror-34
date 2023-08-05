8bba121843765f957eaf4f8e930e11fe82a8984d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:05 +0000

    Add package indy-plenum-dev/1.5.46/indy-plenum-dev-1.5.46.tar.gz

---
+++ packages/indy-plenum-dev/indy-plenum-dev-1.5.46.tar.gz/indy-plenum-dev-1.5.46/plenum/test/monitoring/test_monitoring_params_with_zfn.py
@@ -0,0 +1,45 @@
+import pytest
+
+from plenum.test.helper import get_key_from_req
+
+nodeCount = 7
+
+@pytest.fixture(scope="module")
+def tconf(tconf):
+    old_thr_window_size = tconf.ThroughputInnerWindowSize
+    old_thr_window_count = tconf.ThroughputMinActivityThreshold
+    tconf.ThroughputInnerWindowSize = 5
+    tconf.ThroughputMinActivityThreshold = 2
+
+    yield tconf
+    tconf.ThroughputInnerWindowSize = old_thr_window_size
+    tconf.ThroughputMinActivityThreshold = old_thr_window_count
+
+
+def testThroughputThreshold(looper, txnPoolNodeSet, tconf, requests):
+    looper.runFor(tconf.ThroughputInnerWindowSize *
+                  tconf.ThroughputMinActivityThreshold)
+    for node in txnPoolNodeSet:
+        masterThroughput, avgBackupThroughput = node.monitor.getThroughputs(
+            node.instances.masterId)
+        for r in node.replicas:
+            print("{} stats: {}".format(r, repr(r.stats)))
+        assert masterThroughput / avgBackupThroughput >= node.monitor.Delta
+
+
+def testReqLatencyThreshold(looper, txnPoolNodeSet, requests):
+    for node in txnPoolNodeSet:
+        for rq in requests:
+            key = get_key_from_req(rq)
+            assert key in node.monitor.masterReqLatenciesTest
+            assert node.monitor.masterReqLatenciesTest[key] <= node.monitor.Lambda
+
+
+def testClientLatencyThreshold(looper, txnPoolNodeSet, requests):
+    rq = requests[0]
+    for node in txnPoolNodeSet:
+        latc = node.monitor.getAvgLatency(
+            node.instances.masterId)[rq['identifier']]
+        avglat = node.monitor.getAvgLatency(
+            *node.instances.backupIds)[rq['identifier']]
+        assert latc - avglat <= node.monitor.Omega

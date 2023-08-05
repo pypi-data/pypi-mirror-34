da635f1b0f84ad5565e402243df4deb829e19bab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:26 +0000

    Add package watson-machine-learning-client/1.0.241/watson_machine_learning_client-1.0.241.tar.gz

---
+++ packages/watson-machine-learning-client/watson_machine_learning_client-1.0.241.tar.gz/watson_machine_learning_client-1.0.241/watson_machine_learning_client/tests/svt/test_runtimes_custom_libs_delete_orphaned_libs.py
@@ -0,0 +1,91 @@
+import unittest
+import datetime
+from preparation_and_cleaning import *
+from models_preparation import *
+from watson_machine_learning_client.log_util import get_logger
+
+
+class TestRuntimeSpec(unittest.TestCase):
+    runtime_uid = None
+    logger = get_logger(__name__)
+
+    @classmethod
+    def setUpClass(self):
+        TestRuntimeSpec.logger.info("Service Instance: setting up credentials")
+
+        self.wml_credentials = get_wml_credentials()
+        self.client = get_client()
+        self.custom_library_path = os.path.join(os.getcwd(), 'artifacts', 'scikit_xgboost_model.tar.gz') # TODO
+
+    def test_1_service_instance_details(self):
+        TestRuntimeSpec.logger.info("Check client ...")
+        self.assertTrue(self.client.__class__.__name__ == 'WatsonMachineLearningAPIClient')
+
+        TestRuntimeSpec.logger.info("Getting instance details ...")
+        details = self.client.service_instance.get_details()
+
+        TestRuntimeSpec.logger.debug(details)
+        self.assertTrue("published_models" in str(details))
+        self.assertEqual(type(details), dict)
+
+    def test_2_create_runtime(self):
+        custom_library_uid = self.client._runtimes._create_custom_library({
+            "name": "libraries_custom",
+            "description": "custom libraries for scoring",
+            "filepath": self.custom_library_path,
+            "version": "1.0",
+            "platform": {"name": "python", "versions": ["3.5"]}
+        })
+
+        meta = {
+            self.client._runtimes.ConfigurationMetaNames.NAME: "runtime_spec_python_3.5",
+            self.client._runtimes.ConfigurationMetaNames.DESCRIPTION: "test",
+            self.client._runtimes.ConfigurationMetaNames.PLATFORM: {
+                "name": "python",
+                "version": "3.5"
+            },
+            self.client._runtimes.ConfigurationMetaNames.CUSTOM_LIBRARIES_DEFINITIONS: [
+                {
+                    "name": "my_lib",
+                    "description": "t",
+                    "filepath": self.custom_library_path,
+                    "version": "1.0",
+                    "platform": {"name": "python", "versions": ["3.5"]}
+                },
+                {
+                    "name": "my_lib",
+                    "filepath": self.custom_library_path,
+                    "version": "1.1",
+                    "platform": {"name": "python", "versions": ["3.5"]}
+                }
+            ],
+            self.client._runtimes.ConfigurationMetaNames.CUSTOM_LIBRARIES_UIDS: [custom_library_uid]
+        }
+        runtime_details = self.client._runtimes.create(meta)
+        TestRuntimeSpec.runtime_uid = self.client._runtimes.get_uid(runtime_details)
+        runtime_url = self.client._runtimes.get_url(runtime_details)
+
+        self.assertTrue(TestRuntimeSpec.runtime_uid is not None)
+
+    def test_3_get_details(self):
+        print(self.client._runtimes.get_details(TestRuntimeSpec.runtime_uid))
+
+        print(self.client._runtimes.get_details())
+
+    def test_4_list(self):
+        self.client._runtimes.list()
+
+    def test_5_list_custom_libs(self):
+        self.client._runtimes.list_custom_libraries(TestRuntimeSpec.runtime_uid)
+        self.client._runtimes.list_custom_libraries()
+
+    def test_6_list_runtimes_for_libraries(self):
+        self.client._runtimes.list_runtimes_for_libraries()
+
+    def test_7_delete_runtime(self):
+        self.client._runtimes.delete(TestRuntimeSpec.runtime_uid)
+        self.client._runtimes.delete_orphaned_custom_libraries()
+
+
+if __name__ == '__main__':
+    unittest.main()

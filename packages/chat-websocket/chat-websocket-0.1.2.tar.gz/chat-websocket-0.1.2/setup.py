c674232ceb0d22f6b339a61bb76a5ab3f1bf1b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:45 +0000

    Add package chat-websocket/0.1.2/chat-websocket-0.1.2.tar.gz

---
+++ packages/chat-websocket/chat-websocket-0.1.2.tar.gz/chat-websocket-0.1.2/setup.py
@@ -0,0 +1,29 @@
+# !/usr/bin/env python
+from __future__ import print_function
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='chat-websocket',
+    version='0.1.2',
+    description='This is a websocket chat',
+    long_description=long_description,
+    url='http://github.com/storborg/funniest',
+    author='Kevin, Michael',
+    author_email='123kangpeng@gmail.com',
+    license='None',
+    packages=['chat-websocket'],
+    zip_safe=False,
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ],
+)
+

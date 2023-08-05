0a6b114311aa47fa2113f19191753fa1521c2be6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:34 +0000

    Add package flask-redisosgearth/0.0.2/flask_redisosgearth-0.0.2.tar.gz

---
+++ packages/flask-redisosgearth/flask_redisosgearth-0.0.2.tar.gz/flask_redisosgearth-0.0.2/setup.py
@@ -0,0 +1,36 @@
+"""
+flask_redisosgearth
+----------
+
+A Flask extension for redis
+
+Please refer to the online documentation for details.
+
+"""
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='flask_redisosgearth',
+    version='0.0.2',
+    license='BSD',
+    author='duoduoaixiaogai',
+    author_email='490850477@qq.com',
+    maintainer='duoduoaixiaogai',
+    maintainer_email='490850477@qq.com',
+    description='Flask extension for redis',
+    long_description=long_description,
+    packages=setuptools.find_packages(),
+    platforms='any',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)

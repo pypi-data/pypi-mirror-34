6b9cc868d4f074d112552b308a42c1bb411d4848
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:55 +0000

    Add package light-cyr/2.0/light_cyr-2.0.tar.gz

---
+++ packages/light-cyr/light_cyr-2.0.tar.gz/light_cyr-2.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="light_cyr",
+    version="2.0",
+    author="cyr",
+    author_email="15501766562@163.com",
+    description="A light flask",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)

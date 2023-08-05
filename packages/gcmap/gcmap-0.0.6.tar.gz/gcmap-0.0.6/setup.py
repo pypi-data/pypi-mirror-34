b5175187ef1c0879d421ba9e6653ab2195bcf294
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:25 +0000

    Add package gcmap/0.0.6/gcmap-0.0.6.tar.gz

---
+++ packages/gcmap/gcmap-0.0.6.tar.gz/gcmap-0.0.6/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name = 'gcmap',
+    version = '0.0.6',
+    url = 'https://github.com/paulgb/gcmap',
+    description = 'Great circle visualization of many coordinate pairs',
+    author = 'Paul Butler',
+    author_email = 'paulgb@gmail.com',
+    packages = ['gcmap'],
+    install_requires = [
+        'aggdraw>=1.2,<2',
+        'numpy>=1.7.1',
+        'Pillow>=2.0.0',
+        'pyproj>=1.9.3'
+    ]
+)

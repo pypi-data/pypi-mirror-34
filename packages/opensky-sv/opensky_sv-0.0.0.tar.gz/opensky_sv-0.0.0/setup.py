56a69f66c6a572d70109525a7cc0d77bc69174ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:00 +0000

    Add package opensky-sv/0.0.0/opensky_sv-0.0.0.tar.gz

---
+++ packages/opensky-sv/opensky_sv-0.0.0.tar.gz/opensky_sv-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+NAME='opensky_sv'
+
+setup(
+    name=NAME,
+    description="A library for Opensky Network",
+    long_description=open("README.md").read(),
+    url="https://github.com/sm-svyat/opensky_asv",
+    packages=[NAME],
+)

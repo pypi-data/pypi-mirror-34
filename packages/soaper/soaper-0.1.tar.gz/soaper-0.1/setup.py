8b14721d3b0d5ae2633a75f95410a80af6d94f64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:27 +0000

    Add package soaper/0.1/soaper-0.1.tar.gz

---
+++ packages/soaper/soaper-0.1.tar.gz/soaper-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+from os import path
+
+
+setup(name='soaper',
+      version='0.1',
+      description='soap data parser',
+      url='https://github.com/Jisu9921/soaper',
+      author='Jisu9921',
+      author_email='h41469921@gmail.com',
+      packages=find_packages(),
+      classifiers=[
+        'Programming Language :: Python :: 3.6'
+      ]
+      )

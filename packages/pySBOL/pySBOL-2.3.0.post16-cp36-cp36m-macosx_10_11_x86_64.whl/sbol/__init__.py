79221eeb207c5cccc3419ea5f4db7ea7daa63d66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:46 +0000

    Add package pySBOL/2.3.0.post16/pySBOL-2.3.0.post16-cp36-cp36m-macosx_10_11_x86_64.whl

---
+++ packages/pySBOL/pySBOL-2.3.0.post16-cp36-cp36m-macosx_10_11_x86_64.whl/sbol/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+__version__ = '2.3.0'
+__commit__ = '308f2ae9aa3b097801c046e816b2b796e04f162f'
+
+from sbol.libsbol import *
+from sbol.sbol import Activity
+import sbol.unit_tests

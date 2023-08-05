37922b9f96e18003f4df4c56b88785cbc65a9f1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:26 +0000

    Add package sample-kd/0.1.6/sample-kd-0.1.6.tar.gz

---
+++ packages/sample-kd/sample-kd-0.1.6.tar.gz/sample-kd-0.1.6/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="sample-kd",
+    version="0.1.6",
+    author="Jefung",
+    author_email="jefung865424525@gmail.com",
+    description="sample data and analysis",
+    long_description="",
+    url="https://github.com/KD-Group/Half.Sample",
+    packages=["sample"],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    data_files=[('cpp_build/sample.exe')]
+)

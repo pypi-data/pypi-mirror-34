08d12b7129d018f3bcb1163972968dc6dae8fd30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:46 +0000

    Add package jpredapi/1.5.5/jpredapi-1.5.5.tar.gz

---
+++ packages/jpredapi/jpredapi-1.5.5.tar.gz/jpredapi-1.5.5/jpredapi/api.py
@@ -0,0 +1,267 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+import re
+import os
+import tarfile
+from collections import OrderedDict
+
+import requests
+from retrying import retry
+
+
+WAIT_INTERVAL = 60000  # 60000 milliseconds = 60 seconds
+MAX_ATTEMPTS = 10
+
+
+def check_rest_version(host="http://www.compbio.dundee.ac.uk/jpred4/cgi-bin/rest", suffix="version", silent=False):
+    """Check version of JPred REST interface.
+
+    :param str host: JPred host address.
+    :param str suffix: Host address suffix.
+    :param silent: Should the work be done silently?
+    :type silent: :py:obj:`True` or :py:obj:`False`
+    :return: Version of JPred REST API.
+    :rtype: :py:class:`str`
+    """
+    version_url = "{}/{}".format(host, suffix)
+    response = requests.get(version_url)
+    version = re.search(r"VERSION=(v\.[0-9]*.[0-9]*)", response.text).group(1)
+    if not silent:
+         print(version)
+    return version
+
+
+def quota(email, host="http://www.compbio.dundee.ac.uk/jpred4/cgi-bin/rest", suffix="quota", silent=False):
+    """Check how many jobs you have already submitted on a given day
+    (out of 1000 maximum allowed jobs per user per day).
+
+    :param str email: E-mail address.
+    :param str host: JPred host address.
+    :param str suffix: Host address suffix.
+    :param silent: Should the work be done silently?
+    :type silent: :py:obj:`True` or :py:obj:`False`
+    :return: Response.
+    :rtype: requests.Response
+    """
+    quota_url = "{}/{}/{}".format(host, suffix, email)
+    response = requests.get(quota_url)
+    if not silent:
+        print(response.text)
+    return response
+
+
+def submit(mode, user_format, file=None, seq=None, skipPDB=True, email=None, name=None, silent=False,
+           host="http://www.compbio.dundee.ac.uk/jpred4/cgi-bin/rest"):
+    """Submit job to JPred REST API.
+
+    :param str mode: Submission mode, possible values: `single`, `batch`, `msa`.
+    :param str user_format: Submission format, possible values: `raw`, `fasta`, `msf`, `blc`.
+    :param str file: File path to a file with the job input (sequence or msa).
+    :param str seq: Alternatively, amino acid sequence passed as string of single-letter code without spaces, e.g. --seq=ATWFGTHY
+    :param skipPDB: Should the PDB query be skipped?
+    :type skipPDB: :py:obj:`True` or :py:obj:`False`
+    :param str email: For a batch job submission, where to send the results?
+    :param str name: A name for the job.
+    :param silent: Should the work be done silently?
+    :type silent: :py:obj:`True` or :py:obj:`False`
+    :return: Response.
+    :rtype: requests.Response
+    """
+    rest_format = _resolve_rest_format(mode=mode, user_format=user_format)
+    query = _create_jpred_query(rest_format=rest_format, file=file, seq=seq,
+                                skipPDB=skipPDB, email=email, name=name, silent=silent)
+
+    response = requests.post("{}/{}".format(host, "job"),
+                             data=query.encode("utf-8"),
+                             headers={"Content-type": "text/txt"})
+
+    if response.status_code == 202 and "created jred job" in response.text.lower():
+        if rest_format != "batch":
+            result_url = response.headers['Location']
+            jobid = re.search(r"(jp_.*)$", result_url).group(1)
+
+            if not silent:
+                print("Created JPred job with jobid:", jobid)
+                print("You can check the status of the job using the following URL:", result_url)
+
+        elif rest_format == "batch":
+            if not silent:
+                print(response.text)
+    else:
+        if not silent:
+            print(response.text, response.reason)
+
+    return response
+
+
+@retry(wait_fixed=WAIT_INTERVAL, stop_max_attempt_number=MAX_ATTEMPTS)
+def status(jobid, results_dir_path=None, extract=False, silent=False,
+           host="http://www.compbio.dundee.ac.uk/jpred4/cgi-bin/rest",
+           jpred4="http://www.compbio.dundee.ac.uk/jpred4"):
+    """Check status of the submitted job.
+
+    :param str jobid: Job id.
+    :param str results_dir_path: Directory path where to save results if job is finished.
+    :param extract: Extract (True) or not (False) results into directory.
+    :type extract: :py:obj:`True` or :py:obj:`False`
+    :param silent: Should the work be done silently?
+    :type silent: :py:obj:`True` or :py:obj:`False`
+    :param str host: JPred host address.
+    :param str jpred4: JPred address for results retrieval.
+    :return: Response.
+    :rtype: requests.Response
+    """
+    if not silent:
+        print("Your job status will be checked with the following parameters:")
+        print("Job id:", jobid)
+        print("Get results:", bool(results_dir_path))
+
+    job_url = "{}/{}/{}/{}".format(host, "job", "id", jobid)
+    response = requests.get(job_url)
+
+    if response.reason == "OK":
+        if not silent:
+            print(response.text)
+
+        if "finished" in response.text.lower():
+            if results_dir_path is not None:
+                results_dir_path = os.path.join(results_dir_path, jobid)
+                if not os.path.exists(results_dir_path):
+                    os.makedirs(results_dir_path)
+
+                archive_url = "{}/{}/{}/{}.{}".format(jpred4, "results", jobid, jobid, "tar.gz")
+                archive_path = os.path.join(results_dir_path, "{}.{}".format(jobid, "tar.gz"))
+
+                archive_response = requests.get(archive_url, stream=True)
+                with open(archive_path, "wb") as outfile:
+                    for chunk in archive_response.iter_content(chunk_size=1024):
+                        outfile.write(chunk)
+
+                if extract:
+                    tar_archive = tarfile.open(archive_path)
+                    tar_archive.extractall(path=results_dir_path)
+
+                if not silent:
+                    print("Saving results to: {}".format(os.path.abspath(results_dir_path)))
+    else:
+        response.raise_for_status()
+
+    return response
+
+
+def get_results(jobid, results_dir_path=None, extract=False, silent=False,
+                host="http://www.compbio.dundee.ac.uk/jpred4/cgi-bin/rest",
+                jpred4="http://www.compbio.dundee.ac.uk/jpred4"):
+    """Download results from JPred server.
+
+    :param str jobid: Job id.
+    :param str results_dir_path: Directory path where to save results if job is finished.
+    :param extract: Extract (True) or not (False) results into directory.
+    :type extract: :py:obj:`True` or :py:obj:`False`
+    :param silent: Should the work be done silently?
+    :type silent: :py:obj:`True` or :py:obj:`False`
+    :param str host: JPred host address.
+    :param str jpred4: JPred address for results retrieval.
+    :return: Response.
+    :rtype: requests.Response
+    """
+    if results_dir_path is None:
+        results_dir_path = os.path.join(os.getcwd(), jobid)
+    return status(jobid=jobid, results_dir_path=results_dir_path, extract=extract, silent=silent, host=host, jpred4=jpred4)
+
+
+def _resolve_rest_format(mode, user_format):
+    """Resolve format of submission to JPred REST interface based on provided mode and user format.
+
+    :param str mode: Submission mode, possible values: `single`, `batch`, `msa`.
+    :param str user_format: Submission format, possible values: `raw`, `fasta`, `msf`, `blc`.
+    :return: Format for JPred REST interface.
+    :rtype: :py:class:`str`
+    """
+    if user_format == "raw" and mode == "single":
+        rest_format = "seq"
+    elif user_format == "fasta" and mode == "single":
+        rest_format = "seq"
+    elif user_format == "fasta" and mode == "msa":
+        rest_format = "fasta"
+    elif user_format == "msf" and mode == "msa":
+        rest_format = "msf"
+    elif user_format == "blc" and mode == "msa":
+        rest_format = "blc"
+    elif user_format == "fasta" and mode == "batch":
+        rest_format = "batch"
+    else:
+        raise ValueError("""Invalid mode/format combination.
+        Valid combinations are: --mode=single --format=raw
+                                --mode=single --format=fasta
+                                --mode=msa    --format=fasta
+                                --mode=msa    --format=msf
+                                --mode=msa    --format=blc
+                                --mode=batch  --format=fasta""")
+    return rest_format
+
+
+def _create_jpred_query(rest_format, file=None, seq=None, skipPDB=True, email=None, name=None, silent=False):
+    """Create query string to be submitted to JPred server.
+
+    :param str rest_format: Format for JPred REST interface.
+    :param str file: File path to a file with the job input (sequence or msa).
+    :param str seq: Alternatively, amino acid sequence passed as string of single-letter code without spaces, e.g. --seq=ATWFGTHY
+    :param skipPDB: Should the PDB query be skipped?
+    :type skipPDB: :py:obj:`True` or :py:obj:`False`
+    :param str email: For a batch job submission, where to send the results?
+    :param str name: A name for the job.
+    :param silent: Should the work be done silently?
+    :type silent: :py:obj:`True` or :py:obj:`False`
+    :return: Query string.
+    :rtype: :py:class:`str`
+    """
+    if file is None and seq is None:
+        raise ValueError("""Neither input sequence nor input file are defined.
+        Please provide either --file or --seq parameters.""")
+    elif file and seq:
+        raise ValueError("""Both input sequence and input file are defined.
+        Please choose --file or --seq parameter.""")
+
+    if file is not None:
+        with open(file, "r") as infile:
+            sequence_query = infile.read()
+    elif seq is not None:
+        sequence_query = ">query\n{}".format(seq)
+    else:
+        sequence_query = ""
+
+    if skipPDB:
+        skipPDB = "on"
+    else:
+        skipPDB = "off"
+
+    if rest_format == "batch" and email is None:
+        raise ValueError("""When submitting batch job email is obligatory.
+        You will receive detailed report, list of links and a link to archive
+        to all results via email.""")
+
+    if not silent:
+        print("Your job will be submitted with the following parameters:")
+        print("format:", rest_format)
+        print("skipPDB:", skipPDB)
+        if file is not None:
+            print("file:", file)
+        elif seq is not None:
+            print("seq:", seq)
+        if email is not None:
+            print("email:", email)
+        if name is not None:
+            print("name:", name)
+
+    parameters_dict = OrderedDict([("skipPDB", skipPDB),
+                                   ("format", rest_format),
+                                   ("email", email),
+                                   ("name", name)])
+
+    parameters_list = ["{}={}".format(k, v) for k, v in parameters_dict.items() if v]
+    parameters_list.append(sequence_query)
+    query = u"£€£€".join(parameters_list)
+    return query
---
+++ packages/jpredapi/jpredapi-1.5.5.tar.gz/jpredapi-1.5.5/jpredapi/cli.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+jpredapi command-line interface
+
+The JPred API allows users to submit jobs from the command-line.
+
+Usage:
+    jpredapi submit (--mode=<mode> --format=<format>) (--file=<filename> | --seq=<sequence>) [--email=<name@domain.com>] [--name=<name>] [--rest=<address>] [--skipPDB] [--silent]
+    jpredapi status (--jobid=<id>) [--results=<path>] [--wait=<interval>] [--attempts=<max>] [--rest=<address>] [--jpred4=<address>] [--extract] [--silent]
+    jpredapi get_results (--jobid=<id>) [--results=<path>] [--wait=<interval>] [--attempts=<max>] [--rest=<address>] [--jpred4=<address>] [--extract] [--silent]
+    jpredapi quota (--email=<name@domain.com>) [--rest=<address>] [--silent]
+    jpredapi check_rest_version [--rest=<address>] [--silent]
+    jpredapi -h | --help
+    jpredapi -v | --version
+
+Options:
+    -h, --help                 Show this help message.
+    -v, --version              Show jpredapi package version.
+    --silent                   Do not print messages.
+    --extract                  Extract results tar.gz archive.
+    --skipPDB                  PDB check.
+    --mode=<mode>              Submission mode, possible values: single, batch, msa.
+    --format=<format>          Submission format, possible values: raw, fasta, msf, blc.
+    --file=<filename>          Filename of a file with the job input (sequence(s)).
+    --seq=<sequence>           Instead of passing input file, for single-sequence submission.
+    --email=<name@domain.com>  E-mail address where job report will be sent (optional for all but batch submissions).
+    --name=<name>              Job name.
+    --jobid=<id>               Job id.
+    --results=<path>           Path to directory where to save archive with results.
+    --rest=<address>           REST address of server [default: http://www.compbio.dundee.ac.uk/jpred4/cgi-bin/rest].
+    --jpred4=<address>         Address of Jpred4 server [default: http://www.compbio.dundee.ac.uk/jpred4].
+    --wait=<interval>          Wait interval before retrying to check job status in seconds [default: 60].
+    --attempts=<max>           Maximum number of attempts to check job status [default: 10].
+"""
+
+from . import api
+
+
+def cli(cmdargs):
+    """jpredapi command-line interface processor."""
+
+    api.WAIT_INTERVAL = float(cmdargs["--wait"]) * 1000  # convert from seconds to milliseconds
+    api.MAX_ATTEMPTS = int(cmdargs["--attempts"])
+
+    if cmdargs["submit"]:
+        api.submit(mode=cmdargs["--mode"],
+                   user_format=cmdargs["--format"],
+                   file=cmdargs["--file"],
+                   seq=cmdargs["--seq"],
+                   skipPDB=cmdargs["--skipPDB"],
+                   email=cmdargs["--email"],
+                   name=cmdargs["--name"],
+                   silent=cmdargs["--silent"],
+                   host=cmdargs["--rest"])
+
+    elif cmdargs["status"]:
+        api.status(jobid=cmdargs["--jobid"],
+                   results_dir_path=cmdargs["--results"],
+                   extract=cmdargs["--extract"],
+                   silent=cmdargs["--silent"],
+                   host=cmdargs["--rest"],
+                   jpred4=cmdargs["--jpred4"])
+
+    elif cmdargs["get_results"]:
+        api.get_results(jobid=cmdargs["--jobid"],
+                        results_dir_path=cmdargs["--results"],
+                        extract=cmdargs["--extract"],
+                        silent=cmdargs["--silent"],
+                        host=cmdargs["--rest"],
+                        jpred4=cmdargs["--jpred4"])
+
+    elif cmdargs["quota"]:
+        api.quota(email=cmdargs["--email"],
+                  host=cmdargs["--rest"],
+                  suffix="quota",
+                  silent=cmdargs["--silent"])
+
+    elif cmdargs["check_rest_version"]:
+        api.check_rest_version(host=cmdargs["--rest"],
+                               suffix="version",
+                               silent=cmdargs["--silent"])

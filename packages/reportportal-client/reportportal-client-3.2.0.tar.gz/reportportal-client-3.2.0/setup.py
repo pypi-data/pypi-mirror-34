3715ed845357526eab282a610ffdd851688a1246
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:44 +0000

    Add package reportportal-client/3.2.0/reportportal-client-3.2.0.tar.gz

---
+++ packages/reportportal-client/reportportal-client-3.2.0.tar.gz/reportportal-client-3.2.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='reportportal-client',
+    packages=find_packages(),
+    version='3.2.0',
+    description='Python client for Report Portal',
+    author='Artsiom Tkachou',
+    author_email='SupportEPMC-TSTReportPortal@epam.com',
+    url='https://github.com/reportportal/client-Python',
+    download_url='https://github.com/reportportal/client-Python/tarball/3.2.0',
+    license='GNU General Public License v3',
+    keywords=['testing', 'reporting', 'reportportal'],
+    classifiers=[],
+    install_requires=['requests>=2.4.2', 'six'],
+)

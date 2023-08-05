3972e449bf7158aed039ce6fcdb7315b1b105dbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:57 +0000

    Add package desdeo-vis/0.1.2/desdeo-vis-0.1.2.tar.gz

---
+++ packages/desdeo-vis/desdeo-vis-0.1.2.tar.gz/desdeo-vis-0.1.2/desdeo_vis/nbextension/static/lib/src/widgets/PrefInput.vue.d.ts
@@ -0,0 +1,14 @@
+import { Vue } from "vue-property-decorator";
+import { DimPref, SliderConf } from './utils';
+export default class PrefInput extends Vue {
+    conf: SliderConf;
+    pref: DimPref;
+    readonly PREF_KINDS: string[];
+    kindChange(input: any): void;
+    valueChange(input: any): void;
+    onPrefChange(pref: any): void;
+    updatePref(pref: DimPref): void;
+    readonly hasValue: boolean;
+    readonly value: number | null;
+    readonly dispKinds: string[];
+}

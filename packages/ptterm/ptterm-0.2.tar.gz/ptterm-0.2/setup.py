7ca9cffccec14a24f9eac2b620409b8d9f9dec2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:24 +0000

    Add package ptterm/0.2/ptterm-0.2.tar.gz

---
+++ packages/ptterm/ptterm-0.2.tar.gz/ptterm-0.2/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import os
+import sys
+from setuptools import setup, find_packages
+
+
+long_description = open(
+    os.path.join(
+        os.path.dirname(__file__),
+        'README.rst'
+    )
+).read()
+
+requirements = [
+    'prompt_toolkit>=2.0.0,<2.1.0',
+    'pyte>=0.5.1',
+    'six>=1.9.0',
+]
+
+# Install yawinpty on Windows only.
+if sys.platform.startswith('win'):
+    requirements.append('yawinpty')
+
+
+setup(
+    name='ptterm',
+    author='Jonathan Slenders',
+    version='0.2',
+    license='LICENSE',
+    url='https://github.com/jonathanslenders/ptterm',
+    description='Terminal emulator for prompt_toolkit.',
+    long_description=long_description,
+    packages=find_packages('.'),
+    install_requires = requirements,
+)

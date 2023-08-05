6dba0450225c8495c3e6a833701cb63feba11085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:59 +0000

    Add package otta/0.0.1/otta-0.0.1.tar.gz

---
+++ packages/otta/otta-0.0.1.tar.gz/otta-0.0.1/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+readme = open('README.md').read()
+
+setup(
+    name = 'otta',
+    packages = ['otta'],
+    install_requires = [
+      'pyyaml',
+      'jsonschema',
+      'termcolor'
+    ],
+    version = '0.0.1',
+    description = 'Multi-file compose/stack helper',
+    long_description = readme,
+    long_description_content_type='text/markdown',
+    author = 'Wolphin',
+    author_email = "wolphin@wolph.in",
+    url = "https://gitlab.com/q_wolphin/otta",
+    py_modules = ['otta'],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    entry_points={
+        'console_scripts': [
+            'otta=otta:otta',
+            'skara=otta:skara',
+            'kumla=otta:kumla',
+        ]
+    },
+)

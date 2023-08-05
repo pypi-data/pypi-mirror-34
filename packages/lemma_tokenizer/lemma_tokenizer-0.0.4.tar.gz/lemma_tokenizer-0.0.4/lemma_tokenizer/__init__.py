c278177db74af5e756071e29e61a9fb2b8f4684d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:20 +0000

    Add package lemma_tokenizer/0.0.4/lemma_tokenizer-0.0.4.tar.gz

---
+++ packages/lemma_tokenizer/lemma_tokenizer-0.0.4.tar.gz/lemma_tokenizer-0.0.4/lemma_tokenizer/__init__.py
@@ -0,0 +1,11 @@
+"""
+This file imports the objects that are used for data analysis for
+LemmaTokenizer
+Usage
+-----
+In the ipython notebook or python console, type:
+    from lemma_tokenizer import *
+To view the definition of each function, use docstring.
+"""
+
+from .lemma_tokenizer import *

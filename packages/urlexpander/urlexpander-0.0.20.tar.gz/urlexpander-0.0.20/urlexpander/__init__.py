61cca23d530d5e502d9ab6cce74dc3e0ab3fbc50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:32 +0000

    Add package urlexpander/0.0.20/urlexpander-0.0.20.tar.gz

---
+++ packages/urlexpander/urlexpander-0.0.20.tar.gz/urlexpander-0.0.20/urlexpander/__init__.py
@@ -0,0 +1,19 @@
+from urlexpander.core.api import(
+    strip_url,
+    get_domain,
+    is_short,
+    is_short_domain,
+    expand,
+    multithread_expand,
+    count_matrix
+)
+
+from urlexpander.core import (
+    tweet_parser, 
+    html_parser, 
+    constants, 
+    datasets
+)
+
+__version__ = '0.0.20'
+__author__ = 'Leon Yin'

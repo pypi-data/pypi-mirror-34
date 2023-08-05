137e8ed77c74cf733e2b0be6b177f65799b6621c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:15 +0000

    Add package mr-clean/0.0.1/mr_clean-0.0.1.tar.gz

---
+++ packages/mr-clean/mr_clean-0.0.1.tar.gz/mr_clean-0.0.1/README.md
@@ -0,0 +1,3 @@
+# Mr Clean
+
+This is a small package for cleaning data. Feel free to use it to clean other, non data-related things though.
---
+++ packages/mr-clean/mr_clean-0.0.1.tar.gz/mr_clean-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup,find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="mr_clean",
+    version="0.0.1",
+    author="A1Liu",
+    author_email="albertymliu@gmail.com",
+    description="A small package for cleaning data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/A1Liu/mr_clean",
+    packages=find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+    ),
+)

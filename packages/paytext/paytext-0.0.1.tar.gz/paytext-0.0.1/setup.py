1e99de883865e39a15e2d23634d3f4cba0b097db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:23 +0000

    Add package paytext/0.0.1/paytext-0.0.1.tar.gz

---
+++ packages/paytext/paytext-0.0.1.tar.gz/paytext-0.0.1/setup.py
@@ -0,0 +1,23 @@
+# pyre-strict
+
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='paytext',
+    version='0.0.1',
+    author='Yngve Høiseth',
+    author_email='yngve@hoiseth.net',
+    description='Generalize texts from payment card transactions',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/yhoiseth/paytext',
+    packages=setuptools.find_packages(),
+    classifiers=(
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ),
+)

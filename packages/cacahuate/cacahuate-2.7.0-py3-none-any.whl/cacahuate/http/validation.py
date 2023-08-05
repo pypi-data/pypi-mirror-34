85673e28ec6d3c6ceadf7cfc45c4837713117ac1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:18 +0000

    Add package cacahuate/2.7.0/cacahuate-2.7.0-py3-none-any.whl

---
+++ packages/cacahuate/cacahuate-2.7.0-py3-none-any.whl/cacahuate/http/validation.py
@@ -0,0 +1,47 @@
+from flask import abort
+
+from cacahuate.errors import RequiredInputError, HierarchyError, \
+    MisconfiguredProvider
+from cacahuate.http.errors import BadRequest, Forbidden
+from cacahuate.http.wsgi import app
+from cacahuate.utils import user_import
+
+
+def validate_json(json_data: dict, req: list):
+    errors = []
+
+    for item in req:
+        if item not in json_data:
+            errors.append(RequiredInputError(
+                item,
+                'request.body.{}'.format(item)
+            ).to_json())
+
+    if errors:
+        raise BadRequest(errors)
+
+
+def validate_auth(node, user, state=None):
+    if not node.auth_backend:
+        return
+
+    try:
+        HiPro = user_import(
+            node.auth_backend,
+            'HierarchyProvider',
+            app.config['HIERARCHY_PROVIDERS'],
+            'cacahuate.auth.hierarchy',
+        )
+    except MisconfiguredProvider:
+        abort(500, 'Misconfigured hierarchy provider, sorry')
+
+    hipro = HiPro(app.config)
+
+    try:
+        hipro.validate_user(user, **node.resolve_params(state))
+    except HierarchyError:
+        raise Forbidden([{
+            'detail': 'The provided credentials do not match the specified'
+                      ' hierarchy',
+            'where': 'request.authorization',
+        }])
---
+++ packages/cacahuate/cacahuate-2.7.0-py3-none-any.whl/cacahuate/utils.py
@@ -0,0 +1,44 @@
+from case_conversion import pascalcase
+from importlib import import_module
+from coralillo.errors import ModelNotFoundError
+import os
+import sys
+
+from cacahuate.errors import MisconfiguredProvider
+from cacahuate.models import User
+
+
+def user_import(module_key, class_sufix, import_maper, default_path):
+    ''' import a provider defined by the user '''
+    if module_key in import_maper:
+        import_path = import_maper[module_key]
+
+        cwd = os.getcwd()
+
+        if cwd not in sys.path:
+            sys.path.insert(0, cwd)
+    else:
+        import_path = default_path + '.' + module_key
+
+    cls_name = pascalcase(module_key) + class_sufix
+
+    try:
+        mod = import_module(import_path)
+        cls = getattr(mod, cls_name)
+    except ModuleNotFoundError as e:
+        raise MisconfiguredProvider(
+            'Could not import provider module {}'.format(import_path)
+        )
+    except AttributeError as e:
+        raise MisconfiguredProvider('Provider does not define class {}'.format(
+            cls_name,
+        ))
+
+    return cls
+
+
+def get_or_create(identifier, data):
+    try:
+        return User.get_by_or_exception('identifier', identifier)
+    except ModelNotFoundError:
+        return User(**data).save()

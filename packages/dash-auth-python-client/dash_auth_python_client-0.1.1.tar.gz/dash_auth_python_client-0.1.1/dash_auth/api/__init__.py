62f794d53b6b5e9067bfddd6718c5ff883a04bcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:08 +0000

    Add package dash-auth-python-client/0.1.1/dash_auth_python_client-0.1.1.tar.gz

---
+++ packages/dash-auth-python-client/dash_auth_python_client-0.1.1.tar.gz/dash_auth_python_client-0.1.1/dash_auth/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from dash_auth.api.account_api import AccountApi
+from dash_auth.api.login_api import LoginApi
+from dash_auth.api.logout_api import LogoutApi
+from dash_auth.api.password_api import PasswordApi

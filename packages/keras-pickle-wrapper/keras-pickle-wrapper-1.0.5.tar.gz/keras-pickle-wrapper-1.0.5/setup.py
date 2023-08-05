e055c155fc64012dcd4118d118cd7596af90cb07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:38 +0000

    Add package keras-pickle-wrapper/1.0.5/keras-pickle-wrapper-1.0.5.tar.gz

---
+++ packages/keras-pickle-wrapper/keras-pickle-wrapper-1.0.5.tar.gz/keras-pickle-wrapper-1.0.5/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+import os
+
+try:
+    with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'),
+            'rt') as f:
+        long_desc = f.read()
+except:
+    # Doesn't need to be in PyPI package.
+    long_desc = None
+
+setup(
+        name='keras-pickle-wrapper',
+        packages=['keras_pickle_wrapper'],
+        install_requires=['keras', 'h5py'],
+        version='1.0.5',
+        description='A small library that wraps Keras models to pickle them.',
+        long_description=long_desc,
+        author='Walt Woods',
+        author_email='woodswalben@gmail.com',
+        url='https://github.com/wwoods/keras_pickle_wrapper',
+        keywords=['keras', 'pickle'],
+)
+

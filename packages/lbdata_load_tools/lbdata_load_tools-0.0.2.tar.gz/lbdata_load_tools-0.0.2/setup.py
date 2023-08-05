ee7c3c948da4733f3e094f020adcc78240dda696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:52 +0000

    Add package lbdata_load_tools/0.0.2/lbdata_load_tools-0.0.2.tar.gz

---
+++ packages/lbdata_load_tools/lbdata_load_tools-0.0.2.tar.gz/lbdata_load_tools-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='lbdata_load_tools',
+    version='0.0.2',
+    author='Alexandre PECCAUD',
+    author_email='alexandre.peccaud@gmail.com',
+    description='Tools to help loading data in Redshift',
+    long_description=long_description,
+    url='https://github.com/lovebonito/lbdata_load_tools',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'boto3==1.7.44',
+        'botocore==1.10.44',
+        'psycopg2==2.7.5',
+        'psycopg2-binary==2.7.5'
+    ]
+)

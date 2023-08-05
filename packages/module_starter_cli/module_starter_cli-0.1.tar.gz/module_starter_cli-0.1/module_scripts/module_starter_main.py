48beed9e35456fed31edc534ab5cff4eab12c9ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:28 +0000

    Add package module_starter_cli/0.1/module_starter_cli-0.1.tar.gz

---
+++ packages/module_starter_cli/module_starter_cli-0.1.tar.gz/module_starter_cli-0.1/module_scripts/module_starter_main.py
@@ -0,0 +1,9 @@
+def main():
+    # Module logic should go here
+    # Should define logic in seperate functions.
+    #print "haha"
+    print "Module starter project!"
+    
+if __name__ == '__main__':
+    main()
+    #print "haha"

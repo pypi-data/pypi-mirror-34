24141bcf39ada1d2bfcdc6842c074904fab35f37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:49 +0000

    Add package channels-redux/1.4/channels_redux-1.4-py3-none-any.whl

---
+++ packages/channels-redux/channels_redux-1.4-py3-none-any.whl/channels_redux/signals.py
@@ -0,0 +1,25 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from channels_redux.notify import NotifyConsumer, NotifierMixin
+
+print('-' * 80)
+print('IMPORTED')
+print('-' * 80)
+
+
+@receiver(post_save, dispatch_uid="channels_redux.signals.object_saved")
+def object_saved(sender, instance: NotifierMixin, created, **kwargs):
+    if not isinstance(instance, NotifierMixin):
+        return
+    if created:
+        NotifyConsumer.object_created(instance)
+    else:
+        NotifyConsumer.object_updated(instance)
+
+
+@receiver(post_delete, dispatch_uid="channels_redux.signals.object_deleted")
+def object_deleted(sender, instance: NotifierMixin, **kwargs):
+    if not isinstance(instance, NotifierMixin):
+        return
+    NotifyConsumer.object_deleted(instance)

96aa5222ee9a725bd724849eb1b9966b1dba55b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:22 +0000

    Add package python-pinyin/0.1.0/python-pinyin-0.1.0.tar.gz

---
+++ packages/python-pinyin/python-pinyin-0.1.0.tar.gz/python-pinyin-0.1.0/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+setup(
+    name="python-pinyin",
+    author="mozillazg",
+    author_email='mozillazg101@gmail.com',
+    version="0.1.0",
+    url="https://pypi.org/project/pypinyin/",
+    long_description="Use https://pypi.org/project/pypinyin/ instead",
+    install_requires=['pypinyin'],
+    classifiers=[],
+)

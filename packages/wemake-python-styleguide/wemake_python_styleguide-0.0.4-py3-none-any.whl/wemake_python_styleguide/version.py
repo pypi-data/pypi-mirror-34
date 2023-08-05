de713a5eea29585fc3477499aeb67257603fba6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:20 +0000

    Add package wemake-python-styleguide/0.0.4/wemake_python_styleguide-0.0.4-py3-none-any.whl

---
+++ packages/wemake-python-styleguide/wemake_python_styleguide-0.0.4-py3-none-any.whl/wemake_python_styleguide/version.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+import pkg_resources
+
+version = pkg_resources.get_distribution('wemake-python-styleguide').version
---
+++ packages/wemake-python-styleguide/wemake_python_styleguide-0.0.4-py3-none-any.whl/wemake_python_styleguide/visitors/base/visitor.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from ast import NodeVisitor
+from typing import List
+
+from wemake_python_styleguide.errors import BaseStyleViolation
+
+
+class BaseNodeVisitor(NodeVisitor):
+    """This class allows to store errors while traversing node tree."""
+
+    def __init__(self) -> None:
+        """Creates new visitor instance."""
+        super().__init__()
+        self._errors: List[BaseStyleViolation] = []
+
+    @property
+    def errors(self) -> List[BaseStyleViolation]:
+        """Return errors collected by this visitor."""
+        return self._errors
+
+    def add_error(self, error: BaseStyleViolation) -> None:
+        """Adds error to the visitor."""
+        self._errors.append(error)
+
+    def provide_options(self, options) -> None:
+        """Provides options for checking."""
+        self.options = options

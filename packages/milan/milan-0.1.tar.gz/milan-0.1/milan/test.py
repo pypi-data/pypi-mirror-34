f997a2fab8340f84cd05909749ebadd2c67d69c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:31 +0000

    Add package milan/0.1/milan-0.1.tar.gz

---
+++ packages/milan/milan-0.1.tar.gz/milan-0.1/milan/test.py
@@ -0,0 +1,3 @@
+def test_function():
+    print "test code for package."
+    print "Developer package."
---
+++ packages/milan/milan-0.1.tar.gz/milan-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name='milan',
+      version='0.1',
+      description='Test package',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='',
+      packages=['milan'],
+      zip_safe=False)

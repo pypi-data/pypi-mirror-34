99557152935a7c37fd8133186ae20a994f0f5de3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:35 +0000

    Add package kenl380.pylib/1.0.0rc1/kenl380.pylib-1.0.0rc1.tar.gz

---
+++ packages/kenl380.pylib/kenl380.pylib-1.0.0rc1.tar.gz/kenl380.pylib-1.0.0rc1/kenl380/pylib/__version__.py
@@ -0,0 +1,11 @@
+
+VERSION = (1, 0, '0rc1')
+
+__title__ = 'kenl380.pylib'
+__description__ = 'Python Library for common code used across my packages.'
+__url__ = 'https://github.com/kenlowrie/pylib'
+__author__ = 'Ken Lowrie'
+__author_email__ = 'ken@kenlowrie.com'
+__version__ = '.'.join(map(str, VERSION))
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2018 Kenneth Lowrie'

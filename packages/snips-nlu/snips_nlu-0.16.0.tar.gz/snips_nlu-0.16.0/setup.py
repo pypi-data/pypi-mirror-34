7415fcb1c7b8de5a0b5d29b7ce59b9b88f87feb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:30 +0000

    Add package snips-nlu/0.16.0/snips_nlu-0.16.0.tar.gz

---
+++ packages/snips-nlu/snips_nlu-0.16.0.tar.gz/snips_nlu-0.16.0/setup.py
@@ -0,0 +1,82 @@
+import io
+import os
+
+from setuptools import setup, find_packages
+
+packages = [p for p in find_packages()
+            if "tests" not in p and "debug" not in p]
+
+root = os.path.abspath(os.path.dirname(__file__))
+
+with io.open(os.path.join(root, "snips_nlu", "__about__.py"),
+             encoding="utf8") as f:
+    about = dict()
+    exec(f.read(), about)
+
+
+with io.open(os.path.join(root, "README.rst"), encoding="utf8") as f:
+    readme = f.read()
+
+nlu_metrics_version = "0.12.0"
+
+required = [
+    "enum34==1.1.6",
+    "pathlib==1.0.1",
+    "numpy==1.14.0",
+    "scipy==1.0.0",
+    "scikit-learn==0.19.1",
+    "sklearn-crfsuite==0.3.6",
+    "semantic_version==2.6.0",
+    "snips_nlu_utils==0.6.1",
+    "snips_nlu_ontology==0.57.2",
+    "num2words==0.5.6",
+    "plac==0.9.6",
+    "requests==2.18.4"
+]
+
+extras_require = {
+    "doc": [
+        "sphinx==1.7.1",
+        "sphinxcontrib-napoleon==0.6.1",
+        "sphinx-rtd-theme==0.2.4"
+    ],
+    "metrics": [
+        "snips_nlu_metrics==%s" % nlu_metrics_version,
+    ],
+    "test": [
+        "mock==2.0.0",
+        "snips_nlu_metrics==%s" % nlu_metrics_version,
+        "pylint==1.8.2",
+        "coverage==4.4.2"
+    ],
+    "integration_test": [
+        "snips_nlu_metrics==%s" % nlu_metrics_version,
+    ]
+}
+
+setup(name=about["__title__"],
+      description=about["__summary__"],
+      long_description=readme,
+      version=about["__version__"],
+      author=about["__author__"],
+      author_email=about["__email__"],
+      license=about["__license__"],
+      url=about["__uri__"],
+      install_requires=required,
+      extras_require=extras_require,
+      classifiers=[
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+      ],
+      packages=packages,
+      include_package_data=True,
+      entry_points={
+          "console_scripts": [
+              "snips-nlu=snips_nlu.__main__:main"
+          ]
+      },
+      zip_safe=False)
---
+++ packages/snips-nlu/snips_nlu-0.16.0.tar.gz/snips_nlu-0.16.0/snips_nlu/pipeline/processing_unit.py
@@ -0,0 +1,96 @@
+import json
+from abc import ABCMeta, abstractmethod
+from builtins import object
+from pathlib import Path
+
+from future.utils import with_metaclass
+
+from snips_nlu.pipeline.configs import ProcessingUnitConfig
+from snips_nlu.utils import classproperty, json_string
+
+
+class ProcessingUnit(with_metaclass(ABCMeta, object)):
+    """Abstraction of a NLU pipeline unit
+
+    Pipeline processing units such as intent parsers, intent classifiers and
+    slot fillers must implement this class.
+
+    A :class:`ProcessingUnit` is associated with a *config_type*, which
+    represents the :class:`.ProcessingUnitConfig` used to initialize it.
+    """
+
+    def __init__(self, config):
+        if config is None or isinstance(config, ProcessingUnitConfig):
+            self.config = config
+        elif isinstance(config, dict):
+            self.config = self.config_type.from_dict(config)
+        else:
+            raise ValueError("Unexpected config type: %s" % type(config))
+
+    def persist_metadata(self, path, **kwargs):
+        metadata = {"unit_name": self.unit_name}
+        metadata.update(kwargs)
+        metadata_json = json_string(metadata)
+        with (path / "metadata.json").open(mode="w") as f:
+            f.write(metadata_json)
+
+    @classproperty
+    def unit_name(cls):  # pylint:disable=no-self-argument
+        raise NotImplementedError
+
+    @classproperty
+    def config_type(cls):  # pylint:disable=no-self-argument
+        raise NotImplementedError
+
+    @abstractmethod
+    def persist(self, path):
+        pass
+
+    @classmethod
+    def from_path(cls, path):
+        raise NotImplementedError
+
+
+def _get_unit_type(unit_name):
+    from snips_nlu.pipeline.units_registry import NLU_PROCESSING_UNITS
+
+    unit = NLU_PROCESSING_UNITS.get(unit_name)
+    if unit is None:
+        raise ValueError("ProcessingUnit not found: %s" % unit_name)
+    return unit
+
+
+def get_processing_unit_config(unit_config):
+    """Returns the :class:`.ProcessingUnitConfig` corresponding to
+        *unit_config*"""
+    if isinstance(unit_config, ProcessingUnitConfig):
+        return unit_config
+    elif isinstance(unit_config, dict):
+        unit_name = unit_config["unit_name"]
+        processing_unit_type = _get_unit_type(unit_name)
+        return processing_unit_type.config_type.from_dict(unit_config)
+    else:
+        raise ValueError("Expected `unit_config` to be an instance of "
+                         "ProcessingUnitConfig or dict but found: %s"
+                         % type(unit_config))
+
+
+def build_processing_unit(unit_config):
+    """Creates a new :class:`ProcessingUnit` from the provided *unit_config*
+
+    Args:
+        unit_config (:class:`.ProcessingUnitConfig`): The processing unit
+            config
+    """
+    unit = _get_unit_type(unit_config.unit_name)
+    return unit(unit_config)
+
+
+def load_processing_unit(unit_path):
+    """Load a :class:`ProcessingUnit` from a persisted processing unit
+    directory"""
+    unit_path = Path(unit_path)
+    with (unit_path / "metadata.json").open() as f:
+        metadata = json.load(f)
+    unit = _get_unit_type(metadata["unit_name"])
+    return unit.from_path(unit_path)

67137f664f56adfe83cda1cd8bba53f8fb8a3f2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:46 +0000

    Add package lost-n-phoned/0.1.2/lost_n_phoned-0.1.2-py3-none-any.whl

---
+++ packages/lost-n-phoned/lost_n_phoned-0.1.2-py3-none-any.whl/lostnphoned/schema.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  phone_number TEXT UNIQUE NOT NULL,
+  token TEXT NOT NULL,
+  refresh_token TEXT,
+  token_uri TEXT NOT NULL,
+  client_id TEXT NOT NULL,
+  client_secret TEXT NOT NULL
+);

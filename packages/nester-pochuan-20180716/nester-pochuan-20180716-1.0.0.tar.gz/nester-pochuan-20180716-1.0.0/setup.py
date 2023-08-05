4da7995a4aac16d85ed784abe2d6515a7a473717
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:24 +0000

    Add package nester-pochuan-20180716/1.0.0/nester-pochuan-20180716-1.0.0.tar.gz

---
+++ packages/nester-pochuan-20180716/nester-pochuan-20180716-1.0.0.tar.gz/nester-pochuan-20180716-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='nester-pochuan-20180716',
+    version='1.0.0',
+    py_modules=['nester'],
+    url='',
+    license='',
+    author='Pochuan',
+    author_email='pochuan0720@sina.com',
+    description='A simple printer of nested lists'
+)

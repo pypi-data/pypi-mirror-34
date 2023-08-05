86fcac5c31ab516f0cbe738f67fbc3c68755f624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:23 +0000

    Add package pre-commit-hooks/1.4.0/pre_commit_hooks-1.4.0-py2.py3-none-any.whl

---
+++ packages/pre-commit-hooks/pre_commit_hooks-1.4.0-py2.py3-none-any.whl/pre_commit_hooks/autopep8_wrapper.py
@@ -0,0 +1,30 @@
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import io
+import sys
+
+import autopep8
+
+
+def main(argv=None):
+    argv = argv if argv is not None else sys.argv[1:]
+    args = autopep8.parse_args(argv, apply_config=True)
+
+    retv = 0
+    for filename in args.files:
+        with io.open(filename, encoding='UTF-8') as f:
+            original_contents = f.read()
+        new_contents = autopep8.fix_code(original_contents, args)
+        if original_contents != new_contents:
+            print('Fixing {}'.format(filename))
+            retv = 1
+            with io.open(filename, 'w', encoding='UTF-8') as output_file:
+                output_file.write(new_contents)
+
+    return retv
+
+
+if __name__ == '__main__':
+    exit(main())

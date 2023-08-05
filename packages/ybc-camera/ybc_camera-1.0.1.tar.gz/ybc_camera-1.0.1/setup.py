2550d21b89af0333b165eb2743b26b8df7863cb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:49 +0000

    Add package ybc-camera/1.0.1/ybc_camera-1.0.1.tar.gz

---
+++ packages/ybc-camera/ybc_camera-1.0.1.tar.gz/ybc_camera-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'ybc_camera',
+  packages = ['ybc_camera'],
+  version = '1.0.1',
+  description = 'ybc camera',
+  long_description='ybc camera',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'python3','python','camera'],
+  license='MIT',
+  install_requires=['opencv-python']
+)

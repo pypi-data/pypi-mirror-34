35e6fa7f22632f105a240021fcb4a42a1ed8e6d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:25 +0000

    Add package pyleap/0.1.2/pyleap-0.1.2.tar.gz

---
+++ packages/pyleap/pyleap-0.1.2.tar.gz/pyleap-0.1.2/README.md
@@ -0,0 +1,16 @@
+# pyleap: A simple 2D game engine base on pyglet
+
+upload to pypi
+```shell
+$ python setup.py sdist bdist_wheel
+$ twine upload dist/* -u VicWang -p PYPI******
+```
+
+pyinstaller
+
+```shell
+$ pyinstaller -F leap_server.py -i pyleap.ico
+```
+
+
+
---
+++ packages/pyleap/pyleap-0.1.2.tar.gz/pyleap-0.1.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyleap",
+    version="0.1.2",
+    author="Vic Wang",
+    author_email="305880887@qq.com",
+    description='A simple 2D game engine base on pyglet',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://github.com/XRDX/pyleap',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pyglet',
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ),
+)

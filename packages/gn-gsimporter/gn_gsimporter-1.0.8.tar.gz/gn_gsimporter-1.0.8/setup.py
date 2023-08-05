7502d7316d96a8f55d79decbb8d8b16da603a4e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:51 +0000

    Add package gn-gsimporter/1.0.8/gn_gsimporter-1.0.8.tar.gz

---
+++ packages/gn-gsimporter/gn_gsimporter-1.0.8.tar.gz/gn_gsimporter-1.0.8/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name = "gn_gsimporter",
+    version = "1.0.8",
+    description = "GeoNode GeoServer Importer Client",
+    keywords = "GeoNode GeoServer Importer",
+    license = "MIT",
+    url = "https://github.com/GeoNode/gsimporter",
+    author = "Ian Schneider",
+    author_email = "ischneider@opengeo.org",
+    install_requires = [
+        'httplib2',
+        'urllib3'
+    ],
+    tests_require = [
+        'gisdata>=0.5.4',
+        'gsconfig>=1.0.0',
+        'psycopg2',
+        'OWSLib>=0.7.2',
+        'unittest2',
+    ],
+    packages=find_packages(),
+    include_package_data = True,
+    zip_safe = False,
+    classifiers  = [],
+    test_suite = 'test.uploadtests'
+)

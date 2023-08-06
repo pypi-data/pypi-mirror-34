9c84d92edf9c52f35b4bdddf39236ff46c4aeef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:20 +0000

    Add package isgithubdown/1.0.1/isgithubdown-1.0.1.tar.gz

---
+++ packages/isgithubdown/isgithubdown-1.0.1.tar.gz/isgithubdown-1.0.1/isgithubdown/isgithubdown.py
@@ -0,0 +1,13 @@
+import requests
+
+def main():
+	endpoint = 'https://isitup.org/github.com.json'
+	response = requests.get(endpoint, verify = True)
+
+	data = response.json()
+
+	if data['status_code'] == 1: # Up
+		print('\n 🐈  It\'s up. Go back to work!')
+
+	if data['status_code'] == 2: # Down
+		print('\n 🦄  It\'s down. Go outside!')

658de262bf38ca5b715cebfdfd932d5a02f8ecfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:01 +0000

    Add package fastscore/1.8.0/fastscore-1.8.0-py2-none-any.whl

---
+++ packages/fastscore/fastscore-1.8.0-py2-none-any.whl/fastscore/__init__.py
@@ -0,0 +1,12 @@
+from .model import Model
+from .schema import Schema
+from .stream import Stream
+from .sensor import Sensor
+
+from .pneumo import PneumoSock
+
+from .errors import FastScoreError
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions

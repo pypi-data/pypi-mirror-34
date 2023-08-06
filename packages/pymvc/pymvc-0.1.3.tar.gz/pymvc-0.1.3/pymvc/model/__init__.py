aadc70ef7cd953d5dc81a176e3c643c73fd9178a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:21 +0000

    Add package pymvc/0.1.3/pymvc-0.1.3.tar.gz

---
+++ packages/pymvc/pymvc-0.1.3.tar.gz/pymvc-0.1.3/pymvc/model/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from .model_base import Model
+from .datatypes import StringType, IntType, FloatType, UniqueIdType
+from .datatypes import ForeignType, EnumType, ListType, BoolType, DatetimeType, HashType
+
+from . import binder
---
+++ packages/pymvc/pymvc-0.1.3.tar.gz/pymvc-0.1.3/pymvc/model/binder.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from .. import wsgi
+from .model_base import get_loaded_model
+
+
+@wsgi.app.context_processor
+def model_loader():
+    """
+    Flask (Jinja2) function for loading model data
+    :return: Flask (Jinja2) functions
+    """
+    def load_one(model, primary=None, **query):
+        """
+        load one model's data
+        :param model: model class name or snake case class name
+        :param primary: primary key (optional)
+        :param query: query (optional)
+        :return: data or None
+        """
+        model = get_loaded_model(model)
+        model = model.load(primary=primary, **query)
+        return model
+
+    def load_many(model, primary=None, **query):
+        """
+        load model's data
+        :param model: model class name or snake case class name
+        :param primary: primary key (optional)
+        :param query: query (optional)
+        :return: data list
+        """
+        model = get_loaded_model(model)
+        model = model.load(multi=True, primary=primary, **query)
+        return model
+
+    return {"load_one": load_one, "load_many": load_many}

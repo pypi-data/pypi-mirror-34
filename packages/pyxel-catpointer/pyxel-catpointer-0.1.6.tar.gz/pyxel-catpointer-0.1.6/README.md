e11b13d22e9a434e84ce9f2984f004a4d24a2916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:10 +0000

    Add package pyxel-catpointer/0.1.6/pyxel-catpointer-0.1.6.tar.gz

---
+++ packages/pyxel-catpointer/pyxel-catpointer-0.1.6.tar.gz/pyxel-catpointer-0.1.6/README.md
@@ -0,0 +1,13 @@
+# Cat Pointer
+
+Silly game powered by [pyxel](https://github.com/kitao/pyxel).
+
+![](screenshots/scr-1.png)
+![](screenshots/scr-2.png)
+
+## Installation
+
+- Make sure you have the [requirements of pyxel](https://github.com/kitao/pyxel#windows)
+- `pip install pyxel-catpointer`
+
+Run `catpointer`
---
+++ packages/pyxel-catpointer/pyxel-catpointer-0.1.6.tar.gz/pyxel-catpointer-0.1.6/setup.py
@@ -0,0 +1,37 @@
+from os import path
+
+from setuptools import find_packages, setup
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pyxel-catpointer',
+    description='Silly game powered by pyxel',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version='0.1.6',
+    author='Santos Gallegos',
+    author_email='santos_g@outlook.com',
+    license='MIT',
+    url='https://github.com/stsewd/cat-pointer-game',
+    packages=find_packages(),
+    package_data={
+        '': ['assets/*'],
+    },
+    install_requires=['pyxel<1'],
+    entry_points={
+        'console_scripts': [
+            'catpointer = catpointer.game:App',
+        ],
+    },
+    python_requires='>=3.7',
+    classifiers=[
+        'Topic :: Games/Entertainment :: Arcade',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+    ],
+)

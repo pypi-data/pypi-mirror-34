403b024113b8fff1699b931e29865421ee23121d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:32 +0000

    Add package takumi/0.1.6/takumi-0.1.6.tar.gz

---
+++ packages/takumi/takumi-0.1.6.tar.gz/takumi-0.1.6/setup.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='takumi',
+    version='0.1.6',
+    description='Takumi service framework',
+    long_description=open("README.rst").read(),
+    author="Eleme Lab",
+    author_email="imaralla@icloud.com",
+    packages=find_packages(),
+    package_data={'': ['LICENSE', 'README.rst']},
+    url='https://github.com/elemepi/takumi',
+    install_requires=[
+        'takumi-config',
+        'takumi-thrift',
+        'thriftpy>=0.3.9',
+        'gevent>=1.2.1',
+    ],
+)

d82786413d15ec9d0773d176af98f18f56280a35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:22 +0000

    Add package better-utils/0.3.3/better_utils-0.3.3.tar.gz

---
+++ packages/better-utils/better_utils-0.3.3.tar.gz/better_utils-0.3.3/better_utils/args_util.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import argparse
+
+
+class ArgsUtil:
+    def __init__(self, description='parser desc'):
+        self.parser = argparse.ArgumentParser(description=description)
+
+    def setArg(self, name, type, required=True, help="say some help"):
+        if type == bool:
+            type = ArgsUtils.bool_handle
+        self.parser.add_argument(name, type=type, required=required, help=help)
+        return self
+
+    def get(self):
+        return self.parser.parse_args()
+
+    @staticmethod
+    def bool_handle(v):
+        if v.lower() in ('yes', 'true', 't', 'y', '1'):
+            return True
+        elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+            return False
+        else:
+            raise argparse.ArgumentTypeError('Boolean value expected.')

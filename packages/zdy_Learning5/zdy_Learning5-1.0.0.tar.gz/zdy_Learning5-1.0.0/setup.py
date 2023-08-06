2db2454456e84368da9c243e36428e95128df469
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:02 +0000

    Add package zdy_Learning5/1.0.0/zdy_Learning5-1.0.0.tar.gz

---
+++ packages/zdy_Learning5/zdy_Learning5-1.0.0.tar.gz/zdy_Learning5-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='zdy_Learning5',
+    version='1.0.0',
+    py_modules=['chapter5'],
+    author='zdy826',
+    author_email='747342197@qq.com',
+    description='R&W File which includes various type data',
+)

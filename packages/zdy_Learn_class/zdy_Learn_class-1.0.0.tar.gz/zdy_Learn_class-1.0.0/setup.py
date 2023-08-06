774cce43fd503fd4e726824530f793d900233551
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:03 +0000

    Add package zdy_Learn_class/1.0.0/zdy_Learn_class-1.0.0.tar.gz

---
+++ packages/zdy_Learn_class/zdy_Learn_class-1.0.0.tar.gz/zdy_Learn_class-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='zdy_Learn_class',
+    version='1.0.0',
+    py_modules=['chapter6'],
+    author='zdy826',
+    author_email='747342197@qq.com',
+    description='R&W File which use and inherit class',
+)

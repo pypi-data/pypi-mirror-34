8a012a532ca2af43debd4fd53d0969492e738c22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:04 +0000

    Add package simplecel/0.2.5/simplecel-0.2.5-py3-none-any.whl

---
+++ packages/simplecel/simplecel-0.2.5-py3-none-any.whl/simplecel/static/default.js
@@ -0,0 +1,14 @@
+let defaultConfig = {
+  rowHeaders: true,
+  colHeaders: true,
+  manualRowResize: true,
+  manualColumnResize: true,
+  // fixedRowsTop: 1,
+  filters: true,
+  dropdownMenu: true,
+  contextMenu: true,
+  maxColWidth: 200,
+  hasHeader: false,
+  // renderers: 'markdownRenderer',
+  allowInsertCol: true
+};

ebdeabd194eb9f1f37742b5972675b372561be34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:09 +0000

    Add package howwasde/1.1.2/howwasde-1.1.2.tar.gz

---
+++ packages/howwasde/howwasde-1.1.2.tar.gz/howwasde-1.1.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+      name = 'howwasde',
+      version= '1.1.2',
+      py_modules = ['nester'],
+      author = 'sdfs',
+      author_email = '362606310@qq.com',
+      url = 'http://headfirstlabs.com',
+      description = 'file'
+     )
+

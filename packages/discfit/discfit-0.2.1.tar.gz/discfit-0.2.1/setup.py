ceb4ba687e1d76a46fbec35214ce9586874f565a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:33 +0000

    Add package discfit/0.2.1/discfit-0.2.1.tar.gz

---
+++ packages/discfit/discfit-0.2.1.tar.gz/discfit-0.2.1/setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='discfit',
+    version='0.2.1',
+    description='Disc permeameter fitting program',
+    long_description=long_description,
+    url='https://github.com/sekika/discfit',
+    author='Katsutoshi Seki',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Environment :: Console',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Natural Language :: English',
+    ],
+    keywords='Disc permeameter',
+    packages=['discfit'],
+    install_requires=['numpy', 'scipy'],
+    entry_points={  
+        'console_scripts':  
+            'discfit = discfit.main:main'  
+    },
+)

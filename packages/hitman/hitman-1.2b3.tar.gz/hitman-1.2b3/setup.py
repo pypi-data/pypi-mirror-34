aa1e94360e0ba56a644d242651f9718934777104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:29 +0000

    Add package hitman/1.2b3/hitman-1.2b3.tar.gz

---
+++ packages/hitman/hitman-1.2b3.tar.gz/hitman-1.2b3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+setup(
+    name='hitman',
+    version='1.2b3',
+    author='Jack Laxson',
+    author_email='jackjrabbit+hitman@gmail.com',
+    url='https://github.com/jrabbit/hitman',
+    license='GPL v3',
+    long_description="Hitman: The professional RSS enclosure (Podcast) downloader. Supports OPML. Supports various downloads methods: requests and urlgrabber (pycurl based).",
+    description="The professional RSS enclosure (Podcast) downloader.",
+    scripts=['hitman.py'],
+    install_requires=['semidbm', 'requests', 'clint',
+                      'feedparser>=5.1.3', 'beautifulsoup4>=4.3.2', 'click>=6'],
+    extras_require={'platform_system=="Windows"':["win10toast"]},
+    entry_points='''
+        [console_scripts]
+        hitman=hitman:cli_base
+    ''',
+    classifiers=['Environment :: Console',
+                 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+                 'Topic :: Internet :: WWW/HTTP', 'Operating System :: BeOS',
+                 'Topic :: Multimedia', 'Operating System :: OS Independent',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.6' ]
+)

aa0ab155d80b73fc08afd3fcf45b85fe07910157
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:38 +0000

    Add package junos-eznc/2.1.9/junos_eznc-2.1.9-py2.py3-none-any.whl

---
+++ packages/junos-eznc/junos_eznc-2.1.9-py2.py3-none-any.whl/jnpr/junos/version.py
@@ -0,0 +1,10 @@
+VERSION = "2.1.9"
+DATE = "2018-Aug-8"
+
+# Augment with the internal version if present
+try:
+    from jnpr.junos.internal_version import INTERNAL_VERSION
+    VERSION += '+internal.' + str(INTERNAL_VERSION)
+except ImportError:
+    # No internal version
+    pass

78020675c96fbab9104b51092817173febda1e22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:22 +0000

    Add package rbnf/0.2.9/rbnf-0.2.9-py3-none-any.whl

---
+++ packages/rbnf/rbnf-0.2.9-py3-none-any.whl/rbnf/__release_info__.py
@@ -0,0 +1,2 @@
+__VERSION__ = '0.2.9'
+__AUTHOR__ = "thautwarm"
---
+++ packages/rbnf/rbnf-0.2.9-py3-none-any.whl/rbnf/zero/impl.pyi
@@ -0,0 +1,35 @@
+from .user_interface import *
+from rbnf.core.ParserC import Atom, Parser
+from rbnf.core.Tokenizer import Tokenizer
+from typing import Callable, Iterator, Dict
+from Redy.Tools.PathLib import Path
+from rbnf.edsl.core import Language
+import typing
+import io
+
+__all__ = ['compile', 'ResultDescription']
+
+
+class ZeroExp:
+    _top_parser: Parser
+    _lexer: Callable[[str], Iterator[Tokenizer]]
+    _lang: Language
+    ctx: dict
+
+    def __init__(self, bnf_syntax: str, use: str, custom_lexer_wrapper=None, language_name: str = None,
+                 filename='<zero>'): ...
+
+    def match(self, text) -> ResultDescription: ...
+
+    def dumps(self) -> str:  ...
+
+    def dump(self, file_repr: typing.Union[str, io.BufferedWriter]):
+        if isinstance(file_repr, str):
+            file_repr = Path(file_repr).open('w')
+
+        with file_repr:
+            file_repr.write(self.dumps())
+
+
+def compile(bnf_syntax: str, use: str = None, custom_lexer_wrapper=None, language_name: str = None,
+            filename=None) -> ZeroExp: ...

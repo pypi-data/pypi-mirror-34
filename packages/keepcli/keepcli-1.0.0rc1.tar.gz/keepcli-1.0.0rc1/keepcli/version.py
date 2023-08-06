29e4ae097ba37378138c4964b77f8879512d6744
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:11 +0000

    Add package keepcli/1.0.0rc1/keepcli-1.0.0rc1.tar.gz

---
+++ packages/keepcli/keepcli-1.0.0rc1.tar.gz/keepcli-1.0.0rc1/keepcli/version.py
@@ -0,0 +1,9 @@
+"""keepcli version"""
+
+commit = '49a3560'
+
+version_tag = (1, 0, 0, 'rc1'.format(commit))
+__version__ = '.'.join(map(str, version_tag[:3]))
+
+if len(version_tag) > 3:
+    __version__ = '%s-%s' % (__version__, version_tag[3])

d0f8816d8f7be35589e43c46f4ad89ff1050f5d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:28 +0000

    Add package pycloudlib/18.5.1/pycloudlib-18.5.1.tar.gz

---
+++ packages/pycloudlib/pycloudlib-18.5.1.tar.gz/pycloudlib-18.5.1/pycloudlib/result.py
@@ -0,0 +1,33 @@
+# This file is part of pycloudlib. See LICENSE file for license information.
+"""Base Result Class."""
+
+from collections import UserString
+
+
+class Result(UserString):  # pylint: disable=too-many-ancestors
+    """Result Class."""
+
+    def __init__(self, stdout, stderr, return_code):
+        """Initialize class."""
+        super().__init__(stdout)
+
+        self.stderr = stderr
+        self.return_code = return_code
+
+    def __repr__(self):
+        """Return stdout."""
+        return self.data
+
+    @property
+    def failed(self):
+        """Return boolean if result was failure."""
+        if self.return_code == 0:
+            return False
+        return True
+
+    @property
+    def succeeded(self):
+        """Return boolean if result was failure."""
+        if self.return_code == 0:
+            return True
+        return False

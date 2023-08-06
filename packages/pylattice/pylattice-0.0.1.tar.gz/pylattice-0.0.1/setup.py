181926cf916b35c7dd53dfdbe7aba48cac076c69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:49 +0000

    Add package pylattice/0.0.1/pylattice-0.0.1.tar.gz

---
+++ packages/pylattice/pylattice-0.0.1.tar.gz/pylattice-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pylattice",
+    version="0.0.1",
+    author="Joh Schoeneberg",
+    author_email="joh@schoeneberglab.org",
+    description="A python library for lattice light-sheet image analysis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JohSchoeneberg/pyLattice",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ),
+)

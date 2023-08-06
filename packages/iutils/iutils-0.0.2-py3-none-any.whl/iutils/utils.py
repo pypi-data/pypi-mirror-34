f8fccf4aa91c1f32538a5ea0b7a6db4e3c7ce799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:15 +0000

    Add package iutils/0.0.2/iutils-0.0.2-py3-none-any.whl

---
+++ packages/iutils/iutils-0.0.2-py3-none-any.whl/iutils/utils.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+
+def two_level_split(line, sep=' ', quote='"'):
+    '''Split a line by sep.
+
+    The line may optionally contains fields that are quoted by the quote sign.
+    '''
+
+    in_quotes = False
+    results = []
+    temp = []
+
+    for field in line.split(sep):
+        if not field:
+            results.append(field)
+            continue
+
+        if in_quotes:
+            if field[0] == quote:
+                raise ValueError(f"Non-matching `{quote}' quote: {line}")
+            else:
+                if field[-1] == quote:
+                    temp.append(field.strip(quote))
+                    results.append(sep.join(temp))
+                    temp = []
+                    in_quotes = False
+                else:
+                    temp.append(field)
+        else:
+            if field[0] == quote:
+                if field[-1] == quote:
+                    results.append(field.strip(quote))
+                else:
+                    in_quotes = True
+                    temp.append(field.strip(quote))
+            else:
+                if field[-1] == quote:
+                    raise ValueError(f"Non-matching `{quote}' quote: {line}")
+                else:
+                    results.append(field)
+
+    return results

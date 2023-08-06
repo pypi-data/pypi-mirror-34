4922d919b517195a152a3d8eee4da04b81b69b54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:52 +0000

    Add package cmfrec/0.5.2.1/cmfrec-0.5.2.1.tar.gz

---
+++ packages/cmfrec/cmfrec-0.5.2.1.tar.gz/cmfrec-0.5.2.1/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+setup(
+  name = 'cmfrec',
+  packages = ['cmfrec'],
+  install_requires=[
+   'pandas>=0.21',
+   'numpy',
+   'scipy',
+   'tensorflow>=1.0.0'
+],
+  version = '0.5.2.1',
+  description = 'Collaborative filtering with user and item side information based on collective matrix factorization',
+  author = 'David Cortes',
+  author_email = 'david.cortes.rivera@gmail.com',
+  url = 'https://github.com/david-cortes/cmfrec',
+  download_url = 'https://github.com/david-cortes/cmfrec/archive/0.5.2.1.tar.gz',
+  keywords = ['collaborative filtering', 'collective matrix factorization', 'relational learning'],
+  classifiers = [],
+)

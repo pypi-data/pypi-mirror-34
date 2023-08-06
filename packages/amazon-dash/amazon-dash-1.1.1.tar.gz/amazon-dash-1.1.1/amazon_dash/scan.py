9b15f88ad5c9981f924689a97e0fb67d0608782f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:24 +0000

    Add package amazon-dash/1.1.1/amazon-dash-1.1.1.tar.gz

---
+++ packages/amazon-dash/amazon-dash-1.1.1.tar.gz/amazon-dash-1.1.1/amazon_dash/scan.py
@@ -0,0 +1,26 @@
+import logging
+
+from amazon_dash.exceptions import SocketPermissionError
+
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+from scapy.all import *
+
+
+try:
+    PermissionError
+except NameError:
+    import socket
+    PermissionError = socket.error
+
+
+def scan_devices(fn, lfilter, iface=None):
+    """Sniff packages
+
+    :param fn: callback on packet
+    :param lfilter: filter packages
+    :return: loop
+    """
+    try:
+        sniff(prn=fn, store=0, filter="udp", lfilter=lfilter, iface=iface)
+    except PermissionError:
+        raise SocketPermissionError

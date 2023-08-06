817c33aef6c8ffc61711262d2c2d25bf938f31ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:03 +0000

    Add package auquan-toolbox-beta/1.1.4/auquan_toolbox_beta-1.1.4.tar.gz

---
+++ packages/auquan-toolbox-beta/auquan_toolbox_beta-1.1.4.tar.gz/auquan_toolbox_beta-1.1.4/backtester/features/argmin_feature.py
@@ -0,0 +1,39 @@
+from backtester.features.feature import Feature
+import pandas as pd
+import numpy as np
+
+
+class ArgMinFeature(Feature):
+
+    '''
+    Computing for Instrument.
+    '''
+    @classmethod
+    def computeForInstrument(cls, updateNum, time, featureParams, featureKey, instrumentManager):
+        instrumentLookbackData = instrumentManager.getLookbackInstrumentFeatures()
+        featureDataDf = instrumentLookbackData.getFeatureDf(featureParams['featureName'])
+        if len(featureDataDf) < 1:
+            instrumentDict = instrumentManager.getAllInstrumentsByInstrumentId()
+            zeroSeries = pd.Series([0] * len(instrumentDict), index=instrumentDict.keys())
+            return zeroSeries
+        return featureDataDf[-featureParams['period']:].idxmin().astype(object)
+
+    '''
+    Computing for Market.
+    '''
+    @classmethod
+    def computeForMarket(cls, updateNum, time, featureParams, featureKey, currentMarketFeatures, instrumentManager):
+        lookbackDataDf = instrumentManager.getDataDf()
+        data = lookbackDataDf[featureParams['featureName']]
+        if len(data) < 1:
+            return 0
+        return data[-featureParams['period']:].idxmin()
+
+    @classmethod
+    def computeForInstrumentData(cls, updateNum, featureParams, featureKey, featureManager):
+        data = featureManager.getFeatureDf(featureParams['featureName'])
+        if data is None:
+            logWarn("[%d] instrument data for \"%s\" is not available, can't calculate \"%s\"" % (updateNum, featureParams['featureName'], featureKey))
+            return None
+        argMin = data.rolling(window=featureParams['period'], min_periods=1).apply(np.argmin)
+        return argMin

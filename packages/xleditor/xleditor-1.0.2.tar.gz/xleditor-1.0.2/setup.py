4084ff0392401985e8d226eb086ce68946b553ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:46 +0000

    Add package xleditor/1.0.2/xleditor-1.0.2.tar.gz

---
+++ packages/xleditor/xleditor-1.0.2.tar.gz/xleditor-1.0.2/setup.py
@@ -0,0 +1,38 @@
+
+from setuptools import setup
+
+try:
+    from xleditor import VERSION
+except Exception as e:
+    VERSION = ''
+
+
+setup(
+    name='xleditor',
+    version=VERSION,
+    description='Easy to open xls/xlsx file and edit it (merge xlrd and xlutils)',
+    long_description=open('README.md').read(),
+    author='TaoJY',
+    author_email='taojy123@163.com',
+    maintainer='TaoJY',
+    maintainer_email='taojy123@163.com',
+    license='MIT License',
+    install_requires=open('requirements.txt').read().strip().splitlines(),
+    py_modules=['xleditor'],
+    include_package_data=True,
+    platforms=["all"],
+    url='https://github.com/taojy123/xleditor',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries'
+    ],
+)
+
+

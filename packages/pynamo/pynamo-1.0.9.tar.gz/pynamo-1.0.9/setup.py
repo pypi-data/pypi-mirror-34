343a5aa7bfba086e14885382394d2de261632bb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:58 +0000

    Add package pynamo/1.0.9/pynamo-1.0.9.tar.gz

---
+++ packages/pynamo/pynamo-1.0.9.tar.gz/pynamo-1.0.9/setup.py
@@ -0,0 +1,15 @@
+
+from distutils.core import setup
+setup(
+  name = 'pynamo',
+  packages = ['pynamo'],
+  version = '1.0.9',
+  description = 'Python Library for Handling Dynamo Syntax and requests.',
+  author = 'Barry Howard',
+  author_email = 'barry.howard@ge.com',
+  keywords = ["dynamo", "ge", "cloudops", "aws"],
+  classifiers = [],
+  install_requires=[
+    "boto3"
+  ],
+)

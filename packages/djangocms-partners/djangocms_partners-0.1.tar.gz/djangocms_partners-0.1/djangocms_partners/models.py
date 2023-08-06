4f5022093631b3f207a893b80601435d2ccbc756
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:51 +0000

    Add package djangocms-partners/0.1/djangocms_partners-0.1.tar.gz

---
+++ packages/djangocms-partners/djangocms_partners-0.1.tar.gz/djangocms_partners-0.1/djangocms_partners/models.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+from django.utils.translation import ugettext as _
+from autoslug import AutoSlugField
+from filer.fields.image import FilerImageField
+from djangocms_text_ckeditor.fields import HTMLField
+
+class Partner(models.Model):
+    #Partners"
+    name = models.CharField(_('Name'), max_length=200)# (requis)
+    logo = FilerImageField(related_name=_("parnter_logo"), null=True,blank=True, verbose_name=_("logo"))
+
+    class Meta:
+        verbose_name = _("Partner")
+        verbose_name_plural = _("Partners")
+
+    def __unicode__(self):
+        return self.__str__(self)
+
+    def __str__(self):
+        return self.name

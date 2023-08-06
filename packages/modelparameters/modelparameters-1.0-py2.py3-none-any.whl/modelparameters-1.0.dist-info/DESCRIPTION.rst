bd4f11ddc1d72cd3eeee0c472be954e024398c17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:29 +0000

    Add package modelparameters/1.0/modelparameters-1.0-py2.py3-none-any.whl

---
+++ packages/modelparameters/modelparameters-1.0-py2.py3-none-any.whl/modelparameters-1.0.dist-info/DESCRIPTION.rst
@@ -0,0 +1,8 @@
+A module providing parameter structure for physical modeling
+      
+Home-page: UNKNOWN
+Author: Johan Hake 
+Author-email: hake.dev@gmail.com
+License: UNKNOWN
+Description: UNKNOWN
+Platform: UNKNOWN
---
+++ packages/modelparameters/modelparameters-1.0-py2.py3-none-any.whl/modelparameters/__init__.py
@@ -0,0 +1,16 @@
+from . import codegeneration
+from . import commands
+from . import config
+from . import logger
+from . import sympytools
+
+from . import parameters
+from .parameters import ScalarParam
+
+from . import parameterdict
+from .parameterdict import ParameterDict
+
+from . import units
+from .units import Unit
+
+from . import utils

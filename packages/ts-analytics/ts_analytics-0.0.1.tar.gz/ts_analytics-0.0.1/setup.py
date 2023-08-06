42c09ca80644c53a31a5276c53d757c7e4cf5d0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:11 +0000

    Add package ts-analytics/0.0.1/ts_analytics-0.0.1.tar.gz

---
+++ packages/ts-analytics/ts_analytics-0.0.1.tar.gz/ts_analytics-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "ts_analytics",
+    version = "0.0.1",
+    author="Eneliko Mulokozi",
+    author_email = "emulokozi@aol.com",
+    description = "Traffic data analytics",
+    url = "https://github.com/pypa/ts_project",
+    packages = find_packages(),
+    classifiers = (
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)

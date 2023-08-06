be8168b354560363f10b147078652a2324943142
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:02 +0000

    Add package csr_autoscaler_cli/0.0.1/csr_autoscaler_cli-0.0.1.tar.gz

---
+++ packages/csr_autoscaler_cli/csr_autoscaler_cli-0.0.1.tar.gz/csr_autoscaler_cli-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+project_name = 'csr_autoscaler_cli'
+project_ver = '0.0.1'
+setup(
+    name=project_name,
+    version=project_ver,
+    description='A CLI helper for CSR1000v autoscaler on AWS',
+    author='Christopher Reder',
+    author_email='creder@cisco.com',
+    scripts=["bin/csr_autoscaler"],
+    # use the URL to the github repo
+    url='https://github4-chn.cisco.com/csr1000v-cloud/autoscaler-cli.git',
+    download_url='https://github4-chn.cisco.com/csr1000v-cloud/autoscaler-cli.git',
+    keywords=['cisco', 'aws', 'autoscaler'],
+    classifiers=[],
+    license="MIT",
+    install_requires=[
+        'docopt'
+    ],
+)

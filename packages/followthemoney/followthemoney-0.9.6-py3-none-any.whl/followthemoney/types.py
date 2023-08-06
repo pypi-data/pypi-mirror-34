ea96918e355a7d89228fc0562edc07aa8447e744
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:21 +0000

    Add package followthemoney/0.9.6/followthemoney-0.9.6-py3-none-any.whl

---
+++ packages/followthemoney/followthemoney-0.9.6-py3-none-any.whl/followthemoney/types.py
@@ -0,0 +1,20 @@
+import exactitude
+from collections import namedtuple
+
+PropType = namedtuple('PropType', ['type', 'invert'])
+
+TYPES = {
+    'string': PropType(exactitude.texts, None),
+    'name': PropType(exactitude.names, 'names'),
+    'entity': PropType(exactitude.texts, 'entities'),
+    'url': PropType(exactitude.urls, 'urls'),
+    'uri': PropType(exactitude.urls, 'urls'),
+    'date': PropType(exactitude.dates, 'dates'),
+    'address': PropType(exactitude.addresses, 'addresses'),
+    'country': PropType(exactitude.countries, 'countries'),
+    'email': PropType(exactitude.emails, 'emails'),
+    'phone': PropType(exactitude.phones, 'phones'),
+    'identifier': PropType(exactitude.identifiers, 'identifiers'),
+    'iban': PropType(exactitude.ibans, 'ibans'),
+    'ip': PropType(exactitude.ips, 'ips'),
+}

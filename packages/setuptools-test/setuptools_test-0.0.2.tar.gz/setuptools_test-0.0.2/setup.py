2952e92f5e8b28287b35b659240936ff812840df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:51 +0000

    Add package setuptools-test/0.0.2/setuptools_test-0.0.2.tar.gz

---
+++ packages/setuptools-test/setuptools_test-0.0.2.tar.gz/setuptools_test-0.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+from os import path
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='setuptools_test',
+    version='0.0.2',
+    description='Testing packaging with Python 3',
+    author='Sheel Dandekar',
+    author_email='sheel_dandekar@intuit.com',
+    url="https://github.com/pypa/sampleproject",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+)
---
+++ packages/setuptools-test/setuptools_test-0.0.2.tar.gz/setuptools_test-0.0.2/setuptools_test/__init__.py
@@ -0,0 +1,1 @@
+name = "setuptools_test"

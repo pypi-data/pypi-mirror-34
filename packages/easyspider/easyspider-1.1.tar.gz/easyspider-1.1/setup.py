a0eb501c6fe59e019e160c9fe3080e12d700634c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:18:39 +0000

    Add package easyspider/1.1/easyspider-1.1.tar.gz

---
+++ packages/easyspider/easyspider-1.1.tar.gz/easyspider-1.1/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# @Author: hang.zhang
+# @Email:  hhczy1003@163.com
+# @Date:   2017-08-01 20:37:27
+# @Last Modified by:   hang.zhang
+# @Last Modified time: 2018-07-24 16:56:09
+
+from setuptools import setup
+
+setup(
+    name="easyspider",
+    version="1.1",
+    author="yiTian.zhang",
+    author_email="hhczy1003@163.com",
+    packages=["DBService"],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "scrapy",
+        "scrapy-redis",
+        "pymongo",
+        "apscheduler",
+        "selenium",
+    ],
+)

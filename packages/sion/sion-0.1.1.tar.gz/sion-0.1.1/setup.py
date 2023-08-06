d90c021732059dd3bca49a3dd17ef18e2ed01eb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:48 +0000

    Add package sion/0.1.1/sion-0.1.1.tar.gz

---
+++ packages/sion/sion-0.1.1.tar.gz/sion-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# Created by kamimura on 2018/07/21.
+# Copyright © 2018 kamimura. All rights reserved.
+
+import setuptools
+
+with open('README.md') as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name='sion',
+    version='0.1.1',
+    author='kamimura',
+    author_email='kamimura@live.jp',
+    license='MIT',
+    description='loading and dumping a SION format file',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/kamimura/py-sion',
+    py_modules=['sion', 'SIONParser',
+                'SIONLexer', 'SIONVisitor'],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Environment :: Console"
+    )
+)

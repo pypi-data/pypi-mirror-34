610e16558efd824e38539f9e8f715ce9443a6ebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:57 +0000

    Add package oheye/1.0.3/oheye-1.0.3.tar.gz

---
+++ packages/oheye/oheye-1.0.3.tar.gz/oheye-1.0.3/oheye/analog_in.py
@@ -0,0 +1,17 @@
+import spidev
+
+spi=spidev.SpiDev()
+spi.open(0, 0)
+spi2=spidev.SpiDev()
+spi2.open(0, 1)
+
+def read(channel):
+    if channel >15 or channel<0:
+        return -1
+    if channel < 8:
+        r=spi.xfer2([1, 8+channel<<4, 0])
+    else:
+        r=spi2.xfer2([1, 8+(channel-8)<<4, 0])
+    v=((r[1]&3)<<8)+r[2]
+
+    return v

ce376c85ffb1ef7fa0030a1094d4953b193da4ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:18 +0000

    Add package openstack-opentracing/0.1.17/openstack_opentracing-0.1.17.tar.gz

---
+++ packages/openstack-opentracing/openstack_opentracing-0.1.17.tar.gz/openstack_opentracing-0.1.17/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2
+
+from setuptools import setup
+import os
+setup(
+    name = 'openstack_opentracing',
+    version = '0.1.%s' % (os.getenv('TRAVIS_BUILD_NUMBER', '0')),
+    packages = ['openstack_opentracing', 'openstack_opentracing/test'],
+    install_requires = [
+        'jaeger_client==3.10.0',
+        'opentracing_instrumentation==2.4.1',
+        'opentracing==1.3.0',
+        'oslo.middleware>=3.0.0',
+        'oslo.config>=3.14.0',
+        'oslo.service>=1.10.0',
+        'flask>=0.10.0',
+        'futures',
+        'requests>=2.10.0'
+        ],
+    scripts = [],
+    package_data = {}
+)

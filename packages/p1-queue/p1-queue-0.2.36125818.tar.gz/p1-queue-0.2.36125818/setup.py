4a22172b0052585ab922c68221a4c5da3b7eb2d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:58 +0000

    Add package p1-queue/0.2.36125818/p1-queue-0.2.36125818.tar.gz

---
+++ packages/p1-queue/p1-queue-0.2.36125818.tar.gz/p1-queue-0.2.36125818/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(name='p1-queue',
+      version='0.2.36125818',
+      description='Messaging abstraction for AMQP',
+      author='Turfa Auliarachman',
+      author_email='turfa_auliarachman@rocketmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=['pika'],
+      zip_safe=False,
+      include_package_data=True,
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3'
+      ])

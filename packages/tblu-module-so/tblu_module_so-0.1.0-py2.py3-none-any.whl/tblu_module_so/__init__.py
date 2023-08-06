b0e2ce307dbdcb69102c14481e7d89519a8700ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:31 +0000

    Add package tblu-module-so/0.1.0/tblu_module_so-0.1.0-py2.py3-none-any.whl

---
+++ packages/tblu-module-so/tblu_module_so-0.1.0-py2.py3-none-any.whl/tblu_module_so/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for TBlu Module SO."""
+
+__author__ = """TBlu-Company"""
+__email__ = 'dev@tblu.com.br'
+__version__ = '0.1.0'
+
+from .tblu_module_so import TBLuModule

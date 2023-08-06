bc6be6087e9ba0b821259f935fcd0ac59a303e95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:38 +0000

    Add package traitlite/0.0.1/traitlite-0.0.1.tar.gz

---
+++ packages/traitlite/traitlite-0.0.1.tar.gz/traitlite-0.0.1/setup.py
@@ -0,0 +1,26 @@
+import fnmatch
+from fnmatch import fnmatchcase
+
+def foo(*args, **kwargs):
+    bar = fnmatchcase(*args, **kwargs)
+    print(args, kwargs, bar)
+    return bar
+fnmatch.fnmatchcase = foo
+
+from setuptools import setup, find_packages
+
+setup(
+    name='traitlite',
+    version='0.0.1',
+    author='Christopher Pezley',
+    author_email='pypi@pezley.net',
+    description='A light-weight module that aims to provide control ' \
+                'for class attributes.',
+    url='https://github.com/lejar/traitlite',
+    packages=find_packages(exclude=['tests']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

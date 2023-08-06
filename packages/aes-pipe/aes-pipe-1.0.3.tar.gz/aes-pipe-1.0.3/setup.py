118254d560acbb73a561b9cd1c5ca60878be75ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:45 +0000

    Add package aes-pipe/1.0.3/aes-pipe-1.0.3.tar.gz

---
+++ packages/aes-pipe/aes-pipe-1.0.3.tar.gz/aes-pipe-1.0.3/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+with open("README.md", "r") as f:
+	long_description = f.read()
+
+setuptools.setup(
+	name="aes-pipe",
+	version="1.0.3",
+	
+	author="Seán Hewitt",
+	author_email="contact@2sh.me",
+	
+	description="Encrypting piped data with AES",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	
+	url="https://github.com/2sh/aes-pipe",
+	
+	packages=["aes_pipe"],
+	
+	install_requires=["pycrypto"],
+	python_requires='>=3.4',
+	classifiers=(
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+		"Operating System :: OS Independent",
+		"Topic :: Security :: Cryptography"
+	),
+	
+	entry_points={"console_scripts":["aes-pipe=aes_pipe:_main"]}
+)

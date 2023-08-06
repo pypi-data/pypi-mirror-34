e1e0831c86613e69ca4115e5a8c840cae5ece80f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:29 +0000

    Add package cnt/0.2.2/cnt-0.2.2-py2.py3-none-any.whl

---
+++ packages/cnt/cnt-0.2.2-py2.py3-none-any.whl/cnt/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for cnt."""
+
+__author__ = """Hunt Zhan"""
+__email__ = 'huntzhan.dev@gmail.com'
+__version__ = '0.2.2'
+
+
+import cnt_ruleseg as ruleseg  # noqa: F401

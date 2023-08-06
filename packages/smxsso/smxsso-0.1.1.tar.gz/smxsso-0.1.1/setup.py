9337cee4547bd2e50ca5b4c45c9ae959db901e1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:01 +0000

    Add package smxsso/0.1.1/smxsso-0.1.1.tar.gz

---
+++ packages/smxsso/smxsso-0.1.1.tar.gz/smxsso-0.1.1/setup.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+import codecs
+import os.path
+import re
+import sys
+
+from setuptools import setup, find_packages
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    return codecs.open(os.path.join(here, *parts), 'r').read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+requires = ['boto', 'boto3',
+            'bs4',
+            'lxml',
+            'requests',
+            'awscli']
+
+
+if sys.version_info[:2] == (2, 6):
+    # For python2.6 we have to require argparse since it
+    # was not in stdlib until 2.7.
+    requires.append('argparse>=1.1')
+
+
+setup_options = dict(
+    name='smxsso',
+    version=find_version("smxsso", "__init__.py"),
+    description='Command Line Environment for Smartronix SSO solution.',
+    long_description=open('README.rst').read(),
+    author='Anthony Vultaggio',
+    url='https://github.com/changli3/smx-sso',
+    scripts=['bin/smxsso', 'bin/smxsso.cmd'],
+    install_requires=requires,
+    extras_require={
+        ':python_version=="2.6"': [
+            'argparse>=1.1',
+        ]
+    },
+    packages=['smxsso'],
+    license="Apache License 2.0",
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ),
+)
+
+setup(**setup_options)

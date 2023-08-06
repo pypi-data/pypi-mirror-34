b89e26bfaca0a45bdb01bbb5ed729e8a901f6d63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:29 +0000

    Add package SimpleSteem/1.1.2/SimpleSteem-1.1.2-3.0-none-any.whl

---
+++ packages/SimpleSteem/SimpleSteem-1.1.2-3.0-none-any.whl/simplesteem/config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+logpath = ""
+keys = []
+screenmode = "quiet"
+mainaccount = "ned"
+client_secret = ""
+callback_url = ""
+client_id = ""
+permissions = "login,offline,vote"
+nodes = ["https://steemd.minnowsupportproject.org","https://steemd.privex.io","https://gtg.steem.house:8090","https://steemd.pevo.science","https://rpc.steemliberator.com"]

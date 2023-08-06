f056ed3ad2e47739738751be148f823fe0a8a3d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:59 +0000

    Add package pytest-flask-sqlalchemy-transactions/1.0.2/pytest-flask-sqlalchemy-transactions-1.0.2.tar.gz

---
+++ packages/pytest-flask-sqlalchemy-transactions/pytest-flask-sqlalchemy-transactions-1.0.2.tar.gz/pytest-flask-sqlalchemy-transactions-1.0.2/README.md
@@ -0,0 +1,9 @@
+# This package has moved!
+
+This plugin has been renamed to
+[pytest-flask-sqlalchemy](https://pypi.org/project/pytest-flask-sqlalchemy/).
+Please install that package instead, as no future updates will be made to this
+version of the package.
+
+This page will remain up until September 1st, 2018, at which point the package will be
+officially deprecated in favor of pytest-flask-sqlalchemy.
---
+++ packages/pytest-flask-sqlalchemy-transactions/pytest-flask-sqlalchemy-transactions-1.0.2.tar.gz/pytest-flask-sqlalchemy-transactions-1.0.2/setup.py
@@ -0,0 +1,48 @@
+'''
+pytest-flask-sqlalchemy-transactions
+====================================
+
+Run tests in transactions using pytest, Flask, and SQLAlchemy.
+'''
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name='pytest-flask-sqlalchemy-transactions',
+    author='Jean Cochrane',
+    author_email='jean@jeancochrane.com',
+    url='https://github.com/jeancochrane/pytest-flask-sqlalchemy-transactions',
+    description='Run tests in transactions using pytest, Flask, and SQLalchemy.',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    version='1.0.2',
+    packages=['transactions'],
+    install_requires=['pytest>=3.2.1',
+                      'pytest-mock>=1.6.2',
+                      'SQLAlchemy>=1.2.2',
+                      'Flask-SQLAlchemy>=2.3'],
+    extras_require={'tests': ['pytest-postgresql']},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Plugins',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Framework :: Pytest',
+    ],
+
+    # Make the package available to pytest
+    entry_points={
+        'pytest11': [
+            'pytest-flask-sqlalchemy-transactions = transactions.plugin',
+        ]
+    },
+)

4cc54f2fa5c285987c2bc1f6e630754dbd3eff40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:25 +0000

    Add package spiderpy/1.0.5/spiderpy-1.0.5.tar.gz

---
+++ packages/spiderpy/spiderpy-1.0.5.tar.gz/spiderpy-1.0.5/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='spiderpy',
+      version='1.0.5',
+      description='Python wrapper for the Spider API, a way to manage your Spider installation',
+      url='https://www.github.com/peternijssen/spiderpy/',
+      author='Peter Nijssen',
+      author_email='peter@peternijssen.nl',
+      license='MIT',
+      install_requires=['requests>=2.0'],
+      packages=find_packages(),
+      zip_safe=True)

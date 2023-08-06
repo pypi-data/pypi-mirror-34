fabd4c88a906b272ed4f770796f2a38b7ae9e84b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:48 +0000

    Add package pybel-cx/0.1.1.dev0/pybel_cx-0.1.1.dev0-py3-none-any.whl

---
+++ packages/pybel-cx/pybel_cx-0.1.1.dev0-py3-none-any.whl/pybel_cx/constants.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""Constants for PyBEL-CX."""
+
+__all__ = [
+    'NDEX_USERNAME',
+    'NDEX_PASSWORD',
+]
+
+#: The version of PyBEL-CX
+VERSION = '0.1.1-dev'
+
+#: The name of the environment variable to search or the NDEx username
+NDEX_USERNAME = 'NDEX_USERNAME'
+
+#: The name of the environment variable to search or the NDEx password
+NDEX_PASSWORD = 'NDEX_PASSWORD'

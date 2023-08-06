20ab2bc9067118dfbce56e3ae1860f88dfa521bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:00 +0000

    Add package time2relax/0.5.1/time2relax-0.5.1.tar.gz

---
+++ packages/time2relax/time2relax-0.5.1.tar.gz/time2relax-0.5.1/time2relax/__init__.py
@@ -0,0 +1,24 @@
+#             __
+# _|_ o __  _  _) __ _  |  _
+#  |_ | |||(/_/__ | (/_ | (_|><
+#
+
+"""A CouchDB driver for Python.
+
+Example::
+
+    >>> from time2relax import CouchDB
+    >>> db = CouchDB('http://localhost:5984/dbname')
+    >>> db.insert({'title': 'Ziggy Stardust'})
+    <Response [201]>
+"""
+
+from __future__ import absolute_import
+
+from time2relax.__version__ import (  # noqa: F401
+    __author__, __author_email__, __copyright__, __couch__, __description__, __license__, __title__, __url__,
+    __version__)
+from time2relax.exceptions import (  # noqa: F401
+    BadRequest, Forbidden, HTTPError, MethodNotAllowed, PreconditionFailed, ResourceConflict, ResourceNotFound,
+    ServerError, Unauthorized)
+from time2relax.models import CouchDB  # noqa: F401

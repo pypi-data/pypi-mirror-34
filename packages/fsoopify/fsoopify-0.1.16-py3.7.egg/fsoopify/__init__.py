3e722457f9254ff68c5a9a6b2897096b2d383bf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:37:30 +0000

    Add package fsoopify/0.1.16/fsoopify-0.1.16-py3.7.egg

---
+++ packages/fsoopify/fsoopify-0.1.16-py3.7.egg/fsoopify/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017~2999 - cologler <skyoflw@gmail.com>
+# ----------
+#
+# ----------
+
+from .paths import Path
+from .nodes import (
+    NodeInfo, DirectoryInfo, FileInfo,
+    FormatNotFoundError, SerializeError,
+)

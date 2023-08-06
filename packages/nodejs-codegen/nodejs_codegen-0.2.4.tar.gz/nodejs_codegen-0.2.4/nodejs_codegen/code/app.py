9f281b3f4a75c3bc7e5310d2ac85af2bb0850653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:25 +0000

    Add package nodejs-codegen/0.2.4/nodejs_codegen-0.2.4.tar.gz

---
+++ packages/nodejs-codegen/nodejs_codegen-0.2.4.tar.gz/nodejs_codegen-0.2.4/nodejs_codegen/code/app.py
@@ -0,0 +1,15 @@
+from codegenhelper import debug
+import os
+from code_engine import publish
+from mapper_on_file import mapping
+
+def get_resource_path(name):
+    return os.path.join(os.path.split(__file__)[0], "..", name)
+
+def gen(subscribe_name,  data, output_path):
+    publish(debug(get_resource_path('templates'), "gen:template_path"), \
+            subscribe_name, \
+            mapping(get_resource_path("mappers/mapping.mapper"), data),
+            debug(output_path, 'gen:output_path')
+            )
+    

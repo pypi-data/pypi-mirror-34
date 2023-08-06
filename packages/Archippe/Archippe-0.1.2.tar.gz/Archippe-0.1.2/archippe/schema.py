02314b11e03c54e9d4f42e9ef35bd20070387790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:00 +0000

    Add package Archippe/0.1.2/Archippe-0.1.2.tar.gz

---
+++ packages/Archippe/Archippe-0.1.2.tar.gz/Archippe-0.1.2/archippe/schema.py
@@ -0,0 +1,44 @@
+def get_schema():
+    schema = {
+        "data-persistence": {
+            "description": "Archippe is a data persistence micro service for pelops.",
+            "type": "object",
+            "properties": {
+                "topics": {
+                    "description": "list of topics that should be persisted",
+                    "type": "array",
+                    "items": {
+                        "type": "object",
+                        "properties": {
+                            "topic": {
+                                "type": "string"
+                            },
+                            "type": {
+                                "description": "convert message to this value",
+                                "type": "string",
+                                "enum": ["boolean", "BOOLEAN", "Boolean",
+                                         "string", "String", "STRING",
+                                         "float", "Float", "FLOAT",
+                                         "integer", "Integer", "INTEGER"
+                                         ]
+                            }
+                        },
+                        "required": ["topic", "type"],
+                        "additionalProperties": False
+                    }
+                },
+                "topic-request-prefix": {
+                    "description": "prefix for each topic to request historic data",
+                    "type": "string"
+                },
+                "topic-response-prefix": {
+                    "description": "prefix for each topic to publish historic data",
+                    "type": "string"
+                }
+            },
+            "required": ["topics", "topic-request-prefix", "topic-response-prefix"],
+            "additionalProperties": False
+        }
+    }
+    return schema
+

356ffee9a4804fe435f15d8b0f7578a94d087f12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:27 +0000

    Add package asyncmrq/0.7.0/asyncmrq-0.7.0.tar.gz

---
+++ packages/asyncmrq/asyncmrq-0.7.0.tar.gz/asyncmrq-0.7.0/mrq/parser.py
@@ -0,0 +1,39 @@
+
+class Parser(object):
+
+  def __init__(self, c=None):
+    self.c = c
+    self.buf = b''
+    self.bl = 0
+    self.msglen = 0
+
+  def parse(self, data=b''):
+    """
+    Parses the response from the MrQ server
+    """
+    self.buf += data
+    if self.bl == 0:
+      self.msglen = int.from_bytes(data[1:4], byteorder='little')
+    self.bl += len(data)
+    #print(self.bl)
+
+    if self.bl < self.msglen+5: return
+
+    #b'\x01\x08\x00\x00\x00\x04\x00\x00\x00test'
+    #print( data[0] )
+    #print( int.from_bytes(data[1:4], byteorder='little') )
+    #print( int.from_bytes(data[5:8], byteorder='little') )
+
+    i = 5
+    msgs = []
+    while (i < self.msglen):
+      ml = int.from_bytes(self.buf[i:i+3], byteorder='little')
+      i+=4
+      msgs.append( self.buf[i:i+ml] )
+      i += ml
+
+    self.c.process_read(msgs)
+      
+      
+
+     

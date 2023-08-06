3969fb3582c2afce6d3d8d38fdf908a1a108a028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:57 +0000

    Add package bigQueryExporterEnhanced/1.0.1/bigQueryExporterEnhanced-1.0.1.tar.gz

---
+++ packages/bigQueryExporterEnhanced/bigQueryExporterEnhanced-1.0.1.tar.gz/bigQueryExporterEnhanced-1.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='bigQueryExporterEnhanced',
+    version='1.0.1',
+    description='Package codes to execute queries on BigQuery and save to local machine',
+    author='Icarus So (enhanced by Jason Tsang)',
+    author_email='icarus.so@gmail.com',
+    url='https://github.com/tsangkinhoi/bigQueryExporter-enhanced',  # use the URL to the github repo
+    keywords=['bigquery', 'local', 'export'],  # arbitrary keywords
+    packages=setuptools.find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)

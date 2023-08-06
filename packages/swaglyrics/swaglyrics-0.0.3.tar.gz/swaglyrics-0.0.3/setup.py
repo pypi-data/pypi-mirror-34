36e8013377b636fa0417633e9d2718b90ab7800e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:27 +0000

    Add package swaglyrics/0.0.3/swaglyrics-0.0.3.tar.gz

---
+++ packages/swaglyrics/swaglyrics-0.0.3.tar.gz/swaglyrics-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="swaglyrics",
+    version="0.0.3",
+    author="Aadi Bajpai",
+    author_email="aadibajpai@gmail.com",
+    description="Fetch the currently playing song from Spotify and display lyrics on cmd or in a browser tab.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aadibajpai/SwagLyrics-For-Spotify",
+    scripts=['swaglyrics/run.py'],
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+    ),
+)

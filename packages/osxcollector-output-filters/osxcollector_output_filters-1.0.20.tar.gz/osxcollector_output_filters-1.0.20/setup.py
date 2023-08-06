98fe01688460034480cb699054edd4ddebb8d8a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:42 +0000

    Add package osxcollector-output-filters/1.0.20/osxcollector_output_filters-1.0.20.tar.gz

---
+++ packages/osxcollector-output-filters/osxcollector_output_filters-1.0.20.tar.gz/osxcollector_output_filters-1.0.20/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name="osxcollector_output_filters",
+    version="1.0.20",
+    author="Yelp Security",
+    author_email="opensource@yelp.com",
+    description="Filters that process and transform the output of OSXCollector",
+    license="GNU General Public License",
+    url="https://github.com/Yelp/osxcollector_output_filters",
+    setup_requires="setuptools",
+    packages=find_packages(exclude=["tests"]),
+    provides=["osxcollector"],
+    install_requires=[
+        "simplejson",
+        "threat_intel",
+        "tldextract"
+    ],
+)

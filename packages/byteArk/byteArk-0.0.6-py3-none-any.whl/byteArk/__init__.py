b268a883a235d4ffa1c227c5c9e944ae14c7a20a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:01 +0000

    Add package byteArk/0.0.6/byteArk-0.0.6-py3-none-any.whl

---
+++ packages/byteArk/byteArk-0.0.6-py3-none-any.whl/byteArk/__init__.py
@@ -0,0 +1,9 @@
+name = "ByteArk Project"
+
+class Vision:
+   
+    def __init__(self):
+        print("Init")
+    
+    def face_recognition_image(self, file_name):
+        print("FACE : "+file_name)

dd57ff00aad478dfe7498a4e19227babc534ea86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:40 +0000

    Add package tapioca-trello/0.2/tapioca_trello-0.2-py2.py3-none-any.whl

---
+++ packages/tapioca-trello/tapioca_trello-0.2-py2.py3-none-any.whl/tapioca_trello/resource_mapping/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from .boards import BOARDS_MAPPING
+from .cards import CARDS_MAPPING
+
+RESOURCE_MAPPING = {}
+RESOURCE_MAPPING.update(BOARDS_MAPPING)
+RESOURCE_MAPPING.update(CARDS_MAPPING)

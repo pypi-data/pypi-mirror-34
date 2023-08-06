828ad44ca9edbd4ed679e31660c2895849c250e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:18 +0000

    Add package dj-pysher/0.3/dj-pysher-0.3.tar.gz

---
+++ packages/dj-pysher/dj-pysher-0.3.tar.gz/dj-pysher-0.3/dj_pysher/sender.py
@@ -0,0 +1,35 @@
+from pusher import pusher
+from django.conf import settings
+
+
+class SenderApplication(object):
+    app_id = settings.PUSHER_APP_ID or ''
+    appkey = settings.PUSHER_APP_KEY or ''
+    secret = settings.PUSHER_SECRET or ''
+    cluster = settings.PUSHER_CLUSTER or ''
+    secure = settings.PUSHER_SECURE 
+
+    def __init__(self):
+        """
+        Construtor
+        :param channel: Canal
+        :param event: Evento
+        :param data: Dicionario
+        """
+        self.client = pusher.Pusher(
+            app_id=self.app_id,
+            key=self.appkey,
+            secret=self.secret,
+            cluster=self.cluster,
+            ssl=self.secure
+        )
+
+    def send(self, channel, event, data):
+        self.client.trigger(channel, event, data)
+
+    def set_channel(self, channel):
+        
+        def func(event, data):
+            self.client.trigger(channel, event, data)
+
+        return func

fe869c5204650560e9e91fd26cc43173f7e0516d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:53 +0000

    Add package pooch/0.1a2/pooch-0.1a2.tar.gz

---
+++ packages/pooch/pooch-0.1a2.tar.gz/pooch-0.1a2/pooch/tests/utils.py
@@ -0,0 +1,55 @@
+"""
+Utilities for testing code.
+"""
+import os
+
+from .. import __version__
+from ..utils import check_version
+
+
+def check_tiny_data(fname):
+    """
+    Load the tiny-data.txt file and check that the contents are correct.
+    """
+    assert os.path.exists(fname)
+    with open(fname) as tinydata:
+        content = tinydata.read()
+    true_content = "\n".join(
+        ["# A tiny data file for test purposes only", "1  2  3  4  5  6"]
+    )
+    assert content.strip() == true_content
+
+
+def pooch_test_url():
+    """
+    Get the base URL for the test data used in Pooch itself.
+
+    The URL is a github raw link to the ``pooch/tests/data`` directory from the
+    `Github repository <https://github.com/fatiando/pooch>`__. It matches the pooch
+    version specified in ``pooch.__version__``.
+
+    Returns
+    -------
+    url
+        The versioned URL for pooch's test data.
+
+    """
+    url = "https://github.com/fatiando/pooch/raw/{version}/pooch/tests/data/".format(
+        version=check_version(__version__)
+    )
+    return url
+
+
+def pooch_test_registry():
+    """
+    Get a registry for the test data used in Pooch itself.
+
+    Returns
+    -------
+    registry
+        Dictionary with pooch's test data files and their hashes.
+
+    """
+    sha256 = "baee0894dba14b12085eacb204284b97e362f4f3e5a5807693cc90ef415c1b2d"
+    registry = {"tiny-data.txt": sha256}
+    return registry

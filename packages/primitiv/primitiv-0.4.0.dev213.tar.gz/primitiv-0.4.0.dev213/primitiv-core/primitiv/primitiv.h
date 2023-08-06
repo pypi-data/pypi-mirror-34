e4df53c7347826a2538c85aa763e770e7e05e1f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:37 +0000

    Add package primitiv/0.4.0.dev213/primitiv-0.4.0.dev213.tar.gz

---
+++ packages/primitiv/primitiv-0.4.0.dev213.tar.gz/primitiv-0.4.0.dev213/primitiv-core/primitiv/primitiv.h
@@ -0,0 +1,31 @@
+#ifndef PRIMITIV_PRIMITIV_H_
+#define PRIMITIV_PRIMITIV_H_
+
+#include <primitiv/config.h>
+
+// This header file describes some include directives and may help users to use
+// the primitiv library.
+#include <primitiv/error.h>
+#include <primitiv/functions.h>
+#include <primitiv/graph.h>
+#include <primitiv/initializer_impl.h>
+#include <primitiv/model.h>
+#include <primitiv/naive_device.h>
+#include <primitiv/parameter.h>
+#include <primitiv/shape.h>
+#include <primitiv/tensor.h>
+#include <primitiv/optimizer_impl.h>
+
+// Header files for specific device classes.
+#ifdef PRIMITIV_USE_EIGEN
+#include <primitiv/eigen_device.h>
+#endif  // PRIMITIV_USE_EIGEN
+#ifdef PRIMITIV_USE_CUDA
+#include <primitiv/cuda_device.h>
+#include <primitiv/cuda16_device.h>
+#endif  // PRIMITIV_USE_CUDA
+#ifdef PRIMITIV_USE_OPENCL
+#include <primitiv/opencl_device.h>
+#endif  // PRIMITIV_USE_OPENCL
+
+#endif  // PRIMITIV_PRIMITIV_H_

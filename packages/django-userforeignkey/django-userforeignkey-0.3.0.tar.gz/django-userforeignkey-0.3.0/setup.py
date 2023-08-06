6ff6225ed3d2e210efc2cebc7d16b8b3ae1d9a6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:40 +0000

    Add package django-userforeignkey/0.3.0/django-userforeignkey-0.3.0.tar.gz

---
+++ packages/django-userforeignkey/django-userforeignkey-0.3.0.tar.gz/django-userforeignkey-0.3.0/setup.py
@@ -0,0 +1,43 @@
+import os
+
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-userforeignkey',
+    version='0.3.0',
+    packages=find_packages(),
+    include_package_data=True,
+    license='BSD License',
+    description='A simple Django app that will give you a UserForeignKey model field.',
+    long_description=README,
+    url='https://ks.co.at/',
+    author='Andreas Stocker',
+    author_email='andreas@ks.co.at',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)

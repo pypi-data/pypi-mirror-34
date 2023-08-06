0ca07ff9945c325e1ffd0ef859a1a28572ee47a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:25 +0000

    Add package boatmacro/1.5/boatmacro-1.5.zip

---
+++ packages/boatmacro/boatmacro-1.5.zip/boatmacro-1.5/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+        name = 'boatmacro', 
+        version = '1.5', 
+        py_modules = ['__init__'], 
+        author = 'boatonboat',
+        author_email = 'usbjewon@gmai.com',
+        description = 'auto | macro module'
+        )

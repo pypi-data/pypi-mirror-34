47d6cbbbd6d97a2d2981ed58157084f4793416af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:11 +0000

    Add package lastpy/1.0.8/lastpy-1.0.8.tar.gz

---
+++ packages/lastpy/lastpy-1.0.8.tar.gz/lastpy-1.0.8/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+  name = 'lastpy',
+  packages = ['lastpy'],
+  version = '1.0.8',
+  description = 'A straightforward utility for scrobbling tracks to last.fm',
+  author = 'Noah Huber-Feely',
+  author_email = 'nhuberfeely@gmail.com',
+  url = 'https://github.com/huberf/lastfm-scrobbler',
+  download_url = 'https://github.com/huberf/lastfm-scrobbler/archive/1.0.8.tar.gz',
+  keywords = ['lastfm'],
+  classifiers = [],
+)

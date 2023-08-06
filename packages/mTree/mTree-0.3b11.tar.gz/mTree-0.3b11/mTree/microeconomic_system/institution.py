f90f75f15c1a0524e2c972f80e2d65ebcfdbfb9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:07 +0000

    Add package mTree/0.3b11/mTree-0.3b11.tar.gz

---
+++ packages/mTree/mTree-0.3b11.tar.gz/mTree-0.3b11/mTree/microeconomic_system/institution.py
@@ -0,0 +1,46 @@
+from thespian.actors import *
+from mTree.microeconomic_system.message_space import MessageSpace
+from mTree.microeconomic_system.message import Message
+import uuid
+from mTree.microeconomic_system.message_space import MessageSpace
+from mTree.microeconomic_system.message import Message
+from mTree.microeconomic_system.directive_decorators import *
+
+import logging
+import json
+
+
+
+class Institution(Actor):
+    def mTree_logger(self):
+        return logging.getLogger("mTree")
+
+    def experiment_log(self, *log_message):
+        self.mTree_logger().log(25, log_message)
+
+
+
+    def __str__(self):
+        return "<Institution: " + self.__class__.__name__+ ' @ ' + str(self.myAddress) + ">"
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __init__(self):
+        self.agents = []
+        self.agent_ids = []
+
+    def receiveMessage(self, message, sender):
+        #self.mTree_logger().log(24, "{!s} got {!s}".format(self, message))
+        directive_handler = self._enabled_directives.get(message.get_directive())
+        directive_handler(self, message)
+
+
+    def add_agent(self, agent_class):
+        agent_id = str(uuid.uuid1())
+
+#        agent = asys.createActor(agent_class, globalName = agent_id)
+        agent = self.createActor(agent_class)  # creates agent as child of institution
+        self.agents.append(agent)
+        self.agent_ids.append(agent_id)
+
---
+++ packages/mTree/mTree-0.3b11.tar.gz/mTree-0.3b11/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+setup(
+    name="mTree",
+    version="0.3-BETA-11",
+    packages=find_packages(),
+    include_package_data=True,
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires=['Flask>=1.0.2',
+                      'Flask-APScheduler>=1.8.0',
+                      'Flask-BasicAuth>=0.2.0',
+                      'Flask-Bootstrap>=3.3.7.1',
+                      'Flask-SocketIO>=3.0.1',
+                      'Flask-SQLAlchemy>=2.3.2',
+                      'eventlet>=0.20.1',
+                      'numpy>=1.11.1',
+                      'PyYaml>=3.12',
+                      'thespian>=3.8.0'],
+    scripts=['bin/mTree_control'],
+    # metadata for upload to PyPI
+    author="GMU CSN",
+    author_email="mtree.email@gmail.com",
+    description="This is the base mTree package",
+    license="MIT",
+    keywords="computational experimental economics",
+    url="https://github.com/gmucsn/mTree",   # project home page, if any
+)

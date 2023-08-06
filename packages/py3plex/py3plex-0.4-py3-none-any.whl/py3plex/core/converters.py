56da523f7eba0656f269adb361be06896d7a81d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:37:10 +0000

    Add package py3plex/0.4/py3plex-0.4-py3-none-any.whl

---
+++ packages/py3plex/py3plex-0.4-py3-none-any.whl/py3plex/core/converters.py
@@ -0,0 +1,47 @@
+## converters
+import networkx as nx
+from collections import defaultdict
+from ..visualization.layout_algorithms import *
+
+def prepare_for_visualization(multinet):
+
+    layers = defaultdict(list)
+    for node in multinet.nodes(data=True):
+        try:
+            layers[node[1]['type']].append(node[0])
+        except:
+            pass
+
+    networks = {layer_name : multinet.subgraph(v) for layer_name,v in layers.items()}
+    inverse_mapping = {}
+    layouts = []
+            
+    ## construct the inverse mapping
+    for k,v in layers.items():
+        for x in v:
+            inverse_mapping[x] = k
+
+    multiedges = defaultdict(list)
+    for edge in multinet.edges(data=True):
+        try:
+            if inverse_mapping[edge[0]] != inverse_mapping[edge[1]]:
+                multiedges[edge[2]['type']].append((edge[0],edge[1]))
+        except:
+            pass
+
+    names,networks = zip(*networks.items())
+    return (names,networks,multiedges)
+
+def prepare_for_visualization_hairball(multinet):
+
+    layers = defaultdict(list)
+    for node in multinet.nodes(data=True):
+        layers[node[1]['type']].append(node[0])
+
+    inverse_mapping = {}
+    enumerated_layers = {name : ind for ind,name in enumerate(set(list(layers.keys())))}
+    for k, v in layers.items():
+        for x in v:
+            inverse_mapping[x] = enumerated_layers[k]
+    ordered_names = [inverse_mapping[x] for x in multinet.nodes()]
+    return (ordered_names, multinet)

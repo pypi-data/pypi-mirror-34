75206cae1af066c3ff6ffcf1f8bb258c8c1a3393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:03 +0000

    Add package HTTPReplay/0.2.4/HTTPReplay-0.2.4.tar.gz

---
+++ packages/HTTPReplay/HTTPReplay-0.2.4.tar.gz/HTTPReplay-0.2.4/setup.py
@@ -0,0 +1,36 @@
+# Copyright (C) 2015-2018 Jurriaan Bremer <jbr@cuckoo.sh>
+# This file is part of HTTPReplay - http://jbremer.org/httpreplay/
+# See the file 'LICENSE' for copying permission.
+
+from setuptools import setup
+
+setup(
+    name="HTTPReplay",
+    version="0.2.4",
+    author="Jurriaan Bremer",
+    author_email="jbr@cuckoo.sh",
+    packages=[
+        "httpreplay",
+    ],
+    license="GPLv3",
+    description="Properly interpret, decrypt, and replay pcap files",
+    install_requires=[
+        "dpkt==1.8.7",
+        "tlslite-ng==0.6.0",
+        "click>=6.6, <7",
+    ],
+    extras_require={
+        "mitmproxy": [
+            "mitmproxy==0.18.2",
+        ],
+        "dev": [
+            "pytest>=2.9.1"
+        ]
+    },
+    entry_points={
+        "console_scripts": [
+            "httpreplay = httpreplay.main:httpreplay",
+            "pcap2mitm = httpreplay.main:do_pcap2mitm",
+        ]
+    },
+)

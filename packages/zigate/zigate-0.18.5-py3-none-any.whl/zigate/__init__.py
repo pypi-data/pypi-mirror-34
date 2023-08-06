bb99b8cc7376f7d3ad0ec637a8b782267cab461e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:33 +0000

    Add package zigate/0.18.5/zigate-0.18.5-py3-none-any.whl

---
+++ packages/zigate/zigate-0.18.5-py3-none-any.whl/zigate/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from .core import (ZiGate, ZiGateWiFi)
+from .const import *
+from pydispatch import dispatcher
+
+__version__ = '0.18.5'
+
+__all__ = ['ZiGate', 'ZiGateWiFi',
+           'dispatcher']

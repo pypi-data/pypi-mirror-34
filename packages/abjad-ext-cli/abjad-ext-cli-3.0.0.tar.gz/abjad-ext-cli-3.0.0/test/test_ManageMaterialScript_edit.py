0b028b47dc1edb8ac49524628b4410186650102f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:10 +0000

    Add package abjad-ext-cli/3.0.0/abjad-ext-cli-3.0.0.tar.gz

---
+++ packages/abjad-ext-cli/abjad-ext-cli-3.0.0.tar.gz/abjad-ext-cli-3.0.0/test/test_ManageMaterialScript_edit.py
@@ -0,0 +1,31 @@
+import abjad
+import abjadext.cli
+import pytest
+import uqbar.io
+from io import StringIO
+
+
+def test_success(paths, call_subprocess_mock):
+    string_io = StringIO()
+    call_subprocess_mock.return_value = 0
+    pytest.helpers.create_score(paths.test_directory_path)
+    material_path = pytest.helpers.create_material(
+        paths.test_directory_path, 'test_material')
+    script = abjadext.cli.ManageMaterialScript()
+    command = ['--edit', 'test_material']
+    with uqbar.io.RedirectedStreams(stdout=string_io):
+        with uqbar.io.DirectoryChange(paths.score_path):
+            try:
+                script(command)
+            except SystemExit as e:
+                raise RuntimeError('SystemExit: {}'.format(e.code))
+    pytest.helpers.compare_strings(
+        actual=string_io.getvalue(),
+        expected=r'''Edit candidates: 'test_material' ...''',
+    )
+    definition_path = material_path.joinpath('definition.py')
+    command = '{} {!s}'.format(
+        abjad.abjad_configuration.get_text_editor(),
+        definition_path,
+    )
+    call_subprocess_mock.assert_called_with(command)
---
+++ packages/abjad-ext-cli/abjad-ext-cli-3.0.0.tar.gz/abjad-ext-cli-3.0.0/test/test_ManageSegmentScript_stage.py
@@ -0,0 +1,72 @@
+import abjad
+import abjadext.cli
+import os
+import pytest
+import uqbar.io
+from io import StringIO
+from uqbar.strings import normalize
+
+
+def side_effect(command, **keywords):
+    _, file_name = command.split()
+    contents = normalize('''
+    segment_c
+    segment_b
+    segment_a
+    ''')
+    with open(file_name, 'w') as file_pointer:
+        file_pointer.write(contents)
+
+
+def test_success(paths, call_subprocess_mock):
+    string_io = StringIO()
+    call_subprocess_mock.return_value = 0
+    pytest.helpers.create_score(paths.test_directory_path)
+    pytest.helpers.create_segment(paths.test_directory_path, 'segment_a')
+    pytest.helpers.create_segment(paths.test_directory_path, 'segment_b')
+    pytest.helpers.create_segment(paths.test_directory_path, 'segment_c')
+    script = abjadext.cli.ManageSegmentScript()
+    command = ['--stage']
+    with uqbar.io.RedirectedStreams(stdout=string_io):
+        with uqbar.io.DirectoryChange(paths.score_path):
+            try:
+                script(command)
+            except SystemExit as e:
+                raise RuntimeError('SystemExit: {}'.format(e.code))
+    pytest.helpers.compare_strings(
+        actual=string_io.getvalue(),
+        expected=r'''
+        Staging segments:
+            Reading test_score/segments/metadata.json ... OK!
+        Staged:
+            segment_a
+            segment_b
+            segment_c
+        '''.replace('/', os.path.sep),
+    )
+    call_subprocess_mock.assert_called_with(
+        '{} segments.txt'.format(abjad.abjad_configuration.get_text_editor()),
+    )
+    call_subprocess_mock.side_effect = side_effect
+    string_io = StringIO()
+    with uqbar.io.RedirectedStreams(stdout=string_io):
+        with uqbar.io.DirectoryChange(paths.score_path):
+            try:
+                script(command)
+            except SystemExit as e:
+                raise RuntimeError('SystemExit: {}'.format(e.code))
+    pytest.helpers.compare_strings(
+        actual=string_io.getvalue(),
+        expected=r'''
+        Staging segments:
+            Reading test_score/segments/metadata.json ... OK!
+            Writing test_score/segments/metadata.json
+        Staged:
+            segment_c
+            segment_b
+            segment_a
+        '''.replace('/', os.path.sep),
+    )
+    call_subprocess_mock.assert_called_with(
+        '{} segments.txt'.format(abjad.abjad_configuration.get_text_editor()),
+    )

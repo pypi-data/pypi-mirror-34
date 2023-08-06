5b26bc6ec12a10fbb8e37f4ea1ee1efc80936129
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:43 +0000

    Add package JYTools/1.4.2/JYTools-1.4.2.tar.gz

---
+++ packages/JYTools/JYTools-1.4.2.tar.gz/JYTools-1.4.2/JYTools/__init__.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+# coding: utf-8
+
+import sys
+from MyEmail import EmailManager
+import StringTool
+
+
+__author__ = 'meisanggou'
+
+
+TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+DEFAULT_ENCODING = "utf-8"
+SECOND_ENCODING = "gbk"
+
+
+def jy_input(prompt, prompt_prefix=None):
+    if prompt_prefix is not None and prompt is not None:
+        prompt = StringTool.join([prompt_prefix, prompt], "")
+    if not prompt.endswith("\n"):
+        prompt += "\n"
+    if sys.version_info[0] == 2:
+        return raw_input(prompt)
+    else:
+        return input(prompt)
+
+
+def is_num(v):
+    return isinstance(v, (int, long))
+
+
+def is_number(v):
+    return is_num(v)

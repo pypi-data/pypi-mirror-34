8d63456b0b0743cb9b5d857926009f9619f1abc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:32 +0000

    Add package poacher/0.1/poacher-0.1.tar.gz

---
+++ packages/poacher/poacher-0.1.tar.gz/poacher-0.1/setup.py
@@ -0,0 +1,39 @@
+import unittest
+import os
+from setuptools import setup
+from distutils.core import Command
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+README = os.path.join(HERE, "README.rst")
+
+classifiers = [
+    'License :: OSI Approved :: Apache Software License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Natural Language :: English',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'Intended Audience :: Education',
+    'Intended Audience :: Information Technology',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+]
+
+with open(README, 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='poacher',
+    version='0.1',
+    description=('Watches github.com for new public repositories'),
+    long_description=long_description,
+    url='http://github.com/eriknyquist/GithubPoacher',
+    author='Erik Nyquist',
+    author_email='eknyquist@gmail.com',
+    license='Apache 2.0',
+    packages=['poacher'],
+    scripts=['bin/poacher-monitor'],
+    classifiers = classifiers,
+    include_package_data=True,
+    zip_safe=False
+)

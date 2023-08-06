87d6a70dc2ee3178ea6ecd85763b0960c622b511
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:53 +0000

    Add package ramrodbrain/0.1.52/ramrodbrain-0.1.52.tar.gz

---
+++ packages/ramrodbrain/ramrodbrain-0.1.52.tar.gz/ramrodbrain-0.1.52/brain/controller/__init__.py
@@ -0,0 +1,39 @@
+"""
+manipulate the controller database
+"""
+
+NAME_KEY = "Name"
+ID_KEY = "id"
+SERVICE_KEY = "ServiceName"
+UDP_KEY = "UDPPorts"
+TCP_KEY = "TCPPorts"
+
+CONFLICT_ACTION = "update"
+
+AVAILABLE = "Available"
+ACTIVE = "Active"
+STOPPED = "Stopped"
+RESTARTING = "Restarting"
+
+
+DESIRE_ACTIVE = "Activate"
+DESIRE_RESTART = "Restart"
+DESIRE_STOP = "Stop"
+DESIRE_NONE = ""
+
+ADDRESS_KEY = "Address"
+DESIRED_STATE_KEY = "DesiredState"
+
+ALLOWED_DESIRED_STATES = frozenset((DESIRE_ACTIVE,
+                                    DESIRE_RESTART,
+                                    DESIRE_STOP,
+                                    DESIRE_NONE))
+
+
+ALLOWED_STATES = frozenset((ACTIVE,
+                            AVAILABLE,
+                            STOPPED,
+                            RESTARTING))
+
+from . import plugins
+from . import interfaces

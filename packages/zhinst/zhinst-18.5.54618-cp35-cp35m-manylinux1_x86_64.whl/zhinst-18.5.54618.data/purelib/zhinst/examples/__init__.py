027471c0627743e8f1b87cf57648ee20aa554d32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:09 +0000

    Add package zhinst/18.5.54618/zhinst-18.5.54618-cp35-cp35m-manylinux1_x86_64.whl

---
+++ packages/zhinst/zhinst-18.5.54618-cp35-cp35m-manylinux1_x86_64.whl/zhinst-18.5.54618.data/purelib/zhinst/examples/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Zurich Instruments LabOne Python API Examples.
+"""
+import zhinst.examples.common
+import zhinst.examples.deprecated
+import zhinst.examples.uhf
+import zhinst.examples.hf2
+
+__all__ = ["common", "deprecated", "uhf", "hf2"]
+
+del zhinst

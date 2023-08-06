01d527bd5a2d3cbf0277632b6b095796b5a6f584
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:55 +0000

    Add package frankstest/1/frankstest-1.tar.gz

---
+++ packages/frankstest/frankstest-1.tar.gz/frankstest-1/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup
+setup(
+    name='frankstest',
+    version=1,
+    description=(
+        '项目的简单描述'
+    ),
+    long_description='项目的详细描述',
+    author='frank',
+    author_email='luoziluojun@126.com',
+    maintainer='frank',
+    maintainer_email='luoziluojun@126.com',
+    license='BSD License',
+    # packages=find_packages(),
+    platforms=["all"],
+    url='http://ff2.pw',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries'
+    ],
+)

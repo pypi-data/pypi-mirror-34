edec357b41b4d20e82421ca9d57299665f93f18c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:55 +0000

    Add package x-mroy-1051/0.0.5/x_mroy_1051-0.0.5-py3-none-any.whl

---
+++ packages/x-mroy-1051/x_mroy_1051-0.0.5-py3-none-any.whl/DrMoriaty/utils/setting.py
@@ -0,0 +1,9 @@
+import os
+
+
+TmpReportPATH = os.path.expanduser("/tmp/MasscanReports")
+ReportPATH = os.path.expanduser("~/.config/MasscanReports")
+
+LOGIN_INFO = os.path.expanduser("~/.config/fofa.user")
+LOGIN_INFO_SESSION = os.path.expanduser("~/.config/fofa.sess")
+DB_FOFA = os.path.expanduser("~/.config/fofa.db")

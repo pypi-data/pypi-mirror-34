9624e2b3353946a133924cc4b693a1d822aba86c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:56 +0000

    Add package clang/6.0.0.2/clang-6.0.0.2.tar.gz

---
+++ packages/clang/clang-6.0.0.2.tar.gz/clang-6.0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from os import path
+import sys
+
+if sys.version_info < (3, 0, 0):
+    from io import open  # python 2 builtin open doesn't have an encoding option
+# from https://packaging.python.org/guides/making-a-pypi-friendly-readme/
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name="clang",
+    author="Ethan Smith",
+    author_email="ethan@ethanhs.me",
+    version="6.0.0.2",
+    url="https://github.com/ethanhs/clang",
+    packages=["clang"],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)

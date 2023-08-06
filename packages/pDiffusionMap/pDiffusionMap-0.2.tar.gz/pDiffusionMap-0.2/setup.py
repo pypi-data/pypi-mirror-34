29a351af0c73295f0c6e06712665fc380c712a1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:37:36 +0000

    Add package pDiffusionMap/0.2/pDiffusionMap-0.2.tar.gz

---
+++ packages/pDiffusionMap/pDiffusionMap-0.2.tar.gz/pDiffusionMap-0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='pDiffusionMap',
+    version=0.2,
+    description=(
+        'This package aims to do Diffusion Map in a parallel way. '),
+    author='Haoyuan Li',
+    author_email='hyli16@stanford.edu',
+    maintainer='Haoyuan Li',
+    maintainer_email='hyli16@stanford.edu',
+    license='BSD License',
+    packages=["pDiffusionMap"],
+    platforms=["Linux"],
+    url='https://github.com/haoyuanli93/DiffusionMap',
+    classifiers=[],
+)

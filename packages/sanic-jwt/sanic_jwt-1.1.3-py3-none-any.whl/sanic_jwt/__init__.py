7706c9c40a9359196ae2bf71bdcc8416c9cd845f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:58 +0000

    Add package sanic-jwt/1.1.3/sanic_jwt-1.1.3-py3-none-any.whl

---
+++ packages/sanic-jwt/sanic_jwt-1.1.3-py3-none-any.whl/sanic_jwt/__init__.py
@@ -0,0 +1,26 @@
+__version__ = "1.1.3"
+__author__ = "Adam Hopkins"
+__credits__ = "Richard Kuesters"
+
+import logging
+
+from .authentication import Authentication
+from .configuration import Configuration
+from .decorators import protected, scoped
+from .endpoints import BaseEndpoint
+from .initialization import Initialize, initialize
+from .responses import Responses
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+
+__all__ = [
+    "Authentication",
+    "Initialize",
+    "Configuration",
+    "Responses",
+    "BaseEndpoint",
+    "initialize",
+    "protected",
+    "scoped",
+]

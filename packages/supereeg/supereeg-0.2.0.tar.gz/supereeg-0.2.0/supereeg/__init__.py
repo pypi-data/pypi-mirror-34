57dbfca40704f290cbc8c2fd6f08b6bc6a396176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:19 +0000

    Add package supereeg/0.2.0/supereeg-0.2.0.tar.gz

---
+++ packages/supereeg/supereeg-0.2.0.tar.gz/supereeg-0.2.0/supereeg/__init__.py
@@ -0,0 +1,8 @@
+from .brain import Brain
+from .model import Model
+from .nifti import Nifti
+from .location import Location
+from .load import load
+from .simulate import *
+from .helpers import tal2mni
+

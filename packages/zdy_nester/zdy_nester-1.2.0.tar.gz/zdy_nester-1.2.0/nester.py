aeae434842434bf870af7c2212a345d8fbc8091a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:29 +0000

    Add package zdy_nester/1.2.0/zdy_nester-1.2.0.tar.gz

---
+++ packages/zdy_nester/zdy_nester-1.2.0.tar.gz/zdy_nester-1.2.0/nester.py
@@ -0,0 +1,12 @@
+"""这是nester.py模块，提供了一个名为print_lol函数，该函数的作用是打印列表，其中可能包含或不包含嵌套列表。"""
+def print_lol(the_list, level=0):
+    """这个函数取一个位置参数，名为”the_list“,这可以是任何Python列表（也可以是包含嵌套列表的列表）。所指定的
+    列表中的每个数据项会（递归地）输出到屏幕上，各数据项各占一行"""
+    """第二个参数（level）用来在遇到嵌套列表时插入制表符,增加一个缺省值（赋初值），使其变为可选参数"""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, level+1)
+        else:
+            for tap in range(level):
+                print("\t", end='')
+            print(each_item)

9f7ff6cfd9fdd328cb28f96f03ff6da833186140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:54 +0000

    Add package habu/0.0.77/habu-0.0.77.tar.gz

---
+++ packages/habu/habu-0.0.77.tar.gz/habu-0.0.77/habu/cli/cmd_ip.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import click
+
+from habu.lib.ip import get_ip
+
+
+@click.command()
+def cmd_ip():
+    """Print your current public IP based on the response from https://api.ipify.org
+
+    Example:
+
+    \b
+    $ habu.ip
+    182.26.32.246"""
+
+    print(get_ip())
+

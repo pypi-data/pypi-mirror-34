bcc4911a7b089a91dfb5f2ab35362a5e4f5f4d32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:44 +0000

    Add package pyod/0.5.6/pyod-0.5.6.tar.gz

---
+++ packages/pyod/pyod-0.5.6.tar.gz/pyod-0.5.6/pyod/models/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from .abod import ABOD
+from .cblof import CBLOF
+from .combination import aom, moa, average, maximization
+from .feature_bagging import FeatureBagging
+from .hbos import HBOS
+from .iforest import IForest
+from .knn import KNN
+from .lof import LOF
+from .mcd import MCD
+from .ocsvm import OCSVM
+from .pca import PCA
+
+__all__ = ['ABOD',
+           'CBLOF',
+           'aom', 'moa', 'average', 'maximization',
+           'FeatureBagging',
+           'HBOS',
+           'IForest',
+           'KNN',
+           'LOF',
+           'MCD',
+           'OCSVM',
+           'PCA']

3067bea91b0e3867c04635608cb77889f3c9a23c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:40 +0000

    Add package django-user-payments/0.2.0/django-user-payments-0.2.0.tar.gz

---
+++ packages/django-user-payments/django-user-payments-0.2.0.tar.gz/django-user-payments-0.2.0/user_payments/user_subscriptions/admin.py
@@ -0,0 +1,37 @@
+from django.contrib import admin
+
+from user_payments.exceptions import UnknownPeriodicity
+from . import models
+
+
+@admin.register(models.Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "title",
+        "code",
+        "starts_on",
+        "ends_on",
+        "periodicity",
+        "amount",
+        "paid_until",
+        "renew_automatically",
+    )
+    list_filter = ("renew_automatically",)
+    radio_fields = {"periodicity": admin.HORIZONTAL}
+    raw_id_fields = ("user",)
+    search_fields = ("title",)
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        if not change:
+            try:
+                obj.create_periods()
+            except UnknownPeriodicity as exc:
+                self.message_user(request, exc)
+
+
+@admin.register(models.SubscriptionPeriod)
+class SubscriptionPeriodAdmin(admin.ModelAdmin):
+    list_display = ("subscription", "starts_on", "ends_on", "line_item")
+    raw_id_fields = ("subscription", "line_item")

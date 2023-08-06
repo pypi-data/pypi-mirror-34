f6f2cb39ff10486c04c0e2c147af049d0e0e6192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:48 +0000

    Add package autolens/0.1.8/autolens-0.1.8.tar.gz

---
+++ packages/autolens/autolens-0.1.8.tar.gz/autolens-0.1.8/autolens/commands/base.py
@@ -0,0 +1,36 @@
+"""The base command."""
+
+
+class Base(object):
+    """A base command."""
+
+    def __init__(self, options, *args, **kwargs):
+        self.options = options
+        self.args = args
+        self.kwargs = kwargs
+
+    def run(self):
+        raise NotImplementedError('You must implement the run() method yourself!')
+
+
+def await_input():
+    import termios, fcntl, sys, os
+    fd = sys.stdin.fileno()
+
+    oldterm = termios.tcgetattr(fd)
+    newattr = termios.tcgetattr(fd)
+    newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+    termios.tcsetattr(fd, termios.TCSANOW, newattr)
+
+    oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
+    fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
+
+    try:
+        while 1:
+            try:
+                c = sys.stdin.read(1)
+                return repr(c)
+            except IOError: pass
+    finally:
+        termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
+        fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)

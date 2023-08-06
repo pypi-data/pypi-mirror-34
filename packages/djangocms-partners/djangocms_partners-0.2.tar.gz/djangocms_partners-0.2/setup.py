063d5a85e852bdd677bd53830ada30beb170b8dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:05 +0000

    Add package djangocms-partners/0.2/djangocms_partners-0.2.tar.gz

---
+++ packages/djangocms-partners/djangocms_partners-0.2.tar.gz/djangocms_partners-0.2/setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    # read the contents of a text file
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='djangocms_partners',
+    version='0.2',
+    packages=find_packages(),
+    include_package_data=True,
+    license='BSD 3-Clause "New" or "Revised" License',
+    description='show parteners',
+    long_description=read('README.md'),
+    url='https://github.com/dmodules/djangocms_partners',
+    author='D-Modules',
+    #install_requires=["django-taggit==0.22.1"],
+    author_email='support@d-modules.com',
+)

8540d2a891e9e8f295f2efc83d6136ce19f31ea6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:22 +0000

    Add package scikit-snowland/0.1.2/scikit-snowland-0.1.2.tar.gz

---
+++ packages/scikit-snowland/scikit-snowland-0.1.2.tar.gz/scikit-snowland-0.1.2/snowland/image/api/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author  : 河北雪域网络科技有限公司 A.Star
+# @contact: astar@snowland.ltd
+# @site: 
+# @file: __init__.py.py
+# @time: 2018/7/26 10:32
+# @Software: PyCharm
+
+
+from .img_to_cartoon import cartoonise
+from .img_to_sketch import sketch
+from .img_to_freehand import freehand

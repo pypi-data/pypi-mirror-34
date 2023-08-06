6fbf8e5cb958e155752de9a1c5fe1f3adc6d8c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:18:46 +0000

    Add package vers/1.0.1/vers-1.0.1.tar.gz

---
+++ packages/vers/vers-1.0.1.tar.gz/vers-1.0.1/README.md
@@ -0,0 +1,20 @@
+# Vers
+
+Vers is a version comparision library
+
+## Installation
+
+```bash
+pip install vers
+```
+
+## Usage
+
+```python
+from vers import Version
+
+vers1 = Version("latest")
+vers2 = Version("1.2.3")
+
+print(vers1 >= vers2)
+```
---
+++ packages/vers/vers-1.0.1.tar.gz/vers-1.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="vers",
+    version="1.0.1",
+    author="AmirMohammad Dehghan",
+    author_email="amirmd76@gmail.com",
+    description="Version Comparision",
+    license="MIT",
+    url="https://github.com/amirmd76/vers",
+    packages=['vers'],
+    data_files=[],
+    install_requires=[],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Utilities",
+        "Programming Language :: Python"
+    ],
+)

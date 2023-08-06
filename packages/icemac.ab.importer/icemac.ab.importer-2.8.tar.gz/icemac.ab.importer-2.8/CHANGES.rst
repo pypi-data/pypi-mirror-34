0547b63d7cc2f57114ce452ce7ff07c46359beae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:36 +0000

    Add package icemac.ab.importer/2.8/icemac.ab.importer-2.8.tar.gz

---
+++ packages/icemac.ab.importer/icemac.ab.importer-2.8.tar.gz/icemac.ab.importer-2.8/CHANGES.rst
@@ -0,0 +1,35 @@
+===========
+ Changelog
+===========
+
+2.8 (2018-08-03)
+================
+
+- Update to changes in test infrastructure in `icemac.addressbook >= 7.0`.
+
+- Change license from ZPL to MIT.
+
+
+2.7 (2017-12-26)
+================
+
+- Add breadcrumbs.
+
+- Change `zope.interface.implements[Only]` and `zope.component.adapts` to
+  class decorators.
+
+- Also release as wheel.
+
+
+2.6 (2017-04-08)
+================
+
+- Update to changes in test infrastructure in `icemac.addressbook >= 4.0`.
+
+- Bring test coverage to 100 % including the tests themselves.
+
+
+Previous versions
+=================
+
+See ``OLD_CHANGES.rst`` inside the package.
---
+++ packages/icemac.ab.importer/icemac.ab.importer-2.8.tar.gz/icemac.ab.importer-2.8/src/icemac/ab/importer/browser/wizard/reader.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from icemac.addressbook.i18n import _
+import icemac.ab.importer.browser.wizard.base
+import icemac.ab.importer.source
+import zope.interface
+import zope.schema
+
+
+class IReaderSettings(zope.interface.Interface):
+    """Reader Setttings."""
+
+    reader = zope.schema.Choice(
+        title=_(u'Import file reader'),
+        source=icemac.ab.importer.source.Importers())
+
+    entries_number = zope.schema.Int(
+        title=_(u'Number of e.g. phone numbers per person'),
+        min=1, max=9, default=1)
+
+
+class ReaderSettings(
+        icemac.ab.importer.browser.wizard.base.FileSessionStorageStep):
+    """Step to set the reader and the number of entries."""
+
+    interface = IReaderSettings
+    title = label = _(u'Reader settings')
+
+    @property
+    def showBackButton(self):
+        """Back button condition."""
+        return True
+
+    def doBack(self, action):
+        """Process back action and return True on success."""
+        self.getContent()['edit_file_available'] = True
+        return super(ReaderSettings, self).doBack(action)

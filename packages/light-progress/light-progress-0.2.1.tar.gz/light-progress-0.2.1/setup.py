f71729d5c9342c5b26616be6074d36a9670eb25b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:42 +0000

    Add package light-progress/0.2.1/light-progress-0.2.1.tar.gz

---
+++ packages/light-progress/light-progress-0.2.1.tar.gz/light-progress-0.2.1/setup.py
@@ -0,0 +1,36 @@
+# -*- encoding:utf-8 -*-
+
+import os
+
+from setuptools import setup
+
+from light_progress import __version__
+
+# def get_requirements():
+#     with open('requirements.txt') as f:
+#         requirements = f.read().split('\n')
+#     return requirements
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst'), 'r') as f:
+    long_description = f.read()
+
+
+setup(
+    name='light-progress',
+    version=__version__,
+    packages=['light_progress'],
+    description='Light progress reporting tool for Python',
+    long_description=long_description,
+    url='https://github.com/itkr/light-progress',
+    license='MIT',
+    author='itkr',
+    author_email='itkrst@gmail.com',
+    # install_requires=get_requirements(),
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities'
+    ]
+)

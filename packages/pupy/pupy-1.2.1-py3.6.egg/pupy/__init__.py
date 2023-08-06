588c0e86658bdb93427b77a4e0d1cb80ee42069e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:18 +0000

    Add package pupy/1.2.1/pupy-1.2.1-py3.6.egg

---
+++ packages/pupy/pupy-1.2.1-py3.6.egg/pupy/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# JESSE RUBIN - pupy == Biblioteca
+from pupy.amazon_prime import *
+from pupy.decorations import *
+from pupy.listless import *
+from pupy.maths import *
+from pupy.werd import *
+
+

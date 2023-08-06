c7f5cec8a4563512b0caa39c41256c11802fe586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:48 +0000

    Add package pospell/0.0.6/pospell-0.0.6.tar.gz

---
+++ packages/pospell/pospell-0.0.6.tar.gz/pospell-0.0.6/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+with open('README.md') as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name='pospell',
+    version='0.0.6',
+    description="Spellcheck .po files containing reStructuredText translations",
+    long_description=long_description,
+    long_description_content_type='text/markdown', # This is important!
+    author="Julien Palard",
+    author_email='julien@palard.fr',
+    url='https://github.com/JulienPalard/pospell',
+    py_modules=[
+        'pospell',
+    ],
+    entry_points={
+        'console_scripts': ['pospell=pospell:main']
+    },
+    install_requires=['polib'],
+    license="MIT license",
+    keywords='po spell gettext',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ]
+)

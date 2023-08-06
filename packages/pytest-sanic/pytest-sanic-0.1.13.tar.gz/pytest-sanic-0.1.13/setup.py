ffe76eadc65f5f91f569ddd7c8dc5c0eaec04c65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:28 +0000

    Add package pytest-sanic/0.1.13/pytest-sanic-0.1.13.tar.gz

---
+++ packages/pytest-sanic/pytest-sanic-0.1.13.tar.gz/pytest-sanic-0.1.13/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+base = os.path.dirname(os.path.abspath(__file__))
+
+README_PATH = os.path.join(base, "README.rst")
+
+install_requires = [
+    'pytest',
+    'sanic',
+    'aiohttp',
+    'async_generator',
+]
+
+tests_require = []
+
+setup(name='pytest-sanic',
+      version='0.1.13',
+      description='',
+      long_description=open(README_PATH).read(),
+      author='Yun Xu',
+      author_email='yunxu1992@gmail.com',
+      url='',
+      packages=find_packages(),
+      install_requires=install_requires,
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Operating System :: MacOS',
+          'Operating System :: POSIX :: Linux',
+          'Topic :: System :: Software Distribution',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+      ],
+      entry_points={
+        'pytest11': ['sanic = pytest_sanic.plugin'],
+      },
+)

04137624d4a9cf9fe1a69bbe2ff2f6c20195c963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:03 +0000

    Add package funkpy/0.0.18/funkpy-0.0.18-py3-none-any.whl

---
+++ packages/funkpy/funkpy-0.0.18-py3-none-any.whl/funkpy/__init__.py
@@ -0,0 +1,8 @@
+import funkpy.utils
+from funkpy.Collection import exports as Collection
+import funkpy.Monads
+from funkpy.Object import exports as Object
+
+__version__ = "0.0.18"
+__name__ = "funkpy"
+__license__ = "MIT"

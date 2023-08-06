d3fcfea0686d4d86e046f1ef86d37356227c773c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:09 +0000

    Add package django-ajax-image-upload/0.7.2/django-ajax-image-upload-0.7.2.tar.gz

---
+++ packages/django-ajax-image-upload/django-ajax-image-upload-0.7.2.tar.gz/django-ajax-image-upload-0.7.2/ajaximage/templatetags/ajaximage_filters.py
@@ -0,0 +1,13 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter(name='upload_to')
+def upload_to(model):
+    fields = model._meta.get_fields()
+    field = next((field for field in fields if hasattr(field, 'upload_to')), None)
+    if callable(field.upload_to):
+        return field.upload_to('')
+    return field.upload_to
+

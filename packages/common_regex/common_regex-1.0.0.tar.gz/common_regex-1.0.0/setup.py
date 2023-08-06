51df78f78900130a24a66afc885d63dcbc7bc39e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:08 +0000

    Add package common_regex/1.0.0/common_regex-1.0.0.tar.gz

---
+++ packages/common_regex/common_regex-1.0.0.tar.gz/common_regex-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    long_description='Rerelease of stale package commonregex',
+    name='common_regex',
+    py_modules=['common_regex'],
+    version='1.0.0'
+)

51884e19f372ac7072157f181d6b735576f77165
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:50 +0000

    Add package is99kdf/0.1.1/is99kdf-0.1.1.tar.gz

---
+++ packages/is99kdf/is99kdf-0.1.1.tar.gz/is99kdf-0.1.1/setup.py
@@ -0,0 +1,30 @@
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: mage
+# Mail: mage@woodcol.com
+# Created Time:  2018-1-23 19:17:34
+#############################################
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "is99kdf",
+    version = "0.1.1",
+    keywords = ("pip", "is99kdf"),
+    description = "python tools by dengfeng.ke",
+    long_description = "python tools by dengfeng.ke",
+    license = "BJFU Licence",
+
+    url = "https://github.com/",
+    author = "is99kdf",
+    author_email = "dengfeng.ke@beijinghear.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ["scipy","numpy","librosa"]
+)
+

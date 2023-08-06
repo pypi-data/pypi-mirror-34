28473c4832da6522ad8b530424e66042fa605234
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:16 +0000

    Add package wwx/0.2.15/wwx-0.2.15.tar.gz

---
+++ packages/wwx/wwx-0.2.15.tar.gz/wwx-0.2.15/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: wxnacy(wxnacy@gmail.com)
+# Description:
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'wwx',
+    version = '0.2.15',
+    keywords='wx',
+    description = 'a library for wx Developer',
+    license = 'MIT License',
+    url = 'https://github.com/wxnacy/wwx',
+    author = 'wxnacy',
+    author_email = 'wxnacy@gmail.com',
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+    install_requires = [
+        'requests>=2.19.1',
+        'pycrypto>=2.6.1',
+        'xmltodict>=0.11.0'
+        ],
+)
+

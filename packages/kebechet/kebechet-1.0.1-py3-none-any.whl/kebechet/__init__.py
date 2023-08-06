e70c587bd1756afa71aa7344774c40df7cf7b69b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:01 +0000

    Add package kebechet/1.0.1/kebechet-1.0.1-py3-none-any.whl

---
+++ packages/kebechet/kebechet-1.0.1-py3-none-any.whl/kebechet/__init__.py
@@ -0,0 +1,9 @@
+"""Update dependencies stated in the Pipfile.lock for the given repo or repositories."""
+
+from .enums import ServiceType
+from .managers import InfoManager
+from .managers import UpdateManager
+
+__name__ = 'kebechet'
+__version__ = "1.0.1"
+__author__ = 'Fridolin Pokorny <fridolin.pokorny@gmail.com>'

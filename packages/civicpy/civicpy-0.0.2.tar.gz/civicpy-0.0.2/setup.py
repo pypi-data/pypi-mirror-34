29c91f9e5ff6ca545314d112911e2ddd986b8f98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:03 +0000

    Add package civicpy/0.0.2/civicpy-0.0.2.tar.gz

---
+++ packages/civicpy/civicpy-0.0.2.tar.gz/civicpy-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='civicpy',
+    version='0.0.2',
+    packages=['civicpy'],
+    url='https://github.com/griffithlab/civicpy',
+    license='MIT',
+    author='Alex H. Wagner',
+    author_email='awagner24@wustl.edu',
+    description='a python wrapper and tools for the civic API',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+    install_requires=[
+        'pytest',
+        'requests'
+    ],
+    python_requires='~=3.6',
+    entry_points={},
+
+)

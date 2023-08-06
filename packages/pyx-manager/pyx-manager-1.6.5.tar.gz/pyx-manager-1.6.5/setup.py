c5bd9498339dc00a8639c752d6dccf98fe88fe07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:50 +0000

    Add package pyx-manager/1.6.5/pyx-manager-1.6.5.tar.gz

---
+++ packages/pyx-manager/pyx-manager-1.6.5.tar.gz/pyx-manager-1.6.5/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("PYPI_README.md", 'r') as f:
+    readme = f.read()
+
+setuptools.setup(
+    name="pyx-manager",
+    version="1.6.5",
+    entry_points={
+        'console_scripts': ["pyx = pyx.__main__:main"]
+    },
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'docopt',
+        'colorama',
+        'twine'
+        ],
+    author='Armand Foucault',
+    author_email='armand.foucault@telecom-bretagne.eu',
+    description="Command-line project manager",
+    long_description=readme,
+    url="https://github.com/Afoucaul/pyx"
+)

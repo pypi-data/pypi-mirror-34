a9f66fa32991fe1823070a4f2b9248703c5db7a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:59 +0000

    Add package blueqat/0.0.1/blueqat-0.0.1.tar.gz

---
+++ packages/blueqat/blueqat-0.0.1.tar.gz/blueqat-0.0.1/blueqat/__init__.py
@@ -0,0 +1,10 @@
+from .<ファイル> import *
+
+__copyright__    = 'Copyright (C) 2018 MDR'
+__version__      = '0.0.1'
+__license__      = 'MIT'
+__author__       = 'minato'
+__author_email__ = 'minato@mdrft.com'
+__url__          = 'https://github.com/mdrft/blueqat'
+
+__all__ = ['blueqat', ...]

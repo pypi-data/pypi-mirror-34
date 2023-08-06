25600bc637f22a1ecd9d7f2262b04d520f4f7348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:22 +0000

    Add package minervaboto/1.0.3/minervaboto-1.0.3.tar.gz

---
+++ packages/minervaboto/minervaboto-1.0.3.tar.gz/minervaboto-1.0.3/minervaboto/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from .minervaboto import renew_books
+from .minervaboto import print_books
+from .minervaboto import books_to_string
+from .minervaboto import renew_to_string

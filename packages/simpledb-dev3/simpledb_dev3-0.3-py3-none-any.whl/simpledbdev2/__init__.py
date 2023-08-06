66f8629c164aeaa5030b41a8852d07bc7bc5b8e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:03 +0000

    Add package simpledb-dev3/0.3/simpledb_dev3-0.3-py3-none-any.whl

---
+++ packages/simpledb-dev3/simpledb_dev3-0.3-py3-none-any.whl/simpledbdev2/__init__.py
@@ -0,0 +1,7 @@
+##################################################
+# Copyright (C) 2017, All rights reserved.
+##################################################
+
+__project_name__ = "simpledb-dev3"
+__version__ = "0.3"
+__description__ = "A local SimpleDB server"

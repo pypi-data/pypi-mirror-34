f8e0ff342fd72250a5bfe1eadd4f9b4852d2ea9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:51 +0000

    Add package plone.app.fhirfield/1.0.0b4/plone.app.fhirfield-1.0.0b4.tar.gz

---
+++ packages/plone.app.fhirfield/plone.app.fhirfield-1.0.0b4.tar.gz/plone.app.fhirfield-1.0.0b4/src/plone/app/fhirfield/indexes/PluginIndexes/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# @Date    : 2018-05-19 19:28:37
+# @Author  : Md Nazrul Islam (email2nazrul@gmail.com)
+# @Link    : http://nazrul.me/
+# @Version : $Id$
+# All imports here
+from .FHIRIndex import FhirActivityDefinitionIndex  # noqa: F401
+from .FHIRIndex import FhirAppointmentIndex  # noqa: F401
+from .FHIRIndex import FhirCarePlanIndex  # noqa: F401
+from .FHIRIndex import FhirDeviceIndex  # noqa: F401
+from .FHIRIndex import FhirDeviceRequestIndex  # noqa: F401
+from .FHIRIndex import FhirFieldIndex  # noqa: F401
+from .FHIRIndex import FhirHealthcareServiceIndex  # noqa: F401
+from .FHIRIndex import FhirMedicationAdministrationIndex  # noqa: F401
+from .FHIRIndex import FhirMedicationDispenseIndex  # noqa: F401
+from .FHIRIndex import FhirMedicationRequestIndex  # noqa: F401
+from .FHIRIndex import FhirMedicationStatementIndex  # noqa: F401
+from .FHIRIndex import FhirObservationIndex  # noqa: F401
+from .FHIRIndex import FhirOrganizationIndex  # noqa: F401
+from .FHIRIndex import FhirPatientIndex  # noqa: F401
+from .FHIRIndex import FhirPlanDefinitionIndex  # noqa: F401
+from .FHIRIndex import FhirPractitionerIndex  # noqa: F401
+from .FHIRIndex import FhirProcedureRequestIndex  # noqa: F401
+from .FHIRIndex import FhirQuestionnaireIndex  # noqa: F401
+from .FHIRIndex import FhirQuestionnaireResponseIndex  # noqa: F401
+from .FHIRIndex import FhirRelatedPersonIndex  # noqa: F401
+from .FHIRIndex import FhirTaskIndex  # noqa: F401
+from .FHIRIndex import FhirValueSetIndex  # noqa: F401
+from .FHIRIndex import REGISTRABLE_CLASSES  # noqa: F401
+
+
+__author__ = 'Md Nazrul Islam (email2nazrul@gmail.com)'

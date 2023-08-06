ce2a71f1bc55006d5189152483b5d3d7e6fd1001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:45 +0000

    Add package ml7/0.0.0/ml7-0.0.0.tar.gz

---
+++ packages/ml7/ml7-0.0.0.tar.gz/ml7-0.0.0/ml7/ml7.py
@@ -0,0 +1,19 @@
+#Wikicivi Crawler Client SDK
+import os,time
+import threading
+import datetime
+import urllib
+mport imageio
+
+
+
+
+class ml7:
+    def __init__(self):
+        self.apimap = {}
+    
+
+    #LOG,INFO,WARN,ERROR,
+    def intro():
+        print("这是TechYoung课程的机器学习辅助工具包")
+        return True

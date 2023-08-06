1ac1afe8f5d45777920002ed6a2b665c005a71b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:08 +0000

    Add package BulkImageResizer/1.0.1/BulkImageResizer-1.0.1.tar.gz

---
+++ packages/BulkImageResizer/BulkImageResizer-1.0.1.tar.gz/BulkImageResizer-1.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='BulkImageResizer',
+    version='1.0.1',
+    description='Resizes any number of images, written to use in responsive front end web development',
+    long_description=open('README.md', 'r').read(),
+    install_requires=['Pillow'],
+    url='https://github.com/suhrabjan/BulkImageResizer',
+    author='Suhrab Kurbanov',
+    author_email='sir.suhrab@gmail.com',
+    packages=find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    )
+)

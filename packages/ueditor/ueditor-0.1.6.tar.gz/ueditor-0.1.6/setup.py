b82f308c4fd2af9ad20963dc8283f4b69b4f69f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:59 +0000

    Add package ueditor/0.1.6/ueditor-0.1.6.tar.gz

---
+++ packages/ueditor/ueditor-0.1.6.tar.gz/ueditor-0.1.6/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='ueditor',
+    version='0.1.6',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    url='https://www.88cto.com/page/ueditor',
+    license='',
+    author='panjing',
+    author_email='newpanjing@icloud.com',
+    description='',
+    install_requires=['django'],
+)

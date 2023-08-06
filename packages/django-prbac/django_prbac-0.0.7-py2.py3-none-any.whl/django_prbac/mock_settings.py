e353fbcddc5d21420d60c3215aded1bd51fafb8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:32 +0000

    Add package django-prbac/0.0.7/django_prbac-0.0.7-py2.py3-none-any.whl

---
+++ packages/django-prbac/django_prbac-0.0.7-py2.py3-none-any.whl/django_prbac/mock_settings.py
@@ -0,0 +1,32 @@
+"""
+Settings just for running the django-prbac tests or checking out
+the admin site.
+"""
+
+from __future__ import unicode_literals
+SECRET_KEY = 'Not a secret key at all, actually'
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'django-prbac.db',
+    }
+}
+
+INSTALLED_APPS = [
+    # Django apps necessary to run the admin site
+    'django.contrib.auth',
+    'django.contrib.admin',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.staticfiles',
+
+    # And this app
+    'django_prbac',
+]
+
+STATIC_URL = '/static/'
+
+ROOT_URLCONF = 'django_prbac.urls'

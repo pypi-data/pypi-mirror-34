70aa61778efffac456f06cfbb228a2922482135f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:14 +0000

    Add package nester-pjy/1.1.0/nester_pjy-1.1.0.tar.gz

---
+++ packages/nester-pjy/nester_pjy-1.1.0.tar.gz/nester_pjy-1.1.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester_pjy',
+    version= '1.1.0',
+    py_modules = ['nester'],
+    author = 'pjy',
+    author_email = '1402762252@qq.com',
+    url = 'https://github.com/impjy',
+    description = 'a simple printer of nested lists',
+    
+
+
+    )

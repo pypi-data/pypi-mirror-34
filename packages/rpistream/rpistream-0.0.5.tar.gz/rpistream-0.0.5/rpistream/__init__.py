d5458ec1b38e3c6b84e2f290dec92ebee1ff44f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:14 +0000

    Add package rpistream/0.0.5/rpistream-0.0.5.tar.gz

---
+++ packages/rpistream/rpistream-0.0.5.tar.gz/rpistream-0.0.5/rpistream/__init__.py
@@ -0,0 +1,6 @@
+name="rpistream"
+#import netutils
+#import camera
+#import streamclient
+#import streamserver
+#ian told me to do this
---
+++ packages/rpistream/rpistream-0.0.5.tar.gz/rpistream-0.0.5/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+#with open("../README.md", "r") as fh:
+#    long_description = fh.read()
+
+setuptools.setup(
+    name="rpistream",
+    version="0.0.5",
+    author="Theo Cooper and Ian Huang",
+    author_email="theoac2009@outlook.com",
+    description="A very simple library built for streaming video from a remote Raspberry Pi server in realtime.",
+    long_description="foo",
+    long_description_content_type="text/markdown",
+    url="https://github.com/thatguy1234510/rpistream",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    )
+)

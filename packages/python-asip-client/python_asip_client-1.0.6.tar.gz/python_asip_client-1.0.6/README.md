a27b19c099f623bf2d995e9c7c83228ac8375531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:30 +0000

    Add package python-asip-client/1.0.6/python_asip_client-1.0.6.tar.gz

---
+++ packages/python-asip-client/python_asip_client-1.0.6.tar.gz/python_asip_client-1.0.6/README.md
@@ -0,0 +1,3 @@
+# python-asip
+A Python client for the ASIP protocol.
+Client should support both Python 2.7 and 3. Anyway, compatibility issues may still exist.
---
+++ packages/python-asip-client/python_asip_client-1.0.6.tar.gz/python_asip_client-1.0.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="python_asip_client",
+    version="1.0.6",
+    author="Gianluca Barbon, Adam Jarzebak, Michael Margolis, Franco Raimondi",
+    author_email="adam@jarzebak.eu",
+    description="Serial manager for Mirto robot services",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mdxmase/mirto_python_asip",
+    packages=find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ),
+)

400311f483dcd6c52ee8c5bb13a2c13189e948ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:18:25 +0000

    Add package skale.py/0.2.2/skale-py-0.2.2.tar.gz

---
+++ packages/skale.py/skale-py-0.2.2.tar.gz/skale-py-0.2.2/skale/contracts/token.py
@@ -0,0 +1,7 @@
+from skale.contracts import BaseContract
+import skale.utils.helper as Helper
+
+class Token(BaseContract):
+  def __init__(self, skale):
+    name = 'token'
+    super().__init__(skale, name, skale.abi[f'{name}_address'], skale.abi[f'{name}_abi'] )

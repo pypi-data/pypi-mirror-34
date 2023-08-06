e83b1c0878e783c4a82e270e8c8d0b76150bbdab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:13 +0000

    Add package alphatwirl/0.18.6/alphatwirl-0.18.6.tar.gz

---
+++ packages/alphatwirl/alphatwirl-0.18.6.tar.gz/alphatwirl-0.18.6/alphatwirl/cmsedm/EventBuilderConfig.py
@@ -0,0 +1,12 @@
+# Tai Sakuma <tai.sakuma@gmail.com>
+
+##__________________________________________________________________||
+import collections
+
+##__________________________________________________________________||
+EventBuilderConfig = collections.namedtuple(
+    'EventBuilderConfig',
+    'inputPaths maxEvents start dataset name'
+)
+
+##__________________________________________________________________||

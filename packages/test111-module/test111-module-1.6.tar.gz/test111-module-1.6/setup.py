9f8afdc609466eaaba4910552089da4d6d483a34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:34 +0000

    Add package test111-module/1.6/test111-module-1.6.tar.gz

---
+++ packages/test111-module/test111-module-1.6.tar.gz/test111-module-1.6/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(name='test111-module',
+	version='1.6',
+	py_modules=['soap'],
+	ext_packages=['suds-py3'],
+	#url = "deneme.com",
+	author = "ahmetatakan",
+	maintainer = "ahmetatakan",
+	author_email = "ahmetatakan79@gmail.com",
+	maintainer_email = "ahmetatakan79@gmail.comm",
+)

ab384b5e9236a64bd1712caf256aa8605da931b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:15 +0000

    Add package vulcan-builder/0.2.1/vulcan-builder-0.2.1.tar.gz

---
+++ packages/vulcan-builder/vulcan-builder-0.2.1.tar.gz/vulcan-builder-0.2.1/vulcan/builder/__init__.py
@@ -0,0 +1,17 @@
+'''
+Lightweight Python Build Tool
+'''
+
+from vulcan.builder.common import nsh, dump, safe_cd
+from ._vb import task, async_task, main
+import sh
+import pkgutil
+
+__path__ = pkgutil.extend_path(__path__, __name__)
+
+__all__ = [
+    'task', 'async_task',
+    'main',
+    'nsh', 'sh',
+    'dump', 'safe_cd'
+  ]

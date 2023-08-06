9d575d73a7762f29a52943db63f94b9623855040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:37 +0000

    Add package django-file-form/0.4.0/django_file_form-0.4.0-py2.7.egg

---
+++ packages/django-file-form/django_file_form-0.4.0-py2.7.egg/django_file_form/util.py
@@ -0,0 +1,30 @@
+from django.core.exceptions import ImproperlyConfigured
+from django.db import models
+from django.utils.module_loading import import_string
+
+from . import conf
+
+
+class ModelManager(models.Manager):
+    def try_get(self, **kwargs):
+        qs = self.get_queryset().filter(**kwargs)
+        if qs.exists():
+            return qs.get()
+        else:
+            return None
+
+
+def get_list(v):
+    if isinstance(v, list):
+        return v
+    else:
+        return [v]
+
+
+def load_class(setting_string):
+    try:
+        return import_string(getattr(conf, setting_string))
+    except ImportError:
+        raise ImproperlyConfigured(
+            "{0!s} refers to a class '{1!s}' that is not available".format(setting_string, getattr(conf, setting_string))
+        )

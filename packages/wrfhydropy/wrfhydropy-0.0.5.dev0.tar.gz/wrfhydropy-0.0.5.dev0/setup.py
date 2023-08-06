93a5183481d895b64db31594028f97c1d083283f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:18 +0000

    Add package wrfhydropy/0.0.5.dev0/wrfhydropy-0.0.5.dev0.tar.gz

---
+++ packages/wrfhydropy/wrfhydropy-0.0.5.dev0.tar.gz/wrfhydropy-0.0.5.dev0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='wrfhydropy',
+    version='0.0.5dev0',
+    packages=find_packages(),
+    package_data={'wrfhydropy': ['core/data/*']},
+    url='https://github.com/NCAR/wrf_hydro_py',
+    license='MIT',
+    install_requires=['pandas',
+                      'f90nml',
+                      'deepdiff',
+                      'pathlib',
+                      'xarray',
+                      'datetime',
+                      'pytest',
+                      'pytest-datadir-ng'],
+    author='Joe Mills',
+    author_email='jmills@ucar.edu',
+    description='Crude API for the WRF-Hydro model',
+)
---
+++ packages/wrfhydropy/wrfhydropy-0.0.5.dev0.tar.gz/wrfhydropy-0.0.5.dev0/wrfhydropy/__init__.py
@@ -0,0 +1,7 @@
+from .core.domain import *
+from .core.job import *
+from .core.model import *
+from .core.simulation import *
+from .core import schedulers
+from .core import outputdiffs
+from .core import ioutils

af3a0742bab3b14b75b435d4053d94dcdb536f1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:47 +0000

    Add package tspy/0.1.1/tspy-0.1.1.tar.gz

---
+++ packages/tspy/tspy-0.1.1.tar.gz/tspy-0.1.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+setuptools.setup(
+    name="tspy",
+    version="0.1.1",
+    url="https://github.com/wborgeaud/tspy",
+
+    author="William Borgeaud",
+    author_email="williamborgeaud@gmail.com",
+
+    description="An optimization package for the traveling salesman problem",
+    long_description=open('README.md').read(),
+
+    packages=setuptools.find_packages(),
+
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)

bd8e170d86a794e9c4cc75e00609c7e0fd09c431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:50 +0000

    Add package fluxamasynth/1.0/fluxamasynth-1.0.tar.gz

---
+++ packages/fluxamasynth/fluxamasynth-1.0.tar.gz/fluxamasynth-1.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fluxamasynth",
+    version="1.0",
+    author="Modern Device",
+    author_email="shawn@as220.org",
+    description="A library for the Modern Device Fluxamasynth board.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fluxly/fluxamasynthPi",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)

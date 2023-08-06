2e90d2e33cff2b2d3ab1316e8bd89adf771df4d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:00 +0000

    Add package kcm/1.9/kcm-1.9.tar.gz

---
+++ packages/kcm/kcm-1.9.tar.gz/kcm-1.9/kcm/views.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from django.http import JsonResponse
+from djangoApiDec.djangoApiDec import queryString_required
+from kcm import KCM
+from udic_nlp_API.settings_database import uri
+
+multilanguage_model = {
+	'zh': KCM(lang='zh', uri=uri, ngram=True),
+	'th': KCM(lang='th', uri=uri, ngram=True)
+}
+
+@queryString_required(['lang', 'keyword'])
+def kcm(request):
+	keyword = request.GET['keyword']
+	lang = request.GET['lang']
+	result = multilanguage_model[lang].get(keyword=keyword, amount=int(request.GET['num']) if 'num' in request.GET else 10, keyFlag=request.GET['keyFlag'].split() if 'keyFlag' in request.GET else [], valueFlag=request.GET['valueFlag'].split() if 'valueFlag' in request.GET else [])
+	return JsonResponse(result, safe=False)
+
+@queryString_required(['lang', 'keyword'])
+def search(request):
+	keyword = request.GET['keyword']
+	lang = request.GET['lang']
+	threshold = float(request.GET['threshold']) if 'threshold' in request.GET else 0
+	num = int(request.GET['num']) if 'num' in request.GET else 10
+	return JsonResponse(multilanguage_model[lang].kcmNgram.search(keyword, threshold=threshold)[:num], safe=False)

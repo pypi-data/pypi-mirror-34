bef4c82c7169b38a98d345d7d26f175e92d22fe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:54 +0000

    Add package verkhovna_rada_ukr_pl/0.2.dev0/verkhovna_rada_ukr_pl-0.2.dev0.tar.gz

---
+++ packages/verkhovna_rada_ukr_pl/verkhovna_rada_ukr_pl-0.2.dev0.tar.gz/verkhovna_rada_ukr_pl-0.2.dev0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.2'
+
+setup(name='verkhovna_rada_ukr_pl',
+      version=version,
+      description="Ukrainian and Poland kind of parlament",
+      long_description="""\
+some long desc""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Oleg M',
+      author_email='oleg.matviiv@gmail.com',
+      url='',
+      license='GPL Free License',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points={
+            'console_scripts': [
+                  'rada = vekrhovna_rada_urk_pl.verh_rada:main'
+            ]
+      }
+
+      )
---
+++ packages/verkhovna_rada_ukr_pl/verkhovna_rada_ukr_pl-0.2.dev0.tar.gz/verkhovna_rada_ukr_pl-0.2.dev0/verkhovna_rada_ukr_pl/verh_rada.py
@@ -0,0 +1,14 @@
+from verkhovna_rada_ukr_pl.rada_factories import UkraineRadaFactory, PolandRadaFactory
+
+simulation_config_file = {'rada': 'Poland'}
+
+
+def main():
+    if simulation_config_file['rada'] == 'Ukraine':
+        UkraineRadaFactory()().run()
+    else:
+        PolandRadaFactory()().run()
+
+
+if __name__ == "__main__":
+    main()

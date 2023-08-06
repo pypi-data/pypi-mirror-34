741022dcbd6ce8c0167f5fa1250a34e1bdf9c8a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:28 +0000

    Add package iap/2.1.0/iap-2.1.0.tar.gz

---
+++ packages/iap/iap-2.1.0.tar.gz/iap-2.1.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+from setuptools import setup
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py register sdist bdist_wheel upload')
+    sys.exit()
+
+
+setup(
+    name='iap',
+    version='2.1.0',
+    description='Python utilities for working with Apple In-App Purchases (IAP)',
+    license="MIT",
+    keywords="iap appstore django",
+    author='Educreations Engineering',
+    author_email='engineering@educreations.com',
+    url='https://github.com/educreations/python-iap',
+    packages=["iap"],
+    package_dir={"iap": "iap"},
+    install_requires=['pycryptodome', 'Django>=1.7', 'requests'],
+)

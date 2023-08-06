2df80882be06e2c71657664c972a08cafa505754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:19 +0000

    Add package dicom2tar/0.1.1/dicom2tar-0.1.1.tar.gz

---
+++ packages/dicom2tar/dicom2tar-0.1.1.tar.gz/dicom2tar-0.1.1/README.md
@@ -0,0 +1,5 @@
+# dicom2tar
+
+Tool for extract compressed files(if any), sort dicom files according to given rule, or tar the sorted, to a destination directory. 
+
+Check dicom2tar.py for example.
---
+++ packages/dicom2tar/dicom2tar-0.1.1.tar.gz/dicom2tar-0.1.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+VERSION = "0.1.1"
+setuptools.setup(name='dicom2tar',
+                 version=VERSION,
+                 description="sort or tar CFMM' data with DicomSorter",
+                 author='YingLi Lu,Ali Khan',
+                 author_email='alik.khanlab@gmail.com,yinglilu@gmail.com',
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 url="https://github.com/khanlab/dicom2tar",
+                 license='GNU General Public License v3.0',
+                 entry_points={
+                     'console_scripts': [
+                         'dicom2tar = dicom2tar.main:run']},
+                 packages=setuptools.find_packages(),
+                 install_requires=['pydicom',
+                                   'extractCMRRPhysio', 'DicomRaw'],
+                 zip_safe=False)

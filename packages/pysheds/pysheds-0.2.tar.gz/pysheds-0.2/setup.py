c58c416bbdfe047f92e6d52966793271b683fed5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:05 +0000

    Add package pysheds/0.2/pysheds-0.2.tar.gz

---
+++ packages/pysheds/pysheds-0.2.tar.gz/pysheds-0.2/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pysheds',
+      version='0.2',
+      description='🌎 Simple and fast watershed delineation in python.',
+      author='Matt Bartos',
+      author_email='mdbartos@umich.edu',
+      url='http://open-storm.org',
+      packages=["pysheds"],
+     )

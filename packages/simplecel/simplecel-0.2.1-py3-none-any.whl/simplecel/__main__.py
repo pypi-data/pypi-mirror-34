063dc25d435c53085850884e32669cd2384958de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:30 +0000

    Add package simplecel/0.2.1/simplecel-0.2.1-py3-none-any.whl

---
+++ packages/simplecel/simplecel-0.2.1-py3-none-any.whl/simplecel/__main__.py
@@ -0,0 +1,27 @@
+import os
+import webbrowser
+import click
+
+from . import app
+
+
+@click.command()
+@click.argument('filename')
+@click.option('--config', default='', help='Please input the path to CONFIG yaml, as defined in pyhandsontable.')
+@click.option('--host', default='localhost')
+@click.option('--port', default=7500)
+@click.option('--debug', is_flag=True)
+def load_excel(filename, config, host, port, debug):
+    os.environ['FILENAME'] = filename
+    os.environ['CONFIG'] = config
+
+    app.run(
+        host=host,
+        port=port,
+        debug=debug
+    )
+    webbrowser.open('http://{}:{}'.format(host, port))
+
+
+if __name__ == '__main__':
+    load_excel()

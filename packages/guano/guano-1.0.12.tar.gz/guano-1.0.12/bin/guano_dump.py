e27655a130452bcc79bf06c5722a5b9c61448e0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:23 +0000

    Add package guano/1.0.12/guano-1.0.12.tar.gz

---
+++ packages/guano/guano-1.0.12.tar.gz/guano-1.0.12/bin/guano_dump.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+"""
+Print the GUANO metadata found in a file or files.
+
+usage::
+
+    $> guano_dump.py WAVFILE...
+"""
+
+from __future__ import print_function
+
+import sys
+import os
+import os.path
+
+from guano import GuanoFile
+
+
+def dump(fname):
+    print()
+    print(fname)
+    gfile = GuanoFile(fname, strict=False)
+    print(gfile.to_string())
+
+
+if __name__ == '__main__':
+    from glob import glob
+    import logging
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s\t%(levelname)s\t%(message)s')
+
+    if len(sys.argv) < 2:
+        print('usage: %s FILE...' % os.path.basename(sys.argv[0]), file=sys.stderr)
+        sys.exit(2)
+
+    if os.name == 'nt' and '*' in sys.argv[1]:
+        fnames = glob(sys.argv[1])
+    else:
+        fnames = sys.argv[1:]
+
+    for fname in fnames:
+        if os.path.isdir(fname):
+            for subfname in glob(os.path.join(fname, '*.[Ww][Aa][Vv]')):
+                dump(subfname)
+        else:
+            dump(fname)
---
+++ packages/guano/guano-1.0.12.tar.gz/guano-1.0.12/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+from glob import glob
+
+from guano import __version__
+
+
+setup(
+    name='guano',
+    version=__version__,
+    description='GUANO, the "Grand Unified" bat acoustics metadata format',
+    long_description=open('README.rst').read(),
+    url='https://github.com/riggsd/guano-py',
+    license='MIT',
+    author='David A. Riggs',
+    author_email='driggs@myotisoft.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='bats acoustics metadata guano',
+    py_modules=['guano'],
+    scripts=glob('bin/*.py'),
+)

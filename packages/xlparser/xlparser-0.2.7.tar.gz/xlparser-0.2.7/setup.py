4839371d821242d822121c4145a1486dec2fb0cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:32 +0000

    Add package xlparser/0.2.7/xlparser-0.2.7.tar.gz

---
+++ packages/xlparser/xlparser-0.2.7.tar.gz/xlparser-0.2.7/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from os import path as op
+from setuptools import setup
+version = "0.1.12"
+
+
+def _read(fname='README.md', line=None):
+    try:
+        if line is None:
+            return open(op.join(op.dirname(__file__), fname)).read()
+        return open(op.join(op.dirname(__file__), fname)).readlines()[line].strip()
+    except IOError:
+        return ''
+
+version = "0.2.7"
+
+setup(
+    name='xlparser',
+    author="ahuigo",
+    author_email="ahui132@qq.com",
+    license="MIT",
+    url="http://github.com/ahuigo/xlparser",
+    version=version,
+    python_requires='>=3.6.1',
+    packages=[],
+    package_dir={"": "."},
+    py_modules=['xlparser'],
+    install_requires=['xlrd', 'openpyxl>=2.5.4'],
+    scripts=['xlparser.py'],
+    description=_read(line=1),
+    long_description=_read(),
+    long_description_content_type="text/markdown",
+)

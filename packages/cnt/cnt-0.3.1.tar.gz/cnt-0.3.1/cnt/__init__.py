9318d399797a0b2030a6bf8949dfaa74bb95d316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:49 +0000

    Add package cnt/0.3.1/cnt-0.3.1.tar.gz

---
+++ packages/cnt/cnt-0.3.1.tar.gz/cnt-0.3.1/cnt/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for cnt."""
+
+__author__ = """Hunt Zhan"""
+__email__ = 'huntzhan.dev@gmail.com'
+__version__ = '0.3.1'
+
+
+import cnt_rulebase as rulebase  # noqa: F401

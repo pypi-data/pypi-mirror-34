2ae2a4cd848671d577951fdc7ade6377ad335388
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:52 +0000

    Add package cal-ai/0.2.0.9/cal_ai-0.2.0.9-py2.py3-none-any.whl

---
+++ packages/cal-ai/cal_ai-0.2.0.9-py2.py3-none-any.whl/cal_ai-0.2.0.9.dist-info/DESCRIPTION.rst
@@ -0,0 +1,8 @@
+# project to monitor live boxes
+This is a project based on hadoop cal data, provide user the ability to explore boxes status and build models
+based on these data.
+
+### Tutorials
+http://10.176.5.126:9090/notebooks/cal_ai.ipynb
+
+
---
+++ packages/cal-ai/cal_ai-0.2.0.9-py2.py3-none-any.whl/cal_ai/__init__.py
@@ -0,0 +1,8 @@
+name = "cal_ai"
+
+from .ai_monitor import AIMonitor
+from .cal_crawler import CalCrawler, MachineInfo
+from .cal_plot import CalPlot
+from .common_utils import CommonUtils
+from .data_template import DataType, UrlTemplate
+from .healthy_checker import HealthyChecker

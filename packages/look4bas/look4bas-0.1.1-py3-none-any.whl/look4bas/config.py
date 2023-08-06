113cf21b9fcb7ccdc84fbdf98a4a150b074a6e4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:38 +0000

    Add package look4bas/0.1.1/look4bas-0.1.1-py3-none-any.whl

---
+++ packages/look4bas/look4bas-0.1.1-py3-none-any.whl/look4bas/config.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+dbfile = "~/.local/share/look4bas/basis_sets.db"
+default_download_formats = ["Gaussian94"]
+list_formats = {
+    "default": ["crop", "noelements", "colour"],
+    "extra":   ["crop", "elements", "colour"]
+}

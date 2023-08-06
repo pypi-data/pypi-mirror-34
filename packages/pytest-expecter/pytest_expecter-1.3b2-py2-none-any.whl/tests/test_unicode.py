83499ac94097148291d2f87349058410ac4712f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:26 +0000

    Add package pytest-expecter/1.3b2/pytest_expecter-1.3b2-py2-none-any.whl

---
+++ packages/pytest-expecter/pytest_expecter-1.3b2-py2-none-any.whl/tests/test_unicode.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=unused-variable,expression-not-assigned,redefined-builtin,multiple-statements,bad-continuation
+
+
+from __future__ import unicode_literals
+
+import pytest
+from tests.utils import fail_msg
+
+from expecter import expect
+
+
+try:
+    import builtins as __builtins__
+except ImportError:
+    import __builtin__ as __builtins__
+
+unicode = getattr(__builtins__, 'unicode', str)
+
+
+def describe_expecter():
+
+    def it_shows_diff_when_unicode_strings_differ():
+        value = 'ueber\ngeek'
+        fixture = 'über\ngeek'
+        assert isinstance(value, unicode), "value is a " + repr(type(value))
+        assert isinstance(fixture, unicode), "fixture is a " + repr(type(fixture))
+        def _fails():
+            expect(value) == fixture
+        with pytest.raises(AssertionError):
+            _fails()
+        msg = ("Expected 'über\\ngeek' but got 'ueber\\ngeek'\n"
+               "Diff:\n"
+               "@@ -1,2 +1,2 @@\n"
+               "-über\n"
+               "+ueber\n"
+               " geek"
+               )
+        # Normalize real msg for differences in py2 and py3
+        real = fail_msg(_fails).replace("u'", "'").replace('\\xfc', '\xfc')
+        assert real == msg, '\n' + repr(real) + '\n' + repr(msg)

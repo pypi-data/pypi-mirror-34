8185363540d4bb7886bec4b08e085475a0c3c5b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:12 +0000

    Add package cantal/0.2.4/cantal-0.2.4.tar.gz

---
+++ packages/cantal/cantal-0.2.4.tar.gz/cantal-0.2.4/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.rst'), 'rt') as f:
+    long_description = f.read()
+
+
+setup(name='cantal',
+      version='0.2.4',
+      description='The library that submits metrics to cantal',
+      author='Paul Colomiets',
+      author_email='paul@colomiets.name',
+      url='http://github.com/tailhook/cantal-py',
+      packages=['cantal'],
+      package_data={"cantal": ["py.typed", "*.pyi"]},
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'License :: OSI Approved :: MIT License',
+          'License :: OSI Approved :: Apache Software License',
+      ],
+      long_description=long_description,
+      license="MIT/Apache-2.0",
+      )

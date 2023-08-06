c282be5da3b966802d91f28c7d7bb549af380833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:01 +0000

    Add package pandas-sql/0.1.10/pandas-sql-0.1.10.tar.gz

---
+++ packages/pandas-sql/pandas-sql-0.1.10.tar.gz/pandas-sql-0.1.10/pd_sql/__init__.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+
+@version: 0.1
+@author:  quantpy
+@file:    __init__.py
+@time:    2018/4/12 10:33
+"""
+
+
+from .db_model import MySqlModel, MsSqlModel
+
+
+__version__ = '0.1.10'

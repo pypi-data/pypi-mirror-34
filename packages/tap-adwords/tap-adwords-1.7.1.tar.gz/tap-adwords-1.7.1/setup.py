3465547571f5581bd78c2fabacf92a1ad9873150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:02 +0000

    Add package tap-adwords/1.7.1/tap-adwords-1.7.1.tar.gz

---
+++ packages/tap-adwords/tap-adwords-1.7.1.tar.gz/tap-adwords-1.7.1/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='tap-adwords',
+      version="1.7.1",
+      description='Singer.io tap for extracting data from the Adwords api',
+      author='Stitch',
+      url='http://singer.io',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['tap_adwords'],
+      install_requires=[
+          'singer-python==5.1.5',
+          'requests==2.13.0',
+          'googleads==10.1.0',
+          'suds-jurko==0.6'
+      ],
+      entry_points='''
+          [console_scripts]
+          tap-adwords=tap_adwords:main
+      ''',
+      packages=['tap_adwords'],
+      package_data = {
+          'tap_adwords/schemas': [
+              "accounts.json",
+              "ad_groups.json",
+              "ads.json",
+              "campaigns.json",
+          ],
+          'tap_adwords/metadata': [
+              "accounts.json",
+              "ad_groups.json",
+              "ads.json",
+              "campaigns.json",
+          ],
+      },
+      include_package_data=True,
+)

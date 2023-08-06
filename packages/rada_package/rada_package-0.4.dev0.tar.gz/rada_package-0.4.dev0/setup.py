7e3d7fb5c5f28bc30887f9f9e0f4166fe2f50bfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:37 +0000

    Add package rada_package/0.4.dev0/rada_package-0.4.dev0.tar.gz

---
+++ packages/rada_package/rada_package-0.4.dev0.tar.gz/rada_package-0.4.dev0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.4'
+
+setup(name='rada_package',
+      version=version,
+      description="Rada with abstract factory",
+      long_description="""\
+Ukraine and Poland radas with abstract factory""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='abstract factory python itea',
+      author='Taras Vaskiv',
+      author_email='',
+      url='',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )

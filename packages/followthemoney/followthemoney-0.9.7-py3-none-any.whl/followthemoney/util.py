9cc057a852964e15ebddcac804369ea0513ae002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:34 +0000

    Add package followthemoney/0.9.7/followthemoney-0.9.7-py3-none-any.whl

---
+++ packages/followthemoney/followthemoney-0.9.7-py3-none-any.whl/followthemoney/util.py
@@ -0,0 +1,51 @@
+from __future__ import unicode_literals
+
+import os
+from threading import local
+from normality import stringify
+from gettext import translation
+from banal import is_mapping, is_sequence
+from banal import unique_list, ensure_list
+
+DEFAULT_LOCALE = 'en'
+i18n_path = os.path.join(os.path.dirname(__file__), 'translations')
+state = local()
+
+
+def gettext(*args, **kwargs):
+    if not hasattr(state, 'translation'):
+        set_model_locale(DEFAULT_LOCALE)
+    return state.translation.gettext(*args, **kwargs)
+
+
+def set_model_locale(locale):
+    state.translation = translation('followthemoney', i18n_path, [locale],
+                                    fallback=True)
+
+
+def key_bytes(key):
+    """Convert the given data to a value appropriate for hashing."""
+    key = stringify(key) or ''
+    return key.encode('utf-8')
+
+
+def merge_data(old, new):
+    """Extend the values of the new doc with extra values from the old."""
+    if is_sequence(old) or is_sequence(new):
+        new = ensure_list(new)
+        new.extend(ensure_list(old))
+        if not len(new):
+            return None
+        return unique_list(new)
+    if is_mapping(old) or is_mapping(new):
+        old = old if is_mapping(old) else {}
+        new = new if is_mapping(new) else {}
+        keys = set(new.keys())
+        keys.update(old.keys())
+        combined = {}
+        for key in keys:
+            value = merge_data(old.get(key), new.get(key))
+            if value is not None:
+                combined[key] = value
+        return combined
+    return new or old

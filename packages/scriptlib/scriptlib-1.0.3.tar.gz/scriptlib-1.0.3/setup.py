a7aacaf1f411ad21e2d915f52f417b9aca689d71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:57 +0000

    Add package scriptlib/1.0.3/scriptlib-1.0.3.tar.gz

---
+++ packages/scriptlib/scriptlib-1.0.3.tar.gz/scriptlib-1.0.3/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name='scriptlib',
+    version='1.0.3',
+    description='A collection of functions for easy terminal application development',
+    url='https://github.com/Orbtial/scriptlib',
+    author='Orbtial',
+    author_email='thewardloch@gmail.com',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+    )
+    )

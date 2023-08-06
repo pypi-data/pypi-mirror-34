c3a0efc16d82c0d8191784fcae34de2f3681c324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:56 +0000

    Add package nesterHeHe/1.2.0/nesterHeHe-1.2.0.tar.gz

---
+++ packages/nesterHeHe/nesterHeHe-1.2.0.tar.gz/nesterHeHe-1.2.0/nesterHeHe.py
@@ -0,0 +1,18 @@
+'''This is "nester.py",a module that provides a function called print_lol(),
+ which is used to print lists, containing or not containing a nested list.
+ Indentation occurs when printing nested lists'''
+def print_lol(the_list,level=0):
+    '''This function has two parameters.The first one is "the_list",which can be
+   any Python list(can contain nested lists).The second one is "level",which was
+   used to insert a tab when a nested list is encountered.If you didn't give a
+   value to "level",the function will assign "level" to 0.If you give a appropriate
+   negative to "level",the indentation function can be shut down.Each item of the
+   specified list will output (recursively) to the screen, and each item occupies
+   one line'''
+    for i in the_list:
+        if isinstance(i,list):
+            print_lol(i,level+1)
+        else:
+            for j in range(level):
+                print('\t',end='')
+            print(i)

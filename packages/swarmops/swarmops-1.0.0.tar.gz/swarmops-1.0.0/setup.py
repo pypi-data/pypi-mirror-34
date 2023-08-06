031e91e9d08edc1959389fc9a414799718b7d54c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:11 +0000

    Add package swarmops/1.0.0/swarmops-1.0.0.tar.gz

---
+++ packages/swarmops/swarmops-1.0.0.tar.gz/swarmops-1.0.0/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+MY_VERSION = '1.0.0'
+
+setup(
+    name='swarmops',
+    packages=['swarmops'],
+    version=MY_VERSION,
+    description='Heuristic Optimization for Python',
+    author='Magnus Erik Hvass Pedersen',
+    author_email='my-first-name@hvass-labs.org',
+    url='https://github.com/Hvass-Labs/swarmops',
+#    download_url='git+https://github.com/Hvass-Labs/swarmops.git',
+    license='BSD',
+    keywords=['heuristic optimization', 'particle swarm optimization',
+              'differential evolution', 'meta-optimization'],
+    install_requires=[
+        'numpy>=1.9',
+        'scipy>=0.16',
+        'matplotlib>=1.5',
+    ],
+)

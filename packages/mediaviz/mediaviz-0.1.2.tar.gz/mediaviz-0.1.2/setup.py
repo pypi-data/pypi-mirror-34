d5c61d07780dda63ae9e8991a352eed7f755d0b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:48 +0000

    Add package mediaviz/0.1.2/mediaviz-0.1.2.tar.gz

---
+++ packages/mediaviz/mediaviz-0.1.2.tar.gz/mediaviz-0.1.2/setup.py
@@ -0,0 +1,43 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='mediaviz',
+      version='0.1.2',
+      description='Visualize Networks With Force Atlas 2 Layout',
+      long_description = long_description,
+          long_description_content_type="text/markdown",
+      url='https://github.com/Tahsin-Mayeesha/Mediaviz',
+      author='Tahsin Mayeesha',
+      author_email='tasmiah.tahsin@hotmail.com',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Mathematics',
+
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+
+    install_requires=[ "fa2l",
+        "networkx<2.0.0",
+        "numpy", "matplotlib",'python-louvain',"adjustText"
+    ],
+
+    setup_requires=[
+        'pytest-runner'
+    ],
+    tests_require=[
+        'pytest'
+    ],
+     
+      zip_safe=False)

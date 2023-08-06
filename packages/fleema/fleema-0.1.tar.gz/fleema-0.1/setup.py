0618ba573826ce5e70302003833e1e8646e1018d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:33 +0000

    Add package fleema/0.1/fleema-0.1.tar.gz

---
+++ packages/fleema/fleema-0.1.tar.gz/fleema-0.1/setup.py
@@ -0,0 +1,14 @@
+"""
+Setup for package
+"""
+from setuptools import setup
+
+setup(name='fleema',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['fleema'],
+      zip_safe=False)

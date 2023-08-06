4e87127cb42f51f6c320376f3fa515e98fdc6f68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:55 +0000

    Add package recastai/4.0.0/recastai-4.0.0-py2.py3-none-any.whl

---
+++ packages/recastai/recastai-4.0.0-py2.py3-none-any.whl/recastai/apis/connect/utils.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+
+class Utils():
+  """Endpoints"""
+  CONVERSATION_ENDPOINT = 'https://api.recast.ai/connect/v1/conversations/%s'
+  MESSAGE_ENDPOINT = 'https://api.recast.ai/connect/v1/messages/%s'

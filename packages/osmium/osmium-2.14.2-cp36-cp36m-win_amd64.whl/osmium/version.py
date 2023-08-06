a56196d56fe56ce486bfa23a69bfe14a54507183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:43 +0000

    Add package osmium/2.14.2/osmium-2.14.2-cp36-cp36m-win_amd64.whl

---
+++ packages/osmium/osmium-2.14.2-cp36-cp36m-win_amd64.whl/osmium/version.py
@@ -0,0 +1,13 @@
+"""
+Version information.
+"""
+
+# the major version
+pyosmium_major = '2.14'
+# current release (Pip version)
+pyosmium_release = '2.14.2'
+
+# libosmium version shipped with the Pip release
+libosmium_version = '2.14.2'
+# protozero version shipped with the Pip release
+protozero_version = '1.6.3'

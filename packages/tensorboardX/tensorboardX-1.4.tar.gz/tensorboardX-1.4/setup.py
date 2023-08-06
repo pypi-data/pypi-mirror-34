d3d2b300c90842cc110d7d7c655d8cdde30ca162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:45 +0000

    Add package tensorboardX/1.4/tensorboardX-1.4.tar.gz

---
+++ packages/tensorboardX/tensorboardX-1.4.tar.gz/tensorboardX-1.4/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'numpy',
+    'protobuf >= 3.2.0',
+    'six',
+]
+
+test_requirements = [
+    'pytest',
+    'matplotlib'
+]
+
+setup(
+    name='tensorboardX',
+    version='1.4',
+    description='TensorBoardX lets you watch Tensors Flow without Tensorflow',
+    long_description= history,
+    author='Tzu-Wei Huang',
+    author_email='huang.dexter@gmail.com',
+    url='https://github.com/lanpa/tensorboardX',
+    packages=find_packages(exclude=['docs', 'tests', 'examples']),
+    include_package_data=True,
+    install_requires=requirements,
+    license='MIT license',
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',        
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
+
+# python setup.py sdist bdist_wheel --universal
+# twine upload dist/*

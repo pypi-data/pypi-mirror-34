713b4d9b3d5c6751760e0991d9bfd39af1a97686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:35 +0000

    Add package autokeras/0.2.0/autokeras-0.2.0.tar.gz

---
+++ packages/autokeras/autokeras-0.2.0.tar.gz/autokeras-0.2.0/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='autokeras',
+    packages=['autokeras'],  # this must be the same as the name above
+    install_requires=['torch==0.4.0', 'torchvision==0.2.1', 'numpy==1.14.5', 'keras', 'scikit-learn==0.19.1', 'tensorflow'],
+    version='0.2.0',
+    description='Automated Machine Learning with Keras',
+    author='Haifeng Jin',
+    author_email='jhfjhfj1@gmail.com',
+    url='http://autokeras.com',
+    download_url='https://github.com/jhfjhfj1/autokeras/archive/0.2.0.tar.gz',
+    keywords=['automl'],  # arbitrary keywords
+    classifiers=[]
+)

f3a199cd8ed74c28fa8912553e230ed92f7e5f45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:51 +0000

    Add package MouseTools/1.1.0/MouseTools-1.1.0.tar.gz

---
+++ packages/MouseTools/MouseTools-1.1.0.tar.gz/MouseTools-1.1.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="MouseTools",
+    version="1.1.0",
+    author="Scott Caratozzolo",
+    author_email="scaratozzolo12@gmail.com",
+    description="A Python wrapper for the Disney API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/scaratozzolo/MouseTools",
+    packages=setuptools.find_packages(),
+    install_requires=["tqdm", "requests"],
+    classifiers=(
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ),
+)

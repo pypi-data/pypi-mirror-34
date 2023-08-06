75b7a42283a8a619e0285875a5224a4502f13cd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:31 +0000

    Add package uproxy/0.3.99.11/uproxy-0.3.99.11.tar.gz

---
+++ packages/uproxy/uproxy-0.3.99.11.tar.gz/uproxy-0.3.99.11/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+  name = 'uproxy',
+  packages = ['uproxy'],
+  version = '0.3.99.11',
+  description = 'An ipv4/ipv6 suppported http proxy server',
+  long_description = open('readme.rst').read(), # need to close?
+  author = 'Cong Zhang',
+  author_email = 'congzhangzh@gmail.com',
+  url = 'https://github.com/medlab/uproxy',
+  keywords = ['http','https', 'ipv4/ipv6', 'proxy'],
+  classifiers=[
+    'License :: OSI Approved :: BSD License',
+    'Development Status :: 6 - Mature',
+    'Environment :: Console',
+    'Topic :: Utilities',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
---
+++ packages/uproxy/uproxy-0.3.99.11.tar.gz/uproxy-0.3.99.11/uproxy/__main__.py
@@ -0,0 +1,2 @@
+from .UProxy import main
+main()

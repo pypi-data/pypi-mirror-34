c04376523a891073f7cd82019ce145b46231f932
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:09 +0000

    Add package capyt/1.1.dev1/capyt-1.1.dev1.tar.gz

---
+++ packages/capyt/capyt-1.1.dev1.tar.gz/capyt-1.1.dev1/README.md
@@ -0,0 +1,23 @@
+# A python3 CLI utility to interface with [pastething](https://github.com/lbatalha/pastething)
+
+Example: https://cpy.pt
+
+## Dependencies
+
+* python3
+* requests
+
+## Installation
+
+* install dependencies
+* install to local system however you want 
+* adjust defaults and options if required
+
+## Extra Features
+
+* Transparently open gzip and bzip compressed files
+
+## Warning
+
+The optional encoding flag only serves to open the file, internally everything is handled and converted to utf-8.
+Retrieved pastes are utf-8 encoded.
---
+++ packages/capyt/capyt-1.1.dev1.tar.gz/capyt-1.1.dev1/capyt/capyt.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+import requests
+import sys, fileinput, argparse, copy
+from urllib.parse import urlparse
+from os.path import splitext
+if __name__ == '__main__':	
+	from __init__ import __version__
+else:
+	from . import __version__
+
+
+url = 'https://cpy.pt/'
+
+headers = {
+		'User-Agent': 'capyt/{}'.format(__version__,)
+		}
+
+parser = argparse.ArgumentParser(description="CLI Utility to interact with pastething")
+parser.add_argument('-v', '--version', action='version', version=__version__)
+parser.add_argument('-d', '--delete', help="Delete paste with supplied delete token")
+parser.add_argument('-b', '--burn', type=int, help="Maximum number of paste views before deletion")
+parser.add_argument('-l', '--lexer', help="Lexer to use (pygments get_lexer_by_name)", default="auto")
+parser.add_argument('-t', '--ttl', type=float, help="Time in hours before paste expires", default=1.0)
+parser.add_argument('-r', '--raw', help="Weather to have pastebin return raw or html paste url", default='false')
+parser.add_argument('-e', '--encoding', help="Define file encoding for given file (default: utf-8)")
+
+parser.add_argument('FILE', help="Files to upload", nargs="+") #Re-used as paste ID or URL when deleting
+args = vars(parser.parse_args())
+
+#add encoding option to compressed hook
+def hook_compressed_encoded(encoding):
+	def hook_compressed(filename, mode):
+		ext = splitext(filename)[1]
+		if ext == '.gz':
+			import gzip
+			return gzip.open(filename, mode, encoding=encoding)
+		elif ext == '.bz2':
+			import bz2
+			return bz2.BZ2File(filename, mode, encoding=encoding)
+		else:
+			return open(filename, mode, encoding=encoding)
+	return hook_compressed
+
+def main(args, url, headers):
+	if args['delete']:
+		paste = urlparse(args['FILE'][0])
+		if paste[0] != '':
+			url = args['FILE'][0]
+		else:
+			url = url + paste[2]
+
+		print("DELETE ", url)
+		try:
+			r = requests.delete(url, data={"token": args['delete']}, headers=headers)
+			print(r.text)
+			if r.status_code != 200:
+				exit(1)
+		except requests.ConnectionError:
+			print("Connection to host failed")
+			exit(2)
+	else:
+		parameters = copy.deepcopy(args)
+		del parameters['FILE']
+
+		with fileinput.input(files=args['FILE'], openhook=hook_compressed_encoded(args['encoding'])) as f:
+			parameters['paste'] = ''.join(f)
+			try:
+				r = requests.post(url, data=parameters, headers=headers)
+				print(r.text)
+				if r.status_code != 200:
+					exit(1)
+			except requests.ConnectionError:
+				print("Connection to host failed")
+				exit(2)
+
+main(args, url, headers)

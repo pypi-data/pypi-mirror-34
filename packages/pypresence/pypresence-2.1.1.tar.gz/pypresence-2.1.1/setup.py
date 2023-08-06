b5f4f92d17ec01110be95cb667dfba92c46f3adf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:54 +0000

    Add package pypresence/2.1.1/pypresence-2.1.1.tar.gz

---
+++ packages/pypresence/pypresence-2.1.1.tar.gz/pypresence-2.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pypresence',
+      author='qwertyquerty',
+      url='https://github.com/qwertyquerty/pypresence',
+      version='2.1.1',
+      packages=['pypresence'],
+      license='MIT',
+      description='Discord RPC client written in python',
+	  long_description="A Discord RPC library in python! Wow! Looks like you've come to the right place. Just looking to do Rich Presence? We Got you covered."
+)

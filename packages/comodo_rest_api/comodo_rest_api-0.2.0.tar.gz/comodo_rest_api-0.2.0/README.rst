fd9590ae56826677ff165453a2304616fe6986f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:58 +0000

    Add package comodo_rest_api/0.2.0/comodo_rest_api-0.2.0.tar.gz

---
+++ packages/comodo_rest_api/comodo_rest_api-0.2.0.tar.gz/comodo_rest_api-0.2.0/README.rst
@@ -0,0 +1,7 @@
+comodo_rest_api provides a wrapper around the Comodo REST API to allow for simple(r) interaction
+with their API.
+
+All responses are in jsend format for simplicity: https://labs.omniti.com/labs/jsend
+
+
+
---
+++ packages/comodo_rest_api/comodo_rest_api-0.2.0.tar.gz/comodo_rest_api-0.2.0/setup.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = 'comodo_rest_api'
+DESCRIPTION = 'Simpler python access to the Comodo CCM REST API'
+URL = 'https://github.com/erinn/comodo_rest_api'
+EMAIL = 'erinn.looneytriggs@gmail.com'
+AUTHOR = 'Erinn Looney-Triggs'
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    'pyjsend',
+    'requests',
+]
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.rst' is present in your MANIFEST.in file!
+with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+
+class PublishCommand(Command):
+    """Support setup.py publish."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        sys.exit()
+
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    description=DESCRIPTION,
+    long_description=long_description,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    keywords=['comodo-rest-api'],
+    packages=find_packages(exclude=('tests', 'docs', 'etc')),
+    install_requires=REQUIRED,
+    use_scm_version=True,
+    setup_requires=['setuptools_scm', 'twine'],
+    extras_require={
+        'test': ['betamax', 'pytest'],
+    },
+    python_requires='>=3.4, <4',
+    include_package_data=True,
+    license='AGPL-3.0',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    # $ setup.py publish support.
+    cmdclass={
+        'publish': PublishCommand,
+    },
+)

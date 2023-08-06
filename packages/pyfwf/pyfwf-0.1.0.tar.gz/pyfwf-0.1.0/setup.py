eea8b4c52baedfbf871d70b39aa47ce4101633ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:59 +0000

    Add package pyfwf/0.1.0/pyfwf-0.1.0.tar.gz

---
+++ packages/pyfwf/pyfwf-0.1.0.tar.gz/pyfwf-0.1.0/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+__author__ = 'Kelson da Costa Medeiros <kelsoncm@gmail.com>'
+
+setup(
+    name='pyfwf',
+    packages=['pyfwf',],
+    version='0.1.0',
+    download_url='https://github.com/kelsoncm/pyfwf/releases/tag/0.1.0',
+    description='Python library to manipulate fixed width file',
+    long_description=open('README.md', 'rt').read(),
+    author='Kelson da Costa Medeiros',
+    author_email='kelsoncm@gmail.com',
+    url='https://github.com/kelsoncm/pyfwf',
+    keywords=['python', 'file', 'fixed', 'width', ],
+    classifiers=[]
+)

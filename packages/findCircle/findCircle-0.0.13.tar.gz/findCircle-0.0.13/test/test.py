2be55666e845c9d3ea3a0e22322181bcb872c56a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:18 +0000

    Add package findCircle/0.0.13/findCircle-0.0.13.tar.gz

---
+++ packages/findCircle/findCircle-0.0.13.tar.gz/findCircle-0.0.13/test/test.py
@@ -0,0 +1,7 @@
+from findCircle import findCircle
+points=findCircle.intercept([(0,0,4),(8,0,4),(4,4,4)])
+print(points)
+
+findCircle.plotIntercept([(0,0,4),(8,0,4),(4,4,4)])
+findCircle.plotIntercept([(0,0,4),(8,0,4),(4,4,4)])
+

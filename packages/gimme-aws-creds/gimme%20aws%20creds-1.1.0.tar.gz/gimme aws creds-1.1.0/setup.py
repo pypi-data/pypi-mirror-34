7251005d941b9ffd647470fbf6bbbf97f57bdb26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:08 +0000

    Add package gimme-aws-creds/1.1.0/gimme%20aws%20creds-1.1.0.tar.gz

---
+++ packages/gimme-aws-creds/gimme%20aws%20creds-1.1.0.tar.gz/gimme aws creds-1.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+import gimme_aws_creds
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='gimme aws creds',
+    version=gimme_aws_creds.version,
+    install_requires=requirements,
+    author='Ann Wallace',
+    author_email='ann.wallace@nike.com',
+    description="A CLI to get temporary AWS credentials from Okta",
+    url='https://github.com/Nike-Inc/gimme-aws-creds',
+    license='Apache License, v2.0',
+    packages=find_packages(exclude=('tests', 'docs')),
+    test_suite="tests",
+    scripts=['bin/gimme-aws-creds', 'bin/gimme-aws-creds.py'],
+)

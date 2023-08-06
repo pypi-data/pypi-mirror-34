01c1d70d65b55550e34c0c28b351d5e23227781c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:32 +0000

    Add package luko_rada_package_1/0.6.dev0/luko_rada_package_1-0.6.dev0.tar.gz

---
+++ packages/luko_rada_package_1/luko_rada_package_1-0.6.dev0.tar.gz/luko_rada_package_1-0.6.dev0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.6'
+
+setup(name='luko_rada_package_1',
+      version=version,
+      description="Rada with abstractfactory",
+      long_description="""\
+Ukraine and Poland radas with abstract factory""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='abstract factory python itea',
+      author='Luko Kopach',
+      author_email='',
+      url='',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points={
+            'console_scripts': [
+                  'luko_rada = luko_rada_package_1.verh_rada:main',
+            ]
+      },
+      )

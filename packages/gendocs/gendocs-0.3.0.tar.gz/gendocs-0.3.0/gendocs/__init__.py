6bfd586a7f342bcedd2b702fc0d8da2d1a377f54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:25 +0000

    Add package gendocs/0.3.0/gendocs-0.3.0.tar.gz

---
+++ packages/gendocs/gendocs-0.3.0.tar.gz/gendocs-0.3.0/gendocs/__init__.py
@@ -0,0 +1,11 @@
+"""``gendocs``: automatically build documentation pages for a Python package.
+"""
+
+from .generator import *
+
+__author__ = 'Bane Sullivan'
+__license__ = 'BSD-3-Clause'
+__copyright__ = '2018, Bane Sullivan'
+__version__ = '0.3.0'
+__displayname__ = 'gendocs'
+__name__ = 'gendocs'

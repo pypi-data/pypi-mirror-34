9dae607d57e031a4bebcaf64878e3dc7ef2c70f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:29 +0000

    Add package easy_net_tf/0.0.39/easy_net_tf-0.0.39.tar.gz

---
+++ packages/easy_net_tf/easy_net_tf-0.0.39.tar.gz/easy_net_tf-0.0.39/easy_net_tf/utility/tf.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+
+
+class UtilityTf:
+    @staticmethod
+    def latest_checkpoint(model_dir=None, initial_dir='', hint=''):
+        if model_dir is None:
+            import tkinter
+            import tkinter.filedialog
+
+            r = tkinter.Tk()
+            r.withdraw()
+            model_dir = tkinter.filedialog.askdirectory(title=hint, initialdir=initial_dir)
+            if type(model_dir) == tuple:
+                model_dir = ''
+
+        return tf.train.latest_checkpoint(model_dir)
+
+
+if __name__ == '__main__':
+    path = UtilityTf.latest_checkpoint(hint='hello world')
+
+    print(path)

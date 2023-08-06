48529cc637d1d2547a042646785d96b09a65f3cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:59 +0000

    Add package zaggregator/0.0.10/zaggregator-0.0.10.tar.gz

---
+++ packages/zaggregator/zaggregator-0.0.10.tar.gz/zaggregator-0.0.10/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="zaggregator",
+    version="0.0.10",
+    author="Denis Barov",
+    author_email="dindin+zaggregator@dindin.ru",
+    description="Per-process stat monitoring solution for Zabbix",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fordindin/zaggregator",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    keywords="zabbix monitoring",
+    python_requires='>=3',
+    install_requires=['fuzzywuzzy>=0.16.0', 'psutil>=5.4.5', 'python-Levenshtein>=0.12.0', 'setproctitle>=1.1.10'],
+    packages=setuptools.find_packages(exclude=['contrib', 'docs', 'tests*', 'venv', 'misc', 'build', 'dist']),
+    zip_safe=True,
+    entry_points={
+        'console_scripts': [
+            'zaggregator = zaggregator.daemon:start',
+            'zcheck = zaggregator.client:main',
+        ],},
+    data_files=[
+        ('/var/run/zaggregator', []),
+        ('/usr/local/share/zaggregator', ['misc/zaggregator.service', 'misc/zaggregator.conf', 'README.md', 'LICENSE',]),
+        ('/usr/local/share/zaggregator/init.d', ['misc/init.d/zaggregator'],),
+            ],
+
+)

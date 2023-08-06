fd1e4dc056b8a5d6ecd2fc44c75a8fa8d92aef28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:58 +0000

    Add package kvvliveapi/0.1.1/kvvliveapi-0.1.1.tar.gz

---
+++ packages/kvvliveapi/kvvliveapi-0.1.1.tar.gz/kvvliveapi-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='kvvliveapi',
+    version='0.1.1',
+    scripts=['kvvliveapi/kvvliveapi.py'],
+    packages=['kvvliveapi'],
+
+    author='Nervengift',
+    author_email='github@nervengiftlabs.de',
+    description='KVV live API bindings',
+    keywords='kvv api',
+    url='https://github.com/Nervengift/kvvliveapi'
+)

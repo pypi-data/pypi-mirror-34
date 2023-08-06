b8e2b7ef6fd5920fedf5688100878ae460e7482a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:31 +0000

    Add package PELive-Data-Test/0.1.dev0/PELive%20Data%20Test-0.1dev.tar.gz

---
+++ packages/PELive-Data-Test/PELive%20Data%20Test-0.1dev.tar.gz/PELive Data Test-0.1dev/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='PELive Data Test',
+    version='0.1dev',
+    packages=['data_test'],
+    license='MIT',
+    url='http://www.google.com',
+    long_description=open('README.md').read(),
+    install_requires=['requests', 'pandas', 'pandas']
+)

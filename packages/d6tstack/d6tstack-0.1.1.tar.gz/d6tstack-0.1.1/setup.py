e609bc56aa4083be35d387c219c444e847e67a75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:17 +0000

    Add package d6tstack/0.1.1/d6tstack-0.1.1.tar.gz

---
+++ packages/d6tstack/d6tstack-0.1.1.tar.gz/d6tstack-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='d6tstack',
+    version='0.1.1',
+    packages=['d6tstack'],
+    url='https://github.com/d6t/d6tstack',
+    download_url = 'https://github.com/d6t/d6tstack/archive/0.1.1.tar.gz',
+    license='MIT',
+    author='DataBolt Team',
+    author_email='support@databolt.tech',
+    description='Databolt Python Library',
+    long_description='Databolt python library - accelerate data engineering. '
+                     'DataBolt provides tools to reduce the time it takes to get your data ready for '
+                     'evaluation and analysis.',
+    install_requires=[
+        'numpy',
+        'pandas',
+        'openpyxl',
+        'xlrd',
+        'xlwt'
+    ],
+    include_package_data=True,
+    python_requires='>=3.6',
+    keywords=['d6tstack', 'fast-data-evaluation'],
+    classifiers=[]
+)

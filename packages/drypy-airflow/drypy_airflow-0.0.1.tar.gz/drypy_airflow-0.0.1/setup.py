bb77338e2b6601108760ea38582521f9bcb803e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:32 +0000

    Add package drypy-airflow/0.0.1/drypy_airflow-0.0.1.tar.gz

---
+++ packages/drypy-airflow/drypy_airflow-0.0.1.tar.gz/drypy_airflow-0.0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="drypy_airflow",
+    version="0.0.1",
+    author="dhan16",
+    author_email="chandan.nath@gmail.com",
+    description="Airflow extensions",
+    url="https://github.com/dhan16/drypy-airflow",
+    packages=setuptools.find_packages(),
+    license="Apache License Version 2.0, January 2004",
+)

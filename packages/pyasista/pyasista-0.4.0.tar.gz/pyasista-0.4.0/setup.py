76eabe829dabea3a053a63f9ae0966526a4ae2bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:57 +0000

    Add package pyasista/0.4.0/pyasista-0.4.0.tar.gz

---
+++ packages/pyasista/pyasista-0.4.0.tar.gz/pyasista-0.4.0/setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import os
+import setuptools
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open('README.md', 'r', encoding='utf-8') as long_description_file:
+    long_description = long_description_file.read()
+
+about = {}
+with open(os.path.join(here, 'pyasista', '__version__.py'), 'r',
+          encoding='utf-8') as about_file:
+    exec(about_file.read(), about)
+
+packages = ['pyasista']
+
+setuptools.setup(
+    name=about['__title__'],
+    version=about['__version__'],
+    author=about['__author__'],
+    description=about['__description__'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url=about['__url__'],
+    packages=packages,
+    license=about['__license__'],
+    python_requires='>=3.4',
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers'
+    ),
+)

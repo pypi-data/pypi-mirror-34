cf9895331dbaf21dd6d13227b0e890f4f4b78026
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:30 +0000

    Add package multipy/0.12/multipy-0.12.tar.gz

---
+++ packages/multipy/multipy-0.12.tar.gz/multipy-0.12/multipy/permutation.py
@@ -0,0 +1,158 @@
+# -*- coding: utf-8 -*-
+"""Permutation test based corrections for multiple testing.
+
+Author: Tuomas Puoliväli
+Email: tuomas.puolivali@helsinki.fi
+Last modified: 20th July 2018
+License: 3-clause BSD
+
+TODO:
+
+- Permutation testing for (time, sensor) tuples.
+- Permutation testing for (time, frequency) tuples.
+- Permutation testing for (time, dipole) tuples.
+
+The definition of adjacency can be different in each case and needs to be
+thought first. In [1] the authors chose <= 4 cm distance for two
+(time, sensor) tuples to be adjacent.
+
+References:
+
+[1] Maris E, Oostenveld R (2007): Nonparametric statistical testing of EEG-
+    and MEG-data. Journal of Neuroscience Methods 164(1):177-190.
+
+WARNING: work in progress.
+
+"""
+
+import matplotlib.pyplot as plt
+
+import numpy as np
+from numpy import arange
+from numpy.random import normal, permutation
+
+from scipy.stats import ttest_ind
+
+import seaborn as sns
+
+def _sensor_adjacency(raw, threshold=4):
+    """Function for computing sensor adjacencies using Euclidean
+    distance. The default 4 cm threshold is the one chosen in
+    reference [1]."""
+
+    """Get channel locations."""
+    loc = np.asarray([ch['loc'][0:3] for ch in raw.info['chs']],
+                     dtype='float')
+    #ch_names = np.asarray([ch['ch_name'] for ch in raw.info['chs']])
+
+def _cluster_by_adjacency(sel_samples):
+    """Function for clustering selected samples based on temporal adjacency.
+
+    Input arguments:
+    sel_samples - A vector of booleans indicating which samples have been
+                  selected.
+
+    Output arguments:
+    clusters    - A vector of cluster numbers indicating to which cluster
+                  each sample belongs to. The cluster number zero corresponds
+                  to samples that were not selected.
+    """
+    clusters = np.zeros(len(sel_samples), dtype='int')
+    cluster_number = 1 # Next cluster number.
+    for i, s in enumerate(sel_samples):
+        if (s == True):
+            clusters[i] = cluster_number
+        else:
+            # Update the cluster number at temporal discontinuities.
+            if (i > 0 and sel_samples[i-1] == True):
+                cluster_number += 1
+    return clusters
+
+def _cluster_stat(stat, clusters, statistic='cluster_mass'):
+    """Function for computing the cluster mass statistic.
+
+    Input arguments:
+    stat      - Student's T or some other statistic for each variable.
+    clusters  - A vector of cluster numbers grouping the test statistics.
+    statistic - The computed cluster-level statistic. For now the only
+                available option is 'cluster_mass'. TODO: implement
+                alternatives.
+
+    Output arguments:
+    cstat    - The cluster mass statistic.
+    """
+    if (statistic == 'cluster_mass'):
+        n_clusters = np.max(clusters)
+        if (n_clusters == 0):
+            return np.nan
+        else:
+            cstat = [np.sum(stat[clusters == i]) for i in
+                                                 arange(1, n_clusters+1)]
+            return np.max(cstat)
+    else:
+        raise NotImplementedError('Option \'%s\' for input argument' +
+                                  ' is not available' % statistic)
+
+def permutation_test(X, Y, n_permutations=1000, threshold=1, tail='both'):
+    """Permutation test for a significant difference between two sets of data.
+
+    Input arguments:
+    X              - An N_1-by-m matrix (NumPy ndarray) where N is the number
+                     of samples and m is the number of variables. The first
+                     group of data.
+    Y              - An N_2-by-m matrix. The second group of data.
+    n_permutations - Number of permutations. Consider how accurate p-value
+                     is needed.
+    threshold      -
+    tail           - Only two-sided tests are supported for the moment (i.e.
+                     tail = 'both').
+
+    Output arguments:
+    pval           - Permutation test p-value.
+    """
+
+    """Find the number of samples in each of the two sets of data."""
+    (n_samples_x, _), (n_samples_y, _) = np.shape(X), np.shape(Y)
+    n_total = n_samples_x + n_samples_y
+
+    """Do an independent two-sample t-test for each variable using the
+    original unpermuted data. Compute the cluster mass statistic that is
+    compared to the permutation distribution."""
+    ref_tstat, _ = ttest_ind(X, Y)
+    ref_clusters = _cluster_by_adjacency(ref_tstat > threshold)
+    ref_cstat = _cluster_stat(ref_tstat, ref_clusters)
+
+    """Combine the two sets of data."""
+    Z = np.vstack([X, Y])
+
+    """Do the permutations."""
+    cstat = np.zeros(n_permutations)
+
+    for p in arange(0, n_permutations):
+        """Do a random partition of the data."""
+        permuted_ind = permutation(arange(n_total))
+        T, U = Z[permuted_ind[0:n_samples_x], :], Z[permuted_ind[n_samples_x:], :]
+
+        tstat, _ = ttest_ind(T, U)
+        sel = tstat > threshold
+        clusters = _cluster_by_adjacency(sel)
+
+        """Compute cluster-level statistics."""
+        cstat[p] = _cluster_stat(tstat, clusters)
+
+    cstat = cstat[~np.isnan(cstat)]
+    pval = np.sum(cstat > ref_cstat) / n_permutations
+
+    return pval, cstat, ref_cstat
+
+"""Generate some test data."""
+X = normal(loc=0.25, scale=1, size=(20, 300))
+Y = normal(loc=0, scale=1, size=(20, 300))
+
+pval, cstat, ref_cstat = permutation_test(X, Y)
+
+from viz import plot_permutation_distribution
+
+plot_permutation_distribution(cstat, ref_cstat)
+
+
---
+++ packages/multipy/multipy-0.12.tar.gz/multipy-0.12/multipy/viz.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+"""Functions for visualizing P-values and Q-values, making diagnostic plots,
+and showing permutation distributions etc.
+
+This program code is part of the MultiPy (Multiple Hypothesis Testing in
+Python) package.
+
+Author: Tuomas Puoliväli (tuomas.puolivali@helsinki.fi)
+Last modified: 18th June 2018.
+License: Revised 3-clause BSD
+Source: https://github.com/puolival/multipy/blob/master/viz.py
+
+References:
+
+[1] Storey JD, Tibshirani R (2003): Statistical significance for genomewide
+    studies. The Proceedings of the National Academy of the United States of
+    America 100(16):9440-9445. DOI: 10.1073/pnas.1530509100
+
+WARNING: These functions have not been entirely validated yet.
+
+"""
+
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+import seaborn as sb
+
+def plot_pval_hist(pvals, hist_bins=1e2, show_plot=True):
+    """Plot a simple density histogram of P-values.
+
+    Input arguments:
+    pvals      - The visualized P-values.
+    hist_bins  - Number of histogram bins.
+    """
+    # Keep empty space at minimum.
+    fig = plt.figure(figsize=(6, 4))
+    plt.subplots_adjust(top=0.925, bottom=0.125, left=0.105, right=0.950)
+
+    """Plot the p-value density histogram for the whole data range."""
+    ax1 = fig.add_subplot(111)
+    sb.distplot(pvals, bins=hist_bins, rug=True, kde=False)
+
+    """P-values are in the range [0, 1] so limit the drawing area
+    accordingly. Label the axes etc."""
+    ax1.set_xlim([-0.05, 1.05])
+    ax1.set_xlabel('P-value')
+    ax1.set_ylabel('Density')
+    if (show_plot):
+        plt.show()
+    return fig
+
+def plot_qvalue_diagnostics(stats, pvals, qvals, show_plot=True):
+    """Visualize q-values similar to Storey and Tibshirani [1].
+
+    Input arguments:
+    stats     - Test statistics (e.g. Student's t-values)
+    pvals     - P-values corresponding to the test statistics
+    qvals     - Q-values corresponding to the p-values
+    show_plot - A flag for deciding whether the figure should be opened
+                immediately after being drawn.
+
+    Output arguments:
+    fig       - The drawn figure.
+    """
+    # Sort the p-values into ascending order for improved visualization.
+    stat_sort_ind, pval_sort_ind = np.argsort(stats), np.argsort(pvals)
+    thresholds = np.arange(0., 0.1, 0.001)
+    significant_tests = np.asarray([np.sum(qvals<t) for t in thresholds])
+
+    fig = plt.figure(figsize=(8, 6))
+    fig.subplots_adjust(top=0.95, bottom=0.1, left=0.09, right=0.96)
+
+    """Show q-values corresponding to the test statistics."""
+    ax1 = fig.add_subplot(221)
+    ax1.plot(stats[stat_sort_ind], qvals[stat_sort_ind])
+    ax1.set_xlabel('Test statistic')
+    ax1.set_ylabel('Q-value')
+
+    """Show q-values corresponding to the p-values."""
+    ax2 = fig.add_subplot(222)
+    ax2.plot(pvals[pval_sort_ind], qvals[pval_sort_ind])
+    ax2.set_xlabel('P-value')
+    ax2.set_ylabel('Q-value')
+
+    """Show the number of significant tests as a function of q-value
+    threshold."""
+    ax3 = fig.add_subplot(223)
+    ax3.plot(thresholds, significant_tests)
+    ax3.set_xlabel('Q-value')
+    ax3.set_ylabel('Number of significant tests')
+
+    """Show the number of expected false positives as a function of the
+    number of significant tests."""
+    ax4 = fig.add_subplot(224)
+    ax4.plot(significant_tests, thresholds*significant_tests)
+    ax4.set_xlabel('Number of significant tests')
+    ax4.set_ylabel('Number of expected false positives')
+
+    """Return the drawn figure."""
+    if (show_plot):
+        plt.show() # Open the figure.
+    return fig
+
+def plot_qvalue_pi0_fit(kappa, pik, cs, show_plot=True):
+    """Make a diagnostic plot of the estimate of pi0."""
+    fig = plt.figure(facecolor='white', edgecolor='white')
+    ax = fig.add_subplot(111)
+    ax.plot(kappa, pik, '.', markersize=10)
+    ax.plot(np.linspace(0, 1, 100), cs(np.linspace(0, 1, 100)),
+            '-', markersize=10)
+    ax.set_xlabel('$\lambda$')
+    ax.set_ylabel('$\pi_{0}(\lambda)$')
+    if (show_plot):
+        plt.show()
+    return fig
+
+def plot_permutation_distribution(stat, ref_stat, show_plot=True):
+    """Initialize the plot."""
+    fig = plt.figure(figsize=(6, 4), facecolor='white', edgecolor='white')
+    ax = fig.add_subplot(111)
+
+    """Plot the permutation distribution."""
+    sb.distplot(stat, hist=True, kde=True, norm_hist=True, ax=ax)
+
+    """Plot the reference value."""
+    ax.plot([ref_stat, ref_stat], [0, ax.dataLim.ymax], '-', color='g')
+
+    """Label the axes etc."""
+    ax.set_xlabel('Test statistic')
+    ax.set_ylabel('Density')
+    ax.set_title('Permutation distribution')
+    # Location 1 is the upper right position.
+    # TODO: why the reference value needs to be given first for the legend?
+    ax.legend(['Reference value', 'Permutation distribution'], loc=1)
+    fig.tight_layout()
+
+    """Return and show the plot."""
+    if (show_plot):
+        plt.show()
+    return fig

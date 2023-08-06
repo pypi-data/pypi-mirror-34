4d4b4873e86ffcf7498975e044e21c6172f15162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:20 +0000

    Add package edc-constants/0.2.10/edc_constants-0.2.10-py3-none-any.whl

---
+++ packages/edc-constants/edc_constants-0.2.10-py3-none-any.whl/edc_constants/utils.py
@@ -0,0 +1,9 @@
+
+def get_display(choices, label):
+    """Returns the display value of a choices tuple for label.
+    """
+    for choice in choices:
+        store_value, display_value = choice
+        if label == store_value:
+            return display_value
+    return None

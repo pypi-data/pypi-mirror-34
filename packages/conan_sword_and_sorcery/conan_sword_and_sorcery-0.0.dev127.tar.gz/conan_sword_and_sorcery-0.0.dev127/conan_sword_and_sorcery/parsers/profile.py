157bb49b7d5bf98d95b5e71ebadc0e02e2fcd0d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:19:26 +0000

    Add package conan_sword_and_sorcery/0.0.dev127/conan_sword_and_sorcery-0.0.dev127.tar.gz

---
+++ packages/conan_sword_and_sorcery/conan_sword_and_sorcery-0.0.dev127.tar.gz/conan_sword_and_sorcery-0.0.dev127/conan_sword_and_sorcery/parsers/profile.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import tempfile
+from contextlib import contextmanager
+
+
+@contextmanager
+def profile_for(compiler):
+    # Create profile file
+    tmp = tempfile.NamedTemporaryFile(dir=os.path.expanduser("~"), mode='w', delete=False)  # In local dirname
+    # tmp.write("include(default)\n\n")  # Profile 'default'
+    tmp.write("[settings]\n")
+    compiler.populate_profile_settings(tmp)
+    tmp.write("[options]\n[build_requires]\n")  # TODO: Is it needed?
+    tmp.write("[env]\n")
+    compiler.populate_profile_env(tmp)
+    tmp.close()
+    yield tmp.name
+    os.unlink(tmp.name)
+
+
+def parse_profile(profile_file):
+    eq = re.compile("^([\w\.]+)=([\w_\+\.\s]+)\s")
+    with open(profile_file) as f:
+        matches = {}
+        for line in f.readlines():
+            m = eq.match(line)
+            if m:
+                matches[m.group(1)] = m.group(2)
+    return matches
---
+++ packages/conan_sword_and_sorcery/conan_sword_and_sorcery-0.0.dev127.tar.gz/conan_sword_and_sorcery-0.0.dev127/conan_sword_and_sorcery/utils/conan.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+import os
+import uuid
+import logging
+from contextlib import contextmanager
+from configparser import ConfigParser
+from conan_sword_and_sorcery.utils.cmd import cmd
+from conan_sword_and_sorcery.utils import backup_file
+
+log = logging.getLogger(__name__)
+
+
+@contextmanager
+def remote(url, name=None, insert_first=True):
+    name = name or uuid.uuid4()
+    command = "conan remote add {name} {url}".format(name=name, url=url)
+    if insert_first:
+        command += " --insert 0"
+
+    cmd(command)
+    yield name
+    cmd("conan remote remove {name}".format(name=name))
+
+
+def remote_auth(remote, username, password):
+    command = "conan user -p {password} -r {remote} {username}".format(username=username, remote=remote, password=password)
+    cmd(command, secret=True)
+
+
+@contextmanager
+def conf(new_values, conan_conf=None):
+    conan_conf = conan_conf or os.path.join(os.path.expanduser("~"), '.conan', 'conan.conf')
+    if not os.path.exists(conan_conf):
+        raise RuntimeError("Cannot find configuration file at '{}'".format(conan_conf))
+
+    with backup_file(conan_conf):
+        config = ConfigParser()
+        config.read(conan_conf)
+        for section, item, new_value in new_values:
+            config[section][item] = new_value
+
+        with open(conan_conf, 'w') as f:
+            config.write(conan_conf)
+        yield config
+
+    pass
+

6738c84a1aab7daaa15d019597c11895af28196b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:57 +0000

    Add package trio-asyncio/0.7.5/trio_asyncio-0.7.5.tar.gz

---
+++ packages/trio-asyncio/trio_asyncio-0.7.5.tar.gz/trio_asyncio-0.7.5/trio_asyncio/adapter.py
@@ -0,0 +1,58 @@
+# This code implements a clone of the asyncio mainloop which hooks into
+# Trio.
+
+from async_generator import isasyncgenfunction
+import trio_asyncio
+
+# import logging
+# logger = logging.getLogger(__name__)
+
+from functools import wraps, partial
+
+__all__ = ['trio2aio', 'aio2trio']
+
+
+def trio2aio(proc):
+    if isasyncgenfunction(proc):
+
+        @wraps(proc)
+        def call(*args, **kwargs):
+            proc_ = proc
+            if kwargs:
+                proc_ = partial(proc_, **kwargs)
+            return trio_asyncio.wrap_generator(proc_, *args)
+
+    else:
+
+        @wraps(proc)
+        async def call(*args, **kwargs):
+            proc_ = proc
+            if kwargs:
+                proc_ = partial(proc_, **kwargs)
+            return await trio_asyncio.run_asyncio(proc_, *args)
+
+    return call
+
+
+def aio2trio(proc):
+    """Decorate a Trio function so that it's callable by asyncio (only)."""
+
+    @wraps(proc)
+    async def call(*args, **kwargs):
+        proc_ = proc
+        if kwargs:
+            proc_ = partial(proc_, **kwargs)
+        return await trio_asyncio.run_trio(proc_, *args)
+
+    return call
+
+
+def aio2trio_task(proc):
+    @wraps(proc)
+    async def call(*args, **kwargs):
+        proc_ = proc
+        if kwargs:
+            proc_ = partial(proc_, **kwargs)
+        trio_asyncio.run_trio_task(proc_, *args)
+
+    return call

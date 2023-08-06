c6221a77c3710030dcf83340d8c326e697ad1cc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:17 +0000

    Add package Pillow3f/0.0.5/Pillow3f-0.0.5.tar.gz

---
+++ packages/Pillow3f/Pillow3f-0.0.5.tar.gz/Pillow3f-0.0.5/README.md
@@ -0,0 +1,11 @@
+You can find the current documentation at: https://gist.github.com/Robertobean/d54f96a858d4183ead1e8eb9c02b1264
+
+You can find images rendered using this library here:
+https://github.com/Robertobean/Pillow3f-examples
+
+You can find documentations for the dependencies here:
+
+> Numpy:
+> https://docs.scipy.org/doc/numpy/reference/
+> Pillow:
+> https://pillow.readthedocs.io/en/5.2.x/
---
+++ packages/Pillow3f/Pillow3f-0.0.5.tar.gz/Pillow3f-0.0.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+with open('README.md') as yeet:
+    long_description = yeet.read()
+    
+setup(
+   name='Pillow3f',
+   version='0.0.5',
+   description='3D graphics library using Pillow',
+   author='Robert Bassett',
+   author_email='bassett.w.robert@gmail.com',
+   packages=['Pillow3f'],
+   install_requires=['pillow', 'numpy'],
+   classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ),
+   long_description = long_description
+)

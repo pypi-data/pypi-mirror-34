d2b8fc99396c90311adb420e8b7fdb0d29bb6aed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:59 +0000

    Add package koala2/0.0.21/koala2-0.0.21.tar.gz

---
+++ packages/koala2/koala2-0.0.21.tar.gz/koala2-0.0.21/koala/ExcelError.py
@@ -0,0 +1,12 @@
+
+ErrorCodes = ("#NULL!", "#DIV/0!", "#VALUE!", "#REF!", "#NAME?", "#NUM!", "#N/A")
+
+class ExcelError(Exception):
+    def __init__(self, value, info = None):
+        self.value = value
+        self.info = info
+    def __str__(self):
+    	return self.info
+
+class EmptyCellError(ExcelError):
+    pass

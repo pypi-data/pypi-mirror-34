d02921308991775fff020739fabfdc2ae7ffe85d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:19:27 +0000

    Add package nanopq/0.1.1/nanopq-0.1.1.tar.gz

---
+++ packages/nanopq/nanopq-0.1.1.tar.gz/nanopq-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='nanopq',
+    version='0.1.1',
+    description='Product quantization for nearest neighbor search in a single python file',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Yusuke Matsui',
+    author_email='matsui528@gmail.com',
+    url='https://github.com/matsui528/nanopq',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+

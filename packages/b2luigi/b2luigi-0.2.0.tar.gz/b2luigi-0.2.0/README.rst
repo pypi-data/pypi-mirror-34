95935268a07388f1d506b716fc682fb4a6881ca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:37 +0000

    Add package b2luigi/0.2.0/b2luigi-0.2.0.tar.gz

---
+++ packages/b2luigi/b2luigi-0.2.0.tar.gz/b2luigi-0.2.0/README.rst
@@ -0,0 +1,16 @@
+b2luigi
+=======
+
+``b2luigi`` is a helper package constructed around ``luigi``, that helps you schedule working packages (so called tasks)
+locally or on the batch system.
+Apart from the very powerful dependency management system by ``luigi``, ``b2luigi`` extends the user interface
+and has a build-in support for the queue systems, e.g. LSF.
+
+You can find more information in the `documentation <https://b2luigi.readthedocs.io/en/latest/>`_.
+
+Please note, that most of the core features are handled by ``luigi``, so you might want to have a look into
+the `luigi documentation <https://luigi.readthedocs.io/en/latest/>`_.
+
+If you find any bugs or want to improve the documentation, please send me a pull request.
+
+This project is in beta. Please be extra cautious when using in production mode.
---
+++ packages/b2luigi/b2luigi-0.2.0.tar.gz/b2luigi-0.2.0/b2luigi/cli/runner.py
@@ -0,0 +1,90 @@
+import collections
+
+import luigi
+import luigi.server
+import luigi.configuration
+import os
+
+from b2luigi.batch.workers import SendJobWorkerSchedulerFactory
+from b2luigi.core.settings import set_setting
+from b2luigi.core.utils import task_iterator, get_all_output_files_in_tree
+
+
+def run_as_batch_worker(task_list, cli_args, kwargs):
+    for root_task in task_list:
+        for task in task_iterator(root_task):
+            if task.task_id != cli_args.task_id:
+                continue
+
+            set_setting("local_execution", True)
+
+            # TODO: We do not process the information if (a) we have a new dependency and (b) why the task has failed.
+            # TODO: Would be also nice to run the event handlers
+            try:
+                task.run()
+                task.on_success()
+            except BaseException as ex:
+                task.on_failure(ex)
+                raise ex
+
+
+def run_batched(task_list, cli_args, kwargs):
+    kwargs["worker_scheduler_factory"] = SendJobWorkerSchedulerFactory()
+    run_local(task_list, cli_args, kwargs)
+
+
+def run_local(task_list, cli_args, kwargs):
+    if cli_args.scheduler_host or cli_args.scheduler_port:
+        core_settings = luigi.interface.core()
+        host = cli_args.scheduler_host or core_settings.scheduler_host
+        port = int(cli_args.scheduler_port) or core_settings.scheduler_port
+        kwargs["scheduler_host"] = host
+        kwargs["scheduler_port"] = port
+    else:
+        kwargs["local_scheduler"] = True
+
+    kwargs.setdefault("log_level", "INFO")
+    luigi.build(task_list, **kwargs)
+
+
+def run_test_mode(task_list, cli_args, kwargs):
+    set_setting("local_execution", True)
+    luigi.build(task_list, log_level="DEBUG", local_scheduler=True, **kwargs)
+
+
+def show_all_outputs(task_list, *args, **kwargs):
+    from colorama import Fore, Style
+
+    all_output_files = collections.defaultdict(list)
+
+    for task in task_list:
+        output_files = get_all_output_files_in_tree(task)
+        for key, file_names in output_files.items():
+            all_output_files[key] += file_names
+
+    for key, file_names in all_output_files.items():
+        print(key)
+
+        exists = all(d["exists"] for d in file_names)
+
+        file_names = sorted(set(d["file_name"] for d in file_names))
+        for file_name in file_names:
+            # TODO: this is not correct as it does not check the task status!
+            if exists:
+                print("\t", Fore.GREEN, file_name, Style.RESET_ALL)
+            else:
+                print("\t", Fore.RED, file_name, Style.RESET_ALL)
+        print()
+
+
+def dry_run(task_list):
+    one_is_not_complete = False
+    for root_task in task_list:
+        for task in task_iterator(root_task, only_non_complete=True):
+            one_is_not_complete = True
+            print("Would run", task)
+
+    if one_is_not_complete:
+        exit(1)
+    else:
+        exit(0)

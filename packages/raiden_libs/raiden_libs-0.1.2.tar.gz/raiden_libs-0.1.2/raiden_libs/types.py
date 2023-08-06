c39fbac18e0c92ec48a25f2b14e56f148530f87a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:30 +0000

    Add package raiden_libs/0.1.2/raiden_libs-0.1.2.tar.gz

---
+++ packages/raiden_libs/raiden_libs-0.1.2.tar.gz/raiden_libs-0.1.2/raiden_libs/types.py
@@ -0,0 +1,7 @@
+from typing import NewType
+
+T_Address = str
+Address = NewType('Address', T_Address)
+
+T_ChannelIdentifier = str
+ChannelIdentifier = NewType('ChannelIdentifier', T_ChannelIdentifier)

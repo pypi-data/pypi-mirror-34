738f8ee80730c07159a2cf25c3bb851524fe9b64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:31 +0000

    Add package mosyn/1.1.0/mosyn-1.1.0.tar.gz

---
+++ packages/mosyn/mosyn-1.1.0.tar.gz/mosyn-1.1.0/mosyn/util/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-15 -*-
+
+# Created on 14/11/2015
+# Modified on 14/11/2015
+# Modified by asaelt
+#
+# source: https://github.com/uagdataanalysis/mosynapi
+#
+# @author: asaelt
+
+# from . import eagles
+# from . import events
+# from . import format
+# from . import io
+# from . import labels

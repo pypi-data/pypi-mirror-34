4cc22e3a7dfd4211e0410163099e3aa9a28b243a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:19 +0000

    Add package qradial-menu/0.1.0/qradial_menu-0.1.0.tar.gz

---
+++ packages/qradial-menu/qradial_menu-0.1.0.tar.gz/qradial_menu-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='qradial_menu',
+    version='0.1.0',
+    packages=['qradial_menu'],
+    install_requires=[],
+    url='https://github.com/rubenhenares/qradial_menu',
+    license='Apache License 2.0',
+    author='Ruben Henares',
+    author_email='ruben@404fs.com',
+    description='A simple radial menu for PyQt',
+    keywords=['qt', 'pyqt', 'widgets', 'radial menu']
+)

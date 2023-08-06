92594640fdc415a52c9db5a9d7f1c8dc160ae7d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:25 +0000

    Add package Omnis/0.0.1.3/Omnis-0.0.1.3-py2-none-any.whl

---
+++ packages/Omnis/Omnis-0.0.1.3-py2-none-any.whl/omnis/model/cnn/densenet.py
@@ -0,0 +1,36 @@
+import keras
+
+from keras.preprocessing.image import ImageDataGenerator
+
+from keras.applications import densenet
+
+import os
+
+from .cnn import CNN
+
+class DenseNet121(CNN):
+    def __init__(self, *args, **kwargs):
+        CNN.__init__(self, *args, **kwargs)
+        self.input_shape = (224, 224, 3)
+    
+    def create_model(self, num_classes):
+        model = densenet.DenseNet121( weights=None, classes=num_classes )
+        return model
+
+class DenseNet169(CNN):
+    def __init__(self, *args, **kwargs):
+        CNN.__init__(self, *args, **kwargs)
+        self.input_shape = (224, 224, 3)
+    
+    def create_model(self, num_classes):
+        model = densenet.DenseNet169( weights=None, classes=num_classes )
+        return model
+
+class DenseNet201(CNN):
+    def __init__(self, *args, **kwargs):
+        CNN.__init__(self, *args, **kwargs)
+        self.input_shape = (224, 224, 3)
+    
+    def create_model(self, num_classes):
+        model = densenet.DenseNet201( weights=None, classes=num_classes )
+        return model

00eb7382cd15fa310e4696fd5a06701c4d695f53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:19 +0000

    Add package visonicalarm/1.0.10/visonicalarm-1.0.10.tar.gz

---
+++ packages/visonicalarm/visonicalarm-1.0.10.tar.gz/visonicalarm-1.0.10/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="visonicalarm",
+    version="1.0.10",
+    author="Mikael Schultz",
+    author_email="mikael.schultz@outlook.com",
+    description="A simple API library for the Visonic Alarm system.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MikaelSchultz/visonicalarm",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

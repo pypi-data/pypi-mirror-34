b6d9e7bb1252dd53a5d55bbd3983ccebe7644b13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:22 +0000

    Add package tan-utils/0.0.2/tan_utils-0.0.2.tar.gz

---
+++ packages/tan-utils/tan_utils-0.0.2.tar.gz/tan_utils-0.0.2/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+  name='tan_utils',
+  version='0.0.2',
+  description='Tan\'s utilities',
+  url='https://github.com/tannn1995/tan_utils',
+  author='Tan Nguyen',
+  author_email='tan.nguyen.tn@hotmail.com',
+  packages=setuptools.find_packages(),
+  classifiers=(
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ),
+)

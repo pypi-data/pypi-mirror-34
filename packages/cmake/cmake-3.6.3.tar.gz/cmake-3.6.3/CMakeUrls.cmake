e53b650e9849b0ec9a7b45e42828eb4d4f712856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:15 +0000

    Add package cmake/3.6.3/cmake-3.6.3.tar.gz

---
+++ packages/cmake/cmake-3.6.3.tar.gz/cmake-3.6.3/CMakeUrls.cmake
@@ -0,0 +1,26 @@
+
+#-----------------------------------------------------------------------------
+# CMake sources
+set(unix_source_url       "https://cmake.org/files/v3.6/cmake-3.6.3.tar.gz")
+set(unix_source_sha256    "7d73ee4fae572eb2d7cd3feb48971aea903bb30a20ea5ae8b4da826d8ccad5fe")
+
+set(windows_source_url    "https://cmake.org/files/v3.6/cmake-3.6.3.zip")
+set(windows_source_sha256 "cdaa35f7e3395dad2f1faae64195b18c030c431085a89eb20bddc70cc05c6a76")
+
+#-----------------------------------------------------------------------------
+# CMake binaries
+
+set(linux32_binary_url    "NA")  # Linux 32-bit binaries not available
+set(linux32_binary_sha256 "NA")
+
+set(linux64_binary_url    "https://cmake.org/files/v3.6/cmake-3.6.3-Linux-x86_64.tar.gz")
+set(linux64_binary_sha256 "9d915d505c07d84b610e1be6242c7cad68b4b7a4090ce85ecf9cec5effa47c43")
+
+set(macosx_binary_url    "https://cmake.org/files/v3.6/cmake-3.6.3-Darwin-x86_64.tar.gz")
+set(macosx_binary_sha256 "cba5b50365fa79c36bb954dc44d6e3455e431c47937fe13e6ddd06b61fe428a4")
+
+set(win32_binary_url    "https://cmake.org/files/v3.6/cmake-3.6.3-win32-x86.zip")
+set(win32_binary_sha256 "556628f134b2372b8f02688787ca44b02d893c57b6eeaa9271ac51a4a2d4a4ed")
+
+set(win64_binary_url    "https://cmake.org/files/v3.6/cmake-3.6.3-win64-x64.zip")
+set(win64_binary_sha256 "263b78aba6e63098353b2f8beecf293f5f307f9a70575a81867c3f8cb18a6a99")

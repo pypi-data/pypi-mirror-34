f54d6638a26da40144cf1d7aa685d5530af9692a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:27 +0000

    Add package plover-phenrsteno/0.0.8/plover-phenrsteno-0.0.8.tar.gz

---
+++ packages/plover-phenrsteno/plover-phenrsteno-0.0.8.tar.gz/plover-phenrsteno-0.0.8/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+import codecs
+
+with codecs.open('README.rst', 'r', 'utf_8') as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name = 'plover-phenrsteno',
+    version = '0.0.8',
+    author = 'Gregory Chamberlain',
+    author_email = '15685804+contrum@users.noreply.github.com',
+    description = 'Phonemic non-rhotic English stenotype system for Plover',
+    long_description = long_description,
+    license = 'GNU General Public License v2 or later (GPLv2+)',
+    keywords = 'plover plover_plugin',
+    url = 'https://github.com/contrum/phenrsteno',
+    packages = setuptools.find_packages(),
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'Operating System :: OS Independent',
+        'Environment :: Plugins',
+        'Intended Audience :: End Users/Desktop',
+    ],
+    install_requires =[
+        'plover>=4.0.0.dev0',
+    ],
+    py_modules = [
+        'plover_phenr',
+    ],
+    entry_points = '''
+
+[plover.system]
+Phenrsteno = plover_phenr
+
+''',
+    zip_safe = True,
+)

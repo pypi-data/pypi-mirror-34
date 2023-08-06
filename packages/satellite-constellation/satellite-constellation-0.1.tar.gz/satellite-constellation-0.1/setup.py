66ecfcbfe7534c62f5d323537ea11c56c538c08a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:24 +0000

    Add package satellite-constellation/0.1/satellite-constellation-0.1.tar.gz

---
+++ packages/satellite-constellation/satellite-constellation-0.1.tar.gz/satellite-constellation-0.1/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(name="satellite-constellation",
+      version="0.1",
+      description="Create Satellite Constellations around the Earth, Luna and planets in the solar system",
+      author="Aidan O'Brien - Saber Astronautics Australia",
+      author_email="aidan.obrien@saberastro.com",
+      url="https://github.com/SaberAidan/SatelliteConstellationCreator",
+      packages=["satellite_constellation"],
+      package_dir={'satellite_constellation': 'satellite_constellation'},
+      keywords="satellite satellites constellation orbital mechanics",
+      classifiers=[
+          'Development Status :: 2 - Pre-Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+      ])

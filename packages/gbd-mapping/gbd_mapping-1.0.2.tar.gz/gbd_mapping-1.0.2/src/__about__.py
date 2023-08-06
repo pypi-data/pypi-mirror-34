6426b45a38fde01abde8f7324a9f7c0be78ecdbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:45 +0000

    Add package gbd-mapping/1.0.2/gbd_mapping-1.0.2.tar.gz

---
+++ packages/gbd-mapping/gbd_mapping-1.0.2.tar.gz/gbd_mapping-1.0.2/src/__about__.py
@@ -0,0 +1,12 @@
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",  "__email__",
+]
+
+__title__ = "gbd_mapping"
+__summary__ = "A programmatically accessible mapping of gbd entities."
+__uri__ = "https://stash.ihme.washington.edu/projects/CSTE/repos/gbd_mapping/browse"
+
+__version__ = "1.0.2"
+
+__author__ = "James Collins"
+__email__ = "collijk@uw.edu"

55d780b043ff7000ed689ab9712e9eed69d82ccb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:02 +0000

    Add package funniest-321/0.2/funniest_321-0.2.tar.gz

---
+++ packages/funniest-321/funniest_321-0.2.tar.gz/funniest_321-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniest_321',
+version="0.2",
+description='The funniest joke in the world',
+url="http://github.com/ruknight/funniest_321",
+author='ruknight',
+author_email='manyacservice@gmail.com',
+license='MIT',
+packages=['funniest_321'],
+zip_safe=False)

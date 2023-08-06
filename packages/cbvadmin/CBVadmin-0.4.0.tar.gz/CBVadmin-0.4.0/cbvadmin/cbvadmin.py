fd91a51b07a158818b348bc7e61b4bb9b53134c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:41 +0000

    Add package cbvadmin/0.4.0/CBVadmin-0.4.0.tar.gz

---
+++ packages/cbvadmin/CBVadmin-0.4.0.tar.gz/CBVadmin-0.4.0/cbvadmin/cbvadmin.py
@@ -0,0 +1,58 @@
+from django.conf import settings
+from django.urls import reverse
+from menu import MenuItem
+from .sites import site
+from .options import SimpleAdmin, UserAdmin, GroupAdmin
+from .views.dashboard import Dashboard
+from .views.user import PasswordChange
+from .views.auth import (AdminLoginView, AdminLogoutView,
+                         AdminPasswordResetView,
+                         AdminPasswordResetDoneView,
+                         AdminPasswordResetConfirmView,
+                         AdminPasswordResetCompleteView)
+
+
+class DefaultAdmin(SimpleAdmin):
+    dashboard_view_class = Dashboard
+    default_action = 'dashboard'
+
+    def get_actions(self):
+        return {'dashboard': 'collection'}
+
+    def get_menu(self):
+        return [MenuItem('Dashboard', reverse('cbvadmin:dashboard'))]
+
+
+class AccountsAdmin(SimpleAdmin):
+    login_view_class = AdminLoginView
+    logout_view_class = AdminLogoutView
+    password_change_view_class = PasswordChange
+    password_reset_view_class = AdminPasswordResetView
+    password_reset_done_view_class = AdminPasswordResetDoneView
+    password_reset_confirm_view_class = AdminPasswordResetConfirmView
+    password_reset_complete_view_class = AdminPasswordResetCompleteView
+
+    def get_actions(self):
+        return {
+            'login': 'collection',
+            'logout': 'collection',
+            'password_change': 'collection',
+            'password_reset': 'collection',
+            'password_reset_done': 'collection',
+            'password_reset_confirm': 'collection',
+            'password_reset_complete': 'collection'
+        }
+
+    def get_menu(self):
+        return None
+
+
+site.register('default', DefaultAdmin)
+site.register('accounts', AccountsAdmin)
+
+
+if settings.AUTH_USER_MODEL == 'auth.User' and \
+   getattr(settings, 'CBVADMIN_REGISTER_USER', True):
+    from django.contrib.auth.models import User, Group
+    site.register(User, UserAdmin)
+    site.register(Group, GroupAdmin)
---
+++ packages/cbvadmin/CBVadmin-0.4.0.tar.gz/CBVadmin-0.4.0/cbvadmin/views/list.py
@@ -0,0 +1,35 @@
+from django_tables2 import SingleTableView, RequestConfig
+from .mixins import AdminMixin, FilterMixin, PermissionRequiredMixin
+
+
+class TableListView(PermissionRequiredMixin, AdminMixin, FilterMixin,
+                    SingleTableView):
+    template_name = 'list.html'
+
+    def get_table_class(self):
+        if not self.table_class:
+            return self.admin.get_table_class()
+        return self.table_class
+
+    def get_table(self, **kwargs):
+        table = super(TableListView, self).get_table()
+        paginate = {'page': self.request.GET.get('page'),
+                    'per_page': self.paginate_by}
+        RequestConfig(self.request, paginate=paginate).configure(table)
+        return table
+
+    def get_table_data(self):
+        if self.filterset:
+            return self.filterset.qs
+        else:
+            return self.get_queryset()
+
+    def get_template_names(self, *args, **kwargs):
+        if 'querystring_key' in self.request.GET:
+            return self.get_admin_template('table.html')
+        else:
+            return super(TableListView, self).get_template_names(
+                *args, **kwargs)
+
+
+ListView = TableListView

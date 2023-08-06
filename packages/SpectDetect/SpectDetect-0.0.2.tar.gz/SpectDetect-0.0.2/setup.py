606ae4b749933b63cd6d50e1c8a0a6b4d2ff05e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:16 +0000

    Add package SpectDetect/0.0.2/SpectDetect-0.0.2.tar.gz

---
+++ packages/SpectDetect/SpectDetect-0.0.2.tar.gz/SpectDetect-0.0.2/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="SpectDetect",
+    version="0.0.2",
+    author="Tom Hudson",
+    description="Microseismic detection algorithm package based on using key features in the spectrum of a source to detect earthquakes over a given time period.",
+    url="https://github.com/TomSHudson/SpectDetect",
+    packages=['SpectDetect'],
+    license='MIT',
+    classifiers=(
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    )
+)

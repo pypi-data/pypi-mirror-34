4b261c91d2c825775b0c29b000f323ac1cd68001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:27 +0000

    Add package pushshift.py/0.0.10/pushshift.py-0.0.10-py3-none-any.whl

---
+++ packages/pushshift.py/pushshift.py-0.0.10-py3-none-any.whl/psaw/__init__.py
@@ -0,0 +1,10 @@
+"""
+Pushshift.io API Wrapper (for reddit.com public comment/submission search)
+
+https://github.com/dmarx/psaw
+"""
+
+from psaw.pushshift_api_minimal import PushshiftAPIMinimal
+from psaw.pushshift_api import PushshiftAPI
+
+__version__ = "0.0.5"

f4b23c474056323791bc662004fcd645e3077692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:23 +0000

    Add package lyricsfinder/1.2.7/lyricsfinder-1.2.7.tar.gz

---
+++ packages/lyricsfinder/lyricsfinder-1.2.7.tar.gz/lyricsfinder-1.2.7/lyricsfinder/__init__.py
@@ -0,0 +1,12 @@
+import logging
+
+from .__info__ import *
+from .lyrics_manager import LyricsManager
+from .models import *
+
+log = logging.getLogger(__name__)
+
+extract_lyrics = LyricsManager.extract_lyrics
+search_lyrics = LyricsManager.search_lyrics
+
+log.debug("LyricsFinder loaded")

57ccdbac650b17f8a78f3808ab4a6a30a079c466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:26 +0000

    Add package panjing/2.0/panjing-2.0.tar.gz

---
+++ packages/panjing/panjing-2.0.tar.gz/panjing-2.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='panjing',
+    version='2.0',
+    packages=find_packages(),
+    url='https://github.com/newpanjing',
+    license='Apache Licene 2.0',
+    author='panjing',
+    author_email='newpanjing@icloud.com',
+    description='panjing core'
+)

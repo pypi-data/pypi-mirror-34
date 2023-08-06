8cbbb56f7a721da9d346d615d28d82c208132451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:44 +0000

    Add package fusesoc/1.8.3/fusesoc-1.8.3.tar.gz

---
+++ packages/fusesoc/fusesoc-1.8.3.tar.gz/fusesoc-1.8.3/fusesoc/provider/url.py
@@ -0,0 +1,54 @@
+import logging
+import os.path
+import sys
+import tarfile
+import zipfile
+import shutil
+
+logger = logging.getLogger(__name__)
+
+if sys.version_info[0] >= 3:
+    import urllib.request as urllib
+    from urllib.error import URLError
+    from urllib.error import HTTPError
+else:
+    import urllib
+    from urllib2 import URLError
+    from urllib2 import HTTPError
+
+from fusesoc.provider.provider import Provider
+
+class Url(Provider):
+
+    def _checkout(self, local_dir):
+        url = self.config.get('url')
+        logger.info("Downloading...")
+        try:
+            (filename, headers) = urllib.urlretrieve(url)
+        except URLError as e:
+            raise RuntimeError("Failed to download '{}'. '{}'".format(url, e.reason))
+        except HTTPError:
+            raise RuntimeError("Failed to download '{}'. '{}'".format(url, e.reason))
+
+        (cache_root, core) = os.path.split(local_dir)
+
+        filetype = self.config.get('filetype')
+        if filetype == 'tar':
+            t = tarfile.open(filename)
+            t.extractall(local_dir)
+        elif filetype == 'zip':
+            with zipfile.ZipFile(filename, "r") as z:
+                z.extractall(local_dir)
+        elif filetype == 'simple':
+            # Splits the string at the last occurrence of sep, and
+            # returns a 3-tuple containing the part before the separator,
+            # the separator itself, and the part after the separator.
+            # If the separator is not found, return a 3-tuple containing
+            # two empty strings, followed by the string itself
+            segments = url.rpartition('/')
+            self.path = os.path.join(local_dir)
+            os.makedirs(self.path)
+            self.path = os.path.join(self.path, segments[2])
+            shutil.copy2(filename, self.path)
+        else:
+            raise RuntimeError("Unknown file type '" + filetype + "' in [provider] section")
---
+++ packages/fusesoc/fusesoc-1.8.3.tar.gz/fusesoc-1.8.3/tests/test_provider.py
@@ -0,0 +1,89 @@
+import os
+import pytest
+import shutil
+import tempfile
+
+from fusesoc.core import Core
+
+from test_common import tests_dir
+
+cores_root = os.path.join(tests_dir, 'cores')
+
+def test_coregen_provider():
+
+    cache_root = tempfile.mkdtemp('coregen_')
+    core = Core(os.path.join(cores_root, 'misc', 'coregencore.core'), cache_root)
+
+    os.environ['PATH'] = os.path.join(tests_dir, 'mock_commands')+':'+os.environ['PATH']
+    core.setup()
+
+    for f in ['dummy.cgp',
+	      'dummy.xco',
+	      os.path.join('subdir', 'dummy.extra')]:
+        assert(os.path.isfile(os.path.join(core.files_root, f)))
+
+    with open(os.path.join(core.files_root, 'run.cmd')) as f:
+        assert(f.read() == '-r -b dummy.xco -p dummy.cgp\n')
+    
+def test_git_provider():
+    cache_root = tempfile.mkdtemp('git_')
+    core = Core(os.path.join(cores_root, 'misc', 'gitcore.core'), cache_root)
+    
+    core.setup()
+
+    for f in ['LICENSE',
+	      'README.md',
+	      'wb_common.core',
+	      'wb_common.v',
+	      'wb_common_params.v']:
+        assert(os.path.isfile(os.path.join(core.files_root, f)))
+
+def test_github_provider():
+    cache_root = tempfile.mkdtemp('github_')
+    core = Core(os.path.join(cores_root, 'vlog_tb_utils', 'vlog_tb_utils-1.1.core'), cache_root)
+    
+    core.setup()
+    
+    for f in ['LICENSE',
+              'vlog_functions.v',
+              'vlog_tap_generator.v',
+              'vlog_tb_utils.core',
+              'vlog_tb_utils.v']:
+        assert(os.path.isfile(os.path.join(core.files_root, f)))
+        ref_dir   = os.path.join(os.path.dirname(__file__),  __name__)
+        f = 'vlog_functions.v'
+    with open(os.path.join(ref_dir, f)) as fref, open(os.path.join(core.files_root, f)) as fgen:
+            assert fref.read() == fgen.read(), f
+
+def test_logicore_provider():
+
+    cache_root = tempfile.mkdtemp('logicore_')
+    core = Core(os.path.join(cores_root, 'misc', 'logicorecore.core'), cache_root)
+
+    os.environ['PATH'] = os.path.join(tests_dir, 'mock_commands')+':'+os.environ['PATH']
+    core.setup()
+
+    for f in ['dummy.tcl',
+	      'dummy.xci',
+	      os.path.join('subdir', 'dummy.extra')]:
+        assert(os.path.isfile(os.path.join(core.files_root, f)))
+
+    with open(os.path.join(core.files_root, 'vivado.cmd')) as f:
+        assert(f.read() == '-mode batch -source dummy.tcl\n')
+
+def test_opencores_provider():
+    cache_root = tempfile.mkdtemp('opencores_')
+    core = Core(os.path.join(cores_root, 'misc', 'opencorescore.core'), cache_root)
+
+    core.setup()
+
+    assert(os.path.isfile(os.path.join(core.files_root, 'tap_defines.v')))
+    assert(os.path.isfile(os.path.join(core.files_root, 'tap_top.v')))
+
+def test_url_provider():
+    cache_root = tempfile.mkdtemp('url_')
+    core = Core(os.path.join(cores_root, 'mmuart', 'mmuart.core'), cache_root)
+
+    core.setup()
+
+    assert(os.path.isfile(os.path.join(core.files_root, 'uart_transceiver.v')))

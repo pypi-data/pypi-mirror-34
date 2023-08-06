c5d271302aaa6482aa5108d3c8d4c53075f5d714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:31 +0000

    Add package boatmacro/3.0/boatmacro-3.0.tar.gz

---
+++ packages/boatmacro/boatmacro-3.0.tar.gz/boatmacro-3.0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+
+setuptools.setup(
+    name="boatmacro",
+    version="3.0",
+    author="boatonboat",
+    author_email="usbjewon@gmail.com",
+    description="auto | macro module",
+    url="https://github.com/boatio/boatmacro/",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)

fc5019cb87a6aca4bc340a051ffe6ab8f1dc1960
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:40 +0000

    Add package sparsebitfield/0.2.1/sparsebitfield-0.2.1.tar.gz

---
+++ packages/sparsebitfield/sparsebitfield-0.2.1.tar.gz/sparsebitfield-0.2.1/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import sys
+
+version = '0.2.1'
+
+try:
+    from setuptools import setup, Extension
+except ImportError:
+    from distutils.core import setup, Extentsion
+
+if '--use-cython' in sys.argv:
+    USE_CYTHON = True
+    sys.argv.remove('--use-cython')
+else:
+    USE_CYTHON = False
+
+ext = '.pyx' if USE_CYTHON else '.c'
+extentions = [Extension('sparsebitfield', ['cimpl/field' + ext], depends=['cimpl/field.h', 'cimpl/popcount.h'])]
+
+if USE_CYTHON:
+    from Cython.Build import cythonize
+    extentions = cythonize(extentions, language_level=3)
+
+setup(
+    name='sparsebitfield',
+    version=version,
+    license='BSD',
+    description='A Cython fast number set based on bitfields',
+    author='Steve Stagg, Lars Fenneberg',
+    author_email='stestagg@gmail.com, lf@elemental.net',
+    url='http://github.com/elemental-lf/sparsebitfield',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=[
+        'sortedcontainers>=2.0.4',
+    ],
+    ext_modules=extentions,
+)

7e8c20cbf0eef71d3b8da5b5dc704476fb7b98c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:14 +0000

    Add package pythonfaceregressor/0.3/pythonfaceregressor-0.3.tar.gz

---
+++ packages/pythonfaceregressor/pythonfaceregressor-0.3.tar.gz/pythonfaceregressor-0.3/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'pythonfaceregressor',
+    packages = ['pythonfaceregressor'],
+    version = '0.3',
+    description = 'A package for examining relationships between facial appearance and other variables',
+    author = 'Alex Jones',
+    author_email = 'alexjonesphd@gmail.com',
+    url = 'https://github.com/alexjonesphd/python-face-regressor',
+    download_url = 'https://github.com/alexjonesphd/python-face-regressor/archive/v1.0.tar.gz',
+    keywords = ['faces', 'regression'],
+    classifiers = [],
+)

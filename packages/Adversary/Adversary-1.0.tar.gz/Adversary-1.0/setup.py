008497bfffa88845bf687c74e74ea569d9b57c4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:44 +0000

    Add package Adversary/1.0/Adversary-1.0.tar.gz

---
+++ packages/Adversary/Adversary-1.0.tar.gz/Adversary-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Adversary',
+    version='1.0',
+    packages=['tests', 'Adversary'],
+    url='https://github.com/airbnb/artificial-adversary',
+    license='MIT',
+    author='Devin Soni',
+    author_email='devinsoni1010@gmail.com',
+    description='Creates adversarial text examples for machine learning models'
+)

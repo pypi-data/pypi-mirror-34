b471f6aa43720f203d0a4e9b3662960d04e6f956
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:13 +0000

    Add package apistar-crud/0.6.1/apistar-crud-0.6.1.tar.gz

---
+++ packages/apistar-crud/apistar-crud-0.6.1.tar.gz/apistar-crud-0.6.1/apistar_crud/static/styles.react.js
@@ -0,0 +1,37 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["styles.react"],{
+
+/***/ "./admin/App.css":
+/*!***********************!*\
+  !*** ./admin/App.css ***!
+  \***********************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ "./admin/index.css":
+/*!*************************!*\
+  !*** ./admin/index.css ***!
+  \*************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ "./node_modules/semantic-ui-css/semantic.min.css":
+/*!*******************************************************!*\
+  !*** ./node_modules/semantic-ui-css/semantic.min.css ***!
+  \*******************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ })
+
+}]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9hZG1pbi9BcHAuY3NzP2JiNjQiLCJ3ZWJwYWNrOi8vLy4vYWRtaW4vaW5kZXguY3NzP2EwMjkiLCJ3ZWJwYWNrOi8vLy4vbm9kZV9tb2R1bGVzL3NlbWFudGljLXVpLWNzcy9zZW1hbnRpYy5taW4uY3NzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7OztBQUFBLHVDOzs7Ozs7Ozs7OztBQ0FBLHVDOzs7Ozs7Ozs7OztBQ0FBLHVDIiwiZmlsZSI6InN0eWxlcy5yZWFjdC5qcyIsInNvdXJjZXNDb250ZW50IjpbIi8vIGV4dHJhY3RlZCBieSBtaW5pLWNzcy1leHRyYWN0LXBsdWdpbiIsIi8vIGV4dHJhY3RlZCBieSBtaW5pLWNzcy1leHRyYWN0LXBsdWdpbiIsIi8vIGV4dHJhY3RlZCBieSBtaW5pLWNzcy1leHRyYWN0LXBsdWdpbiJdLCJzb3VyY2VSb290IjoiIn0=

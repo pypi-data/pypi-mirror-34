0f8841b020ca2b7ba5b85c55cc0b15cfa0603885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:31 +0000

    Add package buptelecmon/1.2.0/buptelecmon-1.2.0.tar.gz

---
+++ packages/buptelecmon/buptelecmon-1.2.0.tar.gz/buptelecmon-1.2.0/buptelecmon/version.py
@@ -0,0 +1,6 @@
+__version__ = '1.2.0'
+
+about = '''BUPT Electricity Monitor %s
+For more information, please visit https://github.com/jerrymakesjelly/electricity-monitor
+
+This software is distributed under the MIT License.''' % __version__

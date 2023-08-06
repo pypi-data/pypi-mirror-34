3e6cc2315f1a2fed391ea85e0319a86ca66e902a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:23 +0000

    Add package decaylanguage/0.2.0/decaylanguage-0.2.0-py2.py3-none-any.whl

---
+++ packages/decaylanguage/decaylanguage-0.2.0-py2.py3-none-any.whl/decaylanguage/__init__.py
@@ -0,0 +1,6 @@
+
+__version__ = '0.2.0'
+
+version = __version__
+version_info = __version__.split('.')
+
---
+++ packages/decaylanguage/decaylanguage-0.2.0-py2.py3-none-any.whl/decaylanguage/__main__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from plumbum import cli
+
+from decaylanguage.decay.ampgen2goofit import ampgen2goofit
+
+
+class DecayLanguageDecay(cli.Application):
+    generator = cli.SwitchAttr('-G,--generator', cli.Set('goofit'))
+
+    def main(self, filename):
+        if self.generator == 'goofit':
+            ampgen2goofit(filename)
+
+
+def main():
+    DecayLanguageDecay.run()
+
+
+if __name__ == "__main__":
+    main()

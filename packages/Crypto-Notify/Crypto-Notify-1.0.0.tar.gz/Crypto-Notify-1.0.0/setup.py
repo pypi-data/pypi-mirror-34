e9f454de6dd66ff7bca96f39278a2cd516432c0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:31 +0000

    Add package Crypto-Notify/1.0.0/Crypto-Notify-1.0.0.tar.gz

---
+++ packages/Crypto-Notify/Crypto-Notify-1.0.0.tar.gz/Crypto-Notify-1.0.0/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python2.7
+
+from setuptools import setup
+
+setup(
+    name='Crypto-Notify',
+    version='1.0.0',
+    description='Get the latest rates of popular Cryptocurrencies',
+    long_description='This python package notifies the user of the current market rates of popular Cryptocurrencies',
+    author='Dushyant Rathore',
+    license='MIT',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stablegit
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Natural Language :: English',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords="Cryptocurrency notifier",
+    author_email='dushyant.bgs@gmail.com',
+    url='https://github.com/dushyantRathore/Crypto-Notifier',
+    packages=["cryptonotifier"],
+    install_requires=[
+        "requests<=2.11.1",
+        "beautifulsoup4",
+        "notify2"
+    ],
+entry_points={
+        'console_scripts':
+            ['cryptonotifier = cryptonotifier.Main:notify']
+    }
+)

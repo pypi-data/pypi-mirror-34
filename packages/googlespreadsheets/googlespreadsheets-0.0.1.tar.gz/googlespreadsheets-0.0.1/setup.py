04211057c0c070eae496c6fd73a366bfd7655c49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:23 +0000

    Add package googlespreadsheets/0.0.1/googlespreadsheets-0.0.1.tar.gz

---
+++ packages/googlespreadsheets/googlespreadsheets-0.0.1.tar.gz/googlespreadsheets-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import sys
+from setuptools import setup
+
+setup(
+    name="googlespreadsheets",
+    packages=['googlespreadsheets'],
+    py_modules=['googlespreadsheets'],
+    version='0.0.1',
+    description="A Package for read and writing google sheets to and from Pandas dataframes",
+    long_description="A Package for read and writing google sheets to and from Pandas dataframes",
+    author="leon yin",
+    author_email="whereisleon@gmail.com",
+    url="https://github.com/yinleon/googlespeadsheets",
+    keywords='google sheet spreadsheet pandas',
+    license="MIT",
+    install_requires=[
+        'pandas',
+        'oauth2client',
+        'apiclient',
+    ]
+)

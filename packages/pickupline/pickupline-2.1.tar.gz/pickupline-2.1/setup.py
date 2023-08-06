babae24c8438e57742828f95f8f5d71092ddbdbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:07 +0000

    Add package pickupline/2.1/pickupline-2.1.tar.gz

---
+++ packages/pickupline/pickupline-2.1.tar.gz/pickupline-2.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(name="pickupline",
+      version="2.1",
+      description="Get PickupLines on various categories from web",
+      url="http://github.com/prdpx7/pickup-line",
+      author="Pradeep Khileri",
+      author_email="pradeepchoudhary009@gmail.com",
+      license="MIT",
+      packages=find_packages(),
+      scripts=['bin/pickup-line'],
+      keywords='cli pickupline humour geek',
+      install_requires=[
+          "pyperclip",
+          "requests",
+          "bs4",
+          "html5lib",
+      ],
+      zip_safe=False)
+

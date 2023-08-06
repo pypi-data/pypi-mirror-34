4399c0fde5df5ae8662f58d4802014950ff54928
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:23 +0000

    Add package ws_proxy/0.2.1/ws_proxy-0.2.1.tar.gz

---
+++ packages/ws_proxy/ws_proxy-0.2.1.tar.gz/ws_proxy-0.2.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ws_proxy",
+    version='0.2.1',
+    zip_safe=False,
+    platforms='any',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    install_requires=['gevent-websocket', 'netkit'],
+    url="https://github.com/dantezhu/ws_proxy",
+    license="MIT",
+    author="dantezhu",
+    author_email="zny2008@gmail.com",
+    description="tcp's websocket proxy",
+)

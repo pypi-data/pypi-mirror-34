84d18076f189cdbba83fe4628a65931d92dcd46c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:57 +0000

    Add package ie_mbdbl2018_tamarasamman/0.1/ie_mbdbl2018_tamarasamman-0.1.tar.gz

---
+++ packages/ie_mbdbl2018_tamarasamman/ie_mbdbl2018_tamarasamman-0.1.tar.gz/ie_mbdbl2018_tamarasamman-0.1/ie_mbdbl2018_tamarasamman/__init__.py
@@ -0,0 +1,3 @@
+def  datareader_doc():
+    """ function to return the string 'My first Python package that anyone call install it! How cool!!!' """
+    return("This is Tamara Samman first package that anyone can install it! How cool!!!")
---
+++ packages/ie_mbdbl2018_tamarasamman/ie_mbdbl2018_tamarasamman-0.1.tar.gz/ie_mbdbl2018_tamarasamman-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ie_mbdbl2018_tamarasamman',
+      version='0.1',  # Development release
+      description='Python Packaging Starting Kit - Simple Structure to creater your own package - for training purposes!',
+      url='https://github.com/manoelgadi/pythonpackagesample/',
+      author='Tamara Samman',
+      author_email='tamara.samman@student.ie.edu',
+      license='MIT',
+          packages=['ie_mbdbl2018_tamarasamman'],
+      zip_safe=False)

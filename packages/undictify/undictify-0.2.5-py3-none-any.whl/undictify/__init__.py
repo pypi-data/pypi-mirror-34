23f33fc1d2af960b5cbaee898a25e53dfd6a3725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:38:08 +0000

    Add package undictify/0.2.5/undictify-0.2.5-py3-none-any.whl

---
+++ packages/undictify/undictify-0.2.5-py3-none-any.whl/undictify/__init__.py
@@ -0,0 +1,11 @@
+from ._unpack import type_checked_call, type_checked_call_convert
+from ._unpack import type_checked_call_skip, type_checked_call_skip_convert
+from ._unpack import type_checked_apply, type_checked_apply_convert
+from ._unpack import type_checked_apply_skip, type_checked_apply_skip_convert
+
+name = "undictify"
+
+__author__ = "Tobias Hermann"
+__copyright__ = "Copyright 2018, Tobias Hermann"
+__email__ = "editgym@gmail.com"
+__license__ = "MIT"

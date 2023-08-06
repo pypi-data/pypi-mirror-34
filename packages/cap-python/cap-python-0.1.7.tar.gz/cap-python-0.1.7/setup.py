c4844f151cbcc38652418f68889bd7e71203f9e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:15 +0000

    Add package cap-python/0.1.7/cap-python-0.1.7.tar.gz

---
+++ packages/cap-python/cap-python-0.1.7.tar.gz/cap-python-0.1.7/setup.py
@@ -0,0 +1,27 @@
+#coding:utf-8
+# write  by  zhou
+from setuptools import  setup,find_packages
+setup(
+    name="cap-python",
+    packages=list(set(list(find_packages())+["twisted.plugins"])),
+    version='0.1.7',
+    install_requires=["setuptools",
+                      "MySQL-python",
+                      "twisted==15.3.0",
+                      "txscheduling",
+                      "psutil",
+                      "django==1.4.22",
+                      'netifaces',
+                      'PyCrypto',
+                      'cloudpickle'],
+    include_package_data=True,
+    url="https://github.com/zhoukunpeng504/cap-python",
+    author = 'zhoukunpeng',
+    author_email = "18749679769@163.com",
+    entry_points={
+          'console_scripts': ['cap-master-start = cap.sbin.master_start:main',
+                              'cap-master-stop = cap.sbin.master_stop:main',
+                              'cap-worker-start = cap.sbin.worker_start:main',
+                              'cap-worker-stop = cap.sbin.worker_stop:main']
+    }
+)

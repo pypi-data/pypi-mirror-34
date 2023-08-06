514333e6f9743c77af1f00d470a25e3d33733d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:39 +0000

    Add package simpegEMIP/0.0.11/simpegEMIP-0.0.11.tar.gz

---
+++ packages/simpegEMIP/simpegEMIP-0.0.11.tar.gz/simpegEMIP-0.0.11/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from setuptools import setup, Extension, find_packages
+import numpy
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Mathematics',
+    'Topic :: Scientific/Engineering :: Physics',
+    'Operating System :: Microsoft :: Windows',
+    'Operating System :: POSIX',
+    'Operating System :: Unix',
+    'Operating System :: MacOS',
+    'Natural Language :: English',
+]
+
+setup(
+    name="simpegEMIP",
+    version="0.0.11",
+    packages=find_packages(),
+    setup_requires=[
+        'setuptools>=18.0',
+        'cython',
+        'numpy'
+    ],
+    install_requires=[
+        'numpy>=1.7',
+        'scipy>=0.13',
+        'setuptools>=18.0',
+        'cython',
+        'pymatsolver>=0.1.1',
+        'ipython',
+        'matplotlib',
+        'properties>=0.5.2',
+        'vectormath>=0.2.0',
+        'SimPEG',
+        'discretize>=0.2.0',
+        'geoana>=0.0.4'
+    ],
+    ext_modules=[
+        Extension(
+            "getJpol",
+            sources=["simpegEMIP/TDEM/getJpol.pyx"],
+            include_dirs=[numpy.get_include()]
+        )
+    ],
+    author="Seogi Kang"
+)

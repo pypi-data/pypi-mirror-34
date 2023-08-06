3be8cb598855b1befc812de68bccb37b2a536737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:59 +0000

    Add package score.geoip/0.2.3/score.geoip-0.2.3.tar.gz

---
+++ packages/score.geoip/score.geoip-0.2.3.tar.gz/score.geoip-0.2.3/setup.py
@@ -0,0 +1,46 @@
+import os
+
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.rst')) as f:
+    README = f.read()
+
+setup(
+    name='score.geoip',
+    version='0.2.3',
+    description='IP Geolocalization for The SCORE Framework',
+    long_description=README,
+    author='strg.at',
+    author_email='support@strg.at',
+    url='http://strg.at',
+    keywords='web wsgi bfg pylons pyramid',
+    packages=['score', 'score.geoip'],
+    namespace_packages=['score'],
+    license='LGPL',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Framework :: Pyramid',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General '
+            'Public License v3 or later (LGPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'score.cli': [
+            'geoip = score.geoip.cli:main',
+        ]
+    },
+    install_requires=[
+        'score.init >= 0.3',
+        'dnspython3',
+    ],
+)

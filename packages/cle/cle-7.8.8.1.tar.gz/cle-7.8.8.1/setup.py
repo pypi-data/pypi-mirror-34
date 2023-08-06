ba244be06b3170399622e91954ba670c74d8620e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:10 +0000

    Add package cle/7.8.8.1/cle-7.8.8.1.tar.gz

---
+++ packages/cle/cle-7.8.8.1.tar.gz/cle-7.8.8.1/setup.py
@@ -0,0 +1,23 @@
+try:
+    from setuptools import setup
+    from setuptools import find_packages
+    packages = find_packages()
+except ImportError:
+    from distutils.core import setup
+    import os
+    packages = [x.strip('./').replace('/','.') for x in os.popen('find -name "__init__.py" | xargs -n1 dirname').read().strip().split('\n')]
+
+setup(
+    name='cle',
+    description='CLE Loads Everything (at least, many binary formats!) and provides a pythonic interface to analyze what they are and what they would look like in memory.',
+    version='7.8.8.1',
+    packages=packages,
+    install_requires=[
+        'pyelftools>=0.24',
+        'cffi',
+        'idalink',
+        'archinfo>=7.8.8.1',
+        'pyvex>=7.8.8.1',
+        'pefile',
+    ]
+)

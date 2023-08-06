d64636ef79529cccba0d27d844ca017491bbbb49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:26 +0000

    Add package ada-core/0.3.7/ada_core-0.3.7.tar.gz

---
+++ packages/ada-core/ada_core-0.3.7.tar.gz/ada_core-0.3.7/ada_core/exceptions.py
@@ -0,0 +1,55 @@
+"""
+Author: qiacai
+"""
+
+
+"""
+Exception Classes
+"""
+
+
+class AlgorithmNotFound(Exception):
+
+    """
+    Raise when algorithm can not be found
+    """
+    pass
+
+class AlgorithmOutputFormatError(Exception):
+
+    """
+    Raise when the output format of the algorithm is not correct
+    """
+    pass
+
+
+class ParametersNotPassed(Exception):
+
+    """
+    Raise when algorithm can not be properly initialized because some required parameters are not passed in init.
+    """
+    pass
+
+
+class InvalidDataFormat(Exception):
+
+    """
+    Raise when data has invalid format.
+    """
+    pass
+
+
+class InvalidDataType(Exception):
+
+    """
+    Raise when data has invalid data type.
+    """
+    pass
+
+
+class NotEnoughDataPoint(Exception):
+
+    """
+    Raise when there are not enough data points.
+    """
+    pass

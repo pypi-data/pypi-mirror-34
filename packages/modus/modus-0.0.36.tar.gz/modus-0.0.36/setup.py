c935593496a44728c43610f680745b1a52c86c3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:06 +0000

    Add package modus/0.0.36/modus-0.0.36.tar.gz

---
+++ packages/modus/modus-0.0.36.tar.gz/modus-0.0.36/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='modus',
+    author='cookkkie',
+    url='https://github.com/cookkkie/modus',
+    version='0.0.36',
+    packages=find_packages(),
+    license='MIT',
+    description='',
+    include_package_data=True,
+    install_requires=[]
+)

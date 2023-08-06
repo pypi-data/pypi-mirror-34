279787f869f7094982c308d8e0ceebe25f3ed783
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:30 +0000

    Add package vm/2.0.4/vm-2.0.4.tar.gz

---
+++ packages/vm/vm-2.0.4.tar.gz/vm-2.0.4/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('requirements.txt') as requirements_file:
+    requirements = requirements_file.read()  # type: str
+    install_requires = list(filter(
+        lambda it: it,
+        requirements.splitlines()))
+
+
+setup(
+    name='vm',
+    version='2.0.4',
+    entry_points={
+        "console_scripts": [
+            "version-manager = version_manager.launcher:launch",
+            "vm = version_manager.launcher:launch",
+        ]
+    },
+    description='vm: version manager',
+    long_description=readme,
+    author='Bogdan Mustiata',
+    author_email='bogdan.mustiata@gmail.com',
+    license='BSD',
+    install_requires=install_requires,
+    packages=[
+        'version_manager',
+        'version_manager.matchers'
+    ]
+)
+

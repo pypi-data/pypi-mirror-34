67843fa1f4b93e2279b98877aceef9589dfdcab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:31 +0000

    Add package django-mptt-admin/0.6.0/django-mptt-admin-0.6.0.tar.gz

---
+++ packages/django-mptt-admin/django-mptt-admin-0.6.0.tar.gz/django-mptt-admin-0.6.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+
+version = '0.6.0'
+
+setup(
+    name='django-mptt-admin',
+    version=version,
+    packages=find_packages(),
+    license='Apache License, Version 2.0',
+    include_package_data=True,
+    zip_safe=False,
+    author='Marco Braak',
+    author_email='mbraak@ridethepony.nl',
+    install_requires=['django-mptt', 'six'],
+    description='Django-mptt-admin provides a nice Django Admin interface for Mptt models',
+    long_description='Django-mptt-admin provides a nice Django Admin interface for django-mptt models. Please see https://github.com/mbraak/django-mptt-admin',
+    url='https://github.com/mbraak/django-mptt-admin',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy"
+    ]
+)

fa664dd435d8c4d23f8ed3cf68c0cbdd2033be1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:00 +0000

    Add package wellcome-platform-cli/0.2.1/wellcome-platform-cli-0.2.1.tar.gz

---
+++ packages/wellcome-platform-cli/wellcome-platform-cli-0.2.1.tar.gz/wellcome-platform-cli-0.2.1/src/platformcli/cli.py
@@ -0,0 +1,85 @@
+# -*- encoding: utf-8
+
+import os
+import subprocess
+import sys
+
+import click
+
+from .junkdrawer import git, run_formatting
+from .superprocess import check_call
+
+
+@click.group()
+def cli():
+    """platform-cli is a command-line tool for managing build tasks in
+    the Wellcome Digital Platform repos."""
+
+
+@cli.command()
+def format():
+    """Run autoformatting commands."""
+    run_formatting(run_all=True)
+
+
+@cli.command()
+def autoformat():
+    """Run autoformatting in Travis."""
+    travis_event_type = os.environ['TRAVIS_EVENT_TYPE']
+    assert travis_event_type == 'pull_request'
+    branch_name = os.environ['TRAVIS_PULL_REQUEST_BRANCH']
+
+    run_formatting()
+
+    # If there are any changes, push to GitHub immediately and fail the
+    # build.  This will abort the remaining jobs, and trigger a new build
+    # with the reformatted code.
+    if subprocess.call(['git', 'diff', '--exit-code']):
+        print('*** There were changes from formatting, creating a commit')
+
+        # We checkout the branch before we add the commit, so we don't
+        # include the merge commit that Travis makes.
+        git('fetch', 'ssh-origin')
+        git('checkout', branch_name)
+
+        git('add', '--verbose', '--update')
+        git('commit', '-m', 'Apply auto-formatting rules')
+        git('push', 'ssh-origin', 'HEAD:%s' % branch_name)
+
+        # We exit here to fail the build, so Travis will skip to the next
+        # build, which includes the autoformat commit.
+        sys.exit(1)
+    else:
+        print('*** There were no changes from auto-formatting')
+        sys.exit(0)
+
+
+@cli.group()
+def sbt():
+    """sbt-related commands."""
+
+
+@sbt.command()
+def test():
+    """Run tests with 'sbt test'.  This command also starts Docker containers
+    for testing, if required.
+    """
+    if os.path.exists('docker-compose.yml'):
+        check_call('sbt', 'dockerComposeUp')
+        check_call('sbt', 'test')
+        check_call('sbt', 'dockerComposeStop')
+    else:
+        check_call('sbt', 'test')
+
+
+# @cli.command()
+# def check_release_file():
+#     """Look for a correctly-formatted release file."""
+#     if has_source_changes():
+#         if not has_release():
+#             print(
+#                 'There are source changes but no RELEASE.md. Please create '
+#                 'one to describe your changes.'
+#             )
+#             sys.exit(1)
+#         parse_release_file()
---
+++ packages/wellcome-platform-cli/wellcome-platform-cli-0.2.1.tar.gz/wellcome-platform-cli-0.2.1/src/platformcli/junkdrawer.py
@@ -0,0 +1,42 @@
+# -*- encoding: utf-8
+"""
+A miscellaneous collection of functions and helpers for use in the CLI.
+Named junkdrawer.py because I haven't prganised it properly.
+
+https://twitter.com/betsythemuffin/status/1003313844108824584
+
+"""
+
+from .superprocess import check_call, check_output
+
+
+def git(*args):
+    check_call('git', *args)
+
+
+def get_changed_paths(*args):
+    """
+    Returns a set of changed paths in a given commit range.
+
+    :param commit_range: Arguments to pass to ``git diff``.
+    """
+    diff_output = check_output('git', 'diff', '--name-only', *args)
+
+    return set([
+        line.strip()
+        for line in diff_output.splitlines()
+    ])
+
+
+def run_formatting(run_all=False):
+    """
+    Run all the formatting scripts.
+    """
+    if not run_all:
+        changed_paths = get_changed_paths('HEAD', 'master')
+
+    if run_all or any(f.endswith(('.sbt', '.scala')) for f in changed_paths):
+        check_call('sbt', 'scalafmt')
+
+    if run_all or any(f.endswith('.tf') for f in changed_paths):
+        check_call('terraform', 'fmt')

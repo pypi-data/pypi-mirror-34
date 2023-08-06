de632d78ed0d39446124c8a870b744de8023e4ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:04 +0000

    Add package adlib/1.2.1/adlib-1.2.1-py3-none-any.whl

---
+++ packages/adlib/adlib-1.2.1-py3-none-any.whl/data_reader/binary_input.py
@@ -0,0 +1,188 @@
+from typing import List
+from scipy.sparse import csr_matrix
+from data_reader.real_input import RealFeatureVector
+
+"""
+Created Binary FeatureVector and Instance data structures.
+Support converting the emaildataset object(the csr_matrix) into list of instances.
+"""
+
+
+class BinaryFeatureVector(object):
+    """Feature vector data structure.
+
+    Contains sparse representation of boolean features.
+    Defines basic methods for manipulation and data format changes.
+
+        """
+
+    def __init__(self, num_features: int, feature_indices: List[int]):
+        """Create a feature vector given a set of known features.
+
+        Args:
+                num_features (int): Total number of features.
+                feature_indices (List[int]): Indices of each feature present in instance.
+
+                """
+        self.indptr = [0, len(feature_indices)]  # type: List[int]
+        self.feature_count = num_features  # type: int
+        self.data = [1] * len(feature_indices)  # type: List[int]
+        self.indices = feature_indices  # type: List[int]
+
+    def copy(self, feature_vector):
+        return BinaryFeatureVector(feature_vector.feature_count, feature_vector.indices)
+
+    def __iter__(self):
+        return iter(self.indices)
+
+    def __getitem__(self, key):
+        return self.indices[key]
+
+    def __len__(self):
+        return len(self.indices)
+
+    def get_feature_count(self):
+        """Return static number of features.
+
+                """
+        return self.feature_count
+
+    def get_feature(self, index: int) -> int:
+        """Return value of feature at index
+
+                Args:
+                        index (int): Feature index.
+
+                """
+        if index in self.indices:
+            return 1
+        else:
+            return 0
+
+    def change_bit(self, index, feature):
+        """
+        Change the bit only if the feature is different from the current feature
+        :param index:
+        :param feature:
+        """
+        if feature == 0:
+            if index not in self.indices:
+                return
+            else:
+                self.indices.remove(index)
+                self.indptr[1] -= 1
+                self.data.remove(1)
+        if feature == 1:
+            if index in self.indices:
+                return
+            else:
+                self.indices.append(index)
+                self.indices.sort(reverse=True)
+                self.indptr[1] += 1
+                self.data.append(1)
+
+    def flip_bit(self, index):
+        """Flip feature at given index.
+
+        Switches the current value at the index to the opposite value.
+        {0 --> 1, 1 --> 0}
+
+                Args:
+                        index (int): Index of feature update.
+
+                """
+        if index in self.indices:
+            self.indices.remove(index)
+            self.indptr[1] -= 1
+            self.data.remove(1)
+        else:
+            self.data.append(1)
+            self.indices.append(index)
+            self.indptr[1] += 1
+            self.indices.sort(reverse=True)
+
+    def get_csr_matrix(self) -> csr_matrix:
+        """Return feature vector represented by sparse matrix.
+
+                """
+        data = [1] * len(self.indices)
+        indices = self.indices
+        indptr = [0, len(self.indices)]
+        return csr_matrix((data, indices, indptr), shape=(1, self.feature_count))
+
+    def feature_difference(self, xa) -> List:
+        y_array = self.get_csr_matrix()
+        xa_array = xa.get_csr_matrix()
+
+        C_y = (y_array - xa_array).indices
+
+        return C_y
+
+
+class Instance(object):
+    """Instance data structure.
+
+    Container for feature vector and mapped label.
+
+        """
+
+    def __init__(self, label: int, feature_vector):
+        """Create an instance from an existing feature vector.
+
+        Args:
+                label (int): Classification (-1/1).
+                feature_vector (BinaryFeatureVector): Underlying sparse feature representation.
+
+                """
+        self.label = label  # type: int
+        self.feature_vector = feature_vector
+
+    def get_label(self):
+        return self.label
+
+    def set_label(self, val):
+        self.label = val
+
+    def get_feature_vector(self):
+        """Return underlying feature vector.
+
+                """
+        return self.feature_vector
+
+    def get_feature_count(self):
+        """
+        :return: Number of features in the underlying feature vector
+        """
+        return self.feature_vector.get_feature_count()
+
+    def get_csr_matrix(self):
+        """
+        :return: csr_matrix of the underlying feature vector
+        """
+        return self.feature_vector.get_csr_matrix()
+
+    def get_feature_vector_cost(self, goal_instance):
+        """
+           Get the feature differences between two instances.
+           Sum all the values up.
+        :param goal_vector:
+        :return:  a val indicating the differences
+        """
+        feature_difference = self.get_feature_vector().feature_difference(
+            goal_instance.get_feature_vector())
+        sum = 0
+        for index in range(len(feature_difference)):
+            sum += abs(feature_difference[index])
+        return sum
+
+    def flip(self, index, value):
+        """
+          Chnange the bit at given index
+        :param index:
+        :param value:
+        :return:
+        """
+        if type(self.get_feature_vector()) == RealFeatureVector:
+            self.get_feature_vector().flip_val(index, value)
+        else:
+            self.get_feature_vector().change_bit(index, value)
---
+++ packages/adlib/adlib-1.2.1-py3-none-any.whl/data_reader/operations.py
@@ -0,0 +1,211 @@
+from typing import List
+from scipy.sparse import csr_matrix
+from data_reader.binary_input import Instance, BinaryFeatureVector
+from data_reader.real_input import RealFeatureVector
+from sklearn.feature_selection import SelectFromModel
+from sklearn.feature_selection import chi2
+from sklearn.feature_selection import SelectKBest
+from data_reader.dataset import EmailDataset
+from sklearn.ensemble import ExtraTreesClassifier
+import math
+
+
+def fv_equals(fv1, fv2):
+    if fv1.indices != fv2.indices:
+        return False
+    for i in range(len(fv1.data)):
+        if fv1.data[i] != fv2.data[i]:
+            return False
+    return True
+
+
+def find_centroid(instances: List[Instance]):
+    num_features = instances[0].get_feature_vector().feature_count
+    indices = []
+    data = []
+    for i in range(num_features):
+        sum = 0
+        for instance in instances:
+            if instance.label == -1:
+                sum += instance.get_feature_vector().get_feature(i)
+        sum /= num_features
+        if sum != 0:
+            indices.append(i)
+            data.append(sum)
+    return Instance(-1, RealFeatureVector(num_features, indices, data))
+
+
+def find_max(instances: List[Instance]):
+    """
+    TODO: find_max and find_min should return an array of length num_features, instead of an object
+    :param instances:
+    :return:
+    """
+    num_features = instances[0].get_feature_vector().feature_count
+    max_val_list = []
+    for i in range(num_features):
+        max = 0
+        for instance in instances:
+            value = instance.get_feature_vector().get_feature(i)
+            if value >= max:
+                max = value
+        max_val_list.append(max)
+    return max_val_list
+
+    # num_features = instances[0].get_feature_vector().feature_count
+    # indices = []
+    # data = []
+    # for i in range(num_features):
+    #    max = 0
+    #    for instance in instances:
+    #        value = instance.get_feature_vector().get_feature(i)
+    #        if value >= max:
+    #            max = value
+    #    if max != 0:
+    #        indices.append(i)
+    #        data.append(max)
+    # return RealFeatureVector(num_features, indices, data)
+
+
+def find_min(instances: List[Instance]):
+    num_features = instances[0].get_feature_vector().feature_count
+    min_val_list = []
+    for i in range(num_features):
+        min = 10000
+        for instance in instances:
+            value = instance.get_feature_vector().get_feature(i)
+            if value <= min:
+                min = value
+        min_val_list.append(min)
+    return min_val_list
+
+
+def sparsify(instances: List[Instance]):
+    """
+    Return sparse matrix representation of list of instances
+    :param instances: binary or real value instances
+    :return:
+    """
+    num_features = instances[0].get_feature_vector().feature_count
+    labels = []
+    indptr = [0]
+    indices = []
+    ind_ = 0
+    data = []
+    for instance in instances:
+        labels.append(instance.get_label())
+        fv = instance.get_feature_vector()
+        indptr.append(indptr[ind_] + len(fv.indices))
+        ind_ += 1
+        indices += fv.indices
+        data.extend(instance.get_feature_vector().data)
+
+    return (labels, csr_matrix((data, indices, indptr), shape=(len(instances), num_features)))
+
+
+def csr_mat_to_instances(csr_mat, labels, binary=False):
+    """
+    Return a list of instances
+    :param nd_arr:
+    :param labels:
+    :return:
+    """
+    data = csr_mat.data
+    indices = csr_mat.indices
+    indptr = csr_mat.indptr
+    instance_len, num_features = csr_mat.shape
+    instance_lst = []
+    for i in range(instance_len):
+        label = labels[i]
+        instance_data = data[indptr[i]:indptr[i + 1]]
+        instance_indices = list(indices[indptr[i]:indptr[i + 1]])
+        if binary:
+            instance_lst.append(
+                Instance(label, BinaryFeatureVector(num_features, instance_indices)))
+        else:
+            instance_lst.append(
+                Instance(label, RealFeatureVector(num_features, instance_indices, instance_data)))
+    return instance_lst
+
+
+def nd_arr_to_instances(nd_arr, labels=None, binary=False):
+    """
+    Return a list of instances
+    :param nd_arr:
+    :param labels:
+    :param binary:
+    :return:
+    """
+    num_instances = nd_arr.shape[0]
+    if labels is None:
+        labels = nd_arr[:, :1]
+        data = nd_arr[:, 1:]
+        num_features = nd_arr.shape[1] - 1
+    else:
+        data = nd_arr
+        num_features = nd_arr.shape[1]
+
+    instance_lst = []
+    for i in range(num_instances):
+        if binary:
+            mat_indices = [x for x in range(0, num_features) if data[i][x] != 0]
+            instance_lst.append(
+                Instance(labels[i], BinaryFeatureVector(num_instances, mat_indices)))
+        else:
+            mat_indices = [x for x in range(0, num_features) if data[i][x] != 0]
+            mat_data = [data[i][x] for x in range(0, num_features) if data[0][x] != 0]
+            instance_lst.append(
+                Instance(labels[i], RealFeatureVector(num_instances, mat_indices, mat_data)))
+    return instance_lst
+
+
+def load_dataset(emailData: EmailDataset) -> List[Instance]:
+    """
+    Conversion from dataset object into a list of instances
+    :param emailData:
+    """
+
+    instances = []
+    num_features = emailData.shape[1]
+    indptr = emailData.features.indptr
+    indices = emailData.features.indices
+    data = emailData.features.data
+    for i in range(0, emailData.num_instances):
+        if emailData.binary:
+            tmp_vector = BinaryFeatureVector(num_features,
+                                             indices[indptr[i]:indptr[i + 1]].tolist())
+        else:
+            instance_data = data[indptr[i]:indptr[i + 1]].tolist()
+            tmp_vector = RealFeatureVector(num_features, indices[indptr[i]:indptr[i + 1]].tolist(),
+                                           instance_data)
+        instances.append(Instance(emailData.labels[i], tmp_vector))
+    return instances
+
+
+def mean(numbers):
+    return sum(numbers) / float(len(numbers))
+
+
+def stdev(numbers):
+    means = mean(numbers)
+    variance = sum([pow(x - means, 2) for x in numbers]) / float(len(numbers) - 1)
+    return math.sqrt(variance)
+
+
+def summarize(instances):
+    summaries = []
+    for i in range(instances[0].get_feature_count()):
+        data = []
+        for instance in instances:
+            data.append(instance.get_feature_vector().get_feature(i))
+        summaries.append((mean(data), stdev(data)))
+    return summaries
+
+
+def feature_selection(instances, max_features, selection_type="chi2", binary=False):
+    label, sparse_data = sparsify(instances)
+    data = sparse_data.toarray()
+    if selection_type == "chi2":
+        data_new = SelectKBest(chi2, k=max_features).fit_transform(data, label)
+        return nd_arr_to_instances(data_new, label, binary)
+    # TODO:ADD Other data selection methods if necessary

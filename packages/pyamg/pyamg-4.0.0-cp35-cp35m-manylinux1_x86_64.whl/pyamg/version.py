754e17c2199754e7ee5787a55ddb0187dee88311
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:05 +0000

    Add package pyamg/4.0.0/pyamg-4.0.0-cp35-cp35m-manylinux1_x86_64.whl

---
+++ packages/pyamg/pyamg-4.0.0-cp35-cp35m-manylinux1_x86_64.whl/pyamg/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM SETUP.PY
+short_version = '4.0.0'
+version = '4.0.0'
+full_version = '4.0.0'
+git_revision = '6411d62dd5fe848f9dd8071d5db403bdb264174c'
+release = True
+if not release:
+    version = full_version

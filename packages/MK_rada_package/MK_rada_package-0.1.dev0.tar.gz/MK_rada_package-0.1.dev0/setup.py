d323a0757496cc170bba48e7f0d9ef787077246b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:58 +0000

    Add package MK_rada_package/0.1.dev0/MK_rada_package-0.1.dev0.tar.gz

---
+++ packages/MK_rada_package/MK_rada_package-0.1.dev0.tar.gz/MK_rada_package-0.1.dev0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='MK_rada_package',
+      version=version,
+      description="rada_test",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Mykola Kantsir',
+      author_email='',
+      url='',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )

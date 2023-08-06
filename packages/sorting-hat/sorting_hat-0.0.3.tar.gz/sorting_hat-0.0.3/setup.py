549439a2d80326cb5c7609c116430485d2a59f33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:19 +0000

    Add package sorting-hat/0.0.3/sorting_hat-0.0.3.tar.gz

---
+++ packages/sorting-hat/sorting_hat-0.0.3.tar.gz/sorting_hat-0.0.3/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sorting_hat",
+    version="0.0.3",
+    author="Allison Seiden",
+    author_email="ahseiden@gmail.com",
+    description="Sorts indels into mutational classes",
+    long_description=long_description,
+    url="https://github.com/allisonseiden/sorting_hat",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    entry_points={
+        "console_scripts": ['sorting_hat=sorting_hat.sorting_hat:main']
+        },
+    install_requires=[
+          'pandas', 'numpy'
+    ],
+    python_requires='>=3',
+)

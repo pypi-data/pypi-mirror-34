0b63abea1d0d98a97233125856721f27a8bd6951
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:39 +0000

    Add package easier/0.1.7/easier-0.1.7.tar.gz

---
+++ packages/easier/easier-0.1.7.tar.gz/easier-0.1.7/easier/__init__.py
@@ -0,0 +1,15 @@
+from .version import __version__
+
+from .timer import Timer
+from .clock import Clock
+from .param_state import ParamState
+from .item import Item
+from .simple_cron import Cron
+from .memory import mem_show, mem_get
+from .hvtools import shade, hv_to_html
+from .print_catcher import PrintCatcher
+from .plotting import figure
+
+
+
+

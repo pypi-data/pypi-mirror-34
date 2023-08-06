f7ffe62b569e5c928518545ed7e84c8c660a56e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:58 +0000

    Add package ssml-builder/1.0/ssml_builder-1.0.tar.gz

---
+++ packages/ssml-builder/ssml_builder-1.0.tar.gz/ssml_builder-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ssml_builder',
+    version='1.0',
+    packages=['ssml_builder'],
+    url='',
+    license='Free',
+    author='Yuta Fujisawa',
+    author_email='',
+    description='SSML Builder for Alexa'
+)

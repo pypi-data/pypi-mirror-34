7dafa193ae27fa4ed8cf7f7fc3bc664a1430739d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:14 +0000

    Add package anotherpdfmerger/0.0.4/anotherpdfmerger-0.0.4.tar.gz

---
+++ packages/anotherpdfmerger/anotherpdfmerger-0.0.4.tar.gz/anotherpdfmerger-0.0.4/anotherpdfmerger/version.py
@@ -0,0 +1,5 @@
+"""Hold program info for anotherpdfmerger."""
+
+NAME = "anotherpdfmerger"
+VERSION = "0.0.4"
+DESCRIPTION = "merge PDFs and generate bookmarks"
---
+++ packages/anotherpdfmerger/anotherpdfmerger-0.0.4.tar.gz/anotherpdfmerger-0.0.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from anotherpdfmerger.version import DESCRIPTION, NAME, VERSION
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION.capitalize(),
+    url='https://github.com/mwiens91/anotherpdfmerger',
+    author='Matt Wiens',
+    author_email='mwiens91@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    data_files=[('/usr/local/man/man1', ['anotherpdfmerger.1']),],
+    packages=['anotherpdfmerger'],
+    entry_points={
+        'console_scripts': ['anotherpdfmerger = anotherpdfmerger.anotherpdfmerger:main'],
+    },
+    install_requires=[
+        'PyPDF2',
+    ],
+)

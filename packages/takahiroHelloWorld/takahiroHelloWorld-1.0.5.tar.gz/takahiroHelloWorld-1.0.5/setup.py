d411d123d3a97b2de69372eb33155a7c1044ef2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:19:14 +0000

    Add package takahiroHelloWorld/1.0.5/takahiroHelloWorld-1.0.5.tar.gz

---
+++ packages/takahiroHelloWorld/takahiroHelloWorld-1.0.5.tar.gz/takahiroHelloWorld-1.0.5/setup.py
@@ -0,0 +1,30 @@
+import os
+import setuptools
+# from setuptools import setup
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name = "takahiroHelloWorld",
+    version = "1.0.5",  # major, minor, micro
+    author = "sakurai",
+    author_email = "sakurai@gamil.com",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    packages = setuptools.find_packages(),
+    classifiers = (
+        "Programming Language :: Python :: 3.3", 
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ),
+    zip_safe = False,
+    entry_points = {
+        "console_scripts": [
+            "my_say_hello = helloWorld.hello:main",
+        ]
+    }
+)

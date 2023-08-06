3ba4e9da05dc514b6c0393dee90fa8a4fb850e2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:02 +0000

    Add package nodejs-codegen/0.1.1/nodejs_codegen-0.1.1.tar.gz

---
+++ packages/nodejs-codegen/nodejs_codegen-0.1.1.tar.gz/nodejs_codegen-0.1.1/nodejs_codegen/code/app.py
@@ -0,0 +1,14 @@
+from codegenhelper import debug
+import os
+from code_engine import publish
+
+def get_template_path():
+    return os.path.join(os.path.split(__file__)[0], "..", "templates")
+
+def gen(subscribe_name,  data, output_path):
+    publish(debug(get_template_path(), "gen:template_path"), \
+            subscribe_name, \
+            data,
+            debug(output_path, 'gen:output_path')
+            )
+    

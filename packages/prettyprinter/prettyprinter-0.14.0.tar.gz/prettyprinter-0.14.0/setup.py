f71451c4fe7e0e5c1ad01552fe21710624d11fb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:01 +0000

    Add package prettyprinter/0.14.0/prettyprinter-0.14.0.tar.gz

---
+++ packages/prettyprinter/prettyprinter-0.14.0.tar.gz/prettyprinter-0.14.0/setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'colorful>=0.4.0',
+    'Pygments>=2.2.0',
+]
+
+setup_requirements = [
+    'pytest-runner',
+    # TODO(tommikaikkonen): put setup requirements (distutils extensions, etc.) here
+]
+
+test_requirements = [
+    'pytest',
+    'hypothesis',
+    'dataclasses',
+    'django>=1.10.8',
+    'requests',
+    'attrs',
+    'IPython',
+    'pytz',
+    'tox-pyenv',
+    'pytest-django',
+    'pytest-pythonpath',
+]
+
+setup(
+    name='prettyprinter',
+    version='0.14.0',
+    description="Syntax-highlighting, declarative and composable pretty printer for Python 3.5+",
+    long_description=readme + '\n\n' + history,
+    author="Tommi Kaikkonen",
+    author_email='kaikkonentommi@gmail.com',
+    url='https://github.com/tommikaikkonen/prettyprinter',
+    packages=find_packages(include=['prettyprinter', 'prettyprinter.extras']),
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    keywords='prettyprinter',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+)

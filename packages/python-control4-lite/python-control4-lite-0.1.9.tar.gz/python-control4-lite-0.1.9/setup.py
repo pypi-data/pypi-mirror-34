6cc38dd9117bbd038eae798630042bb7d99ca656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:44 +0000

    Add package python-control4-lite/0.1.9/python-control4-lite-0.1.9.tar.gz

---
+++ packages/python-control4-lite/python-control4-lite-0.1.9.tar.gz/python-control4-lite-0.1.9/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import io
+from setuptools import setup
+
+version = '0.1.9'
+
+setup(
+    name='python-control4-lite',
+    version=version,
+    description='Python Bindings for Control4 API',
+    keywords='control4',
+    author='Aleksi Asikainen',
+    author_email='aleksi.asikainen@solera.com',
+    url='https://github.com/r3pi/python-control4-lite',
+    packages=['control4',],
+    install_requires=['aiohttp>=3.0.0',],
+    license='MIT',
+    long_description=io.open('README.md', encoding='UTF-8').read(),
+    test_suite='nose.collector',
+    tests_require=['nose', 'aiounittest'],
+    python_requires='>=3.6.0'
+)

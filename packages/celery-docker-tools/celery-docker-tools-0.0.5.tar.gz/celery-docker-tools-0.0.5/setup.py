cfcfbea2edbbd4d3dd500c1e2a27c31c3f8dd094
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:45 +0000

    Add package celery-docker-tools/0.0.5/celery-docker-tools-0.0.5.tar.gz

---
+++ packages/celery-docker-tools/celery-docker-tools-0.0.5.tar.gz/celery-docker-tools-0.0.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='celery-docker-tools',
+    version='0.0.5',
+    packages=find_packages(),
+    author='Ellis Percival',
+    author_email='celery-docker-tools_pypi@failcode.co.uk',
+    description='Celery Docker tools',
+    license='MIT',
+    install_requires=['docker', 'celery']
+)

48e30adff650c1bc6901075c73b849b69d6cbac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:40 +0000

    Add package dataflows/0.0.7/dataflows-0.0.7-py2.py3-none-any.whl

---
+++ packages/dataflows/dataflows-0.0.7-py2.py3-none-any.whl/dataflows/processors/load.py
@@ -0,0 +1,31 @@
+import os
+
+from datapackage import Package, Resource
+from .. import DataStreamProcessor
+
+
+class load(DataStreamProcessor):
+
+    def __init__(self, path, name=None, **options):
+        super(load, self).__init__()
+        self.path = path
+        self.options = options
+        self.name = name
+
+    def process_datapackage(self, dp: Package):
+        if os.path.exists(self.path):
+            base_path = os.path.dirname(self.path)
+            self.path = os.path.basename(self.path)
+        else:
+            base_path = None
+        self.res = Resource(dict(path=self.path, profile='tabular-data-resource', **self.options), base_path=base_path)
+        self.res.infer(confidence=1, limit=1000)
+        if self.name is not None:
+            self.res.descriptor['name'] = self.name
+        self.res.descriptor['path'] = '{name}.{format}'.format(**self.res.descriptor)
+        dp.add_resource(self.res.descriptor)
+        return dp
+
+    def process_resources(self, resources):
+        yield from super(load, self).process_resources(resources)
+        yield self.res.iter(keyed=True)

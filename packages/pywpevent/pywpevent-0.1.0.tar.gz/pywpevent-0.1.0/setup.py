d83d3c290e922863c4a9a4516f27c825ee87bc38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:30 +0000

    Add package pywpevent/0.1.0/pywpevent-0.1.0.tar.gz

---
+++ packages/pywpevent/pywpevent-0.1.0.tar.gz/pywpevent-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='pywpevent',
+    packages=['pywpevent'],
+    version='0.1.0',
+    description='Simple event management like wordpress',
+    author='Praiwan N.',
+    author_email='npraiwan@outlook.com',
+    url='https://github.com/praiwann/pywpevent',
+    keywords=['wordpress', 'event', 'action', 'filter'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3'
+    ],
+)

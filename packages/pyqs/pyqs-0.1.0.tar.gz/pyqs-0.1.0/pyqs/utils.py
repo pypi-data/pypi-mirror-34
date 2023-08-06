e6fb7212140102e0bb38011de25e7d7cdbcc5223
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:36 +0000

    Add package pyqs/0.1.0/pyqs-0.1.0.tar.gz

---
+++ packages/pyqs/pyqs-0.1.0.tar.gz/pyqs-0.1.0/pyqs/utils.py
@@ -0,0 +1,28 @@
+import base64
+import json
+import pickle
+
+
+def decode_message(message):
+    message_body = message['Body']
+    json_body = json.loads(message_body)
+    if 'task' in message_body:
+        return json_body
+    else:
+        # Fallback to processing celery messages
+        return decode_celery_message(json_body['body'])
+
+
+def decode_celery_message(json_task):
+    message = base64.b64decode(json_task)
+    try:
+        return json.loads(message)
+    except ValueError:
+        pass
+    return pickle.loads(message)
+
+
+def function_to_import_path(function, override=False):
+    if override:
+        return function
+    return "{}.{}".format(function.__module__, function.__name__)

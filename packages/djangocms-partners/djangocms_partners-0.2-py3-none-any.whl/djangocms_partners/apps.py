94633d91b71f2f685cd9bab978b057c771c80e1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:05 +0000

    Add package djangocms-partners/0.2/djangocms_partners-0.2-py3-none-any.whl

---
+++ packages/djangocms-partners/djangocms_partners-0.2-py3-none-any.whl/djangocms_partners/apps.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.utils.translation import ugettext as _
+from django.apps import AppConfig
+
+
+class DjangocmsPartnersConfig(AppConfig):
+    name = 'djangocms_partners'
+    verbose_name = _('Partners')

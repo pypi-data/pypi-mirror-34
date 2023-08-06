2eede8492a175790e6b2ac6241797ddebf8b4b72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:28 +0000

    Add package sqlcop/0.10.9/sqlcop-0.10.9.tar.gz

---
+++ packages/sqlcop/sqlcop-0.10.9.tar.gz/sqlcop-0.10.9/sqlcop/checks/__init__.py
@@ -0,0 +1,12 @@
+import sqlparse
+
+
+def _patch():
+    from sqlalchemy.dialects.mysql.base import RESERVED_WORDS
+
+    for key in list(sqlparse.keywords.KEYWORDS.keys()):
+        if key not in RESERVED_WORDS:
+            del sqlparse.keywords.KEYWORDS[key]
+
+
+_patch()

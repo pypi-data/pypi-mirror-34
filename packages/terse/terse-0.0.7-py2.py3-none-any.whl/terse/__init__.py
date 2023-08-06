27ee9658cd5ef8a6351004e5997daacaf75fcfca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:33 +0000

    Add package terse/0.0.7/terse-0.0.7-py2.py3-none-any.whl

---
+++ packages/terse/terse-0.0.7-py2.py3-none-any.whl/terse/__init__.py
@@ -0,0 +1,9 @@
+# Change Directory
+from .in_directory import InDirectory
+from .in_directory import in_dir
+# Invoke
+from .invoke import main
+# On Exit
+from .on_exit import on_raised
+from .on_exit import on_returned
+from .on_exit import no_raise

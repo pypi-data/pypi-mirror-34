a4228968f3995d68df434826c3672ff45517feda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:39 +0000

    Add package numkit/1.1.2/numkit-1.1.2.tar.gz

---
+++ packages/numkit/numkit-1.1.2.tar.gz/numkit-1.1.2/README.rst
@@ -0,0 +1,57 @@
+======
+numkit
+======
+
+|docs| |build| |cov|
+
+**numkit** is a small collection of numerical helper functions and
+classes ("numerical first aid kit") that have been useful for
+GromacsWrapper_ and related projects.
+
+Originally, **numkit** was part of GromacsWrapper_ but was split off
+into its own small package in version 0.7.0 of GromacsWrapper.
+
+.. _GromacsWrapper: https://gromacswrapper.readthedocs.org/
+
+
+Getting numkit
+==============
+
+Install from PyPi with ::
+
+    pip install numkit
+
+The source code is available under the BSD 3-clause license (see file
+LICENSE) from GitHub in the repository
+https://github.com/Becksteinlab/numkit ::
+
+  git clone https://github.com/Becksteinlab/numkit
+
+Python 3
+========
+
+Python 3 support for this package is in alpha state
+
+
+Contributing
+============
+
+Contributions are very welcome in all forms, especially through issue
+reports and pull requests.
+
+
+.. |build| image:: https://travis-ci.org/Becksteinlab/numkit.svg?branch=master
+   :target: https://travis-ci.org/Becksteinlab/numkit
+   :alt: Build status
+   :scale: 100%
+   
+.. |docs| image:: https://readthedocs.org/projects/numkit/badge/?version=latest
+   :target: https://numkit.readthedocs.org/en/latest/?badge=latest
+   :alt: Documentation
+   :scale: 100%
+   
+.. |cov| image:: https://codecov.io/gh/Becksteinlab/numkit/branch/master/graph/badge.svg
+   :target: https://codecov.io/gh/Becksteinlab/numkit?branch=master
+   :alt: Code Coverage
+   :scale: 100%
+
---
+++ packages/numkit/numkit-1.1.2.tar.gz/numkit-1.1.2/setup.py
@@ -0,0 +1,50 @@
+#! /usr/bin/python
+"""Setuptools-based setup script for numkit.
+
+For a basic installation just type the command::
+
+  python setup.py install
+
+"""
+
+from setuptools import setup, find_packages
+import versioneer
+
+setup(name='numkit',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      description='numerical first aid kit',
+      author='Oliver Beckstein',
+      author_email='orbeckst@gmail.com',
+      url = 'https://github.com/Becksteinlab/numkit',
+      project_urls={'Documentation': 'https://numkit.readthedocs.org/',
+                    'Issue Tracker': 'https://github.com/Becksteinlab/numkit/issues',
+                    'Source': 'https://github.com/Becksteinlab/numkit',
+      },
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      scripts=[],
+      license='BSD',
+      long_description=open('README.rst').read(),
+      long_description_content_type='text/x-rst',
+      tests_require = ['numpy>=1.9', 'pytest'],
+      install_requires=['numpy>=1.9', 'scipy>=1.0', 'six']
+)

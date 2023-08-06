fb67f1caee63441229b9e729e3d125c55c5a0edb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:44 +0000

    Add package messages/0.4.3/messages-0.4.3.tar.gz

---
+++ packages/messages/messages-0.4.3.tar.gz/messages-0.4.3/messages/text.py
@@ -0,0 +1,115 @@
+"""
+Module designed to make creating and sending text messages easy.
+
+1.  Twilio
+    - Uses the Twilio API to send text messages.
+    - Must have an account_sid, auth_token, and a twilio phone number
+      in order to use.
+    - Go to https://www.twilio.com to register.
+"""
+
+import sys
+
+import requests
+
+from ._config import configure
+from ._eventloop import MESSAGELOOP
+from ._interface import Message
+from ._utils import timestamp
+
+
+class Twilio(Message):
+    """
+    Create and send text SMS/MMS text messages using the Twilio API.
+
+    Args:
+        :from_: (str) phone number of originating text, e.g. '+15558675309'
+        :to: (str) phone number of destination text, e.g. '+15558675309'
+        :acct_sid: (str) api credential from twilio
+        :auth_token: (str) api credential from twilio
+        :body: (str) message to send
+        :attachments: (str) url of any image to send along with message
+        :profile: (str) use a separate account profile specified by name
+        :save: (bool) save pertinent values in the messages config file,
+            such as from_, acct_sid, auth_token (encrypted keyring) to make
+            sending messages faster.
+
+    Attributes:
+        :client: (Client) twilio.rest client for authentication
+        :sid: (str) return value from send, record of sent message
+
+    Usage:
+        Create a text message (SMS/MMS) object with required Args above.
+        Send text message with self.send() or self.send_async() methods.
+
+    Notes:
+        For API description:
+        https://www.twilio.com/docs/api/messaging/send-messages
+    """
+
+    def __init__(
+        self, from_=None, to=None, acct_sid=None, auth_token=None,
+        body='', attachments=None, profile=None, save=False, verbose=False
+    ):
+
+        config_kwargs = {'from_': from_, 'acct_sid': acct_sid,
+                'auth_token': auth_token, 'profile': profile, 'save': save}
+
+        configure(self, params=config_kwargs,
+                to_save={'from_', 'acct_sid'}, credentials={'auth_token'})
+
+        self.to = to
+        self.body = body
+        self.attachments = attachments
+        self.sid = None
+        self.verbose = verbose
+
+
+    def __str__(self, indentation='\n'):
+        """print(Email(**args)) method.
+           Indentation value can be overridden in the function call.
+           The default is new line"""
+        return('{}From: {}'
+               '{}To: {}'
+               '{}Body: {}...'
+               '{}Attachments: {}'
+               '{}SID: {}'
+               .format(indentation, self.from_,
+                       indentation, self.to,
+                       indentation, self.body[0:40],
+                       indentation, self.attachments,
+                       indentation, self.sid))
+
+
+    def send(self):
+        """
+        Send the SMS/MMS message.
+        Set self.sid to return code of message.
+        """
+        url = ('https://api.twilio.com/2010-04-01/Accounts/'
+               + self.acct_sid + '/Messages.json')
+        data = {
+            'From': self.from_,
+            'To': self.to,
+            'Body': self.body,
+            'MediaUrl': self.attachments,
+        }
+
+        if self.verbose:
+            print('Debugging info'
+                  '\n--------------'
+                  '\n{} Message created.'.format(timestamp()))
+
+        r = requests.post(url, data=data, auth=(self.acct_sid, self.auth_token))
+        self.sid = r.json()['sid']
+
+        if self.verbose:
+            print(timestamp(), type(self).__name__ + ' info:',
+                self.__str__(indentation='\n * '))
+
+        print('Message sent.')
+
+
+    def send_async(self):
+        """Send message asynchronously."""
+        MESSAGELOOP.add_message(self)
---
+++ packages/messages/messages-0.4.3.tar.gz/messages-0.4.3/tests/test_config.py
@@ -0,0 +1,110 @@
+"""messages._config tests."""
+
+import pytest
+import builtins
+
+import jsonconfig
+
+import messages._config
+from messages._config import configure
+from messages._config import set_default_profile
+from messages._config import create_config
+from messages._config import getpass
+from messages._exceptions import UnknownProfileError
+
+
+##############################################################################
+# FIXTURES
+##############################################################################
+
+class Msg:
+    """A test message class."""
+    def __init__(self, from_, password, profile=None, save=False):
+        self.config_kwargs = {'from_': from_, 'password': password,
+                     'profile': profile, 'save': save}
+
+
+@pytest.fixture()
+def get_msg():
+    return Msg(from_='me', password='passw0rd')
+
+
+##############################################################################
+# TESTS: configure
+##############################################################################
+
+def test_configure_profileNone_saveFalse(get_msg, cfg_mock):
+    """
+    GIVEN a valid message object
+    WHEN `configure` is called with the specified args
+    THEN assert appropriate attributes are set
+    """
+    msg = get_msg
+    configure(msg, params=msg.config_kwargs, to_save={'from_'}, credentials={'password'})
+    assert msg.from_ == 'me'
+    assert msg.password == 'passw0rd'
+
+
+def test_configure_profileYes_saveTrue(cfg_mock):
+    """
+    GIVEN a valid message object
+    WHEN `configure` is called with the specified args
+    THEN assert appropriate attributes are set
+    """
+    msg = Msg(from_='me', password='passw0rd', profile='myProf', save=True)
+    configure(msg, params=msg.config_kwargs, to_save={'from_'}, credentials={'password'})
+    assert msg.from_ == 'me'
+    assert msg.password == 'passw0rd'
+
+
+def test_configure_noPassword(cfg_mock, mocker):
+    """
+    GIVEN a valid message object
+    WHEN `configure` is called with the specified args
+    THEN assert appropriate attributes are set
+    """
+    getpass_mock = mocker.patch.object(messages._config, 'getpass')
+    msg = Msg(from_='me', password=None, profile='NewProf', save=False)
+    configure(msg, params=msg.config_kwargs, to_save={'from_'}, credentials={'password'})
+    assert getpass_mock.call_count == 1
+
+
+##############################################################################
+# TESTS: set_default_profile
+##############################################################################
+
+def test_set_default_profileGood(cfg_mock):
+    """
+    GIVEN a profile to make as the default
+    WHEN set_default_profile(profile) is called
+    THEN verify no errors occur with call to jsonconfig.Config
+    """
+    set_default_profile('tester')
+
+
+def test_set_default_profileBad(cfg_mock):
+    """
+    GIVEN a profile to make as the default
+    WHEN set_default_profile(profile) is called with an unknown profile name
+    THEN assert UnknownProfileError is raised
+    """
+    with pytest.raises(UnknownProfileError):
+        set_default_profile('UnknownProfile')
+
+
+##############################################################################
+# TESTS: create_config
+##############################################################################
+
+def test_create_config_withParams(cfg_mock, mocker):
+    """
+    GIVEN a configuration profile to create
+    WHEN create_config() is called with correct params
+    THEN assert correct functionality is called
+    """
+    input_mock = mocker.patch.object(builtins, 'input')
+    getpass_mock = mocker.patch.object(messages._config, 'getpass')
+    create_config('email', 'myProfile', {'defaults': ['from_'],
+        'credentials': ['password']})
+    assert input_mock.call_count == 1
+    assert getpass_mock.call_count == 1

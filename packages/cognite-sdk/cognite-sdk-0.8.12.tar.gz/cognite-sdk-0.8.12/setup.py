113166ac2db4a353d09cbb066786a6aac6eb3615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:43 +0000

    Add package cognite-sdk/0.8.12/cognite-sdk-0.8.12.tar.gz

---
+++ packages/cognite-sdk/cognite-sdk-0.8.12.tar.gz/cognite-sdk-0.8.12/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+import cognite
+
+packages = find_packages()
+packages.remove('tests')
+
+setup(name='cognite-sdk',
+      version=cognite.__version__,
+      description='Cognite API SDK for Python',
+      url='http://cognite-sdk-python.readthedocs.io/',
+      download_url='https://github.com/cognitedata/cognite-sdk-python/archive/{}.tar.gz'.format(cognite.__version__),
+      author='Erlend Vollset',
+      author_email='erlend.vollset@cognite.com',
+      packages=packages,
+      install_requires=[
+          'requests',
+          'pandas',
+          'protobuf',
+          'cognite-logger'
+      ],
+      zip_safe=False,
+      include_package_data=True)

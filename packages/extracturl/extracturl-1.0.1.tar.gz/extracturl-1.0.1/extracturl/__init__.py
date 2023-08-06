d8e207be4194495780abd8503821191fd8ee3965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:17 +0000

    Add package extracturl/1.0.1/extracturl-1.0.1.tar.gz

---
+++ packages/extracturl/extracturl-1.0.1.tar.gz/extracturl-1.0.1/extracturl/__init__.py
@@ -0,0 +1,7 @@
+import re
+import string
+
+def extract(text):
+    url = r"""((?:(?:https|ftp|http)?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|org|uk)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|uk|ac)\b/?(?!@)))"""
+    urls = re.findall(url, text)
+    print([''.join(x for x in url if x in string.printable) for url in urls])

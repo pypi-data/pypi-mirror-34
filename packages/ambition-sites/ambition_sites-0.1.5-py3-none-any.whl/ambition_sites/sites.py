43b0d1e5b922f3c7630a95df405f7edb9684c234
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:20 +0000

    Add package ambition-sites/0.1.5/ambition_sites-0.1.5-py3-none-any.whl

---
+++ packages/ambition-sites/ambition_sites-0.1.5-py3-none-any.whl/ambition_sites/sites.py
@@ -0,0 +1,13 @@
+
+fqdn = 'ambition.clinicedc.org'
+
+ambition_sites = (
+    (1, 'reviewer', ''),
+    (10, 'gaborone', 'Botswana-Harvard Partnership'),
+    (20, 'harare', 'University of Zimbabwe'),
+    (30, 'lilongwe', 'UNC Project Malawi'),
+    (40, 'blantyre', 'Malawi Liverpool Wellcome Trust Clinical Research Programme'),
+    (50, 'capetown', 'University of Cape Town'),
+    (60, 'kampala', 'IDI Uganda'),
+    (70, 'mbarara', 'IDI Uganda'),
+)

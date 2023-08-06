20d664a4aa93cb37c39a1a21a3aa2965e5407024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:20 +0000

    Add package nbrmd/0.4.3/nbrmd-0.4.3.tar.gz

---
+++ packages/nbrmd/nbrmd-0.4.3.tar.gz/nbrmd-0.4.3/nbrmd/__init__.py
@@ -0,0 +1,25 @@
+"""R markdown notebook format for Jupyter
+
+Use this module to read or write Jupyter notebooks as R Markdown documents
+(methods 'read', 'reads', 'write', 'writes')
+
+Use the RmdFileContentsManager to open Rmd and Jupyter notebooks in Jupyter
+
+Use the 'nbrmd' conversion script to convert Jupyter notebooks from/to
+R Markdown notebooks.
+"""
+
+from .nbrmd import readf, writef, writes, reads, NOTEBOOK_EXTENSIONS, readme
+
+try:
+    from .rmarkdownexporter import RMarkdownExporter
+except ImportError as err:
+    RMarkdownExporter = str(err)
+
+try:
+    from .contentsmanager import RmdFileContentsManager
+except ImportError as err:
+    RmdFileContentsManager = str(err)
+
+__all__ = ['readf', 'writef', 'writes', 'reads', 'NOTEBOOK_EXTENSIONS',
+           'readme', 'RMarkdownExporter', 'RmdFileContentsManager']

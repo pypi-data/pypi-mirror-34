41bf92599b48ef220e80007cc3193fac4aff824b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:20 +0000

    Add package tomodachi/0.13.0/tomodachi-0.13.0.tar.gz

---
+++ packages/tomodachi/tomodachi-0.13.0.tar.gz/tomodachi-0.13.0/tomodachi/__init__.py
@@ -0,0 +1,59 @@
+from typing import Any
+import inspect
+from tomodachi.__version__ import __version__, __version_info__  # noqa
+try:
+    import tomodachi.helpers.logging
+    from tomodachi.invoker import decorator
+except Exception:  # pragma: no cover
+    pass
+try:
+    from tomodachi.transport.amqp import (amqp,
+                                          amqp_publish)
+except Exception:  # pragma: no cover
+    pass
+try:
+    from tomodachi.transport.aws_sns_sqs import (aws_sns_sqs,
+                                                 aws_sns_sqs_publish)
+except Exception:  # pragma: no cover
+    pass
+try:
+    from tomodachi.transport.http import (http,
+                                          http_error,
+                                          http_static,
+                                          websocket,
+                                          HttpException,
+                                          Response as HttpResponse)
+except Exception:  # pragma: no cover
+    pass
+try:
+    from tomodachi.transport.schedule import (schedule,
+                                              heartbeat,
+                                              minutely,
+                                              hourly,
+                                              daily,
+                                              monthly)
+except Exception:  # pragma: no cover
+    pass
+
+__all__ = ['service', 'Service', '__version__', '__version_info__',
+           'amqp', 'amqp_publish',
+           'aws_sns_sqs', 'aws_sns_sqs_publish',
+           'http', 'http_error', 'http_static', 'websocket', 'HttpResponse', 'HttpException',
+           'schedule', 'heartbeat', 'minutely', 'hourly', 'daily', 'monthly']
+
+CLASS_ATTRIBUTE = 'TOMODACHI_SERVICE_CLASS'
+
+
+def service(cls: Any) -> Any:
+    setattr(cls, CLASS_ATTRIBUTE, True)
+    if not getattr(cls, 'log', None):
+        cls.log = tomodachi.helpers.logging.log
+    if not getattr(cls, 'log_setup', None):
+        cls.log_setup = tomodachi.helpers.logging.log_setup
+    return cls
+
+
+class Service(object):
+    TOMODACHI_SERVICE_CLASS = True
+    log = tomodachi.helpers.logging.log
+    log_setup = tomodachi.helpers.logging.log_setup

781c77705de1e0bc312eb015f85e1dd562301f23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:06 +0000

    Add package cs200/0.0.2/cs200-0.0.2.tar.gz

---
+++ packages/cs200/cs200-0.0.2.tar.gz/cs200-0.0.2/cs200/main.py
@@ -0,0 +1,35 @@
+import argparse
+from cs200.scraper import get_url
+from cs200.summarizer import analyze_data
+
+
+
+# Function with default options loaded for quickstart usage. 
+def default_main():
+    
+    # Initialize ArgumentParser instance.
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("--c", "--concept", help="The concept to simplify", type=str)
+
+    args = parser.parse_args()
+ 
+    concept = args.c
+
+    req = get_url(concept)
+
+    summarization = analyze_data(concept, req)
+
+    return summarization[:5]
+
+def main(concept):
+    req = get_url(concept)
+
+    summarization = analyze_data(concept, req)
+
+    return summarization[:5]
+
+if __name__ == "__main__":
+    print(default_main())
+
+

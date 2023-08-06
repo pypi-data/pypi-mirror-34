b984cef3dd92625e75e94680a4fd2881d2633621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:37:52 +0000

    Add package chariot/0.4.1/chariot-0.4.1.tar.gz

---
+++ packages/chariot/chariot-0.4.1.tar.gz/chariot-0.4.1/chariot/transformer/formatter/categorical_label.py
@@ -0,0 +1,32 @@
+import numpy as np
+from chariot.transformer.formatter.base import BaseFormatter
+from chariot.preprocessor import Preprocessor
+
+
+class CategoricalLabel(BaseFormatter):
+
+    def __init__(self, class_count=-1):
+        super().__init__()
+        self.class_count = class_count
+
+    @classmethod
+    def from_(cls, vocabulary_or_preprocessor):
+        vocabulary = vocabulary_or_preprocessor
+        if isinstance(vocabulary_or_preprocessor, Preprocessor):
+            vocabulary = vocabulary_or_preprocessor.vocabulary
+
+        return CategoricalLabel(vocabulary.count)
+
+    def transform(self, column):
+        y = np.array(column, dtype="int")
+        input_shape = y.shape
+        y = y.ravel()
+        n = y.shape[0]
+        categorical = np.zeros((n, self.class_count), dtype=np.float32)
+        categorical[np.arange(n), y] = 1
+        output_shape = input_shape + (self.class_count,)
+        categorical = np.reshape(categorical, output_shape)
+        return categorical
+
+    def inverse_transform(self, batch):
+        return np.argmax(batch, axis=1)

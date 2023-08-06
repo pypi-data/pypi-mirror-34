a42e30ba1a0e80f23bc2f9f60854e83f1f45e728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:56 +0000

    Add package ebi-ols-client/1.0.0/ebi-ols-client-1.0.0.tar.gz

---
+++ packages/ebi-ols-client/ebi-ols-client-1.0.0.tar.gz/ebi-ols-client-1.0.0/ebi/ols/api/client.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from coreapi import Client
+
+import ebi.ols.api.helpers as helpers
+from ebi.ols.api.base import decoders, site, DetailClientMixin, ListClientMixin, SearchClientMixin
+
+
+class OlsClient(object):
+    """
+    TODO: add individuals and properties services
+    OLS Client ontologies client
+
+    """
+    def __init__(self):
+        self.client = Client(decoders=decoders)
+        document = self.client.get(site)
+        self.ontologies = ListClientMixin('ontologies', helpers.Ontology, document)
+        self.ontology = DetailClientMixin('ontologies', helpers.Ontology)
+        self.terms = ListClientMixin('terms', helpers.Term, document)
+        self.term = DetailClientMixin('terms', helpers.Term)
+        self.search = SearchClientMixin('search', helpers.Term, document)
+
+
+if __name__ == "__main__":
+    # TODO add arg parse and associated method calls
+    print('Coming soon')

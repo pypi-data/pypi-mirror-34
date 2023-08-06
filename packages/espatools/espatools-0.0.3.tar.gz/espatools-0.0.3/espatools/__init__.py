216818a6d996005f9653f60cce2d2635caa76bcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:42 +0000

    Add package espatools/0.0.3/espatools-0.0.3.tar.gz

---
+++ packages/espatools/espatools-0.0.3.tar.gz/espatools-0.0.3/espatools/__init__.py
@@ -0,0 +1,10 @@
+from .meta import *
+from .raster import *
+from .read import *
+
+
+__author__ = 'Bane Sullivan'
+__license__ = 'BSD-3-Clause'
+__copyright__ = '2018, Bane Sullivan'
+__version__ = '0.0.3'
+__displayname__ = 'espatools'

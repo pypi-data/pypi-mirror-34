d11d0a9c2389200e69863a5c17a6a57a459faaea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:53 +0000

    Add package aliyun-python-sdk-ecs/4.9.5/aliyun-python-sdk-ecs-4.9.5.tar.gz

---
+++ packages/aliyun-python-sdk-ecs/aliyun-python-sdk-ecs-4.9.5.tar.gz/aliyun-python-sdk-ecs-4.9.5/aliyunsdkecs/request/v20140526/DescribeNetworkInterfacesRequest.py
@@ -0,0 +1,176 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class DescribeNetworkInterfacesRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Ecs', '2014-05-26', 'DescribeNetworkInterfaces','ecs')
+
+	def get_Tag4Value(self):
+		return self.get_query_params().get('Tag.4.Value')
+
+	def set_Tag4Value(self,Tag4Value):
+		self.add_query_param('Tag.4.Value',Tag4Value)
+
+	def get_ResourceOwnerId(self):
+		return self.get_query_params().get('ResourceOwnerId')
+
+	def set_ResourceOwnerId(self,ResourceOwnerId):
+		self.add_query_param('ResourceOwnerId',ResourceOwnerId)
+
+	def get_Tag2Key(self):
+		return self.get_query_params().get('Tag.2.Key')
+
+	def set_Tag2Key(self,Tag2Key):
+		self.add_query_param('Tag.2.Key',Tag2Key)
+
+	def get_SecurityGroupId(self):
+		return self.get_query_params().get('SecurityGroupId')
+
+	def set_SecurityGroupId(self,SecurityGroupId):
+		self.add_query_param('SecurityGroupId',SecurityGroupId)
+
+	def get_Tag3Key(self):
+		return self.get_query_params().get('Tag.3.Key')
+
+	def set_Tag3Key(self,Tag3Key):
+		self.add_query_param('Tag.3.Key',Tag3Key)
+
+	def get_Type(self):
+		return self.get_query_params().get('Type')
+
+	def set_Type(self,Type):
+		self.add_query_param('Type',Type)
+
+	def get_PageNumber(self):
+		return self.get_query_params().get('PageNumber')
+
+	def set_PageNumber(self,PageNumber):
+		self.add_query_param('PageNumber',PageNumber)
+
+	def get_Tag1Value(self):
+		return self.get_query_params().get('Tag.1.Value')
+
+	def set_Tag1Value(self,Tag1Value):
+		self.add_query_param('Tag.1.Value',Tag1Value)
+
+	def get_ResourceGroupId(self):
+		return self.get_query_params().get('ResourceGroupId')
+
+	def set_ResourceGroupId(self,ResourceGroupId):
+		self.add_query_param('ResourceGroupId',ResourceGroupId)
+
+	def get_PageSize(self):
+		return self.get_query_params().get('PageSize')
+
+	def set_PageSize(self,PageSize):
+		self.add_query_param('PageSize',PageSize)
+
+	def get_Tag3Value(self):
+		return self.get_query_params().get('Tag.3.Value')
+
+	def set_Tag3Value(self,Tag3Value):
+		self.add_query_param('Tag.3.Value',Tag3Value)
+
+	def get_NetworkInterfaceName(self):
+		return self.get_query_params().get('NetworkInterfaceName')
+
+	def set_NetworkInterfaceName(self,NetworkInterfaceName):
+		self.add_query_param('NetworkInterfaceName',NetworkInterfaceName)
+
+	def get_Tag5Key(self):
+		return self.get_query_params().get('Tag.5.Key')
+
+	def set_Tag5Key(self,Tag5Key):
+		self.add_query_param('Tag.5.Key',Tag5Key)
+
+	def get_ResourceOwnerAccount(self):
+		return self.get_query_params().get('ResourceOwnerAccount')
+
+	def set_ResourceOwnerAccount(self,ResourceOwnerAccount):
+		self.add_query_param('ResourceOwnerAccount',ResourceOwnerAccount)
+
+	def get_OwnerAccount(self):
+		return self.get_query_params().get('OwnerAccount')
+
+	def set_OwnerAccount(self,OwnerAccount):
+		self.add_query_param('OwnerAccount',OwnerAccount)
+
+	def get_OwnerId(self):
+		return self.get_query_params().get('OwnerId')
+
+	def set_OwnerId(self,OwnerId):
+		self.add_query_param('OwnerId',OwnerId)
+
+	def get_Tag5Value(self):
+		return self.get_query_params().get('Tag.5.Value')
+
+	def set_Tag5Value(self,Tag5Value):
+		self.add_query_param('Tag.5.Value',Tag5Value)
+
+	def get_Tag1Key(self):
+		return self.get_query_params().get('Tag.1.Key')
+
+	def set_Tag1Key(self,Tag1Key):
+		self.add_query_param('Tag.1.Key',Tag1Key)
+
+	def get_VSwitchId(self):
+		return self.get_query_params().get('VSwitchId')
+
+	def set_VSwitchId(self,VSwitchId):
+		self.add_query_param('VSwitchId',VSwitchId)
+
+	def get_InstanceId(self):
+		return self.get_query_params().get('InstanceId')
+
+	def set_InstanceId(self,InstanceId):
+		self.add_query_param('InstanceId',InstanceId)
+
+	def get_Tag2Value(self):
+		return self.get_query_params().get('Tag.2.Value')
+
+	def set_Tag2Value(self,Tag2Value):
+		self.add_query_param('Tag.2.Value',Tag2Value)
+
+	def get_VpcId(self):
+		return self.get_query_params().get('VpcId')
+
+	def set_VpcId(self,VpcId):
+		self.add_query_param('VpcId',VpcId)
+
+	def get_Tag4Key(self):
+		return self.get_query_params().get('Tag.4.Key')
+
+	def set_Tag4Key(self,Tag4Key):
+		self.add_query_param('Tag.4.Key',Tag4Key)
+
+	def get_PrimaryIpAddress(self):
+		return self.get_query_params().get('PrimaryIpAddress')
+
+	def set_PrimaryIpAddress(self,PrimaryIpAddress):
+		self.add_query_param('PrimaryIpAddress',PrimaryIpAddress)
+
+	def get_NetworkInterfaceIds(self):
+		return self.get_query_params().get('NetworkInterfaceIds')
+
+	def set_NetworkInterfaceIds(self,NetworkInterfaceIds):
+		for i in range(len(NetworkInterfaceIds)):	
+			if NetworkInterfaceIds[i] is not None:
+				self.add_query_param('NetworkInterfaceId.' + str(i + 1) , NetworkInterfaceIds[i]);

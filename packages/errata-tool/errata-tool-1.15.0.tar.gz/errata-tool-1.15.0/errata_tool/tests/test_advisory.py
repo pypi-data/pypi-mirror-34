80e488505bc6ebdb70a163fb2b3a9dfca7cff4df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:00 +0000

    Add package errata-tool/1.15.0/errata-tool-1.15.0.tar.gz

---
+++ packages/errata-tool/errata-tool-1.15.0.tar.gz/errata-tool-1.15.0/errata_tool/tests/test_advisory.py
@@ -0,0 +1,93 @@
+from errata_tool import Erratum
+
+
+class TestAdvisory(object):
+
+    def test_instance(self, advisory):
+        assert isinstance(advisory, Erratum)
+
+    def test_errata_state(self, advisory):
+        assert advisory.errata_state == 'SHIPPED_LIVE'
+
+    def test_url(self, advisory):
+        expected = 'https://errata.devel.redhat.com/advisory/26175'
+        assert advisory.url() == expected
+
+    def test_synopsis(self, advisory):
+        expected = 'Red Hat Ceph Storage 2.2 bug fix and enhancement update'
+        assert advisory.synopsis == expected
+
+    def test_topic(self, advisory):
+        expected = 'Red Hat Ceph Storage 2.2 is now available.'
+        assert advisory.topic == expected
+
+    def test_cve_names(self, advisory):
+        expected = None
+        assert advisory.cve_names == expected
+
+    def test_description(self, advisory):
+        expected = 'Red Hat Ceph Storage is a scalable'
+        assert advisory.description.startswith(expected)
+
+    def test_solution(self, advisory):
+        expected = 'Before applying'
+        assert advisory.solution.startswith(expected)
+
+    def test_qe_email(self, advisory):
+        assert advisory.qe_email == 'hnallurv@redhat.com'
+
+    def test_qe_group(self, advisory):
+        assert advisory.qe_group == 'Default'
+
+    def test_errata_type(self, advisory):
+        assert advisory.errata_type == 'RHBA'
+
+    def test_owner_email(self, advisory):
+        assert advisory.package_owner_email == 'kdreyer@redhat.com'
+
+    def test_manager_email_is_none(self, advisory):
+        # NOTE: the ET does not give this information when querying a
+        # pre-existing advisory. We can only update it with POST/PUT, but we
+        # cannot GET it.
+        #  assert advisory.manager_email == 'gmeno@redhat.com'
+        assert advisory.manager_email is None
+
+    def test_text_only(self, advisory):
+        assert advisory.text_only is False
+
+    def test_text_only_cpe(self, advisory):
+        assert advisory.text_only_cpe is None
+
+    def test_publish_date_override(self, advisory):
+        assert advisory.publish_date_override is None
+
+    def test_creation_date(self, advisory):
+        assert advisory.creation_date == '2017-Jan-10'
+
+    def test_ship_date(self, advisory):
+        assert advisory.ship_date == '2017-Mar-14'
+
+    def test_age(self, advisory):
+        assert advisory.age == 63
+
+    def test_errata_bugs(self, advisory):
+        # Only sanity-check one for brevity.
+        assert 1425771 in advisory.errata_bugs
+        assert len(advisory.errata_bugs) == 61
+
+    def test_errata_builds(self, advisory):
+        expected = {'RHEL-7-CEPH-2': ['ceph-10.2.5-37.el7cp']}
+        assert advisory.errata_builds == expected
+
+    def test_missing_prod_listings(self, advisory):
+        # (note, all builds have product listings for this advisory)
+        assert advisory.missing_prod_listings == []
+
+    def test_current_flags(self, advisory):
+        assert advisory.current_flags == []
+
+    def test_content_types(self, advisory):
+        assert advisory.content_types == ['rpm']
+
+    def test_batch_id(self, advisory):
+        assert advisory.batch_id is None
---
+++ packages/errata-tool/errata-tool-1.15.0.tar.gz/errata-tool-1.15.0/errata_tool/tests/test_release.py
@@ -0,0 +1,111 @@
+from datetime import date
+import requests
+from errata_tool.release import Release
+from errata_tool.connector import ErrataConnector
+
+
+class TestGet(object):
+    def test_id(self, release):
+        assert release.id == 783
+
+    def test_name(self, release):
+        assert release.name == 'rhceph-3.0'
+
+    def test_description(self, release):
+        assert release.description == 'Red Hat Ceph Storage 3.0'
+
+    def test_type(self, release):
+        assert release.type == 'QuarterlyUpdate'
+
+    def test_is_active(self, release):
+        assert release.is_active is True
+
+    def test_enabled(self, release):
+        assert release.enabled is True
+
+    def blocker_flags(self, release):
+        expected = ['ceph-3.0', 'pm_ack', 'devel_ack', 'qa_ack']
+        assert release.blocker_flags == expected
+
+    def test_is_pdc(self, release):
+        assert release.is_pdc is True
+
+    def test_url(self, release):
+        expected = 'https://errata.devel.redhat.com/release/show/783'
+        assert release.url == expected
+
+    def test_edit_url(self, release):
+        expected = 'https://errata.devel.redhat.com/release/edit/783'
+        assert release.edit_url == expected
+
+
+class TestAdvisories(object):
+    def test_advisories(self, release, monkeypatch, mock_get):
+        monkeypatch.setattr(requests, 'get', mock_get)
+        result = release.advisories()
+        # Validate URL
+        expected_url = 'https://errata.devel.redhat.com/release/783/advisories.json'  # NOQA: E501
+        assert mock_get.response.url == expected_url
+        # Validate return data
+        expected = [{"id": 32972,
+                     "advisory_name": "RHSA-2018:0546",
+                     "product": "Red Hat Ceph Storage",
+                     "release": "rhceph-3.0",
+                     "synopsis": "Important: ceph security update",
+                     "release_date": None,
+                     "qe_owner": "someone@redhat.com",
+                     "qe_group": "RHC (Ceph) QE",
+                     "status": "SHIPPED_LIVE",
+                     "status_time": "March 15, 2018 18:29"}]
+        assert result == expected
+
+
+class TestCreate(object):
+    # Note: we must be able to "GET" this release from a fixture file
+    #       because we call GET at the end of Release.create().
+    create_kwargs = dict(
+        name='rhceph-3.0',
+        product='RHCEPH',
+        product_versions=['RHEL-7-CEPH-3'],
+        type='QuarterlyUpdate',
+        program_manager='anharris',
+        blocker_flags='ceph-3.0',
+        default_brew_tag='ceph-3.0-rhel-7-candidate',
+    )
+
+    def test_create_url(self, monkeypatch, mock_get, mock_post):
+        monkeypatch.setattr(requests, 'get', mock_get)
+        monkeypatch.setattr(requests, 'post', mock_post)
+        monkeypatch.setattr(ErrataConnector, '_username', 'test')
+        Release.create(**self.create_kwargs)
+        expected = 'https://errata.devel.redhat.com/release/create'
+        assert mock_post.response.url == expected
+
+    def test_create_data(self, monkeypatch, mock_get, mock_post):
+        monkeypatch.setattr(requests, 'get', mock_get)
+        monkeypatch.setattr(requests, 'post', mock_post)
+        monkeypatch.setattr(ErrataConnector, '_username', 'test')
+        Release.create(**self.create_kwargs)
+        today = date.today()
+        ship_date = today.strftime("%Y-%b-%d")
+        expected = {
+            'type': 'QuarterlyUpdate',
+            'release[allow_blocker]': 0,
+            'release[allow_exception]': 0,
+            'release[allow_pkg_dupes]': 1,
+            'release[allow_shadow]': 0,
+            'release[blocker_flags]': 'ceph-3.0',
+            'release[description]': 'Red Hat Ceph Storage 3.0',
+            'release[default_brew_tag]': 'ceph-3.0-rhel-7-candidate',
+            'release[enable_batching]': 0,
+            'release[enabled]': 1,
+            'release[is_deferred]': 0,
+            'release[is_pdc]': 0,
+            'release[name]': 'rhceph-3.0',
+            'release[product_id]': 104,
+            'release[program_manager_id]': 3003046,
+            'release[product_version_ids][]': set([665]),
+            'release[ship_date]': ship_date,
+            'release[type]': 'QuarterlyUpdate',
+        }
+        assert mock_post.kwargs['data'] == expected

60b71320ca4effd9aed9b36a6b60cc8f04a2c2e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:30 +0000

    Add package mosyn/1.1.0/mosyn-1.1.0-py3-none-any.whl

---
+++ packages/mosyn/mosyn-1.1.0-py3-none-any.whl/mosyn/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-15 -*-
+
+# Created on 10/11/2015
+# Modified on 23/11/2015
+# Modified by Axel Garcia(axelgafu)
+#
+# source: https://github.com/uagdataanalysis/mosynapi
+#
+# @author: asaelt
+
+# from .util.eagles import AnalysisManager
+# from .util.eagles import AnalysisProcess
+# from .util.eagles import MorphologicalAnalysis
+# from .util.eagles import MorphologicalDictionary
+# from .util.eagles import AbstractMorphology
+
+name = "mosyn"

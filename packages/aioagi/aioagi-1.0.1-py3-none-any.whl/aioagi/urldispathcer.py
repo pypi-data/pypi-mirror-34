94cb6dab50fccac8cc476993e6843b7984bf67fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:02 +0000

    Add package aioagi/1.0.1/aioagi-1.0.1-py3-none-any.whl

---
+++ packages/aioagi/aioagi-1.0.1-py3-none-any.whl/aioagi/urldispathcer.py
@@ -0,0 +1,43 @@
+from aiohttp.web_urldispatcher import UrlDispatcher, MatchInfoError
+
+from aiohttp import web
+
+from aioagi.hdrs import AGIMethod
+from aioagi.exceptions import AGIMethodNotAllowed, AGINotFound
+from aioagi.request import AGIRequest
+
+
+class AGIUrlDispatcher(UrlDispatcher):
+    async def resolve(self, request: AGIRequest):
+        allowed_methods = set()
+
+        for resource in self._resources:
+            match_dict, allowed = await resource.resolve(request)
+            if match_dict is not None:
+                return match_dict
+            else:
+                allowed_methods |= allowed
+        else:
+            if allowed_methods:
+                return MatchInfoError(AGIMethodNotAllowed())
+            else:
+                return MatchInfoError(AGINotFound())
+
+
+class AGIView(web.View):
+    def __init__(self, request: AGIRequest):
+        self.agi = request.agi
+        super().__init__(request)
+
+    async def dispatch(self):
+        if self.request.method not in AGIMethod.all():
+            self._raise_allowed_methods()
+
+        method = getattr(self, self.request.method.lower(), None)
+        if method is None:
+            self._raise_allowed_methods()
+        return await method()
+
+    def _raise_allowed_methods(self):
+        allowed_methods = {m for m in AGIMethod.all() if hasattr(self, m.lower())}
+        raise AGIMethodNotAllowed(self.request.method, allowed_methods)

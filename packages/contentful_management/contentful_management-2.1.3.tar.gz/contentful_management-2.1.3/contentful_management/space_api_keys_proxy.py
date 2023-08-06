ca7c7d61ca17b4d39e9472b773bccdbfb25abc32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:40 +0000

    Add package contentful_management/2.1.3/contentful_management-2.1.3.tar.gz

---
+++ packages/contentful_management/contentful_management-2.1.3.tar.gz/contentful_management-2.1.3/contentful_management/space_api_keys_proxy.py
@@ -0,0 +1,24 @@
+from .space_resource_proxy import SpaceResourceProxy
+from .api_keys_proxy import ApiKeysProxy
+
+
+"""
+contentful_management.space_api_keys_proxy
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+This module implements the SpaceApiKeysProxy class.
+
+API reference: https://www.contentful.com/developers/docs/references/content-management-api/#/reference/api-keys
+
+:copyright: (c) 2018 by Contentful GmbH.
+:license: MIT, see LICENSE for more details.
+"""
+
+
+class SpaceApiKeysProxy(SpaceResourceProxy):
+    """
+    API reference: https://www.contentful.com/developers/docs/references/content-management-api/#/reference/api-keys
+    """
+
+    def _resource_proxy_class(self):
+        return ApiKeysProxy
---
+++ packages/contentful_management/contentful_management-2.1.3.tar.gz/contentful_management-2.1.3/contentful_management/space_roles_proxy.py
@@ -0,0 +1,24 @@
+from .space_resource_proxy import SpaceResourceProxy
+from .roles_proxy import RolesProxy
+
+
+"""
+contentful_management.space_roles_proxy
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+This module implements the SpaceRolesProxy class.
+
+API reference: https://www.contentful.com/developers/docs/references/content-management-api/#/reference/roles
+
+:copyright: (c) 2018 by Contentful GmbH.
+:license: MIT, see LICENSE for more details.
+"""
+
+
+class SpaceRolesProxy(SpaceResourceProxy):
+    """
+    API reference: https://www.contentful.com/developers/docs/references/content-management-api/#/reference/roles
+    """
+
+    def _resource_proxy_class(self):
+        return RolesProxy

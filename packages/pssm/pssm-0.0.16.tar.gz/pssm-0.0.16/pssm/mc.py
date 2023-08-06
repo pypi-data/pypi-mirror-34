d33421a1a74a9a2cff9e3a9beb21c83bfab83c2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:31 +0000

    Add package pssm/0.0.16/pssm-0.0.16.tar.gz

---
+++ packages/pssm/pssm-0.0.16.tar.gz/pssm-0.0.16/pssm/mc.py
@@ -0,0 +1,75 @@
+import numpy as np
+from numpy.linalg import inv
+from numpy.random import gamma
+from numpy.random import multivariate_normal as mvn
+
+from pssm.filters import KalmanFilter
+
+
+class FFBS:
+    def __init__(self, structure, m0, C0, vprior, wprior, data):
+        self._structure = structure
+        self._m0 = m0
+        self._C0 = C0
+        self._vprior = vprior
+        self._wprior = wprior
+        self._data = data
+        self._nobs = len(data)
+
+    @staticmethod
+    def backward_sampling_step(theta, m, C, W):
+        _inv = inv(C + W)
+        mean = m + np.dot(C, np.dot(_inv, theta - m).A1).A1
+        cov = C - C * np.transpose(C * _inv)
+
+        return mvn(mean=mean, cov=cov)
+
+    @staticmethod
+    def backward_sampling(ms, Cs, W):
+        T = len(ms) - 1
+        theta = mvn(mean=np.atleast_1d(ms[T]), cov=np.atleast_2d(Cs[T]))
+        thetas = [theta]
+        for t in reversed(range(1, T)):
+            theta = FFBS.backward_sampling_step(theta, ms[t], Cs[t], W)
+            thetas.append(theta)
+        return list(reversed(thetas))
+
+    def _states(self, V, W):
+        ms = [self._m0]
+        Cs = [self._C0]
+        structure = self._structure
+        structure._W = W
+        kf = KalmanFilter(structure=structure, V=V)
+        for t in range(1, self._nobs):
+            m, C = kf.filter(self._data[t], ms[t - 1], Cs[t - 1])
+            ms.append(m)
+            Cs.append(C)
+        return ms, Cs
+
+    def run(self, V, W, states=False):
+        Ft = self._structure.F.T
+        n = self._nobs - 1
+
+        ms, Cs = self._states(V=V, W=W)
+
+        thetas = FFBS.backward_sampling(ms, Cs, W)
+
+        ssy = np.zeros(n)
+        sstheta = np.zeros((n, len(self._wprior)))
+        for t in range(1, n):
+            ssy[t] = np.power(self._data[t] - np.dot(Ft, thetas[t]), 2.0)
+            sstheta[t] = np.power(
+                thetas[t] - np.dot(self._structure.G, thetas[t - 1]), 2.0)
+
+        v_rate = self._vprior + 0.5 * sum(ssy)
+        _V = 1.0 / gamma(self._vprior + 0.5 * n, scale=v_rate)
+
+        w_rate = [self._vprior + 0.5 * d for d in sstheta.sum(axis=0)]
+        _W = np.diag(
+            [1.0 / gamma(prior + 0.5 * n, scale=scale)
+             for scale, prior in zip(w_rate, self._wprior)])
+
+        if states:
+            return _W, _V, thetas
+        else:
+            return _W, _V
---
+++ packages/pssm/pssm-0.0.16.tar.gz/pssm-0.0.16/pssm/structure.py
@@ -0,0 +1,114 @@
+import numpy as np
+from pssm.utils import block_diag
+
+
+class DimensionError(Exception):
+    pass
+
+
+class UnivariateStructure:
+    def __init__(self, F, G, W):
+        self._F = F
+        self._G = G
+        self._W = W
+
+    @property
+    def F(self):
+        return self._F
+
+    @property
+    def G(self):
+        return self._G
+
+    @property
+    def W(self):
+        return self._W
+
+    def __add__(self, other):
+        # type: (UnivariateStructure) -> UnivariateStructure
+        F = np.vstack((self.F, other.F))
+        G = block_diag(*[self.G, other.G])
+        W = block_diag(*[self.W, other.W])
+
+        return UnivariateStructure(F=F, G=G, W=W)
+
+    @staticmethod
+    def locally_constant(W):
+        # type: (float) -> UnivariateStructure
+        F = np.matrix([[1]])
+        G = np.matrix([[1]])
+        W = np.matrix([[W]])
+        return UnivariateStructure(F=F, G=G, W=W)
+
+    @staticmethod
+    def locally_linear(W):
+        # type: (ndarray) -> UnivariateStructure
+        F = np.matrix([[1], [0]])
+        G = np.matrix([[1, 1], [0, 1]])
+        return UnivariateStructure(F=F, G=G, W=W)
+
+    @staticmethod
+    def arma(p, betas, W):
+        # type: (ndarray, List[float], float) -> UnivariateStructure
+        if p < 1:
+            raise ValueError("`p` must be 1 or higher.")
+        if len(betas) != p:
+            raise ValueError("`betas` must have `p` elements.")
+
+        F = np.transpose(np.matrix([[1.0] + [0.0] * (p - 1)]))
+        if p == 1:  # can simplify for ARMA(1)
+            G = np.identity(p) * betas[0]
+        else:
+            G = np.identity(p - 1)
+            G = np.hstack((G, np.zeros((p - 1, 1))))
+            G = np.vstack((betas, G))
+        _W = np.zeros((p, p))
+        _W[0, 0] = W
+        return UnivariateStructure(F=F, G=G, W=_W)
+
+    @staticmethod
+    def cyclic_fourier(period, harmonics, W):
+        # type: (int, int, ndarray) -> UnivariateStructure
+        om = 2.0 * np.pi / period
+        harmonic1 = np.identity(2) * np.cos(om)
+        harmonic1[0, 1] = np.sin(om)
+        harmonic1[1, 0] = -harmonic1[0, 1]
+
+        _G = [None] * harmonics
+        _G[0] = np.copy(harmonic1)
+        if harmonics > 1:
+            for i in np.arange(1, harmonics):
+                _G[i] = np.dot(_G[i - 1], harmonic1)
+            G = block_diag(*_G)
+        else:
+            G = harmonic1
+
+        F = np.transpose(np.matrix([[1.0, 0.0] * harmonics]))
+
+        return UnivariateStructure(F=F, G=G, W=W)
+
+
+class MultivariateStructure:
+    def __init__(self, F, G, W):
+        self._F = F
+        self._G = G
+        self._W = W
+
+    @property
+    def F(self):
+        return self._F
+
+    @property
+    def G(self):
+        return self._G
+
+    @property
+    def W(self):
+        return self._W
+
+    @staticmethod
+    def build(*dglms):
+        _F = block_diag(*[model.F for model in dglms])
+        _G = block_diag(*[model.G for model in dglms])
+        _W = block_diag(*[model.W for model in dglms])
+        return MultivariateStructure(F=_F, G=_G, W=_W)

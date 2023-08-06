3f174fcacdbde17e1ad12f65e50f881d3d0369a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:47 +0000

    Add package QQLoginTool/0.2.0/QQLoginTool-0.2.0.tar.gz

---
+++ packages/QQLoginTool/QQLoginTool-0.2.0.tar.gz/QQLoginTool-0.2.0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "QQLoginTool",
+    version = "0.2.0",
+    keywords = ("pip", "pathtool","timetool", "magetool", "mage"),
+    description = "time and path tool",
+    long_description = "time and path tool",
+    license = "MIT Licence",
+
+    url = "https://github.com/AugustHub/QQLogin.git",
+    author = "august",
+    author_email = "august@163.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ['requests']
+)

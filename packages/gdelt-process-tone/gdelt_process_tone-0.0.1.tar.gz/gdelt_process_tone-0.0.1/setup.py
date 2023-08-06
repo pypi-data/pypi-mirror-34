ad3c429c078b4399dd618ddba52bb50180d188e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:04 +0000

    Add package gdelt-process-tone/0.0.1/gdelt_process_tone-0.0.1.tar.gz

---
+++ packages/gdelt-process-tone/gdelt_process_tone-0.0.1.tar.gz/gdelt_process_tone-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gdelt_process_tone',
+      version='0.0.1',
+      description='Returns tone and location json files',
+      url='',
+      author='Development Seed',
+      author_email='aimee@developmentseed.org',
+      license='MIT',
+      py_modules=['gdelt_process_tone'],
+      depends=['numpy', 'pandas'])

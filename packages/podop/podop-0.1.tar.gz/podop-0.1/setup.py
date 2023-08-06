46d84f81276422db455abfb3961d550165ceef3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:06 +0000

    Add package podop/0.1/podop-0.1.tar.gz

---
+++ packages/podop/podop-0.1.tar.gz/podop-0.1/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="podop",
+    version="0.1",
+    description="Postfix and Dovecot proxy",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Pierre Jaury",
+    author_email="pierre@jaury.eu",
+    url="https://github.com/mailu/podop.git",
+    packages=["podop"],
+    scripts=["scripts/podop"]
+)

d42b148c4e0da51b7f8df752de4750d7c1a0d609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:38 +0000

    Add package weka/1.0.3/weka-1.0.3.tar.gz

---
+++ packages/weka/weka-1.0.3.tar.gz/weka-1.0.3/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+from setuptools import setup, find_packages
+
+import weka
+
+CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
+
+def get_reqs(*fns):
+    lst = []
+    for fn in fns:
+        for package in open(os.path.join(CURRENT_DIR, fn)).readlines():
+            package = package.strip()
+            if not package:
+                continue
+            lst.append(package.strip())
+    return lst
+
+setup(name='weka',
+    version=weka.__version__,
+    description='A Python wrapper for the Weka data mining library.',
+    author='Chris Spencer',
+    author_email='chrisspen@gmail.com',
+    url='https://github.com/chrisspen/weka',
+    license='LGPL License',
+    packages=find_packages(),
+    package_data={
+        'weka': [
+            'fixtures/*'
+        ],
+    },
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: General",
+    ],
+    platforms=['OS Independent'],
+    install_requires=get_reqs('pip-requirements.txt'),
+)

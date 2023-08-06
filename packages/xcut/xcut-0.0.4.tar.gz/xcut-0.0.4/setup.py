e117ab24e75d039e680994561f1406fb964c268b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:18:33 +0000

    Add package xcut/0.0.4/xcut-0.0.4.tar.gz

---
+++ packages/xcut/xcut-0.0.4.tar.gz/xcut-0.0.4/setup.py
@@ -0,0 +1,33 @@
+'''
+setup for pypi package
+'''
+from setuptools import setup
+
+
+setup(
+    name='xcut',
+    version='0.0.4',
+    python_requires='>=3.6.1',
+
+    packages=[],
+    # py_modules=['xcut'], # single module
+    install_requires=[],
+    scripts=['xcut'],
+    description=open('README.md').readlines()[1].strip(),
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author="ahuigo",
+    author_email="ahui132@qq.com",
+    license="MIT",
+    url="http://github.com/ahuigo/xcut",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python',
+    ],
+
+)

1f37d84ca727787de25cd6be6c9654efdc5b7eec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:32 +0000

    Add package is99kdf/0.5.4/is99kdf-0.5.4.tar.gz

---
+++ packages/is99kdf/is99kdf-0.5.4.tar.gz/is99kdf-0.5.4/setup.py
@@ -0,0 +1,30 @@
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: dengfeng.ke
+# Mail: dengfeng.ke@beijinghear.com
+# Created Time:  2018-8-1 16:13
+#############################################
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "is99kdf",
+    version = "0.5.4",
+    keywords = ["pip", "is99kdf"],
+    description = "python tools by dengfeng.ke",
+    long_description = "python tools by dengfeng.ke",
+    license = "BJFU Licence",
+
+    url = "https://pypi.org/project/is99kdf/",
+    author = "is99kdf",
+    author_email = "dengfeng.ke@beijinghear.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+    install_requires = ["scipy","numpy","librosa"]
+)
+

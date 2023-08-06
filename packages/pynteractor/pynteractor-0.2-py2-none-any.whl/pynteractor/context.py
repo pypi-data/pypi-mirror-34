c23688a194f0443f350bf3aa55ef85e8737b6fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:56 +0000

    Add package pynteractor/0.2/pynteractor-0.2-py2-none-any.whl

---
+++ packages/pynteractor/pynteractor-0.2-py2-none-any.whl/pynteractor/context.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+"""
+Implement the context helper to help share variables with the outside of the
+organizer, but also within the organizer itself.
+"""
+
+from pynteractor.errors import LogicError, StopExecution
+
+
+class Context(object):
+    """
+    Organizer data bank and termination decider.
+    """
+
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            self[key] = value
+
+        self.interrupted = False
+        self.set_success(True)
+
+    def set_success(self, value):
+        """
+        Set the outcoming results
+        """
+        self.success = value
+        self.failure = not value
+
+    def __getattr__(self, attr):
+        return self.__dict__.get(attr, None)
+
+    def __getitem__(self, attr):
+        return getattr(self, attr)
+
+    def __setitem__(self, attr, value):
+        return setattr(self, attr, value)
+
+    def unroll(self):
+        """
+        Expose the whole set of context-related attributes
+        """
+        return self.__dict__
+
+    def terminate(self, success=True, **kwargs):
+        """
+        Stop running the current interactor and organizer and populate the
+        current context with passed arguments.
+        """
+        for arg, value in kwargs.items():
+            self[arg] = value
+
+        self.interrupted = True
+        self.set_success(success)
+
+    def fail(self, **kwargs):
+        """
+        Stop running the current organizer but raise an error to signal
+        something went wrong.
+        """
+        self.terminate(success=False, **kwargs)
+        raise LogicError()
+
+    def stop(self, **kwargs):
+        """
+        Stop running the current organizer but raise an error to signal the
+        user just wants to terminate the current organizer/interactor
+        execution.
+        """
+        self.terminate(success=True, **kwargs)
+        raise StopExecution()
---
+++ packages/pynteractor/pynteractor-0.2-py2-none-any.whl/pynteractor/interactor.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+"""
+Small brick of Business Logic.  An interactor usually never goes over a simple
+action.
+
+Examples:
+- Checks
+- PersistData
+- MarkAsDone
+- Notify
+"""
+
+from pynteractor.errors import LogicError, StopExecution
+from pynteractor.context import Context
+
+
+class Interactor(object):
+    """
+    Smallest brick of Business Logic
+    """
+
+    @classmethod
+    def call(klass, **kwargs):
+        """
+        Call the interactor with keywords arguments.
+
+        Example:
+
+        def MyInteractor(Interactor):
+            pass
+
+        MyInteractor.call(firstname='John', lastname='Doe')
+        """
+        return klass.call_with_context(Context(**kwargs))
+
+    @classmethod
+    def call_with_context(klass, context):
+        """
+        Call the current interactor with a given context of type
+        pynteractor.Context.
+
+        Mostly used internally to chain interactor calls.
+        """
+        instance = klass(context)
+
+        try:
+            instance.run()
+        except StopExecution:
+            pass
+        except LogicError:
+            klass.call_rollback(context)
+
+        return context
+
+    @classmethod
+    def call_rollback(klass, context):
+        instance = klass(context)
+        try:
+            instance.rollback()
+        except AttributeError:
+            pass
+        return instance.context
+
+    def __init__(self, context=None):
+        if context is None:
+            context = Context
+
+        self.context = context
+        self.ctx = context
+
+    def run(self):
+        """
+        Implement the actual Business Logic.
+        A context will alway be provided.
+        """
+        raise NotImplementedError()

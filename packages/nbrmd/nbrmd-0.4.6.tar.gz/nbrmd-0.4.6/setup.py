a84bfef77dd843144627e51a858cbb4078d2e2fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:26 +0000

    Add package nbrmd/0.4.6/nbrmd-0.4.6.tar.gz

---
+++ packages/nbrmd/nbrmd-0.4.6.tar.gz/nbrmd-0.4.6/setup.py
@@ -0,0 +1,36 @@
+from os import path
+from setuptools import setup, find_packages
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    name='nbrmd',
+    version='0.4.6',
+    author='Marc Wouts',
+    author_email='marc.wouts@gmail.com',
+    description='Jupyter from/to R markdown notebooks',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/mwouts/nbrmd',
+    packages=find_packages(),
+    entry_points={'console_scripts': ['nbrmd = nbrmd.cli:main'],
+                  'nbconvert.exporters':
+                      ['rmarkdown = nbrmd:RMarkdownExporter']},
+    tests_require=['pytest', 'testfixtures'],
+    install_requires=['nbformat>=4.0.0', 'mock', 'pyyaml'],
+    license='MIT',
+    classifiers=('Development Status :: 4 - Beta',
+                 'Environment :: Console',
+                 'Framework :: Jupyter',
+                 'Intended Audience :: Science/Research',
+                 'Programming Language :: Python',
+                 'Topic :: Text Processing :: Markup',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6')
+)

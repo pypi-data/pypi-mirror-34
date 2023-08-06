ee68faec4b149ec037e741634ba1c7b9092f3e05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:38 +0000

    Add package ClusterAPIServer/0.2/ClusterAPIServer-0.2.tar.gz

---
+++ packages/ClusterAPIServer/ClusterAPIServer-0.2.tar.gz/ClusterAPIServer-0.2/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(name='ClusterAPIServer',
+      version='0.2',
+      author='Xin Liang',
+      author_email='XLiang@suse.com',
+      packages=find_packages(),
+      install_requires=['Flask', 'xmltodict', 'PyJWT', 'python-pam'])

5fbf156713aca0f3a195d78824f1062a5be272e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:39 +0000

    Add package tango-comments/0.8.2/tango-comments-0.8.2.tar.gz

---
+++ packages/tango-comments/tango-comments-0.8.2.tar.gz/tango-comments-0.8.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+with open('docs/requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='tango-comments',
+    version='0.8.2',
+    author='Tim Baxter',
+    author_email='mail.baxter@gmail.com',
+    url='http://github.com/tBaxter/tango-comments',
+    license='LICENSE',
+    description='A simple port of the old django.contrib.comments into Tango.',
+    long_description=open('README.md').read(),
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=required,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities'],
+)

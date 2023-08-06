e8f20771902ae164c47fbc88c30831307986fde4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:55 +0000

    Add package yxspkg_songzgif/1.4.1/yxspkg_songzgif-1.4.1.tar.gz

---
+++ packages/yxspkg_songzgif/yxspkg_songzgif-1.4.1.tar.gz/yxspkg_songzgif-1.4.1/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+from setuptools import setup ,find_packages
+
+install_modules=['yxspkg_songzviewer']
+for i in ['PyQt5','numpy','imageio','yxspkg_encrypt']:
+	try:
+		exec('import '+i)
+	except:
+		install_modules.append(i)
+setup(name='yxspkg_songzgif',   
+      version='1.4.1',    
+      description='A GUI to make gif based on pyqt5',    
+      author='Blacksong',    
+      install_requires=install_modules,
+      author_email='blacksong@qq.com',
+      long_description=open('README.rst', 'r').read(),       
+      url='https://github.com/blacksong',
+      packages=find_packages(), 
+      classifiers = [
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+)   

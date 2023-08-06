e62e6c80407f9389f0a2a37dcf14a11ae5b46a88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:02 +0000

    Add package pywindowx/0.0.3/pywindowx-0.0.3.tar.gz

---
+++ packages/pywindowx/pywindowx-0.0.3.tar.gz/pywindowx-0.0.3/setup.py
@@ -0,0 +1,9 @@
+#from distutils.core import setup
+from setuptools import setup
+
+setup(name='pywindowx',
+      author='Marcin Miklitz',
+      author_email='marcin.miklitz@gmail.com',
+      url='https://www.github.com/marcinmiklitz/pywindow',
+      version='0.0.3',
+      packages=['pywindow'])

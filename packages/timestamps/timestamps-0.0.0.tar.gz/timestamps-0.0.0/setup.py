16616f7b20b9c4c5bb0ffbeb072cccbff54d95e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:06 +0000

    Add package timestamps/0.0.0/timestamps-0.0.0.tar.gz

---
+++ packages/timestamps/timestamps-0.0.0.tar.gz/timestamps-0.0.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="timestamps",
+    version="0.0.0",
+    author="GHOUL Nadir",
+    author_email="gndu91@gmail.com",
+    description="Timestamp manager",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gndu91/timestamps",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)

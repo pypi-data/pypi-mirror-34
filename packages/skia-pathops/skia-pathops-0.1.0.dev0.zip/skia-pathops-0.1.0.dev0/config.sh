65d5514b0d6634d086f93682fb5c02d422433d94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:45 +0000

    Add package skia-pathops/0.1.0.dev0/skia-pathops-0.1.0.dev0.zip

---
+++ packages/skia-pathops/skia-pathops-0.1.0.dev0.zip/skia-pathops-0.1.0.dev0/config.sh
@@ -0,0 +1,14 @@
+# Define custom utilities
+# Test for OSX with [ -n "$IS_OSX" ]
+
+function pre_build {
+    # Any stuff that you need to do before you start building the wheels
+    # Runs in the root directory of this repository.
+    :
+}
+
+function run_tests {
+    # The function is called from an empty temporary directory.
+    cd ..
+    pytest
+}

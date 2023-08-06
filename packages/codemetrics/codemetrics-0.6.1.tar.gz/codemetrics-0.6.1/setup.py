6aef23058c4c01a2ea0c3883456e87876560cf47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:52 +0000

    Add package codemetrics/0.6.1/codemetrics-0.6.1.tar.gz

---
+++ packages/codemetrics/codemetrics-0.6.1.tar.gz/codemetrics-0.6.1/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""Packaging script."""
+
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+readme = open(os.path.join(here, 'README.rst')).read()
+
+setup(
+    name="codemetrics",
+    description='SCM mining utility classes',
+    long_description=readme,
+    long_description_content_type='text/x-rst',
+    version="0.6.1",
+    author="Jérôme Lecomte",
+    author_email="elmotec@gmx.com",
+    license="MIT",
+    keywords="code metrics mining scm subversion svn Adam Tornhill utilities",
+    url="http://github.com/elmotec/codemetrics",
+    py_modules=['codemetrics'],
+    entry_points={'console_scripts': ['codemetrics=codemetrics:main']},
+    test_suite='tests',
+    setup_requires=['pandas'],
+    tests_require=['tqdm'],
+    python_requires='>=3.6',
+    classifiers=["Development Status :: 4 - Beta",
+                 "License :: OSI Approved :: MIT License",
+                 "Environment :: Console",
+                 "Natural Language :: English",
+                 "Programming Language :: Python :: 3.6",
+                 "Topic :: Software Development",
+                 "Topic :: Utilities",
+                 "Topic :: Software Development :: Version Control",
+                 "Intended Audience :: Developers",
+                ],
+)

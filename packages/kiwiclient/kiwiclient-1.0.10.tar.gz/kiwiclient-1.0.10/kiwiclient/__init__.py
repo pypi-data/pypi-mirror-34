53f876e83dd1bdfa7c7ef39e19853627a7600aac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:18 +0000

    Add package kiwiclient/1.0.10/kiwiclient-1.0.10.tar.gz

---
+++ packages/kiwiclient/kiwiclient-1.0.10.tar.gz/kiwiclient-1.0.10/kiwiclient/__init__.py
@@ -0,0 +1,8 @@
+#  _*_ coding: utf-8
+
+# Client to the KiWi engine for box calculations.
+# (c) ISC Clemenz & Weinbrecht GmbH 2018
+# License: https://www.apache.org/licenses/LICENSE-2.0
+#
+name = 'kiwiclient'
+

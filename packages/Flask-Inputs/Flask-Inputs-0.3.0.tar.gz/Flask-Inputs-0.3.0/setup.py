656283cdad40f1bcbe8f2b6f3c31860fffb27de4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:28 +0000

    Add package Flask-Inputs/0.3.0/Flask-Inputs-0.3.0.tar.gz

---
+++ packages/Flask-Inputs/Flask-Inputs-0.3.0.tar.gz/Flask-Inputs-0.3.0/setup.py
@@ -0,0 +1,25 @@
+
+from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='Flask-Inputs',
+    version='0.3.0',
+    description='Flask request data validation',
+    long_description=long_description,
+    url='http://pythonhosted.org/Flask-Inputs/',
+    author='Nathan Cahill',
+    author_email='nathan@nathancahill.com',
+    license='MIT',
+    keywords='flask validation wtforms',
+    packages=['flask_inputs'],
+    install_requires=['flask', 'wtforms', 'future'],
+    tests_require=['nose', 'coverage', 'jsonschema'],
+)

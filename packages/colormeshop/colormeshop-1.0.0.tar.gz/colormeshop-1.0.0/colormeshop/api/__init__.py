81d2c30dbea0814f9b59d5012ab4500f0654b890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:24 +0000

    Add package colormeshop/1.0.0/colormeshop-1.0.0.tar.gz

---
+++ packages/colormeshop/colormeshop-1.0.0.tar.gz/colormeshop-1.0.0/colormeshop/api/__init__.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from colormeshop.api.category_api import CategoryApi
+from colormeshop.api.charge_api import ChargeApi
+from colormeshop.api.customer_api import CustomerApi
+from colormeshop.api.delivery_api import DeliveryApi
+from colormeshop.api.etc_api import EtcApi
+from colormeshop.api.gift_api import GiftApi
+from colormeshop.api.group_api import GroupApi
+from colormeshop.api.payment_api import PaymentApi
+from colormeshop.api.product_api import ProductApi
+from colormeshop.api.sale_api import SaleApi
+from colormeshop.api.script_api import ScriptApi
+from colormeshop.api.shop_api import ShopApi
+from colormeshop.api.stock_api import StockApi

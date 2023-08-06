5486d8435e840f7cbb16476108fa37718334679c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:25 +0000

    Add package roshi/1.2.2/roshi-1.2.2.tar.gz

---
+++ packages/roshi/roshi-1.2.2.tar.gz/roshi-1.2.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="roshi",
+    version="1.2.2",
+    author="Abinav Bukkaraya",
+    author_email="abinavbukkaraya@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Bukkaraya/Roshi",
+    license="MIT"
+)

b536c12998936e0226c0568dd1735257c072b486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:44 +0000

    Add package pip-lock/1.2.0/pip-lock-1.2.0.tar.gz

---
+++ packages/pip-lock/pip-lock-1.2.0.tar.gz/pip-lock-1.2.0/setup.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import codecs
+import re
+
+from setuptools import setup
+
+
+def get_version(filename):
+    with codecs.open(filename, 'r', 'utf-8') as fp:
+        contents = fp.read()
+    return re.search(r"__version__ = ['\"]([^'\"]+)['\"]", contents).group(1)
+
+
+version = get_version('pip_lock.py')
+
+
+with codecs.open('README.rst', 'r', 'utf-8') as readme_file:
+    readme = readme_file.read()
+
+with codecs.open('HISTORY.rst', 'r', 'utf-8') as history_file:
+    history = history_file.read()
+
+
+setup(
+    name='pip-lock',
+    version=version,
+    description="Check for differences between requirements.txt files and your environment.",
+    long_description=readme + '\n\n' + history,
+    author="Aaron Kirkbride, Adam Johnson, et al.",
+    author_email='me@adamj.eu',
+    url='https://github.com/adamchainz/pip-lock',
+    py_modules=['pip_lock'],
+    include_package_data=True,
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    license="ISCL",
+    zip_safe=False,
+    keywords='pip, requirements',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
+)

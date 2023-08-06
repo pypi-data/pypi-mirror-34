854ddd77e2f9ebd6db5c423aa2d041a5bfeb83c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:48 +0000

    Add package zdy_nester/1.4.0/zdy_nester-1.4.0.tar.gz

---
+++ packages/zdy_nester/zdy_nester-1.4.0.tar.gz/zdy_nester-1.4.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='zdy_nester',
+    version='1.4.0',
+    py_modules=['nester'],
+    author='zdy826',
+    author_email='747342197@qq.com',
+    description='A simple printer of nested lists',
+)

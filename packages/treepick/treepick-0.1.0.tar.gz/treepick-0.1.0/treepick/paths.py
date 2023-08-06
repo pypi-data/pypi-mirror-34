489c77244e8bf5732073183686a3393d775fa633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:21 +0000

    Add package treepick/0.1.0/treepick-0.1.0.tar.gz

---
+++ packages/treepick/treepick-0.1.0.tar.gz/treepick-0.1.0/treepick/paths.py
@@ -0,0 +1,186 @@
+import os
+import curses
+
+from .color import Color
+from pdu import du
+
+
+class Paths:
+    def __init__(self, win, name, hidden, picked=set(), expanded=set(), sized=dict()):
+        self.win = win
+        self.name = name
+        self.hidden = hidden
+        self.picked = picked
+        self.expanded = expanded
+        self.sized = sized
+        self.color = Color(self.win, self.picked)
+        self.paths = None
+        self.marked = False
+        self.children = self.getchildren()
+
+    ###########################################################################
+    #                           LINE JUMPING METHODS                          #
+    ###########################################################################
+
+    def nextparent(self, parent, curline, depth):
+        '''
+        Get index of current parent directory, in the context of it's traversal,
+        count until we reach the node corresponding to that index + 1 - ie) the
+        next parent.
+        '''
+        line = 0
+        count = 0
+        if depth > 1:
+            curpar = os.path.dirname(os.path.dirname(self.name))
+            cpaths = Paths(self.win, curpar, self.hidden)
+            curdir = os.path.basename(os.path.dirname(self.name))
+            curidx = cpaths.children.index(curdir)
+            nextdir = cpaths.children[curidx + 1]
+            for c, d in parent.traverse():
+                if os.path.basename(c.name) == nextdir:
+                    break
+                if line > curline:
+                    self.color.default(self.name)
+                    count += 1
+                line += 1
+        else:
+            # if we're in root then skip to next dir
+            for c, d in parent.traverse():
+                if line > curline + 1:
+                    self.color.default(self.name)
+                    count += 1
+                    if os.path.isdir(c.name):
+                        break
+                line += 1
+        return count
+
+    def prevparent(self, parent, curline, depth):
+        '''
+        Count lines from top of parent until we reach our current path and then
+        return that count so that we can set curline to it.
+        '''
+        p = os.path.dirname(self.name)
+        # once we hit the parent directory, break, and set the
+        # curline to the line number we got to.
+        if depth > 1:
+            curline = 0
+            for c, d in parent.traverse():
+                if c.name == p:
+                    curline -= 1
+                    c.drawline(d, 0, curline)
+                    self.color.default(self.name)
+                    p = c
+                    break
+                curline += 1
+        else:
+            curline -= 1
+            p = self.name
+        return curline, p
+
+    ###########################################################################
+    #                       CURSES LINE DRAWING METHODS                       #
+    ###########################################################################
+
+    def getnode(self):
+        if not os.path.isdir(self.name):
+            return '    ' + os.path.basename(self.name)
+        elif self.name in self.expanded:
+            return '[-] ' + os.path.basename(self.name) + '/'
+        elif self.getpaths():
+            return '[+] ' + os.path.basename(self.name) + '/'
+        elif self.children is None:
+            return '[?] ' + os.path.basename(self.name) + '/'
+        else:
+            return '[ ] ' + os.path.basename(self.name) + '/'
+
+    def mkline(self, depth, width):
+        pad = ' ' * 4 * depth
+        node = self.getnode()
+        if os.path.abspath(self.name) in self.sized:
+            size = self.sized[os.path.abspath(self.name)]
+        else:
+            size = ''
+        nodestr = '{}{}{}'.format(pad, node, size)
+        return nodestr + ' ' * (width - len(nodestr))
+
+    def drawline(self, depth, curline, line):
+        max_y, max_x = self.win.getmaxyx()
+        offset = max(0, curline - max_y + 8)
+        y = line - offset
+        x = 0
+        string = self.mkline(depth - 1, max_x)
+        if 0 <= line - offset < max_y - 1:
+            self.win.addstr(y, x, string)  # paint str at y, x co-ordinates
+
+    def drawtree(self, curline):
+        '''
+        Loop over the object, process path attribute sets, and drawlines based on
+        their current contents.
+        '''
+        self.win.erase()
+        l = 0
+        for c, d in self.traverse():
+            if d == 0:
+                continue
+            if l == curline:
+                c.color.curline(c.name)
+            else:
+                c.color.default(c.name)
+            if c.name in self.picked:
+                c.marked = True
+            path = os.path.abspath(c.name)
+            if path in self.sized and not self.sized[path]:
+                self.sized[path] = " (" + du(c.name) + ")"
+            c.drawline(d, curline, l)
+            l += 1
+        self.win.refresh()
+
+    ###########################################################################
+    #                    PATH OBJECT INSTANTIATION METHODS                    #
+    ###########################################################################
+
+    def listdir(self, path):
+        '''
+        Return a list of all non dotfiles in a given directory.
+        '''
+        for f in os.listdir(path):
+            if not f.startswith('.'):
+                yield f
+
+    def getchildren(self):
+        '''
+        Create list of paths to be used to instantiate path objects for traversal,
+        based on whether or not hidden attribute is set.
+        '''
+        try:
+            if self.hidden:
+                return sorted(self.listdir(self.name))
+            else:
+                return sorted(os.listdir(self.name))
+        except:
+            return None  # probably permission denied
+
+    def getpaths(self):
+        '''
+        If we have children, use a list comprehension to instantiate new paths
+        objects to traverse.
+        '''
+        self.children = self.getchildren()
+        if self.children or not self.paths:
+            return [Paths(self.win,
+                          os.path.join(self.name, child),
+                          self.hidden,
+                          self.picked,
+                          self.expanded,
+                          self.sized)
+                    for child in self.children]
+
+    def traverse(self):
+        '''
+        Recursive generator that lazily unfolds the filesystem.
+        '''
+        yield self, 0
+        if self.name in self.expanded:
+            for child in self.getpaths():
+                for c, depth in child.traverse():
+                    yield c, depth + 1
---
+++ packages/treepick/treepick-0.1.0.tar.gz/treepick-0.1.0/treepick/pick.py
@@ -0,0 +1,130 @@
+import os
+import cgitb
+import curses
+import pdb
+import readline
+import sys
+from .paths import Paths
+from .keys import parse
+from .color import Color
+
+# Get more detailed traceback reports
+cgitb.enable(format="text")  # https://pymotw.com/2/cgitb/
+
+
+def process(parent, action, curline):
+    '''
+    Traverse parent object & process the action returned from keys.parse
+    '''
+    line = 0
+    for child, depth in parent.traverse():
+        if depth == 0:
+            continue  # don't process root node
+        if line == curline:
+            if action == 'expand':
+                if os.path.isdir(child.name):
+                    child.expanded.add(child.name)
+                curline += 1
+            elif action == 'collapse':
+                if child.name in child.expanded:
+                    child.expanded.remove(child.name)
+            elif action == 'expand_all':
+                if os.path.isdir(child.name):
+                    child.expanded.add(child.name)
+                    for c in child.children:
+                        if os.path.isdir(child.name + "/" + c):
+                            child.expanded.add(child.name + "/" + c)
+                curline += 1
+            elif action == 'collapse_all':
+                if depth > 1:
+                    curline, p = child.prevparent(parent, curline, depth)
+                    child.expanded.remove(p.name)
+                    for x in list(child.expanded):  # iterate over copy
+                        parent = os.path.abspath(p.name)
+                        path = os.path.abspath(x)
+                        if path.startswith(parent):
+                            child.expanded.remove(x)
+            elif action == 'toggle_expand':
+                if child.name in child.expanded:
+                    child.expanded.remove(child.name)
+                elif os.path.isdir(child.name):
+                    child.expanded.add(child.name)
+                else:
+                    curline += 1
+            elif action == 'toggle_mark':
+                if child.name in child.picked:
+                    child.picked.remove(child.name)
+                else:
+                    child.picked.add(child.name)
+                curline += 1
+            elif action == 'next_parent':
+                curline += child.nextparent(parent, curline, depth)
+            elif action == 'prev_parent':
+                curline = child.prevparent(parent, curline, depth)[0]
+            elif action == 'get_size':
+                child.sized[os.path.abspath(child.name)] = None
+                curline += 1
+            elif action == 'get_size_all':
+                for c, d in parent.traverse():
+                    child.sized[os.path.abspath(c.name)] = None
+            action = None  # reset action
+        line += 1  # keep scrolling!
+    return curline, line
+
+
+def header(screen):
+    msg = "Use arrow keys, Vi [h,j,k,l], or Emacs [b,f,p,n] keys to navigate"
+    try:
+        screen.addstr(msg)
+        screen.chgat(0, 19, 9, curses.A_BOLD | curses.color_pair(3))
+        screen.chgat(0, 39, 9, curses.A_BOLD | curses.color_pair(3))
+    except:
+        pass
+
+
+def footer(screen):
+    msg = "[SPC] toggle mark, [?] show all keybindings, [q] to quit."
+    try:
+        l = curses.LINES - 1
+        screen.addstr(l, 0, msg)
+        screen.chgat(l, 0, 5, curses.A_BOLD | curses.color_pair(3))
+        screen.chgat(l, 19, 3, curses.A_BOLD | curses.color_pair(3))
+        screen.chgat(l, 45, 3, curses.A_BOLD | curses.color_pair(3))
+    except:
+        pass
+
+
+def reset(win, root, hidden):
+    parent = Paths(win, root, hidden, expanded=set(root), sized=dict())
+    action = None
+    curline = 0
+    return parent, action, curline
+
+
+def init(screen):
+    curses.curs_set(0)  # get rid of cursor
+    header(screen)
+    footer(screen)
+
+
+def pick(screen, root, hidden):
+    init(screen)
+    Color(screen)
+    win = curses.newwin(curses.LINES - 3, curses.COLS, 2, 0)
+    parent, action, curline = reset(win, root, hidden)
+    screen.refresh()
+    while True:
+        # to reset or toggle view of dotfiles we need to create a new Path
+        # object before erasing the screen & descending into process function.
+        if action == 'reset':
+            parent, action, curline = reset(win, root, hidden)
+        elif action == 'toggle_hidden':
+            if parent.hidden:
+                parent.hidden = False
+            else:
+                parent.hidden = True
+        elif action == 'quit':
+            return parent.picked
+        curline, line = process(parent, action, curline)
+        parent.drawtree(curline)
+        action, curline = parse(screen, win, curline, line)

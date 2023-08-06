2e37b67b53a8b8048b08fbbd5e823e7eb4884d37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:16 +0000

    Add package tango-admin/0.3.3/tango-admin-0.3.3.tar.gz

---
+++ packages/tango-admin/tango-admin-0.3.3.tar.gz/tango-admin-0.3.3/tango_admin/models.py
@@ -0,0 +1,38 @@
+from django.conf import settings
+from django.db import models
+
+UserModel = getattr(settings, "AUTH_USER_MODEL", "auth.User")
+
+"""
+# Add some description and featured to Site and auth.Group models
+Site.add_to_class('description', models.CharField(
+    max_length=100,
+    blank=True,
+    help_text='A brief description to differentiate this site.')
+)
+
+Group.add_to_class('description', models.TextField(
+    blank=True,
+    help_text='A brief description of this group')
+)
+"""
+
+
+class Blacklist(models.Model):
+    user = models.ForeignKey(UserModel, editable=False, on_delete="CASCADE")
+    date = models.DateTimeField(auto_now_add=True)
+    blacklister = models.ForeignKey(UserModel, editable=False, related_name="blacklister", on_delete="CASCADE")
+    reason = models.TextField(help_text="""
+        The reason for the blacklist for our records.
+        This will not be public.""")
+    is_spammer = models.BooleanField(
+        default=False,
+        help_text="""If they are a spammer, please check this.
+        All of their comments will be permanently removed."""
+    )
+
+    class Meta:
+        app_label = 'tango_admin'
+
+    def __unicode__(self):
+        return self.user.username

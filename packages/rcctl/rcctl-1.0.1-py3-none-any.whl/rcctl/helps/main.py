b61c36ec4913503236616c908794895d5a800dcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:12 +0000

    Add package rcctl/1.0.1/rcctl-1.0.1-py3-none-any.whl

---
+++ packages/rcctl/rcctl-1.0.1-py3-none-any.whl/rcctl/helps/main.py
@@ -0,0 +1,26 @@
+# -----------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# -----------------------------------------------------------------------------
+
+"""Help documentation for Service Fabric groups"""
+
+from knack.help_files import helps
+
+helps[''] = """
+    type: group
+    short-summary: Commands for managing Service Fabric reliable collections.
+    long-summary: Commands follow the noun-verb pattern. See subgroups for more
+        information.
+"""
+
+helps['cluster'] = """
+    type: group
+    short-summary: Select a Service Fabric cluster to operate against. Defaults to http://localhost:19080/
+"""
+
+helps['collections'] = """
+    type: group
+    short-summary: Query and perform operations on reliable collections
+"""

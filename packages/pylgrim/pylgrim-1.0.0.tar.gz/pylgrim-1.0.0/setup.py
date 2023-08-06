4d3d28ecc1d9f0fa4a41552bff337022df9522ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:03 +0000

    Add package pylgrim/1.0.0/pylgrim-1.0.0.tar.gz

---
+++ packages/pylgrim/pylgrim-1.0.0.tar.gz/pylgrim-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pylgrim",
+    version="1.0.0",
+    author="Toon Weyens",
+    author_email="weyenst@gmail.com",
+    description="Elementary shortest path problems, with or without resource constraints",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/ToonWeyens/pylgrim",
+    packages=find_packages(),
+    keywords = ['espp', 'espprc', 'shortest-path', 'graph', 'python'], # arbitrary keywords
+    requires = ['networkx', 'logging', 'numpy', 'copy', 'collections', 'matplotlib', 'random', 'timeit'],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ),
+)

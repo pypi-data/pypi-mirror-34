ad0e160d4dd557ba658a7a905d664c5e44c87144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:18 +0000

    Add package imod/0.3.0/imod-0.3.0.tar.gz

---
+++ packages/imod/imod-0.3.0.tar.gz/imod-0.3.0/imod/rasterio.py
@@ -0,0 +1,62 @@
+import rasterio
+import numpy as np
+from pathlib import Path
+
+
+def write(path, da, driver=None, nodata=np.nan):
+    """Write DataArray to GDAL supported geospatial rasters using rasterio
+    
+    Parameters
+    ----------
+    path: str or Path
+        path to the output raster
+    da: xarray DataArray
+        The DataArray to be written. Should have only x and y dimensions.
+    driver: str; optional
+        Which GDAL format driver to use. The complete list is at
+        http://www.gdal.org/formats_list.html.
+        By default tries to guess from the file extension.
+    nodata: float
+        Nodata value to use. Should be convertible to the DataArray and GDAL dtype.
+        Default value is np.nan
+    """
+    # Not directly related to iMOD, but provides a missing link, together
+    # with xarray.open_rasterio.
+    # Note that this function can quickly become outdated as
+    # the xarray rasterio connection matures, see for instance:
+    # https://github.com/pydata/xarray/issues/1736
+    # https://github.com/pydata/xarray/pull/1712
+    if isinstance(path, str):
+        path = Path(path)
+    profile = da.attrs.copy()
+    if driver is None:
+        ext = path.suffix.lower()
+        if ext in (".tif", ".tiff"):
+            driver = "GTiff"
+        elif ext == ".asc":
+            driver = "AAIGrid"
+        elif ext == ".map":
+            driver = "PCRaster"
+        else:
+            raise ValueError("Unknown extension {}, specifiy driver".format(ext))
+    # prevent rasterio warnings
+    if driver == "AAIGrid":
+        profile.pop("res", None)
+        profile.pop("is_tiled", None)
+    # transform will be affine object in next xarray
+    profile["transform"] = profile["transform"][:6]
+    profile["driver"] = driver
+    profile["height"] = da.y.size
+    profile["width"] = da.x.size
+    profile["count"] = 1
+    profile["dtype"] = da.dtype
+    profile["nodata"] = nodata
+    if (nodata is None) or np.isnan(nodata):
+        # NaN is the default missing value in xarray
+        # None is different in that the raster won't have a nodata value
+        dafilled = da
+    else:
+        dafilled = da.fillna(nodata)
+    with rasterio.Env():
+        with rasterio.open(path, "w", **profile) as ds:
+            ds.write(dafilled.values, 1)
---
+++ packages/imod/imod-0.3.0.tar.gz/imod-0.3.0/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+
+with open("README.rst") as f:
+    long_description = f.read()
+
+setup(
+    name="imod",
+    version="0.3.0",
+    description="Work with iMOD MODFLOW models",
+    long_description=long_description,
+    url="https://gitlab.com/deltares/imod-python",
+    author="Martijn Visser",
+    author_email="mgvisser@gmail.com",
+    license="MIT",
+    packages=["imod"],
+    test_suite="tests",
+    python_requires=">=3.6",
+    install_requires=[
+        "numpy",
+        "xarray>=0.10",
+        "pandas",
+        "dask",
+        "cytoolz",  # optional dask dependency we need
+        "toolz",  # optional dask dependency we need
+        "affine",
+    ],
+    extras_require={
+        "dev": ["pytest", "sphinx", "sphinx_rtd_theme"],
+        "optional": ["rasterio>=1"],
+    },
+    classifiers=[
+        # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords="imod modflow groundwater modeling",
+)

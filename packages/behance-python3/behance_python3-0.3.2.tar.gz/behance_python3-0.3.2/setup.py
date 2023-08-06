f1aa779188e070bdfa52ceff9bf4c389250ee54a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:11 +0000

    Add package behance-python3/0.3.2/behance_python3-0.3.2.tar.gz

---
+++ packages/behance-python3/behance_python3-0.3.2.tar.gz/behance_python3-0.3.2/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+from distutils.core import setup
+
+setup(
+        name='behance_python3',
+        packages=['behance_python3',],
+        install_requires=['requests'],
+        url='https://github.com/Menda0/behance_python3',
+        license='MIT',
+        author='Alex Ravenel',
+        author_email='ravenel@gmail.com',
+        version='0.3.2',
+        )

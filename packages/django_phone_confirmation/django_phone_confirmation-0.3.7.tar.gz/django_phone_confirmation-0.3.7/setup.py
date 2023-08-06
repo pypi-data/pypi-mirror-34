bcd9a065d99f7868c40551eeca73737b57f53727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:27 +0000

    Add package django_phone_confirmation/0.3.7/django_phone_confirmation-0.3.7.tar.gz

---
+++ packages/django_phone_confirmation/django_phone_confirmation-0.3.7.tar.gz/django_phone_confirmation-0.3.7/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from io import open
+from setuptools import setup, find_packages
+
+
+setup(
+    name='django_phone_confirmation',
+    version='0.3.7',
+    packages=find_packages(),
+    license='MIT',
+    author='Ricardo S. A. Silva',
+    description='A Django app to validate cell phone numbers through SMS messages and REST.',
+    keywords='django cell phone confirmation',
+    author_email='ricardo@salamandra.cc',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'djangorestframework',
+        'django-sendsms==0.3.1',
+        'django-phonenumber-field==2.0.0'
+    ],
+    include_package_data=True,
+    url='https://github.com/ricardosasilva/django_phone_confirmation',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ]
+)

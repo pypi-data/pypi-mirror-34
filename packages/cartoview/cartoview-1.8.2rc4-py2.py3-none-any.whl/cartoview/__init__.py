dc6b801fec24b558ee490be8afeda8d937f52536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:42 +0000

    Add package cartoview/1.8.2rc4/cartoview-1.8.2rc4-py2.py3-none-any.whl

---
+++ packages/cartoview/cartoview-1.8.2rc4-py2.py3-none-any.whl/cartoview/__init__.py
@@ -0,0 +1,9 @@
+from .celery import app
+
+__version__ = (1, 8, 2, 'rc', 4)
+__compatible_with__ = []
+
+
+def get_current_version():
+    from .version import get_version
+    return get_version(__version__)

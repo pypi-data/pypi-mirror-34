38b3ef381eed322c972bb87bb1f27970805b458d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:30 +0000

    Add package My-Logger/0.0.2/My-Logger-0.0.2.zip

---
+++ packages/My-Logger/My-Logger-0.0.2.zip/My-Logger-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='My-Logger',
+    version='0.0.2',
+    description='Customised logging library',
+    license='MIT',
+    packages=['My-Logger'],
+    author='Md Sadique',
+    author_email='msadique265@yahoo.com',
+    keywords=['python logging'],
+    url='https://github.com/msadique265/Modules'
+)

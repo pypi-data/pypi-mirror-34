5cea2e2c4b9132ca3f0fc3764f5d4707bfb7ced3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:41 +0000

    Add package omrdatasettools/0.12/omrdatasettools-0.12.tar.gz

---
+++ packages/omrdatasettools/omrdatasettools-0.12.tar.gz/omrdatasettools-0.12/omrdatasettools/downloaders/MuscimaPlusPlusDatasetDownloader.py
@@ -0,0 +1,39 @@
+import argparse
+import os
+
+from omrdatasettools.downloaders.DatasetDownloader import DatasetDownloader
+
+
+class MuscimaPlusPlusDatasetDownloader(DatasetDownloader):
+    """ Downloads the Muscima++ dataset
+        https://ufal.mff.cuni.cz/muscima
+        Copyright 2017 Jan Hajic jr. under CC-BY-NC-SA 4.0 license
+    """
+
+    def get_dataset_download_url(self) -> str:
+        return "https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11372/LRT-2372/MUSCIMA-pp_v1.0.zip?sequence=1&isAllowed=y"
+
+    def get_dataset_filename(self) -> str:
+        return "MUSCIMA-pp_v1.0.zip"
+
+    def download_and_extract_dataset(self, destination_directory: str):
+        if not os.path.exists(self.get_dataset_filename()):
+            print("Downloading MUSCIMA++ Dataset...")
+            self.download_file(self.get_dataset_download_url(), self.get_dataset_filename())
+
+        print("Extracting MUSCIMA++ Dataset...")
+        self.extract_dataset(os.path.abspath(destination_directory))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--dataset_directory",
+        type=str,
+        default="../data/muscima_pp_raw",
+        help="The directory, where the extracted dataset will be copied to")
+
+    flags, unparsed = parser.parse_known_args()
+
+    dataset = MuscimaPlusPlusDatasetDownloader()
+    dataset.download_and_extract_dataset(flags.dataset_directory)

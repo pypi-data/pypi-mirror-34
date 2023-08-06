ecbe2965f8447a0731d4d72e7e2b398a5ca1ff2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:53 +0000

    Add package mountainlab-pytools/0.4.3/mountainlab_pytools-0.4.3.tar.gz

---
+++ packages/mountainlab-pytools/mountainlab_pytools-0.4.3.tar.gz/mountainlab_pytools-0.4.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+setuptools.setup(
+    name="mountainlab_pytools",
+    version="0.4.3",
+    author="Jeremy Magland",
+    author_email="jmagland@flatironinstitute.org",
+    description="Tools for using MountainLab with python",
+    url="https://github.com/magland/mountainlab_pytools",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'requests',
+        'numpy',
+        'numpydoc',
+	'ipython',
+	'vdom',
+	'ipywidgets'
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ),
+)

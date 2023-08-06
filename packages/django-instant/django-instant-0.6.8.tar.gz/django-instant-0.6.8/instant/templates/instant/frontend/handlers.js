b55176eb6a9c8241116425319516a64dbceff510
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:11 +0000

    Add package django-instant/0.6.8/django-instant-0.6.8.tar.gz

---
+++ packages/django-instant/django-instant-0.6.8.tar.gz/django-instant-0.6.8/instant/templates/instant/frontend/handlers.js
@@ -0,0 +1,23 @@
+{% load instant_tags %}
+//console.log("NUM: {% num_excluded_chans %}");
+{% if num_excluded_chans != 0 %}
+	var exclude = [{% exclude_chans %}];
+	//console.log("EX VAR: "+exclude);
+	for (i=0;i<exclude.length;i++) {
+		//console.log(channel+" / "+exclude[i]);
+		if (event_class === exclude[i]) {
+			return
+		}
+	}
+{% else %}
+//console.log("NO");
+{% endif %}
+app.msgIconClass["fa-envelope-o"] = false;
+app.msgIconClass["fa-envelope"] = true;
+setTimeout(function(){
+	app.msgIconClass["fa-envelope-o"] = true;
+	app.msgIconClass["fa-envelope"] = false;
+},1200);
+app.msgs.unshift({"event_class": event_class, "message": message, "uid": uid});
+document.getElementById("instant_msgs").style.display = "inline-block";
+app.numMsgs++;

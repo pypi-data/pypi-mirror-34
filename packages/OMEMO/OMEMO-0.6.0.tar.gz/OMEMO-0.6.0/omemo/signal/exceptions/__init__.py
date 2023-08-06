1b958a93f295a7e1b287e32d5a74649916607a16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:10 +0000

    Add package OMEMO/0.6.0/OMEMO-0.6.0.tar.gz

---
+++ packages/OMEMO/OMEMO-0.6.0.tar.gz/OMEMO-0.6.0/omemo/signal/exceptions/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+from .doubleratchetexceptions import InvalidConfigurationException
+
+from .wireformatexceptions import (
+    IncompleteMessageException,
+    InvalidFieldError,
+    VersionException
+)
+
+from .x3dhexceptions import UnknownKeyException
---
+++ packages/OMEMO/OMEMO-0.6.0.tar.gz/OMEMO-0.6.0/omemo/signal/exceptions/wireformatexceptions.py
@@ -0,0 +1,8 @@
+class IncompleteMessageException(Exception):
+    pass
+
+class InvalidFieldError(Exception):
+    pass
+
+class VersionException(Exception):
+    pass

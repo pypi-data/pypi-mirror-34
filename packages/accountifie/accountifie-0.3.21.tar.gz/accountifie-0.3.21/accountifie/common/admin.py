220d26b4973b44e56465eacae7ba84de0e15e02d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:42 +0000

    Add package accountifie/0.3.21/accountifie-0.3.21.tar.gz

---
+++ packages/accountifie/accountifie-0.3.21.tar.gz/accountifie-0.3.21/accountifie/common/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import *
+
+
+class LogAdmin(admin.ModelAdmin):
+    list_display = ('level', 'time', 'message', 'corrId', 'traceback', 'request',)
+    
+admin.site.register(Log, LogAdmin)
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('label', 'name', 'company',)
+    
+admin.site.register(Address, AddressAdmin)

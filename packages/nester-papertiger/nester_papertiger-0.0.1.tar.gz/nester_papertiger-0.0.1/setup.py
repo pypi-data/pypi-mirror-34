ea37db0bec3203c66af0b85864587cb6c659434f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:26 +0000

    Add package nester-papertiger/0.0.1/nester_papertiger-0.0.1.tar.gz

---
+++ packages/nester-papertiger/nester_papertiger-0.0.1.tar.gz/nester_papertiger-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nester_papertiger",
+    version="0.0.1",
+    author="Xu Chang",
+    author_email="596784344@qq.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)

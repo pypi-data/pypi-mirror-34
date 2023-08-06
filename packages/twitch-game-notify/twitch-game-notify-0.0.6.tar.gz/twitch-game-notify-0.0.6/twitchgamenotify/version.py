d9843ec6baa38601b3525c7cd2d0f7c29e27c41a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:22 +0000

    Add package twitch-game-notify/0.0.6/twitch-game-notify-0.0.6.tar.gz

---
+++ packages/twitch-game-notify/twitch-game-notify-0.0.6.tar.gz/twitch-game-notify-0.0.6/twitchgamenotify/version.py
@@ -0,0 +1,7 @@
+"""Contains name, version, description."""
+
+NAME = 'twitch-game-notify'
+VERSION = '0.0.6'
+DESCRIPTION = (
+    "get notified when your favourite Twitch streamers "
+    "play your favourite games")

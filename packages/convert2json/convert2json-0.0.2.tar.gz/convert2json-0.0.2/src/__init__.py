591b290733e93eca3221840aefa4480878a89024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:46 +0000

    Add package convert2json/0.0.2/convert2json-0.0.2.tar.gz

---
+++ packages/convert2json/convert2json-0.0.2.tar.gz/convert2json-0.0.2/src/__init__.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+# created_on: 2018-08-08 13:21
+
+"""
+__init__.py
+"""
+
+
+__version__ = '0.0.2'
+__author__ = 'Toran Sahu  <toran.sahu@yahoo.com>'
+__license__ = 'Distributed under terms of the AGPL license.'
+
+
+name='convert2json'

d8b80987bdb2f2e02babc946248b4a4b9950ec00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:46 +0000

    Add package behave-classy/0.0.1a0/behave-classy-0.0.1a0.tar.gz

---
+++ packages/behave-classy/behave-classy-0.0.1a0.tar.gz/behave-classy-0.0.1a0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='behave-classy',
+    version='0.0.1a',
+    packages=['behave_classy'],
+    install_requires=['behave'],
+    url='https://github.com/spyoungtech/behave-classy',
+    license='MIT',
+    author='Spencer Young',
+    author_email='spencer.young@spyoung.com',
+    description='Class-based step implementations for behave'
+)

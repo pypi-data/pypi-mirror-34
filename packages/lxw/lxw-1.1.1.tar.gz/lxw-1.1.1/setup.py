ec54fcbf1efe4e38eae0d8cda2269fbc552d651c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:39 +0000

    Add package lxw/1.1.1/lxw-1.1.1.tar.gz

---
+++ packages/lxw/lxw-1.1.1.tar.gz/lxw-1.1.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="lxw",
+    version='1.1.1',
+    author="lxw",
+    author_email="723862367@qq.com",
+    description="lxwtest python modules",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    )
+)

78fb5efa6bc18b4d69164015fdedff2d3b018b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:22 +0000

    Add package zb/0.0.2/zb-0.0.2.tar.gz

---
+++ packages/zb/zb-0.0.2.tar.gz/zb-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import zb
+
+setup(
+    name="zb",
+    version=zb.__version__,
+    keywords=("zb", "tool", "algorithm"),
+    description="My Personal Package",
+    long_description="Personal established tools and implement of algorithm. ",
+    license="Apache-2.0",
+
+    url="https://github.com/zengbin93/zb",
+    author=zb.__author__,
+    author_email="zeng_bin8888@163.com",
+
+    packages=find_packages(exclude=['code_set', 'test', 'images']),
+    include_package_data=True,
+    install_requires=[
+        "requests", "pandas", "click", 'retrying',
+        "numpy", "wxpy"
+    ],
+    entry_points={
+        "console_scripts": [
+            "zb=zb.cli:zb"
+        ]
+    }
+)

e95b65df8d6a1bc16b7297c8e83403e00eb5d080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:36 +0000

    Add package ssdata/0.13/ssdata-0.13.tar.gz

---
+++ packages/ssdata/ssdata-0.13.tar.gz/ssdata-0.13/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(name='ssdata',
+      version='0.13',
+      description='test',
+      url='http://github.com/tongling/clinicaltrial',
+      author='zejin',
+      author_email='ginynu@gmail.com',
+      license='MIT',
+      packages=['ssdata'],
+      install_requires=["pandas","requests"],
+      zip_safe=False)

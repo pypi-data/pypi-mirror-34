30c009de855eb55fd57e87ae14c9309b94a6cb9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:53 +0000

    Add package redvox/0.5/redvox-0.5.tar.gz

---
+++ packages/redvox/redvox-0.5.tar.gz/redvox-0.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt", "r") as requirements_file:
+    requirements = list(map(lambda line: line.strip(), requirements_file.readlines()))
+    setup(name='redvox',
+          version='0.5',
+          url='https://bitbucket.org/redvoxhi/redvox-api900-python-reader/src/master/',
+          license='Apache',
+          author='RedVox',
+          author_email='dev@redvoxsound.com',
+          description='Library for working with RedVox files.',
+          packages=find_packages(include=["redvox"], exclude=['tests']),
+          long_description=open('README.md').read(),
+          install_requirements=requirements,
+          zip_safe=False)

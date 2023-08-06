a5305417cf104adc9d49d44b8876442c5d7a23b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:52 +0000

    Add package abc1234/10.0/abc1234-10.0.tar.gz

---
+++ packages/abc1234/abc1234-10.0.tar.gz/abc1234-10.0/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+	name='abc1234',
+	version='10.0',
+	description='kmeans',
+	url='',
+	author='abhi',
+	author_email='abhi@yahoo.com',
+	license='MIT',
+	py_modules=['demo'],
+	packages=['myPackage'],
+	classifiers=[
+		'Development Status :: 4 - Beta',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python :: 3',		
+		],
+	entry_points={
+		'console_scripts': [
+			'hello=mypackage.demo:demoprint',
+			],
+		}
+	)

11baff8eaf1776ecf56472bce0783e74ebaa9a2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:22 +0000

    Add package influxpy/0.0.2/influxpy-0.0.2.tar.gz

---
+++ packages/influxpy/influxpy-0.0.2.tar.gz/influxpy-0.0.2/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name="influxpy",
+    version="0.0.2",
+    description="Python logging handler that sends messages to InfluxDB using the line protocol over UDP.",
+    long_description=open("README.md").read(),
+    keywords="influxdb grafana logger logging udp",
+    author="Arne Welzel",
+    author_email="arne.welzel@gmail.com",
+    url="https://github.com/awelzel/influxpy",
+    license="BSD License",
+    packages=find_packages(),
+    classifiers=["License :: OSI Approved :: BSD License",
+                 "Programming Language :: Python :: 2",
+                 "Programming Language :: Python :: 3"],
+)

8af8a2b86189f9d5b1307c170072a209e6ae67ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:22 +0000

    Add package ie_mbdbl2018_leticiajaramillo/0.1/ie_mbdbl2018_leticiajaramillo-0.1.tar.gz

---
+++ packages/ie_mbdbl2018_leticiajaramillo/ie_mbdbl2018_leticiajaramillo-0.1.tar.gz/ie_mbdbl2018_leticiajaramillo-0.1/ie_mbdbl2018_leticiajaramillo/__init__.py
@@ -0,0 +1,3 @@
+def  datareader_doc():
+    """ function to return the string 'This is Leticia Jaramillo first package that anyone can install it! How cool!!!"' """
+    return("This is Leticia Jaramillo first package that anyone can install it! How cool!!!"")
---
+++ packages/ie_mbdbl2018_leticiajaramillo/ie_mbdbl2018_leticiajaramillo-0.1.tar.gz/ie_mbdbl2018_leticiajaramillo-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ie_mbdbl2018_leticiajaramillo',
+      version='0.1',  # Development release
+      description='Python Packaging Starting Kit - Simple Structure to creater your own package - for training purposes!',
+      url='https://github.com/Leticiajaramillo/ie_mbdbl2018_leticiajaramillo',
+      author='Leticia Jaramillo',
+      author_email='leticiajaramillo@student.ie.edu',
+      license='MIT',
+          packages=['ie_mbdbl2018_leticiajaramillo'],
+      zip_safe=False)

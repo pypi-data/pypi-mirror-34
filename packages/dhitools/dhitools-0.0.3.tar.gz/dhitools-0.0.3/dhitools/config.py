261ff5b5ad491fcd1d683d5d2c7a6484d5df60dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:50 +0000

    Add package dhitools/0.0.3/dhitools-0.0.3.tar.gz

---
+++ packages/dhitools/dhitools-0.0.3.tar.gz/dhitools-0.0.3/dhitools/config.py
@@ -0,0 +1,9 @@
+"""dhitools config file paths
+
+Author: Robert Wall
+
+"""
+
+MIKE_SDK = r'C:\Program Files (x86)\DHI\2014\MIKE SDK\bin'
+MIKE_DFS = r'DHI.Generic.MikeZero.DFS.dll'
+MIKE_EUM = r'DHI.Generic.MikeZero.EUM.dll'

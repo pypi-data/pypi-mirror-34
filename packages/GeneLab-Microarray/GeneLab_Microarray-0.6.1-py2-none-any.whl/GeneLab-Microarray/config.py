cd92879d98731c39478ed0fbee54b1a3835be704
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:53 +0000

    Add package GeneLab-Microarray/0.6.1/GeneLab_Microarray-0.6.1-py2-none-any.whl

---
+++ packages/GeneLab-Microarray/GeneLab_Microarray-0.6.1-py2-none-any.whl/GeneLab-Microarray/config.py
@@ -0,0 +1,19 @@
+indir = "./test_batch.txt"
+outdir = "assay_test"
+srcdir = "/Users/jonathanrubin/Google_Drive/NASA/home/GeneLab-Microarray/GeneLab-Microarray"
+wrkdir = "/Users/jonathanrubin/Google_Drive/NASA/home"
+tempdir = "/Users/jonathanrubin/Google_Drive/NASA/home/GeneLab-Microarray/temp"
+R_dir = "/Users/jonathanrubin/Google_Drive/NASA/home/GeneLab-Microarray/GeneLab-Microarray/R_scripts"
+md5sum = {"original": [], "new": []}
+batch = "True"
+visualize = "False"
+def get_md5sum(filepath,key,action=False):
+    import os, subprocess
+    if not action:
+        md5sum_command = ["md5sum",filepath]
+        md5sum_character = subprocess.check_output(md5sum_command).split(' ')[0].encode("utf-8")
+        md5sum[key].append((os.path.basename(os.path.normpath(filepath)),md5sum_character))
+    else:
+        md5sum_command = ["md5sum",filepath]
+        md5sum_character = subprocess.check_output(md5sum_command).split(' ')[0].encode("utf-8")
+        md5sum[key].append((action,os.path.basename(os.path.normpath(filepath)),md5sum_character))

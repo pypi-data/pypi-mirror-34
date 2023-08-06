568933a23a9c82128546b06442cccf168995450a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:50 +0000

    Add package Vipc/0.0.8/Vipc-0.0.8.tar.gz

---
+++ packages/Vipc/Vipc-0.0.8.tar.gz/Vipc-0.0.8/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Vipc",
+    packages=find_packages(),
+    version='0.0.8',
+    description="command line tool for auto tuner",
+    author="liyang",
+    author_email='liyang@vipcode.com',
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    url='https://github.com/ly1016799291/ceshi.git',
+    install_requires=[
+        'baidu-aip',
+        'pyaudio',
+        'soundfile',
+        'pygame',
+        'PyQt5',
+        'qtpy',
+    ]
+)

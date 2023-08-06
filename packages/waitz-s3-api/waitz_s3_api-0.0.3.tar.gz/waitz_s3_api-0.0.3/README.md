dcd98d894390d43216d894c29547d99bdb6895f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:46 +0000

    Add package waitz-s3-api/0.0.3/waitz_s3_api-0.0.3.tar.gz

---
+++ packages/waitz-s3-api/waitz_s3_api-0.0.3.tar.gz/waitz_s3_api-0.0.3/README.md
@@ -0,0 +1,11 @@
+# Waitz S3 API Docs
+
+#### download_file(bucket, key, local_path)
+	
+    Grab a file from S3 and bring it onto your local machine at the
+    specified path.
+
+    bucket (str)     -- Bucket that the file exists in
+    key (str)        -- Key that the file exists in
+    local_path (str) -- Where the file will be stored on the caller's machine
+    
---
+++ packages/waitz-s3-api/waitz_s3_api-0.0.3.tar.gz/waitz_s3_api-0.0.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="waitz_s3_api",
+    version="0.0.3",
+    author="Daniel Fritsch",
+    author_email="dfritsch99@gmail.com",
+    description='API created by Waitz that provides useful functionality to read and write from AWS S3.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/Waitz-Inc/waitz_s3_api',
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)

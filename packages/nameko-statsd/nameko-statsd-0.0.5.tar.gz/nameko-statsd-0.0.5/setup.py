b91668dff4d8f9089ed9223d777ee3726851957d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:46 +0000

    Add package nameko-statsd/0.0.5/nameko-statsd-0.0.5.tar.gz

---
+++ packages/nameko-statsd/nameko-statsd-0.0.5.tar.gz/nameko-statsd-0.0.5/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+def reqs(filepath):
+    with open(filepath) as stream:
+        return [line.strip() for line in stream.readlines()]
+
+
+setup(
+    name='nameko-statsd',
+    version='0.0.5',
+    description='StatsD dependency for nameko services',
+    author='Sohonet product team',
+    author_email='fabrizio.romano@sohonet.com',
+    url='https://github.com/sohonetlabs/nameko-statsd',
+    packages=['nameko_statsd'],
+    install_requires=reqs('requirements/base.txt'),
+    extras_require={
+        'dev': reqs('requirements/dev.txt'),
+    },
+    zip_safe=True,
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Developers",
+    ]
+)

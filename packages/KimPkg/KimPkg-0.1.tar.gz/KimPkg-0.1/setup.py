493ba23c1e2d580353aaa20f22538fa0f09168d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:42 +0000

    Add package KimPkg/0.1/KimPkg-0.1.tar.gz

---
+++ packages/KimPkg/KimPkg-0.1.tar.gz/KimPkg-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='KimPkg',
+      version='0.1',
+      description='Kim Package',
+      url='http://github.com/hgkmail/KimPkg',
+      author='Kim Huang',
+      author_email='hgkformap@gmail.com',
+      license='MIT',
+      packages=['KimPkg'],
+      zip_safe=False)

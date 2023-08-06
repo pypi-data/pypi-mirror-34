87c3c605a18116e237757e267faf56fd46b0f40f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:44 +0000

    Add package uptick/0.1.9/uptick-0.1.9.tar.gz

---
+++ packages/uptick/uptick-0.1.9.tar.gz/uptick-0.1.9/uptick/witness.py
@@ -0,0 +1,84 @@
+import click
+from prettytable import PrettyTable
+from bitshares.witness import Witnesses
+from bitshares.storage import configStorage as config
+from pprint import pprint
+from .decorators import (
+    onlineChain,
+    unlockWallet
+)
+from .main import main
+
+
+@main.command()
+@click.pass_context
+@onlineChain
+@click.argument(
+    'witnesses',
+    nargs=-1)
+@click.option(
+    "--account",
+    default=config["default_account"],
+    help="Account that takes this action",
+    type=str)
+@unlockWallet
+def approvewitness(ctx, witnesses, account):
+    """ Approve witness(es)
+    """
+    pprint(ctx.bitshares.approvewitness(
+        witnesses,
+        account=account
+    ))
+
+
+@main.command()
+@click.pass_context
+@onlineChain
+@click.argument(
+    'witnesses',
+    nargs=-1)
+@click.option(
+    "--account",
+    help="Account that takes this action",
+    default=config["default_account"],
+    type=str)
+@unlockWallet
+def disapprovewitness(ctx, witnesses, account):
+    """ Disapprove witness(es)
+    """
+    pprint(ctx.bitshares.disapprovewitness(
+        witnesses,
+        account=account
+    ))
+
+
+@main.command()
+@click.pass_context
+@onlineChain
+def witnesses(ctx):
+    t = PrettyTable([
+        "weight",
+        "account",
+        "signing_key",
+        "vote_id",
+        "url",
+        "total_missed",
+        "last_confirmed_block_num"
+    ])
+    t.align = 'l'
+    for witness in sorted(
+        Witnesses(),
+        key=lambda x: x.weight,
+        reverse=True
+    ):
+        witness.refresh()
+        t.add_row([
+            "{:.2f}%".format(witness.weight * 100),
+            witness.account["name"],
+            witness["signing_key"],
+            witness["vote_id"],
+            witness["url"],
+            witness["total_missed"],
+            witness["last_confirmed_block_num"]
+        ])
+    click.echo(t)

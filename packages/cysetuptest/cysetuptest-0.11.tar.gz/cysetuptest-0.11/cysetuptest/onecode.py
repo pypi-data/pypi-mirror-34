47a7952386d72cd2581d08a3d9c35beb43675581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:06 +0000

    Add package cysetuptest/0.11/cysetuptest-0.11.tar.gz

---
+++ packages/cysetuptest/cysetuptest-0.11.tar.gz/cysetuptest-0.11/cysetuptest/onecode.py
@@ -0,0 +1,4 @@
+﻿# -*- coding:utf-8 -*-
+class onecode():
+    def run():
+        print('这是第一个测试程序的更新程序');
---
+++ packages/cysetuptest/cysetuptest-0.11.tar.gz/cysetuptest-0.11/setup.py
@@ -0,0 +1,18 @@
+﻿# -*- coding:utf-8 -*-
+from setuptools import setup, find_packages
+import cysetuptest
+
+setup(
+    name='cysetuptest',
+    version='0.11',
+    packages=find_packages(),
+    zip_safe=False,
+    author='YuChen',
+    author_email = '14075840@qq.com',
+    keywords=['firstsetuptest'],
+    url='http://pypi.python.org/pypi/cysetuptest/',
+    license = "GPL",
+    description='first setup test',
+    long_description='first setup test of mine',
+    platforms = "Independant",
+)

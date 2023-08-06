6cd42766eb58556d5e080480995c2903e85e0d6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:59 +0000

    Add package score.distlock/0.2.5/score.distlock-0.2.5.tar.gz

---
+++ packages/score.distlock/score.distlock-0.2.5.tar.gz/score.distlock-0.2.5/setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.rst')) as f:
+    README = f.read()
+
+setup(
+    name='score.distlock',
+    version='0.2.5',
+    description='Mutex for distributed operations with The SCORE Framework',
+    long_description=README,
+    author='strg.at',
+    author_email='score@strg.at',
+    url='http://score-framework.org',
+    keywords='score framework mutex synchronization',
+    packages=['score', 'score.distlock'],
+    namespace_packages=['score'],
+    license='LGPL',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Framework :: Pyramid',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General '
+            'Public License v3 or later (LGPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
+    install_requires=[
+        'score.init >= 0.3',
+        'SQLAlchemy >= 0.9',
+    ],
+)

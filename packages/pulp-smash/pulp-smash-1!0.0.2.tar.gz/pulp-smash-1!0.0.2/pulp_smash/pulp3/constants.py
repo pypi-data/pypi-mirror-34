d70042c08dd5380a41ad1c0e72fc61ff7a8ca3e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:31 +0000

    Add package pulp-smash/1!0.0.2/pulp-smash-1!0.0.2.tar.gz

---
+++ packages/pulp-smash/pulp-smash-1!0.0.2.tar.gz/pulp-smash-1!0.0.2/pulp_smash/pulp3/constants.py
@@ -0,0 +1,41 @@
+# coding=utf-8
+"""Constants for Pulp 3 tests."""
+from urllib.parse import urljoin
+
+from pulp_smash.api import _P3_TASK_END_STATES as P3_TASK_END_STATES  # pylint:disable=unused-import
+
+BASE_PATH = '/pulp/api/v3/'
+
+API_DOCS_PATH = urljoin(BASE_PATH, 'docs/')
+
+ARTIFACTS_PATH = urljoin(BASE_PATH, 'artifacts/')
+
+BASE_REMOTE_PATH = urljoin(BASE_PATH, 'remotes/')
+
+BASE_PUBLISHER_PATH = urljoin(BASE_PATH, 'publishers/')
+
+CONTENT_PATH = urljoin(BASE_PATH, 'content/')
+
+DISTRIBUTION_PATH = urljoin(BASE_PATH, 'distributions/')
+
+FILE_CONTENT_PATH = urljoin(CONTENT_PATH, 'file/files/')
+
+FILE_REMOTE_PATH = urljoin(BASE_REMOTE_PATH, 'file/')
+
+FILE_PUBLISHER_PATH = urljoin(BASE_PUBLISHER_PATH, 'file/')
+
+JWT_PATH = urljoin(BASE_PATH, 'jwt/')
+
+ORPHANS_PATH = urljoin(BASE_PATH, 'orphans/')
+
+PUBLICATIONS_PATH = urljoin(BASE_PATH, 'publications/')
+
+REPO_PATH = urljoin(BASE_PATH, 'repositories/')
+
+STATUS_PATH = urljoin(BASE_PATH, 'status/')
+
+TASKS_PATH = urljoin(BASE_PATH, 'tasks/')
+
+USER_PATH = urljoin(BASE_PATH, 'users/')
+
+WORKER_PATH = urljoin(BASE_PATH, 'workers/')

71ca29aa2a28e965c29b8d9b315cc2f8d43707bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:49 +0000

    Add package aioice/0.6.4/aioice-0.6.4.tar.gz

---
+++ packages/aioice/aioice-0.6.4.tar.gz/aioice-0.6.4/tests/test_turn.py
@@ -0,0 +1,55 @@
+import asyncio
+import socket
+import unittest
+
+from aioice import turn
+
+from .turnserver import TurnServerProtocol
+from .utils import run
+
+
+class DummyClientProtocol(asyncio.DatagramProtocol):
+    received_addr = None
+    received_data = None
+
+    def connection_made(self, transport):
+        transport.sendto(b'ping', ('8.8.8.8', 53))
+
+    def datagram_received(self, data, addr):
+        self.received_data = data
+        self.received_addr = addr
+
+
+class TurnClientProtocolTest(unittest.TestCase):
+    def test_repr(self):
+        protocol = turn.TurnClientProtocol(('1.2.3.4', 1234), 'foo', 'bar', 600)
+        self.assertEqual(repr(protocol), 'turn')
+
+
+class TurnTest(unittest.TestCase):
+    def setUp(self):
+        # create turn server
+        loop = asyncio.get_event_loop()
+        transport, protocol = run(loop.create_datagram_endpoint(
+            lambda: TurnServerProtocol(realm='test', users={'foo': 'bar'}),
+            local_addr=('127.0.0.1', 0),
+            family=socket.AF_INET))
+        self.server = protocol
+        self.server_addr = transport.get_extra_info('sockname')
+
+    def tearDown(self):
+        self.server.transport.close()
+
+    def test_transport(self):
+        transport, protocol = run(turn.create_turn_endpoint(
+            DummyClientProtocol,
+            server_addr=self.server_addr,
+            username='foo',
+            password='bar',
+            lifetime=6))
+        self.assertEqual(transport.get_extra_info('peername'), None)
+        self.assertEqual(transport.get_extra_info('sockname'), ('1.2.3.4', 1234))
+        run(asyncio.sleep(10))
+        self.assertEqual(protocol.received_addr, ('8.8.8.8', 53))
+        self.assertEqual(protocol.received_data, b'pong')
+        transport.close()

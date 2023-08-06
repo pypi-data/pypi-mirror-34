c438cd07eea2cb76260e220814045309a3cacc0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:48 +0000

    Add package revfyawo.mongo/0.0.5/revfyawo.mongo-0.0.5.tar.gz

---
+++ packages/revfyawo.mongo/revfyawo.mongo-0.0.5.tar.gz/revfyawo.mongo-0.0.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='revfyawo.mongo',
+    version='0.0.5',
+    description='A mongo ODM',
+    url='https://github.com/revfyawo/revfyawo-mongo',
+    author='Lucien Haurat',
+    author_email='lucien.haurat@gmail.com',
+    packages=['revfyawo.mongo'],
+    install_requires=['pymongo~=3.7'],
+)
+

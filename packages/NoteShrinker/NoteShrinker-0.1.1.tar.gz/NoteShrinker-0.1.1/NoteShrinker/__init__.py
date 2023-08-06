ced88703a7e08e896123269d136f87dc1840cde8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:17 +0000

    Add package NoteShrinker/0.1.1/NoteShrinker-0.1.1.tar.gz

---
+++ packages/NoteShrinker/NoteShrinker-0.1.1.tar.gz/NoteShrinker-0.1.1/NoteShrinker/__init__.py
@@ -0,0 +1,8 @@
+# credits:   Matt Zucker
+# author:    Andrew Challis
+# website:   https://www.andrewchallis.co.uk
+
+from .noteshrinker import NoteShrinker
+
+# set the version number
+__version__ = "0.1.1"

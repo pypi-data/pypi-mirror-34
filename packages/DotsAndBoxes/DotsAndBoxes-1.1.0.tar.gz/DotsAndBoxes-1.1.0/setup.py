da55717cdb01b498e268639e2284fd40dd54ff85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:20 +0000

    Add package DotsAndBoxes/1.1.0/DotsAndBoxes-1.1.0.tar.gz

---
+++ packages/DotsAndBoxes/DotsAndBoxes-1.1.0.tar.gz/DotsAndBoxes-1.1.0/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='DotsAndBoxes',
+    version='1.1.0',
+    description=(
+        '点格棋对弈软件'
+    ),
+    long_description=open('README.rst', 'rb').read().decode('utf8'),
+    author='高铭',
+    author_email='gaomingshsf@hotmail.com',
+    maintainer='高铭',
+    maintainer_email='gaomingshsf@hotmail.com',
+    license='MIT License',
+    packages=find_packages(),
+    platforms=["all"],
+    url='https://github.com/Everyb0dyLies/Dots-and-Boxes',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Natural Language :: Chinese (Simplified)',
+        'Operating System :: Microsoft :: Windows :: Windows 10',
+        'Operating System :: POSIX :: Linux',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+    install_requires=[
+        'PyQt5'
+    ]
+)
+

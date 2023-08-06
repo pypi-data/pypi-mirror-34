9d9545e69dccc275fb818a8ebc6cff9a17e4cd09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:59 +0000

    Add package minervaboto/1.0.7/minervaboto-1.0.7.tar.gz

---
+++ packages/minervaboto/minervaboto-1.0.7.tar.gz/minervaboto-1.0.7/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name='minervaboto',
+      description='A renewal tool for Acervo Minerva',
+      # long_description=long_description,
+      version='1.0.7',
+      url='https://github.com/erickpires/minervaboto',
+      author='Erick Pires',
+      author_email='rckkas@gmail.com',
+      license='MIT',
+      packages=['minervaboto'],
+      install_requires=[
+          'appdirs',
+          'beautifulsoup4',
+          'configparser',
+          'requests'
+      ],
+      entry_points={
+          'console_scripts': [
+              'minervaboto=minervaboto.__main__:main'
+          ]
+      }
+)

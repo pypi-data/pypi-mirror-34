6c24005093b1b0e3d786faeb213a1da291bb7a8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:26 +0000

    Add package python-testing-advanced/0.1.1/python_testing_advanced-0.1.1.tar.gz

---
+++ packages/python-testing-advanced/python_testing_advanced-0.1.1.tar.gz/python_testing_advanced-0.1.1/python_testing_advanced/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Python Testing Advanced."""
+
+__author__ = """Julio Antúnez Tarín"""
+__email__ = 'julio.antunez.tarin@gmail.com'
+__version__ = '0.1.1'

3e947265f12b773ac837edbd90dadb3640434706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:18:32 +0000

    Add package input-form-dialog/1.0.0/input_form_dialog-1.0.0.tar.gz

---
+++ packages/input-form-dialog/input_form_dialog-1.0.0.tar.gz/input_form_dialog-1.0.0/setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    author='Serguei Kalentchouk',
+    description='InputFormDialog provides a simple multi-type input dialog for Qt5',
+    install_requires=['Qt.py'],
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    py_modules=['input_form_dialog'],
+    name='input_form_dialog',
+    url='https://github.com/serguei-k/input-form-dialog/python',
+    version='1.0.0',
+    zip_safe=False
+)

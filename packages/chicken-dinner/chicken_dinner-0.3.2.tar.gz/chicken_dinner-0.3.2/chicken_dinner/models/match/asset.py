34eb90b8caea90c7657fb04a73980f72cb2dae4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:57 +0000

    Add package chicken-dinner/0.3.2/chicken_dinner-0.3.2.tar.gz

---
+++ packages/chicken-dinner/chicken_dinner-0.3.2.tar.gz/chicken_dinner-0.3.2/chicken_dinner/models/match/asset.py
@@ -0,0 +1,38 @@
+"""Match asset model."""
+
+
+class Asset(object):
+
+
+    def __init__(self, pubg, match, data, shard=None):
+        self._pubg = pubg
+        self._shard = shard
+        #: The Match object for this asset
+        self.match = match
+        #: The data payload associated with this asset
+        self.data = data
+
+    @property
+    def shard(self):
+        """The shard for the match associated with this asset."""
+        return self._shard or self._pubg.shard
+
+    @property
+    def id(self):
+        """The asset id."""
+        return self.data["id"]
+
+    @property
+    def description(self):
+        """A description of the asset."""
+        return self.data["attributes"]["description"]
+
+    @property
+    def created_at(self):
+        """When the asset was created."""
+        return self.data["attributes"]["createdAt"]
+
+    @property
+    def url(self):
+        """The URL of the asset."""
+        return self.data["attributes"]["URL"]

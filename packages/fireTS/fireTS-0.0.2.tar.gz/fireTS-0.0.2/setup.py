8c0af0ece692c3f341c97286667230771deeed24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:02 +0000

    Add package fireTS/0.0.2/fireTS-0.0.2.tar.gz

---
+++ packages/fireTS/fireTS-0.0.2.tar.gz/fireTS-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='fireTS',
+    version='0.0.2',
+    description='A python package for multi-variate time series prediction',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/jxx123/fireTS.git',
+    author='Jinyu Xie',
+    author_email='xjygr08@gmail.com',
+    license='MIT',
+    packages=['fireTS'],
+    install_requires=[
+        'numpy',
+        'sklearn',
+    ],
+    include_package_data=True,
+    zip_safe=False)

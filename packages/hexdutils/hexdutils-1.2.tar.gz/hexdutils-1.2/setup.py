17a81cd10a67f2417278b541dbc67adc707f0256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:21 +0000

    Add package hexdutils/1.2/hexdutils-1.2.tar.gz

---
+++ packages/hexdutils/hexdutils-1.2.tar.gz/hexdutils-1.2/setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name = "hexdutils",
+    url = "https://github.com/davix3f/hexutils",
+    author = "davix3f",
+    description = "Python library to deal with hexadecimals",
+    author_email = "davide_fiorito@libero.it",
+    version = "1.2",
+    packages = find_packages(),
+    include_package_data = False
+)

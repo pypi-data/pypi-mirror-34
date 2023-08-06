64d1023aff84e49e55941a31b4fb926ccd0cd567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:07 +0000

    Add package aperture-cli/1.0.0/aperture_cli-1.0.0-py2.py3-none-any.whl

---
+++ packages/aperture-cli/aperture_cli-1.0.0-py2.py3-none-any.whl/aperture/commands/__init__.py
@@ -0,0 +1,8 @@
+"""
+The command module. Provides the list of available commands that can be
+executed in Aperture.
+
+To add a new command, add a new import statement: from .<command-name> import *
+"""
+
+from .aperture import *

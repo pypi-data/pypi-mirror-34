242537c906dfc5d7839da55f1db4ea4a9ed99f26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:18:50 +0000

    Add package wwx/0.0.5/wwx-0.0.5.tar.gz

---
+++ packages/wwx/wwx-0.0.5.tar.gz/wwx-0.0.5/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: wxnacy(wxnacy@gmail.com)
+# Description:
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'wwx',
+    version = '0.0.5',
+    keywords='wx',
+    description = 'a library for wx Developer',
+    license = 'MIT License',
+    url = 'https://github.com/wxnacy/wwx',
+    author = 'wxnacy',
+    author_email = 'wxnacy@gmail.com',
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+    install_requires = [
+        'requests>=2.19.1'
+        ],
+)
+

bd474845e48f94a5d4a69f2374a508e32eb1835e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:17 +0000

    Add package aioamqp-consumer-best/1.0.0/aioamqp-consumer-best-1.0.0.tar.gz

---
+++ packages/aioamqp-consumer-best/aioamqp-consumer-best-1.0.0.tar.gz/aioamqp-consumer-best-1.0.0/README.md
@@ -0,0 +1,3 @@
+# aioamqp-consumer-best
+
+[![Build Status](https://travis-ci.org/tkukushkin/aioamqp-consumer-best.svg?branch=master)](https://travis-ci.org/tkukushkin/aioamqp-consumer-best)
---
+++ packages/aioamqp-consumer-best/aioamqp-consumer-best-1.0.0.tar.gz/aioamqp-consumer-best-1.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='aioamqp-consumer-best',
+    version='1.0.0',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'aioamqp',
+        'dataclasses',
+    ],
+    extras_require={
+        'test': [
+            'mypy==0.600',
+            'pycodestyle',
+            'pylint',
+            'pytest',
+            'pytest-asyncio',
+            'pytest-cov',
+            'pytest-mock',
+        ],
+    },
+)

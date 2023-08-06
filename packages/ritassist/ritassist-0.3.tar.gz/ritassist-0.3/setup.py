8559ffe0b6596085e7045f88db0e2771bba7fb6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:34 +0000

    Add package ritassist/0.3/ritassist-0.3.tar.gz

---
+++ packages/ritassist/ritassist-0.3.tar.gz/ritassist-0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='ritassist',
+      version='0.3',
+      description='RitAssist API Access',
+      url='http://github.com/depl0y/ritassist-py',
+      author='Wim Haanstra',
+      author_email='wim@wim.me',
+      license='MIT',
+      packages=['ritassist'],
+      install_requires=[
+          'requests',
+      ],
+      python_requires='>=3',
+      zip_safe=False)

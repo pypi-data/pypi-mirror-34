f5441f36086f15207289526e6d5f641313238c5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:10 +0000

    Add package pywpevent/0.1.5/pywpevent-0.1.5-py3-none-any.whl

---
+++ packages/pywpevent/pywpevent-0.1.5-py3-none-any.whl/pywpevent/__init__.py
@@ -0,0 +1,1 @@
+from pywpevent.main import initialize, add_action, add_filter, do_action, apply_filter, list_event
---
+++ packages/pywpevent/pywpevent-0.1.5-py3-none-any.whl/pywpevent/main.py
@@ -0,0 +1,31 @@
+from pywpevent.events import EventCtrl
+
+
+def initialize():
+    ctrl = EventCtrl()
+    ctrl.initialize_plugin()
+
+
+def add_action(name, func, priority=0):
+    ctrl = EventCtrl()
+    ctrl.add_action(name, func, priority)
+
+
+def add_filter(name, func, priority=0):
+    ctrl = EventCtrl()
+    ctrl.add_filter(name, func, priority)
+
+
+def do_action(name, *args):
+    ctrl = EventCtrl()
+    ctrl.do_action(name, *args)
+
+
+def apply_filter(name, *args):
+    ctrl = EventCtrl()
+    return ctrl.apply_filter(name, *args)
+
+
+def list_event():
+    ctrl = EventCtrl()
+    ctrl.list_event()

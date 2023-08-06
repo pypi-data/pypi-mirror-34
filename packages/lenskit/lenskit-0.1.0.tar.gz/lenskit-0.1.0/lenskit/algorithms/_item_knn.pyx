e732ad01261d808faaf5518643cdbc6f2d22f806
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:02 +0000

    Add package lenskit/0.1.0/lenskit-0.1.0.tar.gz

---
+++ packages/lenskit/lenskit-0.1.0.tar.gz/lenskit-0.1.0/lenskit/algorithms/_item_knn.pyx
@@ -0,0 +1,254 @@
+#cython: language_level=3
+from cpython cimport array
+import array
+import pandas as pd
+import numpy as np
+cimport numpy as np
+from cython.parallel cimport parallel, prange, threadid
+from libc.stdlib cimport malloc, free, realloc, abort, calloc
+from libc.math cimport isnan
+from openmp cimport omp_get_thread_num, omp_get_num_threads
+import logging
+
+cdef _logger = logging.getLogger('_item_knn')
+
+cdef class BuildContext:
+    cdef readonly int n_users
+    cdef readonly int n_items
+    cdef readonly matrix
+    cdef readonly cscmat
+
+    cdef np.int32_t [:] uptrs
+    cdef np.int32_t [:] items
+    cdef np.float_t [:] ratings
+
+    cdef np.int32_t [:] r_iptrs
+    cdef np.int32_t [:] r_users
+
+    def __cinit__(self, matrix):
+        assert np.logical_not(np.isnan(matrix.data)).all()
+        self.n_users = matrix.shape[0]
+        self.n_items = matrix.shape[1]
+        _logger.debug('creating build context for %d users and %d items (%d ratings)',
+                      self.n_users, self.n_items, matrix.nnz)
+
+        self.matrix = matrix
+        self.cscmat = matrix.tocsc(copy=False)
+
+        self.uptrs = matrix.indptr
+        self.items = matrix.indices
+        self.ratings = matrix.data
+
+        self.r_iptrs = self.cscmat.indptr
+        self.r_users = self.cscmat.indices
+
+
+cdef struct ThreadState:
+    size_t size
+    size_t capacity
+    np.int32_t *items
+    np.int32_t *nbrs
+    np.float_t *sims
+    np.float_t *work
+
+cdef ThreadState* tr_new(size_t nitems) nogil:
+    cdef ThreadState* tr = <ThreadState*> malloc(sizeof(ThreadState))
+
+    if tr == NULL:
+        abort()
+
+    tr.size = 0
+    tr.capacity = nitems
+    tr.items = <np.int32_t*> malloc(sizeof(np.int32_t) * nitems)
+    if tr.items == NULL:
+        abort()
+    tr.nbrs = <np.int32_t*> malloc(sizeof(np.int32_t) * nitems)
+    if tr.nbrs == NULL:
+        abort()
+    tr.sims = <np.float_t*> malloc(sizeof(np.float_t) * nitems)
+    if tr.sims == NULL:
+        abort()
+    tr.work = <np.float_t*> malloc(sizeof(np.float_t) * nitems)
+    if tr.work == NULL:
+        abort()
+
+    return tr
+
+cdef void tr_free(ThreadState* self) nogil:
+    free(self.items)
+    free(self.nbrs)
+    free(self.sims)
+    free(self.work)
+    free(self)
+
+cdef void tr_ensure_capacity(ThreadState* self, size_t n) nogil:
+    cdef size_t tgt
+    if n > self.capacity:
+        tgt = self.capacity * 2
+        if n >= tgt:
+            tgt = n
+        with gil:
+            _logger.debug('thread %d: resizing storage from %d to %d',
+                          omp_get_thread_num(), self.capacity, tgt)
+        self.items = <np.int32_t*> realloc(self.items, sizeof(np.int32_t) * tgt)
+        if self.items == NULL:
+            abort()
+        self.nbrs = <np.int32_t*> realloc(self.nbrs, sizeof(np.int32_t) * tgt)
+        if self.nbrs == NULL:
+            abort()
+        self.sims = <np.float_t*> realloc(self.sims, sizeof(np.float_t) * tgt)
+        if self.sims == NULL:
+            abort()
+        self.capacity = tgt
+
+cdef void tr_add_all(ThreadState* self, int item, size_t nitems,
+                     double threshold) nogil:
+    cdef int j
+    for j in range(nitems):
+        if self.work[j] < threshold: continue
+        tr_ensure_capacity(self, self.size + 1)
+        
+        self.items[self.size] = item
+        self.nbrs[self.size] = j
+        self.sims[self.size] = self.work[j]
+
+        self.size = self.size + 1
+
+cdef void ind_upheap(int pos, int len, int* keys, double* values) nogil:
+    cdef int current, parent, kt
+    current = pos
+    parent = (current - 1) // 2
+    while current > 0 and values[keys[parent]] > values[keys[current]]:
+        # swap up
+        kt = keys[parent]
+        keys[parent] = keys[current]
+        keys[current] = kt
+        current = parent
+        parent = (current - 1) // 2
+
+cdef void ind_downheap(int pos, int len, int* keys, double* values) nogil:
+    cdef int left, right, min, kt
+    min = pos
+    left = 2*pos + 1
+    right = 2*pos + 2
+    if left < len and values[keys[left]] < values[keys[min]]:
+        min = left
+    if right < len and values[keys[right]] < values[keys[min]]:
+        min = right
+    if min != pos:
+        kt = keys[min]
+        keys[min] = keys[pos]
+        keys[pos] = kt
+        ind_downheap(min, len, keys, values)
+
+cdef void tr_add_nitems(ThreadState* self, int item, size_t nitems,
+                        double threshold, int nmax) nogil:
+    cdef int* keys
+    cdef int j, kn, ki
+    cdef np.int64_t nbr
+    keys = <int*> calloc(nmax + 1, sizeof(int))
+    
+    tr_ensure_capacity(self, self.size + nmax)
+
+    kn = 0
+    for j in range(nitems):
+        if self.work[j] < threshold: continue
+        
+        ki = kn
+        keys[ki] = j
+        ind_upheap(ki, kn, keys, self.work)
+
+        if kn < nmax:
+            kn = kn + 1
+        else:
+            # we just added the (nmax+1)th thing
+            # drop the smallest of our nmax largest
+            keys[0] = keys[kn]
+            ind_downheap(0, kn, keys, self.work)
+
+    # now that we have the heap built, let us unheap!
+    while kn > 0:
+        nbr = keys[0]
+        self.items[self.size] = item
+        self.nbrs[self.size] = nbr
+        self.sims[self.size] = self.work[nbr]
+        self.size = self.size + 1
+        keys[0] = keys[kn - 1]
+        kn = kn - 1
+        if kn > 0:
+            ind_downheap(0, kn, keys, self.work)
+
+    free(keys)
+
+
+cdef dict tr_results(ThreadState* self):
+    cdef np.npy_intp size = self.size
+    cdef np.ndarray items, nbrs, sims
+    items = np.asarray(<np.int32_t[:self.size]> self.items)
+    nbrs = np.asarray(<np.int32_t[:self.size]> self.nbrs)
+    sims = np.asarray(<np.float_t[:self.size]> self.sims)
+    # items = np.PyArray_SimpleNewFromData(1, &size, np.NPY_INT32, self.items)
+    # nbrs = np.PyArray_SimpleNewFromData(1, &size, np.NPY_INT32, self.nbrs)
+    # sims = np.PyArray_SimpleNewFromData(1, &size, np.NPY_DOUBLE, self.sims)
+    return {'item': items.copy(), 'neighbor': nbrs.copy(), 'similarity': sims.copy()}
+
+
+cpdef sim_matrix(BuildContext context, double threshold, int nnbrs):
+    cdef int i
+    cdef ThreadState* tres
+    cdef list neighborhoods = []
+
+    with nogil, parallel():
+        tres = tr_new(context.n_items)
+        with gil:
+            _logger.debug('thread %d/%d: starting with context 0x%x',
+                          omp_get_thread_num(), omp_get_num_threads(),
+                          <unsigned long> tres)
+        
+        for i in prange(context.n_items, schedule='dynamic', chunksize=10):
+            train_row(i, tres, context, threshold, nnbrs)
+        
+        with gil:
+            _logger.debug('thread %d computed %d pairs', omp_get_thread_num(), tres.size)
+            if tres.size > 0:
+                neighborhoods.append(tr_results(tres))
+            tr_free(tres)
+            _logger.debug('finished parallel item-item build')
+
+    _logger.debug('stacking %d neighborhood frames', len(neighborhoods))
+    return pd.concat([pd.DataFrame(d) for d in neighborhoods],
+                     ignore_index=True)
+
+cdef void train_row(int item, ThreadState* tres, BuildContext context,
+                    double threshold, int nnbrs) nogil:
+    cdef int j, u, uidx, iidx, nbr, urp
+    cdef double ur = 0
+
+    for j in range(context.n_items):
+        tres.work[j] = 0
+
+    for uidx in range(context.r_iptrs[item], context.r_iptrs[item+1]):
+        u = context.r_users[uidx]
+        # find user's rating for this item
+        urp = -1
+        for iidx in range(context.uptrs[u], context.uptrs[u+1]):
+            if context.items[iidx] == item:
+                urp = iidx
+                ur = context.ratings[urp]
+                break
+        if urp < 0:
+            # should never ever get here
+            with gil:
+                raise AssertionError('failed to find rating (%d,%d)', uidx, item)
+
+        # accumulate pieces of dot products
+        for iidx in range(context.uptrs[u], context.uptrs[u+1]):
+            nbr = context.items[iidx]
+            if nbr != item:
+                tres.work[nbr] = tres.work[nbr] + ur * context.ratings[iidx]
+
+    # now copy the accepted values into the results
+    if nnbrs > 0:
+        tr_add_nitems(tres, item, context.n_items, threshold, nnbrs)
+    else:
+        tr_add_all(tres, item, context.n_items, threshold)
---
+++ packages/lenskit/lenskit-0.1.0.tar.gz/lenskit-0.1.0/setup.py
@@ -0,0 +1,114 @@
+import sys
+import os
+from setuptools import setup, find_packages
+from distutils.extension import Extension
+
+
+# Set up to lazily resolve the NumPy include path
+# On supported systems (python >= 3.6), this will enable setup_requires to work well
+if hasattr(os, 'PathLike'):
+    class LazyNPPath(os.PathLike):
+        "NumPy include path, but evaluated lazily"
+        def __fspath__(self):
+            import numpy
+            return numpy.get_include()
+
+        def __str__(self):
+            return self.__fspath__()
+else:
+    def LazyNPPath():
+        import numpy
+        return numpy.get_include()
+
+
+if sys.platform == 'win32':
+    # MSVC - we do not yet support mingw or clang
+    openmp_cflags = ['/openmp']
+    openmp_lflags = []
+else:
+    # assume we are using GCC or compatible
+    openmp_cflags = ['-fopenmp']
+    openmp_lflags = ['-fopenmp']
+
+debug_cflags = []
+cython_opts = {}
+if 'COVERAGE' in os.environ:
+    debug_cflags += ['-DCYTHON_TRACE=1', '-DCYTHON_TRACE_NOGIL=1']
+    cython_opts['linetrace'] = True
+
+
+def extmod(name, openmp=False, cflags=[], ldflags=[]):
+    "Create an extension object for one of our extension modules."
+    comp_args = cflags + debug_cflags
+    link_args = list(ldflags)
+    if openmp:
+        comp_args += openmp_cflags
+        link_args += openmp_lflags
+    parts = name.split('.')
+    parts[-1] += '.pyx'
+    path = os.path.join(*parts)
+    return Extension(name, [path], include_dirs=[LazyNPPath()],
+                     extra_compile_args=comp_args,
+                     extra_link_args=link_args)
+
+
+def maybe_cythonize(mods):
+    "Pre-Cythonize modules if feasible"
+    try:
+        from Cython.Build import cythonize
+    except ImportError:
+        # use distutils' built-in Cythonization
+        return mods
+
+    # we have Cython! pre-cythonize so we can set options
+    return cythonize(mods, compiler_directives={
+        'warn.undeclared': True,
+        'warn.unused': True,
+        'warn.maybe_uninitialized': True,
+        **cython_opts
+    })
+
+
+with open('README.md', 'r') as fh:
+    readme = fh.read()
+
+setup(
+    name="lenskit",
+    version="0.1.0",
+    author="Michael Ekstrand",
+    author_email="michaelekstrand@boisestate.edu",
+    description="Run recommender algorithms and experiments",
+    long_description=readme,
+    url="https://lkpy.lenskit.org",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence"
+    ],
+
+    setup_requires=[
+        'pytest-runner',
+        'cython',
+        'numpy',
+        'scipy'
+    ],
+    install_requires=[
+        'pandas',
+        'numpy',
+        'scipy',
+        'tables >= 3.2.0'
+    ],
+    tests_require=[
+        'pytest >= 3.5.1',
+        'pytest-arraydiff'
+    ],
+
+    packages=find_packages(),
+    ext_modules=[
+        extmod('lenskit.algorithms._item_knn', openmp=True),
+        extmod('lenskit.algorithms._funksvd')
+    ]
+)

bfc635a54437963bc0661a2643b1ae8e07801dec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:34 +0000

    Add package django-zipfile/0.4.0/django-zipfile-0.4.0.tar.gz

---
+++ packages/django-zipfile/django-zipfile-0.4.0.tar.gz/django-zipfile-0.4.0/setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup, find_packages
+
+VERSION = "0.4.0"
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="django-zipfile",
+    version=VERSION,
+    description="A ZipFile subclass that accepts Django Templates",
+    long_description=read('README.rst'),
+    url='https://github.com/fcurella/django-zipfile',
+    license='MIT',
+    author='Flavio Curella',
+    author_email='flavio.curella@gmail.com',
+    packages=find_packages(exclude=['tests']),
+    test_suite='tests.runtests.runtests',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    install_requires=['six'],
+)

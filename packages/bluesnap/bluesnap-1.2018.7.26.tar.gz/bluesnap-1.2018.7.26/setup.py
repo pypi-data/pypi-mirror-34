f45c8e3102025a627d78aa087aaa321d99245e0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:04 +0000

    Add package bluesnap/1.2018.7.26/bluesnap-1.2018.7.26.tar.gz

---
+++ packages/bluesnap/bluesnap-1.2018.7.26.tar.gz/bluesnap-1.2018.7.26/setup.py
@@ -0,0 +1,46 @@
+# Always prefer setuptools over distutils
+# To use a consistent encoding
+from setuptools import setup, find_packages
+from os import path
+
+from codecs import open
+
+here = path.abspath(path.dirname(__file__))
+__version__ = None
+with open('bluesnap/version.py') as f:
+    exec(f.read())
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    requires = f.readlines()
+
+setup(
+    name='bluesnap',
+    version=__version__,
+    author='Alon Diamant',
+    author_email='alon@selectom.com',
+    url='https://github.com/selectom/bluesnap',
+    license='LICENSE',
+    description='Python module to interact with Bluesnap API. '
+                'Based on bluesnap (https://github.com/justyoyo/bluesnap) '
+                'by Jian Yuan Lee (jian@justyoyo.com)',
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=requires,
+    setup_requires=requires,
+    test_suite='nose.collector',
+    # For a list of valid classifiers, see https://pypi.org/classifiers/
+    classifiers = [  # Optional
+                  'Development Status :: 3 - Alpha',
+                  'Intended Audience :: Developers',
+                  'License :: OSI Approved :: MIT License',
+                  'Programming Language :: Python :: 3',
+                  'Programming Language :: Python :: 3.4',
+                  'Programming Language :: Python :: 3.5',
+                  'Programming Language :: Python :: 3.6',
+                  'Programming Language :: Python :: 3.7',
+              ],
+
+)

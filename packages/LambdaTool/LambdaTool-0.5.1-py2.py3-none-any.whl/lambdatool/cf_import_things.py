883e28234f1591a96500d7f78dbaeab41d88881f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:28 +0000

    Add package LambdaTool/0.5.1/LambdaTool-0.5.1-py2.py3-none-any.whl

---
+++ packages/LambdaTool/LambdaTool-0.5.1-py2.py3-none-any.whl/lambdatool/cf_import_things.py
@@ -0,0 +1,42 @@
+'''
+Stuff for the lambda role
+'''
+role_parameter_section = """  role:
+    Description: name of the role
+    Type: String"""
+parameter_role_spec = 'Ref: role'
+imported_role_spec = 'Fn::ImportValue: {}'
+
+'''
+Stuff for the subnet(s)
+'''
+subnets_parameter_section = """  subnetIds:
+    Description: list of subnets
+    Type: CommaDelimitedList"""
+subnets_parameter_spec = 'Ref: subnetIds'
+imported_subnets_spec = '- Fn::ImportValue: {}'
+
+'''
+Stuff for the security group(s)
+'''
+sg_parameter_section = """  securityGroupIds:
+    Description: list of security groups
+    Type: CommaDelimitedList"""
+sg_parameter_spec = 'Ref: securityGroupIds'
+imported_sg_spec = '- Fn::ImportValue: {}'
+
+output_section = '''Outputs:
+  LambdaFunction:
+    Description: The name of a LambdaTool function
+    Value:
+      Ref: LambdaFunction
+    Export:
+      Name: {}-Name
+  LambdaFunctionARN:
+    Description: The ARN of a LambdaTool function
+    Value:
+      Fn::GetAtt:
+        - LambdaFunction
+        - Arn
+    Export:
+      Name: {}-Arn'''

803745dd1f600f96e7fc5fc5d5fce1969fff2ca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:36 +0000

    Add package test111-module/1.8/test111-module-1.8.tar.gz

---
+++ packages/test111-module/test111-module-1.8.tar.gz/test111-module-1.8/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(name='test111-module',
+	version='1.8',
+	py_modules=['soap'], 
+	#ext_packages=['xmltodict'],
+	#url = "deneme.com",
+	author = "ahmetatakan",
+	maintainer = "ahmetatakan",
+	author_email = "ahmetatakan79@gmail.com",
+	maintainer_email = "ahmetatakan79@gmail.comm",
+)

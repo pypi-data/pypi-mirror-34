d0ae9f0178a82f5820a1fb6256216699173e10af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:23 +0000

    Add package parsita/1.3.2/parsita-1.3.2.tar.gz

---
+++ packages/parsita/parsita-1.3.2.tar.gz/parsita-1.3.2/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+
+setuptools.setup(
+    name='parsita',
+    version='1.3.2',
+
+    description='Parser combinator library for Python.',
+    long_description=open('README.rst').read(),
+    keywords='parser combinator',
+
+    author='David Hagen',
+    author_email='david@drhagen.com',
+    url='https://github.com/drhagen/parsita',
+    license='MIT',
+
+    extras_require={
+        ':python_version<"3.5"': ['typing'],
+    },
+    packages=setuptools.find_packages(),
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)

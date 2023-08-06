7979215f9a41a13e34fe4f028e6cb8ea07d9b718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:00 +0000

    Add package ansit/0.0.1/ansit-0.0.1.tar.gz

---
+++ packages/ansit/ansit-0.0.1.tar.gz/ansit-0.0.1/ansit/util.py
@@ -0,0 +1,35 @@
+import logging
+import sys
+import traceback
+import inspect
+import collections
+import json
+
+import yaml
+
+
+def read_json_file(path):
+    with open(path, 'r', encoding='utf-8') as src:
+        return json.load(src)
+
+
+def read_yaml_file(path):
+    with open(path, 'r', encoding='utf-8') as src:
+        return yaml.load(src)
+
+
+def update(d, u):
+    '''Recursive dictionary update.'''
+    for k, v in u.items():
+        if isinstance(v, collections.Mapping):
+            d[k] = update(d.get(k, {}), v)
+        else:
+            d[k] = v
+    return d
+
+
+def get_element_by_path(structure, path):
+    '''Retrive element from structure with index keys path.'''
+    for i in path:
+        structure = structure[i]
+    return structure

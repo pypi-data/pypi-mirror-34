4e2d0248be5736cda03d3b3ebe5567479f7ac699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:47 +0000

    Add package rawsteel-music-player/1.0.3/rawsteel-music-player-1.0.3.tar.gz

---
+++ packages/rawsteel-music-player/rawsteel-music-player-1.0.3.tar.gz/rawsteel-music-player-1.0.3/setup.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='rawsteel-music-player',
+    url="https://github.com/baijifeilong/rawsteelp",
+    license='GPL3',
+    author='BaiJiFeiLong',
+    author_email='baijifeilong@gmail.com',
+    version='1.0.3',
+    description='A minimal music player with lyric show',
+    packages=find_packages(),
+    long_description=open('README.md').read(),
+    zip_safe=False,
+    setup_requires=['chardet', 'pytaglib'],
+    entry_points = {
+        'console_scripts': [
+            'rawsteel-music-player = rawsteelp.rawsteelp:main'
+        ]
+    }
+)

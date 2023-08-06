3319d34bc9037549682532da8f8b78382523ad0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:56 +0000

    Add package z123155/1.3.0/z123155-1.3.0.tar.gz

---
+++ packages/z123155/z123155-1.3.0.tar.gz/z123155-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+
+setup(
+        name        = 'z123155',
+        version     = '1.3.0',
+        py_modules  = ['nester'],
+        author      = 'afpython',
+        description = 'A simple printer of nested lists',
+      )

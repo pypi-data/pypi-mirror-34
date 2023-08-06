7d6db9ba5b5bada6eed1d34e16c6c8c1b51d4250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:07 +0000

    Add package tdda/1.0.10/tdda-1.0.10-py3-none-any.whl

---
+++ packages/tdda/tdda-1.0.10-py3-none-any.whl/tdda/CHANGES.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+"""1.0.02
+Improved documentation to use shorter import forms.
+
+Also bumped version number.
+
+Also planning to tag this with a version to see whether that encourages
+readthedocs to add the version properly.
+
+29.05.2018 1.0.03
+Add pushv (for maintainer use only) and this CHANGES.py file.
+
+29.05.2018 1.0.04
+Correctly cast strings to native strings in check_shell_output.
+
+29.05.2018 1.0.05
+Tests for feather are now ignored if pmmif/feather are not installed.
+
+29.05.2018 1.0.06
+Windows fixes.
+
+31.05.2018 1.0.07
+Fix for UTF-8 encoding in subprocesses on Windows.
+
+Also added documentation on setting up fonts on Windows to display
+ticks and crosses correctly.
+
+6.06.2018 1.0.08
+Improved metadata in TDDA files.
+
+11.06.2018 1.0.09
+Add support for postloadhook and postdicthook.
+
+25.07.2018 1.0.10
+Minor bug fixes.
+
+ - Fuzzy verification of 'min' constraint was printing out the type of the col.
+ - Typo in documentation of the properties available in a Verification result.
+ - Verification of 'rex' constraint was not checking that the field exists.
+"""

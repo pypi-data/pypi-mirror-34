0ac1bbcb879d56f3ce2ea90df4251b47cca1237d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:29 +0000

    Add package pyzure/0.0.5/pyzure-0.0.5-py3-none-any.whl

---
+++ packages/pyzure/pyzure-0.0.5-py3-none-any.whl/pyzure/execute.py
@@ -0,0 +1,27 @@
+import pyodbc
+
+from pyzure.connection import connect
+
+
+def execute_query(instance, query, data=None):
+    cnxn = connect(instance)
+    cursor = cnxn.cursor()
+    print(query)
+    if data:
+        cursor.execute(query, data)
+        cnxn.commit()
+        result = None
+    else:
+        cursor.execute(query)
+        result = []
+        try:
+            for row in cursor.fetchall():
+                result.append(row)
+        except pyodbc.ProgrammingError:
+            pass
+        cnxn.commit()
+
+    cursor.close()
+    cnxn.close()
+
+    return result

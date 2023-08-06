d5bd09220dea9b3ff138b8de881230001c7acc81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:28 +0000

    Add package mattermostdriver/5.0.1/mattermostdriver-5.0.1.tar.gz

---
+++ packages/mattermostdriver/mattermostdriver-5.0.1.tar.gz/mattermostdriver-5.0.1/src/mattermostdriver/endpoints/emoji.py
@@ -0,0 +1,50 @@
+from .base import Base
+
+
+class Emoji(Base):
+	endpoint = '/emoji'
+
+	def create_custom_emoji(self, emoji_name, files):
+		return self.client.post(
+			self.endpoint,
+			data={'emoji': {'name': emoji_name}},
+			files=files
+		)
+
+	def get_emoji_list(self, params=None):
+		return self.client.get(
+			self.endpoint,
+			params=params
+		)
+
+	def get_custom_emoji(self, emoji_id):
+		return self.client.get(
+			self.endpoint + '/' + emoji_id
+		)
+
+	def delete_custom_emoji(self, emoji_id):
+		return self.client.delete(
+			self.endpoint + '/' + emoji_id
+		)
+
+	def get_custom_emoji_by_name(self, name):
+		return self.client.get(
+			self.endpoint + '/name/' + name
+		)
+
+	def get_custom_emoji_image(self, emoji_id):
+		return self.client.get(
+			self.endpoint + '/' + emoji_id + '/image'
+		)
+
+	def search_custom_emoji(self, options=None):
+		return self.client.post(
+			self.endpoint + '/search',
+			options=options
+		)
+
+	def autocomplete_custom_emoji(self, params=None):
+		return self.client.get(
+			self.endpoint + '/autocomplete',
+			params=params
+		)

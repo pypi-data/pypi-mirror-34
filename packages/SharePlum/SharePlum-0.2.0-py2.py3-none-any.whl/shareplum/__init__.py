a8166f64fc0e9bea47df0eaf78379a74fd303d47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:26 +0000

    Add package SharePlum/0.2.0/SharePlum-0.2.0-py2.py3-none-any.whl

---
+++ packages/SharePlum/SharePlum-0.2.0-py2.py3-none-any.whl/shareplum/__init__.py
@@ -0,0 +1,11 @@
+# SharePlum
+# This library simplfies the code necessary
+# to automate interactions with a SharePoint
+# server using python
+
+__title__ = 'SharePlum SharePoint Library'
+__author__ = 'Jason Rollins'
+
+from .shareplum import *
+from .ListDict import *
+from .version import __version__

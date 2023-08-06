9eeed01d6e45bd9ee30492c41ca8b2f9889d0de6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:03 +0000

    Add package showit-again/1.1.4/showit-again-1.1.4.tar.gz

---
+++ packages/showit-again/showit-again-1.1.4.tar.gz/showit-again-1.1.4/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import json
+from setuptools import setup
+
+with open('showit/VERSION', 'r') as fh:
+    version = json.loads(fh.read())
+
+setup(
+    name='showit-again',
+    version=version['string'],
+    description='simple and sensible display of images',
+    author='freeman-lab',
+    author_email='the.freeman.lab@gmail.com',
+    packages=['showit'],
+    url='https://github.com/freeman-lab/showit',
+    install_requires=open('requirements.txt').read().split(),
+    long_description='See https://github.com/freeman-lab/showit',
+    include_package_data=True,
+)

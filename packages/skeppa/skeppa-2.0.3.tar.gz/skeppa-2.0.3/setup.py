c7da90a3b7369876a9f115174e9d177759c615fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:11 +0000

    Add package skeppa/2.0.3/skeppa-2.0.3.tar.gz

---
+++ packages/skeppa/skeppa-2.0.3.tar.gz/skeppa-2.0.3/setup.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+import skeppa
+
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")
+    sys.exit()
+
+package_exclude = ("tests*", "examples*")
+packages = find_packages(exclude=package_exclude)
+
+# Handle requirements
+install_requires = [
+    "Fabric==1.10.2",
+    "PyCrypto==2.6.1",
+    "Jinja2==2.8",
+    "PyYAML==3.11",
+    "six==1.10.0",
+]
+
+# Convert markdown to rst
+try:
+    from pypandoc import convert
+    long_description = convert("README.md", "rst")
+except:
+    long_description = ""
+
+
+setup(
+    name="skeppa",
+    version=skeppa.__version__,
+    description=("A docker deployment tool based on fabric and "
+                 "docker-compose"),
+    long_description=long_description,
+    author="Marteinn",
+    author_email="martin@marteinn.se",
+    url="https://github.com/marteinn/skeppa",
+    packages=packages,
+    include_package_data=True,
+    install_requires=install_requires,
+    entry_points={
+        "console_scripts": [
+            "skeppa = skeppa.scripts.skeppa:main",
+        ]
+    },
+    license="MIT",
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: System :: Software Distribution",
+        "Topic :: System :: Systems Administration",
+    ],
+)
---
+++ packages/skeppa/skeppa-2.0.3.tar.gz/skeppa-2.0.3/skeppa/ext/ecr.py
@@ -0,0 +1,95 @@
+from __future__ import absolute_import
+
+import imp
+
+from fabric.api import local, run
+from fabric.utils import abort
+
+from . import Extension
+
+
+class Ecr(Extension):
+    def register(self):
+        try:
+            imp.find_module('awscli')
+        except ImportError:
+            abort('The ecr extension requires the awscli package '
+                  '(hint: pip install awscli)')
+
+    def _verify_image(self, image):
+        repository = image.get('repository')
+        repro_type = repository.get('type', None)
+        return repro_type == 'ecr'
+
+    @staticmethod
+    def _build_cli_args(values):
+        command = ""
+        for key in values:
+            if not values[key]:
+                continue
+            command += " --%s=%s" % (key, values[key])
+
+        return command
+
+    def before_push(self, image):
+        if not self._verify_image(image):
+            return
+
+        repository = image.get('repository')
+        release_tag = image.get('tag', 'latest')
+
+        repository_url = repository.get('url')
+        region = repository.get('aws_region', 'us-east-1')
+        profile = repository.get('aws_local_profile', '')
+
+        self._login(local, image, region=region, profile=profile)
+
+        # Try to delete previous release_tag from ECR
+        try:
+            registry_id = repository_url.split(".")[0]
+            repository_name = repository_url.split("/")[-1]
+
+            delete_args = {
+                "region": region,
+                "profile": profile
+            }
+
+            delete_command = "aws ecr batch-delete-image --registry-id {0} \
+                --repository-name {1} \
+                --image-ids imageTag={2}".format(registry_id, repository_name,
+                                                 release_tag)
+
+            delete_command += self._build_cli_args(delete_args)
+            local(delete_command)
+        except:
+            pass
+
+    def before_deploy(self, image):
+        if not self._verify_image(image):
+            return
+
+        repository = image.get('repository')
+        region = repository.get('aws_region', 'us-east-1')
+        profile = repository.get('aws_profile', '')
+
+        self._login(run, image, region=region, profile=profile)
+
+    def _login(self, method, image, region, profile=""):
+        # Authenticate with ecr
+        auth_args = {
+            "region": region,
+            "profile": profile
+        }
+
+        auth_command = "aws ecr get-login"
+
+        for arg in auth_args:
+            if not auth_args[arg]:
+                continue
+
+            auth_command += " --%s=%s" % (arg, auth_args[arg])
+
+        method("$(%s --no-include-email)" % auth_command)
+
+
+extension = Ecr

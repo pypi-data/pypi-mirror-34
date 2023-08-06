25ee254794eac52d96a91850bd5ba6a2216a999d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:39 +0000

    Add package class-only-design/0.1.4/class_only_design-0.1.4.tar.gz

---
+++ packages/class-only-design/class_only_design-0.1.4.tar.gz/class_only_design-0.1.4/class_only_design/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Class Only."""
+
+__author__ = """Tom Rutherford"""
+__email__ = "foreverwintr@gmail.com"
+__version__ = "0.1.4"
+
+from class_only_design.core import class_only
+from class_only_design.core import constant

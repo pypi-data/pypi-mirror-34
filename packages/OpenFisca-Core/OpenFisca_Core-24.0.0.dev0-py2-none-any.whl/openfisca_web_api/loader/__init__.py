66f1514743f341b6d06e96c9b7ace7621fcc8c44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:24 +0000

    Add package OpenFisca-Core/24.0.0.dev0/OpenFisca_Core-24.0.0.dev0-py2-none-any.whl

---
+++ packages/OpenFisca-Core/OpenFisca_Core-24.0.0.dev0-py2-none-any.whl/openfisca_web_api/loader/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+from openfisca_web_api.loader.parameters import build_parameters
+from openfisca_web_api.loader.variables import build_variables
+from openfisca_web_api.loader.spec import build_openAPI_specification
+
+
+def extract_description(items):
+    return {
+        name: {'description': item['description']}
+        for name, item in items.items()
+        }
+
+
+def build_data(tax_benefit_system):
+    country_package_metadata = tax_benefit_system.get_package_metadata()
+    parameters = build_parameters(tax_benefit_system)
+    variables = build_variables(tax_benefit_system, country_package_metadata)
+    openAPI_spec = build_openAPI_specification(tax_benefit_system, country_package_metadata)
+    return {
+        'tax_benefit_system': tax_benefit_system,
+        'country_package_metadata': tax_benefit_system.get_package_metadata(),
+        'openAPI_spec': openAPI_spec,
+        'parameters': parameters,
+        'parameters_description': extract_description(parameters),
+        'variables': variables,
+        'variables_description': extract_description(variables),
+        }

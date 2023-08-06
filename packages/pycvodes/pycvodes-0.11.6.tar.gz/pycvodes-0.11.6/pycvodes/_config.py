d0db45362421934dafacf5bb71de14f79a009d93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:30 +0000

    Add package pycvodes/0.11.6/pycvodes-0.11.6.tar.gz

---
+++ packages/pycvodes/pycvodes-0.11.6.tar.gz/pycvodes-0.11.6/pycvodes/_config.py
@@ -0,0 +1,7 @@
+env = {'INDEX_TYPE': 'int32_t',
+ 'LAPACK': 'blas,lapack',
+ 'NO_KLU': '1',
+ 'NO_LAPACK': '0',
+ 'REAL_TYPE': 'double',
+ 'SUNDIALS_LIBS': 'sundials_nvecserial,sundials_cvodes,sundials_sunlinsollapackdense,sundials_sunlinsollapackband',
+ 'SUNDIALS_PRECISION': 'double'}

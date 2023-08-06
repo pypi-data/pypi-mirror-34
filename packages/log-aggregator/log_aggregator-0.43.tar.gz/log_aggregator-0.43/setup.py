0bb122280d0a378dd1e365040b1a366340d08236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:19 +0000

    Add package log-aggregator/0.43/log_aggregator-0.43.tar.gz

---
+++ packages/log-aggregator/log_aggregator-0.43.tar.gz/log_aggregator-0.43/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(name='log_aggregator',
+      version='0.43',
+      description='Create a configurable Tarball of various files in the file system',
+      author='Thomas Kostas',
+      url='https://github.com/yotabits/log_aggregator',
+      author_email='tkostas75@gmail.com',
+      license='MIT',
+      packages=['log_aggregator'],
+      scripts=['bin/log_aggregator'],
+      install_requires=['pycurl',
+                        'appjar',
+                        'gopher_robot_version',
+                        'pillow',
+                        'tqdm',
+                        'psutil'
+      ],
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        ],
+      zip_safe=False)

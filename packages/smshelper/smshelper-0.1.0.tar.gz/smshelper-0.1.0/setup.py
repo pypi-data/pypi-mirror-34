c6ecd863a61a71c3fd3af7ae706f340731da71fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:37:03 +0000

    Add package smshelper/0.1.0/smshelper-0.1.0.tar.gz

---
+++ packages/smshelper/smshelper-0.1.0.tar.gz/smshelper-0.1.0/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="smshelper",
+    version="0.1.0",
+    author="Elpedio Adoptante Jr",
+    author_email="adoptante.elpedio@gmail.com",
+    description="SMS helper and tool",
+    long_description=long_description,
+    url="https://github.com/jaygel179/smshelper",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)

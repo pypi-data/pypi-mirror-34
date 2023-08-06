13156ba89dbb8aad1ae4b39277d45969a5219556
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:58 +0000

    Add package nc-dnsapi/0.1.2/nc_dnsapi-0.1.2.tar.gz

---
+++ packages/nc-dnsapi/nc_dnsapi-0.1.2.tar.gz/nc_dnsapi-0.1.2/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='nc_dnsapi',
+    version='0.1.2',
+    description='API wrapper for the netcup DNS api',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Nicolai Buchwitz',
+    author_email='nb@tipi-net.de',
+    url='https://github.com/nbuchwitz/nc_dnsapi',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'requests',
+    ],
+    packages=setuptools.find_packages(),
+)

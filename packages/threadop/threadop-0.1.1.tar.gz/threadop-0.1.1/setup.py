2066ff18b9ab210b2153757905262c3783b17781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:03 +0000

    Add package threadop/0.1.1/threadop-0.1.1.tar.gz

---
+++ packages/threadop/threadop-0.1.1.tar.gz/threadop-0.1.1/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+modules = \
+['threadop']
+setup_kwargs = {
+    'name': 'threadop',
+    'version': '0.1.1',
+    'description': 'Adds a threading operator to Python.',
+    'long_description': None,
+    'author': 'Bogdan Popa',
+    'author_email': 'popa.bogdanp@gmail.com',
+    'url': None,
+    'py_modules': modules,
+}
+
+
+setup(**setup_kwargs)

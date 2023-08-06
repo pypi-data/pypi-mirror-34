9d5dd78dc60bab518dba5eaedbc34441fda592de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:21 +0000

    Add package mcpartools/0.5.2/mcpartools-0.5.2-py2.py3-none-any.whl

---
+++ packages/mcpartools/mcpartools-0.5.2-py2.py3-none-any.whl/mcpartools/mcengine/data/collect.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a simple command exits with a non-zero status.
+set -e
+
+INPUT_WILDCARD={output_dir:s}/workspace/job_*/{wildcard:s}
+OUTPUT_DIRECTORY={output_dir:s}/output
+
+# make output folder
+mkdir -p $OUTPUT_DIRECTORY
+
+{collect_action:s}

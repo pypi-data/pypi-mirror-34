73613d56e732422430ad74a7c1c71ae68f10084b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:45 +0000

    Add package PyJSONViewer/0.0.1/PyJSONViewer-0.0.1.tar.gz

---
+++ packages/PyJSONViewer/PyJSONViewer-0.0.1.tar.gz/PyJSONViewer-0.0.1/README.md
@@ -0,0 +1,79 @@
+# PyJSONViewer
+A JSON file data viewer using pure python
+
+- Mac OS
+
+![img1](https://github.com/AtsushiSakai/PyJSONViewer/raw/master/img/mac.png)
+
+- Windows10
+
+![img1](https://github.com/AtsushiSakai/PyJSONViewer/raw/master/img/windows.png)
+
+# What is this?
+
+This is a JSON file data viewer based on python.
+
+It only uses build-in libraries of python (tkinter, json).
+
+Features:
+
+1. Minimum dependency. 
+
+2. Multi-platform (Mac, Windows, Linux).
+
+3. Easy to use.
+
+
+# Requirements
+
+- Python 3.6.x
+
+# Download
+
+1. Install python 3.6.x.
+
+2. Clone or download as zip this repository. 
+
+# How to use
+
+## Selct JSON file with CUI.
+
+1. Run pyjsonviewer.py with -f option and the path to a JSON file:
+
+> $ python pyjsonviewer.py -f path\_to\_json\_file/sample.json
+
+2. JSON data tree will be shown.
+
+## Select JSON file with GUI.
+
+1. Run pyjsonviewer.py
+
+2. File-\>Open and then select json file.
+
+3. JSON data tree will be shown.
+
+You can set initial directory with CUI:
+
+> $ python pyjsonviewer.py -d path\_to\_json\_file\_dir
+
+## Select JSON file with drag and drop.
+
+1. Run pyjsonviewer.py with the option -o
+
+> $ python pyjsonviewer.py -o path\_to\_json\_file\_dir
+
+2. File browser is shown.
+
+3. You can drag and drop a JSON file to the file browser.
+
+# License 
+
+MIT
+
+# Author
+
+- [Atsushi Sakai](https://github.com/AtsushiSakai/) ([@Atsushi_twi](https://twitter.com/Atsushi_twi))
+
+
+
+
---
+++ packages/PyJSONViewer/PyJSONViewer-0.0.1.tar.gz/PyJSONViewer-0.0.1/pyjsonviewer/pyjsonviewer.py
@@ -0,0 +1,125 @@
+"""
+
+A JSON viewer using pure python
+
+author: Atsushi Sakai (@Atsushi_twi)
+
+"""
+
+import tkinter as tk
+import tkinter.ttk as ttk
+from tkinter import filedialog
+import json
+import argparse
+from tkinter import messagebox
+
+MAX_N_ITEM = 20
+
+
+class JSONTreeFrame(ttk.Frame):
+
+    def __init__(self, master, jsonpath=None, initialdir="~/"):
+        super().__init__(master)
+        self.create_widgets()
+        self.initialdir = initialdir
+
+        if jsonpath:
+            self.importjson(jsonpath)
+
+    def create_widgets(self):
+        self.tree = ttk.Treeview(self)
+
+        ysb = ttk.Scrollbar(
+            self, orient=tk.VERTICAL, command=self.tree.yview)
+        self.tree.configure(yscroll=ysb.set)
+
+        self.tree.grid(row=0, column=0, sticky=(tk.N, tk.S, tk.E, tk.W))
+        ysb.grid(row=0, column=1, sticky=(tk.N, tk.S))
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+
+    def insert_node(self, parent, key, value):
+        node = self.tree.insert(parent, 'end', text=key, open=False)
+
+        if value is None:
+            return
+
+        if type(value) is not dict:
+            if type(value) is list:
+                value = value[0:MAX_N_ITEM]
+            node = self.tree.insert(node, 'end', text=value, open=False)
+        else:
+            for (key, value) in value.items():
+                self.insert_node(node, key, value)
+
+    def select_json_file(self):
+        file_path = filedialog.askopenfilename(
+            initialdir=self.initialdir, filetypes=[("JSON files", "*.json")])
+        self.importjson(file_path)
+
+    def importjson(self, file_path):
+        f = open(file_path)
+        data = json.load(f)
+        f.close()
+
+        self.delte_all_nodes()
+        self.insert_nodes(data)
+
+    def delte_all_nodes(self):
+        for i in self.tree.get_children():
+            self.tree.delete(i)
+
+    def insert_nodes(self, data):
+        parent = ""
+
+        for (key, value) in data.items():
+            self.insert_node(parent, key, value)
+
+    def showinfo(self):
+        msg = """
+        PyJSONViewer
+        by Atsushi Sakai(@Atsushi_twi)
+        Ver.1.0
+        GitHub:https://github.com/AtsushiSakai/PyJSONViewer
+        """
+        messagebox.showinfo("About", msg)
+
+
+def main():
+    print(__file__ + " start!!")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file', type=str, help='JSON file path')
+    parser.add_argument('-d', '--dir', type=str, help='JSON file directory')
+    parser.add_argument('-o', '--open', action='store_true',
+                        default=False, help='Open with finder')
+    args = parser.parse_args()
+
+    root = tk.Tk()
+    root.title('PyJSONViewer')
+    root.geometry("500x500")
+    menubar = tk.Menu(root)
+
+    if args.open:
+        args.file = filedialog.askopenfilename(
+            initialdir=args.dir,
+            filetypes=[("JSON files", "*.json")])
+
+    app = JSONTreeFrame(root, jsonpath=args.file, initialdir=args.dir)
+
+    filemenu = tk.Menu(menubar, tearoff=0)
+    filemenu.add_command(label="Open", command=app.select_json_file)
+    menubar.add_cascade(label="File", menu=filemenu)
+    helpmenu = tk.Menu(menubar, tearoff=0)
+    helpmenu.add_command(label="About", command=app.showinfo)
+    menubar.add_cascade(label="Help", menu=helpmenu)
+    app.grid(column=0, row=0, sticky=(tk.N, tk.S, tk.E, tk.W))
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=1)
+
+    root.config(menu=menubar)
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()

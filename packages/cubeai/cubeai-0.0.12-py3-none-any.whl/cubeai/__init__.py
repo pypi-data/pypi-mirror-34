472ed5ee9e3912d9c48f713bf75f797d33c947dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:07 +0000

    Add package cubeai/0.0.12/cubeai-0.0.12-py3-none-any.whl

---
+++ packages/cubeai/cubeai-0.0.12-py3-none-any.whl/cubeai/__init__.py
@@ -0,0 +1,5 @@
+from cubeai.solver.search import dfs, bfs, ucs, a_star, null_heuristic
+from cubeai.cube.cube import Cube, Face, Action, NUM_FACES
+from cubeai.solver.solver import solve, random_actions
+
+name = "cubeai"

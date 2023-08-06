555c9ce2a3e07443e41e9b87a2c8621a33410719
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:17 +0000

    Add package pqi/2.0.6/pqi-2.0.6.tar.gz

---
+++ packages/pqi/pqi-2.0.6.tar.gz/pqi-2.0.6/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+VERSION = '2.0.6'
+
+with open("README.md", "r", encoding="utf-8") as fp:
+    long_description = fp.read()
+
+setup(name='pqi',
+      version=VERSION,
+      description="Fast switching PyPi mirror image source",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='python、PyPi source、terminal',
+      author='HangfengYang',
+      author_email='yhf5fhy@gmail.com',
+      url='https://github.com/yhangf/PyQuickInstall',
+      license='MIT',
+      packages=["PQI"],
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+        'docopt',
+      ],
+      entry_points={
+        'console_scripts':[
+            'pqi = PQI.pqi:main'
+        ]
+      },
+)

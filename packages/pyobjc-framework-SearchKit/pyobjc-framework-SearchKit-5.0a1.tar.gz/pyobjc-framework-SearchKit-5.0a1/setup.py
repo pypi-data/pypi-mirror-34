0f0cd4b32f8e61072d0bbfa88163336712eb7ef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:40 +0000

    Add package pyobjc-framework-SearchKit/5.0a1/pyobjc-framework-SearchKit-5.0a1.tar.gz

---
+++ packages/pyobjc-framework-SearchKit/pyobjc-framework-SearchKit-5.0a1.tar.gz/pyobjc-framework-SearchKit-5.0a1/setup.py
@@ -0,0 +1,29 @@
+'''
+Wrappers for the "SearchKit" framework on macOS. SearchKit is a content
+indexing and search solution.
+
+These wrappers don't include documentation, please check Apple's documention
+for information on how to use this framework and PyObjC's documentation
+for general tips and tricks regarding the translation between Python
+and (Objective-)C frameworks
+
+NOTE: These wrappers are pretty fragile, sadly enough not all run-time
+information that needed to build reliable wrappers is exported from
+the framework.
+'''
+from pyobjc_setup import setup
+
+VERSION="5.0a1"
+
+setup(
+    name='pyobjc-framework-SearchKit',
+    description = "Wrappers for the framework SearchKit on macOS",
+    min_os_level='10.5',
+    packages = [ "SearchKit" ],
+    version=VERSION,
+    install_requires = [
+        'pyobjc-core>='+VERSION,
+        'pyobjc-framework-Cocoa>='+VERSION,
+    ],
+    long_description=__doc__,
+)

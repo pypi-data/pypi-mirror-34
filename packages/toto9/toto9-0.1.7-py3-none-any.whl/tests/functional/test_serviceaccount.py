9f2b22130f56d1233908911fb758b7e36377c709
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:11 +0000

    Add package toto9/0.1.7/toto9-0.1.7-py3-none-any.whl

---
+++ packages/toto9/toto9-0.1.7-py3-none-any.whl/tests/functional/test_serviceaccount.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import unittest
+
+from toto9.serviceaccount import ServiceAccount
+
+#TODO test the role bindings of service accounts
+
+class TestServiceAccount(unittest.TestCase):
+
+    def test_foo(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()

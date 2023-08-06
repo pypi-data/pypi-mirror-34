421bb864411656316810fb98a53dea0a2e93d833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:30 +0000

    Add package pyppl/1.0.1/PyPPL-1.0.1-py2.py3-none-any.whl

---
+++ packages/pyppl/PyPPL-1.0.1-py2.py3-none-any.whl/pyppl/runners/runner.py
@@ -0,0 +1,169 @@
+"""
+The base runner class
+"""
+import sys
+import re
+from time import sleep
+from multiprocessing import Value, Lock
+from subprocess import Popen, list2cmdline
+
+from .. import utils
+
+flushlock = Lock()
+
+class Runner (object):
+	"""
+	The base runner class
+	"""
+	
+	INTERVAL = 1
+	
+	def __init__ (self, job):
+		"""
+		Constructor
+		@params:
+			`job`:    The job object
+		"""
+		self.job       = job
+		self.script    = utils.chmodX(self.job.script)
+		self.cmd2run   = list2cmdline (self.script)
+		self.ntry      = Value('i', 0, lock = Lock())
+
+	def submit (self):
+		"""
+		Try to submit the job use Popen
+		"""
+		indexstr = self.job._indexIndicator()
+		if self.job.index not in self.job.proc.ncjobids:
+			return True
+		elif self.isRunning():
+			self.job.proc.log ("%s is already running, skip submission." % indexstr, 'submit')
+			return True
+		else:
+			self.job.reset(self.ntry.value)
+			
+			ferrw = open(self.job.errfile, 'w')
+			foutw = open(self.job.outfile, 'w')
+			succ  = True
+
+			try:
+				#self.job.proc.log ('Submitting job #%-3s ...' % self.job.index, 'submit')
+				# retry may open the files again
+				p  = Popen (self.script, stderr=ferrw, stdout=foutw, close_fds=True)
+				
+				rc = p.wait()
+				if rc != 0:
+					self.job.proc.log ('%s Submission failed with return code: %s.' % (indexstr, rc), 'error')
+					succ = False
+					
+			except Exception as ex:
+				self.job.proc.log ('%s Submission failed with exception: %s' % (indexstr, str(ex)), 'error')
+				ferrw.write(str(ex))
+				succ = False
+			finally:
+				ferrw.close()
+				foutw.close()
+				
+			if not succ:
+				self.job.rc(self.job.RC_SUBMITFAIL)
+				return False
+			else:
+				self.getpid()
+				return True
+
+	def finish(self):
+		self.job.done()
+	
+	def getpid (self):
+		"""
+		Get the job id
+		"""
+		pass
+
+	def run(self):
+		"""
+		@returns:
+			True: success/fail
+			False: needs retry
+		"""
+		# cached jobs
+		if self.job.index not in self.job.proc.ncjobids:
+			self.finish()
+			return True
+		
+		ferr = open(self.job.errfile)
+		fout = open(self.job.outfile)
+		lastout = ''
+		lasterr = ''
+		while self.job.rc() == self.job.RC_NOTGENERATE: # rc not generated yet
+			sleep (self.INTERVAL)
+			lastout, lasterr = self._flush(fout, ferr, lastout, lasterr)
+
+		self._flush(fout, ferr, lastout, lasterr, True)
+		ferr.close()
+		fout.close()
+		self.finish()
+		
+		return self.job.succeed()
+
+	def retry(self):
+		if self.job.proc.errhow == 'retry' and self.ntry.value < self.job.proc.errntry:
+			self.ntry.value += 1
+			self.job.proc.log ("%s Retrying job (%s/%s) ..." % (self.job._indexIndicator(), self.ntry.value, self.job.proc.errntry), 'RETRY')
+			return True
+		else:
+			return False
+
+	def isRunning (self):
+		"""
+		Try to tell whether the job is still running.
+		@returns:
+			`True` if yes, otherwise `False`
+		"""
+		jobpid = self.job.pid()
+		if not jobpid:
+			return False
+		return utils.dumbPopen (['kill', '-s', '0', jobpid]).wait() == 0
+		
+	def _flush (self, fout, ferr, lastout, lasterr, end = False):
+		"""
+		Flush stdout/stderr
+		@params:
+			`fout`: The stdout file handler
+			`ferr`: The stderr file handler
+			`lastout`: The leftovers of previously readlines of stdout
+			`lasterr`: The leftovers of previously readlines of stderr
+			`end`: Whether this is the last time to flush
+		"""
+		if self.job.index not in self.job.proc.echo['jobs']:
+			return None, None
+
+		if 'stdout' in self.job.proc.echo['type']:
+			lines, lastout = utils.flushFile(fout, lastout, end)
+			outfilter      = self.job.proc.echo['type']['stdout']
+			
+			for line in lines:
+				if not outfilter or re.search(outfilter, line):
+					with flushlock:
+						sys.stdout.write(line)
+
+		lines, lasterr = utils.flushFile(ferr, lasterr, end)		
+		for line in lines:
+			if line.startswith('pyppl.log'):
+				line = line.rstrip('\n')
+				logstrs  = line[9:].lstrip().split(':', 1)
+				if len(logstrs) == 1:
+					logstrs.append('')
+				(loglevel, logmsg) = logstrs
+				
+				loglevel = loglevel[1:] if loglevel else 'log'
+				
+				# '_' makes sure it's not filtered by log levels
+				self.job.proc.log (self.job._indexIndicator() + ' ' + logmsg.lstrip(), '_' + loglevel)
+			elif 'stderr' in self.job.proc.echo['type']:
+				errfilter = self.job.proc.echo['type']['stderr']
+				if not errfilter or re.search(errfilter, line):
+					with flushlock:
+						sys.stderr.write(line)
+		
+		return (lastout, lasterr)
---
+++ packages/pyppl/PyPPL-1.0.1-py2.py3-none-any.whl/pyppl/runners/runner_slurm.py
@@ -0,0 +1,121 @@
+import copy
+from subprocess import check_output
+from .runner import Runner
+
+class RunnerSlurm (Runner):
+	"""
+	The slurm runner
+	"""
+	
+	INTERVAL = 5
+	
+	def __init__ (self, job):
+		"""
+		Constructor
+		@params:
+			`job`:    The job object
+			`config`: The properties of the process
+		"""
+		super(RunnerSlurm, self).__init__(job)
+
+		# construct an slurm script
+		slurmfile = self.job.script + '.slurm'
+		
+		slurmsrc  = ['#!/usr/bin/env bash']
+	
+		conf = {}
+		if 'slurmRunner' in self.job.proc.props or 'slurmRunner' in self.job.proc.config:
+			conf = copy.copy (self.job.proc.slurmRunner)
+
+		self.commands = {'sbatch': 'sbatch', 'srun': 'srun', 'squeue': 'squeue'}
+		if 'sbatch' in conf:
+			self.commands['sbatch'] = conf['sbatch']
+		if 'srun' in conf:
+			self.commands['srun']   = conf['srun']
+		if 'squeue' in conf:
+			self.commands['squeue'] = conf['squeue']
+		
+		cmdPrefix = self.commands['srun']
+		if 'cmdPrefix' in conf:
+			cmdPrefix = conf['cmdPrefix']
+		
+		if not 'slurm.J' in conf:
+			jobname = '.'.join([
+				self.job.proc.id,
+				self.job.proc.tag,
+				self.job.proc._suffix(),
+				str(self.job.index + 1)
+			])
+			slurmsrc.append('#SBATCH -J %s' % jobname)
+		else:
+			jobname = conf['slurm.J']
+			slurmsrc.append('#SBATCH -J %s' % jobname)
+			del conf['slurm.J']
+		
+		if 'slurm.o' in conf:
+			slurmsrc.append('#SBATCH -o %s' % conf['slurm.o'])
+			del conf['slurm.o']
+		else:
+			slurmsrc.append('#SBATCH -o %s' % self.job.outfile)
+			
+		if 'slurm.e' in conf:
+			slurmsrc.append('#SBATCH -e %s' % conf['slurm.e'])
+			del conf['slurm.e']
+		else:
+			slurmsrc.append('#SBATCH -e %s' % self.job.errfile)
+		
+		for k in sorted(conf.keys()):
+			if not k.startswith ('slurm.'): continue
+			v = conf[k]
+			k = k[6:].strip()
+			src = '#SBATCH -' + (k if len(k)==1 else '-' + k)
+			if v != True: # {'notify': True} ==> -notify
+				src += ' ' + str(v)
+			slurmsrc.append(src)
+
+		slurmsrc.append ('')
+		slurmsrc.append ('trap "status=\\$?; echo \\$status >\'%s\'; exit \\$status" 1 2 3 6 7 8 9 10 11 12 15 16 17 EXIT' % self.job.rcfile)
+		
+		if 'preScript' in conf:
+			slurmsrc.append (conf['preScript'])
+
+		slurmsrc.append ('')
+		slurmsrc.append (cmdPrefix + ' ' + self.cmd2run)
+
+		if 'postScript' in conf:
+			slurmsrc.append (conf['postScript'])
+		
+		with open (slurmfile, 'w') as f:
+			f.write ('\n'.join(slurmsrc) + '\n')
+		
+		self.script = [self.commands['sbatch'], slurmfile]
+
+	def getpid (self):
+		"""
+		Get the job identity and save it to job.pidfile
+		"""
+		# sbatch: Submitted batch job 99999999
+		content = ''
+		with open(self.job.outfile) as f:
+			content = f.read().strip()
+		if not 'Submitted batch job' in content:
+			return
+		pid = int (content.split(' ')[-1])
+		self.job.pid (pid)
+
+	def isRunning (self):
+		"""
+		Tell whether the job is still running
+		@returns:
+			True if it is running else False
+		"""
+		jobid = self.job.pid ()
+		if not jobid: return False
+		try:
+			# header line
+			# jid xxx xxx
+			jid = str(check_output([self.commands['squeue'], '-j', jobid]).splitlines()[1].split()[0].decode())
+			return jobid == jid
+		except Exception:
+			return False
+

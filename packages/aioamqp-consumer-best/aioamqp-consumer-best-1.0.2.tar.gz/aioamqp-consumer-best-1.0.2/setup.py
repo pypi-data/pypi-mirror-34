fbf583390aba4395baf23f9edf9247f67639bd66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:08 +0000

    Add package aioamqp-consumer-best/1.0.2/aioamqp-consumer-best-1.0.2.tar.gz

---
+++ packages/aioamqp-consumer-best/aioamqp-consumer-best-1.0.2.tar.gz/aioamqp-consumer-best-1.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='aioamqp-consumer-best',
+    version='1.0.2',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'aioamqp',
+        'aionursery',
+        'dataclasses',
+    ],
+    extras_require={
+        'test': [
+            'mypy==0.600',
+            'pycodestyle',
+            'pylint',
+            'pytest',
+            'pytest-asyncio',
+            'pytest-cov',
+            'pytest-mock',
+        ],
+    },
+)

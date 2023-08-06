2def8a28600071dafc1c9bf1d0030a44e63d453b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:33 +0000

    Add package gym-guesswho/0.0.2/gym-guesswho-0.0.2.tar.gz

---
+++ packages/gym-guesswho/gym-guesswho-0.0.2.tar.gz/gym-guesswho-0.0.2/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name='gym-guesswho',
+    version='0.0.2',
+    author="Charles Rule, William (Alex) Fallin",
+    author_email="alexfallin@gmail.com",
+    description="An OpenAI Gym framework for machine learning with the game \"Guess Who?\"",
+    #long_description=long_description,
+    #long_description_content_type="text/markdown",
+    url="https://github.com/cerule7/Guess-Who",
+    packages=setuptools.find_packages(),
+    install_requires=['gym', 'numpy', 'torch'],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Natural Language :: English",
+        "License :: Other/Proprietary License",
+    ),
+)

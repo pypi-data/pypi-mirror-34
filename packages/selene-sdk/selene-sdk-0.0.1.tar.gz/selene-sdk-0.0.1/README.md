a3dae08c87e5267a8b9fd412ad71078038c6f0df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:17 +0000

    Add package selene-sdk/0.0.1/selene-sdk-0.0.1.tar.gz

---
+++ packages/selene-sdk/selene-sdk-0.0.1.tar.gz/selene-sdk-0.0.1/README.md
@@ -0,0 +1,37 @@
+![logo](docs/source/_static/img/selene_logo.png)
+
+---
+
+You have found *selene*, a Python library and command line interface for training deep neural networks from biological sequence data such as genomes.
+
+## Installation
+
+### Installing selene with pip:
+```sh
+pip install selene-sdk
+```
+
+### Installing selene with [Anaconda](https://www.anaconda.com/download/):
+COMING SOON. Has not been uploaded to conda yet, though we will recommend using Selene with conda once it is up.
+Currently, we recommend installing and running selene (using [selene_cli.py](selene_cli.py)) from the source build for better control over the PyTorch-related dependencies. 
+```sh
+conda install selene-sdk
+```
+
+### Installing selene from source:
+```sh
+git clone git@github.com:FunctionLab/selene
+```
+If you plan on working in the `selene` repository directly, we recommend setting up a conda environment using `selene-cpu.yml` or `selene-gpu.yml` and activating it. Please also build the Cython files by running
+```sh
+python setup.py build_ext --inplace
+```
+
+## Tutorials and examples
+
+Tutorials for selene are available [here](https://github.com/FunctionLab/selene/tree/master/tutorials).
+
+## Documentation
+
+The documentation for selene is available [here](https://selene.flatironinstitute.org/).
+
---
+++ packages/selene-sdk/selene-sdk-0.0.1.tar.gz/selene-sdk-0.0.1/setup.py
@@ -0,0 +1,61 @@
+import numpy as np
+from setuptools import find_packages
+from setuptools import setup
+
+
+try:
+    from Cython.Distutils.extension import Extension
+    from Cython.Distutils import build_ext
+except ImportError:
+    from setuptools.extension import Extension
+    USING_CYTHON = False
+else:
+    USING_CYTHON = True
+
+ext = '.pyx' if USING_CYTHON else '.c'
+
+genome_module = Extension(
+    "selene_sdk.sequences._sequence",
+    ["selene_sdk/sequences/_sequence" + ext],
+    include_dirs=[np.get_include()])
+
+genomic_features_module = Extension(
+    "selene_sdk.targets._genomic_features",
+    ["selene_sdk/targets/_genomic_features" + ext],
+    include_dirs=[np.get_include()])
+
+ext_modules = [genome_module, genomic_features_module]
+cmdclass = {'build_ext': build_ext} if USING_CYTHON else {}
+
+setup(name="selene-sdk",
+      version="0.0.1",
+      description=("framework for developing sequence-level "
+                   "deep learning networks"),
+      packages=find_packages(),
+      url="https://github.com/FunctionLab/selene",
+      package_data={
+        "selene-sdk": ["interpret/data/gencode_v28_hg38/*", "interpret/data/gencode_v28_hg19/*"]
+      },
+      classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: Scientific/Engineering :: Bio-Informatics"
+      ),
+      ext_modules=ext_modules,
+      cmdclass=cmdclass,
+      install_requires=[
+        "h5py",
+        "matplotlib",
+        "numpy",
+        "pandas",
+        "plotly",
+        "pyfaidx",
+        "pytabix",
+        "pyyaml",
+        "scikit-learn",
+        "scipy",
+        "seaborn",
+        "statsmodels",
+        "torch",
+        "torchvision"
+    ])

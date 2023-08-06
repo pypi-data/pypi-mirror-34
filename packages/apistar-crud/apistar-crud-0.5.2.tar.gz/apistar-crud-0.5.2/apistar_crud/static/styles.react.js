3c9816b5ea7dfbfb400afdf4adc5d71ad8979c60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:53 +0000

    Add package apistar-crud/0.5.2/apistar-crud-0.5.2.tar.gz

---
+++ packages/apistar-crud/apistar-crud-0.5.2.tar.gz/apistar-crud-0.5.2/apistar_crud/static/styles.react.js
@@ -0,0 +1,26 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["styles.react"],{
+
+/***/ "./admin/App.css":
+/*!***********************!*\
+  !*** ./admin/App.css ***!
+  \***********************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ "./admin/index.css":
+/*!*************************!*\
+  !*** ./admin/index.css ***!
+  \*************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ })
+
+}]);
+//# sourceMappingURL=styles.react.js.map

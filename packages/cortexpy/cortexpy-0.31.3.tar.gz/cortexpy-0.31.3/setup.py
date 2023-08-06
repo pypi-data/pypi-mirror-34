ca60dd3f32fbf4a648ff96770efd9ad5902811c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:27 +0000

    Add package cortexpy/0.31.3/cortexpy-0.31.3.tar.gz

---
+++ packages/cortexpy/cortexpy-0.31.3.tar.gz/cortexpy-0.31.3/setup.py
@@ -0,0 +1,79 @@
+import os
+import re
+from glob import glob
+from io import open
+
+from os.path import dirname, splitext, basename
+from setuptools import setup, find_packages, Extension
+
+# version_re = re.compile(r'\d+\.\d+\.\d+')
+# with open(os.path.join(dirname(__file__), 'src', 'cortexpy', '__init__.py'), 'rt') as fp:
+#     for line in fp:
+#         print(line)
+#         match = version_re.search(line)
+#         print(match)
+#         if match:
+#             version = match.group()
+#             break
+#
+# assert version
+
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    USE_CYTHON = False
+else:
+    USE_CYTHON = True
+
+ext = '.pyx' if USE_CYTHON else '.cpp'
+extensions = [Extension(
+    "cortexpy.graph.parser.kmer_ext",
+    ["src/cortexpy/graph/parser/kmer_ext" + ext],
+    extra_compile_args=["-std=c++11"],
+    extra_link_args=["-std=c++11"]
+)]
+
+if USE_CYTHON:
+    extensions = cythonize(extensions)  # , annotate=True)
+
+version='0.31.3'
+setup(
+    name='cortexpy',
+    version=version,
+    description='The python sister project to CortexJDK',
+    author='Warren W. Kretzschmar, Kiran V Garimella',
+    author_email='winni@warrenwk.com, kiran.garimella@gmail.com',
+    url='https://github.com/winni2k/cortexpy',
+    download_url='https://github.com/winni2k/cortexpy/archive/{}.tar.gz'.format(version),
+    license='Apache-2.0',
+    long_description=open('README.rst').read(),
+    install_requires="""
+    attrs
+    biopython
+    numpy
+    networkx
+    schema
+    delegation
+    msgpack
+    """.split('\n'),
+    tests_require=['coverage', 'pytest'],
+    python_requires=">=3.6",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    ext_modules=extensions,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    entry_points={
+        'console_scripts': ['cortexpy=cortexpy.__main__:main'],
+    },
+    include_package_data=True,
+)

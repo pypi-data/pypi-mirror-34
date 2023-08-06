a416829aae7d317bcb01d5ef9cab2b1e3b5a914d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:00 +0000

    Add package thermohw/0.2.4/thermohw-0.2.4.tar.gz

---
+++ packages/thermohw/thermohw-0.2.4.tar.gz/thermohw-0.2.4/thermohw/__init__.py
@@ -0,0 +1,6 @@
+"""Convert thermo homework assignments."""
+
+from .convert_thermo_hw import (assignment_nb_exp, solution_nb_exp,  # noqa: F401
+                                assignment_pdf_exp, solution_pdf_exp)  # noqa: F401
+from .extract_attachments import ExtractAttachmentsPreprocessor  # noqa: F401
+from ._version import __version__  # noqa: F401

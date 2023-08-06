30c7adcd83dc99ccb3d90a5efe5e0dc7a0f1ad47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:55 +0000

    Add package rcctl/1.1.0/rcctl-1.1.0.tar.gz

---
+++ packages/rcctl/rcctl-1.1.0.tar.gz/rcctl-1.1.0/rcctl/commands.py
@@ -0,0 +1,50 @@
+# -----------------------------------------------------------------------------
+# Adapted from Microsoft OSS
+# see https://github.com/Microsoft/service-fabric-cli
+# -----------------------------------------------------------------------------
+
+"""Command and help loader for the Service Fabric CLI.
+
+Commands are stored as one to one mappings between command line syntax and
+python function.
+"""
+
+from collections import OrderedDict
+from knack.commands import CLICommandsLoader, CommandSuperGroup
+from knack.help import CLIHelp
+from rcctl.apiclient import create as client_create
+
+# Need to import so global help dict gets updated
+import rcctl.helps.main # pylint: disable=unused-import
+
+class SFCommandHelp(CLIHelp):
+    """Service Fabric CLI help loader"""
+
+    def __init__(self, ctx=None):
+        header_msg = 'Service Fabric Command Line'
+
+        super(SFCommandHelp, self).__init__(ctx=ctx,
+                                            welcome_message=header_msg)
+
+class SFCommandLoader(CLICommandsLoader):
+    """Service Fabric CLI command loader, containing command mappings"""
+
+    def load_command_table(self, args): #pylint: disable=too-many-statements
+        """Load all Service Fabric commands"""
+
+        # Need an empty client for the select and upload operations
+        with CommandSuperGroup(__name__, self,
+                               'rcctl.custom_cluster#{}') as super_group:
+            with super_group.group('cluster') as group:
+                group.command('select', 'select')
+
+        with CommandSuperGroup(__name__, self, 'rcctl.custom_reliablecollections#{}',
+                               client_factory=client_create) as super_group: 
+            with super_group.group('dictionary') as group:
+                group.command('query', 'query_reliabledictionary')
+                group.command('execute', 'execute_reliabledictionary')
+                group.command('schema', 'get_reliabledictionary_schema')
+                group.command('list', 'get_reliabledictionary_list')
+                group.command('type-schema', 'get_reliabledictionary_type_schema')
+        
+        return OrderedDict(self.command_table)
---
+++ packages/rcctl/rcctl-1.1.0.tar.gz/rcctl-1.1.0/setup.py
@@ -0,0 +1,66 @@
+# -----------------------------------------------------------------------------
+# Adapted from Microsoft OSS
+# see https://github.com/Microsoft/service-fabric-cli
+# -----------------------------------------------------------------------------
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='rcctl',
+    version='1.1.0',
+    description='Azure Service Fabric Reliable Collections command line',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/shalabhms/reliable-collections-cli',
+    author='Antonio Menarde, Shalabh Mohan Shrivastava',
+    author_email='amenarde@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+    keywords='servicefabric azure',
+    python_requires='>=2.7,!=3.4,!=3.3,!=3.2,!=3.1,!=3.0,<3.7',
+    packages=[
+        'rcctl',
+        'rcctl.helps'
+    ],
+    install_requires=[
+        'knack==0.1.1',
+        'msrest>=0.4.26',
+        'msrestazure',
+        'requests',
+        'azure-servicefabric==6.2.0.0',
+        'jsonpickle',
+        'adal',
+        'future',
+	'sfquery>=0.2.0',
+        'xmltodict',
+        'xmljson',
+        'pandas'
+    ],
+    extras_require={
+        'test': [
+            'coverage',
+            'nose2',
+            'pylint',
+            'vcrpy',
+            'mock',
+            'contextlib2'
+        ]
+    },
+    entry_points={
+        'console_scripts': ['rcctl=rcctl:launch']
+    }
+)

1647abc07b648f735057d6a3e8ce2d8e57b5fad2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:38 +0000

    Add package novigo/0.0.6/novigo-0.0.6-py3-none-any.whl

---
+++ packages/novigo/novigo-0.0.6-py3-none-any.whl/novigo/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  5 18:41:37 2018
+
+@author: kmy07
+"""
+name = "novigo"
+
+def predictSalary():
+    import pickle
+    import numpy as np
+    file = r'./models/trained_model.sav'
+    loaded_model = pickle.load(open(file,'rb'))
+    experience = int(input("Enter the Experience in years to predict the salary:\t"))
+    print("The salary for the specified experience will be:%10d" % loaded_model.predict(np.array([[experience]])))

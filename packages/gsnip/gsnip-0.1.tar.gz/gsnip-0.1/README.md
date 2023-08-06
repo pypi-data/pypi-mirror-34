20abc460cd0aeaf0fdd69b0d40f432b800a03150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:31 +0000

    Add package gsnip/0.1/gsnip-0.1.tar.gz

---
+++ packages/gsnip/gsnip-0.1.tar.gz/gsnip-0.1/README.md
@@ -0,0 +1,19 @@
+# G-snip
+
+Command line tool for Gists
+
+## Setup
+
+1. Get GitHub Authentication Token [Settings] -> `.tokens.ini`.
+
+1. Copy files to Home directory
+
+```
+cp -v .tokens.ini .vimrc.more ~
+```
+
+## Usage
+
+```
+gsnip -h
+```
---
+++ packages/gsnip/gsnip-0.1.tar.gz/gsnip-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup (
+    name='gsnip',
+    version='0.1',
+    description='Command line tool for gists',
+    author='jongwony',
+    author_email='lastone9182@gmail.com',
+    scripts=['gsnip'],
+    url='https://github.com/jongwony/gsnip',
+)

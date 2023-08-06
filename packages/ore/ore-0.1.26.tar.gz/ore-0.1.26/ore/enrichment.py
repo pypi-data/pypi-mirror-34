b28cd5bb66e55099d0718c220f53b9648f1370a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:40 +0000

    Add package ore/0.1.26/ore-0.1.26.tar.gz

---
+++ packages/ore/ore-0.1.26.tar.gz/ore-0.1.26/ore/enrichment.py
@@ -0,0 +1,242 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Calculate enrichment.
+
+:Author: Felix Richter <Felix.Richter@icahn.mssm.edu>
+:Date: 2018-02-01
+:Copyright: 2018, Felix Richter
+:License: CC BY-SA
+"""
+
+
+import itertools
+import copy
+from functools import partial
+# from multiprocessing import Pool, cpu_count
+
+from .enrich_utils import calculate_gene_enrichment
+# from .enrich_utils import calculate_var_enrichment
+
+
+class Enrich(object):
+    """Methods and objects for enrichment.
+
+    TODO:
+        Separate the process of joining the final dataframe and
+        calculating enrichment into different modules
+
+    """
+
+    def __init__(self, joined_df, enrich_loc, rv_outlier_loc, distribution):
+        """Load and join variants and outliers.
+
+        Args:
+            joined_df (:obj:`DataFrame`): variants and outliers
+            enrich_loc (:obj:`str`): output file for enrichment calculations
+            rv_outlier_loc (:obj:`str`): file location with the final set of
+                outlier-variant pairs
+            distribution (:obj:`str`): type of distribution for outliers
+
+        Attributes:
+            `joined_df`: variants and outliers in a single dataframe
+
+        """
+        self.joined_df = joined_df
+        self.enrich_loc = enrich_loc
+        self.distribution = distribution
+        self.rv_outlier_loc = rv_outlier_loc
+
+    def loop_enrichment(self, n_processes, expr_cut_off_vec,
+                        tss_cut_off_vec, af_cut_off_vec,
+                        af_vcf, intracohort_rare_ac):
+        """Loop over enrichment.
+
+        Args:
+            n_processes (:obj:`int`): number of processes to use
+
+        """
+        if isinstance(expr_cut_off_vec, float):
+            expr_cut_off_vec = [expr_cut_off_vec]
+        if isinstance(tss_cut_off_vec, float):
+            tss_cut_off_vec = [tss_cut_off_vec]
+        if isinstance(af_cut_off_vec, float):
+            af_cut_off_vec = [af_cut_off_vec]
+        cartesian_iter = itertools.product(expr_cut_off_vec,
+                                           tss_cut_off_vec,
+                                           af_cut_off_vec)
+        # https://stackoverflow.com/questions/533905/get-the-cartesian-product-of-a-series-of-lists
+        enrichment_per_tuple_partial = partial(
+            self.enrichment_per_tuple,
+            af_vcf=af_vcf, intracohort_rare_ac=intracohort_rare_ac)
+        # print("Using {} cores, less than all {} cores".format(
+        #       n_processes, cpu_count()))
+        # with Pool(n_processes) as p:
+        #     out_line_list = p.map(enrichment_per_tuple_partial,
+        #                           cartesian_iter)
+        out_line_list = []
+        for cut_off_tuple in cartesian_iter:
+            print(cut_off_tuple)
+            out_list = enrichment_per_tuple_partial(cut_off_tuple)
+            print(out_list)
+            out_line_list.append(out_list)
+        self.write_enrichment_to_file(out_line_list)
+
+    def enrichment_per_tuple(self, cut_off_tuple, af_vcf=False,
+                             intracohort_rare_ac=None):
+        """Calculate enrichment for each tuple.
+
+        Deep copy the joined_df to avoid race conditions. Shallow copy is
+            probably enough but deepcopy just to be safe
+
+        """
+        print("Calculating enrichment for", cut_off_tuple)
+        enrich_df = copy.deepcopy(self.joined_df)
+        # replace af_cut_off with intra-cohort minimum if former is
+        # smaller than latter
+        print(cut_off_tuple)
+        max_intrapop_af = self.get_max_intra_pop_af(
+            enrich_df, cut_off_tuple[2])
+        max_vcf_af = self.get_max_vcf_af(enrich_df, cut_off_tuple[2])
+        enrich_df = self.identify_rows_to_keep(
+            enrich_df, max_intrapop_af, max_vcf_af,
+            af_vcf, intracohort_rare_ac,
+            self.distribution, cut_off_tuple)
+        # var_list = calculate_var_enrichment(enrich_df)
+        gene_list = calculate_gene_enrichment(enrich_df)
+        out_list = list(cut_off_tuple)
+        # out_list.extend(var_list)
+        out_list.extend(gene_list)
+        return "\t".join([str(i) for i in out_list])
+
+    @staticmethod
+    def identify_rows_to_keep(joined_df, max_intrapop_af, max_vcf_af,
+                              af_vcf, intracohort_rare_ac,
+                              distribution, cut_off_tuple):
+        """Reclassify rare variants and outliers and identify rows to keep.
+
+        Keep only rows for genes with at least 1 outlier
+
+        """
+        expr_cut_off, tss_cut_off, af_cut_off = cut_off_tuple
+        print("Parameters", expr_cut_off, tss_cut_off, af_cut_off)
+        # classify as within x kb of TSS
+        joined_df["near_TSS"] = abs(joined_df.tss_dist) <= tss_cut_off
+        joined_df = joined_df.loc[joined_df.near_TSS]
+        # print("filtered by TSS:", joined_df.shape)
+        # update outliers based on more extreme cut-offs
+        if distribution == "normal":
+            joined_df.expr_outlier = (
+                joined_df.z_abs >= expr_cut_off) & joined_df.expr_outlier
+            joined_df.expr_outlier_neg = (joined_df.expr_outlier &
+                                          joined_df.expr_outlier_neg)
+        elif distribution == "rank":
+            hi_expr_cut_off = 1 - expr_cut_off
+            joined_df.loc[:, "expr_outlier_neg"] = (
+                (joined_df.expr_rank <= expr_cut_off) &
+                joined_df.expr_outlier_neg)
+            joined_df.loc[:, "expr_outlier"] = (
+                (joined_df.expr_rank >= hi_expr_cut_off) |
+                joined_df.expr_outlier_neg) & joined_df.expr_outlier
+        # expression outliers should be preset as 0 and 1 for custom
+        # i.e., there should be no parameter iteration in this dimension
+        # else: raise error
+        # only keep if there is any outlier in the gene
+        joined_df.loc[:, 'gene_has_out_w_vars'] = joined_df.groupby(
+            'gene')['expr_outlier'].transform('sum') > 0
+        joined_df.loc[:, 'gene_has_NEG_out_w_vars'] = joined_df.groupby(
+            'gene')['expr_outlier_neg'].transform('sum') > 0
+        joined_df = joined_df.loc[joined_df.gene_has_out_w_vars]
+        # classify as rare/common
+        rare_variant_status = joined_df.popmax_af <= af_cut_off
+        if af_vcf:
+            rare_variant_status = rare_variant_status & (
+                joined_df.VCF_af <= max_vcf_af)
+        if intracohort_rare_ac:
+            rare_variant_status = rare_variant_status & (
+                joined_df.intra_cohort_ac <= intracohort_rare_ac)
+        else:
+            rare_variant_status = rare_variant_status & (
+                joined_df.intra_cohort_af <= max_intrapop_af)
+        joined_df.loc[:, "rare_variant_status"] = rare_variant_status
+        return joined_df
+
+    def write_enrichment_to_file(self, out_line_list):
+        """Write the enrichment results to a file."""
+        with open(self.enrich_loc, 'w') as enrich_f:
+            header_list = ["expr_cut_off", "tss_cut_off", "af_cut_off",
+                           # "var_or", "var_p", "var_neg_or", "var_neg_p",
+                           "not_rare_not_out", "not_rare_out",
+                           "rare_not_out", "rare_out",
+                           "not_rare_not_out_neg", "not_rare_out_neg",
+                           "rare_not_out_neg", "rare_out_neg",
+                           "gene_or", "gene_p", "gene_ci_lo", "gene_ci_hi",
+                           "gene_neg_or", "gene_neg_p", "gene_neg_ci_lo",
+                           "gene_neg_ci_hi"]
+            enrich_f.write("\t".join(header_list) + "\n")
+            for out_line in out_line_list:
+                if not out_line.startswith("NA_line"):
+                    enrich_f.write(out_line + "\n")
+                else:
+                    print("Skipping", out_line)
+
+    @staticmethod
+    def get_max_intra_pop_af(df_w_af, af_cut_off):
+        """Obtain the maximum wihtin population AF to classify variants as rare.
+
+        Args:
+            `df_w_af` (:obj:`DataFrame`): a DF with a `intra_cohort_af` column
+            `af_cut_off` (:obj:`float`): current allele frequency cut-off
+
+        """
+        if af_cut_off < min(df_w_af.intra_cohort_af):
+            max_intra_pop_af = min(df_w_af.intra_cohort_af)
+        else:
+            max_intra_pop_af = af_cut_off
+        return max_intra_pop_af
+
+    @staticmethod
+    def get_max_vcf_af(df_w_vcf_af, af_cut_off):
+        """Obtain the maximum VCF AF to classify variants as rare.
+
+        Args:
+            `df_w_vcf_af` (:obj:`DataFrame`): a DF with a `VCF_af` column
+            `af_cut_off` (:obj:`float`): current allele frequency cut-off
+
+        """
+        if af_cut_off < min(df_w_vcf_af.VCF_af):
+            max_vcf_af = min(df_w_vcf_af.VCF_af)
+        else:
+            max_vcf_af = af_cut_off
+        return max_vcf_af
+
+    def write_rvs_w_outs_to_file(self, out_cut_off, tss_cut_off, af_cut_off,
+                                 af_vcf, intracohort_rare_ac):
+        """Write outliers with specified cut-offs to a file.
+
+        Args:
+            `out_cut_off`
+
+        """
+        cut_off_tuple = (out_cut_off, tss_cut_off, af_cut_off)
+        enrich_df = copy.deepcopy(self.joined_df)
+        max_intrapop_af = self.get_max_intra_pop_af(enrich_df, af_cut_off)
+        max_vcf_af = self.get_max_vcf_af(enrich_df, af_cut_off)
+        outlier_df = self.identify_rows_to_keep(
+            enrich_df, max_intrapop_af, max_vcf_af,
+            af_vcf, intracohort_rare_ac,
+            distribution=self.distribution, cut_off_tuple=cut_off_tuple)
+        # only keep outliers with rare variants
+        outlier_df = outlier_df.loc[
+            outlier_df.rare_variant_status & outlier_df.expr_outlier]
+        # cols_to_keep = ["blinded_id", "gene", "z_expr", "tss_dist",
+        #                 "var_id", "popmax_af", "intra_cohort_af",
+        #                 "intra_cohort_ac", "VCF_af"]
+        # outlier_df = outlier_df[cols_to_keep]
+        outlier_df.to_csv(self.rv_outlier_loc, index=False, sep="\t",
+                          float_format='%g')
+
+#
+#
+#
+#
---
+++ packages/ore/ore-0.1.26.tar.gz/ore-0.1.26/ore/utils.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Various utilities.
+
+:Author: Felix Richter <Felix.Richter@icahn.mssm.edu>
+:Date: 2018-03-26
+:Copyright: 2018, Felix Richter
+:License: CC BY-SA
+"""
+
+import logging
+import sys
+import os
+import glob
+import multiprocessing as mp
+import itertools
+
+from pkg_resources import resource_filename
+import pandas as pd
+
+
+def initialize_logger(log_file, logAppName):
+    """Set up logging.
+
+    Returns:
+        logger (:obj:`logging object`): log with file and stream handlers
+
+    """
+    logger = logging.getLogger(logAppName)
+    logger.setLevel(logging.DEBUG)
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(logging.DEBUG)
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(logging.DEBUG)
+    # create formatter and add it to the handlers
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    file_handler.setFormatter(formatter)
+    formatter = logging.Formatter(
+        "%(asctime)s - %(message)s")
+    console_handler.setFormatter(formatter)
+    # add the handlers to the logger
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+    return(logger)
+
+
+def checkCPUcount(n_processes):
+    """Confirm CPU count input by user is less than max cores and int.
+
+    Args:
+        n_processes (:obj:`int`): number of cores that the user wants to use
+
+    """
+    if n_processes > mp.cpu_count():
+        print("Current machine only has {} cores, but input was {}".format(
+              mp.cpu_count(), n_processes))
+        raise ValueError
+
+
+def check_variant_inputs(args):
+    """Confirm variant-related inputs.
+
+    Args:
+        args (:obj:`argparse object`): contains user inputs
+
+    """
+    # TSS distance > 0
+    if any([i <= 0 for i in args.tss_dist]):
+        print("TSS distance must be greater than 0")
+        raise ValueError
+    if args.upstream and args.downstream:
+        print("Cannot use BOTH --upstream and --downstream")
+        raise ValueError
+
+
+def check_ANNOVAR_inputs(args):
+    """Confirm that ANNOVAR related inputs are available."""
+    if args.annovar:
+        if not os.path.exists(args.annovar_dir + "table_annovar.pl"):
+            print("table_annovar.pl does not exist in", args.annovar_dir)
+            raise FileNotFoundError
+        if not os.path.exists(args.annovar_dir + "annotate_variation.pl"):
+            print("annotate_variation.pl does not exist in", args.annovar_dir)
+            raise FileNotFoundError
+        # list of inputs needed for ANNOVAR in humandb directory
+        # provide code to install if not available
+        # what inputs are needed in humandb?
+
+
+def prepare_directory(new_dir, clean_run=False):
+    """Prepare per chromosome directory FOR NEW RUN.
+
+    Args:
+        new_dir (:obj:`str`): Location of new directory to create
+
+    Create a per_chrom directory if it does not exist. If it does exist,
+        ask the user if the directory should be cleaned for
+        a new run.
+
+    """
+    # print("Working in this directory:", os.getcwd())
+    if not os.path.exists(new_dir):
+        print("Creating", new_dir)
+        os.makedirs(new_dir)
+    if clean_run:
+        [os.remove(f) for f in glob.iglob(new_dir + "/*")]
+
+
+def multiprocess_by_chrom_cmd(n_processes, contigs, mp_function):
+    """Loop over chromosomes (by sending to multiple processes).
+
+    Args:
+        n_processes (:obj:`int`): number of workers/cores to run at a time
+        contigs (:obj:`list`): contigs present in the VCF
+        mp_function (:obj:`function`): function being processed across
+            multiple genomic regions
+
+    Returns:
+        chroms_completed (:obj:`list`): genomic regions that were
+            processed by `mp_function`
+
+    """
+    chrom_iter = itertools.chain([str(i) for i in contigs])
+    pool = mp.Pool(processes=n_processes)
+    print("Using {} out of {} cores".format(n_processes, mp.cpu_count()))
+    chroms_completed = pool.map(mp_function, chrom_iter)
+    # chroms_completed = []
+    # for chrom_i in chrom_iter:
+    #     chrom_done = mp_function(chrom_i)
+    #     chroms_completed.append(chrom_done)
+    return chroms_completed
+
+
+def applyParallel(dfGrouped, func, n_processes):
+    """Parallelize the pandas apply function.
+
+    Source:
+        https://stackoverflow.com/questions/26187759/parallelize-apply-after-pandas-groupby
+
+    Args:
+        dfGrouped (:obj:`DataFrame`): grouped dataframe, where
+            `func` is to be applied to each group separately
+        func (:obj:`int`):
+        n_processes (:obj:`int`): number of workers/cores to run at a time
+
+    Returns:
+        pd.concat(ret_list) (:obj:`DataFrame`): `dfGrouped` with function
+            applied to every group
+
+    """
+    # from multiprocessing import Pool, cpu_count
+    print("Using {} out of {} cores".format(n_processes, mp.cpu_count()))
+    # print("Total available cores: " + str(mp.cpu_count()))
+    with mp.Pool(processes=n_processes) as p:
+        ret_list = p.map(func, [group for name, group in dfGrouped])
+    try:
+        return pd.concat(ret_list)
+    except ValueError:
+        print("Was the dataframe being passed empty?", dfGrouped.head())
+        raise ValueError
+
+
+def anno_file_locations():
+    """Import hardcoded annotation locations.
+
+    Returns:
+        file_loc_list (:obj:`list`): list of BED file annotations
+
+    """
+    data_dir = resource_filename('ore', 'data/hg19_genome_masks/')
+    # mappability annotations
+    rmsk = data_dir + "rmsk/rmsk.merged.sorted.bed"
+    segdup = data_dir + "hg19_segdup.bed.gz"
+    lcr = data_dir + "hg19_lcr_hs37d5.bed.gz"
+    map300 = data_dir + "mappability300/mappability1_300.txt"
+    hla_muc = data_dir + "genome/genes.MUC.HLA.bed"
+    dac_blacklist = data_dir + "dac_blacklist.bed"
+    duke_blacklist = data_dir + "encode_duke_blacklist.bed"
+    pseudoauto_XY = data_dir + "pseudoautosomal_XY.bed"
+    file_loc_list = [rmsk, segdup, lcr, map300, hla_muc, dac_blacklist,
+                     duke_blacklist, pseudoauto_XY]
+    file_loc_list = [segdup, lcr]
+    return file_loc_list

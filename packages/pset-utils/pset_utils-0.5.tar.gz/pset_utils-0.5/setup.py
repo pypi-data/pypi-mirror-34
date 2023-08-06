0bc18dc8a552043a1a71e0a18d72c41e85a4d889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:01 +0000

    Add package pset-utils/0.5/pset_utils-0.5.tar.gz

---
+++ packages/pset-utils/pset_utils-0.5.tar.gz/pset_utils-0.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='pset_utils',
+      version='0.5',
+      description='Utilities to help with pyplot',
+      url='http://github.com/zyxw59/pset_utils',
+      author='Emily Crandall Fleischman',
+      author_email='emilycf@mit.edu',
+      license='MIT',
+      install_requires=['cycler'],
+      packages=['pset_utils'],
+      zip_safe=False)
+

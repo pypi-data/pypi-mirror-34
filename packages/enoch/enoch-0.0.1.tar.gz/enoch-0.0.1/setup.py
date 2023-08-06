606fd1fc04001ff0d37e88bcdeb990a1bc95127f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:58 +0000

    Add package enoch/0.0.1/enoch-0.0.1.tar.gz

---
+++ packages/enoch/enoch-0.0.1.tar.gz/enoch-0.0.1/setup.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*_
+# Copyright (C) 2018 Liu Yang <mkliuyang@gmail.com> All rights reserved.
+
+from distutils.core import setup
+
+setup(name='enoch',
+      version='0.0.1',
+      description='Python Base Utilities',
+      author='Liu Yang',
+      author_email='mkliuyang@gmail.com',
+      url='https://www.python.org/liu946/enoch/',
+     )

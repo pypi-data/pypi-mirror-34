0cbff436e44d48cdd9570696897a7617131e7a35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:22 +0000

    Add package nhlscrapy/0.0.1/nhlscrapy-0.0.1.tar.gz

---
+++ packages/nhlscrapy/nhlscrapy-0.0.1.tar.gz/nhlscrapy-0.0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="nhlscrapy",
+    version="0.0.1",
+    author="Xavier Thierry",
+    author_email="xaverthierry0@gmail.com",
+    description="A fast downloader of NHL data.",
+    url="https://github.com/xaaaaav/nhlscrapy",
+    packages=setuptools.find_packages(),
+    license = "http://www.apache.org/licenses/LICENSE-2.0"
+)

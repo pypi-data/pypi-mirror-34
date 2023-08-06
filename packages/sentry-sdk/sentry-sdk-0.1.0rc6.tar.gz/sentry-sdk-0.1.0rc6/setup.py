0ee7d4344908328b305da4e8f5867114bfcd01de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:59 +0000

    Add package sentry-sdk/0.1.0rc6/sentry-sdk-0.1.0rc6.tar.gz

---
+++ packages/sentry-sdk/sentry-sdk-0.1.0rc6.tar.gz/sentry-sdk-0.1.0rc6/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md")) as f:
+    long_description = "\n".join(f.read().splitlines()[6:])
+
+setup(
+    name="sentry-sdk",
+    version="0.1.0-preview6",
+    author="Sentry Team and Contributors",
+    author_email="hello@getsentry.com",
+    url="https://github.com/getsentry/sentry-sdk",
+    description="Python client for Sentry (https://getsentry.com)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=("tests", "tests.*")),
+    zip_safe=False,
+    license="BSD",
+    install_requires=["urllib3", "certifi"],
+    extras_require={"flask": ["flask>=0.8", "blinker>=1.1"]},
+)

e84b46b3c1c2a29ecd2b11b6c51caf4939b9f670
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:41 +0000

    Add package sed_eval/0.2.1/sed_eval-0.2.1.tar.gz

---
+++ packages/sed_eval/sed_eval-0.2.1.tar.gz/sed_eval-0.2.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+long_description = open('README.rst').read()
+
+setup(
+    name='sed_eval',
+    version='0.2.1',
+    description='Evaluation toolbox for Sound Event Detection',
+    author='Toni Heittola',
+    author_email='toni.heittola@gmail.com',
+    url='https://github.com/TUT-ARG/sed_eval',
+    packages=find_packages(),
+    long_description=long_description,
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        'Development Status :: 5 - Production/Stable',
+        "Intended Audience :: Developers",
+        "Topic :: Multimedia :: Sound/Audio :: Analysis",
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords=['audio analysis', 'sound event detection', 'dsp'],
+    license='MIT',
+    install_requires=[
+        'numpy >= 1.7.0',
+        'dcase_util >= 0.2.4'
+    ],
+)

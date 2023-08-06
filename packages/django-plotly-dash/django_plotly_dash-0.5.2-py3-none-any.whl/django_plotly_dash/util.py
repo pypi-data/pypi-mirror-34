522b346e136192df1c81771bb441bc43d8bf5b50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:21 +0000

    Add package django-plotly-dash/0.5.2/django_plotly_dash-0.5.2-py3-none-any.whl

---
+++ packages/django-plotly-dash/django_plotly_dash-0.5.2-py3-none-any.whl/django_plotly_dash/util.py
@@ -0,0 +1,30 @@
+'''
+Utility functions
+'''
+
+from django.conf import settings
+
+def _get_settings():
+    try:
+        the_settings = settings.PLOTLY_DASH
+    except AttributeError:
+        the_settings = None
+
+    return the_settings if the_settings else {}
+
+def pipe_ws_endpoint_name():
+    'Return the endpoint for pipe websocket connections'
+    return _get_settings().get('ws_route', 'dpd/ws/channel')
+
+def dpd_http_endpoint_root():
+    'Return the root of the http endpoint for direct insertion of pipe messages'
+    return _get_settings().get('http_route', 'dpd/views')
+
+def http_endpoint(stem):
+    'Form the http endpoint for a specific stem'
+    return "^%s/%s/$" % (dpd_http_endpoint_root(), stem)
+
+def insert_demo_migrations():
+    'Check settings and report if objects for demo purposes should be inserted during migration'
+
+    return _get_settings().get('insert_demo_migrations', False)

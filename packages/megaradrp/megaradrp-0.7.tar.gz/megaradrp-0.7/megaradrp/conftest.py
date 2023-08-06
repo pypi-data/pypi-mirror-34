f1af61d10827ebd8bd8fba5591e477471fda5f0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:59 +0000

    Add package megaradrp/0.7/megaradrp-0.7.tar.gz

---
+++ packages/megaradrp/megaradrp-0.7.tar.gz/megaradrp-0.7/megaradrp/conftest.py
@@ -0,0 +1,10 @@
+#
+# Copyright 2011-2017 Universidad Complutense de Madrid
+#
+# This file is part of Megara DRP
+#
+# SPDX-License-Identifier: GPL-3.0+
+# License-Filename: LICENSE.txt
+#
+
+from numina.tests.pytest_plugins import *

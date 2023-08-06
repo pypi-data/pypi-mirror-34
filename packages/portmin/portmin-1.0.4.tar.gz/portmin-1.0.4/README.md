8f6211a4bbc0f8fb26d7a3bffae3474cf4c81e44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:30 +0000

    Add package portmin/1.0.4/portmin-1.0.4.tar.gz

---
+++ packages/portmin/portmin-1.0.4.tar.gz/portmin-1.0.4/README.md
@@ -0,0 +1,11 @@
+# Portmin
+
+**Portmin** is a simple wrapper around FORTRAN **PORT** optimization routines. It was developed as a side-task for our **SEMOpt** package.
+
+## Author
+
+* **Mescheryakov Georgy Andreyevich** - [Herrberg](https://bitbucket.org/herrberg) - undegraduate student, SPbSTU
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
---
+++ packages/portmin/portmin-1.0.4.tar.gz/portmin-1.0.4/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+from distutils.extension import Extension
+from Cython.Build import build_ext
+import numpy
+import os
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+src_dir = 'portmin'
+
+ext = Extension(src_dir + '.optimization',
+		[
+	         src_dir + '/optimization.pyx'],
+		 libraries=[],
+		 include_dirs=[numpy.get_include()])
+
+setup(
+      name="portmin",
+      version="1.0.4",
+      author="Meshcheryakov Georgy Andreyevich",
+      author_email="metsheryakov_ga@spbstu.ru",
+      description="PORT unconstrained optimization routines wrapper.",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://bitbucket.org/herrberg/portmin/",
+      ext_modules=[ext],
+      packages=find_packages(),
+      cmdclass={'build_ext': build_ext},
+      data_files=[('pxd', [src_dir + '/optimization.pxd']),
+		  ('c', [src_dir + '/toms611.c'])],
+      zip_safe=False,
+      classifiers=(
+              "Programming Language :: Python :: 3",
+              "License :: OSI Approved :: MIT License",
+              "Operating System :: OS Independent"))

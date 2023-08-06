d01e960e8500735bee7a3f99df73a8903c7d0181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:36 +0000

    Add package iotedgehubdev/0.2.0/iotedgehubdev-0.2.0-py2.py3-none-any.whl

---
+++ packages/iotedgehubdev/iotedgehubdev-0.2.0-py2.py3-none-any.whl/iotedgehubdev/__init__.py
@@ -0,0 +1,8 @@
+import pkg_resources
+import six  # noqa: F401
+
+pkg_resources.declare_namespace(__name__)
+
+__author__ = 'Microsoft Corporation'
+__version__ = '0.2.0'
+__AIkey__ = 'fed7fc65-5b4a-4e66-9d46-c5f016d4e2b4'

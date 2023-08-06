f6e335c345c37af0ed5822feba5b1b6f624d70c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:25 +0000

    Add package ywdblogmath2/0.2/ywdblogmath2-0.2.tar.gz

---
+++ packages/ywdblogmath2/ywdblogmath2-0.2.tar.gz/ywdblogmath2-0.2/setup.py
@@ -0,0 +1,11 @@
+ #!/usr/bin/env python
+ 
+from setuptools import setup
+setup(name='ywdblogmath2',
+version='0.2',
+description='A silly ywdblogmath package',
+author='ywdblog',
+author_email='ywdblog@gmail.com',
+url='http://www.yudadan.com/',
+packages=['ywdblogmath2', 'ywdblogmath2.adv'],
+)

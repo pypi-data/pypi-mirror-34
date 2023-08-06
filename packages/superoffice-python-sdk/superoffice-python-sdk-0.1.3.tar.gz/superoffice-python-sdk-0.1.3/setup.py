afa5ba09a33bb1635d5655ad1300300ca875259e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:04 +0000

    Add package superoffice-python-sdk/0.1.3/superoffice-python-sdk-0.1.3.tar.gz

---
+++ packages/superoffice-python-sdk/superoffice-python-sdk-0.1.3.tar.gz/superoffice-python-sdk-0.1.3/setup.py
@@ -0,0 +1,21 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+long_description = '''
+SuperOffice Rest API Python SDK
+'''
+
+setup(
+    name='superoffice-python-sdk',
+    version='0.1.3',
+    author='Ignotas Petrulis',
+    author_email='ignotas.petrulis@gmail.com',
+    packages=['superofficesdk'],
+    install_requires=[
+        'requests'
+    ],
+    description='SuperOffice Python SDK',
+    long_description=long_description
+)

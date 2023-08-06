68b08308039b9b7dbedf00db5f422f408e224802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:41 +0000

    Add package trie/1.3.8/trie-1.3.8.tar.gz

---
+++ packages/trie/trie-1.3.8.tar.gz/trie-1.3.8/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(
+    name='trie',
+    # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
+    version='1.3.8',
+    description="""Python implementation of the Ethereum Trie structure""",
+    long_description_markdown_filename='README.md',
+    author='Piper Merriam',
+    author_email='pipermerriam@gmail.com',
+    url='https://github.com/ethereum/py-trie',
+    include_package_data=True,
+    py_modules=['trie'],
+    setup_requires=['setuptools-markdown'],
+    install_requires=[
+        "eth-hash>=0.1.0,<1.0.0",
+        "eth-utils>=1.0.1,<2.0.0",
+        "rlp>=0.4.7,<2.0.0",
+        "cytoolz>=0.8.0,<1",
+    ],
+    license="MIT",
+    zip_safe=False,
+    keywords='ethereum blockchain evm trie merkle',
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)

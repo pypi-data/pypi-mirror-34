52f43a77a31f0677ef309149eb372354d3841f9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:24 +0000

    Add package simplecel/0.2.10.3/simplecel-0.2.10.3.tar.gz

---
+++ packages/simplecel/simplecel-0.2.10.3.tar.gz/simplecel-0.2.10.3/simplecel/static/default.js
@@ -0,0 +1,16 @@
+let defaultConfig = {
+  rowHeaders: true,
+  colHeaders: true,
+  manualRowResize: true,
+  manualColumnResize: true,
+  // fixedRowsTop: 1,
+  filters: true,
+  dropdownMenu: true,
+  contextMenu: true,
+  maxColWidth: 200,
+  hasHeader: false,
+  // renderers: 'markdownRenderer',
+  allowInsertCol: true,
+  autoInsertEmptyRow: true,
+  undo: true
+};

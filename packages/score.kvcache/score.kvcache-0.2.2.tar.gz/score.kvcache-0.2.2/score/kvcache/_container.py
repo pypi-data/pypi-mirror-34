bcd35337988204c9305d0965b8121d0822ee2049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:02 +0000

    Add package score.kvcache/0.2.2/score.kvcache-0.2.2.tar.gz

---
+++ packages/score.kvcache/score.kvcache-0.2.2.tar.gz/score.kvcache-0.2.2/score/kvcache/_container.py
@@ -0,0 +1,69 @@
+# Copyright © 2015-2018 STRG.AT GmbH, Vienna, Austria
+#
+# This file is part of the The SCORE Framework.
+#
+# The SCORE Framework and all its parts are free software: you can redistribute
+# them and/or modify them under the terms of the GNU Lesser General Public
+# License version 3 as published by the Free Software Foundation which is in the
+# file named COPYING.LESSER.txt.
+#
+# The SCORE Framework and all its parts are distributed without any WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. For more details see the GNU Lesser General Public
+# License.
+#
+# If you have not received a copy of the GNU Lesser General Public License see
+# http://www.gnu.org/licenses/.
+#
+# The License-Agreement realised between you as Licensee and STRG.AT GmbH as
+# Licenser including the issue of its valid conclusion and its pre- and
+# post-contractual effects is governed by the laws of Austria. Any disputes
+# concerning this License-Agreement including the issue of its valid conclusion
+# and its pre- and post-contractual effects are exclusively decided by the
+# competent court, in whose district STRG.AT GmbH has its registered seat, at
+# the discretion of STRG.AT GmbH also the competent court, in whose district the
+# Licensee has his registered seat, an establishment or assets.
+
+
+class CacheContainer:
+    """
+    A cache container to wrap key-value pairs.
+    """
+
+    def __init__(self, name, backend, expire=None):
+        self.name = name
+        self.backend = backend
+        self.expire = expire
+        self.generator = None
+
+    def __delitem__(self, key):
+        """
+        Invalidates a value for given key.
+        """
+        self.backend.invalidate(self.name, key)
+
+    def __setitem__(self, key, value):
+        """
+        Explicitly sets a value for given key ignoring validity.
+        """
+        self.backend.store(self.name, key, value, self.expire)
+
+    def __getitem__(self, key):
+        """
+        Gets a value for given key and takes care of not found cache items.
+        """
+        try:
+            value = self.backend.retrieve(self.name, key)
+        except NotFound:
+            if self.generator is None:
+                raise
+            value = self.generator(key)
+            self[key] = value
+        return value
+
+
+class NotFound(Exception):
+    """
+    Thrown if cache is invalid (either expired or not available).
+    """
+    pass
---
+++ packages/score.kvcache/score.kvcache-0.2.2.tar.gz/score.kvcache-0.2.2/setup.py
@@ -0,0 +1,41 @@
+import os
+
+from setuptools import setup
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.rst')) as f:
+    README = f.read()
+
+setup(
+    name='score.kvcache',
+    version='0.2.2',
+    description='Key/Value cache of The SCORE Framework',
+    long_description=README,
+    author='strg.at',
+    author_email='support@strg.at',
+    url='http://strg.at',
+    keywords='web wsgi caching',
+    packages=['score', 'score.kvcache'],
+    namespace_packages=['score'],
+    license='LGPL',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Framework :: Pyramid',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General '
+            'Public License v3 or later (LGPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'score.init >= 0.3.1',
+    ],
+)

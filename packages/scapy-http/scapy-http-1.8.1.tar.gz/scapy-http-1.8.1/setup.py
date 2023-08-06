7417e45570441df4923d5c8e74c9d0e3986cf6c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:16 +0000

    Add package scapy-http/1.8.1/scapy-http-1.8.1.tar.gz

---
+++ packages/scapy-http/scapy-http-1.8.1.tar.gz/scapy-http-1.8.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+from distutils.command.install import install
+import os.path
+import shutil
+
+VERSION = '1.8.1'
+
+
+def install_into_scapy(a):
+    print('Installing the HTTP layer extension into Scapy...')
+    import scapy
+    target_path = os.path.join(
+        os.path.dirname(scapy.__file__),
+        'layers'
+    )
+    source_path = os.path.join(
+        os.path.dirname(__file__),
+        'scapy_http/http.py'
+    )
+    shutil.copy2(source_path, target_path)
+    print('done!')
+
+
+install.sub_commands.append(('install_into_scapy', install_into_scapy))
+
+setup(
+    name="scapy-http",
+    packages=['scapy_http'],
+    version=VERSION,
+    description="HTTP-layer support for Scapy",
+    install_requires=['scapy'],
+    author=['Luca Invernizzi, Steeve Barbeau'],
+    author_email='invernizzi.l@gmail.com',
+    url='https://github.com/invernizzi/scapy-http',
+    download_url='https://github.com/invernizzi/scapy-http/tarball/' + VERSION,
+    keywords=['http', 'scapy', 'newtork', 'dissect', 'packets']
+)

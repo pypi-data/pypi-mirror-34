52b03e940dd3d04823ef0a0484464d842768c3cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:07 +0000

    Add package cmstoolbox/0.9.3/cmstoolbox-0.9.3.tar.gz

---
+++ packages/cmstoolbox/cmstoolbox-0.9.3.tar.gz/cmstoolbox-0.9.3/setup.py
@@ -0,0 +1,14 @@
+import glob
+import setuptools
+
+setuptools.setup(
+    name='cmstoolbox',
+    version='0.9.3',
+    packages=setuptools.find_packages(),
+    author='Daniel Abercrombie',
+    author_email='dabercro@mit.edu',
+    description='Tools used by CMS Computing Operations',
+    url='https://github.com/CMSCompOps/OpsSpace',
+    install_requires=['testfixtures'],
+    scripts=[s for s in glob.glob('bin/*') if not s.endswith('~')]
+    )

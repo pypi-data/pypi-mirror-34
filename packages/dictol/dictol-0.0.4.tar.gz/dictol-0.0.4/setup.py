647505bc00e01a9795a24a453cb27f26eb49f9c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:15 +0000

    Add package dictol/0.0.4/dictol-0.0.4.tar.gz

---
+++ packages/dictol/dictol-0.0.4.tar.gz/dictol-0.0.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    include_package_data=True,
+    name="dictol",
+    version="0.0.4",
+    author="Tiep Vu",
+    author_email="vuhuutiep@gmail.com",
+    description="A dictionary learning package for classification",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tiepvupsu/dictol_python",
+    packages=['dictol'],
+    package_dir = {'dictol': 'dictol'},
+    parkage_data={'dictol':['data/*.mat']},
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

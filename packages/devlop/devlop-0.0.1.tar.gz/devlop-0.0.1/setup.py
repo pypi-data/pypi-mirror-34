1098287f4ccbd9b334566e689ecbb8a7e901cc96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:43 +0000

    Add package devlop/0.0.1/devlop-0.0.1.tar.gz

---
+++ packages/devlop/devlop-0.0.1.tar.gz/devlop-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="devlop",
+    version="0.0.1",
+    author="Stefan Foulis",
+    author_email="stefan@foulis.ch",
+    description="A tool for awesomely easy local web development.",
+    url="https://github.com/devlopsite/devlop-cli",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ),
+)

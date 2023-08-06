56b6e1d07d35ae6f2521cf92156e0b663b62579d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:42 +0000

    Add package wwu-tinker/0.1a2/wwu_tinker-0.1a2.tar.gz

---
+++ packages/wwu-tinker/wwu_tinker-0.1a2.tar.gz/wwu_tinker-0.1a2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='wwu_tinker',
+      version='0.1a2',
+      description='Free hyperparameter optimization webservice',
+      url='http://github.com/hutchresearch/wwu_tinker',
+      author='WWU',
+      author_email='brian.hutchinson@wwu.edu',
+      license='MIT',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Programming Language :: Python :: 3',
+      ],
+      packages=['wwu_tinker'],
+      install_requires=[
+          'requests',
+      ],
+      zip_safe=False
+)
+

d7505674702f678a617104e4540ef84506f75601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:05 +0000

    Add package txupnp/0.0.1a5/txupnp-0.0.1a5.tar.gz

---
+++ packages/txupnp/txupnp-0.0.1a5.tar.gz/txupnp-0.0.1a5/txupnp/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "0.0.1a5"
+__name__ = "txupnp"
+__author__ = "Jack Robison"
+__maintainer__ = "Jack Robison"
+__license__ = "MIT"
+__email__ = "jackrobison@lbry.io"
+
+
+import logging
+log = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter('%(asctime)-15s-%(filename)s:%(lineno)s->%(message)s'))
+log.addHandler(handler)
+log.setLevel(logging.INFO)

1eeb1137c35f4722cbc659fb4b5ff8928d7f3b85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:17 +0000

    Add package zoocut/0.1/zoocut-0.1.tar.gz

---
+++ packages/zoocut/zoocut-0.1.tar.gz/zoocut-0.1/setup.py
@@ -0,0 +1,13 @@
+import  setuptools
+
+setuptools.setup(name='zoocut',
+      version='0.1',
+      description='A Chinese word segmentation tool',
+      url='http://github.com/tongling/clinicaltrial',
+      author='BearAndGirl',
+      author_email='yuanhaoda17@mails.ucas.ac.cn',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      include_package_data = True,
+      install_requires = [''],
+      zip_safe=False)

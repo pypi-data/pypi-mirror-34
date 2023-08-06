0078353a79fb7646fe8b9e642b8ec27160a1519a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:07 +0000

    Add package couchutils/0.2.0/couchutils-0.2.0-py2.py3-none-any.whl

---
+++ packages/couchutils/couchutils-0.2.0-py2.py3-none-any.whl/couchutils/__init__.py
@@ -0,0 +1,16 @@
+#
+#  _  _     _ |_    _|_ o  |  _
+# (_ (_)|_|(_ | ||_| |_ |  | _>
+#
+
+"""A collection of CouchDB utils.
+
+Example::
+
+    >>> from couchutils import compile_docs
+    >>> compile_doc.compile_docs('<DOC_DIR>')
+    {...}
+"""
+
+from couchutils.__version__ import (  # noqa: F401
+    __author__, __author_email__, __copyright__, __description__, __license__, __title__, __url__, __version__)

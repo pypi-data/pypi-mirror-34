260b384e52867a643c803d508a939fa2d5239118
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:18:50 +0000

    Add package InterferencePackage/0.0.2/InterferencePackage-0.0.2.tar.gz

---
+++ packages/InterferencePackage/InterferencePackage-0.0.2.tar.gz/InterferencePackage-0.0.2/InterferencePackage/InterferencePackage.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 24 12:07:46 2018
+
+@author: Stefan
+"""
+
+"""
+Declarations of package methods
+"""
+def testo():
+    print "Ein test!"
+
+"""
+Testing and stuff, later it should be real tests
+"""
+if __name__ == "__main__":
+    print "hello world"
---
+++ packages/InterferencePackage/InterferencePackage-0.0.2.tar.gz/InterferencePackage-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="InterferencePackage",
+    version="0.0.2",
+    author="Stefan Richter",
+    author_email="stefan.richter@physical-perception.de",
+    description="A package for calculating incoherent (or coherent) diffractiom images from FELs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.physical-engine.de",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)

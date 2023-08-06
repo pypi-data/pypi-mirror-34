b705eb937608c6daf0de7ef6a63b247de9c7d351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:28 +0000

    Add package aio-openapi/0.4.3/aio_openapi-0.4.3-py3-none-any.whl

---
+++ packages/aio-openapi/aio_openapi-0.4.3-py3-none-any.whl/openapi/spec/__init__.py
@@ -0,0 +1,25 @@
+import os
+
+from aiohttp import web
+
+from .operation import op
+from .path import ApiPath
+from .spec import OpenApi, OpenApiSpec, SchemaParser, spec_root
+from ..data.exc import Error
+
+
+__all__ = [
+    'op', 'ApiPath', 'OpenApi', 'OpenApiSpec', 'SchemaParser', 'setup_app'
+]
+
+SPEC_ROUTE = os.environ.get('SPEC_ROUTE', '/spec')
+
+
+def setup_app(app):
+    app['exc_schema'] = Error
+    if SPEC_ROUTE:
+        app.add_routes(
+            [
+                web.get(SPEC_ROUTE, spec_root)
+            ]
+        )

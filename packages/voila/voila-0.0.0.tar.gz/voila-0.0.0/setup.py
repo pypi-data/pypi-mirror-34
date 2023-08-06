cd9b32e89ff16626a49039901307b29e086eefd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:01 +0000

    Add package voila/0.0.0/voila-0.0.0.tar.gz

---
+++ packages/voila/voila-0.0.0.tar.gz/voila-0.0.0/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+if __name__ == '__main__':
+    setup(
+        name='voila',
+        version='0.0.0',
+        packages=[],
+        description='Voila',
+        long_description='Voila',
+        author='Sylvain Corlay',
+        license='BSD',
+        platforms="Linux, Mac OS X, Windows",
+        keywords=['Interactive', 'Visualization', 'Web'],
+        classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ])

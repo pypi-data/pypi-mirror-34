e7a2b2f6dde10fabe8b0b3b5cde2d5bf7c8b50d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:30 +0000

    Add package experinet/0.0.0/experinet-0.0.0-py2.py3-none-any.whl

---
+++ packages/experinet/experinet-0.0.0-py2.py3-none-any.whl/scripts/experinet_cli.py
@@ -0,0 +1,30 @@
+"""Defines a command-line interface for experinet."""
+
+import click
+
+import experinet
+
+
+@click.group()
+def cli():
+    """Command-line interface for the experinet package."""
+    pass
+
+
+EXP_DOC = "Run all experiments in the given directory."
+
+
+@cli.command(help=EXP_DOC + (
+    "\n\n Arguments:\n\n DIRECTORY  The directory to search for experiments."))
+@click.argument("directory", type=str, nargs=1)
+@click.option(
+    '-r', '--recursive/--flat', default=False,
+    help=(
+        "Recurse into sub-directories in experiment discovery."
+        "Defaults to flat."))
+@click.option('-s', '--silent/--verbose', default=False,
+              help="Don't print any messages to screen. Defaults to verbose.")
+def bydir(directory, recursive, silent):
+    """{}""".format(EXP_DOC)
+    experinet.run_experiments_by_dir(
+        directory=directory, recursive=recursive, silent=silent)

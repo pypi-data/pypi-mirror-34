4cd37118895707af29a002d8f09b33be5248d4a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:41 +0000

    Add package excel2json-3/0.0.7/excel2json_3-0.0.7-py3-none-any.whl

---
+++ packages/excel2json-3/excel2json_3-0.0.7-py3-none-any.whl/excel2json/__init__.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+# created_on: 2018-08-08 23:08
+
+"""
+__init__.py
+"""
+
+from api.convert import convert_from_file, convert_from_url, jsonify
+
+
+__author__ = 'Toran Sahu  <toran.sahu@yahoo.com>'
+__license__ = 'Distributed under terms of the MIT license.'

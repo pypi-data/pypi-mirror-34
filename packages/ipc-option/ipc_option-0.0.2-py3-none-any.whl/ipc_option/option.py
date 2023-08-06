9d44208d79793d5e00eb532861a43cebcfaf08b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:27 +0000

    Add package ipc-option/0.0.2/ipc_option-0.0.2-py3-none-any.whl

---
+++ packages/ipc-option/ipc_option-0.0.2-py3-none-any.whl/ipc_option/option.py
@@ -0,0 +1,44 @@
+import os
+
+
+class OptionMeta(type):
+    def __new__(cls, name, bases, namespace, env_name=None, required=False, default=None):
+        if name != 'Option' and env_name is not None:
+            # subclass, set class name = env_name in globals
+            if required:
+                value = os.getenv(env_name)
+            else:
+                value = os.getenv(env_name, default)
+            set_or_raise(name, value)
+
+        for attr, option in namespace.items():
+            if not OptionMeta.is_dunder(attr) and isinstance(option, Option):
+                set_or_raise(attr, option.value)
+
+        return type.__new__(cls, name, bases, namespace)
+
+    @staticmethod
+    def is_dunder(name):
+        return name.startswith('__') and name.endswith('__') and len(name) > 4
+
+
+class Option(metaclass=OptionMeta):
+    def __init__(self, env_name, required=False, default=None):
+        if required:
+            value = os.getenv(env_name)
+            if value is None:
+                raise ValueError('env var {} for option not found'.format(env_name))
+        else:
+            value = os.getenv(env_name, default)
+
+        if value is None:
+            self.value = 'NOT_SET'
+        else:
+            self.value = value
+
+
+def set_or_raise(name, value):
+    if name in globals():
+        raise Exception('{} already exists in globals'.format(name))
+    else:
+        globals()[name] = value

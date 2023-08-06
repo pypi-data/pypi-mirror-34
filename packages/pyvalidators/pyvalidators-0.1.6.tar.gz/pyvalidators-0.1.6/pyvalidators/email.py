bae6741e67f0799e01008271cfde24b95b6e1930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:12 +0000

    Add package pyvalidators/0.1.6/pyvalidators-0.1.6.tar.gz

---
+++ packages/pyvalidators/pyvalidators-0.1.6.tar.gz/pyvalidators-0.1.6/pyvalidators/email.py
@@ -0,0 +1,9 @@
+"""pyvalidators email."""
+import re
+
+EMAIL_REGEX = '[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}'
+
+
+def is_valid_email(email):
+    """Validate email is valid."""
+    return re.match(EMAIL_REGEX, email)

316150404c3739d56013582ef31640a094299bc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:34 +0000

    Add package chaoNester/1.0.0/chaoNester-1.0.0.tar.gz

---
+++ packages/chaoNester/chaoNester-1.0.0.tar.gz/chaoNester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'chaoNester',
+	version		= '1.0.0',
+	py_modules	= ['chaoNester'],
+	author		= 'chao',
+	author_email= '13460057294@163.com',
+	url			= 'https://github.com/pypa/sampleproject',
+	description	= 'A simple printer of nested lists',
+	)

46d6fae6fd429826e0241a9a78d45a853de57bf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:23 +0000

    Add package fsoopify/0.1.18/fsoopify-0.1.18.tar.gz

---
+++ packages/fsoopify/fsoopify-0.1.18.tar.gz/fsoopify-0.1.18/fsoopify/extras/pickle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017~2999 - cologler <skyoflw@gmail.com>
+# ----------
+#
+# ----------
+
+import pickle
+from ..nodes import FileInfo
+
+# pylint: disable=R0201,C0111
+
+@FileInfo.register_format(__name__.split('.')[-1])
+class PickleSerializer:
+
+    def load(self, src: FileInfo, kwargs):
+        return pickle.loads(src.read_bytes(), **kwargs)
+
+    def dump(self, src: FileInfo, obj, kwargs):
+        return src.write_bytes(pickle.dumps(obj, **kwargs), append=False)

69f67434043e84933540a6a819b8feae7167eac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:45 +0000

    Add package popular-times/0.1/popular_times-0.1.tar.gz

---
+++ packages/popular-times/popular_times-0.1.tar.gz/popular_times-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='popular_times',
+      version='0.1',
+      description='This package allows for retrieval of Google Popular Times data.',
+      url='http://github.com/apollojain/popular_times',
+      author='Adawgrhinopower',
+      author_email='apollojain@berkeley.edu',
+      license='MIT',
+      packages=['popular_times'],
+      zip_safe=False)

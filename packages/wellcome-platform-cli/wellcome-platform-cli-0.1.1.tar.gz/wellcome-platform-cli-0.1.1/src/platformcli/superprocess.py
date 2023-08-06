266d95d87a0bdf04aaf6709cc48216489f2ef216
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:55 +0000

    Add package wellcome-platform-cli/0.1.1/wellcome-platform-cli-0.1.1.tar.gz

---
+++ packages/wellcome-platform-cli/wellcome-platform-cli-0.1.1.tar.gz/wellcome-platform-cli-0.1.1/src/platformcli/superprocess.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8
+"""
+Like subprocess, but better.
+"""
+
+import subprocess
+import sys
+
+
+def check_call(*args):
+    print('*** Running command %r' % args)
+    try:
+        subprocess.check_call(args)
+    except subprocess.CalledProcessError as err:
+        sys.exit(err.exitcode)
+
+
+def check_output(*args):
+    print('*** Running command %r' % args)
+    try:
+        return subprocess.check_output(args).decode('utf8').strip()
+    except subprocess.CalledProcessError as err:
+        sys.exit(err.exitcode)

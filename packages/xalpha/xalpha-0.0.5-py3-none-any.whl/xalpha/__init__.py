49da67bf6e48a0b23970cb133f490159c8026d70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:20 +0000

    Add package xalpha/0.0.5/xalpha-0.0.5-py3-none-any.whl

---
+++ packages/xalpha/xalpha-0.0.5-py3-none-any.whl/xalpha/__init__.py
@@ -0,0 +1,11 @@
+__version__ = '0.0.5'
+__author__ = 'refraction-ray'
+__name__ = 'xalpha'
+
+from xalpha.record import record
+from xalpha.info import fundinfo, indexinfo, cashinfo
+from xalpha.evaluate import evaluate
+from xalpha.trade import trade
+from xalpha.multiple import mul, mulfix
+import xalpha.policy
+

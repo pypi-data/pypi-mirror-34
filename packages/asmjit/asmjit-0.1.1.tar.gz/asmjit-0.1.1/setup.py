8b089d2491525e9eb3aae0ed95f7782cb270ea43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:19 +0000

    Add package asmjit/0.1.1/asmjit-0.1.1.tar.gz

---
+++ packages/asmjit/asmjit-0.1.1.tar.gz/asmjit-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='asmjit',
+    version='0.1.1',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    url='',
+    license='AGPLv3',
+    author='Julian Hammer',
+    author_email='julian.hammer@u-sys.org',
+    description='A Benchmark Toolkit for Assembly Instructions Using the LLVM JIT',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    install_requires=['llvmlite>=0.23.2', 'psutil'],
+)

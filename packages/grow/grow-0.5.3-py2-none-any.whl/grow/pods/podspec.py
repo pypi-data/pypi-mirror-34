7933236a03948845798b092a368d2d6ff96580d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:05 +0000

    Add package grow/0.5.3/grow-0.5.3-py2-none-any.whl

---
+++ packages/grow/grow-0.5.3-py2-none-any.whl/grow/pods/podspec.py
@@ -0,0 +1,49 @@
+"""Podspec helper."""
+
+from grow.translations import locales
+
+
+class Error(Exception):
+    pass
+
+
+class PodSpecParseError(Error):
+    pass
+
+
+class PodSpec(object):
+
+    def __init__(self, yaml, pod):
+        yaml = yaml or {}
+        self.yaml = yaml
+        self.pod = pod
+        self.grow_version = yaml.get('grow_version')
+        _default_locale = yaml.get('localization', {}).get('default_locale', None)
+        self.default_locale = locales.Locale.parse(_default_locale)
+        self.fields = yaml
+
+    def get_config(self):
+        return self.yaml
+
+    def __getattr__(self, name):
+        if name in self.fields:
+            return self.fields[name]
+        tagged_name = '{}@'.format(name)
+        if tagged_name in self.fields:
+            return self.fields[tagged_name]
+        return object.__getattribute__(self, name)
+
+    def __iter__(self):
+        return self.yaml.__iter__()
+
+    @property
+    def home(self):
+        return self.pod.get_home_doc()
+
+    @property
+    def root(self):
+        return self.fields.get('root', '')
+
+    @property
+    def localization(self):
+        return self.fields.get('localization')

486b4bd987fc4283ef988579616cd29bcb082cc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:28 +0000

    Add package seglearn/1.0.0/seglearn-1.0.0-py3-none-any.whl

---
+++ packages/seglearn/seglearn-1.0.0-py3-none-any.whl/seglearn/tests/test_util.py
@@ -0,0 +1,23 @@
+# Author: David Burns
+# License: BSD
+
+from seglearn.datasets import load_watch
+from seglearn.base import TS_Data
+from seglearn import util
+
+def test_util():
+    df = load_watch()
+
+    data = TS_Data(df['X'], df['side'])
+    util.get_ts_data_parts(data)
+
+    util.check_ts_data(data, df['y'])
+    util.check_ts_data(df['X'], df['y'])
+
+    util.ts_stats(df['X'], df['y'], fs = 1., class_labels=df['y_labels'])
+
+
+
+
+
+

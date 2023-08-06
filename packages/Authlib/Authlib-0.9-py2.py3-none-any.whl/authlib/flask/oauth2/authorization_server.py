00fb6c317a4a1cb8420e11a71e27ecf7614eb510
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:22 +0000

    Add package Authlib/0.9/Authlib-0.9-py2.py3-none-any.whl

---
+++ packages/Authlib/Authlib-0.9-py2.py3-none-any.whl/authlib/flask/oauth2/authorization_server.py
@@ -0,0 +1,275 @@
+from werkzeug.utils import import_string
+from flask import Response, json
+from flask import request as flask_req
+from authlib.specs.rfc6749 import (
+    OAuth2Request,
+    AuthorizationServer as _AuthorizationServer,
+)
+from authlib.specs.rfc6749 import ClientAuthentication
+from authlib.specs.rfc6750 import BearerToken
+from authlib.common.security import generate_token
+from authlib.common.encoding import to_unicode
+from authlib.deprecate import deprecate
+from .signals import client_authenticated, token_revoked
+
+GRANT_TYPES_EXPIRES = {
+    'authorization_code': 864000,
+    'implicit': 3600,
+    'password': 864000,
+    'client_credentials': 864000
+}
+
+
+class AuthorizationServer(_AuthorizationServer):
+    """Flask implementation of :class:`authlib.rfc6749.AuthorizationServer`.
+    Initialize it with ``query_client``, ``save_token`` methods and Flask
+    app instance::
+
+        def query_client(client_id):
+            return Client.query.filter_by(client_id=client_id).first()
+
+        def save_token(token, request):
+            if request.user:
+                user_id = request.user.get_user_id()
+            else:
+                user_id = None
+            client = request.client
+            tok = Token(
+                client_id=client.client_id,
+                user_id=user.get_user_id(),
+                **token
+            )
+            db.session.add(tok)
+            db.session.commit()
+
+        server = AuthorizationServer(app, query_client, save_token)
+        # or initialize lazily
+        server = AuthorizationServer()
+        server.init_app(app, query_client, save_token)
+    """
+    def __init__(self, app=None, query_client=None, save_token=None, **config):
+        super(AuthorizationServer, self).__init__(
+            query_client, None, save_token, **config)
+
+        # register hooks
+        def after_authenticate_client(client, grant):
+            client_authenticated.send(self, client=client, grant=grant)
+
+        def after_revoke_token(token, client):
+            token_revoked.send(self, token=token, client=client)
+
+        self.register_hook(
+            'after_authenticate_client',
+            after_authenticate_client
+        )
+        self.register_hook('after_revoke_token', after_revoke_token)
+
+        self.app = app
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app, query_client=None, save_token=None):
+        """Initialize later with Flask app instance."""
+        if query_client is not None:
+            self.query_client = query_client
+            self.authenticate_client = ClientAuthentication(query_client)
+        if save_token is not None:
+            self.save_token = save_token
+
+        self.app = app
+        self.generate_token = self.create_bearer_token_generator(app)
+        if app.config.get('OAUTH2_JWT_ENABLED'):
+            self.init_jwt_config(app)
+
+    def init_jwt_config(self, app):
+        """Initialize JWT related configuration."""
+        jwt_iss = app.config.get('OAUTH2_JWT_ISS')
+        if not jwt_iss:
+            raise RuntimeError('Missing "OAUTH2_JWT_ISS" configuration.')
+        jwt_key_path = app.config.get('OAUTH2_JWT_KEY_PATH')
+        if jwt_key_path:
+            with open(jwt_key_path, 'r') as f:
+                if jwt_key_path.endswith('.json'):
+                    jwt_key = json.load(f)
+                else:
+                    jwt_key = to_unicode(f.read())
+        else:
+            jwt_key = app.config.get('OAUTH2_JWT_KEY')
+
+        if not jwt_key:
+            raise RuntimeError('Missing "OAUTH2_JWT_KEY" configuration.')
+
+        jwt_alg = app.config.get('OAUTH2_JWT_ALG')
+        if not jwt_alg:
+            raise RuntimeError('Missing "OAUTH2_JWT_ALG" configuration.')
+
+        jwt_exp = app.config.get('OAUTH2_JWT_EXP', 3600)
+        self.config.setdefault('jwt_iss', jwt_iss)
+        self.config.setdefault('jwt_key', jwt_key)
+        self.config.setdefault('jwt_alg', jwt_alg)
+        self.config.setdefault('jwt_exp', jwt_exp)
+
+    def get_translations(self):
+        # TODO: try Flask-Babel and Flask-Babelex
+        return None
+
+    def get_error_uris(self):
+        error_uris = self.app.config.get('OAUTH2_ERROR_URIS')
+        if error_uris:
+            return dict(error_uris)
+
+    def create_token_expires_in_generator(self, app):
+        """Create a generator function for generating ``expires_in`` value.
+        Developers can re-implement this method with a subclass if other means
+        required. The default expires_in value is defined by ``grant_type``,
+        different ``grant_type`` has different value. It can be configured
+        with::
+
+            OAUTH2_TOKEN_EXPIRES_IN = {
+                'authorization_code': 864000,
+                'urn:ietf:params:oauth:grant-type:jwt-bearer': 3600,
+            }
+        """
+        expires_conf = {}
+        expires_conf.update(GRANT_TYPES_EXPIRES)
+
+        _old_expires = app.config.get('OAUTH2_EXPIRES_IN')
+        if _old_expires:  # pragma: no cover
+            deprecate('Deprecate "OAUTH2_EXPIRES_IN".', '0.11', 'vhL75', 'ae')
+            expires_conf.update(_old_expires)
+        expires_conf.update(app.config.get('OAUTH2_TOKEN_EXPIRES_IN', {}))
+
+        def expires_in(client, grant_type):
+            conf_key = 'OAUTH2_EXPIRES_{}'.format(grant_type.upper())
+            expires = app.config.get(conf_key)
+            if expires:  # pragma: no cover
+                deprecate('Deprecate "{}".'.format(conf_key), '0.10', 'vpCH5', 'as')
+                return expires
+            return expires_conf.get(grant_type, BearerToken.DEFAULT_EXPIRES_IN)
+
+        return expires_in
+
+    def create_bearer_token_generator(self, app):
+        """Create a generator function for generating ``token`` value. This
+        method will create a Bearer Token generator with
+        :class:`authlib.specs.rfc6750.BearerToken`. By default, it will not
+        generate ``refresh_token``, which can be turn on by configuration
+        ``OAUTH2_REFRESH_TOKEN_GENERATOR=True``.
+        """
+        access_token_generator = app.config.get(
+            'OAUTH2_ACCESS_TOKEN_GENERATOR', True)
+
+        if isinstance(access_token_generator, str):
+            access_token_generator = import_string(access_token_generator)
+        elif not callable(access_token_generator):
+            def access_token_generator(**kwargs):
+                return generate_token(42)
+
+        refresh_token_generator = app.config.get(
+            'OAUTH2_REFRESH_TOKEN_GENERATOR', False)
+        if isinstance(refresh_token_generator, str):
+            refresh_token_generator = import_string(refresh_token_generator)
+        elif refresh_token_generator is True:
+            def refresh_token_generator(**kwargs):
+                return generate_token(48)
+        elif not callable(refresh_token_generator):
+            refresh_token_generator = None
+
+        if hasattr(self, 'create_expires_generator'):  # pragma: no cover
+            deprecate('Method `create_expires_generator` has been renamed', '0.10', 'vhL75', 'ceg')
+            expires_generator = self.create_expires_generator(app)
+        else:
+            expires_generator = self.create_token_expires_in_generator(app)
+        return BearerToken(
+            access_token_generator,
+            refresh_token_generator,
+            expires_generator
+        )
+
+    def validate_consent_request(self, request=None, end_user=None):
+        """Validate current HTTP request for authorization page. This page
+        is designed for resource owner to grant or deny the authorization::
+
+            @app.route('/authorize', methods=['GET'])
+            def authorize():
+                try:
+                    grant = server.validate_consent_request(end_user=current_user)
+                    return render_template(
+                        'authorize.html',
+                        grant=grant,
+                        user=current_user
+                    )
+                except OAuth2Error as error:
+                    return render_template(
+                        'error.html',
+                        error=error
+                    )
+        """
+        req = _create_oauth2_request(request)
+        grant = self.get_authorization_grant(req)
+        grant.validate_authorization_request()
+        if hasattr(grant, 'validate_prompt'):
+            # prompt is designed for OpenID Connect
+            grant.validate_prompt(end_user=end_user)
+        if not hasattr(grant, 'prompt'):
+            grant.prompt = None
+        return grant
+
+    def create_authorization_response(self, request=None, grant_user=None):
+        """Create the HTTP response for authorization. If resource owner
+        granted the authorization, pass the resource owner as the user
+        parameter, otherwise None::
+
+            @app.route('/authorize', methods=['POST'])
+            def confirm_authorize():
+                if request.form['confirm'] == 'ok':
+                    grant_user = current_user
+                else:
+                    grant_user = None
+                return server.create_authorization_response(grant_user=grant_user)
+        """
+        if request and not grant_user:  # pragma: no cover
+            grant_user = request
+            request = None
+
+        status, body, headers = self.create_valid_authorization_response(
+            _create_oauth2_request(request),
+            grant_user=grant_user
+        )
+        if isinstance(body, dict):
+            body = json.dumps(body)
+        return Response(body, status=status, headers=headers)
+
+    def create_token_response(self, request=None):
+        """Create the HTTP response for token endpoint. It is ready to use, as
+        simple as::
+
+            @app.route('/token', methods=['POST'])
+            def issue_token():
+                return server.create_token_response()
+        """
+        req = _create_oauth2_request(request)
+        status, body, headers = super(
+            AuthorizationServer, self).create_token_response(req)
+        return Response(json.dumps(body), status=status, headers=headers)
+
+    def create_endpoint_response(self, name, request=None):
+        req = _create_oauth2_request(request)
+        status, body, headers = super(
+            AuthorizationServer, self).create_endpoint_response(name, req)
+        return Response(json.dumps(body), status=status, headers=headers)
+
+
+def _create_oauth2_request(request):
+    q = request or flask_req
+    if q.method == 'POST':
+        body = q.form.to_dict(flat=True)
+    else:
+        body = None
+
+    return OAuth2Request(
+        q.method,
+        q.url,
+        body,
+        q.headers
+    )
---
+++ packages/Authlib/Authlib-0.9-py2.py3-none-any.whl/authlib/specs/rfc6749/errors.py
@@ -0,0 +1,232 @@
+"""
+    authlib.specs.rfc6749.errors
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Implementation for OAuth 2 Error Response. A basic error has
+    parameters:
+
+    error
+         REQUIRED.  A single ASCII [USASCII] error code.
+
+    error_description
+         OPTIONAL.  Human-readable ASCII [USASCII] text providing
+         additional information, used to assist the client developer in
+         understanding the error that occurred.
+
+    error_uri
+         OPTIONAL.  A URI identifying a human-readable web page with
+         information about the error, used to provide the client
+         developer with additional information about the error.
+         Values for the "error_uri" parameter MUST conform to the
+         URI-reference syntax and thus MUST NOT include characters
+         outside the set %x21 / %x23-5B / %x5D-7E.
+
+    state
+         REQUIRED if a "state" parameter was present in the client
+         authorization request.  The exact value received from the
+         client.
+
+    https://tools.ietf.org/html/rfc6749#section-5.2
+
+    :copyright: (c) 2017 by Hsiaoming Yang.
+"""
+from authlib.common.errors import AuthlibHTTPError
+from authlib.common.security import is_secure_transport
+from authlib.deprecate import deprecate
+
+__all__ = [
+    'register_error_uri', 'OAuth2Error',
+    'InsecureTransportError', 'InvalidRequestError',
+    'InvalidClientError', 'InvalidGrantError',
+    'UnauthorizedClientError', 'UnsupportedGrantTypeError',
+    'InvalidScopeError', 'AccessDeniedError',
+    'MissingAuthorizationError', 'UnsupportedTokenTypeError',
+    'MissingCodeException', 'MissingTokenException',
+    'MissingTokenTypeException', 'MismatchingStateException',
+]
+
+
+_error_uris = {}
+
+
+def register_error_uri(error, error_uri):
+    """Register ``error_uri`` for each error code. When raise an OAuth2Error
+    without ``error_uri``, it will use the URI in this registry::
+
+        register_error_uri('invalid_client', 'https://example.com/errors#invalid-client')
+
+    :param error: OAuth 2 error code.
+    :param error_uri: A human-readable web page with information about the error.
+    """
+    global _error_uris
+    _error_uris[error] = error_uri
+    deprecate('This function is deprecated.', version='0.10')
+
+
+class OAuth2Error(AuthlibHTTPError):
+    def __init__(self, description=None, uri=None,
+                 status_code=None, state=None):
+        if uri is None:
+            uri = _error_uris.get(self.error)
+        super(OAuth2Error, self).__init__(None, description, uri, status_code)
+        self.state = state
+
+    def get_body(self):
+        """Get a list of body."""
+        error = super(OAuth2Error, self).get_body()
+        if self.state:
+            error.append(('state', self.state))
+        return error
+
+
+class InsecureTransportError(OAuth2Error):
+    error = 'insecure_transport'
+
+    def get_error_description(self):
+        return self.gettext('OAuth 2 MUST utilize https.')
+
+    @classmethod
+    def check(cls, uri):
+        """Check and raise InsecureTransportError with the given URI."""
+        if not is_secure_transport(uri):
+            raise cls()
+
+
+class InvalidRequestError(OAuth2Error):
+    """The request is missing a required parameter, includes an
+    unsupported parameter value (other than grant type),
+    repeats a parameter, includes multiple credentials,
+    utilizes more than one mechanism for authenticating the
+    client, or is otherwise malformed.
+
+    https://tools.ietf.org/html/rfc6749#section-5.2
+    """
+    error = 'invalid_request'
+
+
+class InvalidClientError(OAuth2Error):
+    """Client authentication failed (e.g., unknown client, no
+    client authentication included, or unsupported
+    authentication method).  The authorization server MAY
+    return an HTTP 401 (Unauthorized) status code to indicate
+    which HTTP authentication schemes are supported.  If the
+    client attempted to authenticate via the "Authorization"
+    request header field, the authorization server MUST
+    respond with an HTTP 401 (Unauthorized) status code and
+    include the "WWW-Authenticate" response header field
+    matching the authentication scheme used by the client.
+
+    https://tools.ietf.org/html/rfc6749#section-5.2
+    """
+    error = 'invalid_client'
+    status_code = 400
+
+    def get_headers(self):
+        headers = super(InvalidClientError, self).get_headers()
+        if self.status_code == 401:
+            error_description = self.get_error_description()
+            # safe escape
+            error_description = error_description.replace('"', '|')
+            extras = [
+                'error="{}"'.format(self.error),
+                'error_description="{}"'.format(error_description)
+            ]
+            headers.append(
+                ('WWW-Authenticate', 'Basic ' + ', '.join(extras))
+            )
+        return headers
+
+
+class InvalidGrantError(OAuth2Error):
+    """The provided authorization grant (e.g., authorization
+    code, resource owner credentials) or refresh token is
+    invalid, expired, revoked, does not match the redirection
+    URI used in the authorization request, or was issued to
+    another client.
+
+    https://tools.ietf.org/html/rfc6749#section-5.2
+    """
+    error = 'invalid_grant'
+
+
+class UnauthorizedClientError(OAuth2Error):
+    """ The authenticated client is not authorized to use this
+    authorization grant type.
+
+    https://tools.ietf.org/html/rfc6749#section-5.2
+    """
+    error = 'unauthorized_client'
+
+
+class UnsupportedGrantTypeError(OAuth2Error):
+    """The authorization grant type is not supported by the
+    authorization server.
+
+    https://tools.ietf.org/html/rfc6749#section-5.2
+    """
+    error = 'unsupported_grant_type'
+
+
+class InvalidScopeError(OAuth2Error):
+    """The requested scope is invalid, unknown, malformed, or
+    exceeds the scope granted by the resource owner.
+
+    https://tools.ietf.org/html/rfc6749#section-5.2
+    """
+    error = 'invalid_scope'
+
+    def get_error_description(self):
+        return self.gettext(
+            'The requested scope is invalid, unknown, or malformed.')
+
+
+class AccessDeniedError(OAuth2Error):
+    """The resource owner or authorization server denied the request.
+
+    Used in authorization endpoint for "code" and "implicit". Defined in
+    `Section 4.1.2.1`_.
+
+    .. _`Section 4.1.2.1`: https://tools.ietf.org/html/rfc6749#section-4.1.2.1
+    """
+    error = 'access_denied'
+
+    def get_error_description(self):
+        return self.gettext(
+            'The resource owner or authorization server denied the request')
+
+
+# -- below are extended errors -- #
+
+
+class MissingAuthorizationError(OAuth2Error):
+    error = 'missing_authorization'
+
+    def get_error_description(self):
+        return self.gettext('Missing "Authorization" in headers.')
+
+
+class UnsupportedTokenTypeError(OAuth2Error):
+    error = 'unsupported_token_type'
+
+
+# -- exceptions for clients -- #
+
+
+class MissingCodeException(OAuth2Error):
+    error = 'missing_code'
+    description = 'Missing "code" in response.'
+
+
+class MissingTokenException(OAuth2Error):
+    error = 'missing_token'
+    description = 'Missing "access_token" in response.'
+
+
+class MissingTokenTypeException(OAuth2Error):
+    error = 'missing_token_type'
+    description = 'Missing "token_type" in response.'
+
+
+class MismatchingStateException(OAuth2Error):
+    error = 'mismatching_state'
+    description = 'CSRF Warning! State not equal in request and response.'

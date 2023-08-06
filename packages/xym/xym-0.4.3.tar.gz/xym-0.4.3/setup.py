1cb377e72cdd0317e0eba008d0299ba6b1a4c024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:37:48 +0000

    Add package xym/0.4.3/xym-0.4.3.tar.gz

---
+++ packages/xym/xym-0.4.3.tar.gz/xym-0.4.3/setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup
+from xym import xym
+
+def read(fname):
+	return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+  name = 'xym',
+  version = xym.__version__,
+  description = ('A tool to fetch and extract YANG modules from IETF RFCs and Drafts'),
+  long_description = """xym is a simple tool for fetching and extracting YANG modules from IETF RFCs and drafts as local files and from URLs.""",
+  packages = ['xym'],
+  scripts = ['bin/xym'],
+  author = 'Jan Medved',
+  author_email = 'jmedved@cisco.com',
+  license = 'New-style BSD',
+  url = 'https://github.com/xym-tool/xym',
+  install_requires = ['requests>=2.6'],
+  include_package_data = True,
+  keywords = ['yang', 'extraction'],
+  classifiers = [],
+)

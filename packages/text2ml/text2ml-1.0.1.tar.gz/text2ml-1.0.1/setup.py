8296c34af705ac96b8a97c56b2e1575b1c24db23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:35 +0000

    Add package text2ml/1.0.1/text2ml-1.0.1.tar.gz

---
+++ packages/text2ml/text2ml-1.0.1.tar.gz/text2ml-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='text2ml',
+    version='1.0.1',
+    packages=[''],
+    url='https://github.com/marminino/text2ml',
+    author='Alisson Lauffer',
+    author_email='alissonvitortc@gmail.com',
+    description='A module for Telegram Bot API that can format text + entities into formatted text'
+)

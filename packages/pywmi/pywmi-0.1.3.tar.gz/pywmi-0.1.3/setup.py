d4229e3b91c3bc4dfcc76c3b6d4782e4b82e80f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:38 +0000

    Add package pywmi/0.1.3/pywmi-0.1.3.tar.gz

---
+++ packages/pywmi/pywmi-0.1.3.tar.gz/pywmi-0.1.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pywmi',
+    version='0.1.3',
+    description='Essential tools and interfaces for WMI',
+    url='http://github.com/samuelkolb/pywmi',
+    author='Samuel Kolb',
+    author_email='samuel.kolb@me.com',
+    license='MIT',
+    packages=['pywmi'],
+    zip_safe=False,
+    install_requires=['pysmt', 'numpy']
+)

fe317c47c6a46c24a47e9681402bea7f7b031fe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:43 +0000

    Add package djrad/1.2.3/djrad-1.2.3.tar.gz

---
+++ packages/djrad/djrad-1.2.3.tar.gz/djrad-1.2.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+readme = "Django-JSONRPC-REST-API-Decorators (DJRAD)"
+
+setup(
+    name='djrad',
+    version='1.2.3',
+    description='Decorators for JSONRPC and REST API on django',
+    long_description=readme,
+    author='Divar submit team - AmirMohammad Dehghan',
+    author_email='dehghan@mit.edu',
+    url='https://github.com/amirmd76/djrad.git',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+          'django', 'jsonschema>=2.6.0'
+      ]
+)

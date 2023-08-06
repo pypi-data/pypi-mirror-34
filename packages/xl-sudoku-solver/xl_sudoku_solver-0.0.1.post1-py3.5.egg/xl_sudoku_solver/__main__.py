dc78976c4b55b35fbf66b98c5a3f78e545ce304d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:20 +0000

    Add package xl-sudoku-solver/0.0.1.post1/xl_sudoku_solver-0.0.1.post1-py3.5.egg

---
+++ packages/xl-sudoku-solver/xl_sudoku_solver-0.0.1.post1-py3.5.egg/xl_sudoku_solver/__main__.py
@@ -0,0 +1,27 @@
+from .solver import Solver
+import os, sys, argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file',
+        help='The file in which a string format of a Soduku problem is contained')
+    details = parser.add_mutually_exclusive_group()
+    details.add_argument('-t', '--time', action='store_true', help='Print cost time')
+    # parser.add_argument('-v', '--verbose', action='store_true', help='Give some detail infomation')
+    args = parser.parse_args()
+    if args.file:
+        with open(os.path.join(os.getcwd(), args.file), 'r') as f:
+            problem = f.read()
+    else:
+        print('Please type the problem in:')
+        problem = sys.stdin.read()
+
+    process = Solver.solve(Solver.load(problem))
+    process.draw()
+    if args.time:
+        print('Cost: {}s'.format(process['cost']))
+        
+    return 0
+
+if __name__ == '__main__':
+    main()

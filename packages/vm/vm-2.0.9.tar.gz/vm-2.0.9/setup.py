6184affacfa0366cd6472780f2598266aa92b423
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:25 +0000

    Add package vm/2.0.9/vm-2.0.9.tar.gz

---
+++ packages/vm/vm-2.0.9.tar.gz/vm-2.0.9/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('requirements.txt') as requirements_file:
+    requirements = requirements_file.read()  # type: str
+    install_requires = list(filter(
+        lambda it: it,
+        requirements.splitlines()))
+
+packages = find_packages()
+
+setup(
+    name='vm',
+    version='2.0.9',
+    url='https://github.com/bmustiata/version-manager-py',
+    entry_points={
+        "console_scripts": [
+            "version-manager = version_manager.launcher:launch",
+            "vm = version_manager.launcher:launch",
+        ]
+    },
+    description='vm',
+    long_description=readme,
+    author='Bogdan Mustiata',
+    author_email='bogdan.mustiata@gmail.com',
+    license='BSD',
+    install_requires=install_requires,
+    packages=packages
+)
+

f92b246224cc9b30dd92ad8f4939d0bcabdfc059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:22 +0000

    Add package django-easy-validator/1.1/django-easy-validator-1.1.tar.gz

---
+++ packages/django-easy-validator/django-easy-validator-1.1.tar.gz/django-easy-validator-1.1/tests/__init__.py
@@ -0,0 +1,7 @@
+# PROJECT : django-easy-validator
+# TIME    : 18-1-2 上午9:44
+# AUTHOR  : 申延刚 <Younger Shen>
+# EMAIL   : younger.shen@hotmail.com
+# PHONE   : 13811754531
+# WECHAT  : 13811754531
+# WEBSITE : www.punkcoder.cn

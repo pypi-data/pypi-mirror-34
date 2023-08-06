747f51e03f84facfa2a0286fa1d60d4dd3d570fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:08 +0000

    Add package sphinx-advanced/0.3.0/sphinx_advanced-0.3.0-py2.py3-none-any.whl

---
+++ packages/sphinx-advanced/sphinx_advanced-0.3.0-py2.py3-none-any.whl/sphinx_advanced/calculator.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+"""Calculator module
+
+.. module:: sphinx_advanced.calculator
+    :platform: OS X
+    :synopsis: sphinx advanced tutorial project - calculator
+
+.. moduleauthor:: Julio Antúnez Tarín <julio.antunez.tarin@gmail.com>
+
+Usage:
+    from sphinx_advanced import calculator
+"""
+
+
+def addition(first, second):
+    """ Addition operation
+
+    :param first: first operator
+    :param second: second operator
+
+    Returns:
+        The result of an addition operation
+    """
+    return first + second
+
+
+def substraction(first, second):
+    """Substraction operation
+
+    :param first: first operator
+    :param second: second operator
+
+    Returns:
+        The result of a substraction operation
+    """
+    return first - second
+
+
+def multiplication(first, second):
+    """Multiplication operation
+
+    :param first: first operator
+    :param second: second operator
+
+    Returns:
+        The result of a multiplication operation
+    """
+    return first * second
+
+
+def division(first, second):
+    """Division operation
+
+    :param first: first operator
+    :param second: second operator
+
+    Returns:
+        The result of a division operation
+    """
+    return first / second
+
+
+def exponential(first, second):
+    """Exponential operation
+
+    :param first: first operator
+    :param second: second operator
+
+    Returns:
+        The result of a exponential operation
+    """
+    return first ** second
---
+++ packages/sphinx-advanced/sphinx_advanced-0.3.0-py2.py3-none-any.whl/sphinx_advanced/sphinx_advanced.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+"""Main module
+
+.. note:: Proof of Concept
+
+.. module:: sphinx_advanced.sphinx_advanced
+    :platform: OS X
+    :synopsis: sphinx advanced tutorial project
+
+.. moduleauthor:: Julio Antúnez Tarín <julio.antunez.tarin@gmail.com>
+
+Usage:
+    from sphinx_advanced import sphinx_advanced
+"""
+
+
+def help():
+    """Sphinx Advanced Module help
+
+    .. todo:: Add some subpackage
+
+    Args:
+        none
+
+    Retuns:
+        A brief description of the module
+
+    """
+    return 'Sphinx Advanced Tutorial Module'
+
+
+def init():
+    """Print ``help()`` when run as a script.
+
+    Args:
+        none
+
+    Retuns:
+        :func:`sphinx_advanced.sphinx_advanced.help()`
+    """
+    if __name__ == "__main__":
+        print(help())
+
+
+init()

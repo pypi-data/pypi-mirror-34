3595f8bc84856b6362760b35e9f5f5bcf522590b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:12 +0000

    Add package Python-CAN-Viewer/0.2.0/Python_CAN_Viewer-0.2.0-py3-none-any.whl

---
+++ packages/Python-CAN-Viewer/Python_CAN_Viewer-0.2.0-py3-none-any.whl/python_can_viewer/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# coding: utf-8
+#
+# Copyright (C) 2018 Kristian Sloth Lauszus. All rights reserved.
+#
+# Contact information
+# -------------------
+# Kristian Sloth Lauszus
+# Web      :  http://www.lauszus.com
+# e-mail   :  lauszus@gmail.com
+
+__version__ = '0.2.0'
+
+from .python_can_viewer import *

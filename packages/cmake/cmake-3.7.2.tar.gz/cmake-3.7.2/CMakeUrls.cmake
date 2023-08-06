b4fe21303ef47767856a7b213bb1ca3635787707
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:52 +0000

    Add package cmake/3.7.2/cmake-3.7.2.tar.gz

---
+++ packages/cmake/cmake-3.7.2.tar.gz/cmake-3.7.2/CMakeUrls.cmake
@@ -0,0 +1,26 @@
+
+#-----------------------------------------------------------------------------
+# CMake sources
+set(unix_source_url       "https://cmake.org/files/v3.7/cmake-3.7.2.tar.gz")
+set(unix_source_sha256    "dc1246c4e6d168ea4d6e042cfba577c1acd65feea27e56f5ff37df920c30cae0")
+
+set(windows_source_url    "https://cmake.org/files/v3.7/cmake-3.7.2.zip")
+set(windows_source_sha256 "36672d4fdf01957ab41635689a501bbe280d5cd4475e2736b631d1510cae528c")
+
+#-----------------------------------------------------------------------------
+# CMake binaries
+
+set(linux32_binary_url    "NA")  # Linux 32-bit binaries not available
+set(linux32_binary_sha256 "NA")
+
+set(linux64_binary_url    "https://cmake.org/files/v3.7/cmake-3.7.2-Linux-x86_64.tar.gz")
+set(linux64_binary_sha256 "0e6ec35d4fa9bf79800118916b51928b6471d5725ff36f1d0de5ebb34dcd5406")
+
+set(macosx_binary_url    "https://cmake.org/files/v3.7/cmake-3.7.2-Darwin-x86_64.tar.gz")
+set(macosx_binary_sha256 "0175e97748052dfc15ebd3c0aa65286e5ec20ca22ed606ce88940e699496b03c")
+
+set(win32_binary_url    "https://cmake.org/files/v3.7/cmake-3.7.2-win32-x86.zip")
+set(win32_binary_sha256 "ec5e299d412e0272e01d4de5bf07718f42c96361f83d51cc39f91bf49cc3e5c3")
+
+set(win64_binary_url    "https://cmake.org/files/v3.7/cmake-3.7.2-win64-x64.zip")
+set(win64_binary_sha256 "def3bb81dfd922ce1ea2a0647645eefb60e128d520c8ca707c5996c331bc8b48")

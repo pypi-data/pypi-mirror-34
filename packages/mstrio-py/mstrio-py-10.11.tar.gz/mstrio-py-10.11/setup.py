782317e2e4d50b51a462d24aced4e3228a8c5124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:19 +0000

    Add package mstrio-py/10.11/mstrio-py-10.11.tar.gz

---
+++ packages/mstrio-py/mstrio-py-10.11.tar.gz/mstrio-py-10.11/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='mstrio-py',
+      version='10.11',
+      packages=['mstrio', 'mstrio.api', 'mstrio.utils'],
+      description='Python interface for the MicroStrategy REST API',
+      license='Apache License 2.0',
+      url='https://github.com/MicroStrategy/mstrio-py',
+      author='Scott Rigney, Peter Ott, Sergio Sainz Palacios, Alex Fernandes',
+      author_email='srigney@microstrategy.com, pott@microstrategy.com, '
+                   'ssainz@microstrategy.com, afernandes@microstrategy.com',
+      install_requires=[
+          'requests',
+          'pandas'
+      ]
+)

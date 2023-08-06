ec0e4f6d7bbcc82155abbb31193190832de89420
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:02 +0000

    Add package score.jsapi/0.4.8/score.jsapi-0.4.8.tar.gz

---
+++ packages/score.jsapi/score.jsapi-0.4.8.tar.gz/score.jsapi-0.4.8/score/jsapi/exc2json.py
@@ -0,0 +1,38 @@
+import traceback
+
+
+def exc2json(excinfo, untrace=[]):
+    """
+    Converts exception info (as returned by :func:`sys.exc_info`) into a
+    3-tuple that can be converted into a json string by python's :mod:`json`
+    library. It will consist of the exception name, the message and the stack
+    trace as provided by :func:`traceback.extract_tb`::
+
+        {
+            type: 'ZeroDivisionError',
+            message: 'division by zero',
+            trace: [
+                [<filename>, <lineno>, <line>],
+                ...
+            ]
+        }
+
+    It is possible to omit the last value of *excinfo*, effectively reducing the
+    generated exception description to a class name and a message.
+
+    The optional parameter *untrace* contains file names that will be removed
+    from the beginning of the stack trace.
+    """
+    trace = None
+    if len(excinfo) > 2:
+        trace = traceback.extract_tb(excinfo[2])
+        untrace.append(__file__)
+        while trace and any(skip for skip in untrace if skip in trace[0][0]):
+            trace = trace[1:]
+        trace = list(map(lambda frame: (frame[0], frame[1], frame[2], frame[3]),
+                         trace))
+    return {
+        'type': excinfo[0].__name__,
+        'message': str(excinfo[1]),
+        'trace': trace,
+    }
---
+++ packages/score.jsapi/score.jsapi-0.4.8.tar.gz/score.jsapi-0.4.8/setup.py
@@ -0,0 +1,53 @@
+import os
+
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.rst')) as f:
+    README = f.read()
+
+setup(
+    name='score.jsapi',
+    version='0.4.8',
+    description='Javascript API generator of The SCORE Framework',
+    long_description=README,
+    author='strg.at',
+    author_email='score@strg.at',
+    url='http://score-framework.org',
+    keywords='score framework web javascript rpc api',
+    packages=['score', 'score.jsapi'],
+    namespace_packages=['score'],
+    package_dir={
+        'score.jsapi': 'score/jsapi',
+    },
+    package_data={
+        'score.jsapi': [
+            'js/endpoint/url.js',
+            'js/endpoint.js',
+            'js/exception.js',
+            'js/excformat.js',
+            'js/queue.js',
+            'js/unified.js',
+        ]
+    },
+    zip_safe=False,
+    license='LGPL',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Pyramid',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General '
+            'Public License v3 or later (LGPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: JavaScript',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
+    install_requires=[
+        'score.init >= 0.3',
+        'score.ctx >= 0.3',
+    ],
+)

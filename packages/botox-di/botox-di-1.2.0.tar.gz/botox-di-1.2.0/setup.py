13f7e970cdc0290d6774353cfd1376b1f85894f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:25 +0000

    Add package botox-di/1.2.0/botox-di-1.2.0.tar.gz

---
+++ packages/botox-di/botox-di-1.2.0.tar.gz/botox-di-1.2.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='botox-di',
+    description='The Botox is a lightweight dependency injection pattern implementation for Python.',
+    version='1.2.0',
+    author='Ilya Lebedev',
+    author_email='lebedev.games.mail@gmail.com',
+    license='MIT',
+    classifiers=[
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 3.7'
+    ],
+    packages=['botox']
+)

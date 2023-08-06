8eccb69b31d803902fbab9450cd3bc00248e17f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:53 +0000

    Add package spectrum-client/0.1.2/spectrum-client-0.1.2.tar.gz

---
+++ packages/spectrum-client/spectrum-client-0.1.2.tar.gz/spectrum-client-0.1.2/setup.py
@@ -0,0 +1,42 @@
+# pylint: disable=C0111
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    README = fh.read()
+
+setup(
+    name='spectrum-client',
+    version='0.1.2',
+    description='CA Spectrum Web Services API wrapper',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    author='Renato Orgito',
+    author_email='orgito@gmail.com',
+    maintainer='Renato Orgito',
+    maintainer_email='orgito@gmail.com',
+    url='https://github.com/orgito/spectrum-client',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Monitoring',
+        'Topic :: System :: Networking :: Monitoring',
+    ],
+    keywords='spectrum-client network monitoring spectrum',
+    packages=['spectrum_client'],
+    setup_requires=['setuptools>=38.6.0'],
+    install_requires=['requests'],
+    python_requires='>=3.4',
+    project_urls={
+        'Bug Reports': 'https://github.com/orgito/spectrum-client/issues',
+        'Source': 'https://github.com/orgito/spectrum-client',
+    },
+)

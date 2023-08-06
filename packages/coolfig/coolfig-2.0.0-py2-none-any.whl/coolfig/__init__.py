9e3a6e6e9a09d5baaa4d8314293bff7947337fcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:42 +0000

    Add package coolfig/2.0.0/coolfig-2.0.0-py2-none-any.whl

---
+++ packages/coolfig/coolfig-2.0.0-py2-none-any.whl/coolfig/__init__.py
@@ -0,0 +1,21 @@
+"""
+Support for working with different sources of configuration values.
+
+    class DefaultSettings(schema.Settings):
+        SECRET_KEY = schema.Value(str)
+        DEBUG = schema.Value(types.boolean, default=False)
+        DB_URL = schema.Value(types.sqlalchemy_url)
+        LOCALES = schema.Value(types.list(str))
+
+    settings = DefaultSettings(
+        providers.DictConfig(os.environ, prefix='MYAPP_'))
+"""
+from .schema import Value, Settings, computed_value, Dictionary, Secret
+from .providers import EnvConfig, DictConfig, SecretsConfig
+from .django import load_django_settings
+
+
+__version__ = '2.0.0'
+__url__ = 'https://github.com/GaretJax/coolfig'
+__all__ = ['Value', 'Dictionary', 'Secret', 'computed_value', 'Settings',
+           'EnvConfig', 'DictConfig', 'SecretsConfig', 'load_django_settings']

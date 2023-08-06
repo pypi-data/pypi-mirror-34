b185000d6efd2a0ad44c6199964e419ba02b661f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:28 +0000

    Add package radical-rpc/0.1.7/radical-rpc-0.1.7.tar.gz

---
+++ packages/radical-rpc/radical-rpc-0.1.7.tar.gz/radical-rpc-0.1.7/radical/meta.py
@@ -0,0 +1,19 @@
+VERSION = '0.1.7'
+
+_ESC = '\x1b['
+_RED = _ESC + '1;31m'
+BANNER_COLOR = _ESC + '1;31m'
+VERSION_COLOR = _ESC + '1;33m'
+PROP_COLOR = _ESC + '1;36m'
+SVC_COLOR = _ESC + '1;32m'
+ERROR_COLOR = _ESC + '1;31m'
+R = _ESC + '0m'
+
+FLAIR = f'''
+{BANNER_COLOR}░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░{R} %s
+{BANNER_COLOR}░░█▀▄░█▀█░█▀▄░▀█▀░█▀▀░█▀█░█░░░░░█▀▄░█▀█░█▀▀░░{R} %s
+{BANNER_COLOR}░░█▀▄░█▀█░█░█░░█░░█░░░█▀█░█░░░░░█▀▄░█▀▀░█░░░░{R} %s
+{BANNER_COLOR}░░▀░▀░▀░▀░▀▀░░▀▀▀░▀▀▀░▀░▀░▀▀▀░░░▀░▀░▀░░░▀▀▀░░{R} %s
+{BANNER_COLOR}░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░{R} %s
+
+'''

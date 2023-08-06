46682242ede18e05f2a19f2d765b75cb30ef7859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:03 +0000

    Add package ofxstatement-revolut/1.2.0/ofxstatement-revolut-1.2.0.tar.gz

---
+++ packages/ofxstatement-revolut/ofxstatement-revolut-1.2.0.tar.gz/ofxstatement-revolut-1.2.0/README.md
@@ -0,0 +1,13 @@
+ofxstatement-revolut
+====================
+
+This is a plugin for use with ofxstatement package. It implements
+a parser for the Revolut CSV-formatted bank statement.
+
+The CSV isn't very machine readable, so we need to do some ugly string
+parsing to figure out the different field values.
+
+Issue reports and pull requests welcome.
+
+This module is based on the Osuuspankki ofxstatement parser found at
+https://github.com/koodaamo/banking.statements.osuuspankki
---
+++ packages/ofxstatement-revolut/ofxstatement-revolut-1.2.0.tar.gz/ofxstatement-revolut-1.2.0/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Setup
+#Note: To publish new version: `./setup.py sdist upload`
+"""
+import os
+from setuptools import find_packages
+from distutils.core import setup
+
+version = "1.2.0"
+
+setup(name='ofxstatement-revolut',
+      version=version,
+      author="Miku Laitinen",
+      author_email="miku@avoin.systems",
+      url="https://github.com/mlaitinen/ofxstatement-revolut",
+      description=("Bank statement parser for Revolut"),
+      long_description=open("README.md").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      license="GPLv3",
+      keywords=["ofx", "ofxstatement", "banking", "statement", "revolut"],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Programming Language :: Python :: 3',
+          'Natural Language :: English',
+          'Topic :: Office/Business :: Financial :: Accounting',
+          'Topic :: Utilities',
+          'Environment :: Console',
+          'Operating System :: OS Independent',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'],
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=["ofxstatement", "ofxstatement.plugins"],
+      entry_points={
+          'ofxstatement': [
+              'revolut = ofxstatement.plugins.revolut:RevolutPlugin',
+          ]
+      },
+      install_requires=['ofxstatement'],
+      include_package_data=True,
+      zip_safe=True
+      )

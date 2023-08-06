3fd7016815d938cd2f81ee0fb7196154723ba8c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:39 +0000

    Add package htmlgen/1.1.0/htmlgen-1.1.0-py3-none-any.whl

---
+++ packages/htmlgen/htmlgen-1.1.0-py3-none-any.whl/htmlgen/structure.pyi
@@ -0,0 +1,25 @@
+from typing import Union,  Generator
+
+from htmlgen.element import Element
+
+
+class Section(Element):
+    def __init__(self) -> None: ...
+
+class Article(Element):
+    def __init__(self) -> None: ...
+
+class Navigation(Element):
+    def __init__(self) -> None: ...
+
+class Aside(Element):
+    def __init__(self) -> None: ...
+
+class Header(Element):
+    def __init__(self) -> None: ...
+
+class Footer(Element):
+    def __init__(self) -> None: ...
+
+class Heading(Element):
+    def __init__(self, level: int = ..., *content: Union[str, bytes, Generator]) -> None: ...

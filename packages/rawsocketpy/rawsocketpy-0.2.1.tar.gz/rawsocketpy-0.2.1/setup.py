91bdada1ef88aa8173c635aadfaa5896ade2c518
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:53 +0000

    Add package rawsocketpy/0.2.1/rawsocketpy-0.2.1.tar.gz

---
+++ packages/rawsocketpy/rawsocketpy-0.2.1.tar.gz/rawsocketpy-0.2.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='rawsocketpy',
+      version='0.2.1',
+      description='This library allows you to implement a custom layer 2 communication using raw sockets in Python 2 and Python 3, synchronous and asynchronous, with and without callbacks.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/AlexisTM/rawsocket_python',
+      author='AlexisTM',
+      author_email='alexis.paques@gmail.com',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      classifiers=(
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2",
+          "Development Status :: 4 - Beta",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: POSIX :: Linux",
+          "Topic :: Internet",
+          "Topic :: System :: Networking",
+          "Topic :: System :: Networking :: Monitoring",
+      ),
+      zip_safe=False)

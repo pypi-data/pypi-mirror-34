cf6fd05f622f2f8c55eedb4c4ffbce3bec685fdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:25 +0000

    Add package rada_student_package/0.2.dev0/rada_student_package-0.2.dev0.tar.gz

---
+++ packages/rada_student_package/rada_student_package-0.2.dev0.tar.gz/rada_student_package-0.2.dev0/rada_student_package/verh_rada.py
@@ -0,0 +1,13 @@
+from rada_student_package.rada_factories import UkraineRadaFactory, PolandRadaFactory
+
+simulation_config_file = {'rada': 'Poland'}
+
+
+def main():
+    if simulation_config_file['rada'] == 'Ukraine':
+        UkraineRadaFactory()().run()
+    else:
+        PolandRadaFactory()().run()
+
+if __name__=="__main__":
+    main()
---
+++ packages/rada_student_package/rada_student_package-0.2.dev0.tar.gz/rada_student_package-0.2.dev0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.2'
+
+setup(name='rada_student_package',
+      version=version,
+      description="No description",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Students',
+      author_email='',
+      url='',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )

680b681c73b8f371056400252a4ce85746aa8560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:04 +0000

    Add package tomputils/1.3.0/tomputils-1.3.0-py2.py3-none-any.whl

---
+++ packages/tomputils/tomputils-1.3.0-py2.py3-none-any.whl/tomputils/util/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+#  Purpose: utility functions
+#   Author: Tom Parker
+#
+# -----------------------------------------------------------------------------
+"""
+tomputils.util
+====================
+
+A few utility functions.
+
+:license:
+    CC0 1.0 Universal
+    http://creativecommons.org/publicdomain/zero/1.0/
+"""
+
+from tomputils.util import *
+
+__all__ = ['get_env_var']
+

bd872cdd41faa936285a7845d9520ac7af840ff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:19 +0000

    Add package cynpy/0.11/cynpy-0.11.tar.gz

---
+++ packages/cynpy/cynpy-0.11.tar.gz/cynpy-0.11/setup.py
@@ -0,0 +1,13 @@
+
+import setuptools
+
+setuptools.setup(
+      name='cynpy',
+      version='0.11',
+      description='Ray\'s public DVT utilities in Python 2.7',
+      url='http://canyon-semi.com.tw',
+      author='Ray Huang',
+      author_email='rayhuang@canyon-semi.com.tw',
+      license='Canyon Semiconductor Inc.',
+      packages=setuptools.find_packages(),
+      zip_safe=False)

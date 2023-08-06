bfe26e11639e420f49703f234c42efd6ab3f76a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:16 +0000

    Add package FC-CloudTrails-SDK/1.0.3/FC%20CloudTrails%20SDK-1.0.3.tar.gz

---
+++ packages/FC-CloudTrails-SDK/FC%20CloudTrails%20SDK-1.0.3.tar.gz/FC CloudTrails SDK-1.0.3/setup.py
@@ -0,0 +1,27 @@
+import os
+
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+VERSION = os.environ.get("TAG_VERSION")
+VERSION = VERSION[2:] if VERSION else "1.0.3"
+
+setuptools.setup(
+    name="FC CloudTrails SDK",
+    version=VERSION,
+    author="Yaisel Hurtado, Raydel Miranda, Yordano Gonzalez",
+    author_email="hurta2yaisel@gmail.com, raydel.miranda.gomez@gmail.com, yorda891216@gmail.com",
+    description="SDK for CloudTrails DevOps management",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://bitbucket.org/elasbit/fc-cloudtrails-sdk-py",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 2",
+        "Operating System :: OS Independent",
+    ),
+)

942b5bd3630cc66f55dec34b15e3debd35b768c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:46 +0000

    Add package django-admin-resumable-js/3.0/django-admin-resumable-js-3.0.tar.gz

---
+++ packages/django-admin-resumable-js/django-admin-resumable-js-3.0.tar.gz/django-admin-resumable-js-3.0/setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-admin-resumable-js',
+    version='3.0',
+    packages=['admin_resumable'],
+    include_package_data=True,
+    package_data={
+        'admin_resumable': [
+            'templates/admin_resumable/file_input.html',
+            'static/admin_resumable/js/resumable.js',
+        ]
+    },
+    license='MIT License',
+    description='A Django app for the uploading of large files from the django admin site.',
+    long_description=README,
+    url='https://github.com/jonatron/django-admin-resumable-js',
+    author='Jonatron',
+    author_email='jon4tron@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)

e739b2522671aa6197fb615669e5e486059ef762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:08 +0000

    Add package aiobreaker/1.0.0/aiobreaker-1.0.0.tar.gz

---
+++ packages/aiobreaker/aiobreaker-1.0.0.tar.gz/aiobreaker-1.0.0/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='aiobreaker',
+    version='1.0.0',
+    url='https://github.com/arlyon/aiobreaker',
+    license='BSD',
+    authors='Alexander Lyon',
+    author_email='arlyon@me.com',
+    description='Python implementation of the Circuit Breaker pattern.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    py_modules=['aiobreaker'],
+    install_requires=[],
+    tests_require=['redis', 'fakeredis', 'pytest', 'pytest-asyncio'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries',
+    ],
+)

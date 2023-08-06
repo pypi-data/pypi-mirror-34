434d81c4ddc9f13fa48f8bdd5792885f726fb277
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:49 +0000

    Add package mountainlab-pytools/0.6.2/mountainlab_pytools-0.6.2.tar.gz

---
+++ packages/mountainlab-pytools/mountainlab_pytools-0.6.2.tar.gz/mountainlab_pytools-0.6.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+setuptools.setup(
+    name="mountainlab_pytools",
+    version="0.6.2",
+    author="Jeremy Magland",
+    author_email="jmagland@flatironinstitute.org",
+    description="Tools for using MountainLab with python",
+    url="https://github.com/magland/mountainlab_pytools",
+    packages=setuptools.find_packages(),
+    package_data={
+        '': ['mlproc/*.js']
+    },
+    install_requires=[
+        'requests',
+        'numpy',
+        'numpydoc',
+        'ipython',
+        'vdom',
+        'ipywidgets',
+        'jp_proxy_widget'
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ),
+)

c6d9247bd3e2f2f717573da374eadd51e6cd51da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:59 +0000

    Add package yahoo-fin/0.7/yahoo_fin-0.7.tar.gz

---
+++ packages/yahoo-fin/yahoo_fin-0.7.tar.gz/yahoo_fin-0.7/setup.py
@@ -0,0 +1,17 @@
+
+
+from setuptools import setup
+
+setup(name='yahoo_fin',
+      version='0.7',
+      description="""Download historical stock prices, 
+                    fundamentals data, income statements, cash flows, 
+                    analyst info ,current cryptocurrency prices, 
+                    and more with yahoo_fin.
+                    """,
+      url='http://theautomatic.net/yahoo_fin-documentation/',
+      author='Andrew Treadway',
+      author_email='opensourcecoder11@gmail.com',
+      license='',
+      packages=['yahoo_fin'],
+      zip_safe=False)

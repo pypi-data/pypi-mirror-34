a566d4c1e6ed788a4a144046346a06a4819a78a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:49 +0000

    Add package urlquote/0.1.5/urlquote-0.1.5.tar.gz

---
+++ packages/urlquote/urlquote-0.1.5.tar.gz/urlquote-0.1.5/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+def build_native(spec):
+    # build rust library
+    build = spec.add_external_build(
+        cmd=['cargo', 'build', '--release'],
+        path='./rust'
+    )
+
+    spec.add_cffi_module(
+        module_path='urlquote._native',
+        dylib=lambda: build.find_dylib('urlquote', in_path='target/release'),
+        header_filename=lambda: build.find_header('native.h', in_path='.'),
+        rtld_flags=['NOW', 'NODELETE']
+    )
+
+setup(
+    name='urlquote',
+    packages=['urlquote'],
+    zip_safe=False,
+    platforms='any',
+    setup_requires=['milksnake', 'setuptools_scm'],
+    install_requires=['milksnake'],
+    use_scm_version=True,
+    url='https://github.com/blue-yonder/urlquote',
+    milksnake_tasks=[
+        build_native
+    ],
+    author='Blue Yonder',
+    author_email='oss@blue-yonder.com',
+    description='Fast quoting and unquoting of urls.',
+)

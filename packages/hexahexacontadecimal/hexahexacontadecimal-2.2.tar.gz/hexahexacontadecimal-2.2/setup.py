697f4b3311d27909a2093a77df013bfa3a80a26d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:04 +0000

    Add package hexahexacontadecimal/2.2/hexahexacontadecimal-2.2.tar.gz

---
+++ packages/hexahexacontadecimal/hexahexacontadecimal-2.2.tar.gz/hexahexacontadecimal-2.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+import pypandoc
+# This bit requires pandoc. On Mac OS X:
+#   brew install haskell-platform && cabal update && cabal install pandoc
+description = pypandoc.convert('README.md', 'rst', format='markdown')
+
+setuptools.setup(
+    name='hexahexacontadecimal',
+    version='2.2',
+    description='Encode and decode hexahexacontadecimal numbers, a compact number representation for URLs.',
+    author='Alexander Ljungberg',
+    author_email='aljungberg@slevenbits.com',
+    url='https://github.com/aljungberg/hexahexacontadecimal',
+    packages=['hexahexacontadecimal'],
+    keywords=["base64", "hexahexacontadecimal", "hhc", "base66", "url"],
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX",
+    ],
+    long_description=description
+)

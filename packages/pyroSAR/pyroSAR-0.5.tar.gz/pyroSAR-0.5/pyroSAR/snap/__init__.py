5df4eff73021cf1eccedf00faafbe33ce61ce2cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:34 +0000

    Add package pyroSAR/0.5/pyroSAR-0.5.tar.gz

---
+++ packages/pyroSAR/pyroSAR-0.5.tar.gz/pyroSAR-0.5/pyroSAR/snap/__init__.py
@@ -0,0 +1,4 @@
+from .util import geocode
+from .auxil import ExamineSnap, gpt
+
+snap_config = ExamineSnap()
---
+++ packages/pyroSAR/pyroSAR-0.5.tar.gz/pyroSAR-0.5/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+# Create .pyrosar in HOME - Directory
+directory = os.path.join(os.path.expanduser('~'), '.pyrosar')
+
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='pyroSAR',
+      packages=find_packages(),
+      include_package_data=True,
+      version='0.5',
+      description='a framework for large-scale SAR satellite data processing',
+      classifiers=[
+          'Programming Language :: Python',
+      ],
+      install_requires=['progressbar2',
+                        'pathos>=0.2',
+                        'numpy',
+                        'scoop',
+                        'spatialist>=0.2.4'],
+      url='https://github.com/johntruckenbrodt/pyroSAR.git',
+      author='John Truckenbrodt',
+      author_email='john.truckenbrodt@uni-jena.de',
+      license='MIT',
+      zip_safe=False,
+      long_description=long_description,
+      long_description_content_type='text/markdown')

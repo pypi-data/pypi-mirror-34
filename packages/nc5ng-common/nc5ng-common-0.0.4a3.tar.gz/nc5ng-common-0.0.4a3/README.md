ab629a51aac8f09219334c800fb2293c070ce506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:03 +0000

    Add package nc5ng-common/0.0.4a3/nc5ng-common-0.0.4a3.tar.gz

---
+++ packages/nc5ng-common/nc5ng-common-0.0.4a3.tar.gz/nc5ng-common-0.0.4a3/README.md
@@ -0,0 +1,2 @@
+# nc5ng-gmt
+nc5ng GMT Python Glue Library, part of `nc5ng` package 
---
+++ packages/nc5ng-common/nc5ng-common-0.0.4a3.tar.gz/nc5ng-common-0.0.4a3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+from os import environ
+VERSION= environ.get('NC5NG_VERSION', "0.0.4")
+PKG_INFO= {
+    'packages':['nc5ng.gmt'],
+    'name':"nc5ng-common",
+    'version':VERSION,
+    'description': "Python common packages for nc5ng",
+    'long_description': """Common utilities shared by other packages. 
+    installed by default with target `nc5ng`
+
+For More Information See: https://nc5ng.org/projects/nc5ng-python
+
+For Documentation See: https://docs.nc5ng.org/latest
+""",
+    'author':"Andrey Shmakov",
+    'author_email':"akshmakov@gmail.com",
+    'url':"https://nc5ng.org/projects/nc5ng-python",
+    'download_url':"https://github.com/nc5ng/nc5ng-python-common",
+    'install_requires':[
+        'numpy'
+        ],
+    'license':"MIT"
+}
+
+setup(**PKG_INFO)

f3a7ae6b46facd7e7980d7280a8f58d37f93c9ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:19 +0000

    Add package epic-kitchens/1.2.1/epic-kitchens-1.2.1.tar.gz

---
+++ packages/epic-kitchens/epic-kitchens-1.2.1.tar.gz/epic-kitchens-1.2.1/epic_kitchens/gulp/__init__.py
@@ -0,0 +1,8 @@
+""" Dataset Adapters for GulpIO.
+
+This module contains two adapters for 'gulping' both RGB and flow frames
+which can then be used with the ``EpicVideoDataset`` classes.
+"""
+
+from . import adapter
+

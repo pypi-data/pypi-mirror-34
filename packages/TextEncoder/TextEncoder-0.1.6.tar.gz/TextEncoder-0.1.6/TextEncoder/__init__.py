8bbc5f8de6115e5b32f33575c4f72a410050a330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:41 +0000

    Add package TextEncoder/0.1.6/TextEncoder-0.1.6.tar.gz

---
+++ packages/TextEncoder/TextEncoder-0.1.6.tar.gz/TextEncoder-0.1.6/TextEncoder/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 18/8/1 下午6:20
+# @Author  : Edward
+# @Site    :
+# @File    : __init__.py.py
+# @Software: PyCharm Community Edition
+
+from TextEncoder.transformer import Transformer

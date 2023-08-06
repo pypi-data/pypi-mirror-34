4261126679232a697293c0aaae023afe4554a6de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:27 +0000

    Add package proxy-requests/0.0.2/proxy_requests-0.0.2.tar.gz

---
+++ packages/proxy-requests/proxy_requests-0.0.2.tar.gz/proxy_requests-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="proxy_requests",
+    version="0.0.2",
+    author="James Loye Colley",
+    author_email="colleyloyejames@gmail.com",
+    description="A wrapper for the Python 3 requests module",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rootVIII/proxy_requests",
+    install_requires = ["requests"],
+    packages = setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)

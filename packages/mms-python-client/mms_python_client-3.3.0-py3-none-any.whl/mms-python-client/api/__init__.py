112c45c0d686f6dfddba02bfa8c88d9c0e403c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:21 +0000

    Add package mms-python-client/3.3.0/mms_python_client-3.3.0-py3-none-any.whl

---
+++ packages/mms-python-client/mms_python_client-3.3.0-py3-none-any.whl/mms-python-client/api/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from mms-python-client.api.artifact_api import ArtifactApi
+from mms-python-client.api.element_api import ElementApi
+from mms-python-client.api.org_api import OrgApi
+from mms-python-client.api.other_api import OtherApi
+from mms-python-client.api.project_api import ProjectApi
+from mms-python-client.api.ref_api import RefApi
+from mms-python-client.api.ticket_api import TicketApi

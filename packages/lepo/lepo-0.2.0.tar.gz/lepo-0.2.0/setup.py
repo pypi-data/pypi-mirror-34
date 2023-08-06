16a92a7e77e15050722c8a13415354d88bb1cc65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:51 +0000

    Add package lepo/0.2.0/lepo-0.2.0.tar.gz

---
+++ packages/lepo/lepo-0.2.0.tar.gz/lepo-0.2.0/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+if __name__ == '__main__':
+    setuptools.setup(
+        name='lepo',
+        version='0.2.0',
+        url='https://github.com/akx/lepo',
+        author='Aarni Koskela',
+        author_email='akx@iki.fi',
+        maintainer='Aarni Koskela',
+        maintainer_email='akx@iki.fi',
+        license='MIT',
+        install_requires=['Django', 'iso8601', 'jsonschema', 'marshmallow'],
+        packages=setuptools.find_packages('.', exclude=(
+            'lepo_tests',
+            'lepo_tests.*',
+        )),
+        include_package_data=True,
+    )

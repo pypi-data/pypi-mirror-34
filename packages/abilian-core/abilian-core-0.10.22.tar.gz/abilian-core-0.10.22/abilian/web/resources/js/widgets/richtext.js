aede6870a97167cc310cc7d86fd327abd24de825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:54 +0000

    Add package abilian-core/0.10.22/abilian-core-0.10.22.tar.gz

---
+++ packages/abilian-core/abilian-core-0.10.22.tar.gz/abilian-core-0.10.22/abilian/web/resources/js/widgets/richtext.js
@@ -0,0 +1,19 @@
+require(["AbilianWidget"], function(Abilian) {
+  "use strict";
+
+  function initRichTextWidget(params) {
+    var element = this;
+    // var name = this.attr["name"];
+    // var rows = parseInt(element.attr("rows")) || 10;
+    // var editor = null;
+
+    function setupCkEditor($, ckeditor) {
+      ckeditor.editorConfig = function(config) {};
+      ckeditor.replace(element.get(0));
+    }
+
+    require(["jquery", "ckeditor"], setupCkEditor);
+  }
+
+  Abilian.registerWidgetCreator("richtext", initRichTextWidget);
+});

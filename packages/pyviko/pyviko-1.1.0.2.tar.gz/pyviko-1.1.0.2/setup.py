780f8dee5b1845ac4a1f329a303a3b9643761917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:08 +0000

    Add package pyviko/1.1.0.2/pyviko-1.1.0.2.tar.gz

---
+++ packages/pyviko/pyviko-1.1.0.2.tar.gz/pyviko-1.1.0.2/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+
+#with open("README.md", "r") as fh:
+#    long_description = fh.read()
+
+setup(name='pyviko',
+	version='1.1.0.2',
+	description='Design knockout viruses in Python',
+	author='LJ Taylor',
+	author_email='l'+'taylor'+str(3+4)+'@'+'tu'+'lane.edu',
+	url='https://github.com/louiejtaylor/pyViKO',
+	packages=['pyviko'],
+	license='MIT License',
+	classifiers = ['Intended Audience :: Science/Research',
+				'Environment :: Console',
+				'Environment :: Web Environment',
+				'License :: OSI Approved :: MIT License',
+				'Programming Language :: Python :: 3',
+				'Topic :: Scientific/Engineering :: Bio-Informatics',],
+	py_modules = ['pyviko.core', 'pyviko.mutation', 'pyviko.restriction']
+     )

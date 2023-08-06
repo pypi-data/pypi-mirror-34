d4a43c85c6a5c8d22528907287bd6b9bb12e90f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:15 +0000

    Add package oceanspy/0.0.8/oceanspy-0.0.8-py2.py3-none-any.whl

---
+++ packages/oceanspy/oceanspy-0.0.8-py2.py3-none-any.whl/oceanspy/__init__.py
@@ -0,0 +1,9 @@
+__author__ = """Mattia Almansi"""
+__email__ = 'mattia.almansi@jhu.edu'
+__version__ = '0.0.8'
+
+from . import open_dataset
+from . import subsample
+from . import utils
+from . import compute
+from . import visualize

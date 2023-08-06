9427029baf3b75bad998415a9b0319b9dc91b3c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:30 +0000

    Add package qeGraphMaker/1.0.2rc0/qeGraphMaker-1.0.2rc0.tar.gz

---
+++ packages/qeGraphMaker/qeGraphMaker-1.0.2rc0.tar.gz/qeGraphMaker-1.0.2rc0/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="qeGraphMaker",
+    version="1.0.2.c",
+    author="Kevin Postlethwait",
+    author_email="kpostlet@redhat.com",
+    description="Create PDF Graphs from Google Sheets Data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KPostOffice/QETool_pip",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=[
+      'requests',
+      'matplotlib',
+    ],
+    entry_points = {
+        'console_scripts': [
+            'fileMaker=qeGraphMaker.createFile:main',
+            'graphGen=qeGraphMaker.xeqt:main'
+        ]
+    },
+)

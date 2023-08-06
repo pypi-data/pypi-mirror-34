60bd70bb76b1351158e2d6dfb773423553d5ad6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:57 +0000

    Add package consul-sd/0.1.2/consul-sd-0.1.2.tar.gz

---
+++ packages/consul-sd/consul-sd-0.1.2.tar.gz/consul-sd-0.1.2/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='consul-sd',
+    version='0.1.2',
+    author='Javier Cacheiro',
+    author_email='javier.cacheiro@gmail.com',
+    url='https://github.com/javicacheiro/consul-service-discovery',
+    license='MIT',
+    description='Python client for Consul Service Discovery API',
+    long_description=open('README.rst').read(),
+    py_modules=['consulsd'],
+    install_requires=['requests'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)

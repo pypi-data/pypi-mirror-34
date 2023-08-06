c74dac3a2e66ef8a82a92a04c9c07f72551999c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:59 +0000

    Add package ipromise/0.6/ipromise-0.6.tar.gz

---
+++ packages/ipromise/ipromise-0.6.tar.gz/ipromise-0.6/README.rst
@@ -0,0 +1,7 @@
+====
+ipromise
+====
+.. image:: https://badge.fury.io/py/ipromise.svg
+    :target: https://badge.fury.io/py/ipromise
+
+A Python base class that provides various decorators for specifying promises relating to inheritance.
---
+++ packages/ipromise/ipromise-0.6.tar.gz/ipromise-0.6/ipromise/ipromise.py
@@ -0,0 +1,73 @@
+__all__ = ['AbstractBaseClass']
+
+
+class AbstractBaseClass:
+
+    def __init_subclass__(cls):
+        super().__init_subclass__()
+
+        # Assign the set of abstract method names to __abstractmethods__.
+        # Python will not allow the class to be instantiated if this is not
+        # empty.
+        abstracts = {name
+                     for name, value in vars(cls).items()
+                     if getattr(value, "__isabstractmethod__", False)}
+        for base in cls.__bases__:
+            for name in getattr(base, "__abstractmethods__", set()):
+                value = getattr(cls, name, None)
+                if getattr(value, "__isabstractmethod__", False):
+                    abstracts.add(name)
+        cls.__abstractmethods__ = frozenset(abstracts)
+
+        # Check that each method M implementing a method in class C:
+        # * C is a base class, and
+        # * overrides a virtual method.
+        for name, value in vars(cls).items():
+            if not hasattr(value, "__implemented_from__"):
+                continue
+            interface_class = getattr(value, "__implemented_from__")
+            if not issubclass(cls, interface_class):
+                raise TypeError(f"Interface class {interface_class.__name__} "
+                                f"is not a base class of {cls.__name__}")
+
+        interface_class_of_name = {}
+        defined_in = {}
+        for ancestor in cls.__mro__:
+            for name, value in vars(ancestor).items():
+                if name not in interface_class_of_name:
+                    if not hasattr(value, "__implemented_from__"):
+                        continue
+                    interface_class = getattr(value, "__implemented_from__")
+                    interface_class_of_name[name] = interface_class
+                    defined_in[name] = ancestor
+                elif ancestor is not interface_class_of_name[name]:
+                    raise TypeError(
+                        f"{name} was marked implemented from ancestor "
+                        f"class {interface_class_of_name[name].__name__} "
+                        f"in {defined_in[name].__name__}, but it was "
+                        f"defined again in {ancestor.__name__}.")
+                elif not getattr(value, "__isabstractmethod__", False):
+                    raise TypeError(
+                        f"{name} was marked implemented from "
+                        f"ancestor class {ancestor.__name__}, "
+                        f"but it is not marked abstract there.")
+
+        # Check that each method M overriding a method in class C:
+        # * C is a base class, and
+        # * overrides a non-virtual method.
+        for name, value in vars(cls).items():
+            if not hasattr(value, "__overrides_from__"):
+                continue
+            interface_class = getattr(value, "__overrides_from__")
+            if not issubclass(cls, interface_class):
+                raise TypeError(f"Interface class {interface_class.__name__} "
+                                f"is not a base class of {cls.__name__}")
+            for base in cls.__bases__:
+                if not hasattr(base, name):
+                    continue
+                bases_value = getattr(base, name)
+                if getattr(bases_value, "__isabstractmethod__", False):
+                    raise TypeError(
+                        f"{name} is abstract in base class {base.__name__}, "
+                        f"so it should be marked as implemented rather than "
+                        f"overridden.")

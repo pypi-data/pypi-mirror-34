8441461ce1cacd333b2d69531dbe0c1902483652
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:03 +0000

    Add package rook/0.1.12/rook-0.1.12-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/rook/rook-0.1.12-cp36-cp36m-manylinux1_x86_64.whl/rook/lib/logger/backend_logging.py
@@ -0,0 +1,13 @@
+import os
+
+
+# note: the logger is writing to stack driver -> we read from stack driver
+# stackdriver -> pubsub (google service) -> fluentd from pubsub
+def add_backend_specific_logs(logging_values):
+    if os.environ.get('SYSTEM_APP') == "rookout_backend":
+        import flask
+        if flask.has_request_context():
+            request_url, request_id = flask.request.url, flask.request.headers.get("RUID")
+            logging_values["requestUrl"] = request_url
+            logging_values["requestId"] = request_id
+            logging_values["requestRemoteIp"] = flask.request.remote_addr

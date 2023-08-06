535750cf2a765308404f708d17b10891e2da8a68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:19 +0000

    Add package common_regex/1.0.2/common_regex-1.0.2.tar.gz

---
+++ packages/common_regex/common_regex-1.0.2.tar.gz/common_regex-1.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    author='Cameron Dart',
+    author_email='camerondart13@gmail.com',
+    long_description='Rerelease of stale package commonregex',
+    name='common_regex',
+    packages=find_packages(),
+    py_modules=['common_regex'],
+    url='https://github.com/SkamDart/CommonRegex',
+    version='1.0.2'
+)

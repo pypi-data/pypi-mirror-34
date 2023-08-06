d4e1b1f35c840f310e65f929dcb9a466409b63c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:19:32 +0000

    Add package mgwr/1.0.2/mgwr-1.0.2.tar.gz

---
+++ packages/mgwr/mgwr-1.0.2.tar.gz/mgwr-1.0.2/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+try:
+    from distutils.command.build_py import build_py_2to3 as build_py
+except ImportError:
+    from distutils.command.build_py import build_py
+
+Major = 1
+Feature = 0
+Bug = 2
+version = '%d.%d.%d' % (Major, Feature, Bug)
+
+setup(name='mgwr', #name of package
+      version=version,
+      description='multiscale geographically weighted regression', #short <80chr description
+      url='https://github.com/TaylorOshan/mgwr', #github repo
+      maintainer='Taylor M. Oshan', 
+      maintainer_email='tayoshan@gmail.com', 
+      test_suite = 'nose.collector',
+      tests_require=['nose'],
+      keywords='spatial statistics',
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: GIS',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4'
+        ],
+      license='3-Clause BSD',
+      packages=['mgwr'],
+      install_requires=['numpy', 'scipy', 'libpysal', 'spglm', 'spreg'],
+      zip_safe=False,
+      cmdclass = {'build.py':build_py})

001e01a7bc27746b940512169336fa4b4d2d6083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:20 +0000

    Add package mirtop/0.4.1a0/mirtop-0.4.1a0.tar.gz

---
+++ packages/mirtop/mirtop-0.4.1a0.tar.gz/mirtop-0.4.1a0/mirtop/gff/gff_versions.py
@@ -0,0 +1,4 @@
+current = "1.0"
+GFFv = {'1.0': ["iso_5p", "iso_3p", "iso_add", "iso_snp_seed",
+                "iso_snp_central_offset", "iso_snp_central",
+                "iso_snp_central_supp", "iso_snp", "NA"]}
